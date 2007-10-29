44c77b009d07fec74e5ddcaa64e461f2f610112e
Euke Castellano <ecastellano@esferalia.com>
2007-10-29 14:20:36 +0000

    Versión inicial

---
+++ trunk/aon-ui-cms/src/main/java/com/code/aon/ui/cms/controller/CMSLinesController.java
@@ -0,0 +1,13 @@
+package com.code.aon.ui.cms.controller;
+
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ui.form.LinesController;
+import com.icesoft.faces.component.ext.RowSelectorEvent;
+
+public class CMSLinesController extends LinesController {
+
+	@SuppressWarnings("unused")
+	public void onSelect(RowSelectorEvent event) throws ManagerBeanException {
+		super.onSelect(null);
+	}
+}
