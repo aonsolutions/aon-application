7982ad9a9d55369e35533af96dfd72bfc019ff27
Joseba Urkiri <jurkiri@esferalia.com>
2007-11-22 17:02:50 +0000

    Optimizacion del borrado de remesas

--- aon-finance/src/main/java/com/code/aon/finance/invoicing/FinanceTrackingWriter.java
+++ aon-finance/src/main/java/com/code/aon/finance/invoicing/FinanceTrackingWriter.java
@@ -40,7 +40,7 @@ public class FinanceTrackingWriter {
 			Criteria criteria = new Criteria();
 			criteria.addEqualExpression(financeTrackingBean.getFieldName(IFinanceAlias.FINANCE_TRACKING_TYPE), type);
 			criteria.addOrder(financeTrackingBean.getFieldName(IFinanceAlias.FINANCE_TRACKING_ID),false);
-			Iterator iter = financeTrackingBean.getList(criteria).iterator();
+			Iterator iter = financeTrackingBean.getList(criteria,0,1).iterator();
 			if(iter.hasNext()){
 				FinanceTracking tracking = (FinanceTracking)iter.next();
 				financeTrackingBean.remove(tracking);
