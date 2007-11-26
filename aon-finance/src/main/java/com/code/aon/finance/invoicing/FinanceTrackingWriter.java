7ec8c51dd05dc3b9c03cdad548009850afedff6c
Gorka Irazu <girazu@esferalia.com>
2007-11-26 16:09:47 +0000

    Corregido error que habia en el criteria al borrar el ultimo tracking remesado

--- aon-finance/src/main/java/com/code/aon/finance/invoicing/FinanceTrackingWriter.java
+++ aon-finance/src/main/java/com/code/aon/finance/invoicing/FinanceTrackingWriter.java
@@ -38,6 +38,7 @@ public class FinanceTrackingWriter {
     	try {
 			IManagerBean financeTrackingBean = BeanManager.getManagerBean(FinanceTracking.class);
 			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(financeTrackingBean.getFieldName(IFinanceAlias.FINANCE_TRACKING_FINANCE_ID), finance.getId());
 			criteria.addEqualExpression(financeTrackingBean.getFieldName(IFinanceAlias.FINANCE_TRACKING_TYPE), type);
 			criteria.addOrder(financeTrackingBean.getFieldName(IFinanceAlias.FINANCE_TRACKING_ID),false);
 			Iterator iter = financeTrackingBean.getList(criteria,0,1).iterator();
