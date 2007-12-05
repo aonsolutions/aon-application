8914f254d34a36eb6ac6ceb758f20507302afb9d
Aimar Tellitu <atellitu@esferalia.com>
2007-12-05 17:18:04 +0000

    *** empty log message ***

--- trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/confirmButton/ConfirmButtonHandler.java
+++ trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/confirmButton/ConfirmButtonHandler.java
@@ -1,7 +1,10 @@
 package com.code.aon.faces.component.richfaces.confirmButton;
 
+import java.net.URL;
+
 import javax.el.ValueExpression;
 import javax.el.VariableMapper;
+import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 
 import com.code.aon.faces.component.AonComponentHandler;
@@ -23,7 +26,7 @@ public class ConfirmButtonHandler extends AonComponentHandler implements
 		ILookupTags {
 
 	private static final String CONFIRM_ID = "confirmId";
-	
+
 	private static final String CONFIRM_ACTION = "confirmAction";
 
 	private static final String CONFIRM_ACTION_LISTENER = "confirmActionListener";
@@ -31,11 +34,11 @@ public class ConfirmButtonHandler extends AonComponentHandler implements
 	private static final String CONFIRM_TITLE = "confirmTitle";
 
 	private static final String CONFIRM_MESSAGE = "confirmMessage";
-	
+
 	private static final String IMMEDIATE = "immediate";
 
 	private static final String TEMPLATE_PATH = "com/code/aon/faces/component/richfaces/confirmButton/";
-	
+
 	private static final String TEMPLATE = TEMPLATE_PATH + "template.xhtml";
 
 	private TagAttribute titleTag;
@@ -61,26 +64,21 @@ public class ConfirmButtonHandler extends AonComponentHandler implements
 		mrs.ignore(CONFIRM_TITLE).ignore(CONFIRM_MESSAGE);
 		return mrs;
 	}
-	
-	private String getPanelId( FaceletContext ctx ) {
-		return getId(ctx) + "ModelPanel";	
+
+	private String getPanelId(FaceletContext ctx) {
+		return getId(ctx) + "ModelPanel";
 	}
 
-	private String getShowScript( FaceletContext ctx ) {
+	private String getShowScript(FaceletContext ctx) {
 		return "Richfaces.showModalPanel('" + getPanelId(ctx) + "');";
 	}
-	
+
 	@Override
 	protected void setAttributes(FaceletContext ctx, Object instance) {
 		super.setAttributes(ctx, instance);
 		UIComponent button = (UIComponent) instance;
-		UIComponentTagUtils.setStringProperty(ctx.getFacesContext(), button, "onclick", getShowScript(ctx) );		
-	}
-
-	@Override
-	protected void onComponentPopulated(FaceletContext ctx, UIComponent c,
-			UIComponent parent) {
-		insertTemplate(ctx, parent);
+		UIComponentTagUtils.setStringProperty(ctx.getFacesContext(), button,
+				"onclick", getShowScript(ctx));
 	}
 
 	private ValueExpression getValueExpression(FaceletContext ctx,
@@ -88,33 +86,58 @@ public class ConfirmButtonHandler extends AonComponentHandler implements
 		return tag.getValueExpression(ctx, Object.class);
 	}
 
-	public ValueExpression getMethodExpression(FaceletContext ctx, String name, Class type, Class[] paramTypes ) {
-		return FaceletUtil.getMethodExpression(ctx, getAttribute(name), type, paramTypes);
+	public ValueExpression getMethodExpression(FaceletContext ctx, String name,
+			Class type, Class[] paramTypes) {
+		return FaceletUtil.getMethodExpression(ctx, getAttribute(name), type,
+				paramTypes);
 	}
-	
-	private void insertTemplate(FaceletContext ctx, UIComponent parent) {
-		VariableMapper newMapper = new VariableMapperWrapper(ctx.getVariableMapper());
-		TagAttribute tagImmediate = getAttribute(IMMEDIATE); 
-		if( tagImmediate != null){
-			newMapper.setVariable(IMMEDIATE, getValueExpression(ctx, tagImmediate) );
+
+	private void addAttribues( FaceletContext ctx ) {
+		VariableMapper mapper = ctx.getVariableMapper();
+		TagAttribute tagImmediate = getAttribute(IMMEDIATE);
+		if (tagImmediate != null) {
+			mapper.setVariable(IMMEDIATE, getValueExpression(ctx,
+					tagImmediate));
 		} else {
-			newMapper.setVariable(IMMEDIATE, ctx.getExpressionFactory().createValueExpression(ctx, "false", Boolean.class));
+			mapper.setVariable(IMMEDIATE, ctx.getExpressionFactory()
+					.createValueExpression(ctx, "false", Boolean.class));
 		}
-		ValueExpression id = ctx.getExpressionFactory().createValueExpression(ctx, getPanelId(ctx), String.class);
-		newMapper.setVariable(CONFIRM_ID, id);
-		newMapper.setVariable(CONFIRM_TITLE, getValueExpression(ctx, titleTag));
-		newMapper.setVariable(CONFIRM_TITLE, getValueExpression(ctx, titleTag));
-		newMapper.setVariable(CONFIRM_MESSAGE, getValueExpression(ctx, messageTag));
-		ValueExpression action = getMethodExpression(ctx, CONFIRM_ACTION, String.class, FaceletUtil.ACTION_SIG);
-		if ( action == null ) {
-			action = FaceletUtil.getMethodEmptyExpression(ctx, CONFIRM_ACTION, String.class, FaceletUtil.ACTION_SIG);
+		ValueExpression id = ctx.getExpressionFactory().createValueExpression(
+				ctx, getPanelId(ctx), String.class);
+		mapper.setVariable(CONFIRM_ID, id);
+		mapper.setVariable(CONFIRM_TITLE, getValueExpression(ctx, titleTag));
+		mapper.setVariable(CONFIRM_TITLE, getValueExpression(ctx, titleTag));
+		mapper.setVariable(CONFIRM_MESSAGE, getValueExpression(ctx,
+				messageTag));
+		ValueExpression action = getMethodExpression(ctx, CONFIRM_ACTION,
+				String.class, FaceletUtil.ACTION_SIG);
+		if (action == null) {
+			action = FaceletUtil.getMethodEmptyExpression(ctx, CONFIRM_ACTION,
+					String.class, FaceletUtil.ACTION_SIG);
 		}
-		newMapper.setVariable( CONFIRM_ACTION, action );
-		ValueExpression al = getMethodExpression(ctx, CONFIRM_ACTION_LISTENER, null, FaceletUtil.ACTION_LISTENER_SIG);
-		if ( al != null ) {
-			newMapper.setVariable( CONFIRM_ACTION_LISTENER, al );				
+		mapper.setVariable(CONFIRM_ACTION, action);
+		ValueExpression al = getMethodExpression(ctx, CONFIRM_ACTION_LISTENER,
+				null, FaceletUtil.ACTION_LISTENER_SIG);
+		if (al != null) {
+			mapper.setVariable(CONFIRM_ACTION_LISTENER, al);
 		}
-		insertTemplate(ctx, parent, getTemplate(TEMPLATE), newMapper );
 	}
-	
+
+	@Override
+	protected void applyNextHandler(FaceletContext ctx, UIComponent component) {
+		URL path = getTemplate(TEMPLATE);
+		VariableMapper orig = ctx.getVariableMapper();
+		ctx.setVariableMapper(new VariableMapperWrapper(orig));
+		try {
+			super.nextHandler.apply(ctx, component);
+			addAttribues(ctx);
+			ctx.includeFacelet(component, path );
+		} catch (Exception e) {
+			throw new FacesException("UIInclude component "
+					+ component.getClientId(ctx.getFacesContext())
+					+ " could't include page with path " + path, e);
+		} finally {
+			ctx.setVariableMapper(orig);
+		}
+	}
 }
