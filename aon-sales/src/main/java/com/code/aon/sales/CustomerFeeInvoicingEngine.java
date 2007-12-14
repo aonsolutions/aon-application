a95151191957a4aadb4dbb251d8f64b501e453dc
Joseba Urkiri <jurkiri@esferalia.com>
2007-12-14 10:20:30 +0000

    Cuando se factura a un cliente que sea grupo, se factura tambien a los clientes pertenecientes a ese grupo

--- aon-sales/src/main/java/com/code/aon/sales/CustomerFeeInvoicingEngine.java
+++ aon-sales/src/main/java/com/code/aon/sales/CustomerFeeInvoicingEngine.java
@@ -60,6 +60,7 @@ public class CustomerFeeInvoicingEngine implements IInvoicingEngine {
 		try {
 			Criteria criteria = createInvoicingCriteria(params);
 			List invoicedList = invoiceGroupFees(criteria, params);
+			criteria = completeCriteriaWithCustomerData(criteria, params);
 			List feeList = obtainFeeList(updateCriteria(criteria,invoicedList));
 			invoiceFees(feeList, params);
 		} catch (ExpressionException e) {
@@ -67,14 +68,19 @@ public class CustomerFeeInvoicingEngine implements IInvoicingEngine {
 		}
 	}
 
+	private Criteria completeCriteriaWithCustomerData(Criteria criteria,InvoicingParameters params) throws ManagerBeanException {
+		IManagerBean customerFeeBean = BeanManager.getManagerBean(CustomerFee.class);
+		if(params.getCustomerId() != null){
+			criteria.addEqualExpression(customerFeeBean.getFieldName(ISalesAlias.CUSTOMER_FEE_CUSTOMER_ID), params.getCustomerId());
+		}
+		return criteria;
+	}
+
 	private Criteria createInvoicingCriteria(InvoicingParameters params) throws ManagerBeanException {
 		IManagerBean customerFeeBean = BeanManager.getManagerBean(CustomerFee.class);
 		Criteria criteria = new Criteria();
 		Expression dateExpression = createFromToExpression(params.getMonth(), params.getYear());
 		criteria.addExpression(dateExpression);
-		if(params.getCustomerId() != null){
-			criteria.addEqualExpression(customerFeeBean.getFieldName(ISalesAlias.CUSTOMER_FEE_CUSTOMER_ID), params.getCustomerId());
-		}
 		if(params.getSecurityLevel() != null){
 			criteria.addEqualExpression(customerFeeBean.getFieldName(ISalesAlias.CUSTOMER_FEE_SECURITY_LEVEL), params.getSecurityLevel());
 		}
@@ -147,21 +153,24 @@ public class CustomerFeeInvoicingEngine implements IInvoicingEngine {
 			exp = ExpressionUtilities.getOrExpression(exp, ExpressionUtilities.getEqualExpression(customerFeeBean.getFieldName(ISalesAlias.CUSTOMER_FEE_CUSTOMER_ID), detail.getChild().getId()));
 		}
 		groupCriteria.addExpression(ExpressionUtilities.getAndExpression(criteria.getExpression(), exp));
-		invoicedList.add(group.getParent().getId());
-		Invoice invoice = createInvoice(group, params);
-		getInvoicingDAO().insertInvoice(invoice);
-		getInvoicingFeedBack().addMessage("\t" + "Invoice: " + invoice.getSeries() + "/" + invoice.getNumber());
-		params.setNumber(params.getNumber()+1);
+		// Cuotas tanto del padre como de los hijos
 		Iterator feeIter = customerFeeBean.getList(groupCriteria).iterator();
-		while(feeIter.hasNext()){
-			CustomerFee fee = (CustomerFee)feeIter.next();
-			InvoiceDetail invoiceDetail = createInvoiceDetail(fee, invoice, params);
-			getInvoicingDAO().insertInvoiceDetail(invoiceDetail);
-			getInvoicingDAO().updateSource(fee);
-			getInvoicingFeedBack().addMessage("\t \t" + "InvoiceDetail: " + invoiceDetail.getDescription() + " price= " + invoiceDetail.getTaxableBase());			
-			invoicedList.add(fee.getCustomer().getId());
+		if(feeIter.hasNext()){
+			invoicedList.add(group.getParent().getId());
+			Invoice invoice = createInvoice(group, params);
+			getInvoicingDAO().insertInvoice(invoice);
+			getInvoicingFeedBack().addMessage("\t" + "Invoice: " + invoice.getSeries() + "/" + invoice.getNumber());
+			params.setNumber(params.getNumber()+1);
+			while(feeIter.hasNext()){
+				CustomerFee fee = (CustomerFee)feeIter.next();
+				InvoiceDetail invoiceDetail = createInvoiceDetail(fee, invoice, params);
+				getInvoicingDAO().insertInvoiceDetail(invoiceDetail);
+				getInvoicingDAO().updateSource(fee);
+				getInvoicingFeedBack().addMessage("\t \t" + "InvoiceDetail: " + invoiceDetail.getDescription() + " price= " + invoiceDetail.getTaxableBase());			
+				invoicedList.add(fee.getCustomer().getId());
+			}
+			getInvoicingDAO().createFinances(invoice);
 		}
-		getInvoicingDAO().createFinances(invoice);
 		return invoicedList;
 	}
 
