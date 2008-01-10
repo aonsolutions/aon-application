2e1e20aeca1e7f5fc7f9cd175e49e6905f3d238b
Joseba Urkiri <jurkiri@esferalia.com>
2008-01-10 10:24:50 +0000

    Añadida etiqueta @Temporal para evitar que la consulta contra la BBDD utilizase tambien la hora

--- trunk/aon-project/src/main/java/com/code/aon/project/DailyTracking.java
+++ trunk/aon-project/src/main/java/com/code/aon/project/DailyTracking.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.code.aon.common.ITransferObject;
 import com.code.aon.config.User;
@@ -58,6 +60,7 @@ public class DailyTracking implements ITransferObject {
 	}
 
     @Column(name="tracking_date", nullable=false)
+    @Temporal(TemporalType.DATE)
     public Date getTrackingDate() {
         return trackingDate;
     }
