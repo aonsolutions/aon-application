489fa7b65aeb920a66f4648a1cb85308faa53ee2
Inigo Gayarre <igayarre@esferalia.com>
2008-01-16 15:01:33 +0000

    Problema si viene a null corregido

--- trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/converter/LongStringConverter.java
+++ trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/converter/LongStringConverter.java
@@ -22,7 +22,8 @@ public class LongStringConverter implements Converter {
 
     public String getAsString(FacesContext context, UIComponent component,
                               Object value) throws ConverterException {
-
+    	if (value == null)
+    			return "";
 	    if(((String)value).length() > 120)
         return breakLines((String)value, true);
         else
