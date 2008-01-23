21a90a7506f00f515f6dce9d8e7b6b6635e624ef
Joseba Urkiri <jurkiri@esferalia.com>
2008-01-23 10:07:31 +0000

    soporte para poder importar el presupuesto en cualquier momento, no solo cuando se esta creando el albaran

--- aon-ui-tas-delivery/src/main/java/com/code/aon/ui/tasDelivery/event/DeliveryTasListener.java
+++ aon-ui-tas-delivery/src/main/java/com/code/aon/ui/tasDelivery/event/DeliveryTasListener.java
@@ -53,8 +53,8 @@ public class DeliveryTasListener extends ControllerAdapter {
             tasDelivery.setOffer(obtainOffer(deliveryController.getOfferId()));
 			try {
 				IManagerBean tasDeliveryBean = BeanManager.getManagerBean(TasDelivery.class);
-				tasDeliveryBean.insert(tasDelivery);
-				closeStatuses(tasDelivery.getSupportOrder(), tasDelivery.getOffer());
+				tasDelivery = (TasDelivery)tasDeliveryBean.insert(tasDelivery);
+				closeStatuses(tasDelivery);
 				updateSupportOrderEmployee(tasDelivery.getSupportOrder(), deliveryController.getEmployeeId());
 			} catch (ManagerBeanException e) {
 				LOGGER.log(Level.SEVERE, "Error inserting TasDelivery for Delivery with id= " + ((Delivery)deliveryController.getTo()).getId(), e);
@@ -71,18 +71,42 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 */
 	@Override
 	public void afterBeanUpdated(ControllerEvent event) throws ControllerListenerException {
-		DeliveryController deliveryController = (DeliveryController)event.getController();
-		if(deliveryController.getSupportOrderId() != null){
-			SupportOrder supportOrder = obtainSupportOrder(deliveryController.getSupportOrderId());
-			if(supportOrder.getEmployee() == null && deliveryController.getEmployeeId() != null) {
-				updateSupportOrderEmployee(supportOrder, deliveryController.getEmployeeId());
+		try {
+			DeliveryController deliveryController = (DeliveryController)event.getController();
+			if(deliveryController.getSupportOrderId() != null){
+				SupportOrder supportOrder = obtainSupportOrder(deliveryController.getSupportOrderId());
+				if(supportOrder.getEmployee() == null && deliveryController.getEmployeeId() != null) {
+					updateSupportOrderEmployee(supportOrder, deliveryController.getEmployeeId());
+				}
+				if(supportOrder.getEmployee() != null && !supportOrder.getEmployee().getId().equals(deliveryController.getEmployeeId())){
+					updateSupportOrderEmployee(supportOrder, deliveryController.getEmployeeId());
+				}
 			}
-			if(supportOrder.getEmployee() != null && !supportOrder.getEmployee().getId().equals(deliveryController.getEmployeeId())){
-				updateSupportOrderEmployee(supportOrder, deliveryController.getEmployeeId());
+			if(deliveryController.getSupportOrderId() != null && deliveryController.getOfferId() != null){
+				TasDelivery tasDelivery = obtainTasDelivery(deliveryController.getSupportOrderId(), ((Delivery)deliveryController.getTo()).getId());
+				tasDelivery.setOffer(obtainOffer(deliveryController.getOfferId()));
+				IManagerBean tasDeliveryBean = BeanManager.getManagerBean(TasDelivery.class);
+				tasDeliveryBean.update(tasDelivery);
+				closeStatuses(tasDelivery);
 			}
+		} catch (ManagerBeanException e) {
+			throw new ControllerListenerException(e);
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
+	private TasDelivery obtainTasDelivery(Integer supportOrderId, Integer deliveryId) throws ManagerBeanException {
+		IManagerBean tasDeliveryBean = BeanManager.getManagerBean(TasDelivery.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(tasDeliveryBean.getFieldName(ITasDeliveryAlias.TAS_DELIVERY_SUPPORT_ORDER_ID), supportOrderId);
+		criteria.addEqualExpression(tasDeliveryBean.getFieldName(ITasDeliveryAlias.TAS_DELIVERY_DELIVERY_ID), deliveryId);
+		Iterator iter = tasDeliveryBean.getList(criteria, 0, 1).iterator();
+		if(iter.hasNext()){
+			return (TasDelivery)iter.next();
+		}
+		return null;
+	}
+
 	/**
 	 * Before bean removed. Removes the related <code>TasDelivery</code> if necessary
 	 * 
@@ -91,6 +115,7 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 * @throws ControllerListenerException the controller listener exception
 	 */
 	@Override
+	@SuppressWarnings("unchecked")
 	public void beforeBeanRemoved(ControllerEvent event) throws ControllerListenerException {
 		Delivery delivery = (Delivery)((DeliveryController)event.getController()).getTo();
 		try {
@@ -116,6 +141,7 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 * @throws ControllerListenerException the controller listener exception
 	 */
 	@Override
+	@SuppressWarnings("unchecked")
 	public void afterBeanSelected(ControllerEvent event) throws ControllerListenerException {
         DeliveryController deliveryController = (DeliveryController)event.getController();
         deliveryController.setSupportOrderId(null);
@@ -184,6 +210,7 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 * 
 	 * @return the support order
 	 */
+	@SuppressWarnings("unchecked")
 	private SupportOrder obtainSupportOrder(Integer supportOrderId) {
 		try {
 			IManagerBean supportOrderBean = BeanManager.getManagerBean(SupportOrder.class);
@@ -206,6 +233,7 @@ public class DeliveryTasListener extends ControllerAdapter {
      * 
      * @return the offer
      */
+	@SuppressWarnings("unchecked")
     private Offer obtainOffer(Integer offerId) {
         if (offerId != null) {
             try {
@@ -229,23 +257,28 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 * 
 	 * @param supportOrder the support order
 	 */
-	private void closeStatuses(SupportOrder supportOrder, Offer offer) {
+    @SuppressWarnings("unchecked")
+    private void closeStatuses(TasDelivery tasDelivery) {
 		try {
 			IManagerBean supportOrderBean = BeanManager.getManagerBean(SupportOrder.class);
-			supportOrder.setStatus(SupportOrderStatus.FINISHED);
-			supportOrderBean.update(supportOrder);
+			if(tasDelivery.getOffer()== null){
+				tasDelivery.getSupportOrder().setStatus(SupportOrderStatus.ACTIVE);
+			}else{
+				tasDelivery.getSupportOrder().setStatus(SupportOrderStatus.FINISHED);
+			}
+			supportOrderBean.update(tasDelivery.getSupportOrder());
 
             IManagerBean offerBean = BeanManager.getManagerBean(Offer.class);
-            if(offer != null) {
-				offer.setStatus(OfferStatus.PROCESSED);
-				offerBean.update(offer);
+            if(tasDelivery.getOffer() != null) {
+            	tasDelivery.getOffer().setStatus(OfferStatus.PROCESSED);
+				offerBean.update(tasDelivery.getOffer());
 			}
 
             IManagerBean tasOfferBean = BeanManager.getManagerBean(TasOffer.class);
             Criteria criteria = new Criteria();
-            criteria.addEqualExpression(tasOfferBean.getFieldName(ITasCommercialAlias.TAS_OFFER_SUPPORT_ORDER_ID), supportOrder.getId());
-            if (offer != null) {
-                Expression offerExpr = ExpressionUtilities.getNotEqualExpression(tasOfferBean.getFieldName(ITasCommercialAlias.TAS_OFFER_OFFER_ID), offer.getId());
+            criteria.addEqualExpression(tasOfferBean.getFieldName(ITasCommercialAlias.TAS_OFFER_SUPPORT_ORDER_ID), tasDelivery.getSupportOrder().getId());
+            if (tasDelivery.getOffer() != null) {
+                Expression offerExpr = ExpressionUtilities.getNotEqualExpression(tasOfferBean.getFieldName(ITasCommercialAlias.TAS_OFFER_OFFER_ID), tasDelivery.getOffer().getId());
                 criteria.addExpression(offerExpr);
             }
             Iterator iterator = tasOfferBean.getList(criteria).iterator();
@@ -255,7 +288,7 @@ public class DeliveryTasListener extends ControllerAdapter {
                 offerBean.update(tasOffer.getOffer());
             }
         } catch (ManagerBeanException e) {
-			LOGGER.log(Level.SEVERE, "Error updating statuses for SupportOrder with id= " + supportOrder.getId(), e);
+			LOGGER.log(Level.SEVERE, "Error updating statuses for SupportOrder with id= " + tasDelivery.getSupportOrder().getId(), e);
 		}
 	}
 	
@@ -282,6 +315,7 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 * 
 	 * @return the employee
 	 */
+	@SuppressWarnings("unchecked")
 	private Employee obtainEmployee(Integer employeeId) {
 		try {
 			IManagerBean employeeBean = BeanManager.getManagerBean(Employee.class);
@@ -303,6 +337,7 @@ public class DeliveryTasListener extends ControllerAdapter {
 	 * 
 	 * @param supportOrder the support order
 	 */
+	@SuppressWarnings("unchecked")
 	private void setStatusesPending(SupportOrder supportOrder) {
 		try {
 			IManagerBean supportOrderBean = BeanManager.getManagerBean(SupportOrder.class);
