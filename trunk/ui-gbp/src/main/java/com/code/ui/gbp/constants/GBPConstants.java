f0eef10e884775f1f5f19673b3f4e5a26ebd95c4
Joseba Urkiri <jurkiri@esferalia.com>
2008-02-06 16:48:34 +0000

    version inicial

---
+++ trunk/ui-gbp/src/main/java/com/code/ui/gbp/constants/GBPConstants.java
@@ -0,0 +1,8 @@
+package com.code.ui.gbp.constants;
+
+public class GBPConstants {
+
+	public static final String GBP_MANAGER_ROLE_NAME = "Manager";
+
+	public static final String GBP_SUPERVISOR_ROLE_NAME = "Supervisor";
+}
