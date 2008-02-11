cf0633f6aeab33b9ec1497d7b22d44a91f0e0a80
Joseba Urkiri <jurkiri@esferalia.com>
2008-02-11 11:34:39 +0000

    desde el menu solo se cargan los pendientes

--- ui-gbp/src/main/java/com/code/ui/gbp/controller/ProFormaInvoiceController.java
+++ ui-gbp/src/main/java/com/code/ui/gbp/controller/ProFormaInvoiceController.java
@@ -6,18 +6,19 @@ import javax.faces.event.ValueChangeEvent;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ui.form.BasicController;
 import com.code.gbp.dao.IGBPAlias;
-import com.code.gbp.enumeration.OfferStatus;
+import com.code.gbp.enumeration.ProFormaInvoiceStatus;
 
-public class OfferController extends BasicController {
+public class ProFormaInvoiceController extends BasicController {
 
 	public void addStatusExpression(ValueChangeEvent event) throws ManagerBeanException{
 		if(event.getNewValue() != null){
-			getCriteria().addEqualExpression(getFieldName(IGBPAlias.OFFER_STATUS), event.getNewValue());
+			getCriteria().addEqualExpression(getFieldName(IGBPAlias.PRO_FORMA_INVOICE_STATUS), event.getNewValue());
 		}
 	}
+	
 	public void onInitialModel(ActionEvent event) throws ManagerBeanException{
 		clearCriteria();
-		getCriteria().addEqualExpression(getFieldName(IGBPAlias.OFFER_STATUS), OfferStatus.PENDING);
+		getCriteria().addEqualExpression(getFieldName(IGBPAlias.PRO_FORMA_INVOICE_STATUS), ProFormaInvoiceStatus.PENDING);
 		this.onSearch(event);
 	}
 }
