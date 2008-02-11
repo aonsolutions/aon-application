14adb9d8f287a92ecc92340f14c8d33acb5cdd9e
Inigo Gayarre <igayarre@esferalia.com>
2008-02-11 15:26:51 +0000

    Generacion de links

--- trunk/aon-ui-cms/src/main/java/com/code/aon/ui/cms/velocity/LinkGenerator.java
+++ trunk/aon-ui-cms/src/main/java/com/code/aon/ui/cms/velocity/LinkGenerator.java
@@ -1,12 +1,17 @@
 package com.code.aon.ui.cms.velocity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
+import com.code.aon.cms.FaqConfig;
 import com.code.aon.cms.GenericPage;
 import com.code.aon.cms.GenericPageDetail;
 import com.code.aon.cms.LinkCategory;
 import com.code.aon.cms.LinkCategoryDetail;
+import com.code.aon.cms.LinkConfig;
+import com.code.aon.cms.Section;
 import com.code.aon.cms.dao.ICMSAlias;
 import com.code.aon.cms.enumeration.Templates;
 import com.code.aon.common.BeanManager;
@@ -14,6 +19,7 @@ import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
+import com.code.aon.ui.cms.controller.GeneratorConfigController;
 import com.code.aon.ui.cms.util.ControllerUtil;
 import com.code.aon.ui.cms.util.VelocityUtil;
 import com.code.aon.ui.cms.velocity.attribute.GenericPageHandler;
@@ -24,6 +30,7 @@ public class LinkGenerator extends Generator {
 	public static void generate(VelocityUtil vu) {
 		ArrayList<LinkCategoryHandler> lchList;
 		List<ITransferObject> linkCategoryDetailList;
+		HashMap categoryMap = new HashMap<Section, List>();
 		try {
 			lchList = new ArrayList<LinkCategoryHandler>(); 
 			IManagerBean bean = BeanManager.getManagerBean(LinkCategoryDetail.class);
@@ -31,6 +38,7 @@ public class LinkGenerator extends Generator {
 			criteria.addEqualExpression(bean.getFieldName(ICMSAlias.LINK_CATEGORY_DETAIL_LANGUAGE_ID), ControllerUtil.getCurrentLanguage().getId());
 			linkCategoryDetailList = (List<ITransferObject>)bean.getList(criteria);
 			LinkCategoryDetail lcd;
+			List l;
 			for (int i=0; i < linkCategoryDetailList.size(); i++) {
 				lcd = (LinkCategoryDetail)linkCategoryDetailList.get(i);
 				if (lcd.getLinkCategory().isActive()) {
@@ -38,12 +46,33 @@ public class LinkGenerator extends Generator {
 					lchList.add(lch);
 					vu.put("link_category", lch);
 					vu.addMessage(" Generando categoria Link '" + lcd.getLinkCategory().getAlias() + "'.", VelocityUtil.INFO);
+					CommonGenerator.getCommonGenerator().chargeContext(vu, lcd.getLinkCategory().getSection());
 					generate(vu, Templates.LINK, lcd.getLinkCategory().getAlias());
 					vu.remove("link_category");
+					
+					l = (List) categoryMap.get(lcd.getLinkCategory().getSection());
+					if (l == null)
+						l = new ArrayList<LinkCategoryHandler>();
+					l.add(lch);
+					categoryMap.put(lcd.getLinkCategory().getSection(),l);
 				}
 			}
+			Iterator<Section> iter = categoryMap.keySet().iterator();
+			Section key;
+			ArrayList<LinkCategoryHandler> linkCategoryHandlerSet;
+			while (iter.hasNext()){
+				key = iter.next();
+				linkCategoryHandlerSet = (ArrayList<LinkCategoryHandler>)categoryMap.get(key);
+				vu.put("link_categories", linkCategoryHandlerSet);
+				vu.addMessage(" Generando listado categoria seccion Link.", VelocityUtil.INFO);
+				CommonGenerator.getCommonGenerator().chargeContext(vu, key);
+				generate(vu, Templates.LINK, LINK_CATEGORY_BY_SECTION_PAGE + key.getId());
+				vu.remove("link_categories");
+			}
+			iter = null;
 			vu.put("link_categories", lchList);
 			vu.addMessage(" Generando listado categoria Link.", VelocityUtil.INFO);
+			CommonGenerator.getCommonGenerator().chargeContext(vu, GeneratorConfigController.currentSection(LinkConfig.class));
 			generate(vu, Templates.LINK, LINK_CATEGORY_LIST_PAGE);
 			vu.remove("link_categories");
 		} catch (ManagerBeanException e) {
@@ -51,6 +80,7 @@ public class LinkGenerator extends Generator {
 		} finally {
 			linkCategoryDetailList = null;
 			lchList = null;
+			categoryMap = null;
 		}
 	}
 	
@@ -79,5 +109,7 @@ public class LinkGenerator extends Generator {
 	}
 
 	public static String LINK_CATEGORY_LIST_PAGE = "category";
+
+	public static String LINK_CATEGORY_BY_SECTION_PAGE = "category_section_";
 
 }
