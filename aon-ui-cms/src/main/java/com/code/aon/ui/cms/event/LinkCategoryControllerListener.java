fcb7c54f92082525bb0bd25fb18a61ecf4d50afe
Inigo Gayarre <igayarre@esferalia.com>
2008-02-12 14:08:32 +0000

    Ordenacion de categorias

--- aon-ui-cms/src/main/java/com/code/aon/ui/cms/event/LinkCategoryControllerListener.java
+++ aon-ui-cms/src/main/java/com/code/aon/ui/cms/event/LinkCategoryControllerListener.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.code.aon.cms.LinkCategory;
 import com.code.aon.cms.dao.ICMSAlias;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
@@ -12,6 +14,18 @@ import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
 
 public class LinkCategoryControllerListener extends ControllerAdapter {
+
+	@Override
+	public void beforeModelInitialized(ControllerEvent event)
+			throws ControllerListenerException {
+		try{
+			Criteria criteria = event.getController().getCriteria();
+			IManagerBean bean = BeanManager.getManagerBean(LinkCategory.class);
+			criteria.addOrder(bean.getFieldName(ICMSAlias.LINK_CATEGORY_POSITION));
+			event.getController().setCriteria(criteria);
+		}catch (Exception e) {
+		}
+	}
 
 	@Override
 	public void beforeBeanAdded(ControllerEvent event) throws ControllerListenerException {
