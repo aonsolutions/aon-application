128af406d76a5a74658d63305eca60d943c27bb4
Joseba Urkiri <jurkiri@esferalia.com>
2008-02-14 15:27:48 +0000

    Supplier pasa a su propio proyecto

--- aon-purchase/src/main/java/com/code/aon/purchase/dao/PurchaseAliasWriter.java
+++ aon-purchase/src/main/java/com/code/aon/purchase/dao/PurchaseAliasWriter.java
@@ -6,21 +6,16 @@ import java.io.IOException;
 import com.code.aon.common.dao.AliasWriter;
 import com.code.aon.purchase.Purchase;
 import com.code.aon.purchase.PurchaseDetail;
-import com.code.aon.purchase.Supplier;
-import com.code.aon.purchase.SupplierSegment;
 
 public class PurchaseAliasWriter {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("/PROYECTOS/aon-purchase/src/com/code/aon/purchase/dao/IPurchaseAlias.java");
-        String[] classes = new String[4];
+		File file = new File("/AON-PROJECT/aon-purchase/src/main/java/com/code/aon/purchase/dao/IPurchaseAlias.java");
+        String[] classes = new String[2];
         classes[0] = PurchaseDetail.class.getName();
         classes[1] = Purchase.class.getName();
-        classes[2] = Supplier.class.getName();
-        classes[3] = SupplierSegment.class.getName();
 		AliasWriter writer = new AliasWriter("com.code.aon.purchase.dao");
 		writer.write(classes,file);
 		System.out.println("Alias generados");
 	}
 }
-
