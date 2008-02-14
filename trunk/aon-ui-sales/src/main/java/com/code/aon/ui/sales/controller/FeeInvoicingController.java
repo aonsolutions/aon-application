3446b082bf6dc7f2db286ff69218c50e3b93c38d
Joseba Urkiri <jurkiri@esferalia.com>
2008-02-14 15:37:02 +0000

    cambios en el generador de vencimientos

--- trunk/aon-ui-sales/src/main/java/com/code/aon/ui/sales/controller/FeeInvoicingController.java
+++ trunk/aon-ui-sales/src/main/java/com/code/aon/ui/sales/controller/FeeInvoicingController.java
@@ -373,7 +373,7 @@ public class FeeInvoicingController extends BasicController {
 					Finance finance = (Finance)iter.next();
 					financeBean.remove(finance);
 				}
-				getFinanceGenerator().generateFinances(invoice, getPriceStrategy().getTotalPrice(invoice, invoice));
+				getFinanceGenerator().generateFinances(invoice,invoice.getRegistry(), getPriceStrategy().getTotalPrice(invoice, invoice));
 				feeFinanceController.onSearch(null);
 			}
 		} catch (ManagerBeanException e) {
