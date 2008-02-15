fb8045bd8102cc14b4681045158646e9b90bcf6c
Joseba Urkiri <jurkiri@esferalia.com>
2008-02-15 12:09:25 +0000

    Añadida la validación de la cuenta bancaria de proveedores

--- ui-gbp/src/main/java/com/code/ui/gbp/event/SupplierControllerListener.java
+++ ui-gbp/src/main/java/com/code/ui/gbp/event/SupplierControllerListener.java
@@ -4,10 +4,31 @@ import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.gbp.Supplier;
 import com.code.gbp.dao.IGBPAlias;
+import com.code.ui.gbp.controller.SupplierController;
 
 public class SupplierControllerListener extends ControllerAdapter {
 
+	@Override
+	public void afterBeanSelected(ControllerEvent event) throws ControllerListenerException {
+		SupplierController supplierController = (SupplierController)event.getController();
+		String bankAccount = ((Supplier)supplierController.getTo()).getBankAccount();
+		supplierController.setEntity(bankAccount.substring(0, 4));
+		supplierController.setOffice(bankAccount.substring(4, 8));
+		supplierController.setControl(bankAccount.substring(8, 10));
+		supplierController.setAccount(bankAccount.substring(10, 20));
+	}
+
+	@Override
+	public void afterBeanCreated(ControllerEvent event) throws ControllerListenerException {
+		SupplierController supplierController = (SupplierController)event.getController();
+		supplierController.setEntity("");
+		supplierController.setOffice("");
+		supplierController.setControl("");
+		supplierController.setAccount("");
+	}
+	
 	@Override
 	public void beforeModelInitialized(ControllerEvent event) throws ControllerListenerException {
 		try {
