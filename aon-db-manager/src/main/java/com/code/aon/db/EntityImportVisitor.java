c28db06a98769bac7fbd76178357c1d7b1e9359f
Aimar Tellitu <atellitu@esferalia.com>
2008-02-19 12:11:15 +0000

    *** empty log message ***

--- aon-db-manager/src/main/java/com/code/aon/db/EntityImportVisitor.java
+++ aon-db-manager/src/main/java/com/code/aon/db/EntityImportVisitor.java
@@ -1,14 +1,26 @@
 package com.code.aon.db;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.dom4j.Element;
+import org.dom4j.Node;
 import org.hibernate.EntityMode;
 import org.hibernate.ReplicationMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.type.StringType;
+import org.hibernate.type.Type;
 
 public class EntityImportVisitor  implements IEntityVisitor {
 
+	private static Log LOGGER = LogFactory.getLog(EntityImportVisitor.class.getName());
+	
 	private int counter;
 	
 	private int maxExport;
@@ -23,12 +35,28 @@ public class EntityImportVisitor  implements IEntityVisitor {
 
 	private String className;
 	
+	private List<String> notNullableStringProperties;
+	
 	@SuppressWarnings("unchecked")
 	public EntityImportVisitor(SessionFactory sessionFactory, int maxExport, Class entity) {
 		this.sessionFactory = sessionFactory;
 		this.maxExport = maxExport;
 		this.className = entity.getName();
+		initNotNullableStringProperties(sessionFactory, entity);
 		initTransaction();
+	}
+	
+	private void initNotNullableStringProperties(SessionFactory sessionFactory, Class entity) {
+		notNullableStringProperties = new ArrayList<String>();
+		ClassMetadata cmd = sessionFactory.getClassMetadata(entity);
+		String[] names = cmd.getPropertyNames();
+		Type[] types = cmd.getPropertyTypes();
+		boolean[] nullables = cmd.getPropertyNullability();
+		for( int i = 0; i < names.length; i++ ) {
+			if ( (!nullables[i]) && types[i].getClass().isAssignableFrom(StringType.class) ) {
+				notNullableStringProperties.add( names[i] );
+			}
+		}
 	}
 	
 	private void initTransaction() {
@@ -46,7 +74,17 @@ public class EntityImportVisitor  implements IEntityVisitor {
 		initTransaction();
 	}
 
+	private void patch( Element element ) {
+		for( String property : this.notNullableStringProperties ) {
+			Element e = element.element(property);
+			if ( (e != null) && StringUtils.isEmpty(e.getText()) ) {
+				e.addText(" ");
+			}
+		}
+	}
+	
 	public void visit(Element element) {
+		patch(element);
 		dom4jSession.replicate( className, element, ReplicationMode.EXCEPTION );
     	if ( ++counter == maxExport ) {
 			counter = 0;
--- aon-db-manager/src/main/java/com/code/aon/db/XMLToDBImporter.java
+++ aon-db-manager/src/main/java/com/code/aon/db/XMLToDBImporter.java
@@ -4,10 +4,14 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 import org.apache.commons.lang.ClassUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.xml.sax.InputSource;
 
 public class XMLToDBImporter implements IEntityManager {
 
+	private static Log LOGGER = LogFactory.getLog(XMLToDBImporter.class.getName());
+	
 	private HibernateDataManager hdm;
 	
 	public XMLToDBImporter(HibernateDataManager hdm) {
@@ -15,6 +19,7 @@ public class XMLToDBImporter implements IEntityManager {
 	}
 	
 	public void proccess(Class entity) throws EntityProcessException {
+		LOGGER.info( "Importing entity " + entity );
 		try {
 			String entityName = ClassUtils.getShortClassName(entity);
 			IEntityVisitor visitor = new EntityImportVisitor(hdm.getImportFactory(), hdm.getMaxImport(), entity);
