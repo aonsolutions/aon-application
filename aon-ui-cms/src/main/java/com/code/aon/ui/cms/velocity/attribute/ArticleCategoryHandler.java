fbbad425acc8221bdb54712db026e7c815b215e5
Inigo Gayarre <igayarre@esferalia.com>
2008-02-20 17:04:31 +0000

    *** empty log message ***

--- aon-ui-cms/src/main/java/com/code/aon/ui/cms/velocity/attribute/ArticleCategoryHandler.java
+++ aon-ui-cms/src/main/java/com/code/aon/ui/cms/velocity/attribute/ArticleCategoryHandler.java
@@ -3,11 +3,12 @@ package com.code.aon.ui.cms.velocity.attribute;
 import java.util.ArrayList;
 
 import com.code.aon.cms.ArticleCategoryDetail;
+import com.code.aon.cms.enumeration.Templates;
 
 public class ArticleCategoryHandler {
 
 	private String label;
-	
+
 	private ArrayList<ArticleHandler> list;
 
 	public ArticleCategoryHandler (ArticleCategoryDetail detail, ArrayList<ArticleHandler> list) {
@@ -18,7 +19,7 @@ public class ArticleCategoryHandler {
 	public String getLabel() {
 		return label;
 	}
-
+	
 	public ArrayList<ArticleHandler> getList() {
 		return list;
 	}
