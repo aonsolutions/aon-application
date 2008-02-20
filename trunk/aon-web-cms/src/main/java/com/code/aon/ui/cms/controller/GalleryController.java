2d6752b93c864916aacb20fb256552a684d54e35
Inigo Gayarre <igayarre@esferalia.com>
2008-02-20 14:34:44 +0000

    Rich

--- trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/GalleryController.java
+++ trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/GalleryController.java
@@ -1,42 +1,22 @@
 package com.code.aon.ui.cms.controller;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
 
-import javax.faces.event.ActionEvent;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
 
 import com.code.aon.cms.Image;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ui.cms.IGalleryController;
-import com.code.aon.ui.cms.tree.FileSystemNode;
 import com.code.aon.ui.form.BasicController;
 
 public abstract class GalleryController extends BasicController implements IGalleryController{
 
-	private String currentPath;
+	private String currentPath = revoverFilesPath();
 
-	private String basePath;
-	
-	public GalleryController() {
-		super();
-	}
-		
-	public void onInit(ActionEvent event){
-		this.currentPath = revoverFilesPath();
-		if (this.currentPath != null) {
-			File currentDir = new File(this.currentPath);
-			if (!currentDir.exists()) currentDir.mkdirs();
-			currentPath = currentDir.getAbsolutePath();
-			basePath = currentDir.getAbsolutePath();
-		}
-	}
-
-	private String getRelativePath(String base, String path) {
+	private String getRelativePath(String path) {
+		String base = new File(revoverFilesPath()).getAbsolutePath(); 
 		if (path.startsWith(base)) {
 			if (path.length() == base.length()) return "/";
 			else return path.substring(base.length()).replace('\\', '/');
@@ -46,8 +26,8 @@ public abstract class GalleryController extends BasicController implements IGall
 
 	public void chargeImageList() {
 		ArrayList<Image> list = new ArrayList<Image>();
-		if (this.currentPath==null)
-			this.onInit(null);
+		File currentDir = new File(this.currentPath);
+		if (!currentDir.exists()) currentDir.mkdirs();
 		File dir = new File(this.currentPath);
 		File files[] = dir.listFiles(getFilenameFilter());
 		for (int i=0; i < files.length; i++) {
@@ -56,13 +36,17 @@ public abstract class GalleryController extends BasicController implements IGall
 				Image img = new Image();
 				img.setName(temp.getName());
 				img.setPath(temp.getAbsolutePath());
-				img.setRelativePath(getRelativePath(basePath, temp.getAbsolutePath()));
+				img.setRelativePath(getRelativePath(temp.getAbsolutePath()));
 				list.add(img);
 			}
 		}
 		model = new ListDataModel(list);
 	}
 
+	public String getCurrentRelativePath() {
+		return getRelativePath(new File(currentPath).getAbsolutePath());
+	}
+	
 	public String getCurrentPath() {
 		return currentPath;
 	}
