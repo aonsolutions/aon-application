a1362974546b95dac6bc382c55619b0f8f02270e
Joseba Urkiri <jurkiri@esferalia.com>
2008-02-22 11:37:52 +0000

    ordenacion del listado de las facturas imprimibles por el proveedor

---
+++ trunk/ui-gbp/src/main/java/com/code/ui/gbp/front/event/FrontProofControllerListener.java
@@ -0,0 +1,20 @@
+package com.code.ui.gbp.front.event;
+
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ui.form.event.ControllerAdapter;
+import com.code.aon.ui.form.event.ControllerEvent;
+import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.gbp.dao.IGBPAlias;
+import com.code.gbp.enumeration.ProFormaInvoiceStatus;
+
+public class FrontProofControllerListener extends ControllerAdapter {
+
+	@Override
+	public void beforeModelInitialized(ControllerEvent event)throws ControllerListenerException {
+		try {
+			event.getController().getCriteria().addEqualExpression(event.getController().getFieldName(IGBPAlias.PRO_FORMA_INVOICE_STATUS), ProFormaInvoiceStatus.ACCEPTED);
+		} catch (ManagerBeanException e) {
+			throw new ControllerListenerException(e);
+		}
+	}
+}
