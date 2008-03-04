ca3e5b678952262e3b0c130718334b49e4b51743
Joseba Urkiri <jurkiri@esferalia.com>
2008-03-04 15:58:11 +0000

    Las cuentas que son necesarias para el sistema

---
+++ aon-account-bridge/src/main/java/com/code/aon/account/bridge/util/AccountConstants.java
@@ -0,0 +1,16 @@
+package com.code.aon.account.bridge.util;
+
+public class AccountConstants {
+
+	public static final String BANK_ACCOUNT_PREFIX = "57200";
+
+	public static final String CUSTOMER_ACCOUNT_PREFIX = "43000";
+
+	public static final String SUPPLIER_ACCOUNT_PREFIX = "40000";
+
+	public static final String CREDITOR_ACCOUNT_PREFIX = "41000";
+
+	public static final String LEASING_ACCOUNT_PREFIX = "52000";
+
+	public static final String LOAN_ACCOUNT_PREFIX = "52000";
+}
