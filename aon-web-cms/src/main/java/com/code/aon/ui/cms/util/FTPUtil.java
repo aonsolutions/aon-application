22f0c94c0992e14ccf839a42099fc74b2456f3ca
Jorge Diez <jdiez@esferalia.com>
2008-03-05 16:02:40 +0000

    Arreglado publicacion por FTP

--- aon-web-cms/src/main/java/com/code/aon/ui/cms/util/FTPUtil.java
+++ aon-web-cms/src/main/java/com/code/aon/ui/cms/util/FTPUtil.java
@@ -2,52 +2,78 @@ package com.code.aon.ui.cms.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
 import com.code.aon.cms.Config;
+import com.code.aon.common.AonException;
 import com.code.aon.ui.util.AonUtil;
 
 public class FTPUtil {
 
 	private static Config config;
 
-	public static void uploadFTP() {
+	@SuppressWarnings({ "finally", "finally" })
+	public static boolean uploadFTP() throws IOException {
+		boolean error = false;
 		config = ControllerUtil.getCurrentConfig();
 		String server = config.getFtp_server();
 		String user = config.getFtp_user();
 		String password = config.getFtp_password();
 		String destinationFolder = config.getFtp_path();
 		String sourceFolder = ControllerUtil.getPreviewPath();
+		FTPClient ftp = new FTPClient();
 
 		try {
-			FTPClient ftp = new FTPClient();
 			ftp.connect(server);
+			if (ftp.getReplyCode() >= 500) {
+				AonUtil.addErrorMessage(ftp.getReplyString());
+				throw new AonException();
+			}
+			else System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> " + ftp.getReplyString());
 			ftp.login(user, password);
-			ftp.changeWorkingDirectory(destinationFolder);
+			if (ftp.getReplyCode() >= 500) {
+				AonUtil.addErrorMessage(ftp.getReplyString());
+				throw new AonException();
+			}
+			else System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> " + ftp.getReplyString());
+
 			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> Connected to server");
+			ftp.changeWorkingDirectory(destinationFolder);
 			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> " + ftp.getReplyString());
 			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> " + ftp.getSystemName());
 			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> " + ftp.printWorkingDirectory());
-			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> " + ftp.setFileType(FTPClient.BINARY_FILE_TYPE));
-			FTPFile files[] = ftp.listFiles();
-			if (files.length > 0) {
-				if (!files[0].hasPermission(FTPFile.USER_ACCESS, FTPFile.WRITE_PERMISSION) ) {
-					System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> WRITE PERMISSION DENIED");
+			ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+			if (ftp.isConnected()) {
+				FTPFile files[] = ftp.listFiles();
+				if (files.length > 0) {
+					if (!files[0].hasPermission(FTPFile.USER_ACCESS, FTPFile.WRITE_PERMISSION) ) {
+						System.out.println("FTP ERROR>>>>>>>>>>>>>>>>>>>>>> Error de escritura en el servidor.");
+					}
 				}
+				ftpDir(sourceFolder, ftp, destinationFolder);
 			}
-			ftpDir(sourceFolder, ftp, destinationFolder);
-
-			ftp.logout();
-			ftp.disconnect();
-			//addMessage("La publicacion por FTP de la pagina web a finalizado.", GEN_INFO);
+			else {
+				System.out.println("FTP ERROR>>>>>>>>>>>>>>>>>>>>>> No hubo conexion con el servidor.");
+				error = true;
+			}
 		} catch (Exception e) {
-			AonUtil.addErrorMessage(e.getMessage());
-			//addMessage("FTP Error. Se produjo un error durante la conexion al FTP, si el error persite consulte con su administrador.", GEN_ERROR);
+			AonUtil.addErrorMessage("FTP Error. Se produjo un error durante la conexion al FTP, si el error persite consulte con su administrador.");
+			error = true;
+		} finally {
+			ftp.logout();
+			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> Logout.");
+			ftp.disconnect();
+			System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> Diconnected.");
+			return error;
 		}
+		
 	}
 
+	public static String invalidFolder[] = {"ckfinder", "_thumbs"};
+	
 	public static void ftpDir(String dir2ftp, FTPClient fc, String breadCrum) {
 		try {
 			File ftpDir = new File(dir2ftp);
@@ -57,9 +83,11 @@ public class FTPUtil {
 				if (!f.getName().equals(config.getDomain() + ".zip")) {
 					System.out.println("FTP>>>>>>>>>>>>>>>>>>>>>> Name: " + breadCrum + "/" + f.getName());
 					if (f.isDirectory()) {
-						fc.makeDirectory(breadCrum + "/" + f.getName());
-						String filePath = f.getPath();
-						ftpDir(filePath, fc, breadCrum + "/" + f.getName());
+						if (isValidFolder(f.getName())) { 
+							fc.makeDirectory(breadCrum + "/" + f.getName());
+							String filePath = f.getPath();
+							ftpDir(filePath, fc, breadCrum + "/" + f.getName());
+						}
 						continue;
 					}
 					FileInputStream fis = new FileInputStream(f);
@@ -72,6 +100,15 @@ public class FTPUtil {
 			AonUtil.addErrorMessage(e.getMessage());
 			//addMessage("FTP Error. Se produjo un error al intentar subir el fichero " + file, GEN_ERROR);
 		}
+	}
+
+	private static boolean isValidFolder(String name) {
+		for (int i = 0; i < invalidFolder.length ; i++) {
+			if (name.equals(invalidFolder[i])) {
+				return false;
+			}
+		}
+ 		return true;
 	}
 
 
