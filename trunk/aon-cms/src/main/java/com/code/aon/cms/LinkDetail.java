b6c39f69ef72cf08f798ea3cbc79edc627716ff8
Inigo Gayarre <igayarre@esferalia.com>
2008-03-05 15:29:23 +0000

    label 255

--- trunk/aon-cms/src/main/java/com/code/aon/cms/LinkDetail.java
+++ trunk/aon-cms/src/main/java/com/code/aon/cms/LinkDetail.java
@@ -54,7 +54,7 @@ public class LinkDetail implements ITransferObject {
 		this.language = language;
 	}
 
-	@Column(name = "label", nullable = false, length = 128)
+	@Column(name = "label", nullable = false, length = 255)
 	public String getLabel() {
 		return label;
 	}
