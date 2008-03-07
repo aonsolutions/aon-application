8a9c5342930a165fb0de9ec33b775fe7afc416d6
Joseba Urkiri <jurkiri@esferalia.com>
2008-03-07 12:30:07 +0000

    cuentas que van en el config

--- aon-account/src/main/java/com/code/aon/account/DefaultAccounts.java
+++ aon-account/src/main/java/com/code/aon/account/DefaultAccounts.java
@@ -22,7 +22,10 @@ public class DefaultAccounts {
 	
 	/** The Constant chargedRetentionAccount. */
 	public static final String CHARGED_RETENTION_ACCOUNT = "ACC_DEFAULT_CHARGED_RET_ACC";
-	
+
+	/** The Constant cashAccount. */
+	public static final String CASH_ACCOUNT = "ACC_DEFAULT_CASH_ACC";
+
 	/** The Constant salaryAccount. */
 	public static final String SALARY_ACCOUNT = "ACC_DEFAULT_SALARY_ACC";
 	
@@ -31,4 +34,13 @@ public class DefaultAccounts {
 	
 	/** The Constant socialInsuranceAccount. */
 	public static final String SOCIAL_INSURANCE_ACCOUNT = "ACC_DEFAULT_SOCIAL_INSURANCE_ACC";
+
+	/** The Constant companySocialInsuranceAccount. */
+	public static final String COMPANY_SOCIAL_INSURANCE_ACCOUNT = "ACC_DEFAULT_COMPANY_SOC_INS_ACC";
+
+	/** The Constant debtInterestAccount. */
+	public static final String DEBT_INTEREST_ACCOUNT = "ACC_DEFAULT_DEBT_INTEREST_ACC";
+
+	/** The Constant financialExpensesAccount. */
+	public static final String FINANCIAL_EXPENSES_ACCOUNT = "ACC_DEFAULT_FINAN_EXPENSES_ACC";
 }
