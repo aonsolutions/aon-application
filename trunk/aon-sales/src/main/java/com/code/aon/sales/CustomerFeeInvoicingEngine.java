863eefb1752b276e102945e195b23b8fb510fca1
Joseba Urkiri <jurkiri@esferalia.com>
2008-03-12 14:53:27 +0000

    Cuando se seleccionaba un cliente para facturar o prefacturar también salian los grupos

--- trunk/aon-sales/src/main/java/com/code/aon/sales/CustomerFeeInvoicingEngine.java
+++ trunk/aon-sales/src/main/java/com/code/aon/sales/CustomerFeeInvoicingEngine.java
@@ -128,7 +128,11 @@ public class CustomerFeeInvoicingEngine implements IInvoicingEngine {
 	private List invoiceGroupFees(Criteria criteria, InvoicingParameters params) throws ManagerBeanException, ExpressionException {
 		List<Integer> invoicedList = new LinkedList<Integer>();
 		IManagerBean invoicigGroupBean = BeanManager.getManagerBean(InvoicingGroup.class);
-		Iterator iter = invoicigGroupBean.getList(null).iterator();
+		Criteria groupCriteria = new Criteria();
+		if(params.getCustomerId() != null){
+			groupCriteria.addEqualExpression(invoicigGroupBean.getFieldName(IFinanceAlias.INVOICING_GROUP_PARENT_ID), params.getCustomerId());
+		}
+		Iterator iter = invoicigGroupBean.getList(groupCriteria).iterator();
 		while(iter.hasNext()){
 			InvoicingGroup group = (InvoicingGroup)iter.next();
 			List list = invoiceGroup(group, criteria, params);
