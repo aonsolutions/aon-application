d95ea0e8f634ad2c2df92e073d6561192b014377
Inigo Gayarre <igayarre@esferalia.com>
2008-03-28 12:01:13 +0000

    RichFaces

--- trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/event/EmailControllerListener.java
+++ trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/event/EmailControllerListener.java
@@ -9,7 +9,6 @@ import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
 import com.code.aon.ui.util.AonUtil;
 import com.code.aon.ui.webmail.bean.AonConstants;
-import com.code.aon.ui.webmail.controller.AutoCompleteEmailDictionary;
 import com.code.aon.ui.webmail.controller.WebMailController;
 import com.code.aon.groupware.Contact;
 import com.code.aon.webmail.MailAccount;
@@ -42,24 +41,4 @@ public class EmailControllerListener extends ControllerAdapter {
 		email.setUser(account.getUser());
 	}
 
-	@Override
-	public void afterBeanAdded(ControllerEvent event)
-			throws ControllerListenerException {
-		AutoCompleteEmailDictionary autoCompleteEmailDictionary = (AutoCompleteEmailDictionary)AonUtil.getRegisteredBean(AonConstants.BEAN_AUTOCOMPLETEEMAILDICC);
-		autoCompleteEmailDictionary.init();
-	}
-	
-	@Override
-	public void afterBeanUpdated(ControllerEvent event)
-			throws ControllerListenerException {
-		AutoCompleteEmailDictionary autoCompleteEmailDictionary = (AutoCompleteEmailDictionary)AonUtil.getRegisteredBean(AonConstants.BEAN_AUTOCOMPLETEEMAILDICC);
-		autoCompleteEmailDictionary.init();
-	}
-	
-	@Override
-	public void afterBeanRemoved(ControllerEvent event)
-			throws ControllerListenerException {
-		AutoCompleteEmailDictionary autoCompleteEmailDictionary = (AutoCompleteEmailDictionary)AonUtil.getRegisteredBean(AonConstants.BEAN_AUTOCOMPLETEEMAILDICC);
-		autoCompleteEmailDictionary.init();
-	}
 }
