073a3a853701e407b8936591e5f99da4fc5cebf2
Aimar Tellitu <atellitu@esferalia.com>
2008-04-03 10:58:44 +0000

    *** empty log message ***

---
+++ aon-security/src/main/java/com/code/aon/jaas/vendor/jboss/JBossLdapMBean.java
@@ -0,0 +1,20 @@
+/*
+ * Generated file - Do not edit!
+ */
+package com.code.aon.jaas.vendor.jboss;
+
+import java.util.Map;
+
+import com.code.aon.jaas.client.ast.IOption;
+
+/**
+ * MBean interface.
+ */
+public interface JBossLdapMBean extends org.jboss.system.ServiceMBean {
+
+	//default object name
+	String OBJECT_NAME = "jboss.admin:service=AonLdap";
+
+	Map<String, IOption> getOptions();
+	
+}
