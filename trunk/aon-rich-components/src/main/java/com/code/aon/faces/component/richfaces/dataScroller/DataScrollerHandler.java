2b72b42cde7f3aa71a2dd510b4bc1f617bbe0e90
Aimar Tellitu <atellitu@esferalia.com>
2008-04-17 08:06:29 +0000

    Se ha añadido el atributo pageSize a DataScroller para indicar cuantos registros mostrar por pagina. Por defecto si no se indica nada se obtiene el valor del atributo row de la Data Table (hace falta que tenga el mismo componente padre).

--- trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/dataScroller/DataScrollerHandler.java
+++ trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/dataScroller/DataScrollerHandler.java
@@ -3,6 +3,8 @@ package com.code.aon.faces.component.richfaces.dataScroller;
 import javax.el.ValueExpression;
 import javax.el.VariableMapper;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIData;
+import javax.faces.component.UIViewRoot;
 
 import com.code.aon.faces.component.util.FaceletUtil;
 import com.sun.facelets.FaceletContext;
@@ -10,6 +12,7 @@ import com.sun.facelets.el.VariableMapperWrapper;
 import com.sun.facelets.tag.TagAttribute;
 import com.sun.facelets.tag.TagConfig;
 import com.sun.facelets.tag.TagHandler;
+import com.sun.facelets.tag.jsf.ComponentSupport;
 
 /**
  * The Class TabbedPaneComponentHandler.
@@ -25,6 +28,8 @@ public class DataScrollerHandler extends TagHandler {
 	private static final String DATA_TABLE = "dataTable";
 	
 	private static final String MODEL = "model";
+	
+	private static final String PAGE_SIZE = "pageSize";
 	
    	private static final String SHOW_NOTE = "showNote";
    	
@@ -44,12 +49,30 @@ public class DataScrollerHandler extends TagHandler {
 		model = getRequiredAttribute(MODEL);
 	}
 
+	private int getPageSize( FaceletContext ctx, UIComponent parent ) {
+		int rows = 20;
+		UIData table = (UIData) ComponentSupport.findChild( parent, dataTable.getValue(ctx) );
+		if ( table != null ) {
+			rows = table.getRows();
+		}
+		return rows;
+	}
+	
 	private void insertTemplate(FaceletContext ctx, UIComponent component) {
 		VariableMapper newMapper = new VariableMapperWrapper(ctx.getVariableMapper());
 		newMapper.setVariable(MODEL, model.getValueExpression(ctx, Object.class));
 		newMapper.setVariable(DATA_TABLE, dataTable.getValueExpression(ctx, String.class));		
 		ValueExpression showNote = FaceletUtil.getBooleanValueExpression(ctx, getAttribute(SHOW_NOTE));
 		newMapper.setVariable(SHOW_NOTE, showNote);
+		TagAttribute pageSize = getAttribute(PAGE_SIZE);
+		if (pageSize != null) {
+			newMapper.setVariable(PAGE_SIZE, pageSize.getValueExpression(ctx, Integer.class));
+		} else {
+			int rows = getPageSize(ctx, component);
+			newMapper.setVariable(PAGE_SIZE, ctx.getExpressionFactory()
+					.createValueExpression(ctx, String.valueOf(rows), Integer.class));
+		}
+		
 		FaceletUtil.insertTemplate(ctx, tag, component, FaceletUtil.getTemplate(TEMPLATE), newMapper);
 	}
 
