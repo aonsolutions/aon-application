59a2ffad47d91832af6e4d43b7c39a5407513d7b
Inigo Gayarre <igayarre@esferalia.com>
2008-04-18 08:04:19 +0000

    Footer banner

--- aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/FooterController.java
+++ aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/FooterController.java
@@ -2,15 +2,18 @@ package com.code.aon.ui.cms.controller;
 
 import java.util.List;
 
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import com.code.aon.cms.Footer;
+import com.code.aon.cms.FooterBannerCategory;
+import com.code.aon.cms.dao.ICMSAlias;
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ui.util.AonUtil;
 
 
 public class FooterController extends BasicI18nController {
@@ -37,6 +40,17 @@ public class FooterController extends BasicI18nController {
 				footer.setDefault_(false);
 			bean.update(footer);
 		}
+	}
+
+	public void onSelectBannerCategories(ActionEvent event) throws ManagerBeanException, ExpressionException {
+		FooterBannerCategoryController c = (FooterBannerCategoryController)AonUtil.getController("footer_banner_category");
+		IManagerBean moBean = BeanManager.getManagerBean(FooterBannerCategory.class);
+		Footer footer = (Footer) this.getTo();
+		Criteria criteria = new Criteria();
+		criteria.addExpression(moBean.getFieldName(ICMSAlias.FOOTER_BANNER_CATEGORY_FOOTER_ID), "" + footer.getId());
+		c.setCurrentFooter(footer);
+		c.setCriteria(criteria);
+		c.onSearch(event);
 	}
 
 }
