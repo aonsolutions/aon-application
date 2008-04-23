fcb84ecf631b037b0fef6534dded62b7e88adc88
Gorka Irazu <girazu@esferalia.com>
2008-04-23 15:32:23 +0000

    En el metodo initModel, reseteamos el to.

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/InvoicingAddressController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/InvoicingAddressController.java
@@ -1,32 +1,13 @@
 package com.code.aon.ui.finance.controller;
 
-import java.util.Iterator;
-
-import javax.faces.event.ActionEvent;
-
-import com.code.aon.common.ManagerBeanException;
-import com.code.aon.finance.Invoice;
-import com.code.aon.finance.InvoiceAddress;
-import com.code.aon.finance.dao.IFinanceAlias;
-import com.code.aon.ql.Criteria;
-import com.code.aon.ui.form.IController;
 import com.code.aon.ui.form.LinesController;
 
 public class InvoicingAddressController extends LinesController {
 
-	@SuppressWarnings("unchecked")
-	public void onAddress(ActionEvent event) throws ManagerBeanException{
-		IController invoicingController = getMasterController();
-		Invoice invoice = (Invoice)invoicingController.getTo();
-		Criteria criteria = new Criteria();
-		criteria.addEqualExpression(this.getFieldName(IFinanceAlias.INVOICE_ADDRESS_INVOICE_ID), invoice.getId());
-		Iterator iter = this.getManagerBean().getList(criteria, 0, 1).iterator();
-		if(iter.hasNext()){
-			InvoiceAddress invoiceAddress = (InvoiceAddress)iter.next();
-			this.setTo(invoiceAddress);
-		}else{
-			this.onReset(null);
-			((InvoiceAddress)this.getTo()).setInvoice(invoice);
-		}
+	@Override
+	public void initModel() {
+		super.initModel();
+		super.onReset(null);
 	}
+
 }
