bed21be688d8a598bd564ce504d4107d80efc37b
Gorka Irazu <girazu@esferalia.com>
2008-04-23 15:02:59 +0000

    Se añade un filtro nuevo para los vencimientos que pueden ser añadidos a la remesa, que consiste en que el importe de dichos vencimientos tiene que ser distinto de cero.

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/FBatchController.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/FBatchController.java
@@ -228,9 +228,9 @@ public class FBatchController extends BasicController implements ICollectionProv
             Criteria criteria = new Criteria();
             criteria.addEqualExpression(controller.getFieldName(IFinanceAlias.FINANCE_PAYMENT), new Boolean(payment));
             Expression amountExpr = 
-                ExpressionUtilities.getGreaterThanExpression(controller.getFieldName(IFinanceAlias.FINANCE_AMOUNT), new Double(0));
+                ExpressionUtilities.getNotEqualExpression(controller.getFieldName(IFinanceAlias.FINANCE_AMOUNT), new Double(0));
             Expression expensesExpr = 
-                ExpressionUtilities.getGreaterThanExpression(controller.getFieldName(IFinanceAlias.FINANCE_EXPENSES), new Double(0));
+                ExpressionUtilities.getNotEqualExpression(controller.getFieldName(IFinanceAlias.FINANCE_EXPENSES), new Double(0));
             criteria.addExpression(ExpressionUtilities.getOrExpression(amountExpr, expensesExpr));
             Expression pendingExpr = 
                 ExpressionUtilities.getEqualExpression(controller.getFieldName(IFinanceAlias.FINANCE_FINANCE_STATUS), FinanceStatus.PENDING);
