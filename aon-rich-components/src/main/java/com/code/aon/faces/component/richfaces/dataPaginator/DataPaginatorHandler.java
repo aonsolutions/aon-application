47b48355929ed0b8ec34ade3fc58bdc3e671ec43
Aimar Tellitu <atellitu@esferalia.com>
2008-05-05 14:26:10 +0000

    Los facets para los botones ahora estan en una plantilla para que sea unica para el dataPaginator. Cuando paginator="true" ahora el fastControls esta por defecto a hide y boundaryControls y stepControls por defecto a show

--- aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/dataPaginator/DataPaginatorHandler.java
+++ aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/dataPaginator/DataPaginatorHandler.java
@@ -1,5 +1,10 @@
 package com.code.aon.faces.component.richfaces.dataPaginator;
 
+import java.io.IOException;
+
+import javax.el.ELException;
+import javax.el.VariableMapper;
+import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 
 import org.richfaces.component.UIDatascroller;
@@ -15,11 +20,15 @@ import com.sun.facelets.tag.jsf.ComponentConfig;
 
 public class DataPaginatorHandler extends DataScrollerTagHandler {
 
+	private static final String TEMPLATE_PATH = "com/code/aon/faces/component/richfaces/dataPaginator/";
+
+	private static final String TEMPLATE = TEMPLATE_PATH + "dataPaginator.xhtml";
+	
 	private static final String PAGINATOR_ATTRIBUTE = "paginator";
 	
 	private static final String FOR_ATTRIBUTE = "for";
 	
-	private static final String AUTO_VALUE = "auto";
+	private static final String SHOW_VALUE = "show";
 	
 	private static final String HIDE_VALUE = "hide";
 
@@ -49,7 +58,7 @@ public class DataPaginatorHandler extends DataScrollerTagHandler {
 			UIComponentTagUtils.setStringProperty( ctx.getFacesContext(), component, STEP_CONTROLS_ATTRIBUTE, value );
 		}
 		if (! FaceletUtil.hasValue(ctx, tag, FAST_CONTROLS_ATTRIBUTE) ) {
-			UIComponentTagUtils.setStringProperty( ctx.getFacesContext(), component, FAST_CONTROLS_ATTRIBUTE, value );
+			UIComponentTagUtils.setStringProperty( ctx.getFacesContext(), component, FAST_CONTROLS_ATTRIBUTE, HIDE_VALUE );
 		}
 	}
 	
@@ -83,10 +92,20 @@ public class DataPaginatorHandler extends DataScrollerTagHandler {
 		UIComponent component = (UIComponent) instance;
 		updateForAttribute(ctx, component);
 		if ( isPaginator(ctx) ) {
-			setControlsValue(ctx, component, AUTO_VALUE);
+			setControlsValue(ctx, component, SHOW_VALUE);
 		} else {
 			setControlsValue(ctx, component, HIDE_VALUE);			
 		}
+	}
+	
+	@Override
+	protected void applyNextHandler(FaceletContext ctx, UIComponent c)
+			throws IOException, FacesException, ELException {
+		if ( isPaginator(ctx) ) {
+			VariableMapper mapper = ctx.getVariableMapper();
+			FaceletUtil.insertTemplate(ctx, tag, c, FaceletUtil.getTemplate(TEMPLATE), mapper);
+		}
+		super.applyNextHandler(ctx, c);
 	}
 	
 }
