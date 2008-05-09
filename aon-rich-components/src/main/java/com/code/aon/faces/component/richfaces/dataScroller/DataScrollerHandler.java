e2baef59db2b379b1557cbbec57a3375462dd8db
Aimar Tellitu <atellitu@esferalia.com>
2008-05-09 09:07:03 +0000

    Se ha añadido el atributo rendered

--- aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/dataScroller/DataScrollerHandler.java
+++ aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/dataScroller/DataScrollerHandler.java
@@ -24,6 +24,8 @@ public class DataScrollerHandler extends TagHandler {
 
 	private static final String TEMPLATE = TEMPLATE_PATH + "dataScroller.xhtml";
 	
+	private static final String RENDERED = "rendered";
+	
 	private static final String DATA_TABLE = "dataTable";
 	
 	private static final String FOR = "for";
@@ -86,9 +88,20 @@ public class DataScrollerHandler extends TagHandler {
 		FaceletUtil.insertTemplate(ctx, tag, component, FaceletUtil.getTemplate(TEMPLATE), newMapper);
 	}
 
+	private boolean isRendered( FaceletContext ctx ) {
+		boolean rendered = true;
+		TagAttribute renderedTag = getAttribute(RENDERED);
+		if ( renderedTag != null ) {
+			rendered = renderedTag.getBoolean(ctx);
+		}
+		return rendered;
+	}
+	
 	@Override
 	public void apply(FaceletContext ctx, UIComponent parent) {
-		insertTemplate( ctx, parent );
+		if ( isRendered(ctx) ) {
+			insertTemplate( ctx, parent );
+		}
 	}
 
 }
