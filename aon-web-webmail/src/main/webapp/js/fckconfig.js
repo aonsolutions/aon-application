691f0cd31ecd3b98247e8774f74c6af918c3c13d
Aimar Tellitu <atellitu@esferalia.com>
2008-05-14 16:02:44 +0000

    *** empty log message ***

---
+++ aon-web-webmail/src/main/webapp/js/fckconfig.js
@@ -0,0 +1,14 @@
+FCKConfig.ToolbarSets["AonWebMail"] = [
+['Undo','Redo'],
+['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote'],
+['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+['Link','Unlink'],
+['Image','Table','Rule'],
+['Cut','Copy','Paste','PasteText','PasteWord'],
+['Find','Replace'],
+['FitWindow'],
+'/',
+['Bold','Italic','Underline','StrikeThrough'],
+['Style','FontFormat','FontName','FontSize'],
+['TextColor']
+] ;
