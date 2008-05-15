55551ea8825b7cd9362f6148023710162dc901fd
Gorka Irazu <girazu@esferalia.com>
2008-05-15 10:40:58 +0000

    Al contabilizar las facturas hay que grabar los invoiceDetailAccounts correspondientes de cada linea de factura, asi como al descontabilizarla borrar esta informacion.

--- trunk/aon-ui-sales/src/main/java/com/code/aon/ui/sales/controller/FeeInvoicingController.java
+++ trunk/aon-ui-sales/src/main/java/com/code/aon/ui/sales/controller/FeeInvoicingController.java
@@ -19,6 +19,7 @@ import com.code.aon.account.Account;
 import com.code.aon.account.AccountEntry;
 import com.code.aon.account.DefaultAccounts;
 import com.code.aon.account.Period;
+import com.code.aon.account.bridge.InvoiceDetailAccount;
 import com.code.aon.account.bridge.ProductAccount;
 import com.code.aon.account.bridge.dao.IAccountBridgeAlias;
 import com.code.aon.account.bridge.enumeration.ProductAccountType;
@@ -438,6 +439,7 @@ public class FeeInvoicingController extends BasicController {
 	@SuppressWarnings("unused")
 	public void onUnrecordInvoice(ActionEvent event) throws ManagerBeanException, ExpressionException{
 		Invoice invoice = (Invoice)this.getTo();
+		removeInvoiceDetailAccount(invoice);
 		getAccountEntryInvoiceWriter().unrecordInvoice(invoice);
 		IManagerBean invoiceBean = BeanManager.getManagerBean(Invoice.class);
 		invoice.setStatus(InvoiceStatus.PENDING);
@@ -445,6 +447,18 @@ public class FeeInvoicingController extends BasicController {
 			invoice.setRegistryAddress(null);
 		}
 		invoiceBean.update(invoice);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void removeInvoiceDetailAccount(Invoice invoice) throws ManagerBeanException {
+		IManagerBean invoiceAccountBean = BeanManager.getManagerBean(InvoiceDetailAccount.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(invoiceAccountBean.getFieldName(IAccountBridgeAlias.INVOICE_DETAIL_ACCOUNT_INVOICE_DETAIL_INVOICE_ID), invoice.getId());
+		Iterator iterator = invoiceAccountBean.getList(criteria).iterator();
+		while (iterator.hasNext()) {
+			InvoiceDetailAccount invoiceDetailAccount = (InvoiceDetailAccount)iterator.next();
+			invoiceAccountBean.remove(invoiceDetailAccount);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -457,7 +471,7 @@ public class FeeInvoicingController extends BasicController {
 		entry.setSecurityLevel(invoice.getSecurityLevel());
 		entry = getAccountEntryInvoiceWriter().insertorUpdateAccountEntry(entry, true);
 		List taxBreakDown = getPriceStrategy().getTaxBreakDowns(invoice, invoice);
-		getAccountEntryInvoiceWriter().insertEntryDetails(entry, AccountUtil.obtainCustomerAccount(invoice.getRegistry()), invoice.getSeries(), invoice.getNumber(), getPriceStrategy().getTotalPrice(invoice, invoice), getRetentionTotal(taxBreakDown), getTaxQuota(taxBreakDown), getBasesPerAccount(invoice));
+		getAccountEntryInvoiceWriter().insertEntryDetails(entry, AccountUtil.obtainCustomerAccount(invoice.getRegistry()), invoice.getSeries(), invoice.getNumber(), getPriceStrategy().getTotalPrice(invoice, invoice), getRetentionTotal(taxBreakDown), getTaxQuota(taxBreakDown), obtainBasesPerAccount(invoice));
 		getAccountEntryInvoiceWriter().insertAccountEntryInvoice(entry, invoice);
 		IManagerBean invoiceBean = BeanManager.getManagerBean(Invoice.class);
 		invoice.setStatus(InvoiceStatus.SCORED);
@@ -501,16 +515,19 @@ public class FeeInvoicingController extends BasicController {
 	}
 
 	@SuppressWarnings("unchecked")
-	private Map getBasesPerAccount(Invoice invoice) throws ManagerBeanException {
+	private Map obtainBasesPerAccount(Invoice invoice) throws ManagerBeanException {
 		Map basesPerAccount = new HashMap();
-		Iterator<InvoiceDetail> iterator = invoice.getLines().iterator();
+		IManagerBean invoiceDetailBean = BeanManager.getManagerBean(InvoiceDetail.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_INVOICE_ID), invoice.getId());
+		Iterator iterator = invoiceDetailBean.getList(criteria).iterator();
 		while (iterator.hasNext()) {
-			InvoiceDetail invoiceDetail = iterator.next();
+			InvoiceDetail invoiceDetail = (InvoiceDetail)iterator.next();
 			Account account = null;
 			if (invoiceDetail.getItem() != null) {
 				Integer productId = invoiceDetail.getItem().getProduct().getId();
 				IManagerBean productAccountBean = BeanManager.getManagerBean(ProductAccount.class);
-				Criteria criteria = new Criteria();
+				criteria = new Criteria();
 				criteria.addEqualExpression(productAccountBean.getFieldName(IAccountBridgeAlias.PRODUCT_ACCOUNT_PRODUCT_ID), productId);
 				criteria.addEqualExpression(productAccountBean.getFieldName(IAccountBridgeAlias.PRODUCT_ACCOUNT_TYPE), ProductAccountType.SALES);
 				Iterator iter = productAccountBean.getList(criteria).iterator();
@@ -518,17 +535,19 @@ public class FeeInvoicingController extends BasicController {
 					account = ((ProductAccount)iter.next()).getAccount();
 				}
 			}
-			account = (account==null)?account = obtainDefaultAccount(invoice):account;
+			account = (account==null)?account = obtainDefaultAccount():account;
 
 			double base = invoiceDetail.getTaxableBase();
 			base += (basesPerAccount.containsKey(account))?((Double)basesPerAccount.get(account)).doubleValue():0;
 			basesPerAccount.put(account, new Double(base));
+
+			insertInvoiceDetailAccount(invoiceDetail, account);
 		}
 		return basesPerAccount;
 	}
 
 	@SuppressWarnings("unchecked")
-	private Account obtainDefaultAccount(Invoice invoice) throws ManagerBeanException {
+	private Account obtainDefaultAccount() throws ManagerBeanException {
 		IManagerBean appParamsBean = BeanManager.getManagerBean(ApplicationParameter.class);
 		Criteria criteria = new Criteria();
 		criteria.addEqualExpression(appParamsBean.getFieldName(IConfigAlias.APPLICATION_PARAMETER_NAME), DefaultAccounts.SALES_ACCOUNT);
@@ -544,6 +563,14 @@ public class FeeInvoicingController extends BasicController {
 			}
 		}
 		return null;
+	}
+
+	private void insertInvoiceDetailAccount(InvoiceDetail invoiceDetail, Account account) throws ManagerBeanException {
+		IManagerBean invoiceAccountBean = BeanManager.getManagerBean(InvoiceDetailAccount.class);
+		InvoiceDetailAccount invoiceDetailAccount = new InvoiceDetailAccount();
+		invoiceDetailAccount.setInvoiceDetail(invoiceDetail);
+		invoiceDetailAccount.setAccount(account);
+		invoiceAccountBean.insert(invoiceDetailAccount);
 	}
 
 	public boolean isRecorded(){
