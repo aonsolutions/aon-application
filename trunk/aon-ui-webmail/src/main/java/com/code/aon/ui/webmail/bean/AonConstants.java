a2d1d8dc5ab49f72f636c1d096f3d7840383485a
Aimar Tellitu <atellitu@esferalia.com>
2008-05-20 08:02:59 +0000

    Ha pasado a ser una interface

--- trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/bean/AonConstants.java
+++ trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/bean/AonConstants.java
@@ -1,38 +1,38 @@
 package com.code.aon.ui.webmail.bean;
 
-public class AonConstants {
+public interface AonConstants {
 
 	// ************************************************************
 	// NAVIGATION
 	// ************************************************************
-	public static final String NAVIGATION_LOGIN = "login"; 
-	public static final String NAVIGATION_FOLDER = "folder"; 
-	public static final String NAVIGATION_MESSAGE = "message"; 
-	public static final String NAVIGATION_SEARCH = "messageSearch"; 
-	public static final String NAVIGATION_EMAILS = "emails";
-	public static final String NAVIGATION_SIGNATURE_FORM = "signature_form";
-	public static final String NAVIGATION_MAILACCOUNT_FORM = "mailAccount_form";
-	public static final String NAVIGATION_MAILACCOUNT_LIST = "mailAccount_list";
+	String NAVIGATION_LOGIN = "login"; 
+	String NAVIGATION_FOLDER = "folder"; 
+	String NAVIGATION_MESSAGE = "message"; 
+	String NAVIGATION_SEARCH = "messageSearch"; 
+	String NAVIGATION_EMAILS = "emails";
+	String NAVIGATION_SIGNATURE_FORM = "signature_form";
+	String NAVIGATION_MAILACCOUNT_FORM = "mailAccount_form";
+	String NAVIGATION_MAILACCOUNT_LIST = "mailAccount_list";
 
 	// ************************************************************
 	// BEAN
 	// ************************************************************
-	public static final String BEAN_LOGIN = "login";
-	public static final String BEAN_USER = "user";
-	public static final String BEAN_MAIL_ACCOUNT = "mailAccount";
-	public static final String BEAN_WEBMAIL = "webmail";
-	public static final String BEAN_TREE = "tree";
-	public static final String BEAN_FOLDER = "folder";
-	public static final String BEAN_MESSAGE = "message";
-	public static final String BEAN_ATTACH = "attach";
-	public static final String BEAN_SEARCH = "search";
-	public static final String BEAN_INPUTFILE = "inputFile";
-	public static final String BEAN_CONTACT = "contact";
-	public static final String BEAN_SIGNATURE = "signature";
-	public static final String BEAN_AUTOCOMPLETEEMAILDICC = "autoCompleteEmailDictionary";
-	public static final String BEAN_AUTOCOMPLETEEMAIL = "autoCompleteEmailBean";
-	public static final String BEAN_MULTISELECTIONEMAIL = "multiSelectionEmailBean";
+	String BEAN_LOGIN = "login";
+	String BEAN_USER = "user";
+	String BEAN_MAIL_ACCOUNT = "mailAccount";
+	String BEAN_WEBMAIL = "webmail";
+	String BEAN_TREE = "tree";
+	String BEAN_FOLDER = "folder";
+	String BEAN_MESSAGE = "message";
+	String BEAN_ATTACH = "attach";
+	String BEAN_SEARCH = "search";
+	String BEAN_INPUTFILE = "inputFile";
+	String BEAN_CONTACT = "contact";
+	String BEAN_SIGNATURE = "signature";
+	String BEAN_AUTOCOMPLETEEMAILDICC = "autoCompleteEmailDictionary";
+	String BEAN_AUTOCOMPLETEEMAIL = "autoCompleteEmailBean";
+	String BEAN_MULTISELECTIONEMAIL = "multiSelectionEmailBean";
 
-	public static final String RESOURCE_BUNDLE = "com.code.aon.ui.webmail.i18n.messages";
+	String RESOURCE_BUNDLE = "com.code.aon.ui.webmail.i18n.messages";
 	
 }
