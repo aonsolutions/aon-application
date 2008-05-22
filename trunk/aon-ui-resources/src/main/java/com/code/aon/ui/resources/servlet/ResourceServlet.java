b05274b2c65d0c4fd22e5cfde636e540e0bb6fc0
Aimar Tellitu <atellitu@esferalia.com>
2008-05-22 16:00:17 +0000

    *** empty log message ***

--- trunk/aon-ui-resources/src/main/java/com/code/aon/ui/resources/servlet/ResourceServlet.java
+++ trunk/aon-ui-resources/src/main/java/com/code/aon/ui/resources/servlet/ResourceServlet.java
@@ -44,7 +44,7 @@ public class ResourceServlet extends HttpServlet {
 	
 	private static final int LAST_MODIFIED_MOTH = 5;
 	
-	private static final int LAST_MODIFIED_DAY = 19;
+	private static final int LAST_MODIFIED_DAY = 22;
 	
 	private static final String MODIFY = calcModify();
 
