81d8364eb3f21ba43e722728e831fbf3dde47e73
Aimar Tellitu <atellitu@esferalia.com>
2008-05-23 11:42:04 +0000

    Se permite cambiar directamente la password al Manager

--- aon-desktop/src/main/java/com/code/aon/desktop/controller/AonUserController.java
+++ aon-desktop/src/main/java/com/code/aon/desktop/controller/AonUserController.java
@@ -15,17 +15,27 @@ import com.code.aon.ldap.DistinguishedName;
 import com.code.aon.ldap.IAonObjectClasses;
 import com.code.aon.ldap.ILdapConstants;
 import com.code.aon.ldap.LdapException;
-import com.code.aon.ldap.LdapSession;
+import com.code.aon.ui.config.controller.ConfigConstants;
 import com.code.aon.ui.config.controller.UserController;
 import com.code.aon.ui.config.util.UserUtils;
+import com.code.aon.ui.form.IController;
 import com.code.aon.ui.util.AonUtil;
 
 public class AonUserController extends UserController implements ILdapConstants, IAonObjectClasses {
 
 	private static final Logger LOGGER = Logger.getLogger(AonUserController.class.getName());
 	
-	private boolean accepted = false;
+	private String domain;
 	
+	private boolean managerChangingPassword;
+	
+	private boolean accepted;
+	
+	public AonUserController() {
+		AuthPrincipal user = UserUtils.getInstance().getPrincipal();
+		domain = user.getDomain();
+	}
+
 	public boolean isAccepted() {
 		return accepted;
 	}
@@ -33,59 +43,76 @@ public class AonUserController extends UserController implements ILdapConstants,
 	public void setAccepted(boolean accepted) {
 		this.accepted = accepted;
 	}
+	
+	public boolean isManagerChangingPassword() {
+		return managerChangingPassword;
+	}
+	
+	public String returnAction() {
+		return managerChangingPassword ? "user_form" : "home";
+	}
 
 	public void onLoadCurrentUser(ActionEvent event)  {
 		try {
-			loadUser();
+			this.managerChangingPassword = false;
+			User user = UserUtils.getInstance().getLoggedUser();
+			loadUser( user );
 		} catch (ManagerBeanException e) {
 			e.printStackTrace();
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	private void loadUser() throws ManagerBeanException {
-		User u = UserUtils.getInstance().getLoggedUser();
-		getUserManager().findUser(u.getLogin());
-		setUserTO(u);
+	public void onLoadUser(ActionEvent event)  {
+		try {
+			this.managerChangingPassword = true;
+			IController userController = AonUtil.getController(ConfigConstants.USER);
+			loadUser( (User) userController.getTo() );
+		} catch (ManagerBeanException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private void changeDefaultMailAccountPassword( String newPassword ) {
-		AuthPrincipal user = UserUtils.getInstance().getPrincipal();
-		DistinguishedName accountsDN = AonDN.getUserDefaultAccount(user.getDomain(), user.getShortName() );
+	@SuppressWarnings("unchecked")
+	private void loadUser( User user ) throws ManagerBeanException {
+		getUserManager().findUser(user.getLogin());
+		setUserTO(user);
+	}
+	
+	private void changeDefaultMailAccountPassword( String userName, String newPassword ) {
+		DistinguishedName accountsDN = AonDN.getUserDefaultAccount( domain, userName );
 		BasicLdap ldap = new BasicLdap();
 		if ( ldap.exists(accountsDN, MAIL_ACCOUNT ) ) {
 			try {
 				ldap.getLdapSession().replaceAttribute(accountsDN, USER_PASSWORD_ATTRIBUTE, newPassword);
 			} catch (LdapException e) {
-				AonUtil.addErrorMessage("Error cambiando la contraseña de la cuenta de correo por defecto del usuario " + user.getShortName() );
+				AonUtil.addErrorMessage("Error cambiando la contraseña de la cuenta de correo por defecto del usuario " + userName );
 			} finally {
 				ldap.closeSession();
 			}
 		} else {
-			AonUtil.addErrorMessage("No se ha encontrado la cuenta de correo por defecto del usuario " + user.getShortName() );
+			AonUtil.addErrorMessage("No se ha encontrado la cuenta de correo por defecto del usuario " + userName );
 		}
 	}
 
 	public void accept(ActionEvent event) {
-		if (getUserManager().getNewPassword().length() < 4) {
-			AonUtil.addErrorMessage("La contraseña debe ser al menos de 4 caracteres.");
-		}
-		else {
-			User user = (User)getTo();
-			int status = user.getStatus();
-			user.setStatus(status + 2);
-			try {
+		User user = (User) getTo();
+		int status = user.getStatus();
+		user.setStatus(status + 2);
+		try {
+			if ( managerChangingPassword ) {
+				getUserManager().savePassword();
+			} else {
 				super.accept(event);
-				changeDefaultMailAccountPassword( getUserManager().getPassword() );
-				FacesContext ctx = FacesContext.getCurrentInstance();
-				if ( ctx.getMaximumSeverity() == null ) {
-					AonUtil.addInfoMessage("Su contraseña se ha actualizado con exito.");
-				}
-			} catch (Exception e) {
-				LOGGER.log(Level.SEVERE, "Error cambiando la contraseña.", e );
-				user.setStatus(status);
 			}
+			changeDefaultMailAccountPassword( user.getLogin(), getUserManager().getPassword() );
+			FacesContext ctx = FacesContext.getCurrentInstance();
+			if ( ctx.getMaximumSeverity() == null ) {
+				AonUtil.addInfoMessage("Su contraseña se ha actualizado con exito.");
+			}
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Error cambiando la contraseña.", e );
+			user.setStatus(status);
 		}
 	}
-
+	
 }
