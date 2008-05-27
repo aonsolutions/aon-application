718824b5ba8dd1c18d0f0d30bc726b9731d411d1
Aimar Tellitu <atellitu@esferalia.com>
2008-05-27 09:05:37 +0000

    El aonUSer se pide bajo demanda

--- trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/controller/WebMailController.java
+++ trunk/aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/controller/WebMailController.java
@@ -85,7 +85,6 @@ public class WebMailController implements AonConstants, ILdapConstants {
 	public void initBasic(MailAccount mailAccount){
 		server = new AonServer(mailAccount);
 		server.createBasicFolders();
-		this.aonUser = getAonUser(Utils.getAuthPrincipal());
 		createDefaultSignature(mailAccount);
 		SpamController spamController = (SpamController) AonUtil.getRegisteredBean(BEAN_SPAM);
 		spamController.updateSpamEnabled(mailAccount);
@@ -124,21 +123,6 @@ public class WebMailController implements AonConstants, ILdapConstants {
     	return FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
     }
     
-	private Entry getAonUser( AuthPrincipal principal ) {
-		Entry entry = null;
-		BasicLdap ldap = new BasicLdap();
-		try {
-			DistinguishedName dn = AonDN.getUserDN(principal.getDomain(), principal.getShortName());
-			String filter = LdapSession.getObjectClass("aonUser");
-			entry = ldap.getLdapSession().get(dn.toString(), filter);
-		} catch ( LdapException e ) {
-			throw new AbortProcessingException( "Error getting aonUser for " + principal + ". " + e.getMessage(), e );
-		} finally {
-			ldap.closeSession();
-		}
-		return entry;
-	}		
-
     private void createDefaultSignature(MailAccount mailAccount){
     	if ( mailAccount.getSignature() == null ) {
     		try {
@@ -170,18 +154,40 @@ public class WebMailController implements AonConstants, ILdapConstants {
         	}    		
     	}
     }
+
+	private Entry getAonUser( AuthPrincipal principal ) {
+		Entry entry = null;
+		BasicLdap ldap = new BasicLdap();
+		try {
+			DistinguishedName dn = AonDN.getUserDN(principal.getDomain(), principal.getShortName());
+			String filter = LdapSession.getObjectClass("aonUser");
+			entry = ldap.getLdapSession().get(dn.toString(), filter);
+		} catch ( LdapException e ) {
+			throw new AbortProcessingException( "Error getting aonUser for " + principal + ". " + e.getMessage(), e );
+		} finally {
+			ldap.closeSession();
+		}
+		return entry;
+	}		
+    
+    private Entry getAonUser() {
+    	if ( this.aonUser == null ) {
+    		this.aonUser = getAonUser(Utils.getAuthPrincipal());
+    	}
+    	return this.aonUser;
+    }
     
     public String getCompanyName(){
-    	if (this.aonUser.containsKey(ORGANIZATION_NAME_ATTRIBUTE) ) {
-    		return this.aonUser.getAsString(ORGANIZATION_NAME_ATTRIBUTE);
+    	if (getAonUser().containsKey(ORGANIZATION_NAME_ATTRIBUTE) ) {
+    		return getAonUser().getAsString(ORGANIZATION_NAME_ATTRIBUTE);
     	}    	
     	return null;
     }
 
     public String getLoggedUserName() {
-    	String userName = this.aonUser.getAsString(COMMON_NAME_ATTRIBUTE);
-    	if (this.aonUser.containsKey(SURNAME_ATTRIBUTE) ) {
-    		userName += " " + this.aonUser.getAsString(SURNAME_ATTRIBUTE);
+    	String userName = getAonUser().getAsString(COMMON_NAME_ATTRIBUTE);
+    	if (getAonUser().containsKey(SURNAME_ATTRIBUTE) ) {
+    		userName += " " + getAonUser().getAsString(SURNAME_ATTRIBUTE);
     	}
         return userName;
     }
