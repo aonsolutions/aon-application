3ea8ca143af24e0c35866067ded6df58bb0c869d
Aimar Tellitu <atellitu@esferalia.com>
2008-05-28 15:25:26 +0000

    Se ha añadido un Handle para el InputRichText

--- trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/RegionableInputHandler.java
+++ trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/richfaces/RegionableInputHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.faces.component.UIComponent;
 
+import com.code.aon.faces.component.richfaces.inputRichText.InputRichTextHandler;
 import com.code.aon.faces.component.richfaces.lookup.inputText.LookupInputTextHandler;
 import com.code.aon.faces.component.richfaces.selectInputDate.SelectInputDateHandler;
 import com.code.aon.faces.component.util.BasicComponentConfig;
@@ -29,6 +30,8 @@ public class RegionableInputHandler extends TagHandler implements IRichFacesTags
 	private static final String LOOKUP_INPUT_TEXT_COMPONENT_TYPE = "com.code.aon.faces.HtmlLookupInputText";
 	
 	private static final String SELECT_INPUT_DATE_COMPONENT_TYPE = "com.code.aon.faces.SelectInputDate";
+	
+	private static final String INPUT_RICH_TEXT_COMPONENT_TYPE = "com.code.aon.faces.InputRichText";
 
 	private ComponentConfig config;
 	
@@ -53,6 +56,8 @@ public class RegionableInputHandler extends TagHandler implements IRichFacesTags
 			return new LookupInputTextHandler(config);
 		} else if ( SELECT_INPUT_DATE_COMPONENT_TYPE.equals(componentType) ) {
 			return new SelectInputDateHandler(config);
+		} else if ( INPUT_RICH_TEXT_COMPONENT_TYPE.equals(componentType) ) {
+			return new InputRichTextHandler(config);
 		}
 		return new AonAjaxInputHandler(config);
 	}
