a8b32d9cf04fb5a8b6bde4b45c76fa2305f7c8de
Aimar Tellitu <atellitu@esferalia.com>
2008-05-29 09:07:06 +0000

    *** empty log message ***

--- trunk/aon-rich-components/src/main/resources/com/code/aon/faces/component/richfaces/inputRichText/fckeditorAjax.js
+++ trunk/aon-rich-components/src/main/resources/com/code/aon/faces/component/richfaces/inputRichText/fckeditorAjax.js
@@ -1,19 +1,3 @@
-function applyEditor( sBasePath, sTextAreaName, configPath, toolBar, height, width ) {
-	var oFCKeditor = new FCKeditor( sTextAreaName ) ;
-	oFCKeditor.BasePath = sBasePath;
-	if ( configPath ) {
-		oFCKeditor.Config['CustomConfigurationsPath']= configPath;
-	}
-	oFCKeditor.ToolbarSet= toolBar;
-	if ( height ) {
-		oFCKeditor.Height = height;
-	}
-	if ( width ) {
-		oFCKeditor.Width = width;
-	}
-	oFCKeditor.ReplaceTextarea();
-}
-
 function updateEditorContent( sTextAreaName ) {
 	try {
 		FCKeditorAPI.GetInstance(sTextAreaName).UpdateLinkedField();
