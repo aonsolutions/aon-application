6447dd10e586b9ab233d90dfe3aa7ab9deff0cdf
Gorka Irazu <girazu@esferalia.com>
2008-06-10 08:21:34 +0000

    Añadido campo de total registros al ReportFinanceBatch

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/fbatch/print/FBatchPrinter.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/fbatch/print/FBatchPrinter.java
@@ -29,6 +29,7 @@ public class FBatchPrinter implements ICollectionProvider {
 				FinanceBatch fBatch = (FinanceBatch)iter.next();
 				ReportFinanceBatch rFBatch = new ReportFinanceBatch();
 				rFBatch.setFinanceBatch(fBatch);
+				rFBatch.setRegs(fBatchController.getFinanceBatchTotalDetails(fBatch));
 				rFBatch.setTotal(fBatchController.getFinanceBatchTotalAmount(fBatch));
 				reportFBatchList.add(rFBatch);
 			}
