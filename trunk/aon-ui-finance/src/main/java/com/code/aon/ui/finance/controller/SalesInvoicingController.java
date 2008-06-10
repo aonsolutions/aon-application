c281cf803acd42d82de56342de392f0bb4d3abd9
Gorka Irazu <girazu@esferalia.com>
2008-06-10 13:41:12 +0000

    Cambio en el metodo updateBreadCrumb para recuperar el nodo con el key del nodo hijo directamente. Tambien se ha añadido el metodo getAccountEntryId() para recuperar el codigo del asiento asociado a la remesa

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/SalesInvoicingController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/SalesInvoicingController.java
@@ -9,8 +9,6 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.ValueChangeEvent;
@@ -19,6 +17,7 @@ import javax.faces.model.SelectItem;
 import com.code.aon.account.Account;
 import com.code.aon.account.AccountEntry;
 import com.code.aon.account.DefaultAccounts;
+import com.code.aon.account.bridge.AccountEntryInvoice;
 import com.code.aon.account.bridge.InvoiceDetailAccount;
 import com.code.aon.account.bridge.ProductAccount;
 import com.code.aon.account.bridge.dao.IAccountBridgeAlias;
@@ -95,6 +94,8 @@ public class SalesInvoicingController extends BasicController {
 	/** SalesFinance controllers name. */
 	private static final String SALES_FINANCE_CONTROLLER_NAME = "salesFinance";
 	
+	private static final String MENU_MANAGER_NAME = "menuManager";
+
 	/** The price strategy. */
 	private IPriceStrategy priceStrategy;
 	
@@ -499,11 +500,9 @@ public class SalesInvoicingController extends BasicController {
 	 */
 	@SuppressWarnings("unchecked")
 	private void updateBreadCrumb() {
-		FacesContext ctx = FacesContext.getCurrentInstance();
-		ValueBinding vb = ctx.getApplication().createValueBinding("#{menuManager}");
-		MenuManager menuManager = (MenuManager)vb.getValue(ctx);
-		menuManager.setCurrentMenu("AON_APP");
-		menuManager.getCurrentMenuModel().setSelectedNode("aon_app.sales.salesInvoicing");
+		MenuManager menuManager = (MenuManager)AonUtil.getRegisteredBean(MENU_MANAGER_NAME);
+        menuManager.setCurrentMenu("AON_APP");
+        menuManager.getCurrentMenuModel().setSelectedNode(menuManager.getCurrentMenuModel().getOptionByKey("salesInvoicing").getId());
 	}
 	
 	/**
@@ -932,5 +931,21 @@ public class SalesInvoicingController extends BasicController {
 			return ((Integer)value).intValue() + 1;
 		}
 		return 1;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Integer getAccountEntryId() throws ManagerBeanException {
+    	Invoice invoice = (Invoice)this.getTo();
+		if (invoice != null && invoice.getId() != null) {
+			IManagerBean accountEntryInvoiceBean = BeanManager.getManagerBean(AccountEntryInvoice.class);
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(accountEntryInvoiceBean.getFieldName(IAccountBridgeAlias.ACCOUNT_ENTRY_INVOICE_INVOICE_ID), invoice.getId());
+			Iterator iterator = accountEntryInvoiceBean.getList(criteria).iterator();
+			if (iterator.hasNext()) {
+				AccountEntryInvoice accountEntryInvoice = (AccountEntryInvoice)iterator.next();
+				return accountEntryInvoice.getAccountEntry().getId();
+			}
+		}
+    	return null;
 	}
 }
