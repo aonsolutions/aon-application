9f26dc6e498967d887f11f4285ec6c2648335834
Inigo Gayarre <igayarre@esferalia.com>
2008-06-11 13:13:06 +0000

    RESOLVED - bug 550: Estadísticas. Por Proveedor http://bugzilla.code.es/show_bug.cgi?id=550

--- ui-gbp/src/main/java/com/code/ui/gbp/stats/SupplierEvolutionReport.java
+++ ui-gbp/src/main/java/com/code/ui/gbp/stats/SupplierEvolutionReport.java
@@ -59,8 +59,9 @@ public class SupplierEvolutionReport implements ICollectionProvider {
 	public Collection getCollection() {
 		Session s = HibernateUtil.getSession();
 		String subStmt = "(SELECT SUM(inv.amount) FROM ProFormaInvoice inv WHERE inv.supplier.id = cs.supplier.id AND inv.campaign.id = cs.campaign.id)";
+		String subStmtTotal = "(SELECT SUM(inv.amount) FROM ProFormaInvoice inv WHERE inv.supplier.id = cs.supplier.id AND inv.campaign.startDate <= ? AND inv.campaign.endDate >= ?)";
 		String stmt = "SELECT " + "new com.code.ui.gbp.stats.SupplierEvolution("
-				+ "cs.supplier.id,cs.supplier.name,cs.campaign.code,cs.campaign.name,SUM(off.price)," + subStmt
+				+ "cs.supplier.id,cs.supplier.name,cs.campaign.code,cs.campaign.name,SUM(off.price)," + subStmt +","+ subStmtTotal
 				+ ") FROM CampaignSupplier cs,Offer off WHERE ";
 		StringBuilder sentence = new StringBuilder(stmt);
 		if (getSupplier() != null && getSupplier().getId() != null) {
@@ -77,6 +78,8 @@ public class SupplierEvolutionReport implements ICollectionProvider {
 		Query query = s.createQuery(sentence.toString());
 		query.setDate(0, getToDate());
 		query.setDate(1, getFromDate());
+		query.setDate(2, getToDate());
+		query.setDate(3, getFromDate());
 		List<SupplierEvolution> list = query.list();
 		return list;
 
@@ -90,5 +93,16 @@ public class SupplierEvolutionReport implements ICollectionProvider {
 		c.set(Calendar.MONTH, 0);
 		setFromDate(c.getTime());
 		setToDate(new Date());
+	}
+	
+	public static void main(String[] args) {
+		SupplierEvolutionReport rep = new SupplierEvolutionReport();
+		rep.setFromDate(new Date("01/01/08"));
+		rep.setToDate(new Date("11/06/08"));
+		Supplier supplier = new Supplier();
+		supplier.setId(new Integer(332));
+		rep.setSupplier(supplier);
+		System.out.println(rep.getCollection().size());
+		System.out.println("->");		
 	}
 }
