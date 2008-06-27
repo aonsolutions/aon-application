68ce51e79560b5a63c39b38c9d140a6e7298f987
Inigo Gayarre <igayarre@esferalia.com>
2008-06-27 10:02:52 +0000

    ubicacion de spacer.gif

--- aon-web-cms/src/main/java/com/code/aon/ui/cms/servlet/ThumbnailServlet.java
+++ aon-web-cms/src/main/java/com/code/aon/ui/cms/servlet/ThumbnailServlet.java
@@ -1,13 +1,11 @@
 package com.code.aon.ui.cms.servlet;
 
-import java.awt.Image;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -15,15 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.ImageIcon;
 
 import com.code.aon.cms.Config;
-import com.code.aon.common.enumeration.MimeType;
 import com.code.aon.ui.cms.Constants;
 import com.code.aon.ui.cms.util.ImageUtil;
-import com.sun.jimi.core.Jimi;
-import com.sun.jimi.core.JimiException;
-import com.sun.jimi.core.raster.JimiRasterImage;
 
 /**
  *  
@@ -60,7 +53,7 @@ public class ThumbnailServlet extends HttpServlet implements Constants{
         	res.setHeader("Pragma", "no-cache");
         	res.setHeader("Cache-Control", "no-store");
             res.setCharacterEncoding("ISO-8859-1"); //$NON-NLS-1$
-			if(f.exists()){
+			if(f.exists() && f.isFile()){
                 ImageUtil.resize(file, res.getOutputStream(), maxDim);
             }
             else {
