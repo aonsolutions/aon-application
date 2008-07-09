7a1d21f9e8888b79d7b76c9312b208032b7d0274
Gorka Irazu <girazu@esferalia.com>
2008-07-09 11:52:14 +0000

    Implementados cambios para que sean iguales al FeeFinanceController

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/PurchaseFinanceController.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/PurchaseFinanceController.java
@@ -6,20 +6,27 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.company.Company;
+import com.code.aon.finance.Finance;
+import com.code.aon.finance.PayMethod;
 import com.code.aon.finance.RegistryBank;
 import com.code.aon.finance.dao.IFinanceAlias;
+import com.code.aon.finance.enumeration.FinanceStatus;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ui.form.LinesController;
+import com.code.aon.ui.util.AonUtil;
 
 public class PurchaseFinanceController extends LinesController {
 
 	private static final Logger LOGGER = Logger.getLogger(PurchaseFinanceController.class.getName());
+
+	private static final String PURCHASE_INVOICING_CONTROLLER = "invoicing";
 
 	private Integer registryBankId;
 
@@ -31,12 +38,48 @@ public class PurchaseFinanceController extends LinesController {
 		this.registryBankId = registryBankId;
 	}
 	
+	public boolean isModelToEditable() throws ManagerBeanException{
+		Finance finance = (Finance)this.getModel().getRowData(); 
+		return (finance.getFinanceStatus().equals(FinanceStatus.PENDING) || finance.getFinanceStatus().equals(FinanceStatus.RETURNED));
+	}
+
+	public boolean isModelToPending() throws ManagerBeanException{
+		Finance finance = (Finance)this.getModel().getRowData(); 
+		return (finance.getFinanceStatus().equals(FinanceStatus.PENDING));
+	}
+
 	@SuppressWarnings("unchecked")
-	public List getCompanyBanks(){
+	public boolean isAllPending() throws ManagerBeanException{
+		Iterator iter = ((List)this.getModel().getWrappedData()).iterator();
+		while(iter.hasNext()){
+			Finance finance = (Finance)iter.next();
+			if(!finance.getFinanceStatus().equals(FinanceStatus.PENDING)){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void payMethodChanged(ValueChangeEvent event) throws ManagerBeanException{
+		if(event.getNewValue() != null){
+			IManagerBean payMethodBean = BeanManager.getManagerBean(PayMethod.class);
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(payMethodBean.getFieldName(IFinanceAlias.PAY_METHOD_ID), event.getNewValue());
+			Iterator iter = payMethodBean.getList(criteria,0,1).iterator();
+			if(iter.hasNext()){
+				((Finance)this.getTo()).setPayMethod((PayMethod)iter.next());
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List getRegistryBanks(){
 		List<SelectItem> rBanks = new LinkedList<SelectItem>();
-		Company company = obtainCompany();
-		if(company != null){
-			try {
+		try {
+			InvoicingController invoicingController = (InvoicingController)AonUtil.getController(PURCHASE_INVOICING_CONTROLLER);
+			Company company = invoicingController.obtainCompany();
+			if(company != null){
 				IManagerBean rBankBean = BeanManager.getManagerBean(RegistryBank.class);
 				Criteria criteria = new Criteria();
 				criteria.addEqualExpression(rBankBean.getFieldName(IFinanceAlias.REGISTRY_BANK_REGISTRY_ID), company.getId());
@@ -46,25 +89,10 @@ public class PurchaseFinanceController extends LinesController {
 					SelectItem item = new SelectItem(rBank.getId(), rBank.getBank().getName());
 					rBanks.add(item);
 				}
-			} catch (ManagerBeanException e) {
-				LOGGER.log(Level.SEVERE, "Error obtaining Banks for the company", e);
-			}
-			
-		}
-		return rBanks;
-	}
-
-	@SuppressWarnings("unchecked")
-	private Company obtainCompany() {
-		try {
-			IManagerBean companyBean = BeanManager.getManagerBean(Company.class);
-			Iterator iter = companyBean.getList(null).iterator();
-			if(iter.hasNext()){
-				return (Company)iter.next();
 			}
 		} catch (ManagerBeanException e) {
-			LOGGER.log(Level.SEVERE, "Error the company", e);
+			LOGGER.log(Level.SEVERE, "Error obtaining Banks for the company", e);
 		}
-		return null;
+		return rBanks;
 	}
 }
