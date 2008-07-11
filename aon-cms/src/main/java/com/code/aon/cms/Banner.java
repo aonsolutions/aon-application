1958fae01f6905c866fa923240f2775e13e1d29b
Inigo Gayarre <igayarre@esferalia.com>
2008-07-11 07:05:25 +0000

    banner

--- aon-cms/src/main/java/com/code/aon/cms/Banner.java
+++ aon-cms/src/main/java/com/code/aon/cms/Banner.java
@@ -23,7 +23,7 @@ public class Banner implements ITransferObject, IPositionObject {
 	
 	private String alias;
 	
-	private boolean active;
+	private boolean active = true;
 	
 	private Integer position;
 	
