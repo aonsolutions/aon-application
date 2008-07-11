342f6c4f5e220be468c3b2bfcab5cc67b827ba47
Inigo Gayarre <igayarre@esferalia.com>
2008-07-11 07:06:09 +0000

    banner / comparator de images

--- trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/BannerController.java
+++ trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/BannerController.java
@@ -3,29 +3,21 @@ package com.code.aon.ui.cms.controller;
 import javax.faces.event.ActionEvent;
 
 import com.code.aon.cms.Banner;
-import com.code.aon.cms.BannerCategory;
 import com.code.aon.cms.BannerDetail;
 import com.code.aon.cms.Image;
-import com.code.aon.cms.dao.ICMSAlias;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.ql.Criteria;
-import com.code.aon.ql.util.ExpressionException;
-import com.code.aon.ui.cms.controller.support.OrderedControllerSupport;
-import com.code.aon.ui.cms.controller.support.IOrderedControllerListener;
 import com.code.aon.ui.util.AonUtil;
 
-public class BannerController extends BasicI18nController implements IOrderedControllerListener {
+public class BannerController extends BasicI18nController {
 
-	public OrderedControllerSupport orderedControllerSupport = new OrderedControllerSupport(ICMSAlias.BANNER_POSITION);
-
-	private BannerCategory currentBannerCategory;
+	private int page;
 	
-	public BannerCategory getCurrentBannerCategory() {
-		return currentBannerCategory;
+	public int getPage() {
+		return page;
 	}
 
-	public void setCurrentBannerCategory(BannerCategory currentBannerCategory) {
-		this.currentBannerCategory = currentBannerCategory;
+	public void setPage(int page) {
+		this.page = page;
 	}
 
 	@SuppressWarnings("unused")
@@ -55,10 +47,6 @@ public class BannerController extends BasicI18nController implements IOrderedCon
 		return label;
 	}
 
-	public void onAccept(ActionEvent event) {
-		super.accept(event);
-	}
-	
 	public void onDelImage(ActionEvent event) {
 		BannerDetail current = (BannerDetail)getToI18n();
 		current.setImage(null);
@@ -69,30 +57,6 @@ public class BannerController extends BasicI18nController implements IOrderedCon
 		String image = ((Image)controller.getModel().getRowData()).getRelativePath();
 		BannerDetail current = (BannerDetail)getToI18n();
 		current.setImage(image);
-	}
-
-	public void fireBeforeUseCriteria(Criteria criteria) {
-		try {
-			criteria.addExpression(getManagerBean().getFieldName(ICMSAlias.BANNER_BANNER_CATEGORY_ID), "" + getCurrentBannerCategory().getId());
-		} catch (ManagerBeanException e) {
-		} catch (ExpressionException e) {
-		}
-	}
-
-    public void onMoveUp(ActionEvent event) throws ManagerBeanException, ExpressionException {
-    	orderedControllerSupport.onMoveUp(this);
-    }
-
-    public void onMoveDown(ActionEvent event) throws ManagerBeanException, ExpressionException {
-    	orderedControllerSupport.onMoveDown(this);
-    }
-
-	protected void afterRemoveSelected(){
-		try {
-			orderedControllerSupport.reorderObjects(this);
-		} catch (ManagerBeanException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
