4ab56f165f0a4f5571a8059aebac9e46301cffa6
Inigo Gayarre <igayarre@esferalia.com>
2008-07-18 07:27:59 +0000

    modificaciones Sport

--- aon-web-cms/src/main/java/com/code/aon/ui/cms/event/SportCareerPathControllerListener.java
+++ aon-web-cms/src/main/java/com/code/aon/ui/cms/event/SportCareerPathControllerListener.java
@@ -1,10 +1,7 @@
 package com.code.aon.ui.cms.event;
 
 import com.code.aon.cms.SportCareerPath;
-import com.code.aon.cms.dao.ICMSAlias;
-import com.code.aon.common.BeanManager;
-import com.code.aon.common.IManagerBean;
-import com.code.aon.ql.Criteria;
+import com.code.aon.ui.cms.controller.SportCareerPathController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -12,17 +9,11 @@ import com.code.aon.ui.form.event.ControllerListenerException;
 public class SportCareerPathControllerListener extends ControllerAdapter {
 
 	@Override
-	public void beforeModelInitialized(ControllerEvent event)
+	public void afterBeanCreated(ControllerEvent event)
 			throws ControllerListenerException {
-		try{
-			Criteria criteria = event.getController().getCriteria();
-			IManagerBean bean = BeanManager.getManagerBean(SportCareerPath.class);
-			criteria.addOrder(bean.getFieldName(ICMSAlias.SPORT_CAREER_PATH_SPORT_PLAYER_ID));
-			criteria.addOrder(bean.getFieldName(ICMSAlias.SPORT_CAREER_PATH_INIT_DATE),false);
-			criteria.addOrder(bean.getFieldName(ICMSAlias.SPORT_CAREER_PATH_CLUB));
-			event.getController().setCriteria(criteria);
-		}catch (Exception e) {
-		}
+		SportCareerPathController c = (SportCareerPathController)event.getController();
+		SportCareerPath sportCareerPath = (SportCareerPath) c.getTo();
+		sportCareerPath.setSportPlayer(c.getSportPlayer());
 	}
 
 }
