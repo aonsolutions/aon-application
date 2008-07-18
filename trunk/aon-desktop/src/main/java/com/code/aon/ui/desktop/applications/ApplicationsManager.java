de6025373fbe9bab243995e828d61b04cfc080aa
Inaki Ayerbe <iayerbe@esferalia.com>
2008-07-18 10:56:31 +0000

    Ya no es necesario dirigir las peticiones a la carpeta /auth/index.jsp. Cambian las versiones de aon-security a la 1.3.0 y aon-security-bridge a la 1.4.0

--- trunk/aon-desktop/src/main/java/com/code/aon/ui/desktop/applications/ApplicationsManager.java
+++ trunk/aon-desktop/src/main/java/com/code/aon/ui/desktop/applications/ApplicationsManager.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
+import com.code.aon.bridge.session.BackDoorAuthenticationFilter;
 import com.code.aon.desktop.controller.AonUserController;
 import com.code.aon.jaas.client.ast.IApplication;
 import com.code.aon.jaas.deployment.DeploymentException;
@@ -31,7 +32,7 @@ public class ApplicationsManager {
 		List list = aonUserController.getUserManager().getUserApplications();
         for (int i = 0; i < list.size(); i++) {
 			IApplication app = (IApplication) list.get(i);
-			String context = ec.getRequestContextPath() + app.getContext() + ".auth";
+			String context = app.getContext() + "?" + BackDoorAuthenticationFilter.AUTH_SSO + "=true";
 			String property = services.getProperty( app.getId() );
 			if ( property != null ) {
 				char[] bar = property.substring( 0, property.indexOf( ';' ) ).toCharArray();
