72e540b8f7c2c3a9531b9743349d862978f924b9
Inaki Ayerbe <iayerbe@esferalia.com>
2008-07-18 10:42:45 +0000

    Eliminar el usuario autentificado una vez expira la sesion. Adaptar las clases BackDoorAuthentication y BackDoorAuthenticationFilter a la version 1.3.0 de aon-security.

--- aon-security-bridge/src/main/java/com/code/aon/bridge/session/ConcurrentSessionListener.java
+++ aon-security-bridge/src/main/java/com/code/aon/bridge/session/ConcurrentSessionListener.java
@@ -3,8 +3,6 @@
  */
 package com.code.aon.bridge.session;
 
-import java.io.File;
-
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
@@ -30,10 +28,6 @@ public class ConcurrentSessionListener implements HttpSessionListener {
 	 * @see javax.servlet.http.HttpSessionListener#sessionCreated(javax.servlet.http.HttpSessionEvent)
 	 */
 	public void sessionCreated(HttpSessionEvent se) {
-		File file = new File ( BackDoorAuthenticationValve.RESOURCES_DEFAULT_DIR );
-		if ( !file.exists() ) {
-			file.mkdirs();
-		}
 		LOGGER.debug( "Session Created: " + se.getSession().getId() );
 	}
 
@@ -47,7 +41,7 @@ public class ConcurrentSessionListener implements HttpSessionListener {
 			IConsoleAdmin console = ConsoleAdminFactoryManager.createConsoleAdmin();
 			String oname = console.getAonSessionManagerName();
 			console.invoke( oname, IOperation.REMOVE_SESSION, params, sig );
-			BackDoorAuthenticationValve.remove( se.getSession().getId() );
+			BackDoorAuthenticationValve.removePrincipal( se.getSession().getId() );
 			LOGGER.debug( "Session Destroyed: " + se.getSession().getId() );
 		} catch (DeploymentException e) {
 			LOGGER.fatal( e );
