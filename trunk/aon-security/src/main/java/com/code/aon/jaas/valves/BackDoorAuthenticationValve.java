311ca3cbd3950864fd844620ec62b6f469a2abdb
Inaki Ayerbe <iayerbe@esferalia.com>
2008-07-18 10:32:17 +0000

    Bug Fixed.Permitir Rol y Perfil con el mismo nombre. Carga de usuarios por session en memoria. Independizar la carga de peticiones activas por hashCode(), para su uso en la clase BackDoorAuthentication y BackDoorAuthenticationFilter.

--- trunk/aon-security/src/main/java/com/code/aon/jaas/valves/BackDoorAuthenticationValve.java
+++ trunk/aon-security/src/main/java/com/code/aon/jaas/valves/BackDoorAuthenticationValve.java
@@ -3,17 +3,10 @@
  */
 package com.code.aon.jaas.valves;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.crypto.SealedObject;
-import javax.crypto.SecretKey;
 import javax.servlet.ServletException;
 
 import org.apache.catalina.Session;
@@ -21,8 +14,6 @@ import org.apache.catalina.authenticator.Constants;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 
 /**
@@ -33,18 +24,13 @@ import org.apache.commons.logging.LogFactory;
  */
 public class BackDoorAuthenticationValve extends ValveBase {
 
-	/** Default directory for encrypted serialized sessions file. */
-	public static final String RESOURCES_DEFAULT_DIR= "/home/COMMON-RESOURCES/ENC/";
-	/** Serialized file extension. */
-	public static final String SER_EXT= ".enc";
 	/** Serialized session identifier name. */
 	public static final String SER_SESSION_ID= "serSessionId";
-	/** Maintain the Catalina Request for programmatic web login */
-	public static ThreadLocal<Request> activeRequest = new ThreadLocal<Request>();
-	/** BackDoorAuthenticationValve Log */
-	private static final Log LOGGER = LogFactory.getLog( BackDoorAuthenticationValve.class.getName() );
-	/** List of serialized sessions, destroyed sessions are removed. */
-	private static final List<String> serializedSessions = new ArrayList<String>(); 
+
+	/** Maintain the Principals for programmatic web login */
+	private static final Map<String, BackDoorPrincipal> sessions = new HashMap<String, BackDoorPrincipal>();
+	/** Maintain the Catalina active Requests for programmatic web login */
+	private static final Map<Integer, ThreadLocal<Request>> activeRequests = new HashMap<Integer, ThreadLocal<Request>>();
 
 	/* (non-Javadoc)
 	 * @see org.apache.catalina.valves.ValveBase#invoke(org.apache.catalina.connector.Request, org.apache.catalina.connector.Response)
@@ -56,66 +42,42 @@ public class BackDoorAuthenticationValve extends ValveBase {
 			String nonHashedPassword = (String) session.getNote( Constants.SESS_PASSWORD_NOTE );
 			if ( nonHashedPassword != null ) {
 				String username = (String) session.getNote( Constants.SESS_USERNAME_NOTE );
-				if ( !serializedSessions.contains( session.getId() ) ) {
-					serialize( session.getId(), username, nonHashedPassword );
-					serializedSessions.add( session.getId() );
-				}
+				sessions.put( session.getId(), new BackDoorPrincipal( username, nonHashedPassword, request.hashCode() ) );
 			}
 		}
-		activeRequest.set( request );// Set the active request
+		if ( !activeRequests.containsKey( request.hashCode() ) ) {
+			ThreadLocal<Request> activeRequest = new ThreadLocal<Request>();
+			activeRequest.set( request ); // Set the active request
+			activeRequests.put( request.hashCode(), activeRequest );
+		} else {
+			activeRequests.get( request.hashCode() ).set( request );
+		}
 		try {
-			getNext().invoke(request, response);// Perform the request
+			getNext().invoke( request, response );// Perform the request
 		} finally {
-			activeRequest.set( null );
+			activeRequests.get( request.hashCode() ).set( null );
 		}
 	}
 
 	/**
-	 * Serializes Principal.
+	 * Return the active Request.
 	 * 
-	 * @param id
-	 * @param username
-	 * @param nonHashedPassword
-	 * @throws IOException
-	 */
-	private void serialize(String id, String username, String nonHashedPassword) throws IOException {
-		FileOutputStream ostream = null;
-		try {
-			String path = RESOURCES_DEFAULT_DIR + id + SER_EXT;
-			ostream = new FileOutputStream( path );
-			/* Create the output stream */
-			ObjectOutputStream oopstream = new ObjectOutputStream( ostream );
-			/* Create the serialized principal */
-			BackDoorPrincipal bdp = new BackDoorPrincipal( username, nonHashedPassword );
-			SecretKey key = DesEncrypter.getSecretKeyInstance( SER_EXT + id );
-			// Create encrypter/decrypter class
-			DesEncrypter encrypter = new DesEncrypter(key);
-			SealedObject so = encrypter.encrypt( bdp );
-			oopstream.writeObject( so );
-			oopstream.flush();
-		} catch(IOException e) {
-			LOGGER.fatal( e.getMessage() );
-			throw e;
-		} catch (InvalidKeySpecException e) {
-			LOGGER.fatal( e.getMessage() );
-		} catch (NoSuchAlgorithmException e) {
-			LOGGER.fatal( e.getMessage() );
-		} finally {
-			if ( ostream != null ) 
-				ostream.close();
-		}
-	}
-
-	/**
-	 * Removes serialized session and its bound encrypted file.
-	 * 
-	 * @param id
+	 * @param serSessionId
 	 * @return
 	 */
-	public static final boolean remove(String id) {
-		serializedSessions.remove( id );
-		String path = RESOURCES_DEFAULT_DIR + id + SER_EXT;
-		return new File( path ).delete();
+	public static final synchronized Request getActiveRequest(String serSessionId) {
+		int hashCode = sessions.get( serSessionId ).getRequestHashcode();
+		return activeRequests.get( hashCode ).get();
+	}
+
+	/**
+	 * Return <code>BackDoorPrincipal</code> bound to sessionId.
+	 * 
+	 * @param serSessionId
+	 * @return
+	 */
+	public static final synchronized BackDoorPrincipal getPrincipal(String serSessionId) {
+		return sessions.get( serSessionId );
 	}
 
 }
