fa3dd948dedae40c77f8cb7c0edebf3f26629da3
Aimar Tellitu <atellitu@esferalia.com>
2008-07-31 08:37:55 +0000

    *** empty log message ***

--- aon-web-commercial/src/main/webapp/js/richFaces.js
+++ aon-web-commercial/src/main/webapp/js/richFaces.js
@@ -30,39 +30,6 @@ function resetStatusError() {
 	}
 }
 
-var oldOnSubmit;
-
-function startUpload() {
-	try {
-		var form = document.getElementById( "homepage:webmailForm" );
-		if ( form ) {
-			oldOnSubmit = form.onsubmit;
-			form.onsubmit = null;
-		}
-		var fileItems = document.getElementById( "homepage:webmailForm:attach_file:fileItems");
-		if ( fileItems ) {
-			fileItems.style.height = "70px";
-		} else {
-			alert( "fileItems not found !!" );
-		}
-	} catch(e) {
-		alert( e );
-	}
-}
-
-function endUpload() {
-	try {
-		var form = document.getElementById( "homepage:webmailForm" );
-		if ( form ) {
-			form.onsubmit = oldOnSubmit;
-		}
-		var fileItems = document.getElementById( "homepage:webmailForm:attach_file:fileItems");
-		if ( fileItems ) {
-			fileItems.style.height = "0px";
-		} else {
-			alert( "fileItems not found !!" );
-		}
-	} catch(e) {
-		alert( e );
-	}
+function toUpperCase(obj) {
+	obj.value = obj.value.toUpperCase();
 }
