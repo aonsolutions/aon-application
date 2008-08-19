b3380b1fa5aff6d0f53a40feabcea1216f01bd0e
Inigo Gayarre <igayarre@esferalia.com>
2008-08-19 09:10:28 +0000

    busqueda

--- aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/ProductController.java
+++ aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.code.aon.ui.cms.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.event.ActionEvent;
@@ -8,15 +9,22 @@ import com.code.aon.cms.Image;
 import com.code.aon.cms.Product;
 import com.code.aon.cms.ProductDetail;
 import com.code.aon.cms.dao.ICMSAlias;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
+import com.code.aon.ql.ast.Expression;
+import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ql.util.ExpressionUtilities;
 import com.code.aon.ui.cms.util.ControllerUtil;
 import com.code.aon.ui.util.AonUtil;
 
 
 public class ProductController extends BasicI18nController {
 
+	private String shortDesc;
+	
 	private int page;
 	
 	public int getPage() {
@@ -85,6 +93,53 @@ public class ProductController extends BasicI18nController {
 		String image = ((Image)controller.getModel().getRowData()).getRelativePath();
 		Product current = (Product)getTo();
 		current.setImage(image);
+	}
+
+	@Override
+	public void onEditSearch(ActionEvent event) {
+		setShortDesc(null);
+		super.onEditSearch(event);
+	}
+
+	public void completeDetailCriteria(String alias_value, String value) throws ManagerBeanException {
+		IManagerBean bean = BeanManager.getManagerBean(ProductDetail.class);
+		Criteria criteria = new Criteria();
+		try {
+			criteria.addExpression(bean.getFieldName(alias_value), value);
+			List objects = (List<ITransferObject>)bean.getList(criteria);
+			String alias = getFieldName(ICMSAlias.PRODUCT_ID);
+			Expression expr = null;
+			for (Iterator iterator = objects.iterator(); iterator.hasNext();) {
+				if (expr==null)
+					expr = ExpressionUtilities.getExpression(((ProductDetail) iterator.next()).getProduct().getId().toString(),alias);
+				else
+					expr = ExpressionUtilities.getOrExpression(expr, ExpressionUtilities.getExpression(((ProductDetail) iterator.next()).getProduct().getId().toString(),alias));
+			}
+			if (expr != null)
+				getCriteria().addExpression(expr);
+			else
+				getCriteria().addExpression(alias, "-1");
+		} catch (ExpressionException e) {
+			throw new ManagerBeanException(e);
+		}
+	}
+
+	public void completeCriteria() throws ManagerBeanException {
+		if (getShortDesc() != null && getShortDesc().length()>0) {
+			completeDetailCriteria(ICMSAlias.PRODUCT_DETAIL_SHORT_LABEL, getShortDesc());
+		}
+	}
+
+	// -------------------------------------------------
+	// Getters y setters para los campos de la búsqueda.
+	// -------------------------------------------------
+
+	public String getShortDesc() {
+		return shortDesc;
+	}
+
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
 	}
 	
 }
