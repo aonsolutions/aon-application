ef1e1c259e14d485bd644ddb79f28880ef546f6a
Inigo Gayarre <igayarre@esferalia.com>
2008-08-19 09:10:28 +0000

    busqueda

--- trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/event/ProductControllerListener.java
+++ trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/event/ProductControllerListener.java
@@ -2,6 +2,7 @@ package com.code.aon.ui.cms.event;
 
 import com.code.aon.cms.dao.ICMSAlias;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ui.cms.controller.ProductController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -11,8 +12,10 @@ public class ProductControllerListener extends ControllerAdapter {
 	@Override
 	public void beforeModelInitialized(ControllerEvent event)
 			throws ControllerListenerException {
+		ProductController controller = (ProductController)event.getController(); 
 		try {
-			event.getController().getCriteria().addOrder(event.getController().getManagerBean().getFieldName(ICMSAlias.PRODUCT_ALIAS));
+			controller.completeCriteria();
+			controller.getCriteria().addOrder(controller.getManagerBean().getFieldName(ICMSAlias.PRODUCT_ALIAS));
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException(e);
 		}
