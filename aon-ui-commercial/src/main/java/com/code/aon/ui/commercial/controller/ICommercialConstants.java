09327d13128eff6fa198ad8859ab08d376c4f972
Aimar Tellitu <atellitu@esferalia.com>
2008-08-19 08:42:30 +0000

    *** empty log message ***

---
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/controller/ICommercialConstants.java
@@ -0,0 +1,14 @@
+package com.code.aon.ui.commercial.controller;
+
+public interface ICommercialConstants {
+
+	/** TargetMedia Controller name. */
+	String TARGET_MEDIA_CONTROLLER_NAME = "targetMedia";
+	
+	/** TargetAddress Controller name. */
+	String TARGET_ADDRESS_CONTROLLER_NAME = "targetAddress";
+
+	/** TargetSegment Controller name. */
+	String TARGET_SEGMENT_CONTROLLER_NAME = "targetSegment";
+
+}
