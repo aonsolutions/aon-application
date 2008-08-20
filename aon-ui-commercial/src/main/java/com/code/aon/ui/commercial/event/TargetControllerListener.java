7c871d427ec3c9574882d1f97886181641a36ff3
Aimar Tellitu <atellitu@esferalia.com>
2008-08-20 06:35:13 +0000

    Se ha añadido un metodo para resetear la lista de tracking de un target

--- aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/TargetControllerListener.java
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/TargetControllerListener.java
@@ -11,6 +11,7 @@ import com.code.aon.customer.dao.ICustomerAlias;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ui.commercial.controller.ICommercialConstants;
 import com.code.aon.ui.commercial.controller.TargetController;
+import com.code.aon.ui.form.IController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -36,6 +37,7 @@ public class TargetControllerListener extends ControllerAdapter implements IComm
 		TargetController controller = (TargetController) event.getController();
 		try {
 			controller.refreshSegments();
+			resetTargetTracking( (Target) controller.getTo() );
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException( e.getMessage(), e );
 		}
@@ -72,4 +74,13 @@ public class TargetControllerListener extends ControllerAdapter implements IComm
        	AonUtil.getController(TARGET_SELLER_CONTROLLER_NAME).onCancel(null);
 	}
 
+	private void resetTargetTracking( Target target ) throws ManagerBeanException {
+		IController controller = AonUtil.getController(TARGET_TRACKING_CONTROLLER_NAME);
+		controller.clearCriteria();
+		Criteria criteria = controller.getCriteria();
+		String field = controller.getFieldName(ICommercialAlias.COMMERCIAL_TRACKING_TARGET_ID);
+		criteria.addEqualExpression( field, target.getId() );
+		controller.onSearch(null);
+	}
+	
 }
