bef2f8dee678814309675efaa632949ab134a5e4
Aimar Tellitu <atellitu@esferalia.com>
2008-08-20 11:33:32 +0000

    *** empty log message ***

--- aon-desktop/src/main/java/com/code/aon/ui/desktop/applications/ApplicationsManager.java
+++ aon-desktop/src/main/java/com/code/aon/ui/desktop/applications/ApplicationsManager.java
@@ -32,7 +32,7 @@ public class ApplicationsManager {
         for (int i = 0; i < list.size(); i++) {
 			IApplication app = (IApplication) list.get(i);
 //			String context = ec.getRequestContextPath() + app.getContext() + ".auth?aonDesktop=true";
-			String context = app.getContext() + "/?aonDesktop=true";
+			String context = app.getContext() + "?aonDesktop=true";
 			String property = services.getProperty( app.getId() );
 			if ( property != null ) {
 				char[] bar = property.substring( 0, property.indexOf( ';' ) ).toCharArray();
