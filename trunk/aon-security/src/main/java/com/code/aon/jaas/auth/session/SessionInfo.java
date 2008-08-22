f2d6f74107cd751c1157ed2d8eeb7fab5ed03f40
Inaki Ayerbe <iayerbe@esferalia.com>
2008-08-22 10:39:05 +0000

    Ya no es necesario dirigir las peticiones a la carpeta /auth/index.jsp. Cambian la version de aon-security a la 1.5.0

--- trunk/aon-security/src/main/java/com/code/aon/jaas/auth/session/SessionInfo.java
+++ trunk/aon-security/src/main/java/com/code/aon/jaas/auth/session/SessionInfo.java
@@ -128,4 +128,16 @@ public class SessionInfo {
 		this.lastAccessedTime = new Date().getTime();
 	}
 
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append( "SessionInfo[creationTime:" );sb.append( creationTime );sb.append( ";" );
+		sb.append( "lastAccessedTime:" );sb.append( lastAccessedTime );sb.append( ";" );
+		sb.append( "maxInactiveInterval:" );sb.append( maxInactiveInterval );sb.append( ";" );
+		sb.append( "expired:" );sb.append( expired );sb.append( ";" );
+		sb.append( "shortName:" + principal.getShortName() + ";" );
+		sb.append( "domain:" + principal.getDomain() + "]" );
+		return sb.toString();
+	}
+
 }
