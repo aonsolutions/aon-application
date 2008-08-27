c588074d4ecfadd7b451c5e988b4371c0f92a115
Inaki Ayerbe <iayerbe@esferalia.com>
2008-08-27 07:15:09 +0000

    Version Inicial 0.0.1

---
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/tipos/Tipaut.java
@@ -0,0 +1,39 @@
+package com.code.aon.payroll.tipos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Tipos de Autorización
+ */
+@Entity
+@Table(name="tipaut")
+public class Tipaut implements java.io.Serializable {
+
+	private String cdg;
+	private String descripcion;
+
+	@Id
+	@GeneratedValue
+	@Column(name="cdg", unique=true, nullable=false, length=1)
+    public String getCdg() {
+		return this.cdg;
+    }
+
+	public void setCdg(String cdg) {
+		this.cdg = cdg;
+	}
+    
+	@Column(name="descripcion", nullable=false, length=60)
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+    
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
---
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/tipos/Tipinc.java
@@ -0,0 +1,40 @@
+package com.code.aon.payroll.tipos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Tipos de Incidencia
+ */
+@Entity
+@Table(name="tipinc")
+public class Tipinc  implements java.io.Serializable {
+
+	private String cdg;
+	private String descripcion;
+
+	@Id
+	@GeneratedValue
+	@Column(name="cdg", unique=true, nullable=false, length=1)
+    public String getCdg() {
+		return this.cdg;
+    }
+
+	public void setCdg(String cdg) {
+		this.cdg = cdg;
+	}
+    
+	@Column(name="descripcion", nullable=false, length=60)
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+    
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
+
