38fd6f4b154a15cb9610b8f8e040e31073cbd890
Aimar Tellitu <atellitu@esferalia.com>
2008-09-02 08:27:47 +0000

    *** empty log message ***

--- aon-ui-audit/src/main/java/com/code/aon/ui/audit/controller/AuditCollectionsController.java
+++ aon-ui-audit/src/main/java/com/code/aon/ui/audit/controller/AuditCollectionsController.java
@@ -1,5 +1,6 @@
 package com.code.aon.ui.audit.controller;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
@@ -7,11 +8,20 @@ import java.util.Locale;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
+import com.code.aon.audit.Application;
+import com.code.aon.audit.dao.IAuditAlias;
 import com.code.aon.audit.enumeration.AuditLevel;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Criteria;
 
 public class AuditCollectionsController {
 
 	private List<SelectItem> auditLevels;
+	
+	private List<SelectItem> applications;
 	
 	/**
 	 * Gets the audit levels.
@@ -30,5 +40,23 @@ public class AuditCollectionsController {
 		}
 		return auditLevels;
 	}	
+	
+	public List<SelectItem> getApplications() {
+		return applications;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void refreshApplications() throws ManagerBeanException {
+		applications = new LinkedList<SelectItem>();
+		IManagerBean segmentBean = BeanManager.getManagerBean(Application.class);
+		Criteria criteria = new Criteria();
+		criteria.addOrder(segmentBean.getFieldName(IAuditAlias.APPLICATION_NAME));
+		Iterator<ITransferObject> iter = segmentBean.getList(criteria).iterator();
+		while(iter.hasNext()){
+			Application application = (Application)iter.next();
+			SelectItem item = new SelectItem(application.getId(), application.getName());
+			applications.add(item);
+		}
+	}
 	
 }
