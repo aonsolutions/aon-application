9578e716c872394c8aab5fb0c859d10bc9f1e547
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2008-09-04 14:10:52 +0000

    *** empty log message ***

---
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/event/TiposBonificacionListener.java
@@ -0,0 +1,8 @@
+package com.code.aon.ui.payroll.controller.event;
+
+import com.code.aon.ui.form.event.ControllerAdapter;
+
+
+public class TiposBonificacionListener extends ControllerAdapter {
+
+}
