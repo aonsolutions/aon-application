feb941e4acb2b4b61b9746e3fd9ec6976ef558c4
David Uriarte <duriarte@esferalia.com>
2008-09-19 07:25:36 +0000

    *** empty log message ***

--- trunk/aon-payroll/src/main/java/com/code/aon/payroll/dao/PayrollAliasWriter.java
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/dao/PayrollAliasWriter.java
@@ -9,6 +9,7 @@ import com.code.aon.payroll.cotizacion.Porcentaje;
 import com.code.aon.payroll.cotizacion.PorcentajeMaestro;
 import com.code.aon.payroll.geograficas.Nacion;
 import com.code.aon.payroll.irpf.Cuota;
+import com.code.aon.payroll.irpfforal.CuotaRetencionAlava;
 import com.code.aon.payroll.tipos.Autorizacion;
 import com.code.aon.payroll.tipos.Documento;
 import com.code.aon.payroll.tipos.Empresario;
@@ -33,7 +34,8 @@ public class PayrollAliasWriter {
 				Bonificacion.class.getName(),
 				Cuota.class.getName(),
 				Tipovia.class.getName(),
-				Nacion.class.getName() };
+				Nacion.class.getName(), 
+				CuotaRetencionAlava.class.getName()};
 		AliasWriter writer = new AliasWriter("com.code.aon.payroll.dao");
 		writer.write(classes, file);
 		System.out.println( file.getAbsolutePath() );
