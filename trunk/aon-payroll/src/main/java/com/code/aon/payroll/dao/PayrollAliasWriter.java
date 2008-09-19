6155eee06c114fa739cc0d31d226696aac163aae
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2008-09-19 07:18:46 +0000

    *** empty log message ***

--- trunk/aon-payroll/src/main/java/com/code/aon/payroll/dao/PayrollAliasWriter.java
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/dao/PayrollAliasWriter.java
@@ -7,6 +7,7 @@ import com.code.aon.common.dao.AliasWriter;
 import com.code.aon.payroll.cotizacion.Bonificacion;
 import com.code.aon.payroll.cotizacion.Porcentaje;
 import com.code.aon.payroll.cotizacion.PorcentajeMaestro;
+import com.code.aon.payroll.geograficas.Nacion;
 import com.code.aon.payroll.irpf.Cuota;
 import com.code.aon.payroll.tipos.Autorizacion;
 import com.code.aon.payroll.tipos.Documento;
@@ -15,13 +16,11 @@ import com.code.aon.payroll.tipos.Incidencia;
 import com.code.aon.payroll.tipos.Registro;
 import com.code.aon.payroll.tipos.TipoCnae;
 import com.code.aon.payroll.tipos.Tipovia;
-import com.code.aon.payroll.geograficas.Nacion;
 
 public class PayrollAliasWriter {
 
 	public static void main(String[] args) throws IOException {
 		File file = new File("/AON-PROJECT/aon-payroll/src/main/java/com/code/aon/payroll/dao/IPayrollAlias.java");
-		
 		String[] classes = new String[] {
 				PorcentajeMaestro.class.getName(), 
 				Porcentaje.class.getName(),
@@ -32,9 +31,9 @@ public class PayrollAliasWriter {
 				Empresario.class.getName(),
 				TipoCnae.class.getName(),
 				Bonificacion.class.getName(),
+				Cuota.class.getName(),
 				Tipovia.class.getName(),
-				Nacion.class.getName()};
-
+				Nacion.class.getName() };
 		AliasWriter writer = new AliasWriter("com.code.aon.payroll.dao");
 		writer.write(classes, file);
 		System.out.println( file.getAbsolutePath() );
