93a6d9e2b9d04141cfa8a4f374d055a9916dd9d5
Aimar Tellitu <atellitu@esferalia.com>
2008-09-22 12:41:39 +0000

    *** empty log message ***

---
+++ trunk/aon-ui-marketing/src/main/java/com/code/aon/ui/marketing/controller/CommunicationCenterController.java
@@ -0,0 +1,11 @@
+package com.code.aon.ui.marketing.controller;
+
+import javax.faces.event.ActionEvent;
+
+public class CommunicationCenterController {
+
+	public void onStartSurveyResponse( ActionEvent event ) {
+		
+	}
+	
+}
