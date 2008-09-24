3a468152209d970d14142fcdb65368a0cdac4f25
Aimar Tellitu <atellitu@esferalia.com>
2008-09-24 12:47:51 +0000

    *** empty log message ***

--- aon-marketing/marketing.sql
+++ aon-marketing/marketing.sql
@@ -19,18 +19,18 @@ CREATE TABLE `question_value` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Values of the Question';
 
 CREATE TABLE `survey` (
-  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Campaña',
-  `active` tinyint(1) NOT NULL COMMENT 'Indica si la Campaña esta activa o inactiva',
-  `creationDate` datetime NOT NULL COMMENT 'Fecha de cracion de la Campaña',
-  `description` varchar(64) collate latin1_spanish_ci NOT NULL COMMENT 'Descripción de la Campaña',
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico del Cuestionario',
+  `active` tinyint(1) NOT NULL COMMENT 'Indica si el Cuestionario esta activa o inactiva',
+  `creationDate` datetime NOT NULL COMMENT 'Fecha de cracion del Cuestionario',
+  `description` varchar(64) collate latin1_spanish_ci NOT NULL COMMENT 'Descripción del Cuestionario',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Survey';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Questionario';
 
 CREATE TABLE `survey_question` (
-  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unicdo de la Pregunta de la Campaña',
-  `survey` int(4) NOT NULL COMMENT 'Identificador de la Campaaña',
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Pregunta del Cuestionario',
+  `survey` int(4) NOT NULL COMMENT 'Identificador del Cuestionario',
   `question` int(4) NOT NULL COMMENT 'Identificador de la Pregunta',
-  `position` int(11) default NULL COMMENT 'Posicion de la Pregunta dentro de la Campaña',
+  `position` int(11) default NULL COMMENT 'Posicion de la Pregunta dentro del Cuestionario',
   PRIMARY KEY  (`id`),
   KEY `survey` (`survey`),
   KEY `question` (`question`),
@@ -39,10 +39,10 @@ CREATE TABLE `survey_question` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Survey Question';
 
 CREATE TABLE `survey_workflow` (
-  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico del Workflow de Campaña',
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico del Workflow del Cuestionario',
   `questionValue` int(4) default NULL COMMENT 'Identicador del Valor de la Pregunta',
-  `surveyQuestion` int(4) NOT NULL COMMENT 'Identificador de la Pregunta de la Campaña',
-  `nextSurveyQuestion` int(4) NOT NULL COMMENT 'Identificador de la siguiente Pregunta de la Campaña',
+  `surveyQuestion` int(4) NOT NULL COMMENT 'Identificador de la Pregunta del Cuestionario',
+  `nextSurveyQuestion` int(4) NOT NULL COMMENT 'Identificador de la siguiente Pregunta del Cuestionario',
   PRIMARY KEY  (`id`),
   KEY `questionValue` (`questionValue`),
   KEY `surveyQuestion` (`surveyQuestion`),
@@ -53,10 +53,10 @@ CREATE TABLE `survey_workflow` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Survey Workflow';
 
 CREATE TABLE `survey_response` (
-  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Respuesta de la Campaña',
-  `creationDate` datetime NOT NULL COMMENT 'Fecha de la creacion en el sistema de la Respuesta de la Campaña',
-  `response_date` datetime NOT NULL COMMENT 'Fecha de la Respuesta de la Campaña',
-  `survey` int(4) NOT NULL COMMENT 'Identificador de la Campaña',
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Respuesta del Cuestionario',
+  `creationDate` datetime NOT NULL COMMENT 'Fecha de la creacion en el sistema de la Respuesta del Cuestionario',
+  `response_date` datetime NOT NULL COMMENT 'Fecha de la Respuesta del Cuestionario',
+  `survey` int(4) NOT NULL COMMENT 'Identificador del Cuestionario',
   `target` int(4) NOT NULL COMMENT 'Identificador del Cliente Potencial',
   `user` int(4) NOT NULL COMMENT 'Identificador del Usuario',
   PRIMARY KEY  (`id`),
@@ -69,12 +69,12 @@ CREATE TABLE `survey_response` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Survey Reponse';
 
 CREATE TABLE `survey_response_detail` (
-  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico del Detalle de la Respuesta a la Camapaña',
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico del Detalle de la Respuesta del Cuestionario',
   `value_text` varchar(64) collate latin1_spanish_ci default NULL COMMENT 'Valor de tipo Texto',
   `value_date` datetime default NULL COMMENT 'Valor de tipo Fecha',
   `value_number` double(15,3) default NULL COMMENT 'Valor de tipo Numerico',
   `question` int(4) NOT NULL COMMENT 'Identificador de la Pregunta',
-  `surveyResponse` int(4) NOT NULL COMMENT 'Identificador de la Respuesta de la Campaña',
+  `surveyResponse` int(4) NOT NULL COMMENT 'Identificador de la Respuesta del Cuestionario',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Survy Response Detail';
 
@@ -92,3 +92,50 @@ CREATE TABLE `target_profile` (
   CONSTRAINT `FK_TARGET_PROFILE_TARGET` FOREIGN KEY (`target`) REFERENCES `target` (`registry`),
   CONSTRAINT `FK_TARGET_PROFILE_QUESTION` FOREIGN KEY (`question`) REFERENCES `question` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Perfil de Cliente Potencial';
+
+CREATE TABLE `mk_campaign` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Campaña',
+  `active` tinyint(1) NOT NULL COMMENT 'Indica si la Campaña esta activa o inactiva',
+  `description` varchar(64) collate latin1_spanish_ci NOT NULL COMMENT 'Descripción de la Campaña',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Campaña';
+
+CREATE TABLE `mk_action` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Acción de la Campaña',
+  `campaign` int(11) NOT NULL COMMENT 'Identificador de la Campaña',
+  `media_type` int(4) NOT NULL COMMENT 'Tipo de Contacto de la Acción',
+  `start_date` datetime NOT NULL COMMENT 'Fecha de Inicio',
+  `end_date` datetime default NULL COMMENT 'Fecha de Finalización',
+  `survey` int(4) default NULL COMMENT 'Identificador del Questionario',
+  PRIMARY KEY  (`id`),
+  KEY `survey` (`survey`),
+  KEY `campaign` (`campaign`),
+  CONSTRAINT `FK_MK_ACCION_MK_CAMPAIGN` FOREIGN KEY (`campaign`) REFERENCES `mk_campaign` (`id`),
+  CONSTRAINT `FK_MK_ACCTION_SURVEY` FOREIGN KEY (`survey`) REFERENCES `survey` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Acción de la Campaña';
+
+CREATE TABLE `mk_action_target` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico del Cliente Potencial de la Acción de Campaña',
+  `action` int(4) NOT NULL COMMENT 'Identificador de la Acción de Camapaña',
+  `target` int(4) NOT NULL COMMENT 'Identicador del Cliente Potencial',
+  PRIMARY KEY  (`id`),
+  KEY `action` (`action`),
+  KEY `target` (`target`),
+  CONSTRAINT `MK_ACTION_TARGET_TARGET` FOREIGN KEY (`target`) REFERENCES `target` (`registry`),
+  CONSTRAINT `MK_ACTION_TARGET_MK_ACTION` FOREIGN KEY (`action`) REFERENCES `mk_action` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Cliente Potencial de la Acción de Campaña';
+
+CREATE TABLE `mk_action_target_log` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador Unico del Log de la Acción de un Cliente Potencial',
+  `action` int(4) NOT NULL COMMENT 'Identificador de la Acción de la Campaña',
+  `target` int(11) NOT NULL COMMENT 'Identificador del Cliente Potencial',
+  `status` tinyint(2) NOT NULL COMMENT 'Estado del Log de la Acción de un Cliente Potencial',
+  `survey_response` int(4) default NULL COMMENT 'Identificador de la Respuesta de un Cuestionario',
+  PRIMARY KEY  (`id`),
+  KEY `action` (`action`),
+  KEY `target` (`target`),
+  KEY `survey_response` (`survey_response`),
+  CONSTRAINT `MK_ACTION_TARGET_LOG_SURVEY_RESPONSE` FOREIGN KEY (`survey_response`) REFERENCES `survey_response` (`id`),
+  CONSTRAINT `MK_ACTION_TARGET_LOG_MK_ACTION` FOREIGN KEY (`action`) REFERENCES `mk_action` (`id`),
+  CONSTRAINT `MK_ACTION_TARGET_LOG_TARGET` FOREIGN KEY (`target`) REFERENCES `target` (`registry`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ROW_FORMAT=COMPACT COMMENT='Log de la Acción de un Cliente Potencial';
