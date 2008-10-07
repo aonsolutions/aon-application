734880dc8b8753711720b42ccb1beaa31467847f
Jorge Diez <jdiez@esferalia.com>
2008-10-07 13:07:04 +0000

    *** empty log message ***

---
+++ trunk/aon-web-info/src/main/java/com/code/aon/ui/webinfo/velocity/VelocityConstants.java
@@ -0,0 +1,34 @@
+package com.code.aon.ui.webinfo.velocity;
+
+public interface VelocityConstants {
+
+	public static final String
+
+	//APLICATION CONSTANTS
+	APLICATION_NAME				= "webinfo",
+	TEMPLATE_NAME_PARAM			= "WEBINFO_TEMPLATE_NAME",
+	SESSION_CONFIG 				= "servlet_session_cms_config",
+	SESSION_CURRENT_LANGUAGE	= "servlet_session_cms_current_language",
+	DOMAINS_PATH				= "/home/DOMAINS",
+	WEBSITE_PATH				= "WEBSITES",
+	RESOURCE_PATH				= "DOMAIN-RESOURCES",
+
+	CSS_PATH					= "css",
+	CSS_STYLE_FILE				= "style.vm",
+	CSS_STYLE_DEFAULTS			= "default.properties",
+	CSSIMG_PATH					= "img",
+	IMAGES_PATH					= "images",
+
+	MESSAGES_FILE				= "messages",
+
+	TEMPORAL_PATH				= "/home/COMMON-RESOURCES/aon-web-info/temporal",
+
+	//TEMPLATES CONSTANTS
+	TEMPLATE_PATH				= "/home/COMMON-RESOURCES/aon-web-info/templates",
+		
+	//VELOCITY PROPERTIES
+	VELOCITY_FILE_ENCODING		= "iso-8859-1",
+	VELOCITY_LOG_FILE			= "velocity.log";
+	
+
+}
