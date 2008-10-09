945f0c57a138ebbce30625fb23230d03ae0c8b26
Gorka Irazu <girazu@esferalia.com>
2008-10-09 09:12:33 +0000

    Añadido listener InvoicingAddressListener, a raiz de que en el MasterControllerListener de ui.form se metiera en el afterBeanCreated que se lance un cancel de los controladores de detalle. En este caso, el controlador de detalle esta cargado simultaneamente con el de cabecera con lo que no se puede cancelar, asi que me redefino ese funcionamiento con este nuevo listener.

---
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoicingAddressListener.java
@@ -0,0 +1,16 @@
+package com.code.aon.ui.finance.event;
+
+import com.code.aon.ui.form.event.ControllerEvent;
+import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.aon.ui.form.listener.LinesControllerListener;
+
+public class InvoicingAddressListener extends LinesControllerListener {
+
+	@Override
+	public void afterBeanCreated(ControllerEvent event) throws ControllerListenerException {
+		getLinesController().initModel();
+		super.updateDetailCriteria(event.getController(), true);
+		getLinesController().initializeModel();
+	}
+
+}
