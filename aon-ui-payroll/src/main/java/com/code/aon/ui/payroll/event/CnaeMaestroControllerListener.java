e3d8e098b2ac35bd3d663fc9b189498949ae4eb8
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2008-10-10 08:14:07 +0000

    *** empty log message ***

--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/CnaeMaestroControllerListener.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/CnaeMaestroControllerListener.java
@@ -1,8 +1,29 @@
 package com.code.aon.ui.payroll.event;
 
+import java.util.List;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.jaas.auth.util.Util;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ql.ast.Expression;
+import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ql.util.ExpressionUtilities;
+import com.code.aon.ui.form.IController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.aon.ui.util.AonUtil;
+import com.code.aon.payroll.cotizacion.CnaeMaestro;
+import com.code.aon.payroll.cotizacion.Ocupacion;
+import com.code.aon.payroll.cotizacion.OcupacionMaestro;
+import com.code.aon.payroll.cotizacion.Porcentaje;
+import com.code.aon.payroll.dao.IPayrollAlias;
+
 
 public class CnaeMaestroControllerListener extends ControllerAdapter {
 
@@ -31,11 +52,38 @@ public class CnaeMaestroControllerListener extends ControllerAdapter {
 	}
 
 	@Override
-	public void beforeBeanSelected(ControllerEvent event)
+	public void afterBeanSelected(ControllerEvent event)
 			throws ControllerListenerException {
-		// TODO Auto-generated method stub
-		System.out.println("beforeBeanSelected");
-		super.beforeBeanSelected(event);
+		
+		CnaeMaestro cm = (CnaeMaestro)event.getController().getTo();
+		String ocupaciones = cm.getOcupacion();
+		
+		try {
+			Criteria criteria = new Criteria();
+			
+			IManagerBean ocupacionBean = BeanManager.getManagerBean( Ocupacion.class );
+			IController ocupacion = AonUtil.getController("ocupacion");
+			
+			if(ocupaciones!=null){
+				for (int i = 0; i < ocupaciones.length(); i++) {
+					criteria.addOrExpression(ocupacionBean.getFieldName(IPayrollAlias.OCUPACION_OCUPACION_MAESTRO_CDG), ocupaciones.substring(i, i + 1));
+				}
+				criteria.addEqualExpression(ocupacionBean.getFieldName(IPayrollAlias.OCUPACION_OCUPACION_MAESTRO_EXCLUSIVO), "S");
+			} else {
+				criteria.addEqualExpression(ocupacionBean.getFieldName(IPayrollAlias.OCUPACION_ID_CDG), "");
+			}
+			
+			ocupacion.setCriteria(criteria);
+			ocupacion.onSearch(null);
+			
+		} catch (ManagerBeanException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
