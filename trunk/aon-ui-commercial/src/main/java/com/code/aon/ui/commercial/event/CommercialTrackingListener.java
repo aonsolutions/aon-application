e20356b6e05090a6de8e7c350f2402f762b16198
Aimar Tellitu <atellitu@esferalia.com>
2008-10-13 15:35:11 +0000

    Se ha pasado la lista de actividades al collections controller

--- trunk/aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/CommercialTrackingListener.java
+++ trunk/aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/CommercialTrackingListener.java
@@ -5,10 +5,13 @@ import java.util.logging.Logger;
 
 import com.code.aon.commercial.CommercialTracking;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ui.commercial.controller.CommercialCollectionsController;
 import com.code.aon.ui.commercial.controller.CommercialTrackingController;
+import com.code.aon.ui.commercial.controller.ICommercialConstants;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.aon.ui.util.AonUtil;
 
 
 /**
@@ -33,7 +36,8 @@ public class CommercialTrackingListener extends ControllerAdapter {
 			throws ControllerListenerException {
 		CommercialTrackingController controller = (CommercialTrackingController) event.getController();
 		try {
-			controller.refreshActivities();
+	    	CommercialCollectionsController collections = (CommercialCollectionsController) AonUtil.getRegisteredBean(ICommercialConstants.COLLECTIONS_CONTROLLER_NAME);
+			collections.refreshActivities();
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException( e.getMessage(), e );
 		}
@@ -45,7 +49,8 @@ public class CommercialTrackingListener extends ControllerAdapter {
 			throws ControllerListenerException {
 		CommercialTrackingController controller = (CommercialTrackingController) event.getController();
 		try {
-			controller.refreshActivities();
+	    	CommercialCollectionsController collections = (CommercialCollectionsController) AonUtil.getRegisteredBean(ICommercialConstants.COLLECTIONS_CONTROLLER_NAME);
+			collections.refreshActivities();
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException( e.getMessage(), e );
 		}
