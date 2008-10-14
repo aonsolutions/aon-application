eb7afb1b12813738b2f628b2b2c644b885fb0ccf
Inigo Gayarre <igayarre@esferalia.com>
2008-10-14 12:39:05 +0000

    for firefox

--- trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/ArticleDocumentController.java
+++ trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/ArticleDocumentController.java
@@ -36,7 +36,8 @@ public class ArticleDocumentController extends BasicI18nController {
 		UploadItem item = event.getUploadItem();
 		String upload_name = item.getFileName();
 		upload_name = upload_name.replace('\\', '/');
-		upload_name = upload_name.substring(upload_name.lastIndexOf('/'));
+		if (upload_name.lastIndexOf('/')!=-1)
+			upload_name = upload_name.substring(upload_name.lastIndexOf('/'));
 		upload_name = upload_name.replaceAll("[^A-Za-z0-9._-]+", "");
 		String fileName = File.separator+"article_documents";
 		File file_dir = new File( currentPath+fileName);
