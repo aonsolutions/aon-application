c6b5051b4db543b37694358137309de01127431a
Gorka Irazu <girazu@esferalia.com>
2008-10-14 09:09:16 +0000

    Nuevos metodos getters, añaden en el selectItem el objeto entero como clave en lugar del Id.

--- aon-ui-company/src/main/java/com/code/aon/ui/company/controller/CompanyCollectionsController.java
+++ aon-ui-company/src/main/java/com/code/aon/ui/company/controller/CompanyCollectionsController.java
@@ -25,53 +25,37 @@ public class CompanyCollectionsController {
      * @return the addresses of the company
      * @throws ManagerBeanException 
      */
-    @SuppressWarnings("unchecked")
-    public List<SelectItem> getCompanyAddresses() throws ManagerBeanException{
-    	IManagerBean companyBean = BeanManager.getManagerBean(Company.class);
-    	IManagerBean registryAddressBean = BeanManager.getManagerBean(RegistryAddress.class);
-    	Iterator iter = companyBean.getList(null).iterator();
+	@SuppressWarnings("unchecked")
+    public List<SelectItem> getCompanyAddresses() throws ManagerBeanException {
     	LinkedList<SelectItem> addresses = new LinkedList<SelectItem>();
-    	if(iter.hasNext()){
-    		Company company = (Company)iter.next();
+    	IManagerBean companyBean = BeanManager.getManagerBean(Company.class);
+    	Iterator iterator = companyBean.getList(null).iterator();
+    	if(iterator.hasNext()) {
+    		Company company = (Company)iterator.next();
+    		IManagerBean registryAddressBean = BeanManager.getManagerBean(RegistryAddress.class);
     		Criteria criteria = new Criteria();
     		criteria.addEqualExpression(registryAddressBean.getFieldName(IRegistryAlias.REGISTRY_ADDRESS_REGISTRY_ID), company.getId());
     		criteria.addOrder(registryAddressBean.getFieldName(IRegistryAlias.REGISTRY_ADDRESS_ADDRESS));
-    		iter = registryAddressBean.getList(criteria).iterator();
-    		while(iter.hasNext()){
-    			RegistryAddress rAddress = (RegistryAddress)iter.next();
-    			SelectItem item = new SelectItem(rAddress.getId(),rAddress.getAddress());
-    			addresses.add(item);
+    		List<ITransferObject> list = registryAddressBean.getList(criteria);
+    		for (ITransferObject to : list) {
+    			RegistryAddress rAddress = (RegistryAddress)to;
+    			addresses.add(new SelectItem(rAddress, rAddress.getAddress()));
     		}
     	}
     	return addresses;
     }
 
-	public List<SelectItem> getWorkPlaces() throws ManagerBeanException{
+	public List<SelectItem> getWorkPlaces() throws ManagerBeanException {
 		List<SelectItem> workPlaces = new LinkedList<SelectItem>();
-		Iterator<ITransferObject> iter = obtainWorkplaces().iterator();
-		while(iter.hasNext()){
-			WorkPlace workPlace = (WorkPlace)iter.next();
-			workPlaces.add( new SelectItem(workPlace.getId(), workPlace.getId().toString()));
-		}
-		return workPlaces;
-	}
-
-
-	public List<SelectItem> getWorkPlacesDetailed() throws ManagerBeanException{
-		List<SelectItem> workPlaces = new LinkedList<SelectItem>();
-		Iterator<ITransferObject> iter = obtainWorkplaces().iterator();
-		while(iter.hasNext()){
-			WorkPlace workPlace = (WorkPlace)iter.next();
-			workPlaces.add( new SelectItem(workPlace.getId(), workPlace.getDescription()));
-		}
-		return workPlaces;
-	}
-	
-	private List<ITransferObject> obtainWorkplaces() throws ManagerBeanException{
 		IManagerBean workplaceBean = BeanManager.getManagerBean(WorkPlace.class);
 		Criteria criteria = new Criteria();
 		criteria.addOrder(workplaceBean.getFieldName(ICompanyAlias.WORK_PLACE_ID));
-		return workplaceBean.getList(criteria);
-	}
+		List<ITransferObject> list = workplaceBean.getList(criteria);
+		for (ITransferObject to : list) {
+			WorkPlace workPlace = (WorkPlace)to;
+			workPlaces.add(new SelectItem(workPlace, workPlace.getDescription()));
+		}
+		return workPlaces;
+	}	
 
 }
