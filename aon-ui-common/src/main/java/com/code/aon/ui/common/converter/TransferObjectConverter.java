142f8cc82f700a7868c7866191a82ff5005d9e17
Euke Castellano <ecastellano@esferalia.com>
2008-10-22 12:51:03 +0000

    *** empty log message ***

--- aon-ui-common/src/main/java/com/code/aon/ui/common/converter/TransferObjectConverter.java
+++ aon-ui-common/src/main/java/com/code/aon/ui/common/converter/TransferObjectConverter.java
@@ -1,5 +1,7 @@
 package com.code.aon.ui.common.converter;
 
+import java.lang.reflect.Field;
+
 import javax.el.ValueExpression;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -8,11 +10,13 @@ import javax.faces.convert.ConverterException;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang.ClassUtils;
+import org.hibernate.metadata.ClassMetadata;
 
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 
 /**
  * 
@@ -20,20 +24,43 @@ import com.code.aon.common.ManagerBeanException;
  */
 public class TransferObjectConverter implements Converter {
 
-	/* (non-Javadoc)
-	 * @see javax.faces.convert.Converter#getAsObject(javax.faces.context.FacesContext, javax.faces.component.UIComponent, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.faces.convert.Converter#getAsObject(javax.faces.context.FacesContext
+	 * , javax.faces.component.UIComponent, java.lang.String)
 	 */
-	@SuppressWarnings("unchecked")
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
 		ValueExpression vb = component.getValueExpression("value");
-		Class toType = vb == null ? null : vb.getType(context.getELContext());
+		Class<?> toType = vb == null ? null : vb.getType(context.getELContext());
 		if (toType != null) {
 			if (value != null) {
 				try {
 					IManagerBean bean = BeanManager.getManagerBean(toType);
-					ITransferObject to = bean.get(Integer.valueOf(value));
+					ClassMetadata cm = HibernateUtil.getSessionFactory().getClassMetadata(toType);
+					String id = cm.getIdentifierPropertyName();
+					Field field = toType.getDeclaredField(id);
+					Class<?> typeClass = field.getType();
+					ITransferObject to = null;
+					if (typeClass.equals(String.class)) {
+						to = bean.get(value);
+					} else if (typeClass.equals(Integer.class)) {
+						to = bean.get(Integer.valueOf(value));
+					} else {
+						throw new ConverterException(
+								"Unsupported PK type, types supported: " 
+								+"java.lang.String and java.lang.Integer. Found "
+								+ toType.getName());
+					}
 					return to;
 				} catch (ManagerBeanException e) {
+					throw new ConverterException("Unable to find "
+							+ ClassUtils.getShortClassName(toType) + " with id " + value);
+				} catch (SecurityException e) {
+					throw new ConverterException("Unable to find "
+							+ ClassUtils.getShortClassName(toType) + " with id " + value);
+				} catch (NoSuchFieldException e) {
 					throw new ConverterException("Unable to find "
 							+ ClassUtils.getShortClassName(toType) + " with id " + value);
 				}
@@ -43,8 +70,12 @@ public class TransferObjectConverter implements Converter {
 		throw new ConverterException("Unable to find selectItems with TransferObject values.");
 	}
 
-	/* (non-Javadoc)
-	 * @see javax.faces.convert.Converter#getAsString(javax.faces.context.FacesContext, javax.faces.component.UIComponent, java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.faces.convert.Converter#getAsString(javax.faces.context.FacesContext
+	 * , javax.faces.component.UIComponent, java.lang.Object)
 	 */
 	public String getAsString(FacesContext context, UIComponent component, Object value) {
 		if (value == null) {
@@ -52,7 +83,10 @@ public class TransferObjectConverter implements Converter {
 		}
 		String id = null;
 		try {
-			id = BeanUtils.getProperty(value, "id");
+			Class<?> type = value.getClass();
+			ClassMetadata cm = HibernateUtil.getSessionFactory().getClassMetadata(type);
+			String idName = cm.getIdentifierPropertyName();
+			id = BeanUtils.getProperty(value, idName);
 		} catch (Throwable th) {
 			throw new ConverterException("Unable to get id from " + value);
 		}
