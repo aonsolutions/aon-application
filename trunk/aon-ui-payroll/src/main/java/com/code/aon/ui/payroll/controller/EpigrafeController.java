07027d6f11fc3c76b7f34f2398258d21376bcfcd
David Uriarte <duriarte@esferalia.com>
2008-10-28 14:00:30 +0000

    *** empty log message ***

--- trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/EpigrafeController.java
+++ trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/EpigrafeController.java
@@ -1,25 +1,17 @@
 package com.code.aon.ui.payroll.controller;
 
-import java.lang.reflect.InvocationTargetException;
-import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
-import javax.faces.validator.ValidatorException;
 
-import org.apache.commons.beanutils.PropertyUtils;
-
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.payroll.cotizacion.Linepigr;
 import com.code.aon.payroll.enumeration.Epigrafes;
-import com.code.aon.payroll.enumeration.Prorateo;
 import com.code.aon.ui.form.LinesController;
-import com.code.aon.ui.util.AonUtil;
 
 public class EpigrafeController extends LinesController {
 
@@ -27,11 +19,6 @@ public class EpigrafeController extends LinesController {
 	
 	private List<SelectItem> epigrafes;
 
-
-	
-	
-	
-	
 	public List<SelectItem> getListaEpigrafes() {
 		if(epigrafes==null){
 			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
@@ -49,7 +36,7 @@ public class EpigrafeController extends LinesController {
 		
 			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
 			String s ;
-			s = ((Linepigr)this.getTo()).getEpi().getName(locale);
+			s = ((Linepigr)this.getTo()).getIndit().getName(locale);
 		    return s;
 	}
 	
--- trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/EpigrafeMaestroController.java
+++ trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/EpigrafeMaestroController.java
@@ -1,7 +1,9 @@
 package com.code.aon.ui.payroll.controller;
 
 import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ui.form.BasicController;
 
 public class EpigrafeMaestroController extends BasicController {
@@ -10,4 +12,11 @@ public class EpigrafeMaestroController extends BasicController {
 		// TODO Auto-generated method stub
 	}
 
+	public void addDirectEqualExpression(ValueChangeEvent event) throws ManagerBeanException {
+		if (event.getNewValue() != null) {
+			String property = event.getComponent().getId().replace('_', '.');
+			getCriteria().addEqualExpression(property, event.getNewValue());
+		}
+	}			
+	
 }
