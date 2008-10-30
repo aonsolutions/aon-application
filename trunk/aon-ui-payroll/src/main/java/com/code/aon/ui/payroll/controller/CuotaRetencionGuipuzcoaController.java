2ca9f9ee18e9d9766d814e616d365bdd83f79908
David Uriarte <duriarte@esferalia.com>
2008-10-30 16:28:33 +0000

    *** empty log message ***

---
+++ trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/CuotaRetencionGuipuzcoaController.java
@@ -0,0 +1,57 @@
+package com.code.aon.ui.payroll.controller;
+
+import java.util.Date;
+
+import javax.faces.event.ActionEvent;
+
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.payroll.dao.IPayrollAlias;
+
+public class CuotaRetencionGuipuzcoaController extends PayrollBasicController {
+
+	private Date searchFecini;
+	private Date searchFecfin;
+	
+	public Date getSearchFecini() {
+		return searchFecini;
+	}
+
+	public void setSearchFecini(Date searchFecini) {
+		this.searchFecini = searchFecini;
+	}
+
+	public Date getSearchFecfin() {
+		return searchFecfin;
+	}
+
+	public void setSearchFecfin(Date searchFecfin) {
+		this.searchFecfin = searchFecfin;
+	}
+
+	//añade la fecha al criteria para realizar busquedas
+	@Override
+	public void onSearch(ActionEvent event) {
+	
+		try {
+			if(searchFecini != null){
+				getCriteria().addEqualExpression(getFieldName(IPayrollAlias.CUOTA_RETENCION_GUIPUZCOA_ID_FECINI), searchFecini);
+			}
+			if(searchFecfin != null){
+			
+				getCriteria().addEqualExpression(getFieldName(IPayrollAlias.CUOTA_RETENCION_GUIPUZCOA_FECFIN), searchFecfin);
+			}
+		} catch (ManagerBeanException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		searchFecini=null;
+		searchFecfin=null;
+		
+		super.onSearch(event);
+	}
+	
+
+
+
+
+}
