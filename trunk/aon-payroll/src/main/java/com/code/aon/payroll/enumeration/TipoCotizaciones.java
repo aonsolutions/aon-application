7b58ee575b7c34433e6b5499bd16ed9a9e66bc86
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2008-10-31 16:16:45 +0000

    *** empty log message ***

--- trunk/aon-payroll/src/main/java/com/code/aon/payroll/enumeration/TipoCotizaciones.java
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/enumeration/TipoCotizaciones.java
@@ -4,25 +4,32 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 import com.code.aon.common.enumeration.IResourceable;
+import com.code.aon.common.enumeration.IStringEnum;
 
 /**
  * The Enum EnumCotizaciones.
  */
-public enum TipoCotizaciones implements IResourceable {
+public enum TipoCotizaciones implements IResourceable, IStringEnum {
 
-	Cot1,
-	Cot2,
-	Cot3,
-	Cot4,
-	Cot5,
-	Cot6,
-	Cot7;
+	COT1("1"),
+	COT2("2"),
+	COT3("3"),
+	COT4("4"),
+	COT5("5"),
+	COT6("6"),
+	COT7("7");
 	
 	private static final String BASE_NAME = "com.code.aon.payroll.i18n.messages";
 
 	/** Message key prefix. */
     private static final String MSG_KEY_PREFIX = "aon_enum_cotizaciones_";
 	
+    private String value;
+    
+    TipoCotizaciones( String value ) {
+      	this.value = value;
+  	}
+    
 	/**
 	 * Returns a <code>String</code> with the transalation <code>Locale</code>
 	 * for the locale.
@@ -35,4 +42,9 @@ public enum TipoCotizaciones implements IResourceable {
         ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
 		return bundle.getString(MSG_KEY_PREFIX + toString());
     }
+    
+    @Override
+	public String getValue() {
+		return value;
+	}
 }
