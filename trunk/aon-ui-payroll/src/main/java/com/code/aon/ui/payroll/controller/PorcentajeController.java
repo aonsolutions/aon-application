d9813311eeda95cd8d322afd55d09cab6e58b4b8
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2008-10-31 11:31:40 +0000

    *** empty log message ***

--- trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/PorcentajeController.java
+++ trunk/aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/PorcentajeController.java
@@ -2,6 +2,7 @@ package com.code.aon.ui.payroll.controller;
 
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
+import java.util.Date;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -11,6 +12,8 @@ import javax.faces.validator.ValidatorException;
 
 import org.apache.commons.beanutils.PropertyUtils;
 
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.payroll.dao.IPayrollAlias;
 import com.code.aon.ui.form.LinesController;
 import com.code.aon.ui.util.AonUtil;
 
@@ -53,5 +56,7 @@ public class PorcentajeController extends LinesController {
 			throw new ValidatorException( AonUtil.getMessage( ctx, e.getMessage(), null ) );
 		}
 	}
+	
+	
 
 }
