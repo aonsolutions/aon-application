b7774f0ffaf689bd2ffa31da297ef67c26deb9d6
Aimar Tellitu <atellitu@esferalia.com>
2008-10-31 10:38:31 +0000

    *** empty log message ***

--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/IPayrollConstants.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/IPayrollConstants.java
@@ -4,6 +4,7 @@ public interface IPayrollConstants {
 
 	String EMPTY_STRING = "";
 	
+	String PAIS_CONTROLLER_NAME = "pais";
 	String COMUNIDAD_CONTROLLER_NAME = "comunidad";
 	String PROVINCIA_CONTROLLER_NAME = "provincia";
 	String OCUPACION_CONTROLLER_NAME = "ocupacion";
--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/PaisControllerListener.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/PaisControllerListener.java
@@ -1,16 +1,10 @@
 package com.code.aon.ui.payroll.event;
 
 
-import com.code.aon.common.BeanManager;
-import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.payroll.cotizacion.Ocupacion;
 import com.code.aon.payroll.dao.IPayrollAlias;
-import com.code.aon.payroll.geograficas.Comunidad;
-import com.code.aon.payroll.geograficas.Provincia;
 import com.code.aon.ql.Criteria;
-import com.code.aon.ui.form.BasicController;
-import com.code.aon.ui.form.IController;
+import com.code.aon.ui.form.LinesController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -20,71 +14,27 @@ import com.code.aon.ui.util.AonUtil;
 public class PaisControllerListener extends ControllerAdapter implements IPayrollConstants {
 
 	@Override
-		public void afterBeanSelected(ControllerEvent event)
-				throws ControllerListenerException {
-		/*
-		BasicController comunidad = (BasicController)AonUtil.getController(Comunidad.class.getName());
-		BasicController provincia= (BasicController)AonUtil.getController(Provincia.class.getName());
-		
-		comunidad.initializeModel();
-		provincia.initializeModel();
-		*/
-		
-		}	
-	
-	@Override
-	public void afterBeanCreated(ControllerEvent event)
+	public void afterModelInitialized(ControllerEvent event)
 			throws ControllerListenerException {
-		BasicController comunidad = (BasicController)AonUtil.getController(Comunidad.class.getName());
-		BasicController provincia= (BasicController)AonUtil.getController(Provincia.class.getName());
-		
-		comunidad.initializeModel();
-		provincia.initializeModel();
+		resetComunidadModel();
 	}
 	
 	@Override
 	public void afterBeanCanceled(ControllerEvent event)
 			throws ControllerListenerException {
-		IController comunidad = AonUtil.getController(COMUNIDAD_CONTROLLER_NAME);
-		IController provincia= AonUtil.getController(PROVINCIA_CONTROLLER_NAME);
-		try {
-			IManagerBean comunidadBean = BeanManager.getManagerBean( Comunidad.class );
-			IManagerBean provinciaBean = BeanManager.getManagerBean( Provincia.class );
-			
-			//if(!event.getController().isNew()){
-				Criteria criteria = new Criteria();
-				criteria.addEqualExpression(comunidadBean.getFieldName(IPayrollAlias.COMUNIDAD_CDG), "");
-				comunidad.setCriteria(criteria);
-				criteria = new Criteria();
-				criteria.addEqualExpression(provinciaBean.getFieldName(IPayrollAlias.PROVINCIA_CDG), "");
-				provincia.setCriteria(criteria);
-			//}
-		} catch (ManagerBeanException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		/*
-		comunidad.initializeModel();
-		provincia.initializeModel();
-		*/
-		try {
-			System.out.println("COMUNIDAD------------------------"+comunidad.getModel().getRowCount());
-			System.out.println("PROVINCIA------------------------"+provincia.getModel().getRowCount());
-		} catch (ManagerBeanException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		/*
-		try {
-			IManagerBean comu = BeanManager.getManagerBean(Comunidad.class);
-			
-			comu.initializePOJO(null);
-			
-		} catch (ManagerBeanException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
-		
+		resetComunidadModel();
 	}
+
+	private void resetComunidadModel() throws ControllerListenerException {
+		LinesController comunidadController = (LinesController) AonUtil.getController(COMUNIDAD_CONTROLLER_NAME);
+		try {
+			comunidadController.clearCriteria();
+			Criteria criteria = comunidadController.getCriteria();
+			criteria.addNullExpression(comunidadController.getFieldName(IPayrollAlias.COMUNIDAD_PAIS_CDG));
+			comunidadController.initializeModel();
+		} catch (ManagerBeanException e) {
+			throw new ControllerListenerException( e.getMessage(), e );			
+		}		
+	}
+	
 }
