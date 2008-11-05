bd669db5e27765aa7faea6fafb8c628554d90285
Aimar Tellitu <atellitu@esferalia.com>
2008-11-05 09:32:06 +0000

    *** empty log message ***

--- aon-audit/src/main/java/com/code/aon/audit/Application.java
+++ aon-audit/src/main/java/com/code/aon/audit/Application.java
@@ -1,9 +1,7 @@
 package com.code.aon.audit;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
