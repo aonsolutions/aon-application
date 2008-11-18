4c8ae794113c1b3e2b8828cfc4a30862c98ad3af
Gorka Irazu <girazu@esferalia.com>
2008-11-18 13:18:02 +0000

    Refactor de Tax: pasa de aon-product a aon-config.

--- aon-product/src/main/java/com/code/aon/product/dao/ProductAliasWriter.java
+++ aon-product/src/main/java/com/code/aon/product/dao/ProductAliasWriter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.code.aon.common.dao.AliasWriter;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.product.Brand;
 import com.code.aon.product.Catalogue;
 import com.code.aon.product.CatalogueCategory;
@@ -17,8 +18,6 @@ import com.code.aon.product.ProductCategoryGroup;
 import com.code.aon.product.ProductCategoryTree;
 import com.code.aon.product.Tariff;
 import com.code.aon.product.TariffCatalogue;
-import com.code.aon.product.Tax;
-import com.code.aon.product.TaxDetail;
 
 /**
  * @author Consulting & Development. ecastellano - 22/01/2007
@@ -32,7 +31,7 @@ public class ProductAliasWriter {
 	 */
 	public static void main(String[] args) throws IOException {
 		File file = new File("/AON-PROJECT/aon-product/src/main/java/com/code/aon/product/dao/IProductAlias.java");
-		String[] classes = new String[15]; 
+		String[] classes = new String[13]; 
 		classes[0] = Brand.class.getName();
 		classes[1] = Catalogue.class.getName();
 		classes[2] = CatalogueCategory.class.getName();
@@ -46,8 +45,7 @@ public class ProductAliasWriter {
 		classes[10] = ProductCategoryTree.class.getName();
 		classes[11] = Tariff.class.getName();
 		classes[12] = TariffCatalogue.class.getName();
-		classes[13] = Tax.class.getName();
-		classes[14] = TaxDetail.class.getName();
+		HibernateUtil.getSessionFactory();
 		AliasWriter writer = new AliasWriter("com.code.aon.product.dao");
 		writer.write(classes, file);
 		System.out.println( file.getAbsolutePath() );
