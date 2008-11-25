939069761ed2e1a3fac06e30cc6464e7f954af81
Aimar Tellitu <atellitu@esferalia.com>
2008-11-25 08:32:23 +0000

    *** empty log message ***

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/FinanceFractionController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/FinanceFractionController.java
@@ -22,36 +22,39 @@ import com.code.aon.finance.enumeration.FinanceTrackingType;
 import com.code.aon.finance.invoicing.FinanceTrackingWriter;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ui.form.PageDataModel;
 import com.code.aon.ui.util.AonUtil;
 
 public class FinanceFractionController {
 
 	private static final String FINANCE_CONTROLLER_NAME = "finance";
 	
-	private DataModel fractionModel;
+	private DataModel model;
 	
 	private Finance currentFinance;
 	
 	private Finance targetFinance;
 
+	private String beanName;
+	
 	private boolean isNew;
 	
-	public DataModel getFractionModel() {
-		if(fractionModel == null){
-			fractionModel = new ListDataModel(new LinkedList<Finance>());
+	public DataModel getModel() {
+		if(model == null){
+			model = new ListDataModel(new LinkedList<Finance>());
 		}
-		return fractionModel;
+		return model;
 	}
 
 	public void setFractionModel(DataModel fractionModel) {
-		this.fractionModel = fractionModel;
+		this.model = fractionModel;
 	}
 
-	public Finance getCurrentFinance() {
+	public Finance getTo() {
 		return currentFinance;
 	}
 
-	public void setCurrentFinance(Finance currentFinance) {
+	public void setTo(Finance currentFinance) {
 		this.currentFinance = currentFinance;
 	}
 
@@ -70,40 +73,52 @@ public class FinanceFractionController {
 	public void setNew(boolean isNew) {
 		this.isNew = isNew;
 	}
+	
+	public String getBeanName() {
+		return beanName;
+	}
+
+	public void setBeanName(String beanName) {
+		this.beanName = beanName;
+	}
+	
+	public int getPageLimit() {
+		return PageDataModel.LIMIT;
+	}	
 
 	@SuppressWarnings({"unused","unchecked"})
 	public void onFractionFinance(ActionEvent event) throws ManagerBeanException{
 		initializeController();
 		FinanceController financeController = (FinanceController)AonUtil.getController(FINANCE_CONTROLLER_NAME);
-		List list = (List)getFractionModel().getWrappedData();
+		List list = (List)getModel().getWrappedData();
 		targetFinance = (Finance)financeController.getTo();
 		Finance finance = initializeFinance();
 		finance.setAmount(targetFinance.getAmount());
 		finance.setExpenses(targetFinance.getExpenses());
 		finance.setId(targetFinance.getId());
 		list.add(finance);
-		getFractionModel().setWrappedData(list);
+		getModel().setWrappedData(list);
 	}
 	
 	@SuppressWarnings({"unused","unchecked"})
 	public void onAcceptFractions(ActionEvent event) throws ManagerBeanException{
 		try{
 			IManagerBean financeBean = BeanManager.getManagerBean(Finance.class);
-			List list = (List)getFractionModel().getWrappedData();
+			List list = (List)getModel().getWrappedData();
 			Finance finance = (Finance) list.get(0);
 			targetFinance.setAmount(finance.getAmount());
 			targetFinance.setBank((targetFinance.getBank().getId() == null?null:targetFinance.getBank()));
 			targetFinance.setPayMethod((targetFinance.getPayMethod().getId() == null?null:targetFinance.getPayMethod()));
 			financeBean.update(targetFinance);
 			ResourceBundle bundle = ResourceBundle.getBundle(AonUtil.getConfigurationController().getApplicationBundles().get("financeBundle"),FacesContext.getCurrentInstance().getViewRoot().getLocale());
-			FinanceTrackingWriter.addFinanceTracking(targetFinance, FinanceTrackingType.FRACTIONED, bundle.getString("aon_finance_tracking_fractioned"));
+			FinanceTrackingWriter.addFinanceTracking(targetFinance, FinanceTrackingType.FRACTIONED, bundle.getString("finance_tracking_fractioned"));
 			for(int i = 1;i<list.size();i++){
 				finance = (Finance)list.get(i);
 				finance.setBank((finance.getBank().getId() == null?null:finance.getBank()));
 				finance.setPayMethod((finance.getPayMethod().getId() == null?null:finance.getPayMethod()));
 				financeBean.insert(finance);
 			}
-			initializeFinanceControllerList(((List)getFractionModel().getWrappedData()));
+			initializeFinanceControllerList(((List)getModel().getWrappedData()));
 		}catch (AonException e) {
 			AonUtil.addErrorMessage(e.getMessage());
 			throw new AbortProcessingException(e);
@@ -113,7 +128,7 @@ public class FinanceFractionController {
 	private void initializeController() {
 		this.setNew(false);
 		this.currentFinance = null;
-		this.fractionModel = null;
+		this.model = null;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -136,42 +151,50 @@ public class FinanceFractionController {
 	}
 
 	@SuppressWarnings("unused")
-	public void onNewFraction(ActionEvent event){
+	public void onReset(ActionEvent event){
 		this.isNew = true;
 		this.currentFinance = initializeFinance();
 		this.currentFinance.setAmount(obtainPendingAmount());
 	}
 	
 	@SuppressWarnings("unused")
-	public void onSelectFraction(ActionEvent event){
-		this.currentFinance = (Finance)getFractionModel().getRowData();
+	public void onSelect(ActionEvent event){
+		this.currentFinance = (Finance)getModel().getRowData();
+	}
+	
+	public void onAccept(ActionEvent event) {
+		if (isNew) {
+			onAddFraction(event);
+		} else {
+			onUpdateFraction(event);
+		}
 	}
 	
 	@SuppressWarnings({"unchecked", "unused"})
-	public void onAddFraction(ActionEvent event){
-		((LinkedList)getFractionModel().getWrappedData()).add(this.currentFinance);
+	private void onAddFraction(ActionEvent event){
+		((LinkedList)getModel().getWrappedData()).add(this.currentFinance);
 		this.currentFinance = null;
 		this.setNew(false);
 	}
 	
 	@SuppressWarnings({"unused","unchecked"})
-	public void onRemoveFraction(ActionEvent event){
-		((LinkedList)getFractionModel().getWrappedData()).remove(this.currentFinance);
+	public void onRemove(ActionEvent event){
+		((LinkedList)getModel().getWrappedData()).remove(this.currentFinance);
 		this.currentFinance = null;
 		this.setNew(false);
 	}
 
 	@SuppressWarnings("unused")
-	public void onCancelFraction(ActionEvent event){
+	public void onCancel(ActionEvent event){
 		this.currentFinance = null;
 		this.setNew(false);
 	}
 	
 	@SuppressWarnings({"unused", "unchecked"})
-	public void onUpdateFraction(ActionEvent event){
-		int i = ((LinkedList)getFractionModel().getWrappedData()).indexOf(this.currentFinance);
-		((LinkedList)getFractionModel().getWrappedData()).remove(i);
-		((LinkedList)getFractionModel().getWrappedData()).add(i, this.currentFinance);
+	private void onUpdateFraction(ActionEvent event){
+		int i = ((LinkedList)getModel().getWrappedData()).indexOf(this.currentFinance);
+		((LinkedList)getModel().getWrappedData()).remove(i);
+		((LinkedList)getModel().getWrappedData()).add(i, this.currentFinance);
 		this.currentFinance = null;
 	}
 
@@ -193,7 +216,7 @@ public class FinanceFractionController {
 	@SuppressWarnings("unchecked")
 	private double obtainPendingAmount() {
 		double pending = targetFinance.getTotalAmount();
-		Iterator iter =  ((List)getFractionModel().getWrappedData()).iterator();
+		Iterator iter =  ((List)getModel().getWrappedData()).iterator();
 		while(iter.hasNext()){
 			Finance finance = (Finance)iter.next();
 			pending -= finance.getTotalAmount();
@@ -202,11 +225,11 @@ public class FinanceFractionController {
 	}
 	
 	public boolean isFractionable(){
-		return (getFractionModel().getRowCount() > 1 && obtainPendingAmount() == 0 && getCurrentFinance() == null );
+		return (getModel().getRowCount() > 1 && obtainPendingAmount() == 0 && getTo() == null );
 	}
 	
 	public boolean isRemovableFraction(){
-		return (getCurrentFinance().getId() == null);
+		return (getTo() != null) && (getTo().getId() == null);
 	}
 
 	private double round(double value, int precision) {
