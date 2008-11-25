3d40549405979c95b70c5c6a95ae96d66c542a2e
Aimar Tellitu <atellitu@esferalia.com>
2008-11-25 13:54:56 +0000

    *** empty log message ***

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/PurchaseDirectInvoicingControllerListener.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/PurchaseDirectInvoicingControllerListener.java
@@ -2,7 +2,6 @@ package com.code.aon.ui.finance.event;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.finance.Invoice;
-import com.code.aon.finance.dao.IFinanceAlias;
 import com.code.aon.finance.enumeration.InvoiceStatus;
 import com.code.aon.finance.enumeration.InvoiceType;
 import com.code.aon.ui.finance.controller.PurchaseDirectInvoicingController;
@@ -11,15 +10,6 @@ import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
 
 public class PurchaseDirectInvoicingControllerListener extends ControllerAdapter {
-	
-	@Override
-	public void beforeModelInitialized(ControllerEvent event) throws ControllerListenerException {
-		try {
-			event.getController().getCriteria().addEqualExpression(event.getController().getFieldName(IFinanceAlias.INVOICE_TYPE), InvoiceType.PURCHASE);
-		} catch (ManagerBeanException e) {
-			throw new ControllerListenerException(e);
-		}
-	}
 	
 	@Override
 	public void beforeBeanAdded(ControllerEvent event) throws ControllerListenerException {
