5f1957eaa06a80e1d17aa596096e8af88104ee71
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2008-11-25 16:41:00 +0000

    *** empty log message ***

---
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/EmpresaControllerListener.java
@@ -0,0 +1,12 @@
+package com.code.aon.ui.payroll.event;
+
+
+import com.code.aon.ui.form.event.ControllerAdapter;
+import com.code.aon.ui.payroll.controller.IPayrollConstants;
+
+
+public class EmpresaControllerListener extends ControllerAdapter implements IPayrollConstants {
+	
+	
+
+}
