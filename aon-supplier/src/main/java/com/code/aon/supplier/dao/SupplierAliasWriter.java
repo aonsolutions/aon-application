bbf4f98f4538afae078e876326243df36013d977
Aimar Tellitu <atellitu@esferalia.com>
2008-12-03 08:16:44 +0000

    Se ha actualizado a la nueva API de HibernateUtil

--- aon-supplier/src/main/java/com/code/aon/supplier/dao/SupplierAliasWriter.java
+++ aon-supplier/src/main/java/com/code/aon/supplier/dao/SupplierAliasWriter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.code.aon.common.dao.AliasWriter;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.supplier.Supplier;
 import com.code.aon.supplier.SupplierSegment;
 
@@ -11,9 +12,12 @@ public class SupplierAliasWriter {
 
 	public static void main(String[] args) throws IOException {
 		File file = new File("/AON-PROJECT/aon-supplier/src/main/java/com/code/aon/supplier/dao/ISupplierAlias.java");
-        String[] classes = new String[2];
-        classes[0] = Supplier.class.getName();
-        classes[1] = SupplierSegment.class.getName();
+		String[] classes = new String[] {
+			Supplier.class.getName(),
+			SupplierSegment.class.getName()
+		};
+		String sessionFactoryName = HibernateUtil.getSessionFactoryName(Supplier.class.getName());
+		HibernateUtil.getSessionFactory(sessionFactoryName);
 		AliasWriter writer = new AliasWriter("com.code.aon.supplier.dao");
 		writer.write(classes,file);
 		System.out.println("Alias generados");
