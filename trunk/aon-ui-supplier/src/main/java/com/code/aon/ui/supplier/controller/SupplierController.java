7845de50c9f823513e454c14f3bf42beb85171f2
Aimar Tellitu <atellitu@esferalia.com>
2008-12-04 10:44:12 +0000

    Hace falta el id en los panelTab para que guarde el seleccionado

--- trunk/aon-ui-supplier/src/main/java/com/code/aon/ui/supplier/controller/SupplierController.java
+++ trunk/aon-ui-supplier/src/main/java/com/code/aon/ui/supplier/controller/SupplierController.java
@@ -6,8 +6,18 @@ import com.code.aon.ui.form.BasicController;
  * Controller used in the supplier maintenance.
  */
 public class SupplierController extends BasicController {
+
+	private String selectedTab;
 	
 	private boolean showFinanceRegistryBank = true;
+	
+	public String getSelectedTab() {
+		return selectedTab;
+	}
+
+	public void setSelectedTab(String selectedTab) {
+		this.selectedTab = selectedTab;
+	}
 	
 	public boolean isShowFinanceRegistryBank() {
 		return showFinanceRegistryBank;
