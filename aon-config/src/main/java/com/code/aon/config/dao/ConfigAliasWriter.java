5930f3957972eda0d6a2f0d010e49eeb82e3ed1f
Gorka Irazu <girazu@esferalia.com>
2008-12-09 15:36:03 +0000

    *** empty log message ***

--- aon-config/src/main/java/com/code/aon/config/dao/ConfigAliasWriter.java
+++ aon-config/src/main/java/com/code/aon/config/dao/ConfigAliasWriter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.code.aon.common.dao.AliasWriter;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.config.ApplicationParameter;
 import com.code.aon.config.Bank;
 import com.code.aon.config.PayMethod;
@@ -30,12 +31,13 @@ public class ConfigAliasWriter {
 		classes[2] = PayMethod.class.getName();
 		classes[3] = Scope.class.getName();
 		classes[4] = Series.class.getName();
-		classes[5] = User.class.getName();
-		classes[6] = UserScope.class.getName();
-		classes[7] = UserWorkGroup.class.getName();
-		classes[8] = Tax.class.getName();
-		classes[9] = TaxDetail.class.getName();
+		classes[5] = Tax.class.getName();
+		classes[6] = TaxDetail.class.getName();
+		classes[7] = User.class.getName();
+		classes[8] = UserScope.class.getName();
+		classes[9] = UserWorkGroup.class.getName();
 		classes[10] = WorkGroup.class.getName();
+		HibernateUtil.getSessionFactory();
 		AliasWriter writer = new AliasWriter("com.code.aon.config.dao");
 		writer.write(classes, file);
 		System.out.println( file.getAbsolutePath() );
