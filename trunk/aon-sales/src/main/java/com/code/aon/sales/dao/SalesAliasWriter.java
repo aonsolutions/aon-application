8973c290bed5886509537a21dab52309a4121948
Gorka Irazu <girazu@esferalia.com>
2008-12-11 07:39:59 +0000

    *** empty log message ***

--- trunk/aon-sales/src/main/java/com/code/aon/sales/dao/SalesAliasWriter.java
+++ trunk/aon-sales/src/main/java/com/code/aon/sales/dao/SalesAliasWriter.java
@@ -9,19 +9,17 @@ import com.code.aon.sales.Sales;
 import com.code.aon.sales.SalesAttachment;
 import com.code.aon.sales.SalesDetail;
 import com.code.aon.sales.SalesType;
-import com.code.aon.sales.Seller;
 
 public class SalesAliasWriter {
 
 	public static void main(String[] args) throws Exception{
 		File file = new File("/AON-PROJECT/aon-sales/src/main/java/com/code/aon/sales/dao/ISalesAlias.java");
-		String[] classes = new String[6];
+		String[] classes = new String[5];
 		classes[0] = Sales.class.getName();
 		classes[1] = SalesAttachment.class.getName();
 		classes[2] = SalesDetail.class.getName();
 		classes[3] = SalesType.class.getName();
-		classes[4] = Seller.class.getName();
-		classes[5] = PointOfSale.class.getName();
+		classes[4] = PointOfSale.class.getName();
 		HibernateUtil.getSessionFactory();
 		AliasWriter writer = new AliasWriter("com.code.aon.sales.dao");
 		writer.write(classes, file);
