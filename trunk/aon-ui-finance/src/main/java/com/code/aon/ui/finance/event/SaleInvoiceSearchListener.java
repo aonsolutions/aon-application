30b3f8114bdedd54ff98ed30d08ff8108e0f8017
Aimar Tellitu <atellitu@esferalia.com>
2008-12-15 15:49:15 +0000

    *** empty log message ***

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/SaleInvoiceSearchListener.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/SaleInvoiceSearchListener.java
@@ -2,7 +2,10 @@ package com.code.aon.ui.finance.event;
 
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.config.Bank;
 import com.code.aon.finance.dao.IFinanceAlias;
 import com.code.aon.product.Item;
 import com.code.aon.ql.Criteria;
@@ -14,12 +17,22 @@ public class SaleInvoiceSearchListener extends ControllerSearchListener {
 	
 	private Registry registry;
 	
+	private Integer fromNumber;
+	
+	private Integer toNumber;
+	
 	private Date issueDateFrom;
 	
 	private Date issueDateTo;
 
     private Item item;
+
+	private Bank bank;
+    
+	private Date dueDateFrom;
 	
+	private Date dueDateTo;
+    
 	public Registry getRegistry() {
 		return registry;
 	}
@@ -27,7 +40,24 @@ public class SaleInvoiceSearchListener extends ControllerSearchListener {
 	public void setRegistry(Registry registry) {
 		this.registry = registry;
 	}
-		
+	
+	
+	public Integer getFromNumber() {
+		return fromNumber;
+	}
+
+	public void setFromNumber(Integer fromNumber) {
+		this.fromNumber = fromNumber;
+	}
+
+	public Integer getToNumber() {
+		return toNumber;
+	}
+
+	public void setToNumber(Integer toNumber) {
+		this.toNumber = toNumber;
+	}
+	
 	public Date getIssueDateFrom() {
 		return issueDateFrom;
 	}
@@ -43,6 +73,22 @@ public class SaleInvoiceSearchListener extends ControllerSearchListener {
 	public void setIssueDateTo(Date issueDateTo) {
 		this.issueDateTo = issueDateTo;
 	}
+	
+	public Date getDueDateFrom() {
+		return dueDateFrom;
+	}
+
+	public void setDueDateFrom(Date dueDateFrom) {
+		this.dueDateFrom = dueDateFrom;
+	}
+
+	public Date getDueDateTo() {
+		return dueDateTo;
+	}
+
+	public void setDueDateTo(Date dueDateTo) {
+		this.dueDateTo = dueDateTo;
+	}
 
 	public Item getItem() {
 		return item;
@@ -52,12 +98,25 @@ public class SaleInvoiceSearchListener extends ControllerSearchListener {
 		this.item = item;
 	}
 
+	public Bank getBank() {
+		return bank;
+	}
+
+	public void setBank(Bank bank) {
+		this.bank = bank;
+	}
+	
 	@Override
 	protected void init() throws ManagerBeanException {
+		setFromNumber(null);
+		setToNumber(null);				
 		setIssueDateFrom(null);
 		setIssueDateTo(null);		
 		setRegistry( new Registry() );
 		setItem( new Item() );
+		setDueDateFrom(null);
+		setDueDateTo(null);
+		setBank( new Bank() );
 	}
 	
 	@Override
@@ -66,6 +125,12 @@ public class SaleInvoiceSearchListener extends ControllerSearchListener {
 		if ( (getRegistry() != null) && (getRegistry().getId() != null) ) {
 			criteria.addEqualExpression(getFieldName(IFinanceAlias.INVOICE_REGISTRY_ID), getRegistry().getId());
 		}		
+		if(getFromNumber() != null){
+			criteria.addGreaterThanOrEqualExpression(getFieldName(IFinanceAlias.INVOICE_NUMBER), getFromNumber());
+		}
+		if(getToNumber() !=  null){
+			criteria.addLessThanOrEqualExpression(getFieldName(IFinanceAlias.INVOICE_NUMBER), getToNumber());
+		}				
 		if(getIssueDateFrom() != null){
 			criteria.addGreaterThanOrEqualExpression(getFieldName(IFinanceAlias.INVOICE_ISSUE_DATE), getIssueDateFrom());
 		}
@@ -74,6 +139,15 @@ public class SaleInvoiceSearchListener extends ControllerSearchListener {
 		}		
 		if ( (getItem() != null) && (getItem().getId() != null) ) {
 			criteria.addEqualExpression("Invoice.lines.item.id", getItem().getId());
+		}		
+		if ( getDueDateFrom() != null ) {
+			criteria.addGreaterThanOrEqualExpression("Invoice.finances.dueDate", getDueDateFrom());
+		}
+		if ( getDueDateTo() != null ) {
+			criteria.addGreaterThanOrEqualExpression("Invoice.finances.dueDate", getDueDateTo());			
+		}
+		if ( (getBank() != null) && (!StringUtils.isEmpty(getBank().getCode())) ) {
+			criteria.addEqualExpression("Invoice.finances.bank.code", getBank().getCode());			
 		}		
 	}	
 
