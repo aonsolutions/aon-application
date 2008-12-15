5de6477b34802c426ba306a898ec2c4ae2f79962
Gorka Irazu <girazu@esferalia.com>
2008-12-15 08:11:45 +0000

    *** empty log message ***

--- aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/TargetSearchListener.java
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/TargetSearchListener.java
@@ -15,7 +15,7 @@ import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.registry.enumeration.MediaType;
-import com.code.aon.sales.Seller;
+import com.code.aon.seller.Seller;
 import com.code.aon.ui.commercial.controller.CommercialCollectionsController;
 import com.code.aon.ui.commercial.controller.ICommercialConstants;
 import com.code.aon.ui.form.event.ControllerSearchListener;
