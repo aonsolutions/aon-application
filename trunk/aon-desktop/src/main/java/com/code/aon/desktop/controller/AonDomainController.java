f2a8d316f41b0e7d79a6077c4731d2d2e2250026
Aimar Tellitu <atellitu@esferalia.com>
2008-12-19 09:41:15 +0000

    Se comprueba que se seleccione una fila

--- trunk/aon-desktop/src/main/java/com/code/aon/desktop/controller/AonDomainController.java
+++ trunk/aon-desktop/src/main/java/com/code/aon/desktop/controller/AonDomainController.java
@@ -209,19 +209,24 @@ public class AonDomainController extends BasicController implements IAonObjectCl
 	}
 	
 	public String getCurrentUserProfiles() {
-		IRelation user = (IRelation) this.users.getRowData();
-		List<String> relations = user.relations();
-		if ( (relations != null) && (!relations.isEmpty()) ) {
-			String profiles = StringUtils.join(relations.toArray(), ", ");
-			return StringUtils.abbreviate(profiles, 140);
+		if ( this.users.isRowAvailable() ) {
+			IRelation user = (IRelation) this.users.getRowData();
+			List<String> relations = user.relations();
+			if ( (relations != null) && (!relations.isEmpty()) ) {
+				String profiles = StringUtils.join(relations.toArray(), ", ");
+				return StringUtils.abbreviate(profiles, 140);
+			}
 		}
 		return "";
 	}	
 
 	public String getCurrentUserName() {
-		IRelation user = (IRelation) this.users.getRowData();
-		AonUserController userController = (AonUserController) AonUtil.getRegisteredBean("currentUser");		
-		return userController.getUserName(user.getId());
+		if ( this.users.isRowAvailable() ) {
+			IRelation user = (IRelation) this.users.getRowData();
+			AonUserController userController = (AonUserController) AonUtil.getRegisteredBean("currentUser");		
+			return userController.getUserName(user.getId());
+		}
+		return "";
 	}	
 	
 	@SuppressWarnings("unchecked")
@@ -391,8 +396,11 @@ public class AonDomainController extends BasicController implements IAonObjectCl
 	}
 
 	public boolean isCurrentSystemProfile() {
-		Relation profile = (Relation)this.profiles.getRowData();
-		return isSystemProfile(profile.getId());
+		if ( this.profiles.isRowAvailable() ) {
+			Relation profile = (Relation)this.profiles.getRowData();
+			return isSystemProfile(profile.getId());
+		}
+		return false;
 	}
 
 	public boolean isUserManagement() {
