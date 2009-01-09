f30a4682aa2de34b6f9ad63e0a98453056291cd2
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-01-09 10:01:28 +0000

    *** empty log message ***

--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/Impresos190Controller.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/Impresos190Controller.java
@@ -2,12 +2,18 @@ package com.code.aon.ui.payroll.controller;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.faces.event.ActionEvent;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.payroll.auxiliares.Admon;
+import com.code.aon.payroll.dao.IPayrollAlias;
+import com.code.aon.payroll.geograficas.Provincia;
+import com.code.aon.payroll.principales.empresa.Empresa;
 import com.code.aon.payroll.resultados.irpf.Impresos11x;
 import com.code.aon.payroll.resultados.irpf.Impresos190;
 
@@ -64,6 +70,73 @@ public class Impresos190Controller extends PayrollBasicController {
 		
 		String cdg = ((Impresos190) getTo()).getEmprnif().getAdmon().getCdg().substring(0, 2);
 		((Impresos190) getTo()).getProvincia().setCdg(cdg);
+	}
+	
+	
+	
+	private Empresa emprnif;
+	private Admon admon;
+	private Provincia provincia;
+	
+	@Override
+	public void onEditSearch(ActionEvent arg0) {
+		
+		super.onEditSearch(arg0);
+		
+		emprnif = new Empresa();
+		admon = new Admon();
+		provincia = new Provincia();
+		
+	}
+
+	/**
+	 * Se incluyen manualmente a las búsquedas los campos lookup y de fechas 
+	 */
+	@Override
+	public void onSearch(ActionEvent event) {
+
+		try {
+			//Búsqueda por campos LookUp
+			if (emprnif!=null && (emprnif.getCdg() != null)) {
+				getCriteria().addEqualExpression(getFieldName(IPayrollAlias.IMPRESOS190_EMPRNIF_CDG), emprnif.getCdg());
+			}
+			if (admon!=null && StringUtils.isNotEmpty(admon.getCdg())) {
+				getCriteria().addEqualExpression(getFieldName(IPayrollAlias.IMPRESOS190_ADMON_CDG), admon.getCdg());
+			}
+			if (provincia!=null && StringUtils.isNotEmpty(provincia.getCdg())) {
+				getCriteria().addEqualExpression(getFieldName(IPayrollAlias.IMPRESOS190_PROVINCIA_CDG), provincia.getCdg());
+			}
+			
+		} catch (ManagerBeanException e) {
+			LOGGER.log( Level.SEVERE, e.getMessage(), e );
+			e.printStackTrace();
+		}
+
+		super.onSearch(event);
+	}
+
+	public Empresa getEmprnif() {
+		return emprnif;
+	}
+
+	public void setEmprnif(Empresa emprnif) {
+		this.emprnif = emprnif;
+	}
+
+	public Admon getAdmon() {
+		return admon;
+	}
+
+	public void setAdmon(Admon admon) {
+		this.admon = admon;
+	}
+
+	public Provincia getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(Provincia provincia) {
+		this.provincia = provincia;
 	}
 
 
