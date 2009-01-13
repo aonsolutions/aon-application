8094f803bee3045265cccbf60543776ab97a0cc3
David Uriarte <duriarte@esferalia.com>
2009-01-13 15:15:20 +0000

    *** empty log message ***

---
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/NominaController.java
@@ -0,0 +1,31 @@
+package com.code.aon.ui.payroll.controller;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.payroll.auxiliares.Admon;
+import com.code.aon.payroll.dao.IPayrollAlias;
+import com.code.aon.payroll.enumeration.ModalidadImpuesto;
+import com.code.aon.payroll.enumeration.TipoImpreso;
+import com.code.aon.payroll.geograficas.Provincia;
+import com.code.aon.payroll.principales.empresa.Empresa;
+import com.code.aon.payroll.resultados.irpf.Impresos11x;
+
+public class NominaController extends PayrollBasicController {
+
+
+	
+}
