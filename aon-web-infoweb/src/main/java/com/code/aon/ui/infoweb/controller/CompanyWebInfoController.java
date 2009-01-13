10f25bca1cb9e6d70a8391a83b1f6deb5e759d0d
Aimar Tellitu <atellitu@esferalia.com>
2009-01-13 08:14:01 +0000

    *** empty log message ***

--- aon-web-infoweb/src/main/java/com/code/aon/ui/infoweb/controller/CompanyWebInfoController.java
+++ aon-web-infoweb/src/main/java/com/code/aon/ui/infoweb/controller/CompanyWebInfoController.java
@@ -8,12 +8,12 @@ import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.company.Company;
-import com.code.aon.ql.Criteria;
-import com.code.aon.ui.form.BasicController;
-import com.code.aon.ui.form.IController;
-import com.code.aon.ui.util.AonUtil;
 import com.code.aon.infoweb.WebInfo;
 import com.code.aon.infoweb.dao.IWebInfoAlias;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ui.form.BasicController;
+import com.code.aon.ui.form.FormUtil;
+import com.code.aon.ui.form.IController;
 
 public class CompanyWebInfoController extends BasicController {
 	
@@ -21,7 +21,7 @@ public class CompanyWebInfoController extends BasicController {
 
 	@SuppressWarnings({"unchecked", "unused"})
 	public void onLoadWebInfo(ActionEvent event) throws ManagerBeanException{
-		IController companyController = (IController) AonUtil.getController(COMPANY_CONTROLLER_NAME);
+		IController companyController = (IController) FormUtil.getController(COMPANY_CONTROLLER_NAME);
 		Company company = ((Company)companyController.getTo());
 		IManagerBean webInfoBean = BeanManager.getManagerBean(WebInfo.class);
 		Criteria criteria = new Criteria();
