96e472df4880e30e0e39402029535e70dae4d31c
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-01-14 13:46:28 +0000

    *** empty log message ***

---
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/resultados/salarios/Finidto.java
@@ -0,0 +1,78 @@
+package com.code.aon.payroll.resultados.salarios;
+
+import java.math.BigDecimal;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.code.aon.common.ITransferObject;
+
+/**
+ * Finidto
+ */
+@Entity
+// @Lines(joinProperty="finiquito")
+@Table(name = "finidto")
+public class Finidto implements ITransferObject {
+
+	private FinidtoPK id;
+	private String texto;
+	private BigDecimal importe;
+	private Finiquito finiquito;
+
+	@EmbeddedId
+	@AttributeOverrides( {
+			@AttributeOverride(name = "cdg", column = @Column(name = "cdg", nullable = false, length = 4)),
+			@AttributeOverride(name = "orden", column = @Column(name = "orden", nullable = false, length = 2)) })
+	public FinidtoPK getId() {
+		return this.id;
+	}
+
+	public void setId(FinidtoPK id) {
+		this.id = id;
+	}
+
+	/**
+	 * Texto Descuento
+	 * @return
+	 */
+	@Column(name = "texto", nullable = false, length = 40)
+	public String getTexto() {
+		return this.texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	/**
+	 * Importe Descuento
+	 * @return
+	 */
+	@Column(name = "importe", precision = 11)
+	public BigDecimal getImporte() {
+		return this.importe;
+	}
+
+	public void setImporte(BigDecimal importe) {
+		this.importe = importe;
+	}
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "cdg", insertable = false, updatable = false)
+	public Finiquito getFiniquito() {
+		return this.finiquito;
+	}
+
+	public void setFiniquito(Finiquito finiquito) {
+		this.finiquito = finiquito;
+	}
+
+}
