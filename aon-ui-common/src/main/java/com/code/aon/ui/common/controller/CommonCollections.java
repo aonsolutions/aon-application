bcda3fd5ea89876855cd5848bcded14d0ba7d314
Euke Castellano <ecastellano@esferalia.com>
2009-01-21 09:45:55 +0000

    Se chachean las colecciones de Meses y SecurityLevel.

--- aon-ui-common/src/main/java/com/code/aon/ui/common/controller/CommonCollections.java
+++ aon-ui-common/src/main/java/com/code/aon/ui/common/controller/CommonCollections.java
@@ -1,8 +1,10 @@
 package com.code.aon.ui.common.controller;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
@@ -15,23 +17,28 @@ import com.code.aon.common.enumeration.SecurityLevel;
  * 
  */
 public class CommonCollections {
-
+	Map<Locale,List<SelectItem>> months = new HashMap<Locale,List<SelectItem>>();  
+	Map<Locale,List<SelectItem>> levels = new HashMap<Locale,List<SelectItem>>();
+	
 	/**
      * Get year months.
      * 
      * @return the address types
      */
 	public List<SelectItem> getMonths(){
-		List<SelectItem> monthList = new LinkedList<SelectItem>();
 		Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-		Month[] months = Month.values();
-		for (int i = 0; i < months.length; i++) {
-			Month month = months[i];
-			String name = month.getName(locale);
-			SelectItem item = new SelectItem(month, name);
-			monthList.add(item);
+		if (months.get(locale) == null) {
+			List<SelectItem> monthList = new LinkedList<SelectItem>();
+			Month[] m = Month.values();
+			for (int i = 0; i < m.length; i++) {
+				Month month = m[i];
+				String name = month.getName(locale);
+				SelectItem item = new SelectItem(month, name);
+				monthList.add(item);
+			}
+			months.put(locale,monthList);			
 		}
-		return monthList;
+		return months.get(locale);
 	}
 
 	/**
@@ -39,13 +46,16 @@ public class CommonCollections {
 	 */
 	public List<SelectItem> getSecurityLevels() {
 		Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-		LinkedList<SelectItem> levels = new LinkedList<SelectItem>();
-		for (SecurityLevel level : SecurityLevel.values()) {
-			String name = level.getName(locale);
-			SelectItem item = new SelectItem(level, name);
-			levels.add(item);
+		if (levels.get(locale) == null) {
+			List<SelectItem> levelList = new LinkedList<SelectItem>();
+			for (SecurityLevel level : SecurityLevel.values()) {
+				String name = level.getName(locale);
+				SelectItem item = new SelectItem(level, name);
+				levelList.add(item);
+			}
+			levels.put(locale,levelList);			
 		}
-		return levels;
+		return levels.get(locale);
 	}
 
 }
