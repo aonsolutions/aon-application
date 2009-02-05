2864100f6ad22c76934fe039d91e0507fe5f6382
Aimar Tellitu <atellitu@esferalia.com>
2009-02-05 09:51:23 +0000

    Los date y los rangos ya no hacen falta tenerlos en los searchListener

--- aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesSearchListener.java
+++ aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesSearchListener.java
@@ -1,7 +1,5 @@
 package com.code.aon.ui.sales.event;
 
-import java.util.Date;
-
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.customer.Customer;
 import com.code.aon.ql.Criteria;
@@ -11,27 +9,7 @@ import com.code.aon.ui.form.event.ControllerSearchListener;
 
 public class SalesSearchListener extends ControllerSearchListener {
 
-	private Date issueDateFrom;
-	
-	private Date issueDateTo;
-
 	private Customer customer;
-
-	public Date getIssueDateFrom() {
-		return issueDateFrom;
-	}
-
-	public void setIssueDateFrom(Date issueDateFrom) {
-		this.issueDateFrom = issueDateFrom;
-	}
-
-	public Date getIssueDateTo() {
-		return issueDateTo;
-	}
-
-	public void setIssueDateTo(Date issueDateTo) {
-		this.issueDateTo = issueDateTo;
-	}
 
 	public Customer getCustomer() {
 		return customer;
@@ -43,20 +21,12 @@ public class SalesSearchListener extends ControllerSearchListener {
 
 	@Override
 	protected void init() throws ManagerBeanException {
-		setIssueDateFrom(null);
-		setIssueDateTo(null);
 		setCustomer(new Customer());
 	}
 	
 	@Override
 	protected void completeCriteria() throws ManagerBeanException, ExpressionException {
 		Criteria criteria = getController().getCriteria();
-		if (getIssueDateFrom() != null) {
-			criteria.addGreaterThanOrEqualExpression(getController().getFieldName(ISalesAlias.SALES_ISSUE_DATE), getIssueDateFrom());
-		}
-		if (getIssueDateTo() != null) {
-			criteria.addLessThanOrEqualExpression(getController().getFieldName(ISalesAlias.SALES_ISSUE_DATE), getIssueDateTo());
-		}
 		if (getCustomer() != null && getCustomer().getId() != null) {
 			criteria.addEqualExpression(getController().getFieldName(ISalesAlias.SALES_CUSTOMER_ID), getCustomer().getId());			
 		}
