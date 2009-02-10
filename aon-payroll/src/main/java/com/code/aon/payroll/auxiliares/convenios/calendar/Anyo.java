09d7340f3a3b6f02cc3cdfc99bfe97cbfda5eb24
David Uriarte <duriarte@esferalia.com>
2009-02-10 16:35:01 +0000

    *** empty log message ***

---
+++ aon-payroll/src/main/java/com/code/aon/payroll/auxiliares/convenios/calendar/Anyo.java
@@ -0,0 +1,38 @@
+package com.code.aon.payroll.auxiliares.convenios.calendar;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.code.aon.common.ITransferObject;
+
+
+public class Anyo {
+	
+	
+	
+	 List <Mes> listaMeses;
+	 
+	 
+	
+	public Anyo() {
+		listaMeses = new ArrayList <Mes>();
+	}
+	public List<Mes> getListaMeses() {
+		return listaMeses;
+	}
+	public void setListaMeses(List<Mes> lista) {
+		listaMeses = lista;
+	}
+	 
+	 
+
+
+
+
+
+
+
+	
+	
+}
--- aon-payroll/src/main/java/com/code/aon/payroll/auxiliares/convenios/calendar/Mes.java
+++ aon-payroll/src/main/java/com/code/aon/payroll/auxiliares/convenios/calendar/Mes.java
@@ -1,24 +1,51 @@
 package com.code.aon.payroll.auxiliares.convenios.calendar;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
-
-import com.code.aon.common.ITransferObject;
 
 
 public class Mes {
 	
+	 List <Calendario> listaDias;
+	 private Integer cdg;
+	 private String desc;
+	 
 	
+	public Integer getCdg() {
+		return cdg;
+	}
+	public void setCdg(Integer cdg) {
+		this.cdg = cdg;
+	}
+	public List<Calendario> getlistaDias() {
+		return listaDias;
+	}
+	public void setlistaDias(List<Calendario> lista) {
+		this.listaDias = lista;
+	}
+	
+	public void addDia(Calendario d)
+		
+	{
+		this.listaDias.add(d);
+	}
+	
+	public Mes() {
+		
+		listaDias = new ArrayList <Calendario>();
 
-	List<Calendario> listadias;
-
-	public List<Calendario> getListadias() {
-		return listadias;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
 	}
 
-	public void setListadias(List<Calendario> listadias) {
-		this.listadias = listadias;
-	}
+	 
+	 
+
+
 
 
 
