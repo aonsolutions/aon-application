aa963a081d8625fd911a036e7b9aa2b50a8b388e
Jorge Diez <jdiez@esferalia.com>
2009-02-16 16:40:29 +0000

    *** empty log message ***

--- aon-cms/src/main/java/com/code/aon/cms/dao/ICMSAlias.java
+++ aon-cms/src/main/java/com/code/aon/cms/dao/ICMSAlias.java
@@ -745,28 +745,34 @@ public interface ICMSAlias {
 	DAOConstantsEntry LINK_DETAIL_ENTRY = DAOConstants.getDAOConstant(LinkDetail.class);
 
 	/** 
+	* Alias value: LinkDetail_description
+	* Hibernate value: LinkDetail.description
+	*/
+	String  LINK_DETAIL_DESCRIPTION = LINK_DETAIL_ENTRY.getAliasNames()[0];
+
+	/** 
 	* Alias value: LinkDetail_id
 	* Hibernate value: LinkDetail.id
 	*/
-	String  LINK_DETAIL_ID = LINK_DETAIL_ENTRY.getAliasNames()[0];
+	String  LINK_DETAIL_ID = LINK_DETAIL_ENTRY.getAliasNames()[1];
 
 	/** 
 	* Alias value: LinkDetail_label
 	* Hibernate value: LinkDetail.label
 	*/
-	String  LINK_DETAIL_LABEL = LINK_DETAIL_ENTRY.getAliasNames()[1];
+	String  LINK_DETAIL_LABEL = LINK_DETAIL_ENTRY.getAliasNames()[2];
 
 	/** 
 	* Alias value: LinkDetail_language_id
 	* Hibernate value: LinkDetail.language.id
 	*/
-	String  LINK_DETAIL_LANGUAGE_ID = LINK_DETAIL_ENTRY.getAliasNames()[2];
+	String  LINK_DETAIL_LANGUAGE_ID = LINK_DETAIL_ENTRY.getAliasNames()[3];
 
 	/** 
 	* Alias value: LinkDetail_link_id
 	* Hibernate value: LinkDetail.link.id
 	*/
-	String  LINK_DETAIL_LINK_ID = LINK_DETAIL_ENTRY.getAliasNames()[3];
+	String  LINK_DETAIL_LINK_ID = LINK_DETAIL_ENTRY.getAliasNames()[4];
 
 
 
