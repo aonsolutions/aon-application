163a985f626682d330c8f790dc1038c515698427
Aimar Tellitu <atellitu@esferalia.com>
2009-02-20 10:42:19 +0000

    *** empty log message ***

--- aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/controller/MessageController.java
+++ aon-ui-webmail/src/main/java/com/code/aon/ui/webmail/controller/MessageController.java
@@ -304,6 +304,17 @@ public class MessageController implements WebMailConstants, BundleConstants, IAo
 		}    	
 	}
 
+    public void purgeSelectedMessage(ActionEvent event){
+    	AonMessage[] messagesLst = new AonMessage[] { this.message };
+    	FolderController folderController = (FolderController) AonUtil.getRegisteredBean(BEAN_FOLDER);
+    	try {
+    		folderController.deleteMessages(messagesLst, true);
+		} catch (MessagingException e) {
+			AonUtil.addErrorMessage(e.getMessage());
+			throw new AbortProcessingException(e);
+		}    	
+	}
+    
     public List<AonFile> getFiles(){
     	return newMsgFileList;
     }
