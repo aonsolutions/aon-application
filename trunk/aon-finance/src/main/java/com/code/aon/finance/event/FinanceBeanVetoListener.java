9d8d42724054a8e4aaf6050b3c6b00394a90665d
Euke Castellano <ecastellano@esferalia.com>
2009-02-25 11:01:30 +0000

    *** empty log message ***

--- trunk/aon-finance/src/main/java/com/code/aon/finance/event/FinanceBeanVetoListener.java
+++ trunk/aon-finance/src/main/java/com/code/aon/finance/event/FinanceBeanVetoListener.java
@@ -1,10 +1,13 @@
 package com.code.aon.finance.event;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
 import com.code.aon.config.BankAccount;
 import com.code.aon.finance.Finance;
+import com.code.aon.finance.Invoice;
 
 public class FinanceBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 
@@ -12,14 +15,27 @@ public class FinanceBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 	public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
 		Finance finance = (Finance)evt.getTo();
 		checkFinance(finance);
+		fillConcept(finance);
 	}
 
 	@Override
 	public void vetoableBeanUpdated(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
 		Finance finance = (Finance)evt.getTo();
 		checkFinance(finance);
+		fillConcept(finance);
 	}
-	
+
+	private void fillConcept(Finance finance) {
+		if (StringUtils.isEmpty(finance.getConcept())) {
+			Invoice invoice = finance.getInvoice();
+			String concept = invoice.getReferenceCode();
+			if (invoice.getRegistryName()!=null && !invoice.getRegistryName().equals( invoice.getRegistry().getFullName())) {
+				concept = StringUtils.abbreviate(invoice.getReferenceCode() + " - " + invoice.getRegistryName(), 64);
+			}
+	        finance.setConcept( concept ); 
+		}
+	}
+
 	private void checkFinance(Finance finance) throws ManagerBeanVetoListenerException {
 		if(finance.getAmount() == 0){
 			throw new ManagerBeanVetoListenerException("El importe del vencimiento no puede ser 0.0");
