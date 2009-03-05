fca86265b3e05b71067537cbc93b18461b9d0817
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-03-05 11:44:31 +0000

    *** empty log message ***

--- trunk/aon-payroll/src/main/java/com/code/aon/payroll/resultados/seguros/Tc1.java
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/resultados/seguros/Tc1.java
@@ -693,7 +693,7 @@ public class Tc1  implements ITransferObject {
     }
     @Temporal(TemporalType.DATE)
 	//@DataDefinition(label="Fecha Concesion Bonif.")
-    @Column(name="fecha", length=10)
+    @Column(name="fecha")
     public Date getFecha() {
         return this.fecha;
     }
@@ -804,7 +804,7 @@ public class Tc1  implements ITransferObject {
     }
     @Temporal(TemporalType.DATE)
 	//@DataDefinition(label="Fecha Creacion Fila")
-    @Column(name="fecnew", length=10)
+    @Column(name="fecnew")
     public Date getFecnew() {
         return this.fecnew;
     }
@@ -814,7 +814,7 @@ public class Tc1  implements ITransferObject {
     }
     @Temporal(TemporalType.TIME)
 	//@DataDefinition(label="Hora Creacion Fila")
-    @Column(name="hornew", length=8)
+    @Column(name="hornew")
     public Date getHornew() {
         return this.hornew;
     }
@@ -824,7 +824,7 @@ public class Tc1  implements ITransferObject {
     }
     @Temporal(TemporalType.DATE)
 	//@DataDefinition(label="Fecha Modificacion Fila")
-    @Column(name="fecmod", length=10)
+    @Column(name="fecmod")
     public Date getFecmod() {
         return this.fecmod;
     }
@@ -834,7 +834,7 @@ public class Tc1  implements ITransferObject {
     }
     @Temporal(TemporalType.TIME)
 	//@DataDefinition(label="Hora Modificacion Fila")
-    @Column(name="hormod", length=8)
+    @Column(name="hormod")
     public Date getHormod() {
         return this.hormod;
     }
