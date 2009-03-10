8f77ba47b1797c72a9173e8172b775065ee264b6
David Uriarte <duriarte@esferalia.com>
2009-03-10 09:09:52 +0000

    *** empty log message ***

--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/HttavisoController.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/HttavisoController.java
@@ -1,7 +1,15 @@
 package com.code.aon.ui.payroll.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+
 import com.code.aon.payroll.avanzadas.hojastrabajo.Httaviso;
 import com.code.aon.payroll.avanzadas.hojastrabajo.Httrabajador;
+import com.code.aon.payroll.enumeration.TipoAviso;
 import com.code.aon.ui.form.FormUtil;
 import com.code.aon.ui.form.LinesController;
 
@@ -21,7 +29,21 @@ public class HttavisoController extends LinesController {
 	}
 	
     
+	private List<SelectItem> avisos;
 	
+	public List<SelectItem> getListaavisos() {
+		if(avisos==null){
+			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+			avisos = new LinkedList<SelectItem>();
+			for (TipoAviso paga : TipoAviso.values()) {
+				String name = paga.getName( locale );
+				SelectItem item = new SelectItem( paga, name );
+				avisos.add(item);
+			}
+		}
+		return avisos;
+	}
+
 }
 
 
--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/HttbonificacionController.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/HttbonificacionController.java
@@ -14,7 +14,7 @@ public class HttbonificacionController extends LinesController {
 	public void generateCdg(){
 		
 		Integer cdg= ((Httrabajador)(FormUtil.getController(IPayrollConstants.HTTRABAJADOR_CONTROLLER_NAME)).getTo()).getCdg();
-		Integer orden= Integer.parseInt(Utils.maxCode("Httbonificacion","id.orden"));
+		Integer orden= Integer.parseInt(Utils.maxCode("Httbonificacion","id.orden","id.cdg="+cdg));
 		
 		((Httbonificacion)getTo()).getId().setCdg(((Httrabajador)(FormUtil.getController(IPayrollConstants.HTTRABAJADOR_CONTROLLER_NAME)).getTo()).getCdg());
 		((Httbonificacion)getTo()).getId().setOrden(orden +1);
