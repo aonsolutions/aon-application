093baab5c43dbb84aba94137d1d218e51909bb92
Euke Castellano <ecastellano@esferalia.com>
2009-03-11 21:43:19 +0000

    Presupuestos

--- trunk/aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/AccountBudgetController.java
+++ trunk/aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/AccountBudgetController.java
@@ -3,9 +3,9 @@ package com.code.aon.ui.accounting.controller;
 import com.code.aon.account.Account;
 import com.code.aon.accounting.Period;
 import com.code.aon.common.enumeration.Month;
-import com.code.aon.ui.form.LinesController;
+import com.code.aon.ui.form.BasicController;
 
-public class AccountBudgetController extends LinesController {
+public class AccountBudgetController extends BasicController {
 
 	private Account account;
 	private Month month;
