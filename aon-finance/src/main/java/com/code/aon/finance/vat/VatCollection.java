3c9a7e5eb12fef6c63e3a34e0b646c22f49b1c13
Euke Castellano <ecastellano@esferalia.com>
2009-03-11 09:20:02 +0000

    *** empty log message ***

--- aon-finance/src/main/java/com/code/aon/finance/vat/VatCollection.java
+++ aon-finance/src/main/java/com/code/aon/finance/vat/VatCollection.java
@@ -90,10 +90,11 @@ public class VatCollection {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
-		StringWriter stmt = new StringWriter();
+			String operation = "CEIL((i.investment+ELT((i.type+1),20,10,20,20)) / 10) "; 
+			StringWriter stmt = new StringWriter();
 			stmt.append(" SELECT i.type,i.transaction,i.issue_date,i.reference_code,i.rdocument,i.rname ");
-			stmt.append("  ,it.percentage,it.surcharge,id.taxable_base, ");
-			stmt.append("  CEIL((i.investment+ELT((i.type+1),20,10,20,20)) / 10) vatType ");
+			stmt.append("  ,it.percentage,it.surcharge,SUM(id.taxable_base), ");
+			stmt.append(operation + " vatType ");
 			stmt.append("  FROM invoice_tax it ");
 			stmt.append("  INNER JOIN invoice_detail id ON (it.invoice_detail = id.id) ");
 			stmt.append("  INNER JOIN invoice i ON (id.invoice = i.id) ");
@@ -111,7 +112,7 @@ public class VatCollection {
 				stmt.append(" AND it.surcharge = ?");
 			}
 			if (params.getVatType() != null) {
-				stmt.append(" AND CEIL((i.investment+ELT((i.type+1),20,10,20,20)) / 10) = "+ (params.getVatType().ordinal() + 1));	
+				stmt.append(" AND " + operation + " = "+ (params.getVatType().ordinal() + 1));	
 			}
 			if (params.getVatReportType() != null) {
 				if (params.getVatReportType() == VatReportType.GENERAL) {
@@ -129,6 +130,9 @@ public class VatCollection {
 			if (params.getSecurityLevel() != null) {
 				stmt.append(" AND i.security_level = " + params.getSecurityLevel().ordinal());
 			}
+			stmt.append(" GROUP BY i.type,i.transaction,i.issue_date,i.reference_code,i.rdocument,i.rname ");
+			stmt.append("  ,it.percentage,it.surcharge,");
+			stmt.append(operation);
 			stmt.append(" ORDER BY vatType,i.transaction,i.issue_date,i.reference_code");
 			
 			ps = HibernateUtil.getSQLConnection().prepareStatement(stmt.toString(),
