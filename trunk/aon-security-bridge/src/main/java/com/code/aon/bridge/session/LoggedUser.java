94a8edef8c61cf72cdbe17405cb76e9f33b89f3c
Aimar Tellitu <atellitu@esferalia.com>
2009-03-23 14:14:56 +0000

    Se ha añadido el método para role

--- trunk/aon-security-bridge/src/main/java/com/code/aon/bridge/session/LoggedUser.java
+++ trunk/aon-security-bridge/src/main/java/com/code/aon/bridge/session/LoggedUser.java
@@ -1,6 +1,12 @@
 package com.code.aon.bridge.session;
 
+import java.util.AbstractMap;
+import java.util.Set;
+
+import javax.faces.context.FacesContext;
 import javax.naming.Name;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.code.aon.bridge.plugin.Utils;
 import com.code.aon.jaas.auth.AuthPrincipal;
@@ -16,6 +22,8 @@ public class LoggedUser implements ILdapConstants, IAonObjectClasses {
 	private boolean logged;
 	
 	private Entry aonUser;
+	
+	private static final FakeMap USER_IN_ROLE = new FakeMap();
 	
 	public LoggedUser() {
 		AuthPrincipal principal = Utils.getAuthPrincipal();
@@ -49,5 +57,34 @@ public class LoggedUser implements ILdapConstants, IAonObjectClasses {
     	}
         return userName;
     }    
+    
+	public FakeMap getUserInRole() {
+		return USER_IN_ROLE;
+	}
+    
+	@SuppressWarnings("unchecked")
+	private static class FakeMap extends AbstractMap<String,Boolean> {
+		
+		@Override
+		public Set entrySet() {
+			return null;
+		}
+		
+		@Override
+		public Boolean get(Object key) {
+			if ( key != null ) {
+				String value = key.toString();
+				String[] roles = StringUtils.split(value, ", " );
+				FacesContext ctx = FacesContext.getCurrentInstance();
+				for( String role : roles ) {
+					if ( ctx.getExternalContext().isUserInRole(role) ) {
+						return Boolean.TRUE;
+					}
+				}				
+			}
+			return Boolean.FALSE;
+		}
+		
+	}
 	
 }
