80f6a15c2b576ce6b49fe316358c5bcc974a4b64
Gorka Irazu <girazu@esferalia.com>
2009-03-25 17:23:07 +0000

    Añadida propiedad account para agrupar por cuenta contable (a la hora de contabilizar)

--- trunk/aon-product/src/main/java/com/code/aon/product/strategy/TaxBreakDown.java
+++ trunk/aon-product/src/main/java/com/code/aon/product/strategy/TaxBreakDown.java
@@ -1,5 +1,6 @@
 package com.code.aon.product.strategy;
 
+import com.code.aon.account.Account;
 import com.code.aon.config.enumeration.TaxType;
 
 /**
@@ -40,6 +41,11 @@ public class TaxBreakDown {
 	 * Tax base
 	 */
 	private double base;
+
+	/**
+	 * Tax account
+	 */
+	private Account account;
 
 	/**
 	 * Returns the tax type 
@@ -148,4 +154,23 @@ public class TaxBreakDown {
 	public void setBase(double base) {
 		this.base = base;
 	}
+
+	/**
+	 * Returns the account
+	 * 
+	 * @return the account
+	 */
+	public Account getAccount() {
+		return account;
+	}
+
+	/**
+	 * Assigns the account
+	 * 
+	 * @param account
+	 */
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
 }
