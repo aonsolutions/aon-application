18c7defc4ae04abaf3b88d55246f506846f4a2b1
Aimar Tellitu <atellitu@esferalia.com>
2009-03-26 09:52:27 +0000

    *** empty log message ***

--- aon-desktop/src/main/resources/com/code/aon/desktop/dao/create.database.3.2.0.sql
+++ aon-desktop/src/main/resources/com/code/aon/desktop/dao/create.database.3.2.0.sql
@@ -1,7 +1,7 @@
 # Database : aon_master
-# Version: 3.0.1
+# Version: 3.2.0
 # Created by: girazu
-# Creation Date: 13/02/2009 12:33
+# Creation Date: 25/03/2009 16:18
 
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -829,6 +829,34 @@ CREATE TABLE `appraiser` (
   PRIMARY KEY  (`registry`),
   CONSTRAINT `appraiser_ibfk_1` FOREIGN KEY (`registry`) REFERENCES `registry` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Peritos';
+
+#
+# Structure for the `asset` table : 
+#
+
+CREATE TABLE `asset` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico',
+  `description` varchar(64) collate latin1_spanish_ci NOT NULL COMMENT 'Descripcion del Activo',
+  `name` varchar(10) collate latin1_spanish_ci NOT NULL COMMENT 'Nombre corto del Activo',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Activos';
+
+#
+# Structure for the `asset_activity` table : 
+#
+
+CREATE TABLE `asset_activity` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico',
+  `asset` int(4) NOT NULL COMMENT 'Identificador del Activo',
+  `date` date NOT NULL COMMENT 'Fecha de la Actividad',
+  `from_time` datetime NOT NULL COMMENT 'Hora de inicio de la Actividad',
+  `to_time` datetime NOT NULL COMMENT 'Hora final de la Actividad',
+  `who` varchar(20) collate latin1_spanish_ci default NULL COMMENT 'Quien solicita el Activo',
+  `why` varchar(128) collate latin1_spanish_ci default NULL COMMENT 'Motivo de solicitud del Activo',
+  PRIMARY KEY  (`id`),
+  KEY `asset` (`asset`),
+  CONSTRAINT `asset_activity_fk1` FOREIGN KEY (`asset`) REFERENCES `asset` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Actividades sobre el Activo';
 
 #
 # Structure for the `auto_concept` table : 
@@ -2075,6 +2103,21 @@ CREATE TABLE `invoice_tax` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Impuestos del Detalle de la Factura';
 
 #
+# Structure for the `invoice_tax_account` table : 
+#
+
+CREATE TABLE `invoice_tax_account` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico',
+  `invoice_tax` int(4) NOT NULL default '0' COMMENT 'Identificador de la Linea de Impuesto',
+  `account` char(12) collate latin1_spanish_ci NOT NULL COMMENT 'Identificador de la Cuenta Contable',
+  PRIMARY KEY  (`id`),
+  KEY `invoice_tax` (`invoice_tax`),
+  KEY `account` (`account`),
+  CONSTRAINT `invoice_tax_account_ibfk_1` FOREIGN KEY (`invoice_tax`) REFERENCES `invoice_tax` (`id`),
+  CONSTRAINT `invoice_tax_account_ibfk_2` FOREIGN KEY (`account`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Cuentas Contables asociadas a Impuestos de Facturas';
+
+#
 # Structure for the `invoicing_group` table : 
 #
 
@@ -3203,6 +3246,22 @@ CREATE TABLE `tas_offer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Relacion entre Ordenes de Reparacion y Presupuestos';
 
 #
+# Structure for the `tax_account` table : 
+#
+
+CREATE TABLE `tax_account` (
+  `id` int(4) NOT NULL auto_increment COMMENT 'Identificador unico de la Cuenta Contable del Impuesto',
+  `tax` int(4) NOT NULL default '0' COMMENT 'Identificador del Impuesto',
+  `account` char(12) collate latin1_spanish_ci NOT NULL COMMENT 'Identificador de la Cuenta Contable',
+  `type` tinyint(2) NOT NULL default '0' COMMENT 'Tipo de Cuenta Contable del Impuesto',
+  PRIMARY KEY  (`id`),
+  KEY `tax` (`tax`),
+  KEY `account` (`account`),
+  CONSTRAINT `tax_account_ibfk_1` FOREIGN KEY (`tax`) REFERENCES `tax` (`id`),
+  CONSTRAINT `tax_account_ibfk_2` FOREIGN KEY (`account`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci COMMENT='Cuentas Contables de Impuestos de la Factura';
+
+#
 # Structure for the `tax_detail` table : 
 #
 
@@ -3395,7 +3454,7 @@ RETURN (SELECT IF (SUM(inventory_detail.cost) IS NULL, 0, SUM(inventory_detail.c
        AND inventory.inventory_date = d);
 
 
-INSERT INTO `db_version` (`version_number`) VALUES ('3.0.1');
+INSERT INTO `db_version` (`version_number`) VALUES ('3.2.0');
 
 COMMIT;
 
