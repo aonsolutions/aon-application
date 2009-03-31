4b3c7ad50b84ee4bc3a3afce9b36eefdb608f4c1
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-03-31 11:10:41 +0000

    *** empty log message ***

--- trunk/aon-asset/src/main/java/com/code/aon/asset/dao/IAssetAlias.java
+++ trunk/aon-asset/src/main/java/com/code/aon/asset/dao/IAssetAlias.java
@@ -49,40 +49,46 @@ public interface IAssetAlias {
 	String  ASSET_ACTIVITY_ASSET_ID = ASSET_ACTIVITY_ENTRY.getAliasNames()[0];
 
 	/** 
+	* Alias value: AssetActivity_asset_name
+	* Hibernate value: AssetActivity.asset.name
+	*/
+	String  ASSET_ACTIVITY_ASSET_NAME = ASSET_ACTIVITY_ENTRY.getAliasNames()[1];
+
+	/** 
 	* Alias value: AssetActivity_date
 	* Hibernate value: AssetActivity.date
 	*/
-	String  ASSET_ACTIVITY_DATE = ASSET_ACTIVITY_ENTRY.getAliasNames()[1];
+	String  ASSET_ACTIVITY_DATE = ASSET_ACTIVITY_ENTRY.getAliasNames()[2];
 
 	/** 
 	* Alias value: AssetActivity_fromTime
 	* Hibernate value: AssetActivity.fromTime
 	*/
-	String  ASSET_ACTIVITY_FROM_TIME = ASSET_ACTIVITY_ENTRY.getAliasNames()[2];
+	String  ASSET_ACTIVITY_FROM_TIME = ASSET_ACTIVITY_ENTRY.getAliasNames()[3];
 
 	/** 
 	* Alias value: AssetActivity_id
 	* Hibernate value: AssetActivity.id
 	*/
-	String  ASSET_ACTIVITY_ID = ASSET_ACTIVITY_ENTRY.getAliasNames()[3];
+	String  ASSET_ACTIVITY_ID = ASSET_ACTIVITY_ENTRY.getAliasNames()[4];
 
 	/** 
 	* Alias value: AssetActivity_toTime
 	* Hibernate value: AssetActivity.toTime
 	*/
-	String  ASSET_ACTIVITY_TO_TIME = ASSET_ACTIVITY_ENTRY.getAliasNames()[4];
+	String  ASSET_ACTIVITY_TO_TIME = ASSET_ACTIVITY_ENTRY.getAliasNames()[5];
 
 	/** 
 	* Alias value: AssetActivity_who
 	* Hibernate value: AssetActivity.who
 	*/
-	String  ASSET_ACTIVITY_WHO = ASSET_ACTIVITY_ENTRY.getAliasNames()[5];
+	String  ASSET_ACTIVITY_WHO = ASSET_ACTIVITY_ENTRY.getAliasNames()[6];
 
 	/** 
 	* Alias value: AssetActivity_why
 	* Hibernate value: AssetActivity.why
 	*/
-	String  ASSET_ACTIVITY_WHY = ASSET_ACTIVITY_ENTRY.getAliasNames()[6];
+	String  ASSET_ACTIVITY_WHY = ASSET_ACTIVITY_ENTRY.getAliasNames()[7];
 
 
 }
