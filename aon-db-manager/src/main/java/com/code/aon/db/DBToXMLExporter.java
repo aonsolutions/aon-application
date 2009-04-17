158ead56f5c9d2a6bf3409f4da75b8af180b9905
Aimar Tellitu <atellitu@esferalia.com>
2009-04-17 07:03:44 +0000

    *** empty log message ***

--- aon-db-manager/src/main/java/com/code/aon/db/DBToXMLExporter.java
+++ aon-db-manager/src/main/java/com/code/aon/db/DBToXMLExporter.java
@@ -1,14 +1,7 @@
 package com.code.aon.db;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
-import org.dom4j.tree.DefaultElement;
 
 public class DBToXMLExporter implements IEntityManager<Element> {
 
@@ -20,23 +13,10 @@ public class DBToXMLExporter implements IEntityManager<Element> {
 		this.hdm = hdm;
 		this.entityIterable = entityIterable;
 	}
-
-    private XMLWriter createWriter( File file ) throws IOException {
-        OutputFormat format = OutputFormat.createPrettyPrint();   
-        format.setEncoding( "UTF-8" );
-        BufferedWriter out = new BufferedWriter( new FileWriter(file) );
-        XMLWriter writer = new XMLWriter( out, format );
-        writer.setMaximumAllowedCharacter(0x7F);
-        return writer;
-    }
     
 	public void proccess(Class<Element> entity) throws EntityProcessException {
 		try {
-	        XMLWriter writer = createWriter( hdm.getFile(entity) );
-	
-	        Element root = new DefaultElement( "root" );
-	        writer.startDocument();
-	        writer.writeOpen( root );
+	        XMLWriter writer = hdm.startDocument(entity);
 		       
 			this.entityIterable.setEntity( entity );	        
 			this.entityIterable.orderById();
@@ -48,9 +28,7 @@ public class DBToXMLExporter implements IEntityManager<Element> {
 					counter = 0;
 	        	}
 	        }
-	        writer.writeClose( root );
-	        writer.endDocument();
-	        writer.close();
+	        hdm.endDocument(false);
 		} catch ( Throwable th ) {
 			throw new EntityProcessException( th );
 		}
--- aon-db-manager/src/main/java/com/code/aon/db/XMLToDBImporter.java
+++ aon-db-manager/src/main/java/com/code/aon/db/XMLToDBImporter.java
@@ -2,15 +2,13 @@ package com.code.aon.db;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.logging.Logger;
 
-import org.apache.commons.lang.ClassUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.xml.sax.InputSource;
 
 public class XMLToDBImporter implements IEntityManager {
 
-	private static Log LOGGER = LogFactory.getLog(XMLToDBImporter.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(XMLToDBImporter.class.getName());
 	
 	private HibernateDataManager hdm;
 	
@@ -21,9 +19,8 @@ public class XMLToDBImporter implements IEntityManager {
 	public void proccess(Class entity) throws EntityProcessException {
 		LOGGER.info( "Importing entity " + entity );
 		try {
-			String entityName = ClassUtils.getShortClassName(entity);
 			IEntityVisitor visitor = new EntityImportVisitor(hdm.getImportFactory(), hdm.getMaxImport(), entity);
-			SAXEntityReader reader = new SAXEntityReader( entityName, visitor );
+			SAXEntityReader reader = new SAXEntityReader( visitor );
 			InputStream in = new FileInputStream( hdm.getFile(entity) );
 			reader.parse( new InputSource(in) );
 			in.close();
