9f4154bae089a956cb283e83d052331d59a669b0
Aimar Tellitu <atellitu@esferalia.com>
2009-05-07 08:12:21 +0000

    Repaso a los métodos equals, hashCode y toString

--- trunk/aon-company/src/main/java/com/code/aon/company/WorkActivity.java
+++ trunk/aon-company/src/main/java/com/code/aon/company/WorkActivity.java
@@ -17,6 +17,9 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.hibernate.annotations.ForeignKey;
 import org.hibernate.annotations.Index;
 import org.hibernate.annotations.WhereJoinTable;
@@ -181,24 +184,35 @@ public class WorkActivity implements ITransferObject, IEntity {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null) {
-    		return super.equals(obj);
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final WorkActivity o = (WorkActivity) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.active, o.active)
+				.append(this.calendar, o.calendar)
+				.append(this.description, o.description)
+				.append(this.workPlace, o.workPlace)
+				.isEquals();
 		}
-		if (obj instanceof WorkActivity) {
-			WorkActivity o = (WorkActivity) obj;
-			if (o.getId() == null && id == null) {
-				return super.equals(obj);	
-			}
-			if (ObjectUtils.equals(getId(), o.getId())) {
-				return true;
-			}
-		}
-		return false;
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(active)
+			.append(calendar)
+			.append(description)
+			.append(id)
+			.append(workPlace)
+			.toHashCode();
 	}
 
 	@Override
-    public int hashCode() {
-        return id != null ? this.getClass().hashCode() + id.hashCode() : super.hashCode();
-    }
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
 }
--- trunk/aon-company/src/main/java/com/code/aon/company/WorkPlace.java
+++ trunk/aon-company/src/main/java/com/code/aon/company/WorkPlace.java
@@ -16,6 +16,9 @@ import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.hibernate.annotations.ForeignKey;
 import org.hibernate.annotations.Index;
 import org.hibernate.annotations.WhereJoinTable;
@@ -250,24 +253,34 @@ public class WorkPlace implements ITransferObject, IEntity {
 	
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null) {
-    		return super.equals(obj);
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final WorkPlace o = (WorkPlace) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.active, o.active)
+				.append(this.address, o.address)
+				.append(this.calendar, o.calendar)				
+				.append(this.description, o.description)
+				.isEquals();
 		}
-		if (obj instanceof WorkPlace) {
-			WorkPlace o = (WorkPlace) obj;
-			if (o.getId() == null && id == null) {
-				return super.equals(obj);	
-			}
-			if (ObjectUtils.equals(getId(), o.getId())) {
-				return true;
-			}
-		}
-		return false;
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(active)
+			.append(address)
+			.append(calendar)
+			.append(description)
+			.append(id)
+			.toHashCode();
 	}
 
 	@Override
-    public int hashCode() {
-        return id != null ? this.getClass().hashCode() + id.hashCode() : super.hashCode();
-    }
-
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 }
