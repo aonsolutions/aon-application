019b7196795351154e18b5edc7f0228625836a82
Aimar Tellitu <atellitu@esferalia.com>
2009-05-07 07:49:52 +0000

    Repaso a los métodos equals, hashCode y toString

--- trunk/aon-customer/src/main/java/com/code/aon/customer/Customer.java
+++ trunk/aon-customer/src/main/java/com/code/aon/customer/Customer.java
@@ -12,6 +12,9 @@ import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.hibernate.annotations.ForeignKey;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Index;
@@ -241,24 +244,43 @@ public class Customer implements ITransferObject, ITaxInfo, IScopable, IRegistry
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null) {
-    		return super.equals(obj);
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final Customer o = (Customer) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.customerSegment, o.customerSegment)
+				.append(this.registry, o.registry)
+				.append(this.scope, o.scope)
+				.append(this.status, o.status)
+				.append(this.surcharge, o.surcharge)
+				.append(this.tariff, o.tariff)
+				.append(this.taxFree, o.taxFree)
+				.append(this.withholding, o.withholding)
+				.isEquals();
 		}
-		if (obj instanceof Customer) {
-			Customer o = (Customer) obj;
-			if (o.getId() == null && id == null) {
-				return super.equals(obj);	
-			}
-			if (ObjectUtils.equals(getId(), o.getId())) {
-				return true;
-			}
-		}
-		return false;
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(customerSegment)
+			.append(id)
+			.append(registry)
+			.append(scope)
+			.append(status)
+			.append(surcharge)
+			.append(tariff)
+			.append(taxFree)
+			.append(withholding)
+			.toHashCode();
 	}
 
 	@Override
-    public int hashCode() {
-        return id != null ? this.getClass().hashCode() + id.hashCode() : super.hashCode();
-    }
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
 }
