a332ebe93920f9a203123b0932e057396988f9b8
Gorka Irazu <girazu@esferalia.com>
2009-05-19 12:52:04 +0000

    ProfitAndLossComparator pasa a llamarse SummaryComparator (con cambios).

--- trunk/aon-accounting/src/main/java/com/code/aon/accounting/summary/SummaryComparator.java
+++ trunk/aon-accounting/src/main/java/com/code/aon/accounting/summary/SummaryComparator.java
@@ -2,11 +2,11 @@ package com.code.aon.accounting.summary;
 
 import java.util.Comparator;
 
-public class ProfitAndLossComparator implements Comparator<Object[]> {
+public class SummaryComparator implements Comparator<Summary> {
 
-	public int compare(Object[] obj1, Object[] obj2) {
-		String account1 = ((String) obj1[1]);
-		String account2 = ((String) obj2[1]);
+	public int compare(Summary sum1, Summary sum2) {
+		String account1 = sum1.getId();
+		String account2 = sum2.getId();
 
 		if (account1.substring(0, 1).equals(account2.substring(0, 1))) {
 			return account1.compareTo(account2);
