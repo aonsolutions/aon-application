56520e1f9f4a550b0b4be4ae7ce476e867bf29ca
Aimar Tellitu <atellitu@esferalia.com>
2009-05-26 07:43:43 +0000

    Se ha añadido hashCode, equals y toString

--- trunk/aon-cms/src/main/java/com/code/aon/cms/AlbumCategoryDetail.java
+++ trunk/aon-cms/src/main/java/com/code/aon/cms/AlbumCategoryDetail.java
@@ -9,11 +9,18 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 import com.code.aon.common.ITransferObject;
+import com.code.aon.common.dao.hibernate.PojoToStringBuilder;
 
 @Entity
 @Table(name="album_category_i18n")
 public class AlbumCategoryDetail implements ITransferObject {
+
+	private static final long serialVersionUID = -6487316039123403886L;
 
 	private Integer id;
 	
@@ -62,4 +69,36 @@ public class AlbumCategoryDetail implements ITransferObject {
 	public void setLabel(String label) {
 		this.label = label;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final AlbumCategoryDetail o = (AlbumCategoryDetail) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.albumCategory, o.albumCategory)				
+				.append(this.label, o.label)
+				.append(this.language, o.language)				
+				.isEquals();
+		}
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(albumCategory)
+			.append(id)	
+			.append(label)
+			.append(language)			
+			.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new PojoToStringBuilder(this).toString();
+	}	
+	
 }
