cc293bc7cc9ed9d8e0121d12c99e4a27d8c46610
Aimar Tellitu <atellitu@esferalia.com>
2009-06-03 12:20:42 +0000

    Se estaban creando los itemGroup con disabled=true y no funcionaba bien en firefox

--- trunk/aon-ui-geozone/src/main/java/com/code/aon/ui/geozone/controller/GeozoneCollectionsController.java
+++ trunk/aon-ui-geozone/src/main/java/com/code/aon/ui/geozone/controller/GeozoneCollectionsController.java
@@ -78,7 +78,9 @@ public class GeozoneCollectionsController {
 			Iterator<ITransferObject> iter = geoTreeBean.getList(criteria).iterator();
 			while (iter.hasNext()) {
 				GeoTree geoTree = (GeoTree)iter.next();
-				SelectItemGroup itemGroup = new SelectItemGroup(geoTree.getChild().getName(),geoTree.getChild().getName(),true,obtainGeoTreeChilds(geoTree.getChild().getId()));
+				String name = geoTree.getChild().getName();
+				SelectItem[] selectItems = obtainGeoTreeChilds(geoTree.getChild().getId());
+				SelectItemGroup itemGroup = new SelectItemGroup(name, name,false, selectItems);
 				geoTrees.add(itemGroup);
 			}
 		}
