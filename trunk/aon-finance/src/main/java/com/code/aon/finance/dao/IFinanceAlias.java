b75ed00e64f4c5e79fb8f863181ad77596f1e47c
Gorka Irazu <girazu@esferalia.com>
2009-06-09 16:03:39 +0000

    Añadido invoiceattachment

--- trunk/aon-finance/src/main/java/com/code/aon/finance/dao/IFinanceAlias.java
+++ trunk/aon-finance/src/main/java/com/code/aon/finance/dao/IFinanceAlias.java
@@ -8,6 +8,7 @@ import com.code.aon.finance.FinanceBatch;
 import com.code.aon.finance.FinanceBatchDetail;
 import com.code.aon.finance.Invoice;
 import com.code.aon.finance.InvoiceAddress;
+import com.code.aon.finance.InvoiceAttachment;
 import com.code.aon.finance.InvoiceDetail;
 import com.code.aon.finance.InvoiceTax;
 import com.code.aon.finance.Creditor;
@@ -529,6 +530,49 @@ public interface IFinanceAlias {
 	* Hibernate value: InvoiceAddress.zip
 	*/
 	String  INVOICE_ADDRESS_ZIP = INVOICE_ADDRESS_ENTRY.getAliasNames()[6];
+
+
+
+	/** 
+	* DAOConstantsEntry for InvoiceAttachment entity.
+	*/ 
+	DAOConstantsEntry INVOICE_ATTACHMENT_ENTRY = DAOConstants.getDAOConstant(InvoiceAttachment.class);
+
+	/** 
+	* Alias value: InvoiceAttachment_data
+	* Hibernate value: InvoiceAttachment.data
+	*/
+	String  INVOICE_ATTACHMENT_DATA = INVOICE_ATTACHMENT_ENTRY.getAliasNames()[0];
+
+	/** 
+	* Alias value: InvoiceAttachment_description
+	* Hibernate value: InvoiceAttachment.description
+	*/
+	String  INVOICE_ATTACHMENT_DESCRIPTION = INVOICE_ATTACHMENT_ENTRY.getAliasNames()[1];
+
+	/** 
+	* Alias value: InvoiceAttachment_id
+	* Hibernate value: InvoiceAttachment.id
+	*/
+	String  INVOICE_ATTACHMENT_ID = INVOICE_ATTACHMENT_ENTRY.getAliasNames()[2];
+
+	/** 
+	* Alias value: InvoiceAttachment_invoice_id
+	* Hibernate value: InvoiceAttachment.invoice.id
+	*/
+	String  INVOICE_ATTACHMENT_INVOICE_ID = INVOICE_ATTACHMENT_ENTRY.getAliasNames()[3];
+
+	/** 
+	* Alias value: InvoiceAttachment_mimeType
+	* Hibernate value: InvoiceAttachment.mimeType
+	*/
+	String  INVOICE_ATTACHMENT_MIME_TYPE = INVOICE_ATTACHMENT_ENTRY.getAliasNames()[4];
+
+	/** 
+	* Alias value: InvoiceAttachment_size
+	* Hibernate value: InvoiceAttachment.size
+	*/
+	String  INVOICE_ATTACHMENT_SIZE = INVOICE_ATTACHMENT_ENTRY.getAliasNames()[5];
 
 
 
