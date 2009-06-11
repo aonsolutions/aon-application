dc8588becf72064bd08ba51d2d354d54867ec4f2
Euke Castellano <ecastellano@esferalia.com>
2009-06-11 10:25:46 +0000

    *** empty log message ***

--- aon-accounting/src/main/java/com/code/aon/accounting/BalanceDetail.java
+++ aon-accounting/src/main/java/com/code/aon/accounting/BalanceDetail.java
@@ -9,8 +9,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
@@ -146,6 +148,14 @@ public class BalanceDetail implements ITransferObject {
 			
 	public void setCreditNature(boolean creditNature) {
 		this.creditNature = creditNature;
+	}
+	
+	@Transient
+	public int getLevel() {
+		if ( getCode() != null) {
+			return StringUtils.countMatches(getCode(), ".");
+		}
+		return 0;
 	}
 	
 	@Override
