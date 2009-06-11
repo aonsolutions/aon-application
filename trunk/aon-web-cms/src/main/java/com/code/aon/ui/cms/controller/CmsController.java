d8c252012df9c655259281e6cba17df52386dc1d
Aimar Tellitu <atellitu@esferalia.com>
2009-06-11 10:15:37 +0000

    *** empty log message ***

--- trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/CmsController.java
+++ trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/CmsController.java
@@ -4,12 +4,21 @@ import javax.faces.context.FacesContext;
 
 public class CmsController {
 
-	private boolean adminProfile = false;
+    private static final String DESIGNER_ROLE = "Designer";
+    private static final String EDITOR_ROLE = "Editor";
+    private static final String REVISOR_ROLE = "Revisor";
+    private static final String PUBLISHER_ROLE = "Publisher";
+    	
+	private boolean adminProfile;
 
 	public void assignAdminProfile(){
 		adminProfile = true;
 	}
 	
+    public boolean isAdministrator() {
+		return adminProfile;
+    }
+    
     public boolean isRoleDesigner() {
 		if (adminProfile) return true;
     	return FacesContext.getCurrentInstance().getExternalContext().isUserInRole(DESIGNER_ROLE);
@@ -29,15 +38,5 @@ public class CmsController {
 		if (adminProfile) return true;
     	return FacesContext.getCurrentInstance().getExternalContext().isUserInRole(PUBLISHER_ROLE);
     }
-
-    public boolean isAdministrator() {
-		return adminProfile;
-    }
-
-    private static final String DESIGNER_ROLE = "Designer";
-    private static final String EDITOR_ROLE = "Editor";
-    private static final String REVISOR_ROLE = "Revisor";
-    private static final String PUBLISHER_ROLE = "Publisher";
-    
     
 }
