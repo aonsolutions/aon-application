052ad378e9d4e02f199b8d2a3bbfed161884b35b
Aimar Tellitu <atellitu@esferalia.com>
2009-06-16 12:52:48 +0000

    *** empty log message ***

--- trunk/aon-ui-db-manager/src/main/java/com/code/aon/ui/db/hibernate/ReplicateConfigurationFactory.java
+++ trunk/aon-ui-db-manager/src/main/java/com/code/aon/ui/db/hibernate/ReplicateConfigurationFactory.java
@@ -1,6 +1,5 @@
 package com.code.aon.ui.db.hibernate;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -8,18 +7,12 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.hibernate.cfg.Configuration;
 import org.hibernate.metadata.ClassMetadata;
-import org.hibernate.persister.entity.EntityPersister;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 import com.code.aon.common.dao.hibernate.DefaultConfigurationFactory;
 import com.code.aon.common.dao.hibernate.IConfigurationFactory;
+import com.code.aon.db.hibernate.ReplicateConfigurationPatcher;
 
 public class ReplicateConfigurationFactory extends DefaultConfigurationFactory {
 
@@ -46,54 +39,6 @@ public class ReplicateConfigurationFactory extends DefaultConfigurationFactory {
 		this.entities = entities;
 	}
 
-	private Document newDocument() {
-		DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
-		try {
-			DocumentBuilder builder = dBF.newDocumentBuilder();
-			return builder.newDocument();	
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	private boolean isIdentifierAssignedByInsert( ClassMetadata cm ) {
-		boolean result = false;
-		if ( EntityPersister.class.isAssignableFrom(cm.getClass()) ) {
-			EntityPersister ep = (EntityPersister) cm;
-			result = ep.isIdentifierAssignedByInsert();
-		}
-		return result;
-	}
-	
-	private Document createDocument() {
-		Document document = newDocument();
-		Element root = document.createElement("entity-mappings");
-		root.setAttribute("version", "1.0");
-		document.appendChild(root);
-
-		for( ClassMetadata cm : entities ) {
-			if ( isIdentifierAssignedByInsert(cm) ) {
-				Element entity = document.createElement("entity");
-				entity.setAttribute( "class", cm.getEntityName() );
-				root.appendChild(entity);
-	
-				Element attributes = document.createElement("attributes");
-				entity.appendChild(attributes);
-	
-				Element id = document.createElement("id");
-				String idName = cm.getIdentifierPropertyName();
-				id.setAttribute( "name", idName );
-				attributes.appendChild(id);
-				
-				Element generatedValue = document.createElement("generated-value");
-				generatedValue.setAttribute( "strategy", "TABLE" );
-				id.appendChild(generatedValue);
-			}
-		}		
-		return document;
-	}
-	
 	private void addTestProperties(Configuration configuration) {
 		Properties properties = new Properties();
 		try {
@@ -110,11 +55,8 @@ public class ReplicateConfigurationFactory extends DefaultConfigurationFactory {
 	
 	@Override
 	protected void completeConfiguration(Configuration configuration) {
-		Document document = createDocument();
-		Element root = document.getDocumentElement();
-		if ( root.hasChildNodes() ) {
-			configuration.addDocument(document);	
-		}
+		ReplicateConfigurationPatcher rcp = new ReplicateConfigurationPatcher(entities);
+		rcp.completeConfiguration(configuration);
 		addTestProperties(configuration);
 	}
 
