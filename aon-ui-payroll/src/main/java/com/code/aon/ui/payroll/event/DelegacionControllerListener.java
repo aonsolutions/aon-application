bb65213249c8eb763d92a578cdd66461026a088b
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-06-16 07:07:19 +0000

    *** empty log message ***

--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/DelegacionControllerListener.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/event/DelegacionControllerListener.java
@@ -10,37 +10,27 @@ import com.code.aon.ui.form.event.ControllerListenerException;
 import com.code.aon.ui.util.AonUtil;
 
 public class DelegacionControllerListener extends ControllerAdapter {	
-	
-
-
-
 
 	@Override
 	public void beforeBeanAdded(ControllerEvent event) throws ControllerListenerException {
 	    Delegacion d = (Delegacion) event.getController().getTo();
 	
-			if (d.getCodpos().length()<5 || d.getCodpos().length()>=5){				
+			if (d.getCodpos().length()<5 || d.getCodpos().length()>5){				
 		    	FacesMessage fm = 
 		    		AonUtil.getMessage( FacesContext.getCurrentInstance(),"aon_payroll_1453", null );
 				   throw new ControllerListenerException( fm.getSummary() );
 			}
-
-			
-		
 	}
 
 	@Override
 	public void beforeBeanUpdated(ControllerEvent event) throws ControllerListenerException {
 	    Delegacion d = (Delegacion) event.getController().getTo();
 	
-			if (d.getCodpos().length()<5 ){
+			if (d.getCodpos().length()<5 || d.getCodpos().length()>5){
 				FacesMessage fm = 
 		    		AonUtil.getMessage( FacesContext.getCurrentInstance(),"aon_payroll_1453", null );
-				 throw new ControllerListenerException();
+				 throw new ControllerListenerException( fm.getSummary() );
 			}
-
-			
-		
 	}
 
 }
