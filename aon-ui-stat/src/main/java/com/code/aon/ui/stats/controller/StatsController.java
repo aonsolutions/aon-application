aa9c536223e756e296271fd37ccd6f8ffed78ee2
Euke Castellano <ecastellano@esferalia.com>
2009-06-22 06:40:38 +0000

    Version inicial.

---
+++ aon-ui-stat/src/main/java/com/code/aon/ui/stats/controller/StatsController.java
@@ -0,0 +1,19 @@
+package com.code.aon.ui.stats.controller;
+
+import javax.faces.event.ActionEvent;
+
+/**
+ * Controller used in the issue stats maintenance.
+ * 
+ */
+public class StatsController  {
+	
+	/**
+	 * Assigns payment value, false for sales
+	 * 
+	 * @param event the menu event
+	 */
+	public void setSalesMode(ActionEvent event){
+	}
+
+}
