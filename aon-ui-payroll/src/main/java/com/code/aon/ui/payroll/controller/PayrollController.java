e857aa1db03048b514ab86934f53cfb1b95996fe
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-06-23 14:15:02 +0000

    *** empty log message ***

--- aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/PayrollController.java
+++ aon-ui-payroll/src/main/java/com/code/aon/ui/payroll/controller/PayrollController.java
@@ -7,20 +7,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.faces.context.FacesContext;
-import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
+import javax.naming.Name;
 import javax.servlet.http.HttpSession;
 
 import com.code.aon.bridge.plugin.Utils;
 import com.code.aon.jaas.auth.AuthPrincipal;
-import com.code.aon.ldap.AonDN;
 import com.code.aon.ldap.BasicLdap;
-import com.code.aon.ldap.DistinguishedName;
 import com.code.aon.ldap.Entry;
 import com.code.aon.ldap.IAonObjectClasses;
 import com.code.aon.ldap.ILdapConstants;
-import com.code.aon.ldap.LdapException;
-import com.code.aon.ldap.LdapSession;
+import com.code.aon.ldap.NameResolver;
 
 public class PayrollController implements ILdapConstants, IAonObjectClasses {
 
@@ -44,18 +41,9 @@ public class PayrollController implements ILdapConstants, IAonObjectClasses {
 	}
 
 	private Entry getAonUser( AuthPrincipal principal ) {
-		Entry entry = null;
 		BasicLdap ldap = new BasicLdap();
-		try {
-			DistinguishedName dn = AonDN.getUserDN(principal.getDomain(), principal.getShortName());
-			String filter = LdapSession.getObjectClass(USER);
-			entry = ldap.getLdapSession().get(dn.toString(), filter);
-		} catch ( LdapException e ) {
-			throw new AbortProcessingException( "Error getting aonUser for " + principal + ". " + e.getMessage(), e );
-		} finally {
-			ldap.closeSession();
-		}
-		return entry;
+		Name dn = NameResolver.getUserDN(principal.getDomain(), principal.getShortName());
+		return ldap.get(dn, USER);
 	}		
     
     private Entry getAonUser() {
