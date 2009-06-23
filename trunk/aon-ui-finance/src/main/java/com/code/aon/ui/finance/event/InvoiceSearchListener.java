e43c16907220baf7aa902deaacc0372c2c705746
Gorka Irazu <girazu@esferalia.com>
2009-06-23 10:40:23 +0000

    Poder buscar por multiples formas de pago y estados de vencimientos

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoiceSearchListener.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoiceSearchListener.java
@@ -1,10 +1,19 @@
 package com.code.aon.ui.finance.event;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.config.Bank;
+import com.code.aon.config.PayMethod;
 import com.code.aon.finance.dao.IFinanceAlias;
+import com.code.aon.finance.enumeration.FinanceStatus;
 import com.code.aon.finance.enumeration.InvoiceStatus;
 import com.code.aon.finance.enumeration.InvoiceType;
 import com.code.aon.product.Item;
@@ -16,6 +25,8 @@ import com.code.aon.ui.registry.controller.event.RegistrySearchListener;
 
 public class InvoiceSearchListener extends RegistrySearchListener {
 	
+	private static final PayMethod EMPTY_PAYMETHOD = new PayMethod();
+	
 	private String defaultType;
 	
 	private String defaultStatus;
@@ -26,6 +37,10 @@ public class InvoiceSearchListener extends RegistrySearchListener {
 
 	private Bank bank;
     
+	private FinanceStatus[] financeStatuses;
+	
+	private List<PayMethod> payMethods;
+	
 	public String getDefaultType() {
 		return defaultType;
 	}
@@ -66,6 +81,40 @@ public class InvoiceSearchListener extends RegistrySearchListener {
 		this.bank = bank;
 	}
 	
+	public FinanceStatus[] getFinanceStatuses() {
+		return financeStatuses;
+	}
+
+	public void setFinanceStatuses(FinanceStatus[] financeStatuses) {
+		this.financeStatuses = financeStatuses;
+	}
+	
+	public List<PayMethod> getPayMethods() {
+		return payMethods;
+	}
+
+	public void setPayMethods(List<PayMethod> payMethods) {
+		this.payMethods = payMethods;
+	}
+	
+	public int getPayMethodsSize() {
+		return this.payMethods.size();
+	}	
+
+	public List<Integer> getPayMethodsIds() {
+		List<Integer> ids = new LinkedList<Integer>();
+		for (PayMethod payMethod : getPayMethods()) {
+			if ((payMethod != null) && (payMethod.getId() != null)) {
+				ids.add(payMethod.getId());
+			}
+		}
+		return ids;
+	}
+
+	public PayMethod getEmptyPayMethod() {
+		return EMPTY_PAYMETHOD;
+	}
+	
 	@Override
 	protected void init() throws ManagerBeanException {
 		super.init();
@@ -73,6 +122,9 @@ public class InvoiceSearchListener extends RegistrySearchListener {
 		setItem(new Item());
 		getItem().setProduct(new Product());
 		setBank(new Bank());
+		setFinanceStatuses(new FinanceStatus[0]);
+		setPayMethods(new LinkedList<PayMethod>());
+		getPayMethods().add(EMPTY_PAYMETHOD);
 	}
 	
 	@Override
@@ -96,6 +148,25 @@ public class InvoiceSearchListener extends RegistrySearchListener {
 		if ((getBank() != null) && (!StringUtils.isEmpty(getBank().getCode()))) {
 			criteria.addEqualExpression("Invoice.finances.bank.code", getBank().getCode());			
 		}		
+		if (!ArrayUtils.isEmpty(getFinanceStatuses())) {
+			String status = getController().resolveAlias("Invoice.finances.financeStatus");
+			addEnumToCriteria(criteria, status, getFinanceStatuses());
+		}
+		String payMethod = getController().resolveAlias("Invoice.finances.payMethod.id");
+		addEnumToCriteria(criteria, payMethod, getPayMethodsIds().toArray());
 	}	
+
+	public void onAddPayMethod(ActionEvent event) {
+		getPayMethods().add(EMPTY_PAYMETHOD);
+	}
+
+	public void onRemovePayMethod(ActionEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+		int index = Integer.valueOf(context.getExternalContext().getRequestParameterMap().get("index"));		
+		getPayMethods().remove(index);
+		if (getPayMethods().isEmpty()) {
+			getPayMethods().add(EMPTY_PAYMETHOD);
+		}
+	}
 
 }
