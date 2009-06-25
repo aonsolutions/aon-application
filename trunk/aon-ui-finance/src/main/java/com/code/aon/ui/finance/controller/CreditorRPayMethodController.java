d4075ade4e664943f91d6bba56a3130e9a3907ae
Gorka Irazu <girazu@esferalia.com>
2009-06-25 06:47:11 +0000

    Cambio en la forma de obtener los bancos.

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/CreditorRPayMethodController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/CreditorRPayMethodController.java
@@ -1,16 +1,12 @@
 package com.code.aon.ui.finance.controller;
 
-import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.model.SelectItem;
 
-import com.code.aon.common.BeanManager;
-import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.company.Company;
 import com.code.aon.finance.Creditor;
-import com.code.aon.ui.company.controller.CompanyController;
+import com.code.aon.ui.company.controller.CompanyCollectionsController;
 import com.code.aon.ui.company.controller.ICompanyConstants;
 import com.code.aon.ui.registry.controller.RegistryPayMethodController;
 import com.code.aon.ui.util.AonUtil;
@@ -18,20 +14,13 @@ import com.code.aon.ui.util.AonUtil;
 public class CreditorRPayMethodController extends RegistryPayMethodController{
 	
 	public List<SelectItem> getBanks() throws ManagerBeanException {
-		Integer id = null;
 		if (isBankTransfer()) {
 			Creditor creditor = (Creditor) getMasterController().getTo();
-			id = creditor.getId();
+			return getBanks(creditor.getRegistry());
 		} else {
-			id = obtainCompany().getId();
+			CompanyCollectionsController c = (CompanyCollectionsController)AonUtil.getRegisteredBean(ICompanyConstants.COLLECTIONS_CONTROLLER_NAME);
+			return c.getCompanyBanks();
 		}
-		return getBanks( id );
 	}
 	
-	@SuppressWarnings("unchecked")
-	private Company obtainCompany() throws ManagerBeanException {
-		CompanyController companyController = (CompanyController) AonUtil.getRegisteredBean(ICompanyConstants.COMPANY_CONTROLLER_NAME);
-		return companyController.obtainCompany();
-	}
-
 }
