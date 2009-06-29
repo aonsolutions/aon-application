013cd98c0a384fa996b8df26f0466f1c1b7405a4
Euke Castellano <ecastellano@esferalia.com>
2009-06-29 06:49:29 +0000

    Totales

--- aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/financial/TaxFinancialStatement.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/financial/TaxFinancialStatement.java
@@ -34,19 +34,21 @@ public class TaxFinancialStatement extends AbstractFinancialStatement {
 				criteria.addExpression(accountBean.getFieldName(IAccountAlias.ACCOUNT_ID), "472*");
 				criteria.addEqualExpression(accountBean.getFieldName(IAccountAlias.ACCOUNT_ENTRY_ENABLED),
 						new Boolean(true));
+				double amount = 0; 
 				for (ITransferObject to: accountBean.getList(criteria)) {
 					Account account = (Account) to;
 					Balance balance = util.getPeriodBalance(period.getInitiationDate(), period.getDeadline(), account.getId(), false, false);
-					double amount = CommonUtil.round(balance.getCredit() - balance.getDebit() ) ;
+					amount = CommonUtil.round(balance.getCredit() - balance.getDebit() ) ;
 					if (account.getId().startsWith("472")) {
 						amount = CommonUtil.round(amount * -1 ) ;	
 					}
-					setTotal( CommonUtil.round(getTotal() + amount));  
+					  
 				}
 				FinancialStatement fs = new FinancialStatement();
 				fs.setCode( "4750,477,472" );
 				fs.setDescription("IVA Pendiente de Pago" );
-				fs.setAmount( getTotal() );
+				fs.setAmount( amount );
+				setTotal( CommonUtil.round(getTotal() + amount));
 				getFinancialStatements().add(fs);
 				
 				// IRPF PENDIENTE DE PAGO
@@ -54,16 +56,17 @@ public class TaxFinancialStatement extends AbstractFinancialStatement {
 				criteria.addExpression(accountBean.getFieldName(IAccountAlias.ACCOUNT_ID), "4751*");
 				criteria.addEqualExpression(accountBean.getFieldName(IAccountAlias.ACCOUNT_ENTRY_ENABLED),
 						new Boolean(true));
+				amount = 0;
 				for (ITransferObject to: accountBean.getList(criteria)) {
 					Account account = (Account) to;
 					Balance balance = util.getPeriodBalance(period.getInitiationDate(), period.getDeadline(), account.getId(), false, false);
-					double amount = CommonUtil.round(balance.getCredit() - balance.getDebit() ) ;
-					setTotal( CommonUtil.round(getTotal() + amount));  
+					amount = CommonUtil.round(balance.getCredit() - balance.getDebit() ) ;
 				}
 				fs = new FinancialStatement();
 				fs.setCode( "4751" );
 				fs.setDescription("IRPF Pendiente de Pago" );
-				fs.setAmount( getTotal() );
+				fs.setAmount( amount );
+				setTotal( CommonUtil.round(getTotal() + amount));
 				getFinancialStatements().add(fs);
 				
 				// SS PENDIENTE DE PAGO
@@ -71,16 +74,17 @@ public class TaxFinancialStatement extends AbstractFinancialStatement {
 				criteria.addExpression(accountBean.getFieldName(IAccountAlias.ACCOUNT_ID), "4760*");
 				criteria.addEqualExpression(accountBean.getFieldName(IAccountAlias.ACCOUNT_ENTRY_ENABLED),
 						new Boolean(true));
+				amount = 0;
 				for (ITransferObject to: accountBean.getList(criteria)) {
 					Account account = (Account) to;
 					Balance balance = util.getPeriodBalance(period.getInitiationDate(), period.getDeadline(), account.getId(), false, false);
-					double amount = CommonUtil.round(balance.getCredit() - balance.getDebit() ) ;
-					setTotal( CommonUtil.round(getTotal() + amount));  
+					amount = CommonUtil.round(balance.getCredit() - balance.getDebit() ) ;
 				}
 				fs = new FinancialStatement();
 				fs.setCode( "4760" );
 				fs.setDescription("SS Pendiente de Pago" );
-				fs.setAmount( getTotal() );
+				fs.setAmount( amount );
+				setTotal( CommonUtil.round(getTotal() + amount));
 				getFinancialStatements().add(fs);
 			}
 		} catch (ExpressionException e) {
