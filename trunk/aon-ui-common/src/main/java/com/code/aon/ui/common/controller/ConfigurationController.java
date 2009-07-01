eab162ca71f7088f8a2eac1c0c6d54d43d743517
Aimar Tellitu <atellitu@esferalia.com>
2009-07-01 06:17:54 +0000

    Se han borrado la propiedades obsoletas. Se ha añadido la posibilidad de leer un fichero de configuracion para los proyectos web

--- trunk/aon-ui-common/src/main/java/com/code/aon/ui/common/controller/ConfigurationController.java
+++ trunk/aon-ui-common/src/main/java/com/code/aon/ui/common/controller/ConfigurationController.java
@@ -1,7 +1,10 @@
 package com.code.aon.ui.common.controller;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,41 +14,56 @@ import java.util.List;
 import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.servlet.http.HttpSession;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import com.code.aon.common.util.Classpath;
+import com.code.aon.ui.common.ICommonConstants;
+import com.code.aon.ui.util.AonUtil;
+import com.sun.org.apache.xerces.internal.jaxp.JAXPConstants;
 
 /**
  * The Class ConfigurationController is used to set some default configurable
  * parameters of the application.
  */
-public class ConfigurationController implements Serializable {
+public class ConfigurationController implements Serializable, ICommonConstants, JAXPConstants {
 	
 	private static final long serialVersionUID = -1159615075844874762L;
 	
 	private static final Logger LOGGER = Logger.getLogger(ConfigurationController.class.getName());
 	
-	private static final DateFormat FORMATTER = new SimpleDateFormat("EEEE, dd MMMM yyyy");	
-
-	/** The application logo context relative path. */
-	private String applicationLogoContextRelativePath;
-
-	/** The application welcome context relative path. */
-	private String applicationWelcomeContextRelativePath;
-
-	/** The application report context relative path. */
-	private String applicationReportContextRelativePath;
+	private static final DateFormat FORMATTER = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+	
+	private static final String AON_CONFIG_XML = "/WEB-INF/aon-config.xml";
+	
+	private static final String CONFIG_SHCHEMA = "config.xsd";		
 
 	/** The all style sheets. */
 	private List<String> styleSheets;
 
 	/** The properties. */
 	private Map<String, String> properties;
+	
+	private Map<String,Map<String,Object>> bean;	
 	
 	private boolean versionChecked;
 	
@@ -57,63 +75,10 @@ public class ConfigurationController implements Serializable {
 	public ConfigurationController() {
 		this.properties = new HashMap<String, String>();
 		this.styleSheets = new ArrayList<String>();
-	}
-
-	/**
-	 * Gets the application logo context relative path.
-	 * 
-	 * @return the application logo context relative path
-	 */
-	public String getApplicationLogoContextRelativePath() {
-		return applicationLogoContextRelativePath;
-	}
-
-	/**
-	 * Sets the application logo context relative path.
-	 * 
-	 * @param applicationLogoContextRelativePath the application logo context relative path
-	 */
-	public void setApplicationLogoContextRelativePath(
-			String applicationLogoContextRelativePath) {
-		this.applicationLogoContextRelativePath = applicationLogoContextRelativePath;
-	}
-
-	/**
-	 * Gets the application report context relative path.
-	 * 
-	 * @return the application report context relative path
-	 */
-	public String getApplicationReportContextRelativePath() {
-		return applicationReportContextRelativePath;
-	}
-
-	/**
-	 * Sets the application report context relative path.
-	 * 
-	 * @param applicationReportContextRelativePath the application report context relative path
-	 */
-	public void setApplicationReportContextRelativePath(
-			String applicationReportContextRelativePath) {
-		this.applicationReportContextRelativePath = applicationReportContextRelativePath;
-	}
-
-	/**
-	 * Gets the application welcome context relative path.
-	 * 
-	 * @return the application welcome context relative path
-	 */
-	public String getApplicationWelcomeContextRelativePath() {
-		return applicationWelcomeContextRelativePath;
-	}
-
-	/**
-	 * Sets the application welcome context relative path.
-	 * 
-	 * @param applicationWelcomeContextRelativePath the application welcome context relative path
-	 */
-	public void setApplicationWelcomeContextRelativePath(
-			String applicationWelcomeContextRelativePath) {
-		this.applicationWelcomeContextRelativePath = applicationWelcomeContextRelativePath;
+		Document document = getConfigDocument();
+		if ( document != null ) {
+			bean = loadBeanConfiguration( document );
+		}		
 	}
 
 	/**
@@ -221,4 +186,151 @@ public class ConfigurationController implements Serializable {
     	session.invalidate();    	
     }
     
+	/**
+	 * Gets the bean.
+	 * 
+	 * @return the bean
+	 */
+	public Map<String, Map<String, Object>> getBean() {
+		return bean;
+	}
+
+	private String[] getXmlSchemas() {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        try {
+	        URL[] urls = Classpath.search(cl, "META-INF/", CONFIG_SHCHEMA);
+	        if (! ArrayUtils.isEmpty(urls) ) {
+		        String[] list = new String[urls.length];
+		        for( int i = 0; i < urls.length; i++ ) {
+		        	list[i] = urls[i].toString();
+		        }
+		        return list;
+	        }
+        } catch (IOException e) {
+        	LOGGER.log(Level.SEVERE, "Error searching files: " + CONFIG_SHCHEMA, e);
+        }		
+        return null;
+	}	
+	
+	private Document getConfigDocument() {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		URL config = null;
+		try {
+			config = ec.getResource(AON_CONFIG_XML);
+		} catch (MalformedURLException e) {
+			LOGGER.log(Level.SEVERE, AON_CONFIG_XML + " not found", e);
+			return null;
+		}
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		factory.setNamespaceAware(true);
+		factory.setValidating(true);
+		factory.setIgnoringElementContentWhitespace(true);
+		factory.setAttribute( JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+		
+		String[] schemas = getXmlSchemas();
+		factory.setAttribute( JAXP_SCHEMA_SOURCE, schemas );		
+		
+		Document document = null;
+		LogErrorHandler errorHandler = new LogErrorHandler();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			builder.setErrorHandler( errorHandler );
+			document = builder.parse(config.toString());
+		} catch (Throwable th) {
+			AonUtil.addErrorMessageFromBundle( DEFAULT_BUNDLE, CONFIGURATION_ERROR, AON_CONFIG_XML, th.getMessage() );
+			LOGGER.log(Level.SEVERE, th.getMessage(), th);
+		} finally {
+			if ( errorHandler.isValidationError() ) {
+				AonUtil.addErrorMessageFromBundle( DEFAULT_BUNDLE, CONFIGURATION_ERROR, AON_CONFIG_XML, errorHandler.getException().getMessage() );
+				document = null;
+			}
+		}
+		return document;
+	}
+	
+	private Object getAttributeValue( Node attribute ) {
+		String value = attribute.getNodeValue();
+		if ( Boolean.TRUE.toString().equals(value) ) {
+			return Boolean.TRUE;
+		} else if ( Boolean.FALSE.toString().equals(value) ) {
+			return Boolean.FALSE;
+		} else if ( NumberUtils.isNumber(value) ) {
+			return NumberUtils.createNumber(value);
+		}
+		return value;
+	}
+	
+	private Map<String,Map<String,Object>> loadBeanConfiguration( Document document ) {
+		Map<String,Map<String,Object>> bean = new HashMap<String, Map<String,Object>>();
+		Element root = document.getDocumentElement();
+		NodeList list = root.getChildNodes();
+		for( int i = 0; i < list.getLength(); i++ ) {
+			Node config = list.item(i);
+			if ( config.getNodeType() == Node.ELEMENT_NODE ) {
+				NodeList childs = config.getChildNodes();
+				for( int j = 0; j < childs.getLength(); j++ ) {
+					Node child = childs.item(j);
+					if ( child.getNodeType() == Node.ELEMENT_NODE ) {
+						Map<String,Object> values = new HashMap<String, Object>();
+						NamedNodeMap attributes = child.getAttributes();
+						for( int n = 0; n < attributes.getLength(); n++ ) {
+							Node attribute = attributes.item(n);
+							Object value = getAttributeValue( attribute );
+							values.put( attribute.getLocalName(), value);
+						}
+						bean.put( child.getLocalName(), values );
+					}
+				}
+			}
+		}
+		return bean;
+	}
+	
+	private class LogErrorHandler implements ErrorHandler {
+		
+		private boolean validationError;
+		
+		private SAXParseException exception;
+
+		@Override
+		public void error(SAXParseException exception) throws SAXException {
+			this.validationError = true;
+			this.exception = exception;
+			LOGGER.log( Level.SEVERE, exception.getMessage(), exception );
+		}
+
+		@Override
+		public void fatalError(SAXParseException exception) throws SAXException {
+			this.validationError = true;
+			this.exception = exception;
+			LOGGER.log( Level.SEVERE, exception.getMessage(), exception );
+		}
+
+		@Override
+		public void warning(SAXParseException exception) throws SAXException {
+			LOGGER.log( Level.WARNING, exception.getMessage(), exception );
+		}
+
+		/**
+		 * Checks if is validation error.
+		 * 
+		 * @return true, if is validation error
+		 */
+		public boolean isValidationError() {
+			return validationError;
+		}
+
+		/**
+		 * Gets the exception.
+		 * 
+		 * @return the exception
+		 */
+		public SAXParseException getException() {
+			return exception;
+		}
+		
+	}
+	
 }
