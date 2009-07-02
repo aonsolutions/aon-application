ec93f1c796a2109e5578bb58e18271689f509b18
Aimar Tellitu <atellitu@esferalia.com>
2009-07-02 06:19:38 +0000

    Se ha pasado el java y el resources del web aqui

---
+++ aon-ui-infoweb/src/main/java/com/code/aon/ui/infoweb/controller/IInfoWebConstants.java
@@ -0,0 +1,9 @@
+package com.code.aon.ui.infoweb.controller;
+
+public interface IInfoWebConstants {
+	
+	String BUNDLE_NAME = "appBundle";
+
+	String WEB_INFO_CONTROLLER_NAME = "companyWebInfo";
+	
+}
