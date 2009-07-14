4820a74aff8c5714d5901c302ba7f901bc636fcd
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-07-14 09:23:45 +0000

    nuevo atributo, tipo de item

--- aon-product/src/main/java/com/code/aon/product/dao/IProductAlias.java
+++ aon-product/src/main/java/com/code/aon/product/dao/IProductAlias.java
@@ -308,6 +308,12 @@ public interface IProductAlias {
 	*/
 	String  ITEM_ATTACHMENT_SIZE = ITEM_ATTACHMENT_ENTRY.getAliasNames()[5];
 
+	/** 
+	* Alias value: ItemAttachment_type
+	* Hibernate value: ItemAttachment.type
+	*/
+	String  ITEM_ATTACHMENT_TYPE = ITEM_ATTACHMENT_ENTRY.getAliasNames()[6];
+
 
 
 	/** 
