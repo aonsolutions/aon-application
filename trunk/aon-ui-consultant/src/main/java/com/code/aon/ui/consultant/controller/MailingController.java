5e92c8ecdf11a47d18a5275c3205ad1813972b23
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2009-07-20 10:48:37 +0000

    *** empty log message ***

--- trunk/aon-ui-consultant/src/main/java/com/code/aon/ui/consultant/controller/MailingController.java
+++ trunk/aon-ui-consultant/src/main/java/com/code/aon/ui/consultant/controller/MailingController.java
@@ -6,8 +6,8 @@ import javax.faces.event.ActionEvent;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ui.customer.controller.CustomerController;
+import com.code.aon.ui.form.FormUtil;
 import com.code.aon.ui.mailing.MailingManager;
-import com.code.aon.ui.util.AonUtil;
 
 public class MailingController {
 
@@ -15,7 +15,7 @@ public class MailingController {
 
 	@SuppressWarnings({"unchecked", "unused"})
 	public void onGenerateCustomerMailing(ActionEvent event) throws ManagerBeanException {
-        CustomerController customerController = (CustomerController)AonUtil.getController(CUSTOMER_CONTROLLER_NAME);
+        CustomerController customerController = (CustomerController)FormUtil.getController(CUSTOMER_CONTROLLER_NAME);
         Collection collection = customerController.getManagerBean().getList(customerController.getCriteria());
         MailingManager.generateMailing(collection);
 	}
