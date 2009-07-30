a1614364fd0e9edf9dedce404e2cdd1150f326cd
Aimar Tellitu <atellitu@esferalia.com>
2009-07-30 06:58:07 +0000

    Se ha echo más robusto el método getUnreadMessageCount

--- trunk/aon-webmail/src/main/java/com/code/aon/webmail/bean/AonFolder.java
+++ trunk/aon-webmail/src/main/java/com/code/aon/webmail/bean/AonFolder.java
@@ -200,15 +200,16 @@ public class AonFolder extends AonMessageSortableList {
 		}
     }
     
-    public int getUnreadMessageCount() throws WebmailException {
+    public int getUnreadMessageCount() {
+    	int count = 0;
 		try {
 			if ( isHoldMessages() ) {
-				return folder.getUnreadMessageCount();				
+				count = folder.getUnreadMessageCount();				
 			}
-			return 0;
 		} catch (MessagingException e) {
-			throw new WebmailException(e);
+			LOGGER.log( Level.WARNING, "Error getting unread message count", e );
 		}    	
+		return count;
     }
     
     public boolean isDeleteable() throws WebmailException{
