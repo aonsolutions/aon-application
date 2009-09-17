cb2745ff5820efa3b3e291e8f77f7d3d1569284e
Aimar Tellitu <atellitu@esferalia.com>
2009-09-17 10:21:10 +0000

    Correcion del bug 1280. Fallaba al crear un nuevo perfil

--- aon-desktop/src/main/java/com/code/aon/desktop/controller/AonDomainController.java
+++ aon-desktop/src/main/java/com/code/aon/desktop/controller/AonDomainController.java
@@ -407,7 +407,7 @@ public class AonDomainController extends BasicController implements IAonObjectCl
 	}
 	
 	public boolean isSelectedSystemProfile() {
-		return isSystemProfile(this.profile.getId());
+		return newProfile ? false : isSystemProfile(this.profile.getId());
 	}
 
 	public boolean isCurrentSystemProfile() {
