3fd1315de70575ea310c5f0a15935c2b16fe474f
Aimar Tellitu <atellitu@esferalia.com>
2009-09-18 11:18:40 +0000

    Ya no se hace tratamiento especial para el CMS

--- trunk/aon-desktop/src/main/java/com/code/aon/ui/desktop/applications/ApplicationsManager.java
+++ trunk/aon-desktop/src/main/java/com/code/aon/ui/desktop/applications/ApplicationsManager.java
@@ -13,17 +13,13 @@ import javax.faces.context.FacesContext;
 
 import com.code.aon.desktop.IDesktopConstants;
 import com.code.aon.desktop.controller.AonUserController;
-import com.code.aon.desktop.controller.DomainController;
 import com.code.aon.jaas.auth.util.Util;
 import com.code.aon.jaas.client.ast.IApplication;
 import com.code.aon.jaas.deployment.DeploymentException;
 import com.code.aon.ui.form.FormUtil;
-import com.code.aon.ui.util.AonUtil;
 
 public class ApplicationsManager implements IDesktopConstants {
 
-	public static final String AON_CMS = "aon-cms";
-	
 	private List<App> applicationList;
 
 	@SuppressWarnings("unchecked")
@@ -41,16 +37,10 @@ public class ApplicationsManager implements IDesktopConstants {
 		List list = aonUserController.getUserManager().getUserApplications();
         for (int i = 0; i < list.size(); i++) {
 			IApplication app = (IApplication) list.get(i);
-			String context = null;
-			if ( AON_CMS.equals(app.getId()) ) {
-				AonUserController auc = (AonUserController) AonUtil.getRegisteredBean(CURRENT_USER_CONTROLLER_NAME);
-				context = DomainController.getCMSDomainURL(auc.getDomain());
-			} else {
-				context = app.getContext() + "/?aonDesktop=true";
-				String ip = Util.findStoredApplicationIp( thisIp, app.getContext() );
-				if ( !thisIp.equals( ip ) ) {
-					context = ec.getRequestContextPath() + app.getContext() + ".auth?aonDesktop=true";
-				}
+			String context = app.getContext() + "/?aonDesktop=true";
+			String ip = Util.findStoredApplicationIp( thisIp, app.getContext() );
+			if ( !thisIp.equals( ip ) ) {
+				context = ec.getRequestContextPath() + app.getContext() + ".auth?aonDesktop=true";
 			}
 			String property = services.getProperty( app.getId() );
 			App application;
