31ad753064bd27eaa4391b2e09f495d47ced4652
David Uriarte <duriarte@esferalia.com>
2009-09-22 13:39:39 +0000

    *** empty log message ***

--- trunk/aon-ui-ebackoffice/src/main/java/com/code/aon/ui/ebackoffice/controller/EccatalogueController.java
+++ trunk/aon-ui-ebackoffice/src/main/java/com/code/aon/ui/ebackoffice/controller/EccatalogueController.java
@@ -36,46 +36,42 @@ import com.code.aon.ql.Criteria;
 import com.code.aon.ui.common.io.AonFile;
 import com.code.aon.ui.form.BasicController;
 
-
 public class EccatalogueController extends BasicController {
 	private static final Logger LOGGER = Logger
-	.getLogger(EccatalogueController.class.getName());
+			.getLogger(EccatalogueController.class.getName());
 
 	private AonFile image;
-	private AonFile icon;	
-	private List<SelectItem> catalogues;	
+	private AonFile icon;
+	private List<SelectItem> catalogues;
 	private List<SelectItem> catalogueTypes;
-	
-	
+
 	public List<SelectItem> getCatalogueTypes() {
-		if(catalogueTypes==null){
-			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		if (catalogueTypes == null) {
+			Locale locale = FacesContext.getCurrentInstance().getViewRoot()
+					.getLocale();
 			catalogueTypes = new LinkedList<SelectItem>();
 			for (CatalogueType e : CatalogueType.values()) {
-				String name = e.getName( locale );
-				SelectItem item = new SelectItem( e, name );
+				String name = e.getName(locale);
+				SelectItem item = new SelectItem(e, name);
 				catalogueTypes.add(item);
-			}}
-		return catalogueTypes;}
-		
-		
-	public List<SelectItem> getCatalogues() throws ManagerBeanException {
-		catalogues=null;
-		if (catalogues==null) {
-			
-			refreshCatalogues();
+			}
 		}
-		
-		return catalogues;
+		return catalogueTypes;
 	}
 
+	public List<SelectItem> getCatalogues() throws ManagerBeanException {
+		catalogues = null;
+		if (catalogues == null) {
 
+			refreshCatalogues();
+		}
+
+		return catalogues;
+	}
 
 	public void setCatalogues(List<SelectItem> catalogues) {
 		this.catalogues = catalogues;
 	}
-
-
 
 	public void refreshCatalogues() throws ManagerBeanException {
 		catalogues = new LinkedList<SelectItem>();
@@ -91,21 +87,22 @@ public class EccatalogueController extends BasicController {
 		}
 	}
 
-
-	
 	public AonFile getImage() {
 		return image;
 	}
+
 	public void setImage(AonFile image) {
 		this.image = image;
 	}
+
 	public AonFile getIcon() {
 		return icon;
 	}
+
 	public void setIcon(AonFile icon) {
 		this.icon = icon;
 	}
-	
+
 	public void uploadImage(UploadEvent event) {
 		try {
 			UploadItem item = event.getUploadItem();
@@ -122,7 +119,7 @@ public class EccatalogueController extends BasicController {
 			throw new AbortProcessingException(e.getMessage());
 		}
 	}
-	
+
 	public void uploadIcon(UploadEvent event) {
 		try {
 			UploadItem item = event.getUploadItem();
@@ -139,20 +136,20 @@ public class EccatalogueController extends BasicController {
 			throw new AbortProcessingException(e.getMessage());
 		}
 	}
-	
+
 	public void paintImage(OutputStream out, Object data) throws IOException {
 		try {
 			Integer id = (Integer) data;
 			Eccatalogue e = (Eccatalogue) getManagerBean().get(id);
 			if (e != null && e.getCatalogueImg() != null) {
-				out.write(e.getCatalogueImg());	
+				out.write(e.getCatalogueImg());
 			}
 		} catch (ManagerBeanException e) {
 			e.printStackTrace();
 		}
-		
-	} 
-	
+
+	}
+
 	public void paintIcon(OutputStream out, Object data) throws IOException {
 		try {
 			Integer id = (Integer) data;
@@ -163,8 +160,18 @@ public class EccatalogueController extends BasicController {
 		} catch (ManagerBeanException e) {
 			e.printStackTrace();
 		}
-		
-	} 
+	}
+	public void paintIconn(OutputStream out, Object data) throws IOException {
+		if (icon != null && icon.getData() != null) {
+			out.write(icon.getData());
+		}
+	}
 
-	
+	public void paintImagee(OutputStream out, Object data) throws IOException {
+		if (icon != null && image.getData() != null) {
+			out.write(image.getData());
+		}
+
+	}
+
 }
