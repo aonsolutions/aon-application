107a13d698ffaa93d82792fd9790eda75da94092
Gorka Irazu <girazu@esferalia.com>
2009-10-01 10:58:05 +0000

    Organizado mejor los sets en customerFee

--- trunk/aon-finance/src/main/java/com/code/aon/finance/invoicing/remover/FeeInvoiceDetailRemover.java
+++ trunk/aon-finance/src/main/java/com/code/aon/finance/invoicing/remover/FeeInvoiceDetailRemover.java
@@ -26,25 +26,24 @@ public class FeeInvoiceDetailRemover implements IInvoiceDetailRemover {
 	@Override
 	public void removeDetail(InvoiceDetail invoiceDetail) throws InvoicingException{
 		try {
-			IManagerBean invoiceDetailBean = BeanManager.getManagerBean(InvoiceDetail.class);
-			IManagerBean customerFeeBean = BeanManager.getManagerBean(CustomerFee.class);
-			/* CREAR LA CUOTA RELACIONADA */
 			CustomerFee customerFee = new CustomerFee();
 			customerFee.setCustomer(obtainCustomer(invoiceDetail.getInvoice().getRegistry()));
+			customerFee.setItem(invoiceDetail.getItem());
 			customerFee.setDescription(invoiceDetail.getDescription());
+			customerFee.setQuantity(invoiceDetail.getQuantity());
+			customerFee.setPrice(invoiceDetail.getPrice());
 			customerFee.setDiscountExpression(invoiceDetail.getDiscountExpression());
 			customerFee.setInitialDate(invoiceDetail.getInvoice().getIssueDate());
 			customerFee.setFinalDate(invoiceDetail.getInvoice().getIssueDate());
 			customerFee.setBillingDate(invoiceDetail.getInvoice().getIssueDate());
-			customerFee.setItem(invoiceDetail.getItem());
 			customerFee.setPeriod(BillingPeriod.NO_PERIOD);
-			customerFee.setPrice(invoiceDetail.getPrice());
-			customerFee.setQuantity(invoiceDetail.getQuantity());
 			customerFee.setSecurityLevel(invoiceDetail.getInvoice().getSecurityLevel());
 			customerFee.setWorkPlace(invoiceDetail.getWorkPlace());
+
+			IManagerBean customerFeeBean = BeanManager.getManagerBean(CustomerFee.class);
 			customerFeeBean.insert(customerFee);
-			/* BORRAR LA LINEA DE FACTURA */
-			LOGGER.fine("Attempt to remove Invoice Detail: " + invoiceDetail.getId());
+
+			IManagerBean invoiceDetailBean = BeanManager.getManagerBean(InvoiceDetail.class);
 			invoiceDetailBean.remove(invoiceDetail);
 		} catch (ManagerBeanException e) {
 			LOGGER.log(Level.SEVERE, "Error removing Details", e);
