e4166784b5facd48f6edacc3cd54166a9ea61f1f
Gorka Irazu <girazu@esferalia.com>
2009-10-01 11:06:49 +0000

    Eliminadas fechas de este listener de busqueda, se implementan ya en el propio controlador

--- aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/OfferSearchListener.java
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/OfferSearchListener.java
@@ -1,7 +1,5 @@
 package com.code.aon.ui.commercial.event;
 
-import java.util.Date;
-
 import com.code.aon.commercial.Target;
 import com.code.aon.commercial.dao.ICommercialAlias;
 import com.code.aon.common.ManagerBeanException;
@@ -11,27 +9,7 @@ import com.code.aon.ui.form.event.ControllerSearchListener;
 
 public class OfferSearchListener extends ControllerSearchListener {
 
-	private Date issueDateFrom;
-	
-	private Date issueDateTo;
-
 	private Target target;
-
-	public Date getIssueDateFrom() {
-		return issueDateFrom;
-	}
-
-	public void setIssueDateFrom(Date issueDateFrom) {
-		this.issueDateFrom = issueDateFrom;
-	}
-
-	public Date getIssueDateTo() {
-		return issueDateTo;
-	}
-
-	public void setIssueDateTo(Date issueDateTo) {
-		this.issueDateTo = issueDateTo;
-	}
 
 	public Target getTarget() {
 		return target;
@@ -43,20 +21,12 @@ public class OfferSearchListener extends ControllerSearchListener {
 
 	@Override
 	protected void init() throws ManagerBeanException {
-		setIssueDateFrom(null);
-		setIssueDateTo(null);
 		setTarget(new Target());
 	}
 	
 	@Override
 	protected void completeCriteria() throws ManagerBeanException, ExpressionException {
 		Criteria criteria = getController().getCriteria();
-		if (getIssueDateFrom() != null) {
-			criteria.addGreaterThanOrEqualExpression(getController().getFieldName(ICommercialAlias.OFFER_ISSUE_DATE), getIssueDateFrom());
-		}
-		if (getIssueDateTo() != null) {
-			criteria.addLessThanOrEqualExpression(getController().getFieldName(ICommercialAlias.OFFER_ISSUE_DATE), getIssueDateTo());
-		}
 		if (getTarget() != null && getTarget().getId() != null) {
 			criteria.addEqualExpression(getController().getFieldName(ICommercialAlias.OFFER_TARGET_ID), getTarget().getId());			
 		}
