08607afd3e0f19b6cf9942dfec2668927af78f8b
Gorka Irazu <girazu@esferalia.com>
2009-10-01 11:02:04 +0000

    Añadidas nuevas columnas en Sales y SalesDetail

--- aon-sales/src/main/java/com/code/aon/sales/dao/SalesAliasWriter.java
+++ aon-sales/src/main/java/com/code/aon/sales/dao/SalesAliasWriter.java
@@ -5,20 +5,16 @@ import java.io.File;
 import com.code.aon.common.dao.AliasWriter;
 import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.sales.Sales;
-import com.code.aon.sales.SalesAttachment;
 import com.code.aon.sales.SalesDetail;
-import com.code.aon.sales.SalesType;
 
 public class SalesAliasWriter {
 
 	public static void main(String[] args) throws Exception{
 		File file = new File("/AON-PROJECT/aon-sales/src/main/java/com/code/aon/sales/dao/ISalesAlias.java");
-		String[] classes = new String[4];
+		String[] classes = new String[2];
 		classes[0] = Sales.class.getName();
-		classes[1] = SalesAttachment.class.getName();
-		classes[2] = SalesDetail.class.getName();
-		classes[3] = SalesType.class.getName();
-		HibernateUtil.getSessionFactory();
+		classes[1] = SalesDetail.class.getName();
+		HibernateUtil.getSessionFactory(null);
 		AliasWriter writer = new AliasWriter("com.code.aon.sales.dao");
 		writer.write(classes, file);
 		System.out.println( file.getAbsolutePath() );
