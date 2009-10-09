eaaf663fdbf60c937eee9a8b19244503aae47226
Gorka Irazu <girazu@esferalia.com>
2009-10-09 16:43:17 +0000

    Añadidos checks de status para la busqueda

--- aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/OfferSearchListener.java
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/event/OfferSearchListener.java
@@ -1,7 +1,10 @@
 package com.code.aon.ui.commercial.event;
 
+import org.apache.commons.lang.ArrayUtils;
+
 import com.code.aon.commercial.Target;
 import com.code.aon.commercial.dao.ICommercialAlias;
+import com.code.aon.commercial.enumeration.OfferStatus;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
@@ -11,6 +14,8 @@ public class OfferSearchListener extends ControllerSearchListener {
 
 	private Target target;
 
+	private OfferStatus[] offerStatuses;
+
 	public Target getTarget() {
 		return target;
 	}
@@ -19,9 +24,19 @@ public class OfferSearchListener extends ControllerSearchListener {
 		this.target = target;
 	}
 
+	public OfferStatus[] getOfferStatuses() {
+		return offerStatuses;
+	}
+
+	public void setOfferStatuses(OfferStatus[] offerStatuses) {
+		this.offerStatuses = offerStatuses;
+	}
+	
 	@Override
 	protected void init() throws ManagerBeanException {
 		setTarget(new Target());
+		OfferStatus[] defaultOfferStatus = {OfferStatus.PENDING};
+		setOfferStatuses(defaultOfferStatus);
 	}
 	
 	@Override
@@ -30,5 +45,10 @@ public class OfferSearchListener extends ControllerSearchListener {
 		if (getTarget() != null && getTarget().getId() != null) {
 			criteria.addEqualExpression(getController().getFieldName(ICommercialAlias.OFFER_TARGET_ID), getTarget().getId());			
 		}
+		if (!ArrayUtils.isEmpty(getOfferStatuses())) {
+			String status = getController().resolveAlias(ICommercialAlias.OFFER_STATUS);
+			addEnumToCriteria(criteria, status, getOfferStatuses());
+		}
 	}	
+
 }
--- aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesSearchListener.java
+++ aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesSearchListener.java
@@ -1,15 +1,20 @@
 package com.code.aon.ui.sales.event;
 
+import org.apache.commons.lang.ArrayUtils;
+
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.customer.Customer;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.sales.dao.ISalesAlias;
+import com.code.aon.sales.enumeration.SalesStatus;
 import com.code.aon.ui.form.event.ControllerSearchListener;
 
 public class SalesSearchListener extends ControllerSearchListener {
 
 	private Customer customer;
+
+	private SalesStatus[] salesStatuses;
 
 	public Customer getCustomer() {
 		return customer;
@@ -19,9 +24,19 @@ public class SalesSearchListener extends ControllerSearchListener {
 		this.customer = customer;
 	}
 
+	public SalesStatus[] getSalesStatuses() {
+		return salesStatuses;
+	}
+
+	public void setSalesStatuses(SalesStatus[] salesStatuses) {
+		this.salesStatuses = salesStatuses;
+	}
+	
 	@Override
 	protected void init() throws ManagerBeanException {
 		setCustomer(new Customer());
+		SalesStatus[] defaultSalesStatus = {SalesStatus.PENDING};
+		setSalesStatuses(defaultSalesStatus);
 	}
 	
 	@Override
@@ -29,6 +44,10 @@ public class SalesSearchListener extends ControllerSearchListener {
 		Criteria criteria = getController().getCriteria();
 		if (getCustomer() != null && getCustomer().getId() != null) {
 			criteria.addEqualExpression(getController().getFieldName(ISalesAlias.SALES_CUSTOMER_ID), getCustomer().getId());			
+		}
+		if (!ArrayUtils.isEmpty(getSalesStatuses())) {
+			String status = getController().resolveAlias(ISalesAlias.SALES_STATUS);
+			addEnumToCriteria(criteria, status, getSalesStatuses());
 		}
 	}	
 }
