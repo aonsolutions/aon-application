45d3bb456a653ddacb03fe148fe4e03b29f50c5a
Aimar Tellitu <atellitu@esferalia.com>
2009-10-16 11:11:20 +0000

    Se ha añadido CommercialTerm y modificado OfferTerm

---
+++ aon-commercial/src/main/java/com/code/aon/commercial/CommercialTerm.java
@@ -0,0 +1,156 @@
+package com.code.aon.commercial;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.hibernate.annotations.Type;
+
+import com.code.aon.common.ITransferObject;
+
+/**
+ * Transfer Object that represents a CommercialTerm.
+ */
+@Entity
+@Table(name="commercial_term")
+public class CommercialTerm implements ITransferObject {
+
+	private static final long serialVersionUID = -3406733973884962905L;
+
+	/** The id. */
+	private Integer id;
+
+    /** The name. */
+    private String name;
+	
+    /** The description. */
+    private String description;
+
+    /** If the OfferTerm is particular. */
+    private boolean particular;    
+    
+	/**
+	 * Gets the id.
+	 * 
+	 * @return the id
+	 */
+	@Id
+	@GeneratedValue
+	@Column(nullable=false)
+    public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 * 
+	 * @param id the id
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+    /**
+     * Gets the name.
+     * 
+     * @return the name
+     */
+    @Column(length=32, nullable = false)
+	public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name.
+     * 
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }	
+
+    /**
+     * Gets the description.
+     * 
+     * @return the description
+     */
+	@Lob
+	@Column( nullable = false )
+	@Type(type="stringClob")	
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description.
+     * 
+     * @param description the description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+	/**
+	 * Checks if is particular.
+	 * 
+	 * @return true, if is particular
+	 */
+	@Column(nullable = false)
+	public boolean isParticular() {
+		return particular;
+	}
+
+	/**
+	 * Sets the particular.
+	 * 
+	 * @param signed the new particular
+	 */
+	public void setParticular(boolean particular) {
+		this.particular = particular;
+	}	
+    
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final CommercialTerm o = (CommercialTerm) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.description, o.description)				
+				.append(this.particular, o.particular)
+				.append(this.name, o.name)				
+				.isEquals();
+		}
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(description)	
+			.append(particular)			
+			.append(id)			
+			.append(name)
+			.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this).
+			append("description", StringUtils.abbreviate(description, 64)).
+			append("particular", particular).
+			append("id", id).
+			append("name", name).
+			toString();
+	}
+	
+}
