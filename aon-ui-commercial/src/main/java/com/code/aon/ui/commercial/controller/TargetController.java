14be751681df90c4cc6bcd7c32f24f00fb3b626f
David Uriarte <duriarte@esferalia.com>
2009-10-16 11:19:58 +0000

    *** empty log message ***

--- aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/controller/TargetController.java
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/controller/TargetController.java
@@ -8,12 +8,14 @@ import java.util.ResourceBundle;
 import javax.faces.model.SelectItem;
 
 import com.code.aon.commercial.CommercialSegment;
+import com.code.aon.commercial.Target;
 import com.code.aon.commercial.dao.ICommercialAlias;
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
+import com.code.aon.registry.RegistryMedia;
 import com.code.aon.ui.commercial.ICommercialMessages;
 import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.util.AonUtil;
@@ -29,6 +31,40 @@ public class TargetController extends BasicController {
 	
 	private String selectedTab;
 	
+	/** The phone. */
+	private RegistryMedia phone;
+
+	/** The fax. */
+	private RegistryMedia fax;
+
+	/** The email. */
+	private RegistryMedia email;
+	
+		
+	public RegistryMedia getPhone() {
+		return phone;
+	}
+
+	public void setPhone(RegistryMedia phone) {
+		this.phone = phone;
+	}
+
+	public RegistryMedia getFax() {
+		return fax;
+	}
+
+	public void setFax(RegistryMedia fax) {
+		this.fax = fax;
+	}
+
+	public RegistryMedia getEmail() {
+		return email;
+	}
+
+	public void setEmail(RegistryMedia email) {
+		this.email = email;
+	}
+
 	public TargetController() {
 		setBundleName(ICommercialMessages.BUNDLE_KEY);
 	}
@@ -65,5 +101,32 @@ public class TargetController extends BasicController {
 	public void setBundleName(String bundleName) {
 		this.bundle = AonUtil.getResourceBundle(bundleName);
 	}	
+	
+	
+	public void obtainPhone() throws ManagerBeanException {	
+		if (((Target)this.getTo()).getRegistry().getPhone()!=null)
+		{
+		setPhone(((Target)this.getTo()).getRegistry().getCellular());
+		}
+	}
+	
+	public void obtainFax() throws ManagerBeanException {
+		if (((Target)this.getTo()).getRegistry().getFax()!=null)
+		{		
+		setFax(((Target)this.getTo()).getRegistry().getFax());
+		}			
+	}
+	
+	
+	public void obtainEmail() throws ManagerBeanException {	
+		if (((Target)this.getTo()).getRegistry().getEmail()!=null)
+		{
+		setEmail(((Target)this.getTo()).getRegistry().getEmail());
+		}
+	}
+	
+	
+	
+	
 	
 }
