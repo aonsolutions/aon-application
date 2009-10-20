5ea2ad0533e2eaabe60bf82ceb0d682bcb81fbbc
Aimar Tellitu <atellitu@esferalia.com>
2009-10-20 07:53:26 +0000

    *** empty log message ***

--- aon-commercial/src/main/java/com/code/aon/commercial/dao/ICommercialAlias.java
+++ aon-commercial/src/main/java/com/code/aon/commercial/dao/ICommercialAlias.java
@@ -376,22 +376,34 @@ public interface ICommercialAlias {
 	DAOConstantsEntry OFFER_TERM_ENTRY = DAOConstants.getDAOConstant(OfferTerm.class);
 
 	/** 
+	* Alias value: OfferTerm_description
+	* Hibernate value: OfferTerm.description
+	*/
+	String  OFFER_TERM_DESCRIPTION = OFFER_TERM_ENTRY.getAliasNames()[0];
+
+	/** 
+	* Alias value: OfferTerm_general
+	* Hibernate value: OfferTerm.general
+	*/
+	String  OFFER_TERM_GENERAL = OFFER_TERM_ENTRY.getAliasNames()[1];
+
+	/** 
 	* Alias value: OfferTerm_id
 	* Hibernate value: OfferTerm.id
 	*/
-	String  OFFER_TERM_ID = OFFER_TERM_ENTRY.getAliasNames()[0];
+	String  OFFER_TERM_ID = OFFER_TERM_ENTRY.getAliasNames()[2];
+
+	/** 
+	* Alias value: OfferTerm_name
+	* Hibernate value: OfferTerm.name
+	*/
+	String  OFFER_TERM_NAME = OFFER_TERM_ENTRY.getAliasNames()[3];
 
 	/** 
 	* Alias value: OfferTerm_offer_id
 	* Hibernate value: OfferTerm.offer.id
 	*/
-	String  OFFER_TERM_OFFER_ID = OFFER_TERM_ENTRY.getAliasNames()[1];
-
-	/** 
-	* Alias value: OfferTerm_term_id
-	* Hibernate value: OfferTerm.term.id
-	*/
-	String  OFFER_TERM_TERM_ID = OFFER_TERM_ENTRY.getAliasNames()[2];
+	String  OFFER_TERM_OFFER_ID = OFFER_TERM_ENTRY.getAliasNames()[4];
 
 
 
@@ -561,22 +573,22 @@ public interface ICommercialAlias {
 	String  COMMERCIAL_TERM_DESCRIPTION = COMMERCIAL_TERM_ENTRY.getAliasNames()[0];
 
 	/** 
+	* Alias value: CommercialTerm_general
+	* Hibernate value: CommercialTerm.general
+	*/
+	String  COMMERCIAL_TERM_GENERAL = COMMERCIAL_TERM_ENTRY.getAliasNames()[1];
+
+	/** 
 	* Alias value: CommercialTerm_id
 	* Hibernate value: CommercialTerm.id
 	*/
-	String  COMMERCIAL_TERM_ID = COMMERCIAL_TERM_ENTRY.getAliasNames()[1];
+	String  COMMERCIAL_TERM_ID = COMMERCIAL_TERM_ENTRY.getAliasNames()[2];
 
 	/** 
 	* Alias value: CommercialTerm_name
 	* Hibernate value: CommercialTerm.name
 	*/
-	String  COMMERCIAL_TERM_NAME = COMMERCIAL_TERM_ENTRY.getAliasNames()[2];
-
-	/** 
-	* Alias value: CommercialTerm_particular
-	* Hibernate value: CommercialTerm.particular
-	*/
-	String  COMMERCIAL_TERM_PARTICULAR = COMMERCIAL_TERM_ENTRY.getAliasNames()[3];
+	String  COMMERCIAL_TERM_NAME = COMMERCIAL_TERM_ENTRY.getAliasNames()[3];
 
 
 }
