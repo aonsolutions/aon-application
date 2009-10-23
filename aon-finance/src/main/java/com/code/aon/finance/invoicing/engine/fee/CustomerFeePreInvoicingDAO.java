8a4ea9ea8140f39d08760622b161ef4ed38d64bb
Gorka Irazu <girazu@esferalia.com>
2009-10-23 15:19:48 +0000

    Rellenar los valores de las propiedades de TaxInfo en la prefacturacion, ya que aqui no actua el InvoiceBeanVetoListener que se encarga de rellenar estos datos.

--- aon-finance/src/main/java/com/code/aon/finance/invoicing/engine/fee/CustomerFeePreInvoicingDAO.java
+++ aon-finance/src/main/java/com/code/aon/finance/invoicing/engine/fee/CustomerFeePreInvoicingDAO.java
@@ -2,17 +2,23 @@ package com.code.aon.finance.invoicing.engine.fee;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.company.Company;
+import com.code.aon.customer.Customer;
 import com.code.aon.finance.Invoice;
 import com.code.aon.finance.InvoiceDetail;
 import com.code.aon.finance.invoicing.engine.IInvoicingDAO;
 import com.code.aon.finance.invoicing.pricing.InvoicePriceStrategy;
 import com.code.aon.product.strategy.IPriceStrategy;
+import com.code.aon.registry.Registry;
 
 public class CustomerFeePreInvoicingDAO implements IInvoicingDAO {
 	
@@ -30,6 +36,7 @@ public class CustomerFeePreInvoicingDAO implements IInvoicingDAO {
 
 	public Invoice insertInvoice(Invoice invoice) {
 		invoice.setId(new Integer(preInvoicingCollection.size() + 1));
+		fillTaxInfo(invoice);
 		currentPreInvoice = new PreInvoice(invoice);
 		preInvoicingCollection.add(currentPreInvoice);
 		return invoice;
@@ -54,7 +61,38 @@ public class CustomerFeePreInvoicingDAO implements IInvoicingDAO {
 	public Collection getCollection(){
 		return preInvoicingCollection;
 	}
-	
+
+	private void fillTaxInfo(Invoice invoice) {
+		Company company = obtainCompany();
+		Customer customer = obtainCustomer(invoice.getRegistry());
+		invoice.setWithholding(company.isWithholding() && customer.isWithholding());
+		invoice.setSurcharge(customer.isSurcharge());
+		invoice.setTaxFree(customer.isTaxFree());
+	}
+
+	private Company obtainCompany() {
+		try {
+			IManagerBean companyBean = BeanManager.getManagerBean(Company.class);
+			Iterator<ITransferObject> iterator = companyBean.getList(null, 0, 1).iterator();
+			if (iterator.hasNext()) {
+				return (Company) iterator.next();
+			}
+		} catch (ManagerBeanException e) {
+			LOGGER.log(Level.SEVERE, "Error obtaining company", e);
+		}
+		return null;
+	}
+
+	private Customer obtainCustomer(Registry registry) {
+		try {
+			IManagerBean customerBean = BeanManager.getManagerBean(Customer.class);
+			return (Customer)customerBean.get(registry.getId());
+		} catch (ManagerBeanException e) {
+			LOGGER.log(Level.SEVERE, "Error obtaining customer with id=" + registry.getId(), e);
+		}
+		return null;
+	}
+
 	private IPriceStrategy getPriceStrategy(){
 		if(priceStrategy == null){
 			priceStrategy = new InvoicePriceStrategy();
