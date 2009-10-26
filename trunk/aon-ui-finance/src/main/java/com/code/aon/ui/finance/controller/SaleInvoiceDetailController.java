ff66a1d8ae12d1bf1a7169f0d81b7836e287d794
Gorka Irazu <girazu@esferalia.com>
2009-10-26 11:09:03 +0000

    Añadido metodo getLineSourceInfo() para notificar el origen del invoiceDetail

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/SaleInvoiceDetailController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/SaleInvoiceDetailController.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 
+import com.code.aon.commercial.OfferDetail;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.config.Tariff;
 import com.code.aon.customer.Customer;
@@ -12,9 +15,12 @@ import com.code.aon.finance.InvoiceDetail;
 import com.code.aon.finance.enumeration.InvoiceSource;
 import com.code.aon.finance.invoicing.pricing.InvoicePriceStrategy;
 import com.code.aon.product.Item;
+import com.code.aon.product.strategy.ICalculable;
 import com.code.aon.product.strategy.IPriceStrategy;
 import com.code.aon.ui.common.components.LookupChangeEvent;
 import com.code.aon.ui.form.LinesController;
+import com.code.aon.ui.util.AonUtil;
+import com.code.aon.warehouse.DeliveryDetail;
 
 public class SaleInvoiceDetailController extends LinesController {
 
@@ -49,7 +55,7 @@ public class SaleInvoiceDetailController extends LinesController {
 		if (getModel().isRowAvailable()) {
 			InvoiceDetail invoiceDetail = (InvoiceDetail)this.getModel().getRowData();
 			InvoiceSource source = invoiceDetail.getSource();
-			if (source.equals(InvoiceSource.DELIVERY) || source.equals(InvoiceSource.INCOME) || source.equals(InvoiceSource.OFFER)) {
+			if (source == InvoiceSource.DELIVERY || source == InvoiceSource.INCOME || source == InvoiceSource.OFFER) {
 				return false;
 			}
 		}
@@ -101,8 +107,42 @@ public class SaleInvoiceDetailController extends LinesController {
 	}
 
 	public double getTaxableBase() {
-		InvoiceDetail invoiceDetail = (InvoiceDetail) getTo();
-		return getPriceStrategy().getBasePrice(invoiceDetail);
+		return getPriceStrategy().getBasePrice((ICalculable)getTo());
+	}
+
+	public String getLineSourceInfo() throws ManagerBeanException {
+		StringBuffer info = new StringBuffer(64);
+
+		InvoiceDetail invoiceDetail = (InvoiceDetail)this.getModel().getRowData();
+		if (!isEditable() && invoiceDetail.getSourceId() != null) {
+			String message = "";
+			String refCode = "";
+			int line = 0;
+			if (invoiceDetail.getSource() == InvoiceSource.OFFER) {
+				IManagerBean offerDetailBean = BeanManager.getManagerBean(OfferDetail.class);
+				OfferDetail offerDetail = (OfferDetail)offerDetailBean.get(invoiceDetail.getSourceId());
+				message = AonUtil.getMessage("commercialBundle", "commercial_offer");
+				refCode = offerDetail.getOffer().getReferenceCode();
+				line = offerDetail.getLine().intValue();
+			} else if (invoiceDetail.getSource() == InvoiceSource.DELIVERY) {
+				IManagerBean deliveryDetailBean = BeanManager.getManagerBean(DeliveryDetail.class);
+				DeliveryDetail deliveryDetail = (DeliveryDetail)deliveryDetailBean.get(invoiceDetail.getSourceId());
+				message = AonUtil.getMessage("financeBundle", "finance_invoice_delivery");
+				refCode = deliveryDetail.getDelivery().getReferenceCode();
+				line = deliveryDetail.getLine().intValue();
+			}
+
+			info.append(AonUtil.getMessage("financeBundle", "finance_source"));
+			info.append(" ");
+			info.append(message);
+			info.append(" ");
+			info.append(refCode);
+			info.append(" - ");
+			info.append(AonUtil.getMessage("financeBundle", "finance_invoice_detail_line"));
+			info.append(" ");
+			info.append(line);
+		}
+		return info.toString();
 	}
 
 }
