9aff27c4f700b5b37237678a10cf058c469ffb0b
Gorka Irazu <girazu@esferalia.com>
2009-10-27 16:41:00 +0000

    Error corregido. Se recupera un InvoiceController y no un SaleInvoiceController

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoiceControllerListener.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoiceControllerListener.java
@@ -2,7 +2,7 @@ package com.code.aon.ui.finance.event;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.finance.Invoice;
-import com.code.aon.ui.finance.controller.SaleInvoiceController;
+import com.code.aon.ui.finance.controller.InvoiceController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -12,8 +12,8 @@ public class InvoiceControllerListener extends ControllerAdapter {
 	@Override
 	public void afterBeanCreated(ControllerEvent event) throws ControllerListenerException {
 		try {
-			SaleInvoiceController saleInvoiceController = (SaleInvoiceController)this.getController(); 
-			saleInvoiceController.loadAddresses(null);
+			InvoiceController invoiceController = (InvoiceController)this.getController(); 
+			invoiceController.loadAddresses(null);
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException(e.getMessage());
 		}
@@ -22,9 +22,9 @@ public class InvoiceControllerListener extends ControllerAdapter {
 	@Override
 	public void afterBeanSelected(ControllerEvent event) throws ControllerListenerException {
 		try {
-			SaleInvoiceController saleInvoiceController = (SaleInvoiceController)this.getController(); 
-			Invoice invoice = (Invoice) saleInvoiceController.getTo();
-			saleInvoiceController.loadAddresses(invoice.getRegistry().getId());
+			InvoiceController invoiceController = (InvoiceController)this.getController(); 
+			Invoice invoice = (Invoice) invoiceController.getTo();
+			invoiceController.loadAddresses(invoice.getRegistry().getId());
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException(e.getMessage());
 		}
