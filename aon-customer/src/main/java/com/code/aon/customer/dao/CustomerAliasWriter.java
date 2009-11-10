bc4713adb84d60c65020ac67ce4ca4472594d359
Gorka Irazu <girazu@esferalia.com>
2009-11-10 15:24:21 +0000

    Eliminada clase CustomerSegment

--- aon-customer/src/main/java/com/code/aon/customer/dao/CustomerAliasWriter.java
+++ aon-customer/src/main/java/com/code/aon/customer/dao/CustomerAliasWriter.java
@@ -5,15 +5,13 @@ import java.io.File;
 import com.code.aon.common.dao.AliasWriter;
 import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.customer.Customer;
-import com.code.aon.customer.CustomerSegment;
 
 public class CustomerAliasWriter {
 
 	public static void main(String[] args) throws Exception{
 		File file = new File("/AON-PROJECT/aon-customer/src/main/java/com/code/aon/customer/dao/ICustomerAlias.java");
-		String[] classes = new String[2];
+		String[] classes = new String[1];
 		classes[0] = Customer.class.getName();
-		classes[1] = CustomerSegment.class.getName();
 		HibernateUtil.getSessionFactory( HibernateUtil.getSessionFactoryName() );
 		AliasWriter writer = new AliasWriter("com.code.aon.customer.dao");
 		writer.write(classes, file);
