c23901fb017b00c66054cc9ff125e2bb7001f05f
Gorka Irazu <girazu@esferalia.com>
2009-11-10 15:05:40 +0000

    Añadir el tipo de Transaccion

--- aon-registry/src/main/java/com/code/aon/registry/ITaxInfo.java
+++ aon-registry/src/main/java/com/code/aon/registry/ITaxInfo.java
@@ -1,10 +1,19 @@
 package com.code.aon.registry;
 
+import com.code.aon.config.enumeration.InvoiceTransactionType;
+
 /**
  * Interface that must implement all the entities which are susceptible to pay taxes
  */
 public interface ITaxInfo {
 	
+    /**
+     * Checks if a surcharge has to be applied.
+     * 
+     * @return true, if a surcharge has to be applied
+     */
+    public boolean isSurcharge();
+
     /**
      * Checks if is tax free or not.
      * 
@@ -13,18 +22,17 @@ public interface ITaxInfo {
     public boolean isTaxFree();
 
     /**
-     * Checks if a surcharge has to be applied.
-     * 
-     * @return true, if a surcharge has to be applied
-     */
-    public boolean isSurcharge();
-    
-    
-    /**
      * Checks if a withholding is applied.
      * 
      * @return true, if a withholding is applied
      */
     public boolean isWithholding();
+
+    /**
+     * Return the transaction type for this ITaxInfo
+     * 
+     * @return InvoiceTransactionType the transaction type
+     */
+    public InvoiceTransactionType getTransaction();
 
 }
