b768ccc0731008d52eff9431eba6bf82d977577a
Gorka Irazu <girazu@esferalia.com>
2009-11-10 17:02:24 +0000

    Eliminados metodos en desuso o subidos al RegistryController

--- trunk/aon-ui-customer/src/main/java/com/code/aon/ui/customer/controller/CustomerController.java
+++ trunk/aon-ui-customer/src/main/java/com/code/aon/ui/customer/controller/CustomerController.java
@@ -1,8 +1,5 @@
 package com.code.aon.ui.customer.controller;
 
-import com.code.aon.customer.Customer;
-import com.code.aon.person.Person;
-import com.code.aon.registry.enumeration.RegistryType;
 import com.code.aon.ui.registry.controller.RegistryController;
 
 /**
@@ -10,29 +7,4 @@ import com.code.aon.ui.registry.controller.RegistryController;
  */
 public class CustomerController extends RegistryController {
 	
-	private Person person;
-	
-	private boolean newPerson;
-	
-	public Person getPerson() {
-		return person;
-	}
-
-	public void setPerson(Person person) {
-		this.person = person;
-	}
-
-	public boolean isNewPerson() {
-		return newPerson;
-	}
-
-	public void setNewPerson(boolean newPerson) {
-		this.newPerson = newPerson;
-	}
-
-    public boolean isNaturalType(){
-    	Customer customer = (Customer)getTo();
-    	return customer.getRegistry().getType().equals(RegistryType.NATURAL);
-    }
-
 }
