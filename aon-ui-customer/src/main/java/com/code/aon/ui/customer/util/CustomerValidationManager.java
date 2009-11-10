5a8db4ae4de9ecb076bfe123c31f21eff4d925d3
Gorka Irazu <girazu@esferalia.com>
2009-11-10 17:04:05 +0000

    Ahora extiende de RegistryValidationManager y los metodos comunes se han pasado a ese controlador

--- aon-ui-customer/src/main/java/com/code/aon/ui/customer/util/CustomerValidationManager.java
+++ aon-ui-customer/src/main/java/com/code/aon/ui/customer/util/CustomerValidationManager.java
@@ -1,54 +1,36 @@
 package com.code.aon.ui.customer.util;
 
-import java.util.List;
 import java.util.Locale;
 
 import javax.faces.context.FacesContext;
 
-import com.code.aon.common.BeanManager;
-import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.customer.Customer;
 import com.code.aon.customer.enumeration.CustomerStatus;
-import com.code.aon.ql.Criteria;
-import com.code.aon.registry.Registry;
 import com.code.aon.registry.RegistryNote;
-import com.code.aon.registry.dao.IRegistryAlias;
-import com.code.aon.registry.enumeration.NoteType;
+import com.code.aon.ui.registry.util.RegistryValidationManager;
 import com.code.aon.ui.util.AonUtil;
 
-public class CustomerValidationManager {
+public class CustomerValidationManager extends RegistryValidationManager {
 
-	public boolean isBlocked(Customer customer) {
-		if (customer.getStatus() == CustomerStatus.BLOCKED) {
-			String obs = null;
-			try {
-				RegistryNote observation = getRegistryObservation(customer.getRegistry());
-				obs = observation!=null?observation.getComments():null;
-			} catch (ManagerBeanException e) {
-				// Si falla, no saldrá el mensaje en pantalla. Se desprecia el error a posta.
+	public boolean isBlocked(ITransferObject to) {
+		if (to instanceof Customer) {
+			Customer customer = (Customer)to;
+			if (customer.getStatus() == CustomerStatus.BLOCKED) {
+				String comments = "";
+				try {
+					RegistryNote observation = getRegistryObservation(customer.getRegistry());
+					comments = (observation != null) ? observation.getComments() : comments;
+				} catch (ManagerBeanException e) {
+					// Si falla, no saldrá el mensaje en pantalla. Se desprecia el error a posta.
+				}
+				Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+				AonUtil.addErrorMessage("[" + CustomerStatus.BLOCKED.getName(locale) + "] : " + comments);
+				return true;
 			}
-			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-			AonUtil.addErrorMessage("[" + CustomerStatus.BLOCKED.getName(locale) + "]"
-					+ (obs == null ? "" : ": " + obs));
-			return true;
 		}
 		return false;
-	}
-
-	private RegistryNote getRegistryObservation(Registry registry) throws ManagerBeanException {
-		IManagerBean rNoteBean = BeanManager.getManagerBean(RegistryNote.class);
-		Criteria criteria = new Criteria();
-		criteria.addEqualExpression(rNoteBean
-				.getFieldName(IRegistryAlias.REGISTRY_NOTE_REGISTRY_ID), registry.getId());
-		criteria.addEqualExpression(rNoteBean.getFieldName(IRegistryAlias.REGISTRY_NOTE_NOTETYPE),
-				NoteType.OBSERVATION);
-		List<ITransferObject> l = rNoteBean.getList(criteria);
-		if (!l.isEmpty()) {
-			return (RegistryNote) l.iterator().next();
-		}
-		return null;
 	}
 
 }
