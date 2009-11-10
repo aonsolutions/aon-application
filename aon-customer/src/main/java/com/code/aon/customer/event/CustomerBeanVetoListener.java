16caacfb46f146c2da32098a023103cd42c4e488
Gorka Irazu <girazu@esferalia.com>
2009-11-10 15:24:03 +0000

    Si el Transaction llega a null se le mete NATIONAL por defecto

--- aon-customer/src/main/java/com/code/aon/customer/event/CustomerBeanVetoListener.java
+++ aon-customer/src/main/java/com/code/aon/customer/event/CustomerBeanVetoListener.java
@@ -3,6 +3,7 @@ package com.code.aon.customer.event;
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
+import com.code.aon.config.enumeration.InvoiceTransactionType;
 import com.code.aon.customer.Customer;
 import com.code.aon.customer.enumeration.CustomerStatus;
 
@@ -11,10 +12,12 @@ public class CustomerBeanVetoListener extends ManagerBeanVetoListenerAdapter {
     @Override
     public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
     	Customer to = (Customer)evt.getTo();
-    	if (to.getStatus() == null ) {
-    		to.setStatus( CustomerStatus.ACTIVE );
+    	if (to.getStatus() == null) {
+    		to.setStatus(CustomerStatus.ACTIVE);
+    	}
+    	if (to.getTransaction() == null) {
+    		to.setTransaction(InvoiceTransactionType.NATIONAL);
     	}
     }
-	
 
 }
