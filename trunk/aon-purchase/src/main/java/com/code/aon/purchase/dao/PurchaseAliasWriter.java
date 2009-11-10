af4a49bea2892f562fd9022de4b3329280e34a62
Gorka Irazu <girazu@esferalia.com>
2009-11-10 15:50:11 +0000

    Añadida sesion de hibernate

--- trunk/aon-purchase/src/main/java/com/code/aon/purchase/dao/PurchaseAliasWriter.java
+++ trunk/aon-purchase/src/main/java/com/code/aon/purchase/dao/PurchaseAliasWriter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.code.aon.common.dao.AliasWriter;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.purchase.Purchase;
 import com.code.aon.purchase.PurchaseDetail;
 
@@ -14,6 +15,7 @@ public class PurchaseAliasWriter {
         String[] classes = new String[2];
         classes[0] = PurchaseDetail.class.getName();
         classes[1] = Purchase.class.getName();
+		HibernateUtil.getSessionFactory(null);
 		AliasWriter writer = new AliasWriter("com.code.aon.purchase.dao");
 		writer.write(classes,file);
 		System.out.println("Alias generados");
