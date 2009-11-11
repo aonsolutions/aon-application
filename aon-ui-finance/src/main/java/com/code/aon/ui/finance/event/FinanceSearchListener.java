83b2ac42d448b6482051f8871d58c6faf78122b1
Gorka Irazu <girazu@esferalia.com>
2009-11-11 07:59:21 +0000

    Meter el estado RETURNED en el defaultFinanceStatus

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/FinanceSearchListener.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/FinanceSearchListener.java
@@ -109,7 +109,7 @@ public class FinanceSearchListener extends ControllerSearchListener {
 		setSupplier(new Supplier());
 		setCreditor(new Creditor());
 		setRegistryBank(new RegistryBank());
-		FinanceStatus[] defaultFinanceStatus = {FinanceStatus.PENDING};
+		FinanceStatus[] defaultFinanceStatus = {FinanceStatus.PENDING, FinanceStatus.RETURNED};
 		setFinanceStatuses(defaultFinanceStatus);
 		setPayMethods(new LinkedList<PayMethod>());
 		getPayMethods().add(EMPTY_PAYMETHOD);
