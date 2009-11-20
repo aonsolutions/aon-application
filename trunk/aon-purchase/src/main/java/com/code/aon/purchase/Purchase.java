7afe1c75cb69baa6a1d11964f19dd4e1f8e8fd6d
Gorka Irazu <girazu@esferalia.com>
2009-11-20 07:45:26 +0000

    Adaptacion del proyecto aon.purchase a AON-4

--- trunk/aon-purchase/src/main/java/com/code/aon/purchase/Purchase.java
+++ trunk/aon-purchase/src/main/java/com/code/aon/purchase/Purchase.java
@@ -1,70 +1,132 @@
 package com.code.aon.purchase;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.annotations.Type;
 
+import com.code.aon.common.BeanManager;
 import com.code.aon.common.IHeaderObject;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.enumeration.SecurityLevel;
 import com.code.aon.company.WorkPlace;
+import com.code.aon.config.Bank;
+import com.code.aon.config.BankAccount;
+import com.code.aon.config.IBankAccountContainer;
+import com.code.aon.config.IPayMethod;
 import com.code.aon.config.PayMethod;
+import com.code.aon.config.Scope;
+import com.code.aon.product.strategy.ICalculableContainer;
 import com.code.aon.product.util.DiscountExpression;
+import com.code.aon.purchase.dao.IPurchaseAlias;
 import com.code.aon.purchase.enumeration.PurchaseDocumentType;
 import com.code.aon.purchase.enumeration.PurchaseStatus;
+import com.code.aon.ql.Criteria;
+import com.code.aon.registry.RegistryAddress;
 import com.code.aon.supplier.Supplier;
 
 /**
- * Transfer Object that represents a purchase.
- * 
- * @author Joseba Urkiri
+ * Transfer Object that represents a Purchase.
  */
 @Entity
 @Table(name="purchase")
-public class Purchase implements ITransferObject, IHeaderObject{
+public class Purchase implements ITransferObject, IHeaderObject, ICalculableContainer, IBankAccountContainer, IPayMethod {
 	
 	private static final long serialVersionUID = -7426746100676646853L;
 
+	/** The Constant LOGGER. */
+	private static final Logger LOGGER = Logger.getLogger(Purchase.class.getName());
+	
+	/**
+	 * The Constructor. Sets TODAY to issueDate
+	 */
+	public Purchase() {
+		this.issueDate = new Date();
+	}
+	
 	/** The id. */
 	private Integer id;
 	
-	/** Purchase's serie. */
-	private String series;
+    /** The series. */
+    private String series;
 
-    /** Purchase's number. */
+    /** The number. */
     private int number;
-	
-	/** Purchase's supplier. */
+
+    /** The supplier. */
 	private Supplier supplier;
 	
-	/** The issue date. */
-	private Date issueDate;
-	
-	/** The pay method. */
-	private PayMethod payMethod;
+    /** The address. */
+    private RegistryAddress registryAddress;
 
-	/** The security level. */
-	private SecurityLevel securityLevel;
-	
-	/** The status. */
-	private PurchaseStatus status;
-	
-	/** The discount expression that will be applied. */
-	private DiscountExpression discountExpression;
-	
-	/** The document type. */
-	private PurchaseDocumentType documentType;
+    /** The discount expression. */
+    private DiscountExpression discountExpression;
+    
+    /** The issue date. */
+    private Date issueDate;
 
+    /** The pay method. */
+    private PayMethod payMethod;
+    
+    /** The security level. */
+    private SecurityLevel securityLevel;
+
+    /** The status. */
+    private PurchaseStatus status;
+    
+    /** The document type. */
+    private PurchaseDocumentType documentType;
+    
+    /** The workplace. */
 	private WorkPlace workPlace;
+
+    /** The scope. */
+	private Scope scope;
+
+    /** The number of payments. */
+    private int numberOfPayments;
+
+    /** The days to first payment. */
+    private int daysToFirstPayment;
+
+    /** The days between payments. */
+    private int daysBetweenPayments;
+
+    /** The payment days. */
+    private String paymentDays;
+
+    private int[] paymentDaysArray;
+
+	/** The bank. */
+	private Bank bank;
 	
+	/** The bank account. */
+	private BankAccount bankAccount;
+	
+	/** The detail of this purchase. */
+	private Set<PurchaseDetail> lines = new HashSet<PurchaseDetail>();
+
 	/**
 	 * Gets the id.
 	 * 
@@ -72,8 +134,8 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	 */
 	@Id
 	@GeneratedValue
-	@Column(nullable=false)
-	public Integer getId() {
+	@Column(nullable = false)
+    public Integer getId() {
 		return id;
 	}
 
@@ -85,13 +147,13 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	/**
 	 * Gets the serie.
 	 * 
 	 * @return the serie
 	 */
-	@Column(name="series", nullable=false, length=5)
+	@Column(length=5)
 	public String getSeries() {
 		return series;
 	}
@@ -104,13 +166,13 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	public void setSeries(String series) {
 		this.series = series;
 	}
-	
+
 	/**
 	 * Gets the number.
 	 * 
 	 * @return the number
 	 */
-	@Column(name="number", nullable=false)
+	@Column(nullable = false)
 	public int getNumber() {
 		return number;
 	}
@@ -124,13 +186,22 @@ public class Purchase implements ITransferObject, IHeaderObject{
 		this.number = number;
 	}
 
+    @Transient
+    public String getReferenceCode() {
+    	String referenceCode = "" + getNumber();
+		if (!StringUtils.isEmpty(getSeries())) {
+			referenceCode = getSeries() + "/" + referenceCode;
+		}
+    	return referenceCode;
+    }
+
 	/**
 	 * Gets the supplier.
 	 * 
 	 * @return the supplier
 	 */
 	@ManyToOne
-	@JoinColumn( name="supplier", nullable = false)
+	@JoinColumn( name="supplier", nullable = false )
 	public Supplier getSupplier() {
 		return supplier;
 	}
@@ -142,6 +213,46 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	 */
 	public void setSupplier(Supplier supplier) {
 		this.supplier = supplier;
+	}
+
+	/**
+	 * Gets the address.
+	 * 
+	 * @return the address
+	 */
+	@ManyToOne
+	@JoinColumn( name="address" )
+	public RegistryAddress getRegistryAddress() {
+		return registryAddress;
+	}
+
+	/**
+	 * Sets the address.
+	 * 
+	 * @param address the address
+	 */
+	public void setRegistryAddress(RegistryAddress registryAddress) {
+		this.registryAddress = registryAddress;
+	}
+
+	/**
+	 * Gets the discount expression.
+	 * 
+	 * @return the discount expression
+	 */
+	@Column(name="discount_expr")
+	@Type(type="com.code.aon.product.util.DiscountExpressionUserType")
+	public DiscountExpression getDiscountExpression() {
+		return discountExpression;
+	}
+
+	/**
+	 * Sets the discount expression.
+	 * 
+	 * @param discountExpression the discount expression
+	 */
+	public void setDiscountExpression(DiscountExpression discountExpression) {
+		this.discountExpression = discountExpression;
 	}
 
 	/**
@@ -169,7 +280,7 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	 * @return the pay method
 	 */
 	@ManyToOne
-	@JoinColumn( name="pay_method",updatable=false )
+	@JoinColumn( name="pay_method" )
 	public PayMethod getPayMethod() {
 		return payMethod;
 	}
@@ -207,7 +318,6 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	 * 
 	 * @return the status
 	 */
-	@Column(name="status")
 	public PurchaseStatus getStatus() {
 		return status;
 	}
@@ -219,26 +329,6 @@ public class Purchase implements ITransferObject, IHeaderObject{
 	 */
 	public void setStatus(PurchaseStatus status) {
 		this.status = status;
-	}
-
-	/**
-	 * Gets the discount expression.
-	 * 
-	 * @return the discount expression
-	 */
-	@Column(name="discount_expression")
-	@Type(type="com.code.aon.product.util.DiscountExpressionUserType")
-	public DiscountExpression getDiscountExpression() {
-		return discountExpression;
-	}
-
-	/**
-	 * Sets the discount expression.
-	 * 
-	 * @param discountExpression the discount expression
-	 */
-	public void setDiscountExpression(DiscountExpression discountExpression) {
-		this.discountExpression = discountExpression;
 	}
 
 	/**
@@ -260,13 +350,255 @@ public class Purchase implements ITransferObject, IHeaderObject{
 		this.documentType = documentType;
 	}
 
+	/**
+	 * Gets the workplace.
+	 * 
+	 * @return the workplace
+	 */
     @ManyToOne
     @JoinColumn(name="workplace", nullable = false)
 	public WorkPlace getWorkPlace() {
 		return workPlace;
 	}
 
+	/**
+	 * Sets the workplace.
+	 * 
+	 * @param workplace the workplace
+	 */
 	public void setWorkPlace(WorkPlace workPlace) {
 		this.workPlace = workPlace;
 	}
+
+	/**
+	 * Gets the scope.
+	 * 
+	 * @return the scope
+	 */
+    @ManyToOne
+    @JoinColumn(name="scope", nullable = false)
+	public Scope getScope() {
+		return scope;
+	}
+
+	/**
+	 * Sets the scope.
+	 * 
+	 * @param scope the scope
+	 */
+	public void setScope(Scope scope) {
+		this.scope = scope;
+	}
+	
+    /**
+     * Gets the number of payments.
+     * 
+     * @return the number of payments
+     */
+    @Column(name = "number_of_pymnts")
+    public int getNumberOfPayments() {
+        return numberOfPayments;
+    }
+
+    /**
+     * Sets the number of payments.
+     * 
+     * @param numberOfPayments the number of payments
+     */
+    public void setNumberOfPayments(int numberOfPayments) {
+        this.numberOfPayments = numberOfPayments;
+    }
+    
+    /**
+     * Gets the days to first payment.
+     * 
+     * @return the days to first payment
+     */
+    @Column(name = "days_to_first_pymnt")
+    public int getDaysToFirstPayment() {
+        return daysToFirstPayment;
+    }
+
+    /**
+     * Sets the days to first payment.
+     * 
+     * @param daysToFirstPayment the days to first payment
+     */
+    public void setDaysToFirstPayment(int daysToFirstPayment) {
+        this.daysToFirstPayment = daysToFirstPayment;
+    }
+
+    /**
+     * Gets the days between payments.
+     * 
+     * @return the days between payments
+     */
+    @Column(name = "days_between_pymnts")
+    public int getDaysBetweenPayments() {
+        return daysBetweenPayments;
+    }
+
+    /**
+     * Sets the days between payments.
+     * 
+     * @param daysBetweenPayment the days between payments
+     */
+    public void setDaysBetweenPayments(int daysBetweenPayment) {
+        this.daysBetweenPayments = daysBetweenPayment;
+    }
+
+    /**
+     * Gets the payment days.
+     * 
+     * @return the payment days
+     */
+    @Column(name="pymnt_days", length=8)
+    public String getPaymentDays() {
+        return paymentDays;
+    }
+    
+    /** The DELIM. */
+    private final String DELIM = " ";
+    
+    /**
+     * Sets the payment days.
+     * 
+     * @param paymentDays the payment days
+     */
+    public void setPaymentDays(String paymentDays) {
+        this.paymentDays = paymentDays;
+        StringTokenizer strTknzr = new StringTokenizer(this.paymentDays,DELIM);
+    	int[] values = new int[strTknzr.countTokens()];
+    	for (int i = 0; i < values.length; i++){
+    		values[i] = Integer.parseInt(strTknzr.nextToken());
+    	}    	
+        this.paymentDaysArray = values;
+    }
+
+    @Transient
+    public int[] getPaymentDaysArray() {
+    	return paymentDaysArray;
+    }
+
+	/**
+	 * Gets the bank.
+	 * 
+	 * @return the bank
+	 */
+	@ManyToOne
+    @JoinColumn(name="bank")
+	public Bank getBank() {
+		return bank;
+	}
+
+	/**
+	 * Sets the bank.
+	 * 
+	 * @param bank the bank
+	 */
+	public void setBank(Bank bank) {
+		this.bank = bank;
+	}
+
+	/**
+	 * Gets the bank account.
+	 * 
+	 * @return the bank account
+	 */
+	@Column(name="bank_account", length=30)
+	@Type(type="com.code.aon.config.hibernate.BankAccountType")
+	public BankAccount getBankAccount() {
+		return bankAccount;
+	}
+
+	/**
+	 * Sets the bank account.
+	 * 
+	 * @param bankAccount the bank account
+	 */
+	public void setBankAccount(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	/**
+	 * Gets the lines.
+	 * 
+	 * @return the lines
+	 */
+	@OneToMany(mappedBy = "purchase", cascade={CascadeType.REMOVE})
+	@OrderBy("line")
+	public Set<PurchaseDetail> getLines() {
+		return this.lines;
+	}
+
+	/**
+	 * Sets the lines.
+	 * 
+	 * @param lines the lines
+	 */
+	public void setLines( Set<PurchaseDetail> lines ) {
+		this.lines = lines;
+	}
+
+	/**
+	 * Gets the date. Necessary to implement <code>ICalculableContainer</code>
+	 * 
+	 * @return the date
+	 */
+	@Transient
+	public Date getDate() {
+		return issueDate;
+	}
+
+	/**
+	 * Gets the payMethod. Necessary to implement <code>IPayMethod</code>
+	 * 
+	 * @return the payMethod
+	 */
+	@Transient
+	public PayMethod getPayment() {
+		return payMethod;
+	}
+
+	/**
+	 * Gets the detail list. Used in the reports
+	 * 
+	 * @return the detail list
+	 */
+	@Transient
+	@SuppressWarnings("unchecked")
+	public List getDetailList() {
+		try {
+			IManagerBean purchaseDetailBean = BeanManager.getManagerBean(PurchaseDetail.class);
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(purchaseDetailBean.getFieldName(IPurchaseAlias.PURCHASE_DETAIL_PURCHASE_ID), getId());
+			return purchaseDetailBean.getList(criteria);
+		} catch (ManagerBeanException e) {
+			LOGGER.log(Level.SEVERE, "Error obtaining purchaseDetail list", e);
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+    		return super.equals(obj);
+		}
+		if (obj instanceof Purchase) {
+			Purchase p = (Purchase) obj;
+			if (p.getId() == null && id == null) {
+				return super.equals(obj);	
+			}
+			if (ObjectUtils.equals(getId(), p.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+    public int hashCode() {
+        return id != null ? this.getClass().hashCode() + id.hashCode() : super.hashCode();
+    }
+
 }
