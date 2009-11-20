1a28831927dee59278fc32637d607b4a1d0b34b8
Gorka Irazu <girazu@esferalia.com>
2009-11-20 09:33:11 +0000

    Añadido punteo de albaranes.

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/PurchaseInvoiceController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/PurchaseInvoiceController.java
@@ -1,32 +1,52 @@
 package com.code.aon.ui.finance.controller;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.faces.event.ActionEvent;
+
+import com.code.aon.common.BeanManager;
 import com.code.aon.common.IAttachment;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.enumeration.MimeType;
 import com.code.aon.finance.Invoice;
+import com.code.aon.finance.InvoiceDetail;
 import com.code.aon.finance.dao.IFinanceAlias;
+import com.code.aon.finance.enumeration.InvoiceSource;
+import com.code.aon.finance.invoicing.InvoicingException;
+import com.code.aon.finance.invoicing.ProgressionInvoicingFeedBack;
+import com.code.aon.finance.invoicing.engine.IInvoicingEngine;
+import com.code.aon.finance.invoicing.engine.InvoicingEngineFactory;
+import com.code.aon.finance.invoicing.engine.income.IncomeInvoicingDAO;
+import com.code.aon.finance.invoicing.engine.income.IncomeInvoicingEngine;
 import com.code.aon.ql.Criteria;
 import com.code.aon.supplier.Supplier;
 import com.code.aon.ui.common.components.LookupChangeEvent;
 import com.code.aon.ui.finance.IFinanceMessages;
+import com.code.aon.ui.form.FormUtil;
+import com.code.aon.ui.form.IController;
 import com.code.aon.ui.registry.util.RegistryValidationManager;
 import com.code.aon.ui.sign.controller.SignerController;
 import com.code.aon.ui.supplier.util.SupplierValidationManager;
 import com.code.aon.ui.util.AonUtil;
+import com.code.aon.warehouse.Income;
+import com.code.aon.warehouse.IncomeDetail;
+import com.code.aon.warehouse.bridge.IncomeTransferManager;
+import com.code.aon.warehouse.dao.IWarehouseAlias;
+import com.code.aon.warehouse.enumeration.IncomeStatus;
 
 public class PurchaseInvoiceController extends InvoiceController implements IFinanceConstants, IFinanceMessages {
 	
 	private static final Logger LOGGER = Logger.getLogger(PurchaseInvoiceController.class.getName());
 	
 	private RegistryValidationManager vm;
-	//private DeliveryTransferManager deliveryTransferManager;
-	//private boolean showDeliveryTransferWindow;
+	private IncomeTransferManager incomeTransferManager;
+	private boolean showIncomeTransferWindow;
 
 	public PurchaseInvoiceController() {
 		setInvoiceAddressControllerName(PURCHASE_INVOICE_ADDRESS_CONTROLLER_NAME);
@@ -58,102 +78,102 @@ public class PurchaseInvoiceController extends InvoiceController implements IFin
 		return getRegistryValidationManager().isBlocked(supplier);
 	}
 
-	/*public DeliveryTransferManager getDeliveryTransferManager() {
-		if (deliveryTransferManager == null) {
-			deliveryTransferManager = new DeliveryTransferManager(); 
+	public IncomeTransferManager getIncomeTransferManager() {
+		if (incomeTransferManager == null) {
+			incomeTransferManager = new IncomeTransferManager(); 
 		}
-		return deliveryTransferManager;
+		return incomeTransferManager;
 	}
 
-	public void setDeliveryTransferManager(DeliveryTransferManager deliveryTransferManager) {
-		this.deliveryTransferManager = deliveryTransferManager;
+	public void setIncomeTransferManager(IncomeTransferManager incomeTransferManager) {
+		this.incomeTransferManager = incomeTransferManager;
 	}
 
-	public boolean isShowDeliveryTransferWindow() {
-		return showDeliveryTransferWindow;
+	public boolean isShowIncomeTransferWindow() {
+		return showIncomeTransferWindow;
 	}
 
-	public void setShowDeliveryTransferWindow(boolean value) {
-		this.showDeliveryTransferWindow = value;
+	public void setShowIncomeTransferWindow(boolean value) {
+		this.showIncomeTransferWindow = value;
 	}
 	
-	public void onDeliveryTransferShow(ActionEvent event) throws ManagerBeanException {
-		List<ITransferObject> invoicedDeliveryList = new LinkedList<ITransferObject>();
-		IManagerBean deliveryDetailBean = BeanManager.getManagerBean(DeliveryDetail.class);
+	public void onIncomeTransferShow(ActionEvent event) throws ManagerBeanException {
+		List<ITransferObject> invoicedIncomeList = new LinkedList<ITransferObject>();
+		IManagerBean incomeDetailBean = BeanManager.getManagerBean(IncomeDetail.class);
 		IManagerBean invoiceDetailBean = BeanManager.getManagerBean(InvoiceDetail.class);
 		Criteria criteria = new Criteria();
 		criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_INVOICE_ID), getInvoice().getId());
-		criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_SOURCE), InvoiceSource.DELIVERY);
+		criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_SOURCE), InvoiceSource.INCOME);
 		criteria.addOrder(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_LINE));
 		Iterator<?> iterator = invoiceDetailBean.getList(criteria).iterator();
 		while (iterator.hasNext()) {
 			InvoiceDetail invoiceDetail = (InvoiceDetail)iterator.next();
-			DeliveryDetail deliveryDetail = (DeliveryDetail)deliveryDetailBean.get(invoiceDetail.getSourceId());
-			if (!invoicedDeliveryList.contains(deliveryDetail.getDelivery())) {
-				invoicedDeliveryList.add(deliveryDetail.getDelivery());
-				getDeliveryTransferManager().setDeliveryRowChecked(deliveryDetail.getDelivery(), true);
+			IncomeDetail incomeDetail = (IncomeDetail)incomeDetailBean.get(invoiceDetail.getSourceId());
+			if (!invoicedIncomeList.contains(incomeDetail.getIncome())) {
+				invoicedIncomeList.add(incomeDetail.getIncome());
+				getIncomeTransferManager().setIncomeRowChecked(incomeDetail.getIncome(), true);
 			}
 		}
 
-		getDeliveryTransferManager().setInvoicedDeliveryList(invoicedDeliveryList);
+		getIncomeTransferManager().setInvoicedIncomeList(invoicedIncomeList);
 
-		List<ITransferObject> deliveryList = new LinkedList<ITransferObject>();
-		deliveryList.addAll(invoicedDeliveryList);
-		IManagerBean deliveryBean = BeanManager.getManagerBean(Delivery.class);
+		List<ITransferObject> incomeList = new LinkedList<ITransferObject>();
+		incomeList.addAll(invoicedIncomeList);
+		IManagerBean incomeBean = BeanManager.getManagerBean(Income.class);
 		criteria = new Criteria();
-		criteria.addEqualExpression(deliveryBean.getFieldName(IWarehouseAlias.DELIVERY_CUSTOMER_ID), getInvoice().getRegistry().getId());
-		criteria.addEqualExpression(deliveryBean.getFieldName(IWarehouseAlias.DELIVERY_STATUS), DeliveryStatus.PENDING);
-		criteria.addEqualExpression(deliveryBean.getFieldName(IWarehouseAlias.DELIVERY_SECURITY_LEVEL), getInvoice().getSecurityLevel());
-		criteria.addOrder(deliveryBean.getFieldName(IWarehouseAlias.DELIVERY_ISSUE_TIME));
-		criteria.addOrder(deliveryBean.getFieldName(IWarehouseAlias.DELIVERY_SERIES));
-		criteria.addOrder(deliveryBean.getFieldName(IWarehouseAlias.DELIVERY_NUMBER));
-		deliveryList.addAll(deliveryBean.getList(criteria));
+		criteria.addEqualExpression(incomeBean.getFieldName(IWarehouseAlias.INCOME_SUPPLIER_ID), getInvoice().getRegistry().getId());
+		criteria.addEqualExpression(incomeBean.getFieldName(IWarehouseAlias.INCOME_STATUS), IncomeStatus.PENDING);
+		criteria.addEqualExpression(incomeBean.getFieldName(IWarehouseAlias.INCOME_SECURITY_LEVEL), getInvoice().getSecurityLevel());
+		criteria.addOrder(incomeBean.getFieldName(IWarehouseAlias.INCOME_ISSUE_TIME));
+		criteria.addOrder(incomeBean.getFieldName(IWarehouseAlias.INCOME_SERIES));
+		criteria.addOrder(incomeBean.getFieldName(IWarehouseAlias.INCOME_NUMBER));
+		incomeList.addAll(incomeBean.getList(criteria));
 
-		getDeliveryTransferManager().setDeliveryList(deliveryList);
+		getIncomeTransferManager().setIncomeList(incomeList);
 	}
 
-	public void onDeliveryTransfer(ActionEvent event) throws ManagerBeanException {
-		Iterator<ITransferObject> iterator = getDeliveryTransferManager().getInvoicedDeliveryList().iterator();
+	public void onIncomeTransfer(ActionEvent event) throws ManagerBeanException {
+		Iterator<ITransferObject> iterator = getIncomeTransferManager().getInvoicedIncomeList().iterator();
 		while (iterator.hasNext()) {
-			Delivery delivery = (Delivery)iterator.next();
-			if (!getDeliveryTransferManager().getCheckedDelivery().contains(delivery)) {
-				removeInvoicedDelivery(delivery);
+			Income income = (Income)iterator.next();
+			if (!getIncomeTransferManager().getCheckedIncome().contains(income)) {
+				removeInvoicedIncome(income);
 			}
-			getDeliveryTransferManager().getCheckedDelivery().remove(delivery);
+			getIncomeTransferManager().getCheckedIncome().remove(income);
 		}
 
-		InvoicingEngineFactory.register(InvoicingEngineFactory.CUSTOMER_FEE_ENGINE_KEY, new DeliveryInvoicingEngine());
+		InvoicingEngineFactory.register(InvoicingEngineFactory.INCOME_ENGINE_KEY, new IncomeInvoicingEngine());
 		try {
-			IInvoicingEngine engine = InvoicingEngineFactory.getInvoicingEngine(InvoicingEngineFactory.CUSTOMER_FEE_ENGINE_KEY);
-			engine.setInvoicingDAO(new DeliveryInvoicingDAO());
+			IInvoicingEngine engine = InvoicingEngineFactory.getInvoicingEngine(InvoicingEngineFactory.INCOME_ENGINE_KEY);
+			engine.setInvoicingDAO(new IncomeInvoicingDAO());
 			engine.setInvoicingFeedBack(new ProgressionInvoicingFeedBack());
-			((DeliveryInvoicingEngine)engine).invoiceDeliveryList(getInvoice(), getDeliveryTransferManager().getCheckedDelivery());
+			((IncomeInvoicingEngine)engine).invoiceIncomeList(getInvoice(), getIncomeTransferManager().getCheckedIncome());
 		} catch (InvoicingException e) {
 			throw new ManagerBeanException(e.getMessage(), e);
 		}
 
-		IController detailController = FormUtil.getController(IFinanceConstants.SALE_INVOICE_DETAIL_CONTROLLER_NAME);
+		IController detailController = FormUtil.getController(IFinanceConstants.PURCHASE_INVOICE_DETAIL_CONTROLLER_NAME);
 		detailController.onSearch(null);
 	}
 
-	private void removeInvoicedDelivery(Delivery delivery) throws ManagerBeanException {
+	private void removeInvoicedIncome(Income income) throws ManagerBeanException {
 		IManagerBean invoiceDetailBean = BeanManager.getManagerBean(InvoiceDetail.class);
-		IManagerBean deliveryDetailBean = BeanManager.getManagerBean(DeliveryDetail.class);
+		IManagerBean incomeDetailBean = BeanManager.getManagerBean(IncomeDetail.class);
 		Criteria criteria = new Criteria();
-		criteria.addEqualExpression(deliveryDetailBean.getFieldName(IWarehouseAlias.DELIVERY_DETAIL_DELIVERY_ID), delivery.getId());
-		Iterator<?> iterator = deliveryDetailBean.getList(criteria).iterator();
+		criteria.addEqualExpression(incomeDetailBean.getFieldName(IWarehouseAlias.INCOME_DETAIL_INCOME_ID), income.getId());
+		Iterator<?> iterator = incomeDetailBean.getList(criteria).iterator();
 		while (iterator.hasNext()) {
-			DeliveryDetail deliveryDetail = (DeliveryDetail)iterator.next();
+			IncomeDetail incomeDetail = (IncomeDetail)iterator.next();
 			criteria = new Criteria();
 			criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_INVOICE_ID), getInvoice().getId());
-			criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_SOURCE), InvoiceSource.DELIVERY);
-			criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_SOURCE_ID), deliveryDetail.getId());
+			criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_SOURCE), InvoiceSource.INCOME);
+			criteria.addEqualExpression(invoiceDetailBean.getFieldName(IFinanceAlias.INVOICE_DETAIL_SOURCE_ID), incomeDetail.getId());
 			if (invoiceDetailBean.getList(criteria).iterator().hasNext()) {
 				InvoiceDetail invoiceDetail = (InvoiceDetail)invoiceDetailBean.getList(criteria).iterator().next();
 				invoiceDetailBean.remove(invoiceDetail);
 			}
 		}
-	}*/
+	}
 	
 	@Override
 	public IAttachment generateReportAttachment( ITransferObject to ) {
