194d8d5731546d79f3fc81b02df8a04c3aae05fa
Aimar Tellitu <atellitu@esferalia.com>
2009-11-20 08:31:53 +0000

    *** empty log message ***

--- trunk/aon-payroll/src/main/java/com/code/aon/payroll/cotizacion/Porcentaje.java
+++ trunk/aon-payroll/src/main/java/com/code/aon/payroll/cotizacion/Porcentaje.java
@@ -52,7 +52,7 @@ public class Porcentaje implements ITransferObject {
 		this.fecfin = fecfin;
 	}
 
-	@Column(name="pctemp", precision=3, scale=2)
+	@Column(name="pctemp", precision=8, scale=2)
 	public BigDecimal getPctemp() {
 		return this.pctemp;
 	}
@@ -61,7 +61,7 @@ public class Porcentaje implements ITransferObject {
 		this.pctemp = pctemp;
 	}
 
-	@Column(name="pcttra", precision=3, scale=2)
+	@Column(name="pcttra", precision=8, scale=2)
 	public BigDecimal getPcttra() {
 		return this.pcttra;
 	}
@@ -70,7 +70,7 @@ public class Porcentaje implements ITransferObject {
 		this.pcttra = pcttra;
 	}
 
-	@Column(name="pcttot", precision=3, scale=2)
+	@Column(name="pcttot", precision=8, scale=2)
 	public BigDecimal getPcttot() {
 		return this.pcttot;
 	}
