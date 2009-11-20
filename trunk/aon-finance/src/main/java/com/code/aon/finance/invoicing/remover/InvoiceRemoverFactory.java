5cc45c82d3511c2b4fde4c8a73da428ca5c27bac
Gorka Irazu <girazu@esferalia.com>
2009-11-20 08:15:22 +0000

    Registrados el directPurchase y el income invoiceDetailRemovers

--- trunk/aon-finance/src/main/java/com/code/aon/finance/invoicing/remover/InvoiceRemoverFactory.java
+++ trunk/aon-finance/src/main/java/com/code/aon/finance/invoicing/remover/InvoiceRemoverFactory.java
@@ -56,7 +56,9 @@ public class InvoiceRemoverFactory {
 	static {
 		try {
 			register(DirectSalesInvoiceDetailRemover.class);
+			register(DirectPurchaseInvoiceDetailRemover.class);
 			register(DeliveryInvoiceDetailRemover.class);
+			register(IncomeInvoiceDetailRemover.class);
 			register(FeeInvoiceDetailRemover.class);
 			register(AccountInvoiceDetailRemover.class);
 			register(DirectInvoiceDetailRemover.class);
