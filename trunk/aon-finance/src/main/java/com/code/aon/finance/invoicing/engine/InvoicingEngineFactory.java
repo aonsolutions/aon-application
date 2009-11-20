51b31c3bb04aea0397d864977a1eb372539257d4
Gorka Irazu <girazu@esferalia.com>
2009-11-20 08:14:12 +0000

    Añadidos keys para deliveryInvoicingEngine e incomeInvoicingEngine

--- trunk/aon-finance/src/main/java/com/code/aon/finance/invoicing/engine/InvoicingEngineFactory.java
+++ trunk/aon-finance/src/main/java/com/code/aon/finance/invoicing/engine/InvoicingEngineFactory.java
@@ -8,6 +8,8 @@ import com.code.aon.finance.invoicing.InvoicingException;
 public class InvoicingEngineFactory {
 
 	public static final String CUSTOMER_FEE_ENGINE_KEY = "customerFeeEngine";
+	public static final String DELIVERY_ENGINE_KEY = "deliveryEngine";
+	public static final String INCOME_ENGINE_KEY = "incomeEngine";
 	
 	private static InvoicingEngineFactory instance;
 	
