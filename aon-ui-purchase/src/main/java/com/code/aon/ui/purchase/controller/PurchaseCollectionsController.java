aa183c37d306daa9546cc859ee78b304867ba8c1
Gorka Irazu <girazu@esferalia.com>
2009-11-20 08:54:37 +0000

    Adaptacion del proyecto aon.ui.purchase a AON-4

--- aon-ui-purchase/src/main/java/com/code/aon/ui/purchase/controller/PurchaseCollectionsController.java
+++ aon-ui-purchase/src/main/java/com/code/aon/ui/purchase/controller/PurchaseCollectionsController.java
@@ -7,45 +7,44 @@ import java.util.Locale;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
-import com.code.aon.purchase.enumeration.PurchaseDetailStatus;
+import com.code.aon.purchase.enumeration.PurchaseDocumentType;
 import com.code.aon.purchase.enumeration.PurchaseStatus;
 
+
 /**
- * Controller used to get Collections related with clasess in <code>com.code.aon.purchase</code>
+ * Controller used to get Collections related with clasess in 
+ * <code>com.code.aon.purchase</code>
  * 
  * @author Consulting & Development. Joseba Urkiri - 21-dic-2005
  */
 public class PurchaseCollectionsController {
-	
-    /**
-     * Gets the purchase statuses.
-     * 
-     * @return the purchase statuses
-     */
-    public List<SelectItem> getPurchaseStatuses() {
-        Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-	    LinkedList<SelectItem> types = new LinkedList<SelectItem>();
-        for( PurchaseStatus type : PurchaseStatus.values() ) {
-            String name = type.getName(locale); 
-            SelectItem item = new SelectItem(type, name);
-            types.add( item );
-        }
-        return types;
-    }
-	
-    /**
-     * Gets the purchase detail statuses.
-     * 
-     * @return the purchase detail statuses
-     */
-    public List<SelectItem> getPurchaseDetailStatuses() {
-        Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-	    LinkedList<SelectItem> types = new LinkedList<SelectItem>();
-        for( PurchaseDetailStatus type : PurchaseDetailStatus.values() ) {
-            String name = type.getName(locale); 
-            SelectItem item = new SelectItem(type, name);
-            types.add( item );
-        }
-        return types;
-    }
+	List<SelectItem> purchaseStatuses;
+	List<SelectItem> documentTypes;
+
+	public List<SelectItem> getPurchaseStatuses() {
+		if (purchaseStatuses == null) {
+			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+			purchaseStatuses = new LinkedList<SelectItem>();
+			for (PurchaseStatus status : PurchaseStatus.values()) {
+				String name = status.getName(locale);
+				SelectItem item = new SelectItem(status, name);
+				purchaseStatuses.add(item);
+			}
+		}
+		return purchaseStatuses;
+	}
+
+	public List<SelectItem> getDocumentTypes() {
+		if (documentTypes == null) {
+			Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+			documentTypes = new LinkedList<SelectItem>();
+			for (PurchaseDocumentType type : PurchaseDocumentType.values()) {
+				String name = type.getName(locale);
+				SelectItem item = new SelectItem(type, name);
+				documentTypes.add(item);
+			}
+		}
+		return documentTypes;
+	}
+
 }
