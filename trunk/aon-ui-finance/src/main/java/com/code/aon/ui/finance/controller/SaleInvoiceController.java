4da08a273ab46ebaa8eb71782c5d40e32c113054
Gorka Irazu <girazu@esferalia.com>
2009-11-20 09:37:58 +0000

    DELIVERY_ENGINE_KEY en lugar de CUSTOMER_FEE_ENGINE_KEY

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/SaleInvoiceController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/SaleInvoiceController.java
@@ -184,9 +184,9 @@ public class SaleInvoiceController extends InvoiceController implements ISignatu
 			getDeliveryTransferManager().getCheckedDelivery().remove(delivery);
 		}
 
-		InvoicingEngineFactory.register(InvoicingEngineFactory.CUSTOMER_FEE_ENGINE_KEY, new DeliveryInvoicingEngine());
+		InvoicingEngineFactory.register(InvoicingEngineFactory.DELIVERY_ENGINE_KEY, new DeliveryInvoicingEngine());
 		try {
-			IInvoicingEngine engine = InvoicingEngineFactory.getInvoicingEngine(InvoicingEngineFactory.CUSTOMER_FEE_ENGINE_KEY);
+			IInvoicingEngine engine = InvoicingEngineFactory.getInvoicingEngine(InvoicingEngineFactory.DELIVERY_ENGINE_KEY);
 			engine.setInvoicingDAO(new DeliveryInvoicingDAO());
 			engine.setInvoicingFeedBack(new ProgressionInvoicingFeedBack());
 			((DeliveryInvoicingEngine)engine).invoiceDeliveryList(getInvoice(), getDeliveryTransferManager().getCheckedDelivery());
