47deebf6e398ed25cbf18e2f4f9444331c9cddd8
Aimar Tellitu <atellitu@esferalia.com>
2009-11-24 12:13:37 +0000

    Se ha añadido las busqueda por lineas de sales

--- aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesSearchListener.java
+++ aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesSearchListener.java
@@ -4,17 +4,27 @@ import org.apache.commons.lang.ArrayUtils;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.customer.Customer;
+import com.code.aon.product.Item;
+import com.code.aon.product.Product;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.sales.dao.ISalesAlias;
 import com.code.aon.sales.enumeration.SalesStatus;
-import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.ui.registry.controller.event.RegistrySearchListener;
 
-public class SalesSearchListener extends ControllerSearchListener {
+public class SalesSearchListener extends RegistrySearchListener {
+
+	private static final String REGISTRY_SEARCH_PREFFIX = "Sales_customer_registry_";
 
 	private Customer customer;
-
+	
 	private SalesStatus[] salesStatuses;
+	
+    private Item item;
+	
+	public String getPreffix() throws ManagerBeanException {
+		return REGISTRY_SEARCH_PREFFIX;
+	}	
 
 	public Customer getCustomer() {
 		return customer;
@@ -32,15 +42,27 @@ public class SalesSearchListener extends ControllerSearchListener {
 		this.salesStatuses = salesStatuses;
 	}
 	
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
 	@Override
 	protected void init() throws ManagerBeanException {
+		super.init();
 		setCustomer(new Customer());
 		SalesStatus[] defaultSalesStatus = {SalesStatus.PENDING};
 		setSalesStatuses(defaultSalesStatus);
+		setItem(new Item());
+		getItem().setProduct(new Product());		
 	}
 	
 	@Override
 	protected void completeCriteria() throws ManagerBeanException, ExpressionException {
+		super.completeCriteria();
 		Criteria criteria = getController().getCriteria();
 		if (getCustomer() != null && getCustomer().getId() != null) {
 			criteria.addEqualExpression(getController().getFieldName(ISalesAlias.SALES_CUSTOMER_ID), getCustomer().getId());			
@@ -49,5 +71,8 @@ public class SalesSearchListener extends ControllerSearchListener {
 			String status = getController().resolveAlias(ISalesAlias.SALES_STATUS);
 			addEnumToCriteria(criteria, status, getSalesStatuses());
 		}
+		if ((getItem() != null) && (getItem().getId() != null)) {
+			criteria.addEqualExpression("Sales.lines.item.id", getItem().getId());
+		}				
 	}	
 }
