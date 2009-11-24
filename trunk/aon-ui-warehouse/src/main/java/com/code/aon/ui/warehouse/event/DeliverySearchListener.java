3b985c87ac04e6d7633aa9373c91702a280c9995
Aimar Tellitu <atellitu@esferalia.com>
2009-11-24 14:49:34 +0000

    Se ha añadido las busqueda por lineas de delivery y income

--- trunk/aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/event/DeliverySearchListener.java
+++ trunk/aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/event/DeliverySearchListener.java
@@ -4,18 +4,28 @@ import org.apache.commons.lang.ArrayUtils;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.customer.Customer;
+import com.code.aon.product.Item;
+import com.code.aon.product.Product;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
-import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.ui.registry.controller.event.RegistrySearchListener;
 import com.code.aon.warehouse.dao.IWarehouseAlias;
 import com.code.aon.warehouse.enumeration.DeliveryStatus;
 
-public class DeliverySearchListener extends ControllerSearchListener {
+public class DeliverySearchListener extends RegistrySearchListener {
+	
+	private static final String REGISTRY_SEARCH_PREFFIX = "Delivery_customer_registry_";
 
 	private Customer customer;
 
 	private DeliveryStatus[] deliveryStatuses;
 
+    private Item item;
+	
+	public String getPreffix() throws ManagerBeanException {
+		return REGISTRY_SEARCH_PREFFIX;
+	}	
+	
 	public Customer getCustomer() {
 		return customer;
 	}
@@ -32,15 +42,28 @@ public class DeliverySearchListener extends ControllerSearchListener {
 		this.deliveryStatuses = deliveryStatuses;
 	}
 	
+	
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
 	@Override
 	protected void init() throws ManagerBeanException {
+		super.init();
 		setCustomer(new Customer());
 		DeliveryStatus[] defaultDeliveryStatus = {DeliveryStatus.PENDING};
 		setDeliveryStatuses(defaultDeliveryStatus);
+		setItem(new Item());
+		getItem().setProduct(new Product());		
 	}
 	
 	@Override
 	protected void completeCriteria() throws ManagerBeanException, ExpressionException {
+		super.completeCriteria();
 		Criteria criteria = getController().getCriteria();
 		if (getCustomer() != null && getCustomer().getId() != null) {
 			criteria.addEqualExpression(getController().getFieldName(IWarehouseAlias.DELIVERY_CUSTOMER_ID), getCustomer().getId());			
@@ -49,5 +72,8 @@ public class DeliverySearchListener extends ControllerSearchListener {
 			String status = getController().resolveAlias(IWarehouseAlias.DELIVERY_STATUS);
 			addEnumToCriteria(criteria, status, getDeliveryStatuses());
 		}
+		if ((getItem() != null) && (getItem().getId() != null)) {
+			criteria.addEqualExpression("Delivery.lines.item.id", getItem().getId());
+		}						
 	}	
 }
