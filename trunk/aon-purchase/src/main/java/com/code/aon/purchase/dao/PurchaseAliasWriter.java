2523e76afb3d2ab4a9009e8d7ed743ba8112060a
Euke Castellano <ecastellano@esferalia.com>
2009-11-24 13:02:44 +0000

    Nuevo ItemSupplier, refactor de POJOS (equals, hashCode y toString),

--- trunk/aon-purchase/src/main/java/com/code/aon/purchase/dao/PurchaseAliasWriter.java
+++ trunk/aon-purchase/src/main/java/com/code/aon/purchase/dao/PurchaseAliasWriter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import com.code.aon.common.dao.AliasWriter;
 import com.code.aon.common.dao.hibernate.HibernateUtil;
+import com.code.aon.purchase.ItemSupplier;
 import com.code.aon.purchase.Purchase;
 import com.code.aon.purchase.PurchaseDetail;
 
@@ -12,9 +13,11 @@ public class PurchaseAliasWriter {
 
 	public static void main(String[] args) throws IOException {
 		File file = new File("/AON-PROJECT/aon-purchase/src/main/java/com/code/aon/purchase/dao/IPurchaseAlias.java");
-        String[] classes = new String[2];
-        classes[0] = PurchaseDetail.class.getName();
-        classes[1] = Purchase.class.getName();
+		String[] classes = new String[] {
+	        ItemSupplier.class.getName(),
+	        PurchaseDetail.class.getName(),
+	        Purchase.class.getName()
+	    };
 		HibernateUtil.getSessionFactory(null);
 		AliasWriter writer = new AliasWriter("com.code.aon.purchase.dao");
 		writer.write(classes,file);
