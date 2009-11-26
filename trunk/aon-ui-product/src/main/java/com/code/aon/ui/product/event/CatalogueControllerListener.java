ef6475b56106710a51425d89e5582311ca694385
Euke Castellano <ecastellano@esferalia.com>
2009-11-26 12:24:12 +0000

    Se evita un NullPointer en el control de fecha al grabar.

--- trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/event/CatalogueControllerListener.java
+++ trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/event/CatalogueControllerListener.java
@@ -1,6 +1,7 @@
 package com.code.aon.ui.product.event;
 
-import java.util.logging.Logger;
+import java.util.Date;
+
 import com.code.aon.product.Catalogue;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
@@ -8,40 +9,30 @@ import com.code.aon.ui.form.event.ControllerListenerException;
 import com.code.aon.ui.product.controller.IItemConstants;
 import com.code.aon.ui.util.AonUtil;
 
-public class CatalogueControllerListener extends ControllerAdapter implements
-		IItemConstants {
-
-	private static final Logger LOGGER = Logger
-			.getLogger(CatalogueControllerListener.class.getName());
+public class CatalogueControllerListener extends ControllerAdapter implements IItemConstants {
 
 	@Override
-	public void beforeBeanAdded(ControllerEvent event)
-			throws ControllerListenerException {
-
-		if (((Catalogue) this.getController().getTo()).getEndDate().before(
-				((Catalogue) this.getController().getTo()).getStartDate())) {
-			
-			throw new ControllerListenerException(AonUtil.getMessage("productBundle",
-			"product_catalogue_dates_error"));
+	public void beforeBeanAdded(ControllerEvent event) throws ControllerListenerException {
+		Date endDate = ((Catalogue) this.getController().getTo()).getEndDate();
+		if (endDate != null) {
+			Date startDate = ((Catalogue) this.getController().getTo()).getStartDate();
+			if (endDate.before(startDate)) {
+				throw new ControllerListenerException(AonUtil.getMessage("productBundle",
+						"product_catalogue_dates_error"));
+			}
 		}
-		
-		
 	}
 
 	@Override
-	public void beforeBeanUpdated(ControllerEvent event)
-			throws ControllerListenerException {
-
+	public void beforeBeanUpdated(ControllerEvent event) throws ControllerListenerException {
 
 		if (((Catalogue) this.getController().getTo()).getEndDate().before(
 				((Catalogue) this.getController().getTo()).getStartDate())) {
-			
+
 			throw new ControllerListenerException(AonUtil.getMessage("productBundle",
-			"product_catalogue_dates_error"));
+					"product_catalogue_dates_error"));
 		}
 
 	}
 
-	
-	
 }
