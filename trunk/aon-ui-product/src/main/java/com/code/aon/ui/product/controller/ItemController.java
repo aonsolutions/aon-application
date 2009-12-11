08a26915e2091dd747b99fc3b2abeac7e9da4d7d
Gorka Irazu <girazu@esferalia.com>
2009-12-11 13:47:59 +0000

    Eliminada propiedad itemAlernative de Item

--- trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemController.java
+++ trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemController.java
@@ -3,7 +3,6 @@ package com.code.aon.ui.product.controller;
 import javax.faces.event.ValueChangeEvent;
 
 import com.code.aon.product.Item;
-import com.code.aon.ui.common.components.LookupChangeEvent;
 import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.product.util.ItemControllerUtil;
 
@@ -30,12 +29,4 @@ public class ItemController extends BasicController {
 		ItemControllerUtil.onSalesPriceChanged(event, (Item) getTo() );
 	}
 	
-	public void onAlternativeItemChanged(LookupChangeEvent event) {
-		Item item = (Item)getTo();
-		if (event.getNewValue() != null && !event.getNewValue().toString().equals("")) {
-			Item alternativeItem = (Item)event.getNewValue();
-			item.setAlternativeItem(alternativeItem);
-		}
-	}	
-
 }
