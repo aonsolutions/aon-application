7139492e797071d705ab91d61d7316df79f04ea7
Aimar Tellitu <atellitu@esferalia.com>
2009-12-11 14:13:29 +0000

    *** empty log message ***

--- aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/GenericPageController.java
+++ aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/GenericPageController.java
@@ -23,17 +23,7 @@ import com.code.aon.ui.cms.Constants;
 
 public class GenericPageController extends BasicI18nController implements Constants {
 
-	private int page;
-	
-	public int getPage() {
-		return page;
-	}
-
-	public void setPage(int page) {
-		this.page = page;
-	}
-
-	private boolean richTextEnabled = false;
+	private boolean richTextEnabled;
 
 	public boolean isRichTextEnabled() {
 		return richTextEnabled;
@@ -43,7 +33,6 @@ public class GenericPageController extends BasicI18nController implements Consta
 		this.richTextEnabled = richTextEnabled;
 	}
 
-	@SuppressWarnings("unused")
 	public void onSelect(ActionEvent event) {
 		super.onSelect(event);
 		loadCurrentLanguage();
@@ -77,8 +66,8 @@ public class GenericPageController extends BasicI18nController implements Consta
 		menus.add(item);
 		IManagerBean menuBean = BeanManager.getManagerBean(Menu.class);
 		Criteria criteria = new Criteria();
-		criteria.addExpression(menuBean.getFieldName(ICMSAlias.MENU_TYPE), "" + MenuType.SIDEBAR.ordinal());
-		List<ITransferObject> list = (List<ITransferObject>)menuBean.getList(criteria);
+		criteria.addEqualExpression(menuBean.getFieldName(ICMSAlias.MENU_TYPE), MenuType.SIDEBAR);
+		List<ITransferObject> list = menuBean.getList(criteria);
 		for (int i = 0; i < list.size(); i++) {
 			Menu menu = (Menu)list.get(i);
 			int id = menu.getId();
