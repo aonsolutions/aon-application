3e9f827d47871670b5b4519c7158324c5586dbbc
Euke Castellano <ecastellano@esferalia.com>
2009-12-15 15:48:21 +0000

    Se corrige el Bug de renumerar asientos.

--- aon-accounting/src/main/java/com/code/aon/accounting/util/AccountJournalManager.java
+++ aon-accounting/src/main/java/com/code/aon/accounting/util/AccountJournalManager.java
@@ -61,6 +61,7 @@ public class AccountJournalManager {
 		        int i = 0;
 		        int journal = 2;
 		        for (ITransferObject to : list ) {
+		        	boolean mustAdd = false;
 		        	AccountEntry entry = (AccountEntry) to;
 		        	if (opening && entry.getType() == AccountEntryType.OPENING ) {
 		        		entry.setJournal(1);
@@ -70,12 +71,15 @@ public class AccountJournalManager {
 		        		entry.setJournal(count);
 		        	} else {
 		        		entry.setJournal(journal);	
+		        		mustAdd = true;
 		        	}
 		        	entry.setDateDirty(false);
 			        bean.update(entry);    	
 		        	progressionBean.setProgressionCurrentValue((long) ( i * 100 / count));
 		        	i++;
-		        	journal++;
+		        	if (mustAdd) {
+		        		journal++;
+		        	}
 		        }
 				// FIN operaciones de la transaccion
 				HibernateUtil.getSession(sessionName).flush();
