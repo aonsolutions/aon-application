c0a95937ec7e0cd94aeca8389a04cab87e189acc
Aimar Tellitu <atellitu@esferalia.com>
2009-12-28 18:18:49 +0000

    *** empty log message ***

--- trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/AlbumCategoryController.java
+++ trunk/aon-web-cms/src/main/java/com/code/aon/ui/cms/controller/AlbumCategoryController.java
@@ -2,10 +2,14 @@ package com.code.aon.ui.cms.controller;
 
 import javax.faces.event.ActionEvent;
 
+import com.code.aon.cms.AlbumCategory;
 import com.code.aon.cms.AlbumCategoryDetail;
 import com.code.aon.cms.AlbumConfig;
+import com.code.aon.cms.enumeration.ModularPageOptionType;
+import com.code.aon.cms.enumeration.SidebarType;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ui.cms.Constants;
+import com.code.aon.ui.cms.util.ReferenceChecker;
 import com.code.aon.ui.form.FormUtil;
 import com.code.aon.ui.util.AonUtil;
 
@@ -28,4 +32,10 @@ public class AlbumCategoryController extends BasicI18nController implements ICMS
 		return ALBUM_FORM;
 	}
 
+	public boolean isUsed() throws ManagerBeanException {
+		Integer id = ((AlbumCategory) getTo()).getId();
+		return ReferenceChecker.isInModulaPage(id, ModularPageOptionType.ALBUM_CATEGORY) ||
+			ReferenceChecker.isInSideBar(id, SidebarType.ALBUM_CATEGORY);
+	}
+	
 }
