dad38c6060a3577b37a7e0850f28d23b6208d6b8
Euke Castellano <ecastellano@esferalia.com>
2010-01-14 14:40:07 +0000

    Chequeos de NIFS y DNIS.

--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryController.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryController.java
@@ -1,8 +1,15 @@
 package com.code.aon.ui.registry.controller;
 
+import java.util.List;
+
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Criteria;
 import com.code.aon.registry.IRegistry;
 import com.code.aon.registry.enumeration.RegistryType;
 import com.code.aon.ui.form.BasicController;
@@ -16,7 +23,7 @@ import com.code.aon.ui.webmail.controller.MessageController;
 public class RegistryController extends BasicController {
 
 	private String selectedTab;
-	
+
 	public String getSelectedTab() {
 		return selectedTab;
 	}
@@ -25,17 +32,38 @@ public class RegistryController extends BasicController {
 		this.selectedTab = selectedTab;
 	}
 
-    public boolean isNaturalType(){
-    	IRegistry iRegistry = (IRegistry)getTo();
-    	return iRegistry.getRegistry().getType().equals(RegistryType.NATURAL);
-    }
+	public boolean isNaturalType() {
+		IRegistry iRegistry = (IRegistry) getTo();
+		return iRegistry.getRegistry().getType().equals(RegistryType.NATURAL);
+	}
 
-    public void onSendEmail( ActionEvent event ) {
-		MessageController messageController = (MessageController) AonUtil.getRegisteredBean(WebMailConstants.BEAN_MESSAGE);
+	public void onSendEmail(ActionEvent event) {
+		MessageController messageController = (MessageController) AonUtil
+				.getRegisteredBean(WebMailConstants.BEAN_MESSAGE);
 		messageController.initNewMessage();
 		FacesContext context = FacesContext.getCurrentInstance();
 		Object email = context.getExternalContext().getRequestParameterMap().get("email");
-		messageController.setRecipientsTo( email.toString() );
+		messageController.setRecipientsTo(email.toString());
 	}
 
+	public void onChangeDocument(ActionEvent event) {
+		try {
+			IRegistry iRegistry = (IRegistry) getTo();
+			if (isNew()) {
+				String document = iRegistry.getRegistry().getDocument();
+				if (StringUtils.isNotEmpty(document)) {
+					Criteria criteria = new Criteria();
+					String alias = getManagerBean().getFieldName(
+							getPojoShortName() + "_registry_document");
+					criteria.addEqualExpression(alias, document);
+					List<ITransferObject> list = getManagerBean().getList(criteria);
+					if (list.size() > 0 ) {
+						AonUtil.addWarningMessage("Ya existe un registro para el NIF: " + document);
+					}
+				}
+			}
+		} catch (ManagerBeanException e) {
+			// DO
+		}
+	}
 }
