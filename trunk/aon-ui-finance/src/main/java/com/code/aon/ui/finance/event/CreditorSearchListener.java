82ebbe900a56c4eac15f2bcdfcfa4621b3ffa71b
Aimar Tellitu <atellitu@esferalia.com>
2010-01-14 15:07:01 +0000

    Se ha añadido la creacion rapida de acreedores

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/CreditorSearchListener.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/CreditorSearchListener.java
@@ -7,9 +7,9 @@ import com.code.aon.finance.dao.IFinanceAlias;
 import com.code.aon.finance.enumeration.CreditorStatus;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
-import com.code.aon.ui.registry.controller.event.RegistrySearchListener;
+import com.code.aon.ui.registry.controller.event.RegistryPayMethodSearchListener;
 
-public class CreditorSearchListener extends RegistrySearchListener {
+public class CreditorSearchListener extends RegistryPayMethodSearchListener {
 
 	private CreditorStatus[] creditorStatuses;
 	
