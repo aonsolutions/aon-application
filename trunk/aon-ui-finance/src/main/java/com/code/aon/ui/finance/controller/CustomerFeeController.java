33a1a7272c3e7ab9d8ca1a860c069b71cb39cda9
David Uriarte <duriarte@esferalia.com>
2010-01-22 12:43:26 +0000

    *** empty log message ***

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/CustomerFeeController.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/CustomerFeeController.java
@@ -1,11 +1,24 @@
 package com.code.aon.ui.finance.controller;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.config.Tariff;
 import com.code.aon.customer.Customer;
 import com.code.aon.finance.CustomerFee;
@@ -20,8 +33,39 @@ import com.code.aon.ui.form.LinesController;
 public class CustomerFeeController extends LinesController {
 
 	private static final String CUSTOMER_CONTROLLER_NAME = "customer";
-	
+	/** Message file base path. */
+   private static final String BASE_NAME = "com.code.aon.ui.registry.i18n.report";
+   /** Message key prefix. */
+   private static final String MSG_KEY_PREFIX = "aon_no_fee_customer_report";
+
 	private IPriceStrategy priceStrategy;
+	private DataModel noFeeCustomersModel;
+	private List<Customer> noFeeCustomersList;
+	
+	public String getReportTitle(){
+		Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX);
+	}
+
+	public DataModel getNoFeeCustomersModel() {
+		if (noFeeCustomersModel == null) {
+			noFeeCustomersModel = new ListDataModel(getNoFeeCustomersList());
+		}
+		return noFeeCustomersModel;
+	}
+
+	public void setNoFeeCustomersModel(DataModel noFeeCustomersModel) {
+		this.noFeeCustomersModel = noFeeCustomersModel;
+	}
+
+	public List<Customer> getNoFeeCustomersList() {
+		return noFeeCustomersList;
+	}
+
+	public void setNoFeeCustomersList(List<Customer> noFeeCustomersList) {
+		this.noFeeCustomersList = noFeeCustomersList;
+	}
 
 	public IPriceStrategy getPriceStrategy(){
 		if(priceStrategy == null){
@@ -66,6 +110,21 @@ public class CustomerFeeController extends LinesController {
 			}
 			fee.setPrice(price);
 		}
+	}
+	
+	public void onNoFeeCustomers(ActionEvent event) throws ManagerBeanException {
+
+		Calendar calendar = new GregorianCalendar();
+		String select = "select distinct(customer) "
+			+ "from Customer as customer "
+			+ "where customer.status= 0 AND ( "
+			+ "customer.id NOT IN (select customerFee.customer.id from CustomerFee as customerFee) "
+			+ "AND "
+			+ "customer.id NOT IN (select customerFee.customer.id from CustomerFee as customerFee where finalDate < '"
+			+ calendar.getTime() + "')))";	
+		Session session = HibernateUtil.getSession(HibernateUtil.getSessionFactoryName());
+		Query query = session.createQuery(select);
+		noFeeCustomersList = query.list();
 	}
 
 }
