54041a0c7b6e4dc5b083732fbb812d3a3bd76b01
David Uriarte <duriarte@esferalia.com>
2010-01-27 11:21:46 +0000

    *** empty log message ***

--- trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemLookup.java
+++ trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemLookup.java
@@ -2,21 +2,36 @@ package com.code.aon.ui.product.controller;
 
 import javax.faces.event.ValueChangeEvent;
 
+import com.code.aon.common.util.CommonUtil;
 import com.code.aon.faces.controller.RichLookupBean;
 import com.code.aon.product.Item;
-import com.code.aon.ui.product.util.ItemControllerUtil;
+import com.code.aon.product.Product;
 
 public class ItemLookup extends RichLookupBean {
 
 	public double getSalesPrice() {
-		return ItemControllerUtil.getSalesPrice( (Item) getTo() );
+		return ((Item) getTo()).getSalesPrice();
 	}
 
 	public void setSalesPrice(double salesPrice) {
 	}
 
 	public void onSalesPriceChanged(ValueChangeEvent event) {
-		ItemControllerUtil.onSalesPriceChanged(event, (Item) getTo() );
+		double price = 0;
+		if (event.getNewValue() != null
+				&& !event.getNewValue().toString().equals("")) {
+			double salesPrice = new Double(event.getNewValue().toString())
+					.doubleValue();
+			Product product = ((Item)this.getTo()).getProduct();
+			double vatPercent = (product.getVat() != null) ? product.getVat()
+					.getPercentage() : 0;
+			double retentionPercent = (product.getRetention() != null) ? product
+					.getRetention().getPercentage()
+					: 0;
+			price = CommonUtil.round(salesPrice
+					/ (1 + vatPercent / 100 - retentionPercent / 100));
+		}
+		((Item)this.getTo()).setPrice(price);
 	}
 	
 }
