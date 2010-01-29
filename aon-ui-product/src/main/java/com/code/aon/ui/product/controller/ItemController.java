c62d45b9a9231c7e06549a2e31c5a107d96d22f5
Gorka Irazu <girazu@esferalia.com>
2010-01-29 11:04:07 +0000

    Quitado formateo ilegible.

--- aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemController.java
+++ aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemController.java
@@ -21,18 +21,12 @@ public class ItemController extends BasicController {
 
 	public void onSalesPriceChanged(ValueChangeEvent event) {
 		double price = 0;
-		if (event.getNewValue() != null
-				&& !event.getNewValue().toString().equals("")) {
-			double salesPrice = new Double(event.getNewValue().toString())
-					.doubleValue();
+		if (event.getNewValue() != null	&& !event.getNewValue().toString().equals("")) {
+			double salesPrice = new Double(event.getNewValue().toString()).doubleValue();
 			Product product = ((Item)this.getTo()).getProduct();
-			double vatPercent = (product.getVat() != null) ? product.getVat()
-					.getPercentage() : 0;
-			double retentionPercent = (product.getRetention() != null) ? product
-					.getRetention().getPercentage()
-					: 0;
-			price = CommonUtil.round(salesPrice
-					/ (1 + vatPercent / 100 - retentionPercent / 100));
+			double vatPercent = (product.getVat() != null) ? product.getVat().getPercentage() : 0;
+			double retentionPercent = (product.getRetention() != null) ? product.getRetention().getPercentage()	: 0;
+			price = CommonUtil.round(salesPrice	/ (1 + vatPercent / 100 - retentionPercent / 100));
 		}
 		((Item)this.getTo()).setPrice(price);
 	}
