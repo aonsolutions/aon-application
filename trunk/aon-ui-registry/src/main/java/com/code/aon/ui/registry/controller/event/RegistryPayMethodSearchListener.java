49d84e48260277c43288253dd53ad279be9d3f61
Gorka Irazu <girazu@esferalia.com>
2010-01-29 10:55:25 +0000

    Añadida la funcionalidad para las busquedas por forma de pago

--- trunk/aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/RegistryPayMethodSearchListener.java
+++ trunk/aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/RegistryPayMethodSearchListener.java
@@ -1,9 +1,18 @@
 package com.code.aon.ui.registry.controller.event;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.config.PayMethod;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ui.form.LinesController;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -18,10 +27,72 @@ public class RegistryPayMethodSearchListener extends RegistrySearchListener {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RegistryPayMethodSearchListener.class);
 	
+	private static final PayMethod EMPTY_PAYMETHOD = new PayMethod();
+	
+	private List<PayMethod> payMethods;
+	
 	private LinesController registryPayMethod;
 	
 	private LinesController registryBank;
 	
+	public List<PayMethod> getPayMethods() {
+		if (payMethods == null) {
+			payMethods = new LinkedList<PayMethod>();
+			payMethods.add(EMPTY_PAYMETHOD);
+		}
+		return payMethods;
+	}
+
+	public void setPayMethods(List<PayMethod> payMethods) {
+		this.payMethods = payMethods;
+	}
+	
+	public int getPayMethodsSize() {
+		return this.payMethods.size();
+	}	
+
+	public List<Integer> getPayMethodsIds() {
+		List<Integer> ids = new LinkedList<Integer>();
+		for(PayMethod payMethod : getPayMethods()) {
+			if ((payMethod != null) && (payMethod.getId() != null)) {
+				ids.add(payMethod.getId());
+			}
+		}
+		return ids;
+	}
+	
+	public PayMethod getEmptyPayMethod() {
+		return EMPTY_PAYMETHOD;
+	}
+	
+	@Override
+	protected void init() throws ManagerBeanException {
+		setPayMethods(new LinkedList<PayMethod>());
+		getPayMethods().add(EMPTY_PAYMETHOD);
+		super.init();
+	}
+	
+	@Override
+	protected void completeCriteria() throws ManagerBeanException, ExpressionException {
+		Criteria criteria = getController().getCriteria();
+		String payMethod = resolveAlias("payMethods_payment_id");
+		addEnumToCriteria(criteria, payMethod, getPayMethodsIds().toArray());
+		super.completeCriteria();
+	}
+	
+	public void onAddPayMethod(ActionEvent event) {
+		getPayMethods().add(EMPTY_PAYMETHOD);
+	}
+	
+	public void onRemovePayMethod(ActionEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+		int index = Integer.valueOf(context.getExternalContext().getRequestParameterMap().get("index"));		
+		getPayMethods().remove(index);
+		if (getPayMethods().isEmpty()) {
+			getPayMethods().add(EMPTY_PAYMETHOD);
+		}
+	}
+
 	@Override
 	protected void initControllers( String name ) {
 		super.initControllers(name);
