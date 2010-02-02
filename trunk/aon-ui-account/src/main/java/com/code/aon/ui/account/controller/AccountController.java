055617c36d0bf36cd4ce3066a0c08f2d43f7d3b4
Aimar Tellitu <atellitu@esferalia.com>
2010-02-02 10:54:51 +0000

    La ordenacion ahora es parte del framework

--- trunk/aon-ui-account/src/main/java/com/code/aon/ui/account/controller/AccountController.java
+++ trunk/aon-ui-account/src/main/java/com/code/aon/ui/account/controller/AccountController.java
@@ -1,23 +1,17 @@
 package com.code.aon.ui.account.controller;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
-import javax.faces.model.SelectItem;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.code.aon.account.Account;
-import com.code.aon.account.dao.IAccountAlias;
 import com.code.aon.account.util.AccountUtil;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ui.form.BasicController;
-import com.code.aon.ui.util.AonUtil;
 
 public class AccountController extends BasicController {
 
@@ -26,9 +20,6 @@ public class AccountController extends BasicController {
 	private final static String POINT = "."; 
 	private final static String QUESTION = "?";
 	
-	private List<SelectItem> orderColumns;
-	private String orderColumn;
-	
 	private AccountUtil accountUtil;
 
 	public AccountUtil getAccountUtil() {
@@ -36,27 +27,6 @@ public class AccountController extends BasicController {
 			accountUtil = new AccountUtil();
 		}
 		return accountUtil;
-	}
-
-	public List<SelectItem> getOrderColumns() {
-		if (orderColumns == null) {
-			orderColumns = new LinkedList<SelectItem>();
-			orderColumns.add( new SelectItem(IAccountAlias.ACCOUNT_ID,AonUtil.getMessage("aon_id")));
-			orderColumns.add(new SelectItem(IAccountAlias.ACCOUNT_DESCRIPTION,AonUtil.getMessage("aon_description")));
-		}
-		return orderColumns;
-	}
-
-	public void setOrderColumns(List<SelectItem> orderColumns) {
-		this.orderColumns = orderColumns;
-	}
-	
-	public String getOrderColumn() {
-		return orderColumn;
-	}
-
-	public void setOrderColumn(String orderColumn) {
-		this.orderColumn = orderColumn;
 	}
 
 	public void onIdentifierChanged( ActionEvent event) {
