df5d341478de3a0c5c374adaf74f64c981d8b41a
Aimar Tellitu <atellitu@esferalia.com>
2010-02-08 12:24:08 +0000

    Se ha echo un refactor del ordenamiento

--- trunk/aon-ui-form/src/main/java/com/code/aon/ui/form/IDataModelDataProvider.java
+++ trunk/aon-ui-form/src/main/java/com/code/aon/ui/form/IDataModelDataProvider.java
@@ -2,10 +2,8 @@ package com.code.aon.ui.form;
 
 import java.util.List;
 
-import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.ql.Criteria;
 
 /**
  * Interface to define search methods for the DataModel classes.
@@ -14,13 +12,14 @@ import com.code.aon.ql.Criteria;
  */
 public interface IDataModelDataProvider {
 
-    /**
-     * Return the manager of bean associated to controller.
-     * 
-     * @return IManagerBean
-     * @throws ManagerBeanException
-     */
-    public IManagerBean getManagerBean() throws ManagerBeanException;
+	/**
+	 * Gets the row count.
+	 * 
+	 * @return the row count
+	 * 
+	 * @throws ManagerBeanException the manager bean exception
+	 */
+	public int getRowCount() throws ManagerBeanException;
 	
     /**
      * Search the list of ITransferObject conditioned by the criteria and the row interval delimited by parameters start and count.
@@ -32,14 +31,6 @@ public interface IDataModelDataProvider {
      */
     List<ITransferObject> search(int start, int count) throws ManagerBeanException;
 
-    /**
-     * Return the criteria.
-     * 
-     * @return Criteria
-     * @throws ManagerBeanException
-     */
-    Criteria getCriteria() throws ManagerBeanException;
-    
 	/**
 	 * Gets the page limit.
 	 * 
