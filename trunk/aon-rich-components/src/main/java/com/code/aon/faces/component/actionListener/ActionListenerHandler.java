90e07402c4a8daa43dd07a53fbcc3f07c8e0b535
Aimar Tellitu <atellitu@esferalia.com>
2010-02-23 12:11:54 +0000

    Refactor para usar contstantes definidas en otras clases

--- trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/actionListener/ActionListenerHandler.java
+++ trunk/aon-rich-components/src/main/java/com/code/aon/faces/component/actionListener/ActionListenerHandler.java
@@ -16,6 +16,8 @@ import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 import javax.faces.event.MethodExpressionActionListener;
 
+import com.code.aon.faces.component.richfaces.IRichFacesTags;
+import com.code.aon.faces.component.util.FaceletUtil;
 import com.sun.facelets.FaceletContext;
 import com.sun.facelets.FaceletException;
 import com.sun.facelets.el.LegacyMethodBinding;
@@ -26,15 +28,13 @@ import com.sun.facelets.tag.TagHandler;
 import com.sun.facelets.tag.jsf.ComponentSupport;
 import com.sun.facelets.util.FacesAPI;
 
-public class ActionListenerHandler extends TagHandler {
-
-	private final static Class[] ACTION_LISTENER_SIG = new Class[] { ActionEvent.class };
+public class ActionListenerHandler extends TagHandler implements IRichFacesTags {
 
 	private final TagAttribute method;
 
 	public ActionListenerHandler(TagConfig config) {
 		super(config);
-		this.method = this.getRequiredAttribute("method");
+		this.method = this.getRequiredAttribute(METHOD);
 	}
 
 	public void apply(FaceletContext ctx, UIComponent parent)
@@ -43,13 +43,13 @@ public class ActionListenerHandler extends TagHandler {
 			ActionSource src = (ActionSource) parent;
 			if (ComponentSupport.isNew(parent)) {
 				MethodExpression methodExpr = this.method.getMethodExpression(
-						ctx, null, ACTION_LISTENER_SIG);
+						ctx, null, FaceletUtil.ACTION_LISTENER_SIG);
 
 				ActionListener listener;
 
 				if (FacesAPI.getVersion() >= 12 && src instanceof ActionSource2) {
 					listener = new MethodExpressionActionListener(
-                            this.method.getMethodExpression(ctx, null, ACTION_LISTENER_SIG));
+                            this.method.getMethodExpression(ctx, null, FaceletUtil.ACTION_LISTENER_SIG));
 				} else {
 					listener = new LegacyMethodActionListener(
 							new LegacyMethodBinding(methodExpr));
