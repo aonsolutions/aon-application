3e60dc7fc63fe104dc66db3b6dcb04a85f7c39e7
Gorka Irazu <girazu@esferalia.com>
2010-03-05 12:43:15 +0000

    No grabar a piñon el workplace, ahora esta en la pantalla

--- branches/AON-2/aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/event/DeliveryControllerListener.java
+++ branches/AON-2/aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/event/DeliveryControllerListener.java
@@ -90,7 +90,7 @@ public class DeliveryControllerListener extends LinesControllerListener {
 	public void beforeBeanAdded(ControllerEvent event) throws ControllerListenerException {
 		((Delivery) ((DeliveryController) event.getController()).getTo()).setStatus(DeliveryStatus.PENDING);
 		((Delivery) ((DeliveryController) event.getController()).getTo()).setRaddress(obtainRegistryAddress(((Delivery) ((DeliveryController) event.getController()).getTo()).getCustomer().getRegistry().getId()));
-		((Delivery) ((DeliveryController) event.getController()).getTo()).setWorkPlace(obtainWorkPlace());
+		//((Delivery) ((DeliveryController) event.getController()).getTo()).setWorkPlace(obtainWorkPlace());
 		super.beforeBeanAdded(event);
 	}
 
