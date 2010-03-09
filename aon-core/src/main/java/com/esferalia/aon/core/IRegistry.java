eeaf06731ff31993cf983c25cd62fbbdd7490a61
Raul Trepiana <rtrepiana@esferalia.com>
2010-03-09 10:40:34 +0000

    A trunk

---
+++ aon-core/src/main/java/com/esferalia/aon/core/IRegistry.java
@@ -0,0 +1,11 @@
+package com.esferalia.aon.core;
+
+public interface IRegistry {
+	
+	IDocument getDocument();
+	void setDocument(IDocument document);
+	
+	String getFullName();
+	void setFullName(String Name);
+
+}
