d34a8ef3a0d369b0dc6a74f54fe4ba652b4a43c1
Euke Castellano <ecastellano@esferalia.com>
2010-03-12 13:05:59 +0000

    DbVersion 5.0.0

--- aon-desktop/src/main/java/com/code/aon/desktop/dao/DBManager.java
+++ aon-desktop/src/main/java/com/code/aon/desktop/dao/DBManager.java
@@ -37,7 +37,7 @@ public class DBManager {
 	
 	private static final String CREATE_SQL_PREFIX = "create.database.";
 	
-	private static final String CREATE_SQL = CREATE_SQL_PREFIX + "4.9.0.sql";
+	private static final String CREATE_SQL = CREATE_SQL_PREFIX + "5.0.0.sql";
 	
 	private static final String INSERT_SQL = "default-insert.database.sql";
 	
