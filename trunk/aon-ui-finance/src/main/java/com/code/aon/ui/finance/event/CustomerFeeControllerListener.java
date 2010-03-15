edac4646db397099378e6fb3a3e798bcd1d683c4
Gorka Irazu <girazu@esferalia.com>
2010-03-15 11:46:06 +0000

    Añadir el numero de linea en la cuota. Añadir controles necesarios en edicion y borrados, y gestionar tambien al facturar una cuota o al borrar una factura de cuotas.

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/CustomerFeeControllerListener.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/CustomerFeeControllerListener.java
@@ -3,12 +3,19 @@ package com.code.aon.ui.finance.event;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.enumeration.SecurityLevel;
 import com.code.aon.company.WorkPlace;
+import com.code.aon.customer.Customer;
 import com.code.aon.finance.CustomerFee;
+import com.code.aon.finance.dao.IFinanceAlias;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ql.Projection;
 import com.code.aon.ui.company.controller.CompanyCollectionsController;
 import com.code.aon.ui.company.controller.ICompanyConstants;
+import com.code.aon.ui.finance.controller.CustomerFeeController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -18,11 +25,14 @@ public class CustomerFeeControllerListener extends ControllerAdapter {
 	
 	@Override
 	public void afterBeanCreated(ControllerEvent event) throws ControllerListenerException {
-		CustomerFee customerFee = (CustomerFee)event.getController().getTo();
+		CustomerFeeController controller = (CustomerFeeController)event.getController();
+		CustomerFee customerFee = (CustomerFee)controller.getTo();
 		customerFee.setSecurityLevel(SecurityLevel.OFFICIAL);
 		customerFee.setQuantity(1.0);
 
 		try {
+			customerFee.setLine(calculateNextLine((Customer)controller.getMasterController().getTo()));
+
 			String companyCollections = ICompanyConstants.COLLECTIONS_CONTROLLER_NAME;
 			CompanyCollectionsController compCollections = (CompanyCollectionsController)AonUtil.getRegisteredBean(companyCollections);
 			if (compCollections.getWorkPlacesCount() == 1) {
@@ -44,6 +54,27 @@ public class CustomerFeeControllerListener extends ControllerAdapter {
 	public void beforeBeanUpdated(ControllerEvent event) throws ControllerListenerException {
 		CustomerFee customerFee = (CustomerFee)event.getController().getTo();
 		validateFeeDates(customerFee);
+	}
+
+	@Override
+	public void afterBeanUpdated(ControllerEvent event) throws ControllerListenerException {
+		event.getController().initializeModel();
+	}
+
+	@Override
+	public void afterBeanCanceled(ControllerEvent event) throws ControllerListenerException {
+		if (!event.getController().isNew()) {
+			event.getController().initializeModel();
+		}
+	}
+
+	private	Integer calculateNextLine(Customer customer) throws ManagerBeanException {
+		IManagerBean customerFeeBean = BeanManager.getManagerBean(CustomerFee.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(customerFeeBean.getFieldName(IFinanceAlias.CUSTOMER_FEE_CUSTOMER_ID), customer.getId());
+		Projection projection = Projection.max(customerFeeBean.getFieldName(IFinanceAlias.CUSTOMER_FEE_LINE));
+		Object value = customerFeeBean.getUniqueResult(projection, criteria);
+		return (value != null) ? ((Integer)value) + 1 : 1;
 	}
 
 	private void validateFeeDates(CustomerFee customerFee) throws ControllerListenerException {
