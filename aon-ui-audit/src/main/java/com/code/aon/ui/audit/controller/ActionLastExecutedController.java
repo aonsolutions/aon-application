d68f7d5d3e0e0b6454ea9c2f91312e135c8f484c
Aimar Tellitu <atellitu@esferalia.com>
2010-03-22 18:03:52 +0000

    Se ha añadido la pagina de Mapa de la Aplicación

--- aon-ui-audit/src/main/java/com/code/aon/ui/audit/controller/ActionLastExecutedController.java
+++ aon-ui-audit/src/main/java/com/code/aon/ui/audit/controller/ActionLastExecutedController.java
@@ -12,6 +12,7 @@ import com.code.aon.audit.ActionEntry;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ui.audit.ApplicationCategory;
 import com.code.aon.ui.audit.ApplicationOption;
 import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.util.AonUtil;
@@ -88,7 +89,7 @@ public class ActionLastExecutedController extends BasicController implements IAu
 		return (option != null) ? option.getDescription() : getCurrentActionEntry().getAction().getName();
 	}
 
-	public String getCategory() throws ManagerBeanException {
+	public ApplicationCategory getCategory() throws ManagerBeanException {
 		ApplicationOption option = getOption();
 		return (option != null) ? option.getCategory() : null; 
 	}
