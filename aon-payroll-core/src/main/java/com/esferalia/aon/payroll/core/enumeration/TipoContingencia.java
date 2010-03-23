871dd154f95880c4686ce2d27338fe08629cb203
Euke Castellano <ecastellano@esferalia.com>
2010-03-23 14:29:49 +0000

    dev

--- aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/enumeration/TipoContingencia.java
+++ aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/enumeration/TipoContingencia.java
@@ -1,6 +1,11 @@
 package com.esferalia.aon.payroll.core.enumeration;
 
-public enum TipoContingencia {
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import com.code.aon.common.enumeration.IResourceable;
+
+public enum TipoContingencia implements IResourceable {
 	
 	// Enfermedad comun
 	ENFERMEDAD_COMUN,
@@ -20,5 +25,12 @@ public enum TipoContingencia {
 	public boolean isAccident() {
 		return (this == ACCIDENTE_LABORAL || this == ACCIDENTE_NO_LABORAL);
 	}
-}
 	
+    private static final String BASE_NAME = "com.esferalia.aon.payroll.core.i18n.messages";
+    private static final String MSG_KEY_PREFIX = "aon_enum_tipo_contingencia_";
+
+    public String getName(Locale locale) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
+    }
+}
