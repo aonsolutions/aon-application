abe8abf492ed1d0cb56db19a89ace72bf0263837
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-03-25 17:43:55 +0000

    dev

--- aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/enumeration/Periodicidad.java
+++ aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/enumeration/Periodicidad.java
@@ -1,9 +1,20 @@
 package com.esferalia.aon.payroll.core.enumeration;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public enum Periodicidad {
 	
 	DIARIO,
 	MENSUAL;
+	
+	private static final String BASE_NAME = "com.esferalia.aon.payroll.core.i18n.messages";
+    private static final String MSG_KEY_PREFIX = "aon_enum_periodicidad_";
+
+    public String getName(Locale locale) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
+    }
 	
 }
 	
