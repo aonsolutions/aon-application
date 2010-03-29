3e04473cc4cfbc5b6c92439cfa4cda8f4ff0adb2
Euke Castellano <ecastellano@esferalia.com>
2010-03-29 15:00:55 +0000

    dev

--- trunk/aon-payroll-nominasta/src/main/java/com/esferalia/aon/payroll/ParteITPK.java
+++ trunk/aon-payroll-nominasta/src/main/java/com/esferalia/aon/payroll/ParteITPK.java
@@ -8,6 +8,9 @@ import javax.persistence.Embeddable;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Embeddable
@@ -15,14 +18,14 @@ public class ParteITPK implements Serializable {
 
 	private static final long serialVersionUID = -775104683033314551L;
 	
-	private int cdg;
+	private Integer cdg;
 	private Date fechaBaja;
 
 	@Column(name = "cdg", nullable = false, length = 4)
-	public int getCdg() {
+	public Integer getCdg() {
 		return this.cdg;
 	}
-	public void setCdg(int cdg) {
+	public void setCdg(Integer cdg) {
 		this.cdg = cdg;
 	}
 
@@ -39,4 +42,24 @@ public class ParteITPK implements Serializable {
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final ParteITPK o = (ParteITPK) obj;
+		if (o.getCdg() == null && getCdg() == null) {
+			return new EqualsBuilder()
+				.append(this.fechaBaja, o.fechaBaja)
+				.isEquals();
+		}
+		return ObjectUtils.equals(getCdg(), o.getCdg()) && ObjectUtils.equals(getFechaBaja(), o.getFechaBaja());		
+	}
+	
+//	@Override
+//	public int hashCode() {
+//		return new HashCodeBuilder().append(cdg).append(fechaBaja).hashCode();
+//	}
+	
 }
