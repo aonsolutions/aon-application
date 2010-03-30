b82f18defdf64ab67309b1569bbf7de69d38c421
Euke Castellano <ecastellano@esferalia.com>
2010-03-30 14:05:56 +0000

    dev

---
+++ trunk/aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/fdi/data/DIT.java
@@ -0,0 +1,88 @@
+package com.esferalia.aon.file.payroll.fdi.data;
+
+public class DIT {
+	private String accion;
+	private Integer causa;
+	private String contingencia;
+	private Integer fechaBaja;
+	private Integer fechaAlta;
+	private Integer fechaATEP;
+	private String numeroColegiado;
+	private String cias;
+	private Integer duracionDias;
+	private Integer duracionMeses;
+	private Integer categoria;
+	private String recaida;
+	public String getAccion() {
+		return accion;
+	}
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+	public Integer getCausa() {
+		return causa;
+	}
+	public void setCausa(Integer causa) {
+		this.causa = causa;
+	}
+	public String getContingencia() {
+		return contingencia;
+	}
+	public void setContingencia(String contingencia) {
+		this.contingencia = contingencia;
+	}
+	public Integer getFechaBaja() {
+		return fechaBaja;
+	}
+	public void setFechaBaja(Integer fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
+	public Integer getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(Integer fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	public Integer getFechaATEP() {
+		return fechaATEP;
+	}
+	public void setFechaATEP(Integer fechaATEP) {
+		this.fechaATEP = fechaATEP;
+	}
+	public String getNumeroColegiado() {
+		return numeroColegiado;
+	}
+	public void setNumeroColegiado(String numeroColegiado) {
+		this.numeroColegiado = numeroColegiado;
+	}
+	public String getCias() {
+		return cias;
+	}
+	public void setCias(String cias) {
+		this.cias = cias;
+	}
+	public Integer getDuracionDias() {
+		return duracionDias;
+	}
+	public void setDuracionDias(Integer duracionDias) {
+		this.duracionDias = duracionDias;
+	}
+	public Integer getDuracionMeses() {
+		return duracionMeses;
+	}
+	public void setDuracionMeses(Integer duracionMeses) {
+		this.duracionMeses = duracionMeses;
+	}
+	public Integer getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Integer categoria) {
+		this.categoria = categoria;
+	}
+	public String getRecaida() {
+		return recaida;
+	}
+	public void setRecaida(String recaida) {
+		this.recaida = recaida;
+	}
+}
