9a2ff0e2168a339432bfb891930669e863083703
Euke Castellano <ecastellano@esferalia.com>
2010-03-30 13:16:49 +0000

    dev

---
+++ trunk/aon-file-payroll/src/main/java/com/esferalila/aon/file/payroll/fdi/data/DOM.java
@@ -0,0 +1,77 @@
+package com.esferalila.aon.file.payroll.fdi.data;
+
+public class DOM {
+	
+	private String tipoDomicilio;
+	private String tipoVia;
+	private String nombreVia;
+	private Integer numero;
+	private String bis;
+	private String bloque;
+	private String escalera;
+	private String piso;
+	private String puerta;
+	private String sms;
+	
+	public String getTipoDomicilio() {
+		return tipoDomicilio;
+	}
+	public void setTipoDomicilio(String tipoDomicilio) {
+		this.tipoDomicilio = tipoDomicilio;
+	}
+	public String getTipoVia() {
+		return tipoVia;
+	}
+	public void setTipoVia(String tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+	public String getNombreVia() {
+		return nombreVia;
+	}
+	public void setNombreVia(String nombreVia) {
+		this.nombreVia = nombreVia;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getBis() {
+		return bis;
+	}
+	public void setBis(String bis) {
+		this.bis = bis;
+	}
+	public String getBloque() {
+		return bloque;
+	}
+	public void setBloque(String bloque) {
+		this.bloque = bloque;
+	}
+	public String getEscalera() {
+		return escalera;
+	}
+	public void setEscalera(String escalera) {
+		this.escalera = escalera;
+	}
+	public String getPiso() {
+		return piso;
+	}
+	public void setPiso(String piso) {
+		this.piso = piso;
+	}
+	public String getPuerta() {
+		return puerta;
+	}
+	public void setPuerta(String puerta) {
+		this.puerta = puerta;
+	}
+	public String getSms() {
+		return sms;
+	}
+	public void setSms(String sms) {
+		this.sms = sms;
+	}
+	
+}
---
+++ trunk/aon-file-payroll/src/main/java/com/esferalila/aon/file/payroll/fdi/data/LDD.java
@@ -0,0 +1,37 @@
+package com.esferalila.aon.file.payroll.fdi.data;
+
+public class LDD {
+	private Integer codpos;
+	private String localidad;
+	private Integer provincia;
+	private Integer telefono;
+
+	public Integer getCodpos() {
+		return codpos;
+	}
+	public void setCodpos(Integer codpos) {
+		this.codpos = codpos;
+	}
+	
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	
+	public Integer getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(Integer provincia) {
+		this.provincia = provincia;
+	}
+	
+	public Integer getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(Integer telefono) {
+		this.telefono = telefono;
+	}
+	
+}
