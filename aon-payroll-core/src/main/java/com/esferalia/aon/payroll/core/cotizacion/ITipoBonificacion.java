36ed6e27fb1f5eac377a494156a120ce6f5f0f07
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-04-06 17:05:57 +0000

    dev

--- aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/cotizacion/ITipoBonificacion.java
+++ aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/cotizacion/ITipoBonificacion.java
@@ -11,27 +11,4 @@ public interface ITipoBonificacion extends ITransferObject{
 	String getDescription();
 	void setDescription(String description);
 
-//	FormaCalculo getFormaCalculo();
-//	void setFormaCalculo(String formaCalculo);
-	
-	Double getPorcentajeContingenciasGenerales();
-	void setPorcentajeContingenciasGenerales(Double porcentajeContingenciasGenerales);
-	
-	Double getPorcentajeAccidentes();
-	void setPorcentajeAccidentes(Double porcentajeAccidentes);
-	
-	Double getPorcentajeBaseConjunto();
-	void setPorcentajeBaseConjunto(Double porcentajeBaseConjunto);
-	
-	String getPorcentajeBonificacionSS();
-	void setPorcentajeBonificacionSS(String porcentajeBonificacionSS);
-	
-	String getMayor60();
-	void setMayor60(String mayor60);
-	
-	String getRealDecretoLey052006();
-	void setRealDecretoLey052006(String realDecretoLey052006);
-	
-	String getRestarIT();
-	void setRestarIT(String restarIT);	
 }
