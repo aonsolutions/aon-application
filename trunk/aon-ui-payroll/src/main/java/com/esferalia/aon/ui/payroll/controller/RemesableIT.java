fa23015a041044f21eb3412a7534959e3a6ddec5
Euke Castellano <ecastellano@esferalia.com>
2010-04-12 15:04:40 +0000

    dev

---
+++ trunk/aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/RemesableIT.java
@@ -0,0 +1,37 @@
+package com.esferalia.aon.ui.payroll.controller;
+
+import java.io.Serializable;
+
+import com.esferalia.aon.payroll.core.it.IParteConfirmacionIT;
+import com.esferalia.aon.payroll.core.it.IParteIT;
+
+public class RemesableIT implements Serializable {
+	
+	private static final long serialVersionUID = 422693852102440685L;
+	
+	private boolean selected;
+	private IParteIT parteIT;
+	private IParteConfirmacionIT confirmacionIT;
+
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	public IParteIT getParteIT() {
+		return parteIT;
+	}
+	public void setParteIT(IParteIT parteIT) {
+		this.parteIT = parteIT;
+	}
+	
+	public IParteConfirmacionIT getConfirmacionIT() {
+		return confirmacionIT;
+	}
+	public void setConfirmacionIT(IParteConfirmacionIT confirmacionIT) {
+		this.confirmacionIT = confirmacionIT;
+	}
+
+}
