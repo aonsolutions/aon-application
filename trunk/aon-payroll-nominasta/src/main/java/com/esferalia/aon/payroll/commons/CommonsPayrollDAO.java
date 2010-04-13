eb6f6f22e85df31778b1f132d941396ed396226e
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-04-13 17:13:05 +0000

    dev

--- trunk/aon-payroll-nominasta/src/main/java/com/esferalia/aon/payroll/commons/CommonsPayrollDAO.java
+++ trunk/aon-payroll-nominasta/src/main/java/com/esferalia/aon/payroll/commons/CommonsPayrollDAO.java
@@ -6,6 +6,7 @@ import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.ql.Criteria;
+import com.code.aon.ql.util.ExpressionUtilities;
 import com.esferalia.aon.payroll.PayrollException;
 import com.esferalia.aon.payroll.Usuario;
 import com.esferalia.aon.payroll.core.IUsuario;
@@ -16,18 +17,35 @@ import com.esferalia.aon.payroll.cotizacion.TipoBonificacion;
 import com.esferalia.aon.payroll.dao.IPayrollAlias;
 
 public class CommonsPayrollDAO implements ICommonsPayrollDAO {
+	
+	private static String BONIFICACION_DESCRIPCION;
 
 	static {
 		CommonsPayrollDAOFactory.register(new CommonsPayrollDAO());
 	}
-
+	
+	@Override
+	public void configure() {	
+		try {
+			IManagerBean tipoBonificacionBean = BeanManager.getManagerBean(TipoBonificacion.class);
+			BONIFICACION_DESCRIPCION = tipoBonificacionBean.getFieldName(IPayrollAlias.TIPO_BONIFICACION_DESCRIPCION);
+		} catch (ManagerBeanException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<ITipoBonificacion> getTiposBonificacion()
+	public List<ITipoBonificacion> getTiposBonificacion(String condition)
 			throws PayrollException {
 		try {
+			Criteria criteria=null;
+			if(condition!=null || condition!=""){
+				criteria = new Criteria();
+				criteria.addExpression(ExpressionUtilities.getLikeExpression(BONIFICACION_DESCRIPCION, condition));
+			}
 			IManagerBean bean = BeanManager.getManagerBean(TipoBonificacion.class);
-			List<?> list = bean.getList(null);
+			List<?> list = bean.getList(criteria);
 			return (List<ITipoBonificacion>) list;
 		} catch (ManagerBeanException e) {
 			throw new PayrollException(e);
