80daff3a44ffe622f848cb9a1bd7e6685af416b9
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-04-27 13:56:32 +0000

    dev

---
+++ trunk/aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/it/RemesaINSSParams.java
@@ -0,0 +1,30 @@
+package com.esferalia.aon.payroll.core.it;
+
+import java.util.Date;
+
+public class RemesaINSSParams {
+
+	private Integer id;
+	private Date fecha;
+	private Date hora;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Date getHora() {
+		return hora;
+	}
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+	
+}
