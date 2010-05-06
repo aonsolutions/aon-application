8ba9ddb0cf791e3b4ef4fa4cce6ca11982018230
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-05-06 13:05:41 +0000

    dev

--- aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/enumeration/CausaAlta.java
+++ aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/enumeration/CausaAlta.java
@@ -4,20 +4,21 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 import com.code.aon.common.enumeration.IResourceable;
+import com.code.aon.common.enumeration.IStringEnum;
 
-public enum CausaAlta implements IResourceable {
+public enum CausaAlta implements IResourceable, IStringEnum  {
 	
 	
-	CURACION,
-	FALLECIMIENTO,
-	INSPECCION_MEDICA,
-	INCAPACIDAD,
-	AGOTAMIENTO_PLAZO,
-	MEJORIA,
-	INCOMPARECENCIA,
-	CONTROL_INSS,
-	RECUPERACION,
-	INCOMPARECENCIA_FORMACION;
+	CURACION("01"),
+	FALLECIMIENTO("02"),
+	INSPECCION_MEDICA("03"),
+	INCAPACIDAD("04"),
+	AGOTAMIENTO_PLAZO("05"),
+	MEJORIA("06"),
+	INCOMPARECENCIA("07"),
+	CONTROL_INSS("10"),
+	RECUPERACION("17"),
+	INCOMPARECENCIA_FORMACION("18");
 	
     private static final String BASE_NAME = "com.esferalia.aon.payroll.core.i18n.messages";
     private static final String MSG_KEY_PREFIX = "aon_enum_causa_alta_";
@@ -26,4 +27,15 @@ public enum CausaAlta implements IResourceable {
         ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
 		return bundle.getString(MSG_KEY_PREFIX + toString());
     }
+    
+    private String value;
+    
+    CausaAlta( String value ) {
+      	this.value = value;
+  	}
+    
+    @Override
+	public String getValue() {
+		return value;
+	}
 }
