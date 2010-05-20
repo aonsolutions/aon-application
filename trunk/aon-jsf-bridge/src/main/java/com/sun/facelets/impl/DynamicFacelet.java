0a6cbcc08be02654c5df6bef7b90ee7c6b73a768
Aimar Tellitu <atellitu@esferalia.com>
2010-05-20 09:54:28 +0000

    Proyecto para poder utilizar las plantillas dinamicas cuando RichFaces esta fuera del war

---
+++ trunk/aon-jsf-bridge/src/main/java/com/sun/facelets/impl/DynamicFacelet.java
@@ -0,0 +1,51 @@
+package com.sun.facelets.impl;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URL;
+
+import javax.el.ELException;
+import javax.faces.FacesException;
+
+import com.sun.facelets.Facelet;
+import com.sun.facelets.FaceletContext;
+import com.sun.facelets.FaceletException;
+import com.sun.facelets.FaceletHandler;
+
+public class DynamicFacelet {
+	
+	private static Object getPrivateProperty( Object o, String property ) {
+		try {
+			Field f = o.getClass().getDeclaredField(property);
+			f.setAccessible(true);
+			return f.get(o);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return null;				
+	}
+
+	private static DefaultFaceletFactory getDefaultFaceletFactory( FaceletContext ctx ) {
+		DefaultFacelet facelet = (DefaultFacelet) getPrivateProperty(ctx, "facelet");
+		if ( facelet != null ) {
+			return (DefaultFaceletFactory) getPrivateProperty(facelet, "factory");
+		}
+		return null;
+	}
+	
+	public static Facelet createFacelet(FaceletContext ctx, URL url) throws IOException,
+			FaceletException, FacesException, ELException {
+		String alias = "/" + url.getFile();
+		try {
+			DefaultFaceletFactory dff =  getDefaultFaceletFactory(ctx);
+			FaceletHandler h = dff.getCompiler().compile(url, alias);
+			DefaultFacelet f = new DefaultFacelet(dff, dff.getCompiler()
+					.createExpressionFactory(), url, alias, h);
+			return f;
+		} catch (FileNotFoundException fnfe) {
+			throw new FileNotFoundException("Facelet " + alias
+					+ " not found at: " + url.toExternalForm());
+		}
+	}
+}
