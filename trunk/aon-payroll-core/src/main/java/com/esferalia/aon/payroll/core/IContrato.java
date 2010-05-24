afbee39ccb684bcd6c1a5199f4a174dfefa30e13
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-05-24 16:42:08 +0000

    dev

--- trunk/aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/IContrato.java
+++ trunk/aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/IContrato.java
@@ -1,17 +1,31 @@
 package com.esferalia.aon.payroll.core;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import com.esferalia.aon.payroll.core.enumeration.Periodicidad;
 import com.esferalia.aon.payroll.core.enumeration.TipoContrato;
 
 public interface IContrato {
+	
+	Serializable getId();
+
 	IEmpleado getEmpleado();
 	void setEmpleado(IEmpleado empleado);
 	
 	TipoContrato getTipoContrato();
 	void setTipoContrato(TipoContrato tipoContrato);
 	
-	
 	Periodicidad getProrrateoCotizacion();
 	void setProrrateoCotizacion(Periodicidad period);
+
+	Date getFechaFin();
+	void setFechaFin(Date fechaFin);
+
+	String getIndtp();
+	void setIndtp(String indtp);
+
+	Double getIrpf();
+	void setIrpf(Double irpf);
 	
 }
