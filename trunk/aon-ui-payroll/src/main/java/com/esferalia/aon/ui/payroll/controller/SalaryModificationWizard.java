ede232bc7d87d1996772085d444dc4ad1b50fca4
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-05-24 13:53:17 +0000

    dev

--- trunk/aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/SalaryModificationWizard.java
+++ trunk/aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/SalaryModificationWizard.java
@@ -1,76 +1,35 @@
 package com.esferalia.aon.ui.payroll.controller;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-
+import com.code.aon.common.ICriteriaProvider;
+import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.common.enumeration.MimeType;
-import com.code.aon.file.format.output.FileOutput;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ui.form.ExtendedPageDataModel;
+import com.code.aon.ui.form.IDataModelDataProvider;
 import com.code.aon.ui.util.AonUtil;
 import com.esferalia.aon.payroll.PayrollException;
-import com.esferalia.aon.payroll.core.it.IParteITDAO;
-import com.esferalia.aon.payroll.core.it.ParteITDAOFactory;
-import com.esferalia.aon.payroll.core.remesa.IRemesaINSS;
-import com.esferalia.aon.payroll.core.remesa.IRemesaParteIT;
-import com.esferalia.aon.ui.payroll.file.FDIWriter;
+import com.esferalia.aon.payroll.core.IPersona;
+import com.esferalia.aon.payroll.core.empleado.EmpleadoDAOFactory;
+import com.esferalia.aon.payroll.core.empleado.EmpleadoParams;
+import com.esferalia.aon.payroll.core.empleado.IEmpleadoDAO;
 
-public class SalaryModificationWizard implements Serializable {
+public class SalaryModificationWizard implements Serializable, IDataModelDataProvider, ICriteriaProvider {
 
+	private static final long serialVersionUID = -841247628893188441L;
 
 	private int currentStep;
 	private static final String[] STEPS = { "salaryModificationWizard_step0", "salaryModificationWizard_step1", "salaryModificationWizard_step2", "salaryModificationWizard_step3" };
-//	private FileOutput fileOutput;
-//	private FDIWriter fdiWriter;
-//	private IRemesaINSS remesaINSS;
-//	private IParteITDAO parteITDAO;
-//	private DataModel remesaINSSModel;
+	private EmpleadoParams params;
+	private IEmpleadoDAO empleadoDAO;
+	private DataModel empleadoModel;
 	
-//	public DataModel getRemesaINSSModel() {
-//		if(remesaINSSModel==null){
-//			remesaINSSModel = new ListDataModel(getRemesaINSSList());
-//		}
-//		return remesaINSSModel;
-//	}
-//	
-//	public void setRemesaINSSModel(DataModel remesaINSSModel) {
-//		this.remesaINSSModel = remesaINSSModel;
-//	}
-//	
-//	private FDIWriter getFDIWriter() {
-//		if (fdiWriter == null) {
-//			fdiWriter = new FDIWriter();
-//		}
-//		return fdiWriter;
-//	}
-//	
-//	public FileOutput getFileOutput() {
-//		return fileOutput;
-//	}
-//
-//	public void setFileOutput(FileOutput fileOutput) {
-//		this.fileOutput = fileOutput;
-//	}
-//	
-//	public void setRemesaINSS(IRemesaINSS remesaINSS) {
-//		this.remesaINSS = remesaINSS;
-//	}
-//	public IRemesaINSS getRemesaINSS(){
-//		return remesaINSS;
-//	}
 
 	public int getCurrentStep() {
 		return this.currentStep;
@@ -79,16 +38,86 @@ public class SalaryModificationWizard implements Serializable {
 	public void setCurrentStep(int currentStep) {
 		this.currentStep = currentStep;
 	}
+	
+	public EmpleadoParams getParams() {
+		if (params == null) {
+			params = new EmpleadoParams();
+			params.setFinalizados(false);
+		}
+		return params;
+	}
 
+	public void setParams(EmpleadoParams params) {
+		this.params = params;
+	}
+	
+	public IEmpleadoDAO getEmpleadoDAO() {
+		if (empleadoDAO == null) {
+			empleadoDAO = EmpleadoDAOFactory.getInstance().getEmpleadoDAO();
+		}
+		return empleadoDAO;
+	}
+	
+	public DataModel getEmpleadoModel() {
+		if (empleadoModel == null) {
+			empleadoModel = new ExtendedPageDataModel(this, this);
+		}
+		return empleadoModel;
+	}
+
+	public void setEmpleadoModel(DataModel empleadoModel) {
+		this.empleadoModel = empleadoModel;
+	}
+
+	private void initializeEmpleadoModel() throws ManagerBeanException {
+		if (empleadoModel == null) {
+			empleadoModel = new ExtendedPageDataModel(this, this);
+		}
+		((ExtendedPageDataModel) empleadoModel).update(0, getPageLimit());
+	}
+	
+	@Override
+	public int getPageLimit() {
+		return 20;
+	}
+	@Override
+	public int getRowCount() throws ManagerBeanException {
+		try {
+			return getEmpleadoDAO().getCount(getParams());
+		} catch (PayrollException e) {
+			throw new ManagerBeanException(e);
+		}
+	}
+	@Override
+	public Criteria getCriteria() throws ManagerBeanException {
+		try {
+			return getEmpleadoDAO().getCriteria(getParams());
+		} catch (PayrollException e) {
+			throw new ManagerBeanException(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ITransferObject> search(int start, int count) throws ManagerBeanException {
+		try {
+			List<?> list = getEmpleadoDAO().getDistinctEmpleados(getParams(), start, count);
+			return (List<ITransferObject>) list;
+		} catch (PayrollException e) {
+			throw new ManagerBeanException(e);
+		}
+	}
+	
+//	Action Listeners
 	public void onNext(ActionEvent event) {
 		if (getCurrentStep() == 0) {
-			onValidate(event);
+			onSearch(event);
 			setCurrentStep(getCurrentStep() + 1);
 		} else if (getCurrentStep() == 1) {
 //			onDiskGenerate(event);
 			setCurrentStep(getCurrentStep() + 1);
 		} else if (getCurrentStep() == 2) {
-			
+			onValidate(event);
 			setCurrentStep(getCurrentStep() + 1);
 		} else if (getCurrentStep() == 3) {
 			onFinish(event);
@@ -117,8 +146,9 @@ public class SalaryModificationWizard implements Serializable {
 
 	// ***************************************************
 	public void onStart(ActionEvent event) {
-//		setRemesaINSS(null);
+		params = null;
 		setCurrentStep(0);
+		setEmpleadoModel(null);
 	}
 	
 	private void onValidate(ActionEvent event) {
@@ -128,95 +158,27 @@ public class SalaryModificationWizard implements Serializable {
 //			throw new AbortProcessingException(msg);
 //		}
 	}
-	
-//	public void onDiskGenerate(ActionEvent event) {
-//		try {
-//			String loggedUser = AonUtil.getRemoteUser();
-//			loggedUser = StringUtils.substringBefore(loggedUser, "@");
-//			List<IRemesaParteIT> list = getParteITDAO().getRemesaParteITList(getRemesaINSS());
-//			setFileOutput(getFDIWriter().createFDI(list, loggedUser));
-//			if (getFileOutput() != null) {
-//				if (getFileOutput().getErrors().size() > 0) {
-//					AonUtil.addErrorMessage("Se han producido errores en la generación del fichero.");
-//				}
-//			}
-//		} catch (ManagerBeanException e) {
-//			AonUtil.addErrorMessage(e.getMessage());
-//			// No se lanza excepción, que vaya a la última página.
-//		} catch (PayrollException e) {
-//			AonUtil.addErrorMessage(e.getMessage());
-//		}
-//	}
-//	
-//	public void onDownloadDisk(ActionEvent event) {
-//		try {
-//			FacesContext faces = FacesContext.getCurrentInstance();
-//			HttpServletResponse response = (HttpServletResponse) faces.getExternalContext().getResponse();
-//			String fileName = getFDIWriter().getEti().getFichero() + ".FDI";
-//			response.setContentType(MimeType.MIME_TXT.getName());
-//			response.setHeader("Content-disposition", "attachment; filename=\"" + fileName + ".txt\";");
-//
-//			ServletOutputStream output = response.getOutputStream();
-//			InputStream input = new FileInputStream(getFileOutput().getFile());
-//			int size = IOUtils.copy(input, output);
-//			if (size > 0) {
-//				response.setHeader("Content-Length", String.valueOf(size));
-//			}
-//			output.close();
-//			input.close();
-//
-//			response.flushBuffer();
-//			faces.responseComplete();
-//		} catch (IOException e) {
-//			AonUtil.addErrorMessage(e.getMessage());
-//			throw new AbortProcessingException(e);
-//		}
-//	}
-//
-//	public boolean isDiskOk() {
-//		int errors = 0;
-//		if (getFileOutput() != null) {
-//			errors = getFileOutput().getErrors().size();
-//		}
-//		return (errors == 0);
-//	}
 
 	private void onFinish(ActionEvent event) {
 		onStart(event);
 	}
-
-	public void onSelect(ActionEvent event) {
-//		setRemesaINSS((IRemesaINSS)getRemesaINSSModel().getRowData());
-		setCurrentStep(1);
+	
+	public void onSearch(ActionEvent event) {
+		try {
+			initializeEmpleadoModel();
+		} catch (ManagerBeanException e) {
+			AonUtil.addErrorMessage(e.getMessage());
+			throw new AbortProcessingException(e);
+		}
 	}
 
-//	public IParteITDAO getParteITDAO() {
-//		if (parteITDAO == null) {
-//			parteITDAO = ParteITDAOFactory.getInstance().getParteITDAO();
-//		}
-//		return parteITDAO;
-//	}
-//	
-//	public List<IRemesaINSS> getRemesaINSSList(){
-//		try {
-//			return getParteITDAO().getRemesaINSS(null);
-//		} catch (PayrollException e) {
-//			AonUtil.addErrorMessage(e.getMessage());
-//			throw new AbortProcessingException(e);
-//		}
-//	}
-//	
-//	public List<IRemesaParteIT> getRemesaParteITList(){
-//		try {
-//			return getParteITDAO().getRemesaParteITList(getRemesaINSS());
-//		} catch (PayrollException e) {
-//			AonUtil.addErrorMessage(e.getMessage());
-//			throw new AbortProcessingException(e);
-//		}
-//	}
-//	
-//	public DataModel getRemesaParteITModel(){
-//		return new ListDataModel(getRemesaParteITList());
-//	}
-	
+	public void onSelect(ActionEvent event) {
+		IPersona persona = (IPersona) getEmpleadoModel().getRowData();
+//		setPersona(persona);
+//		empleosModel = null;
+//		setParteITEmpleado(null);
+//		setParteIT(null);
+		setCurrentStep(2);
+	}
+
 }
