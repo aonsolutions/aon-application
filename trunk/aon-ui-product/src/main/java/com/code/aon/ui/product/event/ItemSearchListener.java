dd83791444f5160536fc1bd7660da37d66b50cf0
Gorka Irazu <girazu@esferalia.com>
2010-05-31 16:06:56 +0000

    Implementada busqueda de items por supplier

--- trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/event/ItemSearchListener.java
+++ trunk/aon-ui-product/src/main/java/com/code/aon/ui/product/event/ItemSearchListener.java
@@ -7,11 +7,13 @@ import com.code.aon.product.dao.IProductAlias;
 import com.code.aon.product.enumeration.ProductStatus;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.supplier.Supplier;
 import com.code.aon.ui.registry.controller.event.RegistrySearchListener;
 
 public class ItemSearchListener extends RegistrySearchListener {
 
 	private ProductStatus[] itemStatuses;
+	private Supplier supplier;
 	
 	public ProductStatus[] getItemStatuses() {
 		return itemStatuses;
@@ -20,11 +22,20 @@ public class ItemSearchListener extends RegistrySearchListener {
 	public void setItemStatuses(ProductStatus[] itemStatuses) {
 		this.itemStatuses = itemStatuses;
 	}
-	
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
 	@Override
 	protected void init() throws ManagerBeanException {
 		ProductStatus[] defaultItemStatus = {ProductStatus.ACTIVE};
 		setItemStatuses(defaultItemStatus);
+		setSupplier(new Supplier());
 		super.init();
 	}
 	
@@ -34,6 +45,9 @@ public class ItemSearchListener extends RegistrySearchListener {
 		if (!ArrayUtils.isEmpty(getItemStatuses())) {
 			String status = getController().resolveAlias(IProductAlias.ITEM_STATUS);
 			addEnumToCriteria(criteria, status, getItemStatuses());
+		}
+		if (getSupplier() != null && getSupplier().getId() != null) {
+			criteria.addEqualExpression(getController().resolveAlias("Item_suppliers_supplier_id"), getSupplier().getId());
 		}
 		super.completeCriteria();
 	}
