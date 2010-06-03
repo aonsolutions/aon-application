7e1ec4d371711e382f4a97075250a8740c33734e
Gorka Irazu <girazu@esferalia.com>
2010-06-03 11:24:42 +0000

    Controlar que si ya existe el cliente como cliente potencial no hay que crearlo. Fallaba por ejemplo al facturar un presupuesto, al crear el cliente intentaba crear el cliente potencial que ya existia y kaskaba.

--- trunk/aon-commercial/src/main/java/com/code/aon/commercial/event/TargetFromCustomerBeanListener.java
+++ trunk/aon-commercial/src/main/java/com/code/aon/commercial/event/TargetFromCustomerBeanListener.java
@@ -1,6 +1,7 @@
 package com.code.aon.commercial.event;
 
 import com.code.aon.commercial.Target;
+import com.code.aon.commercial.dao.ICommercialAlias;
 import com.code.aon.commercial.enumeration.Advertising;
 import com.code.aon.commercial.enumeration.TargetStatus;
 import com.code.aon.common.BeanManager;
@@ -10,6 +11,7 @@ import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanListenerAdapter;
 import com.code.aon.customer.Customer;
 import com.code.aon.customer.enumeration.CustomerStatus;
+import com.code.aon.ql.Criteria;
 
 /**
  * Listener Added to the customerController.
@@ -28,13 +30,17 @@ public class TargetFromCustomerBeanListener extends ManagerBeanListenerAdapter {
 		Customer customer = (Customer)event.getTo();
 
 		IManagerBean targetBean = BeanManager.getManagerBean(Target.class);
-		Target target = new Target();
-		target.setRegistry(customer.getRegistry());
-		target.setAdvertising(Advertising.ALLOWED);
-		target.setSurcharge(customer.isSurcharge());
-		target.setWithholding(customer.isWithholding());
-		target.setTransaction(customer.getTransaction());
-		target.setStatus((customer.getStatus() == CustomerStatus.ACTIVE) ? TargetStatus.ACTIVE : TargetStatus.INACTIVE);
-		targetBean.insert(target);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(targetBean.getFieldName(ICommercialAlias.TARGET_REGISTRY_ID), customer.getRegistry().getId());
+		if (targetBean.getCount(criteria) == 0) {
+			Target target = new Target();
+			target.setRegistry(customer.getRegistry());
+			target.setAdvertising(Advertising.ALLOWED);
+			target.setSurcharge(customer.isSurcharge());
+			target.setWithholding(customer.isWithholding());
+			target.setTransaction(customer.getTransaction());
+			target.setStatus((customer.getStatus() == CustomerStatus.ACTIVE) ? TargetStatus.ACTIVE : TargetStatus.INACTIVE);
+			targetBean.insert(target);
+		}
 	}
 }
