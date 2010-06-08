92752f70f70d25f02c6106e1e22ee5a51df4403d
Aimar Tellitu <atellitu@esferalia.com>
2010-06-08 14:35:40 +0000

    Se permite hacer una selección previa de los clientes potenciales buscados

--- trunk/aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/controller/TargetController.java
+++ trunk/aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/controller/TargetController.java
@@ -1,7 +1,16 @@
 package com.code.aon.ui.commercial.controller;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.ResourceBundle;
+import java.util.Set;
 
+import javax.faces.event.ActionEvent;
+
+import com.code.aon.commercial.Target;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Projection;
+import com.code.aon.ql.ProjectionList;
 import com.code.aon.ui.commercial.ICommercialMessages;
 import com.code.aon.ui.registry.controller.RegistryController;
 import com.code.aon.ui.util.AonUtil;
@@ -11,6 +20,8 @@ import com.code.aon.ui.util.AonUtil;
  */
 public class TargetController extends RegistryController {
 
+	private Set<Integer> checks = new HashSet<Integer>();
+	
 	private ResourceBundle bundle;
 
 	public TargetController() {
@@ -27,6 +38,50 @@ public class TargetController extends RegistryController {
 		
 	public String getAliasPreffix() {
 		return getPojoShortName();
+	}
+
+	@Override
+	public void onEditSearch(ActionEvent event) {
+		super.onEditSearch(event);
+		checkNone(event);
+	}
+	
+	public Set<Integer> getCheckedTargets() {
+		return checks;
+	}
+	
+	protected Integer getId( Object o ) {
+		return ((Target) o).getId();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void checkAll(ActionEvent event) throws ManagerBeanException{
+		Iterator iter = this.getManagerBean().getList(this.getCriteria()).iterator();
+		while(iter.hasNext()){
+			checks.add( getId(iter.next()) );
+		}
+	}
+
+	public void checkNone(ActionEvent event) {
+		this.checks.clear();
+	}
+
+	public boolean getRowChecked() {
+		Integer id = getId( model.getRowData() );
+		return checks.contains(id);
+	}
+
+	public void setRowChecked(boolean rowChecked) {
+		Integer id = getId(model.getRowData());		
+		if (rowChecked) {
+			if (!checks.contains(id)) {
+				checks.add(id);
+			}
+		} else {
+			if (checks.contains(id)) {
+				checks.remove(id);
+			}
+		}
 	}
 	
 }
