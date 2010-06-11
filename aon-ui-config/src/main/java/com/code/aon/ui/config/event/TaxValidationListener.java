e954af2c3f18f8d3b01b94cace930883afa5cc3d
Gorka Irazu <girazu@esferalia.com>
2010-06-11 10:36:23 +0000

    Corregidos errores en el listener de validacion de impuestos, en solapes de fechas e insercion de nuevos TaxDetails

--- aon-ui-config/src/main/java/com/code/aon/ui/config/event/TaxValidationListener.java
+++ aon-ui-config/src/main/java/com/code/aon/ui/config/event/TaxValidationListener.java
@@ -5,12 +5,12 @@ import java.util.GregorianCalendar;
 
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
-import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.config.Tax;
 import com.code.aon.config.TaxDetail;
 import com.code.aon.config.dao.IConfigAlias;
 import com.code.aon.ql.Criteria;
+import com.code.aon.ui.config.controller.ConfigConstants;
 import com.code.aon.ui.form.FormUtil;
 import com.code.aon.ui.form.LinesController;
 import com.code.aon.ui.form.event.ControllerAdapter;
@@ -21,76 +21,66 @@ import com.code.aon.ui.util.AonUtil;
 public class TaxValidationListener extends ControllerAdapter {
 
 	private static final String CONFIG_BUNDLE = "configBundle";
-	private static final String ERROR_MESSAGE = "config_invalid_startDate";
-	private static final String OVERLAP_ERROR_MESSAGE = "config_date_overlap";
-	private Tax oldTax;
-	private TaxDetail taxDetail;
+	private static final String START_DATE_ERROR_MESSAGE = "config_invalid_startDate";
 
 	@Override
 	public void beforeBeanUpdated(ControllerEvent event) throws ControllerListenerException {
 		Tax tax = (Tax) event.getController().getTo();
-		searchOldTax(tax);
-		insertTaxDetail(tax);
+		insertTaxDetail(tax, searchOldTax(event, tax));
 	}
 
-	public void insertTaxDetail(Tax tax) throws ControllerListenerException {
-		if(!oldTax.getStartDate().equals(tax.getStartDate()) && hasOverlap(tax)){
-			tax.setStartDate(oldTax.getStartDate());
-			String msg = AonUtil.getMessage(CONFIG_BUNDLE, OVERLAP_ERROR_MESSAGE);
-			throw new ControllerListenerException(msg);			
+	public Tax searchOldTax(ControllerEvent event, Tax tax) throws ControllerListenerException {
+		try {
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(event.getController().getFieldName(IConfigAlias.TAX_ID), tax.getId());
+			return (Tax)event.getController().getManagerBean().getList(criteria).get(0);
+		} catch (ManagerBeanException e) {
+			throw new ControllerListenerException(e.getMessage(), e);
 		}
-		if((oldTax.getPercentage() != tax.getPercentage() && !oldTax.getStartDate().equals(tax.getStartDate()))||(oldTax.getStartDate().before(tax.getStartDate()))){
-				taxDetail = new TaxDetail();
+	}
+	
+	public void insertTaxDetail(Tax tax, Tax oldTax) throws ControllerListenerException {
+		if (!tax.getStartDate().equals(oldTax.getStartDate())) {
+			if (hasOverlap(tax)) {
+				tax.setStartDate(oldTax.getStartDate());
+				throw new ControllerListenerException(AonUtil.getMessage(CONFIG_BUNDLE, START_DATE_ERROR_MESSAGE));
+			}
+
+			if (tax.getStartDate().after(oldTax.getStartDate())) {
+				Calendar calendar = new GregorianCalendar();
+				calendar.setTime(tax.getStartDate());
+				calendar.add(Calendar.DATE, -1);
+
+				TaxDetail taxDetail = new TaxDetail();
 				taxDetail.setTax(tax);
 				taxDetail.setValue(oldTax.getPercentage());
 				taxDetail.setSurcharge(oldTax.getSurcharge());
 				taxDetail.setStartDate(oldTax.getStartDate());
-				Calendar calendar = new GregorianCalendar();
-				calendar.setTime(tax.getStartDate());
-				calendar.add(Calendar.DAY_OF_MONTH, -1);
 				taxDetail.setEndDate(calendar.getTime());
 				try {
-					IManagerBean bean = BeanManager.getManagerBean(TaxDetail.class);
-					bean.insert(taxDetail);
-					LinesController linesController = (LinesController)FormUtil.getController("taxDetail");
+					IManagerBean taxDetailBean = BeanManager.getManagerBean(TaxDetail.class);
+					taxDetailBean.insert(taxDetail);
+
+					LinesController linesController = (LinesController)FormUtil.getController(ConfigConstants.TAX_DETAIL);
 					linesController.onSearch(null);
 				} catch (ManagerBeanException e) {
-					String msg = "ManagerBean insert error";
-					throw new ControllerListenerException(msg);
+					throw new ControllerListenerException(e.getMessage(), e);
 				}
 			}
-	}
-	
-	public void searchOldTax(Tax tax) throws ControllerListenerException {
-		try {
-			IManagerBean bean = BeanManager.getManagerBean(Tax.class);
-			Criteria criteria = new Criteria();
-			criteria.addEqualExpression(bean.getFieldName(IConfigAlias.TAX_ID), tax.getId());
-			oldTax = (Tax)bean.getList(criteria).get(0);
-		} catch (ManagerBeanException e) {
-			String msg = "ManagerBean search error";
-			throw new ControllerListenerException(msg);
 		}
 	}
 	
 	public boolean hasOverlap(Tax tax) throws ControllerListenerException {
 		try {
-			IManagerBean bean = BeanManager.getManagerBean(TaxDetail.class);
+			IManagerBean taxDetailBean = BeanManager.getManagerBean(TaxDetail.class);
 			Criteria criteria = new Criteria();
-			criteria.addEqualExpression(bean.getFieldName(IConfigAlias.TAX_DETAIL_TAX_ID), tax.getId());
-			TaxDetail detail; 
-			for(ITransferObject to: bean.getList(criteria)){
-				detail = (TaxDetail)to;
-				if((tax.getStartDate().after(detail.getStartDate()) || tax.getStartDate().equals(detail.getStartDate()))
-						&& (tax.getStartDate().before(detail.getEndDate()) || tax.getStartDate().equals(detail.getEndDate()))){
-					tax.setStartDate(oldTax.getStartDate());
-					String msg = AonUtil.getMessage(CONFIG_BUNDLE, OVERLAP_ERROR_MESSAGE);
-					throw new ControllerListenerException(msg);
-				}
+			criteria.addEqualExpression(taxDetailBean.getFieldName(IConfigAlias.TAX_DETAIL_TAX_ID), tax.getId());
+			criteria.addGreaterThanOrEqualExpression(taxDetailBean.getFieldName(IConfigAlias.TAX_DETAIL_END_DATE), tax.getStartDate());
+			if (taxDetailBean.getCount(criteria) > 0) {
+				return true;
 			}
 		} catch (ManagerBeanException e) {
-			String msg = "ManagerBean error";
-			throw new ControllerListenerException(msg);
+			throw new ControllerListenerException(e.getMessage(), e);
 		}
 		return false;
 	}
