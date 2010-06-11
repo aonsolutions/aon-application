7b49155d7702be90b26d9a12b4dcb5f48c3a8342
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-06-11 16:36:37 +0000

    dev

---
+++ aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/certificate/data/Periodo.java
@@ -0,0 +1,68 @@
+package com.esferalia.aon.file.payroll.certificate.data;
+
+import java.util.Date;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class Periodo {
+	
+	private static final String PERIODO = "Periodo";
+	private static final String TIPO_DISTRIBUCION = "TipoDistribucion";
+	private static final String FECHA_INICIO_PERIODO = "FechaInicioPeriodo";
+	private static final String FECHA_FIN_PERIODO = "FechaFinPeriodo";
+	private static final String NUMERO_DIAS_TRAB_POR_SEMANA_O_PERIODO = "NumeroDiasTrabajadosPorSemanaOPeriodo";
+	
+	private String tipoDistribucion;
+	private Date fechaInicioPeriodo;
+	private Date fechaFinPeriodo;
+	private Integer numeroDiasTrabajadosPorSemanaOPeriodo;
+
+	public String getTipoDistribucion() {
+		return tipoDistribucion;
+	}
+	public void setTipoDistribucion(String tipoDistribucion) {
+		this.tipoDistribucion = tipoDistribucion;
+	}
+	public Date getFechaInicioPeriodo() {
+		return fechaInicioPeriodo;
+	}
+	public void setFechaInicioPeriodo(Date fechaInicioPeriodo) {
+		this.fechaInicioPeriodo = fechaInicioPeriodo;
+	}
+	public Date getFechaFinPeriodo() {
+		return fechaFinPeriodo;
+	}
+	public void setFechaFinPeriodo(Date fechaFinPeriodo) {
+		this.fechaFinPeriodo = fechaFinPeriodo;
+	}
+	public Integer getNumeroDiasTrabajadosPorSemanaOPeriodo() {
+		return numeroDiasTrabajadosPorSemanaOPeriodo;
+	}
+	public void setNumeroDiasTrabajadosPorSemanaOPeriodo(
+			Integer numeroDiasTrabajadosPorSemanaOPeriodo) {
+		this.numeroDiasTrabajadosPorSemanaOPeriodo = numeroDiasTrabajadosPorSemanaOPeriodo;
+	}
+	
+	public Node getElement(Document xmldoc) {
+		Element tipoDistribucion = xmldoc.createElement(TIPO_DISTRIBUCION);
+		Element fechaInicioPeriodo = xmldoc.createElement(FECHA_INICIO_PERIODO);
+		Element fechaFinPeriodo = xmldoc.createElement(FECHA_FIN_PERIODO);
+		Element numeroDiasTrabajadosPorSemanaOPeriodo = xmldoc.createElement(NUMERO_DIAS_TRAB_POR_SEMANA_O_PERIODO);
+		
+		tipoDistribucion.appendChild(xmldoc.createTextNode(getTipoDistribucion() ));
+		fechaInicioPeriodo.appendChild(xmldoc.createTextNode(getFechaInicioPeriodo().toString() ));
+		fechaFinPeriodo.appendChild(xmldoc.createTextNode(getFechaFinPeriodo().toString() ));
+		numeroDiasTrabajadosPorSemanaOPeriodo.appendChild(xmldoc.createTextNode(getNumeroDiasTrabajadosPorSemanaOPeriodo().toString() ));
+		
+		Element periodo = xmldoc.createElement(PERIODO);
+		periodo.appendChild(tipoDistribucion);
+		periodo.appendChild(fechaInicioPeriodo);
+		periodo.appendChild(fechaFinPeriodo);
+		periodo.appendChild(numeroDiasTrabajadosPorSemanaOPeriodo);
+		
+		return periodo;
+	}
+	
+}
