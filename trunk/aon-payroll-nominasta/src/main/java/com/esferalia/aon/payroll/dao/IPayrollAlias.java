6ce3cac4a9ab09963dc47f94258fcd4644f5c3e2
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-06-17 11:53:06 +0000

    dev

--- trunk/aon-payroll-nominasta/src/main/java/com/esferalia/aon/payroll/dao/IPayrollAlias.java
+++ trunk/aon-payroll-nominasta/src/main/java/com/esferalia/aon/payroll/dao/IPayrollAlias.java
@@ -1353,28 +1353,34 @@ public interface IPayrollAlias {
 	String  TRABAJO_CONTRATO_TC2_CDG = TRABAJO_ENTRY.getAliasNames()[2];
 
 	/** 
+	* Alias value: Trabajo_empleado_id
+	* Hibernate value: Trabajo.empleado.id
+	*/
+	String  TRABAJO_EMPLEADO_ID = TRABAJO_ENTRY.getAliasNames()[3];
+
+	/** 
 	* Alias value: Trabajo_fechaFinCont
 	* Hibernate value: Trabajo.fechaFinCont
 	*/
-	String  TRABAJO_FECHA_FIN_CONT = TRABAJO_ENTRY.getAliasNames()[3];
+	String  TRABAJO_FECHA_FIN_CONT = TRABAJO_ENTRY.getAliasNames()[4];
 
 	/** 
 	* Alias value: Trabajo_fechaInicioCont
 	* Hibernate value: Trabajo.fechaInicioCont
 	*/
-	String  TRABAJO_FECHA_INICIO_CONT = TRABAJO_ENTRY.getAliasNames()[4];
+	String  TRABAJO_FECHA_INICIO_CONT = TRABAJO_ENTRY.getAliasNames()[5];
 
 	/** 
 	* Alias value: Trabajo_id_cdg
 	* Hibernate value: Trabajo.id.cdg
 	*/
-	String  TRABAJO_ID_CDG = TRABAJO_ENTRY.getAliasNames()[5];
+	String  TRABAJO_ID_CDG = TRABAJO_ENTRY.getAliasNames()[6];
 
 	/** 
 	* Alias value: Trabajo_id_fecini
 	* Hibernate value: Trabajo.id.fecini
 	*/
-	String  TRABAJO_ID_FECINI = TRABAJO_ENTRY.getAliasNames()[6];
+	String  TRABAJO_ID_FECINI = TRABAJO_ENTRY.getAliasNames()[7];
 
 
 
