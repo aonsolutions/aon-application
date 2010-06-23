41fd8bf8f8311ca7a9168d87f24c5fc25f79e396
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-06-23 16:04:07 +0000

    dev

--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/file/CertificateWriter.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/file/CertificateWriter.java
@@ -1,30 +1,75 @@
 package com.esferalia.aon.ui.payroll.file;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.apache.commons.lang.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.file.format.output.FileOutput;
 import com.esferalia.aon.file.payroll.certificate.Certificate;
 import com.esferalia.aon.file.payroll.certificate.data.Cotizacion;
 import com.esferalia.aon.file.payroll.certificate.data.CuentaCotizacion;
+import com.esferalia.aon.file.payroll.certificate.data.DistribucionJornada;
 import com.esferalia.aon.file.payroll.certificate.data.Empresa;
-import com.esferalia.aon.file.payroll.certificate.data.Jornada;
 import com.esferalia.aon.file.payroll.certificate.data.Periodo;
 import com.esferalia.aon.file.payroll.certificate.data.Representante;
 import com.esferalia.aon.file.payroll.certificate.data.Trabajador;
 import com.esferalia.aon.file.payroll.certificate.data.Vacaciones;
 import com.esferalia.aon.payroll.PayrollException;
+import com.esferalia.aon.payroll.core.IEmpleado;
+import com.esferalia.aon.payroll.core.IFiniquito;
+import com.esferalia.aon.payroll.core.IFiniquitoDiferencia;
+import com.esferalia.aon.payroll.core.INomina;
+import com.esferalia.aon.payroll.core.INominaDiferencia;
+import com.esferalia.aon.payroll.core.ITrabajo;
+import com.esferalia.aon.payroll.core.empleado.EmpleadoDAOFactory;
+import com.esferalia.aon.payroll.core.empleado.IEmpleadoDAO;
 import com.esferalia.aon.payroll.core.empresa.IRemesaCertificadoEmpresa;
 import com.esferalia.aon.payroll.core.empresa.IRemesaCertificadoEmpresaDetalle;
+import com.esferalia.aon.payroll.core.enumeration.TipoNomina;
+import com.esferalia.aon.payroll.core.nomina.INominaDAO;
+import com.esferalia.aon.payroll.core.nomina.NominaDAOFactory;
+import com.esferalia.aon.payroll.core.nomina.NominaParams;
 
 public class CertificateWriter {
 	
 	private Certificate certificate;
+	private IEmpleadoDAO empleadoDAO;
+	private INominaDAO nominaDAO;
+	
+	private IEmpleadoDAO getEmpleadoDAO() {
+		if (empleadoDAO == null) {
+			empleadoDAO = EmpleadoDAOFactory.getInstance().getEmpleadoDAO();
+		}
+		return empleadoDAO;
+	}
+	
+	private INominaDAO getNominaDAO() {
+		if (nominaDAO == null) {
+			nominaDAO = NominaDAOFactory.getInstance().getNominaDAO();
+		}
+		return nominaDAO;
+	}
 	
 	public Certificate getCertificate() {
 		return certificate;
@@ -41,14 +86,44 @@ public class CertificateWriter {
 			listaCuentas.add(createCuentaCotizacionRecord(remesa, remesaDetail));
 			getCertificate().setCuentaCotizacion(listaCuentas);
 			
-			File file = File.createTempFile("XXXXXXXX", ".XML");
 			FileOutput output = new FileOutput();
+
+			File file = File.createTempFile("XXXXXXXX", ".XML");
+			FileOutputStream out = new FileOutputStream(file);
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document xmldoc = builder.newDocument();
+			Element root = certificate.getElement(xmldoc);
+			xmldoc.appendChild(root);
+			certificate.fillElement(xmldoc, root);
+			DOMSource domSource = new DOMSource(xmldoc);
+			StreamResult streamResult = new StreamResult(out);
+			TransformerFactory tf = TransformerFactory.newInstance();
+			Transformer serializer = tf.newTransformer();
+			serializer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
+			serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+			serializer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			serializer.transform(domSource, streamResult);
+			
+			
+			
+			
 			output.setFile(file);
+//			output.setErrors(fdi.create());
+			output.setErrors(new ArrayList<Exception>());
 			return output;
+			
+			
 		} catch (IOException e) {
 			throw new ManagerBeanException(e);
 		} 
 		catch (PayrollException e) {
+			throw new ManagerBeanException(e);
+		} catch (ParserConfigurationException e) {
+			throw new ManagerBeanException(e);
+		} catch (TransformerConfigurationException e) {
+			throw new ManagerBeanException(e);
+		} catch (TransformerException e) {
 			throw new ManagerBeanException(e);
 		}
 	}
@@ -59,10 +134,10 @@ public class CertificateWriter {
 		representante.setCifNif(remesa.getEmpresa().getRepresentanteDocument());
 		representante.setNombre(remesa.getEmpresa().getNombreRepresentante());
 		representante.setApellido1(remesa.getEmpresa().getApellido1Representante());
-		if(StringUtils.isBlank(remesa.getEmpresa().getApellido2Representante())){
+		if(!StringUtils.isBlank(remesa.getEmpresa().getApellido2Representante())){
 			representante.setApellido2(remesa.getEmpresa().getApellido2Representante());
 		}
-		if(StringUtils.isBlank(remesa.getEmpresa().getCargo())){
+		if(!StringUtils.isBlank(remesa.getEmpresa().getCargo())){
 			representante.setCargo(remesa.getEmpresa().getCargo());
 		}
 		cuentaCotizacion.setRepresentante(representante);
@@ -80,22 +155,33 @@ public class CertificateWriter {
 	}
 
 	private Trabajador createTrabajadorRecord(IRemesaCertificadoEmpresaDetalle detalle) {
+		List<ITrabajo> trabajos = null;
+		try {
+			trabajos = getEmpleadoDAO().getTrabajos(detalle.getEmpleado());
+		} catch (PayrollException e) {
+			// NADA
+		}
+		
 		Trabajador trabajador = new Trabajador();
 		trabajador.setDniNie(detalle.getEmpleado().getPersona().getRegistry().getDocument().getValue());
 		trabajador.setNombre(detalle.getEmpleado().getPersona().getName());
 		trabajador.setApellido1(detalle.getEmpleado().getPersona().getSurname());
 		trabajador.setApellido2(detalle.getEmpleado().getPersona().getLastName());
 		trabajador.setNumSs(detalle.getEmpleado().getPersona().getNumSS());
-		trabajador.setGrupoCotizacion(null);
-		trabajador.setTipoContrato(null);
-		trabajador.setDuracionContrato(null);
+		trabajador.setGrupoCotizacion(trabajos.get(0).getBaseCotizacion().getCdg());
+		trabajador.setTipoContrato(trabajos.get(0).getContratoTc2().getCdg());
+		if(trabajos.get(0).getFechaFinCont()!=null && trabajos.get(0).getFechaInicioCont()!=null){
+			trabajador.setDuracionContrato(differenceBetweenDates(trabajos.get(0).getFechaFinCont(),trabajos.get(0).getFechaInicioCont()).toString());
+		}
+		
 //		trabajador.setIndicadorDuracionContrato();
-		trabajador.setCodProfesion(null);
+		trabajador.setCodProfesion(trabajos.get(0).getCno());
 //		trabajador.setCargoPublicoSindical();
 //		trabajador.setPorcentualDedicacion();
 		trabajador.setFechaAltaEmpresa(detalle.getEmpleado().getFechaInicio().toString());
 		trabajador.setCodCausaSuspension(detalle.getCausaSuspension());
-		trabajador.setFechaSuspensionExtincion(detalle.getEmpleado().getFechaFin().toString());
+//		trabajador.setFechaSuspensionExtincion(detalle.getEmpleado().getFechaFin().toString());
+		trabajador.setFechaSuspensionExtincion(detalle.getFechaBaja().toString());
 //		trabajador.setFechaFinSuspension();
 //		trabajador.setEre();
 //		trabajador.setPorcentualReduccionERE();
@@ -103,47 +189,170 @@ public class CertificateWriter {
 //		trabajador.setCodCausaPorcentReduccion();
 //		trabajador.setFechaDesdePeriodoSalarios();
 //		trabajador.setFechaHastaPeriodoSalarios();
-//		trabajador.setDiasSalarioTramitacion();
+		trabajador.setDiasSalarioTramitacion("0");
 
 		/*
 		 * NODOS
 		 */
-		trabajador.setDistribucionJornada(createDistribucionJornadaRecord());		
+		trabajador.setDistribucionJornada(createDistribucionJornadaRecord(detalle));		
 		List<Cotizacion> listaDatosCotizacion = new ArrayList<Cotizacion>();
 //		for(IRemesaCertificadoEmpresaDetalle detalle: listaDetalle){
-			listaDatosCotizacion.add(createDatosCotizacionRecord(detalle));
+		listaDatosCotizacion.addAll(createDatosCotizacionRecord(detalle));
 //		}
 		trabajador.setDatosCotizacion(listaDatosCotizacion);
-		trabajador.setDatosVacacionesCotizadas(createDatosVacacionesCotizadasRecord());
+		trabajador.setDatosVacacionesCotizadas(createDatosVacacionesCotizadasRecord(detalle.getEmpleado()));
 		
 		return trabajador;
 	}
 
 
-	private Jornada createDistribucionJornadaRecord() {
-		Jornada jornada = new Jornada();
-		List<Periodo> listaPeriodos = new ArrayList<Periodo>();
-		jornada.setListaPeriodos(listaPeriodos);
-		return jornada;
+	private DistribucionJornada createDistribucionJornadaRecord(IRemesaCertificadoEmpresaDetalle detalle) {
+		List<ITrabajo> trabajosTP = null;
+		try {
+			trabajosTP = getEmpleadoDAO().getTrabajosTP(detalle.getEmpleado());
+		} catch (PayrollException e) {
+			// NADA
+		}
+		if(trabajosTP!=null){
+			List<Periodo> listaPeriodos = new ArrayList<Periodo>();
+			for(ITrabajo t: trabajosTP){
+				Periodo periodo = new Periodo();
+				periodo.setTipoDistribucion(t.getTipoTP().getValue());
+				periodo.setFechaInicioPeriodo(t.getFecini());
+				periodo.setFechaFinPeriodo(t.getFecfin());
+				periodo.setNumeroDiasTrabajadosPorSemanaOPeriodo(t.getDiasTP());
+				listaPeriodos.add(periodo);
+			}
+			DistribucionJornada jornada = new DistribucionJornada();
+			jornada.setListaPeriodos(listaPeriodos);
+			return jornada;
+		}
+		return null;
 	}
 	
-	private Cotizacion createDatosCotizacionRecord(
+	private List<Cotizacion> createDatosCotizacionRecord(
 			IRemesaCertificadoEmpresaDetalle detalle) {
-		Cotizacion cotizacion = new Cotizacion();
-		cotizacion.setAno(null);
-		cotizacion.setMes(null);
-		cotizacion.setNumDiasCotizados(null);
-		cotizacion.setBaseCotizacionContingenciasComunes(null);
-		cotizacion.setBaseCotizacionDesempleo(null);
-		cotizacion.setObservaciones(null);
-		return cotizacion;
+		INomina nomina = null;
+		INominaDiferencia nominaDiferencia = null;
+		try {
+	        Double totalBaseCg=0.0;
+	        Double totalBaseDesempleo=0.0;
+	        Integer totalDias = 0;
+			
+			Calendar calInicio = new GregorianCalendar();
+			Calendar calFin = new GregorianCalendar();
+			calInicio.setTime(detalle.getEmpleado().getFechaInicio());
+//			calFin.setTime(detalle.getEmpleado().getFechaFin());
+			calFin.setTime(detalle.getFechaBaja());
+			
+			List<Cotizacion> cotizacionList = new ArrayList<Cotizacion>();
+			while(calInicio.before(calFin) && totalDias < 180 && existNomina(detalle.getEmpleado(),calFin)){
+				NominaParams params = new NominaParams();
+				params.setEmpleado(detalle.getEmpleado());
+				params.setTipo(TipoNomina.NORMAL);
+				params.setMes(calFin.get(Calendar.MONTH));
+				params.setYear(calFin.get(Calendar.YEAR));
+				nomina = getNominaDAO().getNomina(params);
+				
+				nominaDiferencia = getNominaDAO().getNominaDiferencia(params);
+				Double baseAcc = nomina.getBaseAccPts();
+				if(nominaDiferencia!=null){
+					totalBaseCg += nominaDiferencia.getBaseCgPts();
+					totalBaseDesempleo += nominaDiferencia.getBasePerdes();
+					baseAcc = nominaDiferencia.getBaseAccPts();
+				}
+				
+				if(nomina.getBaseHorasExtrasEstructurales()==0 && nomina.getBaseHorasExtrasNoEstructurales()==0){
+					totalBaseDesempleo = baseAcc;
+				} else if(detalle.getEmpleado().getEmpresa().getDivisa().getCdg().equals("2")){
+					totalBaseDesempleo = convertMoney();
+				}
+				
+				totalBaseCg += nomina.getBaseCgPts();
+		        totalBaseDesempleo += nomina.getBasePerdes();
+				totalDias += nomina.getDiasNomina();
+				calFin.set(Calendar.MONTH, calFin.get(Calendar.MONTH)-1);
+
+				
+				Cotizacion cotizacion = new Cotizacion();
+				cotizacion.setAno(nomina.getYear());
+				cotizacion.setMes(nomina.getMes());
+				cotizacion.setNumDiasCotizados(nomina.getDiasNomina());
+				cotizacion.setBaseCotizacionContingenciasComunes(totalBaseCg.intValue());
+				cotizacion.setBaseCotizacionDesempleo(totalBaseDesempleo.intValue());
+				cotizacion.setObservaciones(null);
+				
+				cotizacionList.add(cotizacion);
+			}
+			
+			
+			return cotizacionList;
+		} catch (PayrollException e) {
+			// NADA
+		}
+		return null;
 	}
 	
-	private Vacaciones createDatosVacacionesCotizadasRecord() {
+	private boolean existNomina(IEmpleado empleado, Calendar calFin) throws PayrollException {
+		NominaParams params = new NominaParams();
+		params.setEmpleado(empleado);
+		params.setTipo(TipoNomina.NORMAL);
+		params.setMes(calFin.get(Calendar.MONTH));
+		params.setYear(calFin.get(Calendar.YEAR));
+		if(getNominaDAO().getNomina(params)!=null){
+			return true;
+		} 
+		return false;
+	}
+
+	private Vacaciones createDatosVacacionesCotizadasRecord(IEmpleado empleado) {
+		try {
+			IFiniquito finiquito = getNominaDAO().getFiniquito(empleado);
+			if(finiquito!=null){
+				Vacaciones vacaciones = new Vacaciones();
+				vacaciones.setNumDiasCotizados(finiquito.getDiasVacaciones());
+				vacaciones.setBaseCotizacionDesempleo(finiquito.getBaseAccidentesTrabajo().intValue());
+				vacaciones.setBaseCotizacionContingenciasComunes(finiquito.getBaseContingenciasGenerales().intValue());
+				vacaciones.setObservaciones(null);
+				IFiniquitoDiferencia finiquitodf = getNominaDAO().getFiniquitoDiferencia(empleado);
+				if(finiquitodf!=null){
+					vacaciones.setBaseCotizacionDesempleo(vacaciones.getBaseCotizacionDesempleo()+finiquitodf.getBaseAccidentesTrabajo().intValue());
+					vacaciones.setBaseCotizacionContingenciasComunes(vacaciones.getBaseCotizacionContingenciasComunes()+finiquitodf.getBaseContingenciasGenerales().intValue());
+				}
+				return vacaciones;
+			}
+		} catch (PayrollException e) {
+			// NADA
+		}
+		return null;
+	}
+
+	private Double convertMoney() {
 		
 		return null;
 	}
 	
-	
+	private static Integer differenceBetweenDates(Date date1, Date date2){
+		GregorianCalendar initDate = new GregorianCalendar();
+		GregorianCalendar endDate = new GregorianCalendar();
+		initDate.setTime(date1);
+		endDate.setTime(date2);
+		int days1 = 0;
+		int days2 = 0;
+		int maxYear = Math.max(initDate.get(Calendar.YEAR), endDate.get(Calendar.YEAR));
+		GregorianCalendar gctmp = (GregorianCalendar) initDate.clone();
+		for (int f = gctmp.get(Calendar.YEAR); f < maxYear; f++) {
+			days1 += gctmp.getActualMaximum(Calendar.DAY_OF_YEAR);
+			gctmp.add(Calendar.YEAR, 1);
+		}
+		gctmp = (GregorianCalendar) endDate.clone();
+		for (int f = gctmp.get(Calendar.YEAR); f < maxYear; f++) {
+			days2 += gctmp.getActualMaximum(Calendar.DAY_OF_YEAR);
+			gctmp.add(Calendar.YEAR, 1);
+		}
+		days1 += initDate.get(Calendar.DAY_OF_YEAR) - 1;
+		days2 += endDate.get(Calendar.DAY_OF_YEAR) - 1;
+		return (days2>days1)?(days2-days1):(days1-days2);
+	}
 	
 }
