19d537ed14bb9e83162273e059f8a80492f9dc05
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-06-23 16:03:34 +0000

    dev

--- aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/nomina/INominaDAO.java
+++ aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/nomina/INominaDAO.java
@@ -5,7 +5,11 @@ import java.util.List;
 import com.code.aon.ql.Criteria;
 import com.esferalia.aon.payroll.PayrollException;
 import com.esferalia.aon.payroll.core.IBonificacion;
+import com.esferalia.aon.payroll.core.IEmpleado;
+import com.esferalia.aon.payroll.core.IFiniquito;
+import com.esferalia.aon.payroll.core.IFiniquitoDiferencia;
 import com.esferalia.aon.payroll.core.INomina;
+import com.esferalia.aon.payroll.core.INominaDiferencia;
 
 public interface INominaDAO {
 	
@@ -16,6 +20,15 @@ public interface INominaDAO {
 	Criteria getCriteria(NominaParams params);
 	
 	void accept(IBonificacion bonifacion) throws PayrollException;
+
+	Criteria getCriteriaNominaDiferencia(NominaParams params);
+
+	INominaDiferencia getNominaDiferencia(NominaParams params)
+			throws PayrollException;
+
+	IFiniquito getFiniquito(IEmpleado empleado) throws PayrollException;
+	IFiniquitoDiferencia getFiniquitoDiferencia(IEmpleado empleado)
+			throws PayrollException;
 
 	
 }
