69d42ea3aa481e352230e07004d5589169e841ec
Gorka Irazu <girazu@esferalia.com>
2010-06-24 17:27:43 +0000

    Modificados valores por defecto de la tabla de impuestos. Habia un error.

--- trunk/aon-master/src/main/resources/com/code/aon/master/defaults/academy-insert.database.sql
+++ trunk/aon-master/src/main/resources/com/code/aon/master/defaults/academy-insert.database.sql
@@ -25,7 +25,7 @@ COMMIT;
 
 INSERT INTO `tax_detail` (`id`, `tax`, `start_date`, `end_date`, `value`, `surcharge`) VALUES 
   (1,2,'2000-01-01','2010-06-30',16,4),
-  (1,3,'2000-01-01','2010-06-30',7,1);
+  (2,3,'2000-01-01','2010-06-30',7,1);
 
 COMMIT;
 
