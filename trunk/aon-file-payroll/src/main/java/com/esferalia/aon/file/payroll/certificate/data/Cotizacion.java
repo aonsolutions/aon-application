9873e44b4791f5300edfead8e49bed70236a17b6
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-06-24 16:32:27 +0000

    dev

--- trunk/aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/certificate/data/Cotizacion.java
+++ trunk/aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/certificate/data/Cotizacion.java
@@ -14,41 +14,41 @@ public class Cotizacion {
 	private static final String BASE_COTIZACION_DESEMPLEO = "BaseCotizacionDesempleo";
 	private static final String OBSERVACIONES = "Observaciones";
 
-	private Integer ano;
-	private Integer mes;
-	private Integer numDiasCotizados;
-	private Integer baseCotizacionContingenciasComunes;
-	private Integer baseCotizacionDesempleo;
+	private String ano;
+	private String mes;
+	private String numDiasCotizados;
+	private String baseCotizacionContingenciasComunes;
+	private String baseCotizacionDesempleo;
 	private String observaciones;
 
-	public Integer getAno() {
+	public String getAno() {
 		return ano;
 	}
-	public void setAno(Integer ano) {
+	public void setAno(String ano) {
 		this.ano = ano;
 	}
-	public Integer getMes() {
+	public String getMes() {
 		return mes;
 	}
-	public void setMes(Integer mes) {
+	public void setMes(String mes) {
 		this.mes = mes;
 	}
-	public Integer getNumDiasCotizados() {
+	public String getNumDiasCotizados() {
 		return numDiasCotizados;
 	}
-	public void setNumDiasCotizados(Integer numDiasCotizados) {
+	public void setNumDiasCotizados(String numDiasCotizados) {
 		this.numDiasCotizados = numDiasCotizados;
 	}
-	public Integer getBaseCotizacionContingenciasComunes() {
+	public String getBaseCotizacionContingenciasComunes() {
 		return baseCotizacionContingenciasComunes;
 	}
-	public void setBaseCotizacionContingenciasComunes(Integer baseCotizacionContingenciasComunes) {
+	public void setBaseCotizacionContingenciasComunes(String baseCotizacionContingenciasComunes) {
 		this.baseCotizacionContingenciasComunes = baseCotizacionContingenciasComunes;
 	}
-	public Integer getBaseCotizacionDesempleo() {
+	public String getBaseCotizacionDesempleo() {
 		return baseCotizacionDesempleo;
 	}
-	public void setBaseCotizacionDesempleo(Integer baseCotizacionDesempleo) {
+	public void setBaseCotizacionDesempleo(String baseCotizacionDesempleo) {
 		this.baseCotizacionDesempleo = baseCotizacionDesempleo;
 	}
 	public String getObservaciones() {
@@ -61,26 +61,26 @@ public class Cotizacion {
 		Element datosCotizacion = xmldoc.createElement(DATOS_COTIZACION);
 
 		Element ano = xmldoc.createElement(ANO);
-		ano.appendChild(xmldoc.createTextNode(getAno().toString() ));
+		ano.appendChild(xmldoc.createTextNode(getAno()));
 		datosCotizacion.appendChild(ano);
 
 		Element mes = xmldoc.createElement(MES);
-		mes.appendChild(xmldoc.createTextNode(getMes().toString() ));
+		mes.appendChild(xmldoc.createTextNode(getMes()));
 		datosCotizacion.appendChild(mes);
 		
 		Element numDiasCotizados = xmldoc.createElement(NUM_DIAS_COTIZADOS);
-		numDiasCotizados.appendChild(xmldoc.createTextNode(getNumDiasCotizados().toString() ));
+		numDiasCotizados.appendChild(xmldoc.createTextNode(getNumDiasCotizados() ));
 		datosCotizacion.appendChild(numDiasCotizados);
 		
 		Element baseCotizacionContingenciasComunes = null;
 		if(getBaseCotizacionContingenciasComunes()!=null){
 			baseCotizacionContingenciasComunes = xmldoc.createElement(BASE_COTIZACION_CONTINGENCIAS_COMUNES);
-			baseCotizacionContingenciasComunes.appendChild(xmldoc.createTextNode(getBaseCotizacionContingenciasComunes().toString() ));
+			baseCotizacionContingenciasComunes.appendChild(xmldoc.createTextNode(getBaseCotizacionContingenciasComunes()));
 			datosCotizacion.appendChild(baseCotizacionContingenciasComunes);
 		}
 		
 		Element baseCotizacionDesempleo = xmldoc.createElement(BASE_COTIZACION_DESEMPLEO);
-		baseCotizacionDesempleo.appendChild(xmldoc.createTextNode(getBaseCotizacionDesempleo().toString() ));
+		baseCotizacionDesempleo.appendChild(xmldoc.createTextNode(getBaseCotizacionDesempleo() ));
 		datosCotizacion.appendChild(baseCotizacionDesempleo);
 		
 		Element observaciones = null;
