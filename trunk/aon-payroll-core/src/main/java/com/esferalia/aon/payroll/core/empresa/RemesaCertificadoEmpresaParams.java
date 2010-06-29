e1f27d4ebabca5d1cdba3808c2cdd80789df3c7d
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-06-29 12:33:46 +0000

    dev

---
+++ trunk/aon-payroll-core/src/main/java/com/esferalia/aon/payroll/core/empresa/RemesaCertificadoEmpresaParams.java
@@ -0,0 +1,46 @@
+package com.esferalia.aon.payroll.core.empresa;
+
+import java.io.Serializable;
+
+public class RemesaCertificadoEmpresaParams implements Serializable {
+
+	private static final long serialVersionUID = -3353191952616724056L;
+
+	private String empresa;
+	private String documento;
+	private String nombre;
+	private String apellido;
+	private String apellido2;
+	
+
+	public String getDocumento() {
+		return documento;
+	}
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+}
