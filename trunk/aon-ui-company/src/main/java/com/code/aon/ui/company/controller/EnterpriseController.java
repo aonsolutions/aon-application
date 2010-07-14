c8ee6b4ff801919e25e5975ffa65751d82e38782
Aimar Tellitu <atellitu@esferalia.com>
2010-07-14 13:16:45 +0000

    Se ha cambiado la forma de utlizar WorkActivity y corregido el funcionamiento del arbol de la empresa

--- trunk/aon-ui-company/src/main/java/com/code/aon/ui/company/controller/EnterpriseController.java
+++ trunk/aon-ui-company/src/main/java/com/code/aon/ui/company/controller/EnterpriseController.java
@@ -1,7 +1,64 @@
 package com.code.aon.ui.company.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.company.Enterprise;
+import com.code.aon.company.EnterpriseCCC;
+import com.code.aon.company.dao.ICompanyAlias;
+import com.code.aon.ql.Criteria;
+import com.code.aon.registry.RegistryAddress;
+import com.code.aon.registry.dao.IRegistryAlias;
 import com.code.aon.ui.registry.controller.RegistryController;
 
 public class EnterpriseController extends RegistryController {
+
+    /**
+     * Gets the addresses of the enterprise.
+     * 
+     * @return the addresses of the enterprise
+     * @throws ManagerBeanException 
+     */
+    public List<SelectItem> getAddresses() throws ManagerBeanException {
+    	LinkedList<SelectItem> addresses = new LinkedList<SelectItem>();
+    	Enterprise enterprise = (Enterprise) getTo();
+		IManagerBean registryAddressBean = BeanManager.getManagerBean(RegistryAddress.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(registryAddressBean.getFieldName(IRegistryAlias.REGISTRY_ADDRESS_REGISTRY_ID), enterprise.getRegistry().getId());
+		criteria.addOrder(registryAddressBean.getFieldName(IRegistryAlias.REGISTRY_ADDRESS_ADDRESS));
+		List<ITransferObject> list = registryAddressBean.getList(criteria);
+		for (ITransferObject to : list) {
+			RegistryAddress rAddress = (RegistryAddress)to;
+			addresses.add(new SelectItem(rAddress, rAddress.getAddress()));
+		}
+    	return addresses;
+    }	
+    
+    /**
+     * Gets the CCCs of the enterprise.
+     * 
+     * @return the CCCs of the enterprise
+     * @throws ManagerBeanException 
+     */
+    public List<SelectItem> getCCCs() throws ManagerBeanException {
+    	LinkedList<SelectItem> cccs = new LinkedList<SelectItem>();
+    	Enterprise enterprise = (Enterprise) getTo();
+		IManagerBean bean = BeanManager.getManagerBean(EnterpriseCCC.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(bean.getFieldName(ICompanyAlias.ENTERPRISE_CCC_ENTERPRISE_ID), enterprise.getId());
+		criteria.addOrder(bean.getFieldName(ICompanyAlias.ENTERPRISE_CCC_CCC));
+		List<ITransferObject> list = bean.getList(criteria);
+		for (ITransferObject to : list) {
+			EnterpriseCCC ccc = (EnterpriseCCC)to;
+			cccs.add(new SelectItem(ccc, ccc.getCCC()));
+		}
+    	return cccs;
+    }	    
 
 }
