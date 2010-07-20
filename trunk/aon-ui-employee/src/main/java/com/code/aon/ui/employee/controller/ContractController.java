97be5f941d68f0ebb2cc072f18580a4e80b833b1
Aimar Tellitu <atellitu@esferalia.com>
2010-07-20 10:55:49 +0000

    Primera version

---
+++ trunk/aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/ContractController.java
@@ -0,0 +1,12 @@
+package com.code.aon.ui.employee.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.code.aon.ui.form.BasicController;
+
+public class ContractController extends BasicController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ContractController.class.getName());
+
+}
