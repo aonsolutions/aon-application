43a65d9257928efb981ceeeaf10610de7fbb523f
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-07-30 11:20:05 +0000

    dev

--- trunk/aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/controller/CalendarController.java
+++ trunk/aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/controller/CalendarController.java
@@ -3,15 +3,20 @@ package com.esferalia.aon.ui.calendar.controller;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
+import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 
 import org.richfaces.model.CalendarDataModel;
 
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.enumeration.Month;
 import com.code.aon.ui.form.BasicController;
+import com.code.aon.ui.form.LinesController;
 import com.code.aon.ui.util.AonUtil;
+import com.esferalia.aon.calendar.CalendarPeriod;
 import com.esferalia.aon.calendar.dao.ICalendarAlias;
 import com.esferalia.aon.calendar.enumeration.CalendarSource;
 
@@ -276,6 +281,19 @@ public class CalendarController extends BasicController {
 		} catch (ManagerBeanException e) {
 			AonUtil.addErrorMessage(e.getMessage());
 			throw new AbortProcessingException(e);
+		}
+	}
+
+	public void onChangeMonth(ActionEvent event){
+		Month month = ((CalendarPeriod)((LinesController)AonUtil.getRegisteredBean("calendarPeriod")).getTo()).getMonth();
+		if(month==null){
+			((CalendarPeriod)((LinesController)AonUtil.getRegisteredBean("calendarPeriod")).getTo()).setStartDay(null);
+			((CalendarPeriod)((LinesController)AonUtil.getRegisteredBean("calendarPeriod")).getTo()).setEndDay(null);
+		} else {
+			Calendar cal = new GregorianCalendar(getYear(), month.getValue(), 1);
+			cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+			((CalendarCollections)AonUtil.getRegisteredBean("calendarCollections")).setMonthMaxDays(cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+			((CalendarPeriod)((LinesController)AonUtil.getRegisteredBean("calendarPeriod")).getTo()).setEndDay(cal.getActualMaximum(Calendar.DAY_OF_MONTH));
 		}
 	}
 
