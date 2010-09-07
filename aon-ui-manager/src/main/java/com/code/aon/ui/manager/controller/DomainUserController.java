c3f15eda1fbf73322c8327780ae7773f9ad63df7
Aimar Tellitu <atellitu@esferalia.com>
2010-09-07 16:42:05 +0000

    Primera version del mantenimiento de Usuarios de un Dominio

---
+++ aon-ui-manager/src/main/java/com/code/aon/ui/manager/controller/DomainUserController.java
@@ -0,0 +1,75 @@
+package com.code.aon.ui.manager.controller;
+
+import javax.naming.Name;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.code.aon.common.BasicManagerBean;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.dao.ldap.LdapDAO;
+import com.code.aon.ldap.BasicLdap;
+import com.code.aon.ldap.Entry;
+import com.code.aon.ldap.IAonObjectClasses;
+import com.code.aon.ldap.ILdapConstants;
+import com.code.aon.ldap.LdapException;
+import com.code.aon.ldap.NameResolver;
+import com.code.aon.manager.DomainUser;
+import com.code.aon.ui.form.BasicController;
+
+public class DomainUserController extends BasicController implements IAonObjectClasses {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(DomainUserController.class);
+	
+	private LdapDAO ldapDAO;
+	
+	private BasicManagerBean ldapManagerBean;
+	
+	public DomainUserController() {
+		this.ldapDAO = new LdapDAO(DomainUser.class);		
+		this.ldapManagerBean = new BasicManagerBean(this.ldapDAO);			
+	}
+
+	public void setDomain( String domain ) {
+		Name dasDN = NameResolver.getUsersDN(domain);
+		this.ldapDAO.setBaseDN(dasDN);
+	}
+	
+	@Override
+	public IManagerBean getManagerBean() throws ManagerBeanException {
+		return this.ldapManagerBean;
+	}
+	
+	private void addOrganizationUnit( Name dn ) {
+		BasicLdap ldap = new BasicLdap();
+		try {
+			Entry entry = new Entry(dn);
+			entry.addObjectClasses(new String[]{TOP, ORGANIZATIONAL_UNIT});
+			entry.put( ILdapConstants.ORGANIZATIONAL_UNIT_NAME_ATTRIBUTE, NameResolver.getFirstValue(dn) );
+			LOGGER.info( "Add Organization Unit entry: {}", dn );
+			ldap.getLdapSession().add(entry);
+		} catch ( LdapException e ) {
+			LOGGER.error( e.getMessage(), e);
+		} finally {
+			ldap.closeSession();
+		}
+	}	
+
+	public void createUserWebmailDefaultData( DomainUser user ) {
+		BasicLdap ldap = new BasicLdap();
+		String domain = NameResolver.getValue(user.getId(), 2);
+		Name addressBookDN = NameResolver.getUserAddressBookDN(domain, user.getUid());
+		if (! ldap.exists(addressBookDN, ORGANIZATIONAL_UNIT) ) {
+			addOrganizationUnit(addressBookDN);
+		}
+		Name accountsDN = NameResolver.getUserAccountsDN(domain, user.getUid());
+		if (! ldap.exists(accountsDN, ORGANIZATIONAL_UNIT) ) {
+			addOrganizationUnit(accountsDN);
+		}
+		Name signaturesDN = NameResolver.getUserSignaturesDN(domain, user.getUid());
+		if (! ldap.exists(signaturesDN, ORGANIZATIONAL_UNIT) ) {
+			addOrganizationUnit(signaturesDN);
+		}
+	}
+}
