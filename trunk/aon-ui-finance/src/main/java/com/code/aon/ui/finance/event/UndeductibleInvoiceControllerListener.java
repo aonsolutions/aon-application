d447ec873ed7a7e61c607ad7b749772e5a715bb3
Gorka Irazu <girazu@esferalia.com>
2010-10-21 10:25:03 +0000

    El setStatus se pasa al invoiceController

--- trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/UndeductibleInvoiceControllerListener.java
+++ trunk/aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/UndeductibleInvoiceControllerListener.java
@@ -1,7 +1,6 @@
 package com.code.aon.ui.finance.event;
 
 import com.code.aon.finance.Invoice;
-import com.code.aon.finance.enumeration.InvoiceStatus;
 import com.code.aon.finance.enumeration.InvoiceType;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -12,7 +11,6 @@ public class UndeductibleInvoiceControllerListener extends InvoiceControllerList
 	public void beforeBeanAdded(ControllerEvent event) throws ControllerListenerException {
 		Invoice invoice = (Invoice)event.getController().getTo();
 		invoice.setType(InvoiceType.UNDEDUCTIBLE);
-		invoice.setStatus(InvoiceStatus.PENDING);
 	}
 
 }
