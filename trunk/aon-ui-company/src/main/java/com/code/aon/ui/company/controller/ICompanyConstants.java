e2f92962eacab29ade2455766b0fc4bdcd3ee590
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-10-29 13:16:20 +0000

    dev

--- trunk/aon-ui-company/src/main/java/com/code/aon/ui/company/controller/ICompanyConstants.java
+++ trunk/aon-ui-company/src/main/java/com/code/aon/ui/company/controller/ICompanyConstants.java
@@ -57,6 +57,8 @@ public interface ICompanyConstants {
 	String ENTERPRISE_ACTIVITY_CONTROLLER_NAME = "enterpriseActivity";
 	
 	String EMPLOYEE_CONTROLLER_NAME = "employee";	
+	
+	String CONTRACT_CONTROLLER_NAME = "contract";	
 
 	// ************************************************************
 	// CONFIGURATION
