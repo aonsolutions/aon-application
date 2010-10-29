9f6195f887f5ed6b0219a78197342994c8bf31a7
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-10-29 12:50:52 +0000

    dev

--- aon-employee/src/main/java/com/code/aon/employee/dao/EmployeeAliasWriter.java
+++ aon-employee/src/main/java/com/code/aon/employee/dao/EmployeeAliasWriter.java
@@ -8,6 +8,7 @@ import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.employee.Contract;
 import com.code.aon.employee.ContractTracking;
 import com.code.aon.employee.ContractType;
+import com.code.aon.employee.Salary;
 
 /**
  * @author Consulting & Development. ecastellano - 22/01/2007
@@ -24,7 +25,8 @@ public class EmployeeAliasWriter {
 		String[] classes = new String[] { 
 				Contract.class.getName(),
 				ContractTracking.class.getName(),
-				ContractType.class.getName() };
+				ContractType.class.getName(),
+				Salary.class.getName()};
 		HibernateUtil.getSessionFactory( HibernateUtil.getSessionFactoryName() );
 		AliasWriter writer = new AliasWriter("com.code.aon.employee.dao");
 		writer.write(classes, file);
