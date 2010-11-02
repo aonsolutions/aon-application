bb93f2762b6079370f4b81fc87ba57195edd11de
Raul Trepiana <rtrepiana@esferalia.com>
2010-11-02 09:44:53 +0000

    Enumeraciones para deducciones y percepciones.

---
+++ aon-employee/src/main/java/com/code/aon/employee/enumeration/DeductionType.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.code.aon.employee.enumeration;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import com.code.aon.common.enumeration.IResourceable;
+
+/**
+ * @author rtrepiana
+ *
+ */
+public enum DeductionType implements IResourceable {
+
+	
+	COMMON_CONTINGENCY,
+	UNEMPLOYMENT,
+	JOB_TRAINING,
+	STRUCTURAL_OVERTIME,
+	NON_STRUCTURAL_OVERTIME,
+	IRPF,
+	ADVANCE_PAYMENT,
+	IN_KIND,
+	OTHER
+	;
+	
+	
+	
+	
+	/** Message file base path. */
+    private static final String BASE_NAME = "com.code.aon.employee.i18n.messages";
+    
+    /** Message key prefix. */
+    private static final String MSG_KEY_PREFIX = "aon_enum_deduction_type_";
+
+    /**
+     * Returns a <code>String</code> with the transalation <code>Locale</code>
+     * for the locale.
+     * 
+     * @param locale Required Locale.
+     * 
+     * @return String a <code>String</code>.
+     */
+    public String getName(Locale locale) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
+    }
+	
+}
