9005864880e150e9320572d4409dab2718ff2fbb
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-11-03 15:20:59 +0000

    dev

---
+++ aon-payroll/aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/IEmployeeConstants.java
@@ -0,0 +1,17 @@
+package com.code.aon.ui.employee.controller;
+
+public interface IEmployeeConstants {
+	
+	
+	// ************************************************************
+	// BEAN
+	// ************************************************************
+	String ENTERPRISE_DIR_STAFF_CONTROLLER="enterpriseDirStaff";
+	
+
+	// ************************************************************
+	// CONFIGURATION
+	// ************************************************************
+	String MODEL_PATH = "com/code/aon/ui/employee/contractModel/";
+	
+}
