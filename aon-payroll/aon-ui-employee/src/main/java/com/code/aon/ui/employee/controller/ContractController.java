97bfe95cdfea50736f81f9d829aeba02c74c89c5
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-11-10 17:43:26 +0000

    dev

--- aon-payroll/aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/ContractController.java
+++ aon-payroll/aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/ContractController.java
@@ -27,7 +27,11 @@ import com.code.aon.company.dao.ICompanyAlias;
 import com.code.aon.employee.Contract;
 import com.code.aon.employee.ContractDeduction;
 import com.code.aon.employee.ContractPayment;
+import com.code.aon.employee.SalaryDeduction;
+import com.code.aon.employee.SalaryPayment;
 import com.code.aon.employee.dao.IEmployeeAlias;
+import com.code.aon.employee.enumeration.DeductionType;
+import com.code.aon.employee.enumeration.PaymentType;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.ast.Expression;
 import com.code.aon.ql.util.ExpressionUtilities;
@@ -235,4 +239,85 @@ public class ContractController extends BasicController {
     	String msg = "Creacion de la nomina";
 		AonUtil.addWarningMessage(msg);
     }
+    
+    
+//    private final String SALARY_PAYMENT_CONTROLLER = "salaryPayment";
+//    private final String SALARY_DEDUCTION_CONTROLLER = "salaryDeduction";
+
+	
+//	private ContractPayments payments;
+//	private SalaryDeductions deductions;
+//	
+//	public SalaryPayments getPayments() {
+//		return payments;
+//	}
+//	public void setPayments(SalaryPayments payments) {
+//		this.payments = payments;
+//	}
+//	public SalaryDeductions getDeductions() {
+//		return deductions;
+//	}
+//	public void setDeductions(SalaryDeductions deductions) {
+//		this.deductions = deductions;
+//	}
+//
+//
+//	public void onLoadSalaryReport(ActionEvent event){
+//		setPayments(new SalaryPayments());
+//		getPayments().setSalarySupplements(new LinkedList<SalaryPayment>());
+//		getPayments().setComplementarySuply(new LinkedList<SalaryPayment>());
+//		setDeductions(new SalaryDeductions());
+//		IController pBean = FormUtil.getController(SALARY_PAYMENT_CONTROLLER);
+//		IController dBean = FormUtil.getController(SALARY_DEDUCTION_CONTROLLER);
+//		try {
+//			for(ITransferObject to: pBean.getManagerBean().getList(pBean.getCriteria())){
+//				SalaryPayment sp = (SalaryPayment) to;
+//				if (sp.getType() == PaymentType.BASE_SALARY) {
+//					getPayments().setBaseSalary(sp);
+//				} else if (sp.getType() == PaymentType.SALARY_SUPPLEMENTS) {
+//					 getPayments().getSalarySupplements().add(sp);
+//				} else if (sp.getType() == PaymentType.OVERTIME_HOURS) {
+//					getPayments().setOvertimeHours(sp);
+//				} else if (sp.getType() == PaymentType.SPECIAL_BONUSES) {
+//					getPayments().setSpecialBonuses(sp);
+//				} else if (sp.getType() == PaymentType.SALARY_IN_KIND) {
+//					getPayments().setSalaryInKid(sp);
+//				} else if (sp.getType() == PaymentType.COMPENSATION_SUPLY) {
+//					getPayments().getComplementarySuply().add(sp);
+//				} else if (sp.getType() == PaymentType.SOCIAL_SECURITY_BENEFITS) {
+//					getPayments().setSpecialSecurityBenefits(sp);
+//				} else if (sp.getType() == PaymentType.MOVING_COMPENSATION) {
+//					getPayments().setMovingCompensation(sp);
+//				} else if (sp.getType() == PaymentType.OTHER_NON_WAGE) {
+//					getPayments().setOtherNonWage(sp);
+//				}
+//			}
+//
+//			for(ITransferObject to: dBean.getManagerBean().getList(dBean.getCriteria())){
+//				SalaryDeduction sd = (SalaryDeduction) to;
+//				if (sd.getType() == DeductionType.COMMON_CONTINGENCY) {
+//					getDeductions().setCommonContingency(sd);
+//				} else if (sd.getType() == DeductionType.UNEMPLOYMENT) {
+//					getDeductions().setUnemployment(sd);
+//				} else if (sd.getType() == DeductionType.JOB_TRAINING) {
+//					getDeductions().setJobTraining(sd);
+//				} else if (sd.getType() == DeductionType.STRUCTURAL_OVERTIME) {
+//					getDeductions().setStructuralOvertime(sd);
+//				} else if (sd.getType() == DeductionType.NON_STRUCTURAL_OVERTIME) {
+//					getDeductions().setNonStructuralOvertime(sd);
+//				} else if (sd.getType() == DeductionType.IRPF) {
+//					getDeductions().setIrpf(sd);
+//				} else if (sd.getType() == DeductionType.ADVANCE_PAYMENT) {
+//					getDeductions().setAdvancePayment(sd);
+//				} else if (sd.getType() == DeductionType.IN_KIND) {
+//					getDeductions().setInKid(sd);
+//				} else if (sd.getType() == DeductionType.OTHER) {
+//					getDeductions().setOther(sd);
+//				}
+//			}
+//		} catch (ManagerBeanException e) {
+//			LOGGER.error(e.getMessage(), e);
+//		}
+//	}
+    
 }
