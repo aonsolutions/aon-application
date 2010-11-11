590ca4b8e29d607a7d5e4ac3ee98ef967cd6c28d
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-11-11 16:51:52 +0000

    dev

---
+++ aon-employee/src/main/java/com/code/aon/employee/Payments.java
@@ -0,0 +1,74 @@
+package com.code.aon.employee;
+
+import java.util.List;
+
+public class Payments {
+
+	private IPayment baseSalary;
+	private List<IPayment> salarySupplements;
+	private IPayment overtimeHours;
+	private IPayment specialBonuses;
+	private IPayment salaryInKid;
+	private List<IPayment> complementarySuply;
+	private IPayment specialSecurityBenefits;
+	private IPayment movingCompensation;
+	private IPayment otherNonWage;
+
+	
+	public IPayment getSalaryInKid() {
+		return salaryInKid;
+	}
+	public void setSalaryInKid(IPayment salaryInKid) {
+		this.salaryInKid = salaryInKid;
+	}
+	public IPayment getBaseSalary() {
+		return baseSalary;
+	}
+	public void setBaseSalary(IPayment baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	public List<IPayment> getSalarySupplements() {
+		return salarySupplements;
+	}
+	public void setSalarySupplements(List<IPayment> salarySupplements) {
+		this.salarySupplements = salarySupplements;
+	}
+	public IPayment getOvertimeHours() {
+		return overtimeHours;
+	}
+	public void setOvertimeHours(IPayment overtimeHours) {
+		this.overtimeHours = overtimeHours;
+	}
+	public IPayment getSpecialBonuses() {
+		return specialBonuses;
+	}
+	public void setSpecialBonuses(IPayment specialBonuses) {
+		this.specialBonuses = specialBonuses;
+	}
+	public List<IPayment> getComplementarySuply() {
+		return complementarySuply;
+	}
+	public void setComplementarySuply(List<IPayment> complementarySuply) {
+		this.complementarySuply = complementarySuply;
+	}
+	public IPayment getSpecialSecurityBenefits() {
+		return specialSecurityBenefits;
+	}
+	public void setSpecialSecurityBenefits(IPayment specialSecurityBenefits) {
+		this.specialSecurityBenefits = specialSecurityBenefits;
+	}
+	public IPayment getMovingCompensation() {
+		return movingCompensation;
+	}
+	public void setMovingCompensation(IPayment movingCompensation) {
+		this.movingCompensation = movingCompensation;
+	}
+	public IPayment getOtherNonWage() {
+		return otherNonWage;
+	}
+	public void setOtherNonWage(IPayment otherNonWage) {
+		this.otherNonWage = otherNonWage;
+	}
+	
+
+}
