acf57af211aba864e68348e790c719ef7bc8fe7f
Raul Trepiana <rtrepiana@esferalia.com>
2010-11-18 09:12:39 +0000

    Merged revisions 14159-14161 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r14159 | girazu | 2010-11-18 08:38:04 +0100 (Thu, 18 Nov 2010) | 1 line
      
      Version 5.6.1
    ........
      r14160 | girazu | 2010-11-18 09:39:49 +0100 (Thu, 18 Nov 2010) | 1 line
      
      Corregidos errores de la parte del Commercial reportados por Julio para sacar version 5.6.0. Aparte, en la ficha de Clientes y Clientes potenciales, habilitar ciertas pestañas (valor true en el aon-config.xml)
    ........
      r14161 | girazu | 2010-11-18 09:47:56 +0100 (Thu, 18 Nov 2010) | 1 line
      
      Cambios necesarios para la version 2.0 del Extracto Bancario.
    ........

--- aon-account-bridge/src/main/java/com/code/aon/account/bridge/LoanAccount.java
+++ aon-account-bridge/src/main/java/com/code/aon/account/bridge/LoanAccount.java
@@ -9,6 +9,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.hibernate.annotations.ForeignKey;
 import org.hibernate.annotations.Index;
 
@@ -16,6 +19,7 @@ import com.code.aon.account.Account;
 import com.code.aon.account.IAccount;
 import com.code.aon.accounting.Loan;
 import com.code.aon.common.ITransferObject;
+import com.code.aon.common.dao.hibernate.PojoToStringBuilder;
 
 @Entity
 @Table(name="loan_account")
@@ -24,11 +28,8 @@ public class LoanAccount implements ITransferObject, IAccount {
 	private static final long serialVersionUID = 4415920212702994723L;
 
 	private Integer id;
-	
 	private Loan loan;
-	
 	private Account account;
-
 
 	@Id
 	@Column(nullable=false)
@@ -36,31 +37,28 @@ public class LoanAccount implements ITransferObject, IAccount {
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
 	@ManyToOne
-	@JoinColumn( name="loan", nullable = false)
+	@JoinColumn(name="loan", nullable = false)
 	@ForeignKey(name="FK_LOAN_ACCOUNT_LOAN")
 	@Index(name="IDX_LOAN_ACCOUNT_LOAN")								
 	public Loan getLoan() {
 		return loan;
 	}
-
 	public void setLoan(Loan loan) {
 		this.loan = loan;
 	}
 
 	@ManyToOne
-	@JoinColumn( name="account", nullable = false)
+	@JoinColumn(name="account", nullable = false)
 	@ForeignKey(name="FK_LOAN_ACCOUNT_ACCOUNT")
 	@Index(name="IDX_LOAN_ACCOUNT_ACCOUNT")							
 	public Account getAccount() {
 		return account;
 	}
-
 	public void setAccount(Account account) {
 		this.account = account;
 	}
@@ -72,9 +70,39 @@ public class LoanAccount implements ITransferObject, IAccount {
 	public void setLinkedTo(ITransferObject to) {
 		setLoan((Loan) to);
 	}	
+
 	@Transient
 	public String getAccountDescription() {
-		return getLoan()==null?null:getLoan().getDescription();
+		return (getLoan()==null) ? null : getLoan().getDescription();
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final LoanAccount o = (LoanAccount) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.account,o.account)
+				.append(this.loan,o.loan)
+				.isEquals();
+		}
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(id)		
+			.append(this.account)
+			.append(this.loan)
+			.toHashCode();
+	}	
+
+	@Override
+	public String toString() {
+		return new PojoToStringBuilder(this).toString();
+	}
+
 }
