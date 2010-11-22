c49b5667e4afd5790f1348064fddc079ee88b398
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-11-22 12:23:43 +0000

    dev

--- aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/event/CalendarControllerListener.java
+++ aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/event/CalendarControllerListener.java
@@ -1,8 +1,16 @@
 package com.esferalia.aon.ui.calendar.event;
 
+import javax.faces.event.AbortProcessingException;
+
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.company.Enterprise;
+import com.code.aon.company.WorkPlace;
+import com.code.aon.employee.Contract;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.aon.ui.util.AonUtil;
+import com.esferalia.aon.ui.calendar.controller.CalendarController;
 
 public class CalendarControllerListener extends ControllerAdapter {
 
@@ -12,10 +20,34 @@ public class CalendarControllerListener extends ControllerAdapter {
 	}
 	
 	@Override
+	public void beforeBeanCreated(ControllerEvent event)
+			throws ControllerListenerException {
+		CalendarController c = (CalendarController) getController();
+		c.setEnterprise(new Enterprise());
+		c.setWorkPlace(new WorkPlace());
+		c.setWorkPlaces(null);
+		c.setContract(new Contract());
+	}
+	
+	@Override
 	public void beforeModelInitialized(ControllerEvent event)
 			throws ControllerListenerException {
 		
 	}	
+	
+	@Override
+	public void afterBeanSelected(ControllerEvent event)
+			throws ControllerListenerException {
+		CalendarController c = (CalendarController) getController();
+		c.setSource(c.getTo().getSource());
+		c.setSourceId(c.getTo().getSourceId());
+		try {
+			c.loadSource();
+		} catch (ManagerBeanException e) {
+			AonUtil.addErrorMessage(e.getMessage());
+			throw new AbortProcessingException(e);
+		}
+	}
 	
 	
 
