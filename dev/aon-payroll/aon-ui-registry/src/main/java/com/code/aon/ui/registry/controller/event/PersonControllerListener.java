9381a684950f41c3816579ffa2568c6d5cd59fdd
Aimar Tellitu <atellitu@esferalia.com>
2010-11-23 11:20:55 +0000

    Se ha organizado un poco las plantillas nuevas

--- dev/aon-payroll/aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/PersonControllerListener.java
+++ dev/aon-payroll/aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/PersonControllerListener.java
@@ -1,8 +1,5 @@
 package com.code.aon.ui.registry.controller.event;
 
-import com.code.aon.common.enumeration.Country;
-import com.code.aon.person.Person;
-import com.code.aon.registry.enumeration.DocumentType;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -17,10 +14,7 @@ public class PersonControllerListener extends ControllerAdapter {
 	@Override
 	public void afterBeanCreated(ControllerEvent event) throws ControllerListenerException {
 		PersonController c = (PersonController)event.getController();
-		Person person = (Person) c.getTo();
-		person.getRegistry().setNationality(Country.ES);
-		person.getRegistry().setDocumentCountry(Country.ES);
-		person.getRegistry().setDocumentType(DocumentType.CIF);
+		c.initDocument();
 	}
 
 }
