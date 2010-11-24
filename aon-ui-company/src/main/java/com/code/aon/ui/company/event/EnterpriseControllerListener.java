c2a8f4d87b16fa047a58a6ca82ce6d8f00445eb5
Aimar Tellitu <atellitu@esferalia.com>
2010-11-24 17:29:13 +0000

    Cambios en Enterprise

--- aon-ui-company/src/main/java/com/code/aon/ui/company/event/EnterpriseControllerListener.java
+++ aon-ui-company/src/main/java/com/code/aon/ui/company/event/EnterpriseControllerListener.java
@@ -3,19 +3,13 @@ package com.code.aon.ui.company.event;
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.company.Company;
 import com.code.aon.company.Enterprise;
-import com.code.aon.company.EnterpriseCCC;
 import com.code.aon.company.WorkPlace;
-import com.code.aon.customer.Customer;
-import com.code.aon.geozone.GeoZone;
 import com.code.aon.registry.RegistryAddress;
 import com.code.aon.registry.enumeration.RegistryType;
-import com.code.aon.ui.company.controller.CompanyController;
 import com.code.aon.ui.company.controller.EnterpriseController;
 import com.code.aon.ui.company.controller.EnterpriseTree;
 import com.code.aon.ui.company.controller.ICompanyConstants;
-import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
@@ -31,18 +25,10 @@ public class EnterpriseControllerListener extends ControllerAdapter {
 	public void afterBeanCreated(ControllerEvent event)
 			throws ControllerListenerException {
 		EnterpriseController controller = (EnterpriseController) event.getController();
-		try {
-			Enterprise enterprise = (Enterprise) controller.getTo();
-			enterprise.getRegistry().setType(RegistryType.LEGAL);
-			controller.initDocument();
-			controller.initMainActiviy();			
-			controller.initMainAddress();			
-			controller.initMedias();			
-			controller.initMainWorkPlace();			
-			controller.initMainDirStaff();			
-		} catch (ManagerBeanException e) {
-			throw new ControllerListenerException(e.getMessage(),e);
-		}
+		Enterprise enterprise = (Enterprise) controller.getTo();
+		enterprise.getRegistry().setType(RegistryType.LEGAL);
+		controller.initDocument();
+		controller.reset();
 	}
 
 	@Override
@@ -50,9 +36,9 @@ public class EnterpriseControllerListener extends ControllerAdapter {
 			throws ControllerListenerException {
 		EnterpriseController controller = (EnterpriseController) event.getController();
 		try {
+			controller.reset();
 			controller.initMainActiviy();			
-			controller.initMainAddress();			
-			controller.initMedias();			
+			controller.initRegistryInfo();					
 			controller.initMainWorkPlace();			
 			controller.initMainDirStaff();			
 		} catch (ManagerBeanException e) {
@@ -61,9 +47,6 @@ public class EnterpriseControllerListener extends ControllerAdapter {
 		if ( controller.isTreeView() ) {
 			EnterpriseTree tree = (EnterpriseTree) AonUtil.getRegisteredBean(ICompanyConstants.ENTERPRISE_TREE_CONTROLLER_NAME);
 			tree.loadTree();			
-		} else {
-			BasicController enterpriseActivity = (BasicController) AonUtil.getRegisteredBean(ICompanyConstants.ENTERPRISE_ACTIVITY_CONTROLLER_NAME);
-			enterpriseActivity.onSelectFirst(null);			
 		}
 	}
 
@@ -73,28 +56,11 @@ public class EnterpriseControllerListener extends ControllerAdapter {
 		EnterpriseController controller = (EnterpriseController) event.getController();
 		Enterprise enterprise = (Enterprise) controller.getTo();
 		try {
-			fillData( controller );
-			controller.saveMainActivity();			
-			controller.saveMainAddress();			
-			controller.saveMedias();			
-			controller.saveMainWorkPlace();			
-			controller.saveMainDirStaff();			
+			controller.initRegistryInfo();
+			WorkPlace workPlace = insertWorkPlace(enterprise, controller.getMainAddress() );
+			controller.setWorkplace(workPlace);
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException(e.getMessage(),e);
-		}
-
-		CompanyController companyController = (CompanyController) AonUtil.getRegisteredBean(ICompanyConstants.COMPANY_CONTROLLER_NAME);
-		Company company = companyController.obtainCompany();
-		if (! company.getId().equals(enterprise.getId()) ) {
-			try {
-				IManagerBean bean = BeanManager.getManagerBean(Customer.class);
-				Customer customer = new Customer();
-				customer.setRegistry(enterprise.getRegistry());
-				customer.setScope(enterprise.getScope());
-				bean.insert(customer);
-			} catch (ManagerBeanException e) {
-				throw new ControllerListenerException("Error creating customer from enterprise " + enterprise,e);
-			}
 		}
 	}
 
@@ -103,26 +69,21 @@ public class EnterpriseControllerListener extends ControllerAdapter {
 			throws ControllerListenerException {
 		EnterpriseController controller = (EnterpriseController) event.getController();
 		try {
-			controller.saveMainActivity();			
-			controller.saveMainAddress();			
-			controller.saveMedias();			
-			controller.saveMainWorkPlace();			
-			controller.saveMainDirStaff();			
+			controller.saveMainAddress();
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException(e.getMessage(),e);
 		}
 	}
 	
-	private void fillData( EnterpriseController controller ) {
-		RegistryAddress address = controller.getMainAddress();
-		EnterpriseCCC ccc = controller.getCCC();
-		if ( ccc.getGeozone() == null ) {
-			GeoZone geoZone = address.getGeozone();
-			ccc.setGeozone( geoZone );
-		}
-		WorkPlace workPlace = controller.getWorkplace();
+	private WorkPlace insertWorkPlace( Enterprise enterprise, RegistryAddress address ) throws ManagerBeanException {
+		WorkPlace workPlace = new WorkPlace();
+		workPlace.setEnterprise( enterprise );
+		workPlace.setActive( true );
 		workPlace.setAddress( address );
 		workPlace.setDescription( address.getFullAddress() );
+		IManagerBean bean = BeanManager.getManagerBean(WorkPlace.class);
+		bean.insert( workPlace );
+		return workPlace;
 	}
 	
 }
