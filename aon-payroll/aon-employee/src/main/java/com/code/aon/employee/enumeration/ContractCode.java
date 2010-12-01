419756e8877bfa27fce8f15f2657f1e0d26fe052
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-12-01 15:23:40 +0000

    dev

--- aon-payroll/aon-employee/src/main/java/com/code/aon/employee/enumeration/ContractCode.java
+++ aon-payroll/aon-employee/src/main/java/com/code/aon/employee/enumeration/ContractCode.java
@@ -4,56 +4,57 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 import com.code.aon.common.enumeration.IResourceable;
+import com.code.aon.common.enumeration.IStringEnum;
 
-public enum ContractCode implements IResourceable {
+public enum ContractCode implements IResourceable, IStringEnum {
 
-	C100,
-	C109,
-	C130,
-	C139,
-	C150,
-	C189,
+	C100("100"),
+	C109("109"),
+	C130("130"),
+	C139("139"),
+	C150("150"),
+	C189("189"),
 	
-	C200,
-	C209,
-	C230,
-	C239,
-	C250,
-	C289,
+	C200("200"),
+	C209("209"),
+	C230("230"),
+	C239("239"),
+	C250("250"),
+	C289("289"),
 	
-	C300,
-	C309,
-	C330,
-	C350,
-	C389,
+	C300("300"),
+	C309("309"),
+	C330("330"),
+	C350("350"),
+	C389("389"),
 	
-	C401,
-	C402,
-	C403,
-	C408,
-	C410,
-	C418,
-	C420,
-	C421,
-	C430,
-	C441,
-	C450,
-	C452,
+	C401("401"),
+	C402("402"),
+	C403("403"),
+	C408("408"),
+	C410("410"),
+	C418("418"),
+	C420("420"),
+	C421("421"),
+	C430("430"),
+	C441("441"),
+	C450("450"),
+	C452("452"),
 	
-	C501,
-	C502,
-	C503,
-	C508,
-	C510,
-	C518,
-	C520,
-	C530,
-	C540,
-	C541,
-	C550,
-	C552, 
+	C501("501"),
+	C502("502"),
+	C503("503"),
+	C508("508"),
+	C510("510"),
+	C518("518"),
+	C520("520"),
+	C530("530"),
+	C540("540"),
+	C541("541"),
+	C550("550"),
+	C552("552"), 
 
-	C990,
+	C990("990")
 	
 	;
 
@@ -70,10 +71,16 @@ public enum ContractCode implements IResourceable {
 		return bundle.getString(MSG_KEY_PREFIX + toString());
 	}
 	
-	@Override
-	public String toString() {
-		String name = this.name();
-		return name.substring(1);
+	private String value;
+    
+    ContractCode( String value ) {
+      	this.value = value;
+  	}
+	    
+    @Override
+	public String getValue() {
+		return value;
 	}
+	
 	
 }
