a583f2d2afb2dd15c19d9697e65355413fbed10c
Euke Castellano <ecastellano@esferalia.com>
2010-12-03 10:55:36 +0000

    dev

--- dev/aon-payroll/aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/SalaryPrintController.java
+++ dev/aon-payroll/aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/SalaryPrintController.java
@@ -1,5 +1,6 @@
 package com.code.aon.ui.employee.controller;
 
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -28,6 +29,7 @@ import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.enumeration.MimeType;
+import com.code.aon.common.util.AonFile;
 import com.code.aon.company.Enterprise;
 import com.code.aon.company.WorkPlace;
 import com.code.aon.company.dao.ICompanyAlias;
@@ -43,7 +45,6 @@ import com.code.aon.ui.company.controller.ICompanyConstants;
 import com.code.aon.ui.report.controller.ReportManager;
 import com.code.aon.ui.util.AonUtil;
 import com.code.aon.ui.webmail.controller.MessageController;
-import com.code.aon.webmail.AonFile;
 
 public class SalaryPrintController implements ICollectionProvider, IEmployeeConstants {
 	
