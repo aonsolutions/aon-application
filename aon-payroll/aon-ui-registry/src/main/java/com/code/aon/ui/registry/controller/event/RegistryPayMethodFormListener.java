75477b23d0d67130f476322830a0523c469bf71f
Raul Trepiana <rtrepiana@esferalia.com>
2010-12-03 12:13:41 +0000

    Merged revisions 13951-13952,14127 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r13951 | girazu | 2010-11-02 17:08:58 +0100 (Tue, 02 Nov 2010) | 1 line
      
      Al cambiar de forma de pago que se resetee el numero de vencimientos a 1.
    ........
      r13952 | girazu | 2010-11-02 17:09:54 +0100 (Tue, 02 Nov 2010) | 1 line
      
      Listener de rpaymethod, para que al dar nueva forma de pago se ponga el numero de vencimientos a 1.
    ........
      r14127 | girazu | 2010-11-17 09:21:40 +0100 (Wed, 17 Nov 2010) | 1 line
      
      Mensaje de Trademark Comercial.
    ........

--- aon-payroll/aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/RegistryPayMethodFormListener.java
+++ aon-payroll/aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/RegistryPayMethodFormListener.java
@@ -95,6 +95,7 @@ public class RegistryPayMethodFormListener extends RegistryFormListener {
 		PayMethod newPay = (PayMethod) event.getNewValue();
 		if (oldPay == null || newPay == null || oldPay.getType() != newPay.getType()) {
 			resetRegistryBank();
+			this.registryPayMethod.setNumberOfPayments(1);
 		}
 	}
 	
