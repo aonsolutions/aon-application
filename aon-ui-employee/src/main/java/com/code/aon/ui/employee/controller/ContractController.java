58c4cd7d9f62a8d4f1db0c486ac08afd95598095
Euke Castellano <ecastellano@esferalia.com>
2010-12-08 19:05:50 +0000

    dev

--- aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/ContractController.java
+++ aon-ui-employee/src/main/java/com/code/aon/ui/employee/controller/ContractController.java
@@ -3,135 +3,71 @@ package com.code.aon.ui.employee.controller;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
-import javax.faces.model.SelectItem;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.code.aon.common.BeanManager;
-import com.code.aon.common.IManagerBean;
-import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.enumeration.MimeType;
-import com.code.aon.company.Enterprise;
-import com.code.aon.company.EnterpriseCCC;
-import com.code.aon.company.WorkPlace;
-import com.code.aon.company.dao.ICompanyAlias;
 import com.code.aon.employee.Contract;
-import com.code.aon.ql.Criteria;
-import com.code.aon.ui.common.components.LookupChangeEvent;
+import com.code.aon.employee.dao.IEmployeeAlias;
 import com.code.aon.ui.company.controller.EnterpriseTree;
 import com.code.aon.ui.company.controller.ICompanyConstants;
 import com.code.aon.ui.form.BasicController;
+import com.code.aon.ui.form.FormUtil;
 import com.code.aon.ui.util.AonUtil;
 
 public class ContractController extends BasicController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ContractController.class.getName());
 	
-	private Enterprise enterprise;
-	private List<SelectItem> workPlaces;
-	private List<SelectItem> EnterpriseCccs;
-	
-	public ContractController() {
-		this.workPlaces = new LinkedList<SelectItem>();
-		this.EnterpriseCccs = new LinkedList<SelectItem>();
-	}
-
-	public Enterprise getEnterprise() {
-		return enterprise;
-	}
-
-	public void setEnterprise(Enterprise enterprise) {
-		this.enterprise = enterprise;
-		if ( isEnterpriseSelected() ) {
-			try {
-				this.EnterpriseCccs = loadCCCs();
-				this.workPlaces = loadWorkPlaces();
-			} catch (ManagerBeanException e) {
-				LOGGER.error( "Error in setEnterprise " + enterprise, e);
-			}			
-		} else {
-			this.EnterpriseCccs.clear();
-			this.workPlaces.clear();
+	public void onDownloadSelected( ActionEvent event ) {
+		try {
+			Contract c = (Contract) getTo();
+			download(c);
+		} catch (IOException e) {
+			String msg = "Imposible descargar el Contrato. (" +e.getMessage() + ")"; 
+			LOGGER.error(msg, e);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg,e);
 		}
 	}
 	
-	public boolean isEnterpriseSelected() {
-		return (enterprise != null) && (enterprise.getId() != null);
-	}
-	
-	public List<SelectItem> getWorkPlaces() {
-		return workPlaces;
-	}
-
-	public List<SelectItem> getEnterpriseCccs() {
-		return EnterpriseCccs;
-	}
-
-	public void onEnterpriseChanged( LookupChangeEvent event ) {
-		if ( event.getNewValue() != null ) {
-			setEnterprise( (Enterprise) event.getNewValue() );
-		} else {
-			setEnterprise( new Enterprise() );
-		}
-	}
-	
-    public List<SelectItem> loadCCCs() throws ManagerBeanException {
-    	LinkedList<SelectItem> cccs = new LinkedList<SelectItem>();
-		IManagerBean bean = BeanManager.getManagerBean(EnterpriseCCC.class);
-		Criteria criteria = new Criteria();
-		criteria.addEqualExpression(bean.getFieldName(ICompanyAlias.ENTERPRISE_CCC_ACTIVITY_ENTERPRISE_ID), enterprise.getId());
-		criteria.addOrder(bean.getFieldName(ICompanyAlias.ENTERPRISE_CCC_CCC));
-		List<ITransferObject> list = bean.getList(criteria);
-		for (ITransferObject to : list) {
-			EnterpriseCCC ccc = (EnterpriseCCC)to;
-			cccs.add(new SelectItem(ccc, ccc.getCcc()));
-		}
-    	return cccs;
-    }	  	
-
-    public List<SelectItem> loadWorkPlaces() throws ManagerBeanException {
-    	LinkedList<SelectItem> workPlaces = new LinkedList<SelectItem>();
-		IManagerBean bean = BeanManager.getManagerBean(WorkPlace.class);
-		Criteria criteria = new Criteria();
-		criteria.addEqualExpression(bean.getFieldName(ICompanyAlias.WORK_PLACE_ENTERPRISE_ID), enterprise.getId());
-		criteria.addOrder(bean.getFieldName(ICompanyAlias.WORK_PLACE_DESCRIPTION));
-		List<ITransferObject> list = bean.getList(criteria);
-		for (ITransferObject to : list) {
-			WorkPlace workPlace = (WorkPlace) to;
-			workPlaces.add(new SelectItem(workPlace, workPlace.getDescription()));
-		}
-    	return workPlaces;
-    }	  	
-    
-    public void onDownloadContract( ActionEvent event ) {
-		FacesContext context = FacesContext.getCurrentInstance();
-		HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+	public void onDownloadContract( ActionEvent event ) {
 		try {
 			Contract c = (Contract)this.getModel().getRowData();
-			byte[] buffer = c.getDocument();
-			InputStream in = new ByteArrayInputStream(buffer);
-			int bytes = in.read(buffer);
-			while (bytes != -1) {
-				response.getOutputStream().write(buffer, 0, bytes);
-				bytes = in.read(buffer);
-			}
-			in.close();
-			response.setContentType(MimeType.MIME_PDF.getName()); 
-			response.flushBuffer();
+			download(c);
 		} catch (IOException e) {
-			LOGGER.error(e.getMessage(), e);
+			String msg = "Imposible descargar el Contrato. (" +e.getMessage() + ")"; 
+			LOGGER.error(msg, e);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg,e);
 		} catch (ManagerBeanException e) {
-			LOGGER.error(e.getMessage(), e);
+			String msg = "Imposible descargar el Contrato. (" +e.getMessage() + ")"; 
+			LOGGER.error(msg, e);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg,e);
 		}
+	}
+
+	private void download(Contract c) throws IOException {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+		byte[] buffer = c.getDocument();
+		InputStream in = new ByteArrayInputStream(buffer);
+		int bytes = in.read(buffer);
+		while (bytes != -1) {
+			response.getOutputStream().write(buffer, 0, bytes);
+			bytes = in.read(buffer);
+		}
+		in.close();
+		response.setContentType(MimeType.MIME_PDF.getName()); 
+		response.flushBuffer();
 		context.responseComplete();
 	}
     
@@ -145,5 +81,38 @@ public class ContractController extends BasicController {
 			throw new AbortProcessingException(e.getMessage(), e);
 		}						
 	}    
-	
+	public void onShowPayments( ActionEvent event ) {
+		try {
+			Contract to = (Contract) getTo();
+			ContractPaymentController c = (ContractPaymentController) FormUtil.getController("contractPayment");
+			c.reset(false);
+			c.onEditSearch(event);
+			c.getCriteria().addEqualExpression(c.getFieldName(IEmployeeAlias.CONTRACT_PAYMENT_CONTRACT_ID), to.getId());
+			c.onSearch(event);
+		} catch (ManagerBeanException e) {
+			String msg = "Imposible mostrar las percepciones del contrato (" + e.getMessage() +")";
+			LOGGER.error(msg);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg,e);
+		}						
+	}
+	public void onShowDeductions( ActionEvent event ) {
+		try {
+			Contract to = (Contract) getTo();
+			ContractDeductionController c = (ContractDeductionController) FormUtil.getController("contractDeduction");
+			c.reset(false);
+			c.onEditSearch(event);
+			c.getCriteria().addEqualExpression(c.getFieldName(IEmployeeAlias.CONTRACT_DEDUCTION_CONTRACT_ID), to.getId());
+			c.onSearch(event);
+		} catch (ManagerBeanException e) {
+			String msg = "Imposible mostrar las deducciones del contrato (" + e.getMessage() +")";
+			LOGGER.error(msg);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg,e);
+		}						
+	}
+	public void onShowDetails( ActionEvent event ) {
+		AonUtil.addWarningMessage("No implementado");
+		throw new AbortProcessingException("No implementado");
+	}
 }
