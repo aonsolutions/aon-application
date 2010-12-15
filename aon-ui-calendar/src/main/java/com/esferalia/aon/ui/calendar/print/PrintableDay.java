6b47fea927798f285b978cd30371f6267eebb7aa
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-12-15 18:10:50 +0000

    dev

--- aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/print/PrintableDay.java
+++ aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/print/PrintableDay.java
@@ -4,7 +4,9 @@ package com.esferalia.aon.ui.calendar.print;
 public class PrintableDay {
 	
 	private Integer dayOfMonth;
+	private boolean weekEnd;
 	private boolean holiday;
+	private boolean period;
 	
 	public boolean isHoliday() {
 		return holiday;
@@ -18,7 +20,18 @@ public class PrintableDay {
 	public void setDayOfMonth(Integer dayOfMonth) {
 		this.dayOfMonth = dayOfMonth;
 	}
+	public boolean isWeekEnd() {
+		return weekEnd;
+	}
+	public void setWeekEnd(boolean weekEnd) {
+		this.weekEnd = weekEnd;
+	}
+	public boolean isPeriod() {
+		return period;
+	}
+	public void setPeriod(boolean period) {
+		this.period = period;
+	}
 	
-		
 	
 }
