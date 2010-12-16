ca61c6694fd7189318b945835a792a228e69860e
Aimar Tellitu <atellitu@esferalia.com>
2010-12-16 18:39:53 +0000

    Clase para las constantes

---
+++ aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/controller/ICalendarConstants.java
@@ -0,0 +1,19 @@
+package com.esferalia.aon.ui.calendar.controller;
+
+public interface ICalendarConstants {
+
+	// ************************************************************
+	// MESSAGES
+	// ************************************************************
+
+	/** ResourceBundle name. */
+	String BUNDLE_NAME = "calendarBundle";
+	
+	// ************************************************************
+	// BEAN
+	// ************************************************************
+	String COLLECTIONS_CONTROLLER_NAME = "calendarCollections";
+
+	String CALENDAR_CONTROLLER_NAME = "calendar";	
+	
+}
