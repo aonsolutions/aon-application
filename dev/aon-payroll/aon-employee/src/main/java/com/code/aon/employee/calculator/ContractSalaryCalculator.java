a48dbafbbbe7fcb0cb1ad1d1d5d83cba4a174f44
Euke Castellano <ecastellano@esferalia.com>
2010-12-16 15:01:51 +0000

    dev

--- dev/aon-payroll/aon-employee/src/main/java/com/code/aon/employee/calculator/ContractSalaryCalculator.java
+++ dev/aon-payroll/aon-employee/src/main/java/com/code/aon/employee/calculator/ContractSalaryCalculator.java
@@ -150,7 +150,6 @@ public class ContractSalaryCalculator implements ISalaryCalculator{
 	}
 
 	private void fillSystemExpressions(SalaryCalculatorContext ctx) {
-/*		
 		ExpressionImpl e = new ExpressionImpl();
 		e.setName("dias_mes");
 		e.setScope(ExpressionScope.SYSTEM);
@@ -165,7 +164,6 @@ public class ContractSalaryCalculator implements ISalaryCalculator{
 		Date endDate = contract.getEndDate() != null && contract.getEndDate().before( ctx.getEndDate() )?contract.getEndDate():ctx.getEndDate();
 		e.setExpression(Long.toString(CommonUtil.getDaysBetweenDates(startDate, endDate) + 1));
 		ctx.getExpressionContext().put(e);
-*/
 }
 
 }
