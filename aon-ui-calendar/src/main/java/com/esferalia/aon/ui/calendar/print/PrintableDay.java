da4d9cca95b4ca5642721dea52a5975d928ead85
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-12-16 18:02:51 +0000

    dev

--- aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/print/PrintableDay.java
+++ aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/print/PrintableDay.java
@@ -4,33 +4,62 @@ package com.esferalia.aon.ui.calendar.print;
 public class PrintableDay {
 	
 	private Integer dayOfMonth;
-	private boolean weekEnd;
 	private boolean holiday;
-	private boolean period;
-	
-	public boolean isHoliday() {
-		return holiday;
-	}
-	public void setHoliday(boolean holiday) {
-		this.holiday = holiday;
-	}
+	private boolean notWorkingDay;
+	private boolean vacation;
+	private boolean continuousTime;
+	private boolean other;
+	private Double hours;
+
 	public Integer getDayOfMonth() {
 		return dayOfMonth;
 	}
 	public void setDayOfMonth(Integer dayOfMonth) {
 		this.dayOfMonth = dayOfMonth;
 	}
-	public boolean isWeekEnd() {
-		return weekEnd;
+	public boolean isHoliday() {
+		return holiday;
 	}
-	public void setWeekEnd(boolean weekEnd) {
-		this.weekEnd = weekEnd;
+	public void setHoliday(boolean holiday) {
+		this.holiday = holiday;
 	}
-	public boolean isPeriod() {
-		return period;
+	public boolean isNotWorkingDay() {
+		return notWorkingDay;
 	}
-	public void setPeriod(boolean period) {
-		this.period = period;
+	public void setNotWorkingDay(boolean notWorkingDay) {
+		this.notWorkingDay = notWorkingDay;
+	}
+	public boolean isVacation() {
+		return vacation;
+	}
+	public void setVacation(boolean vacation) {
+		this.vacation = vacation;
+	}
+	public boolean isContinuousTime() {
+		return continuousTime;
+	}
+	public void setContinuousTime(boolean continuousTime) {
+		this.continuousTime = continuousTime;
+	}
+	public boolean isOther() {
+		return other;
+	}
+	public void setOther(boolean other) {
+		this.other = other;
+	}
+	public Double getHours() {
+		return hours;
+	}
+	public void setHours(Double hours) {
+		this.hours = hours;
+	}
+
+	public void disableAllTypes() {
+		setHoliday(false); 
+		setNotWorkingDay(false);
+		setVacation(false);
+		setContinuousTime(false);
+		setOther(false); 
 	}
 	
 	
