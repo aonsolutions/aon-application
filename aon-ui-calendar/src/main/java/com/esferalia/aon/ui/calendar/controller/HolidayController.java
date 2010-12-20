952b2d90357533079cf1e103331c5475f2b4cd11
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2010-12-20 09:40:26 +0000

    filtro por año en mnto de holiday

--- aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/controller/HolidayController.java
+++ aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/controller/HolidayController.java
@@ -1,7 +1,30 @@
 package com.esferalia.aon.ui.calendar.controller;
 
+import javax.faces.event.ActionEvent;
+
 import com.code.aon.ui.form.BasicController;
+import com.code.aon.ui.form.IController;
+import com.code.aon.ui.util.AonUtil;
 
 public class HolidayController extends BasicController {
+	
+	private Integer year;
+	
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+	public void onChangeYear(ActionEvent event){
+		refreshLines();
+	}
+	
+	private void refreshLines(){
+		IController detail = (IController) AonUtil.getRegisteredBean(ICalendarConstants.HOLIDAY_DETAIL_CONTROLLER_NAME);
+		detail.initializeModel();
+	}
 
 }
