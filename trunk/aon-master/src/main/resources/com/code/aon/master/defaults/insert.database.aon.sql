8f3618785b3797af5625cc7894776193cb57951a
Gorka Irazu <girazu@esferalia.com>
2010-12-23 10:40:28 +0000

    ASSIGNED - bug 2053: Valores por defecto de impuestos http://bugzilla.esferalia.com/show_bug.cgi?id=2053

--- trunk/aon-master/src/main/resources/com/code/aon/master/defaults/insert.database.aon.sql
+++ trunk/aon-master/src/main/resources/com/code/aon/master/defaults/insert.database.aon.sql
@@ -139,11 +139,12 @@ COMMIT;
 #
 
 INSERT INTO `tax` (`id`, `name`, `tax_type`, `percentage`, `surcharge`, `start_date`) VALUES 
-  (1,'GENERAL',1,18,4,'2010-07-01'),
-  (2,'REDUCIDO',1,8,1,'2010-07-01'),
-  (3,'SUPERREDUCIDO',1,4,0.5,'2000-01-01'),
-  (4,'SIN IVA',1,0,0,'2000-01-01'),
-  (5,'IRPF',2,15,0,'2000-01-01');
+  (1,'GENERAL',1,18,4,'2010-07-01',0,0),
+  (2,'REDUCIDO',1,8,1,'2010-07-01',0,0),
+  (3,'SUPERREDUCIDO',1,4,0.5,'2000-01-01',0,0),
+  (4,'SIN IVA',1,0,0,'2000-01-01',0,0),
+  (5,'IRPF PROFESIONALES',2,18,0,'2000-01-01',0,0),
+  (6,'IRPF ARRENDAMIENTOS',2,19,0,'2000-01-01',0,1); 
 
 COMMIT;
 
