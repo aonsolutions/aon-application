b74112b6c6108d35cbd72b8dfa2c54b573eb7bbc
Raul Trepiana <rtrepiana@esferalia.com>
2011-01-04 16:32:58 +0000

    Si no hay test falla...

--- trunk/aon-payroll-ctsql2mysql/src/test/java/com/esferalia/aon/payroll/ctsql2mysql/Ctsql2MysqlTest.java
+++ trunk/aon-payroll-ctsql2mysql/src/test/java/com/esferalia/aon/payroll/ctsql2mysql/Ctsql2MysqlTest.java
@@ -39,15 +39,18 @@ public class Ctsql2MysqlTest
      * Rigourous Test :-)
      * @throws SQLException 
      * @throws ClassNotFoundException 
+     */
     public void testCtsql2Mysql() throws SQLException, ClassNotFoundException
     {
+	/*
     	String args [] = {
     			"-dryrun",
     			"-mysqlurl", "jdbc:mysql://127.0.0.1:3306/rtrepiana-esferalia-com",
     			"-ctsqlurl", "jdbc:ctsql://192.168.2.100:1101/empre055;DBPATH=/usr/share/ctsql/data;RTRIMCHAR=true"
     	} ;
     	Ctsql2Mysql.main(args);
+	*/
     }
-     */
+     
     
 }
