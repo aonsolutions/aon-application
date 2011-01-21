6d92cdaa10122b074e8f3aeee1d6cc67ca414dba
Gorka Irazu <girazu@esferalia.com>
2011-01-21 16:50:05 +0000

    EXTRACTO BANCARIO - Correcion de errores en las devoluciones de varios vencimientos, en la asociacion de asientos con lineas del extracto y algun que otro cambio en las pantallas

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/BankStatementLinkManager.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/BankStatementLinkManager.java
@@ -163,7 +163,7 @@ public class BankStatementLinkManager implements IFinanceConstants {
 		trackingList.checkNone(event);
 	}
 
-	public void onEditSearch(ActionEvent event) throws ManagerBeanException {
+	public void onEditFinanceSearch(ActionEvent event) throws ManagerBeanException {
 		FinanceListController financeList = (FinanceListController)FormUtil.getController(FINANCE_LIST_CONTROLLER_NAME);
 		financeList.onEditSearch(event);
 
