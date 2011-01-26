bb37dea21105686976062546c3d127221c6b6466
Raul Trepiana <rtrepiana@esferalia.com>
2011-01-26 17:00:40 +0000

    Merged revisions 14545,14552-14553 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r14545 | girazu | 2010-12-28 16:06:49 +0100 (Tue, 28 Dec 2010) | 1 line
      
      Facturacion de pedidos, albaranes y presupuestos: Controlar que unicamente se creen vencimientos si el importe de la factura es mayor que cero.
    ........
      r14552 | girazu | 2010-12-29 13:23:01 +0100 (Wed, 29 Dec 2010) | 1 line
      
      EXTRACTO BANCARIO - Poder asignar mas Conceptos a un Vencimiento o Remesa punteados.
    ........
      r14553 | girazu | 2010-12-29 13:24:31 +0100 (Wed, 29 Dec 2010) | 1 line
      
      EXTRACTO BANCARIO - Poder asignar mas Conceptos a un Vencimiento o Remesa punteados.
    ........

--- aon-payroll/aon-finance-bridge/src/main/java/com/code/aon/finance/bridge/invoicing/IncomeInvoicingManager.java
+++ aon-payroll/aon-finance-bridge/src/main/java/com/code/aon/finance/bridge/invoicing/IncomeInvoicingManager.java
@@ -44,10 +44,13 @@ public class IncomeInvoicingManager {
 		updateIncomeStatus(income);
 		Invoice invoice = createInvoice(income, referenceCode, issueDate);
 		createInvoiceDetails(invoice, income);
-		if (income.getPayMethod() != null && income.getPayMethod().getId() != null) {
-			getFinanceGenerator().generateFinances(invoice, income, getPriceStrategy().getTotalPrice(invoice, invoice), true);
-		} else {
-			getFinanceGenerator().generateFinances(invoice, getPriceStrategy().getTotalPrice(invoice, invoice), true);
+		double invoiceTotal = getPriceStrategy().getTotalPrice(invoice, invoice);
+		if (invoiceTotal > 0) {
+			if (income.getPayMethod() != null && income.getPayMethod().getId() != null) {
+				getFinanceGenerator().generateFinances(invoice, income, invoiceTotal, true);
+			} else {
+				getFinanceGenerator().generateFinances(invoice, invoiceTotal, true);
+			}
 		}
 		return invoice;
 	}
