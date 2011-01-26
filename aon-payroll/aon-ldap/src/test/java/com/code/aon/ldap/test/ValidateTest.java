beac27df1ee6af636f5ad9dc1945f0c35db2e3e0
Raul Trepiana <rtrepiana@esferalia.com>
2011-01-26 17:13:02 +0000

    Merged revisions 14558,14563,14565,14567-14573 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r14558 | ecastellano | 2011-01-03 09:19:49 +0100 (Mon, 03 Jan 2011) | 2 lines
      
      RESOLVED - bug 2075: FACTURA - Contabilización de facturas 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2075
    ........
      r14563 | ecastellano | 2011-01-04 09:42:33 +0100 (Tue, 04 Jan 2011) | 2 lines
      
      RESOLVED - bug 2075: FACTURA - Contabilización de facturas 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2075
    ........
      r14565 | atellitu | 2011-01-04 16:34:10 +0100 (Tue, 04 Jan 2011) | 2 lines
      
      RESOLVED - bug 2125: No se cierran correctamente los recursos de LDAP 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2125
    ........
      r14567 | eagirrezabal | 2011-01-04 18:58:49 +0100 (Tue, 04 Jan 2011) | 1 line
      
      bug: 2061, 2120, 2121, 2123, 2124; listados en pdf y excel de seguimiento comercial, y ordenacion en la tabla de la lista previa
    ........
      r14568 | rtrepiana | 2011-01-05 12:41:02 +0100 (Wed, 05 Jan 2011) | 1 line
      
      El inicio de los tests... ¿ es real ?
    ........
      r14569 | rtrepiana | 2011-01-05 13:09:21 +0100 (Wed, 05 Jan 2011) | 1 line
      
      Click tests del account....
    ........
      r14570 | rtrepiana | 2011-01-05 13:28:03 +0100 (Wed, 05 Jan 2011) | 1 line
      
      Fix package
    ........
      r14571 | atellitu | 2011-01-05 13:52:52 +0100 (Wed, 05 Jan 2011) | 2 lines
      
      RESOLVED - bug 2122: Error en la creación de dominios desde aon Manager 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2122
    ........
      r14572 | eagirrezabal | 2011-01-05 15:18:44 +0100 (Wed, 05 Jan 2011) | 1 line
      
      bug 1879, 2072; botones de listados de clientes potenciales arreglados
    ........
      r14573 | eagirrezabal | 2011-01-05 15:31:47 +0100 (Wed, 05 Jan 2011) | 1 line
      
      Bug 2128 - Mejoras estéticas en Listados de seguimiento
    ........

--- aon-payroll/aon-ldap/src/test/java/com/code/aon/ldap/test/ValidateTest.java
+++ aon-payroll/aon-ldap/src/test/java/com/code/aon/ldap/test/ValidateTest.java
@@ -1,17 +1,13 @@
 package com.code.aon.ldap.test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.Properties;
 import java.util.Random;
 
 import javax.naming.Name;
 
 import junit.framework.JUnit4TestAdapter;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.StopWatch;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -19,13 +15,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.code.aon.ldap.BasicLdap;
 import com.code.aon.ldap.Entry;
 import com.code.aon.ldap.IAonObjectClasses;
 import com.code.aon.ldap.ILdapConstants;
-import com.code.aon.ldap.LdapException;
 import com.code.aon.ldap.NameResolver;
-import com.code.aon.ldap.Scope;
 
 public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 
@@ -33,146 +26,57 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 	
 	private final static Logger LOGGER = LoggerFactory.getLogger(ValidateTest.class);
 	
-	private static final String LDAP_PROPERTIES = "ldap.properties";
-	
-	private static BasicLdap ldap;
-
-	public static Properties loadProperties() {
-		Properties properties = new Properties();
-		String path = System.getProperty(LDAP_PROPERTIES);
-		try {
-			if (! StringUtils.isEmpty(path) ) {
-				FileInputStream fin = new FileInputStream( path ); 
-				properties.load(fin);
-				fin.close();
-			} else {
-				InputStream in = ValidateTest.class.getResourceAsStream(LDAP_PROPERTIES);
-				properties.load(in);
-				in.close();
-			}
-		} catch ( IOException e ) {
-			Assert.fail( e.getMessage() );
-		}
-		return properties;
-	}
+	private static Util util;
 	
 	@BeforeClass
 	public static synchronized void runBeforeAllTests() {
-		ldap = new BasicLdap( loadProperties() );
+		util = new Util();
 	}
 	
 	@AfterClass
 	public static synchronized void runAfterAllTests() {
-		ldap.closeSession();
-	}
-	
-	
-	
-	private List<Entry> getList( Name dn, String objectClass, String... attributes ) {
-		List<Entry> list = null;
-		try {
-			String filter = NameResolver.getObjectClass(objectClass);
-			list = ldap.getLdapSession().search(dn, filter, attributes );		
-		} catch ( LdapException e ) {
-			Assert.fail( e.getMessage() );
-		} finally {
-			ldap.closeSession();
-		}		
-		return list;
+		util.getLdap().closeSession();
 	}
 
-	private List<Entry> getDeepList( Name dn, String expression, String objectClass, String... attributes ) {
-		List<Entry> list = null;
-		try {
-			String exp1 = NameResolver.getObjectClass(objectClass);
-			String filter = NameResolver.getAndExpression(exp1, expression);
-			list = ldap.getLdapSession().search(dn, filter, Scope.SUBTREE_SCOPE, attributes );		
-		} catch ( LdapException e ) {
-			Assert.fail( e.getMessage() );
-		} finally {
-			ldap.closeSession();
-		}		
-		return list;
-	}	
-	
-	private Name getFullDN( Name name ) {
-		try {
-			return ldap.getLdapSession().getFullDN(name);
-		} catch ( LdapException e ) {
-			Assert.fail( e.getMessage() );
-		} finally {
-			ldap.closeSession();
-		}		
-		return null;		
-	}
-
-	private Name getBaseDN() {
-		try {
-			return ldap.getLdapSession().getBaseDN();
-		} catch ( LdapException e ) {
-			Assert.fail( e.getMessage() );
-		} finally {
-			ldap.closeSession();
-		}		
-		return null;		
-	}
-	
-	private void assertExist( Name dn, String objectClass ) {
-		Assert.assertTrue( objectClass + " doesn't exist " + dn, ldap.exists(dn, objectClass) );
-	}
-	
-	private void addOrganizationUnit( Name dn ) {
-		try {
-			Entry entry = new Entry(dn);
-			entry.addObjectClasses(new String[]{TOP, ORGANIZATIONAL_UNIT});
-			entry.put( ORGANIZATIONAL_UNIT_NAME_ATTRIBUTE, NameResolver.getFirstValue(dn) );
-			LOGGER.info( "Add Organization Unit entry: {}", dn );
-			ldap.getLdapSession().add(entry);
-		} catch ( LdapException e ) {
-			LOGGER.error(e.getMessage(), e);
-		} finally {
-			ldap.closeSession();
-		}
-	}	
 	private void ensureOrganizationalUnit( Name dn ) {
-		if (! ldap.exists(dn, ORGANIZATIONAL_UNIT) ) {
+		if (! util.exists(dn, ORGANIZATIONAL_UNIT) ) {
 			LOGGER.warn(  "{} {} doesn't exist", ORGANIZATIONAL_UNIT, dn );
-			addOrganizationUnit(dn);
+			util.getLdap().addOrganizationUnit(dn);
 		}
 	}
 	
 	@Test
     public void testRoot() {
 		Name applications = NameResolver.getApplicationsDN();
-		assertExist(applications, ORGANIZATIONAL_UNIT );
+		util.assertExist(applications, ORGANIZATIONAL_UNIT );
 		Name domains = NameResolver.getDomainsDN();
-		assertExist(domains, ORGANIZATIONAL_UNIT );
+		util.assertExist(domains, ORGANIZATIONAL_UNIT );
 		Name messages = NameResolver.getMessagesDN();
-		assertExist(messages, ORGANIZATIONAL_UNIT );
+		util.assertExist(messages, ORGANIZATIONAL_UNIT );
 
-		Name fullApplications = getFullDN(applications);
-		assertExist(fullApplications, ORGANIZATIONAL_UNIT );
-		Name fullDomains = getFullDN(domains);
-		assertExist(fullDomains, ORGANIZATIONAL_UNIT );
-		Name fullMessages = getFullDN(messages);
-		assertExist(fullMessages, ORGANIZATIONAL_UNIT );
+		Name fullApplications = util.getFullDN(applications);
+		util.assertExist(fullApplications, ORGANIZATIONAL_UNIT );
+		Name fullDomains = util.getFullDN(domains);
+		util.assertExist(fullDomains, ORGANIZATIONAL_UNIT );
+		Name fullMessages = util.getFullDN(messages);
+		util.assertExist(fullMessages, ORGANIZATIONAL_UNIT );
     }
 
 	@Test
     public void testMessages() {
 		Name message10 = NameResolver.getMessageDN(10);
-		assertExist(message10, MESSAGE );
+		util.assertExist(message10, MESSAGE );
 		Name message100 = NameResolver.getMessageDN(100);
-		assertExist(message100, MESSAGE );
+		util.assertExist(message100, MESSAGE );
 
 		Name message10_es = NameResolver.getMessageDN(10, "es");
-		assertExist(message10_es, MESSAGE );
+		util.assertExist(message10_es, MESSAGE );
 		Name message100_es = NameResolver.getMessageDN(100, "es");
-		assertExist(message100_es, MESSAGE );
+		util.assertExist(message100_es, MESSAGE );
 	}
 	
 	private void checkName( Name name, String objectClass, Name container, Name preffix ) {
-		if ( ldap.exists(name, objectClass) ) {
+		if ( util.exists(name, objectClass) ) {
 			if (! name.startsWith(preffix) ) {
 				LOGGER.error( "{} {} must have preffix {} in {}", new Object[]{objectClass, name, preffix, container} );
 			}
@@ -196,10 +100,10 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 		Name rolesDN = NameResolver.getApplicationRolesDN(name);
 		ensureOrganizationalUnit( rolesDN );
 		
-		List<Entry> roles = getList(rolesDN, ROLE, OBJECT_CLASS_ATTRIBUTE);
+		List<Entry> roles = util.getList(rolesDN, ROLE, OBJECT_CLASS_ATTRIBUTE);
 		Assert.assertFalse( "Application " + name + " with roles empty",  roles.isEmpty() );
 
-		List<Entry> profiles = getList(profilesDN, PROFILE, MEMBER_ATTRIBUTE);
+		List<Entry> profiles = util.getList(profilesDN, PROFILE, MEMBER_ATTRIBUTE);
 		Assert.assertFalse( "Application " + name + " with profiles empty", profiles.isEmpty() );
 		for( Entry profile : profiles ) {
 			testApplicationProfile(profile, application.getDN());
@@ -210,7 +114,7 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 	@Test
     public void testApplications() {
 		Name applications = NameResolver.getApplicationsDN();
-		for( Entry application : getList(applications, APPLICATION,COMMON_NAME_ATTRIBUTE) ) {
+		for( Entry application : util.getList(applications, APPLICATION,COMMON_NAME_ATTRIBUTE) ) {
 			testApplication(application);
 		}
 	}
@@ -219,7 +123,7 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 		Name applicationsDN = NameResolver.getDomainApplicationsDN(domain);
 		String name = user.getAsString(USER_ID_ATTRIBUTE);
 		String filter = NameResolver.getCommonName(name);
-		List<Entry> applications = getDeepList(applicationsDN, filter, DOMAIN_APPLICATION_USER);
+		List<Entry> applications = util.getDeepList(applicationsDN, filter, DOMAIN_APPLICATION_USER);
 		if ( applications.isEmpty() ) {
 			LOGGER.warn( "User not registered in any application: {}", user.getDN() );
 		}
@@ -228,7 +132,7 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 	private void testDB( Entry db, String domain ) {
 		Name applicationsDN = NameResolver.getDomainApplicationsDN(domain);
 		String filter = NameResolver.getEqualExpression(DATA_SOURCE_ATTRIBUTE, db.getDN().toString());
-		List<Entry> applications = getDeepList(applicationsDN, filter, DOMAIN_APPLICATION);
+		List<Entry> applications = util.getDeepList(applicationsDN, filter, DOMAIN_APPLICATION);
 		if ( applications.isEmpty() ) {
 			LOGGER.warn( "DBConnection not used in any application: {}", db.getDN() );
 		}
@@ -237,10 +141,10 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 	private void testDomainApplicationUser( Entry user, String application, String domain ) {
 		String name = user.getAsString(COMMON_NAME_ATTRIBUTE);
 		Name userDN = NameResolver.getUserDN(domain, name);
-		Assert.assertTrue( "User " + userDN + " doesn't exist, buf referenced " + user.getDN(), ldap.exists(userDN, USER) );
+		Assert.assertTrue( "User " + userDN + " doesn't exist, buf referenced " + user.getDN(), util.exists(userDN, USER) );
 		
-		Name profilesDN = getFullDN(NameResolver.getApplicationProfilesDN(application));
-		Name domainProfilesDN = getFullDN(NameResolver.getDomainApplicationProfilesDN(domain, application));
+		Name profilesDN = util.getFullDN(NameResolver.getApplicationProfilesDN(application));
+		Name domainProfilesDN = util.getFullDN(NameResolver.getDomainApplicationProfilesDN(domain, application));
     	for( Object member : user.get(MEMBER_ATTRIBUTE) ) {
     		Name memberName = NameResolver.getName( member.toString() );
     		if ( memberName.startsWith(profilesDN) ) {
@@ -248,7 +152,7 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
     		} else if ( memberName.startsWith(domainProfilesDN) ) {
     			checkName(memberName, PROFILE, user.getDN(), domainProfilesDN);
     		} else {
-    			checkName(memberName, PROFILE, user.getDN(), getBaseDN());
+    			checkName(memberName, PROFILE, user.getDN(), util.getBaseDN());
     		}
     	}		
 	}
@@ -264,19 +168,19 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
     private void testDomainApplication( Entry application, String domain ) {
     	String name = application.getAsString(COMMON_NAME_ATTRIBUTE);
 		Name applicationDN = NameResolver.getApplicationDN(name);
-		assertExist(applicationDN, APPLICATION );
+		util.assertExist(applicationDN, APPLICATION );
 		Name profilesDN = NameResolver.getDomainApplicationProfilesDN(domain, name);
-		assertExist(profilesDN, ORGANIZATIONAL_UNIT );
+		util.assertExist(profilesDN, ORGANIZATIONAL_UNIT );
 		Name usersDN = NameResolver.getDomainApplicationUsersDN(domain, name);
-		assertExist(usersDN, ORGANIZATIONAL_UNIT );    	
+		util.assertExist(usersDN, ORGANIZATIONAL_UNIT );    	
 		
 		if ( application.containsKey(DATA_SOURCE_ATTRIBUTE) ) {
 			Name dataSource = NameResolver.getName( application.getAsString(DATA_SOURCE_ATTRIBUTE) );
-			Name bdsDN = getFullDN(NameResolver.getDomainBDsDN(domain));
+			Name bdsDN = util.getFullDN(NameResolver.getDomainBDsDN(domain));
 			checkName(dataSource, DB_CONNECTION, application.getDN(), bdsDN);
 		}
 		
-		List<Entry> users = getList(usersDN, DOMAIN_APPLICATION_USER);
+		List<Entry> users = util.getList(usersDN, DOMAIN_APPLICATION_USER);
 		if ( users.isEmpty() ) {
 			LOGGER.info( "DomainApplication {} with users empty", application.getDN() );	
 		}
@@ -284,7 +188,7 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 			testDomainApplicationUser( user, name, domain );
 		}
 		
-		List<Entry> profiles = getList(usersDN, DOMAIN_APPLICATION_PROFILE);
+		List<Entry> profiles = util.getList(usersDN, DOMAIN_APPLICATION_PROFILE);
 		for( Entry profile : profiles ) {
 			testDomainApplicationProfile( profile, name );
 		}
@@ -300,29 +204,29 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
     
     private void testWebmail( String domain ) {
     	Name usersDN = NameResolver.getDomainApplicationUsersDN(domain, AON_WEBMAIL);
-		List<Entry> users = getList(usersDN, DOMAIN_APPLICATION_USER, COMMON_NAME_ATTRIBUTE);
+		List<Entry> users = util.getList(usersDN, DOMAIN_APPLICATION_USER, COMMON_NAME_ATTRIBUTE);
 		for( Entry user : users ) {
 			String name = user.getAsString(COMMON_NAME_ATTRIBUTE);
 			Name userDN = NameResolver.getUserDN(domain, name);
-			if ( ldap.exists(userDN, USER) ) {
+			if ( util.exists(userDN, USER) ) {
 				Name addressbook = NameResolver.getUserAddressBookDN(domain, name);
 				ensureOrganizationalUnit( addressbook );
 				
 				Name signaturesDN = NameResolver.getUserSignaturesDN(domain, name);
 				ensureOrganizationalUnit( signaturesDN );
-				List<Entry> signatures = getList(signaturesDN, SIGNATURE);
+				List<Entry> signatures = util.getList(signaturesDN, SIGNATURE);
 				if ( signatures.isEmpty() ) {
 					LOGGER.error( "User {} with signatures empty", userDN );	
 				}
 				
 				Name accounts = NameResolver.getUserAccountsDN(domain, name);
 				ensureOrganizationalUnit( accounts );
-				List<Entry> mailAccounts = getList(accounts, MAIL_ACCOUNT);
+				List<Entry> mailAccounts = util.getList(accounts, MAIL_ACCOUNT);
 				if ( mailAccounts.isEmpty() ) {
 					LOGGER.error( "User {} with mail accounts empty", userDN );	
 				} else {
 					for( Entry mailAccount : mailAccounts ) {
-						testMailAccount(mailAccount, getFullDN(signaturesDN));
+						testMailAccount(mailAccount, util.getFullDN(signaturesDN));
 					}	
 				}				
 			}
@@ -333,32 +237,32 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
     	String name = domain.getAsString(COMMON_NAME_ATTRIBUTE);
     	LOGGER.info( "Start validate domain {}", name );
 		Name applicationsDN = NameResolver.getDomainApplicationsDN(name);
-		assertExist(applicationsDN, ORGANIZATIONAL_UNIT );
+		util.assertExist(applicationsDN, ORGANIZATIONAL_UNIT );
 		Name bdsDN = NameResolver.getDomainBDsDN(name);
-		assertExist(bdsDN, ORGANIZATIONAL_UNIT );
+		util.assertExist(bdsDN, ORGANIZATIONAL_UNIT );
 		Name usersDN = NameResolver.getUsersDN(name);
-		assertExist(usersDN, ORGANIZATIONAL_UNIT );
+		util.assertExist(usersDN, ORGANIZATIONAL_UNIT );
 
-		List<Entry> users = getList(usersDN, USER, USER_ID_ATTRIBUTE);
+		List<Entry> users = util.getList(usersDN, USER, USER_ID_ATTRIBUTE);
 		Assert.assertFalse( "Domain " + name + " with users empty", users.isEmpty() );
 		for( Entry user : users ) {
 			testUser( user, name );
 		}
 
-		List<Entry> bds = getList(bdsDN, DB_CONNECTION, COMMON_NAME_ATTRIBUTE);
+		List<Entry> bds = util.getList(bdsDN, DB_CONNECTION, COMMON_NAME_ATTRIBUTE);
 		Assert.assertFalse( "Domain " + name + " with bds empty", bds.isEmpty() );
 		for( Entry bd : bds ) {
 			testDB( bd, name );
 		}		
 		
-		List<Entry> applications = getList(applicationsDN, DOMAIN_APPLICATION);
+		List<Entry> applications = util.getList(applicationsDN, DOMAIN_APPLICATION);
 		Assert.assertFalse( "Domain " + name + " with applications empty", applications.isEmpty() );		
 		for( Entry application : applications ) {
 			testDomainApplication( application, name );
 		}		
 		
 		Name webmail = NameResolver.getDomainApplicationDN(name, AON_WEBMAIL);
-		if ( ldap.exists(webmail, DOMAIN_APPLICATION) ) {
+		if ( util.exists(webmail, DOMAIN_APPLICATION) ) {
 			testWebmail( name );
 		}
 		LOGGER.info( "End validate domain {}", name );
@@ -366,14 +270,20 @@ public class ValidateTest implements IAonObjectClasses, ILdapConstants {
 	
 	@Test
     public void testDomain() {
+    	StopWatch sw = new StopWatch();
+    	sw.start();
+	    	
 		Name domainsDN = NameResolver.getDomainsDN();
-		List<Entry> domains =  getList(domainsDN, DOMAIN);
+		List<Entry> domains = util.getList(domainsDN, DOMAIN);
 		
 		Random random = new Random();
 		int domainIndex = random.nextInt(domains.size());
 		Entry domain = domains.get(domainIndex);
 
 		testDomain(domain);
+		
+		sw.stop();
+		LOGGER.info( "Total time: " + sw );
 	}
 
 	public static junit.framework.Test suite() {
