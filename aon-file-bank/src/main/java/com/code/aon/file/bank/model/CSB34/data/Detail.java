f3556132a8f910521c87ddf2663282d4a71a1ce4
Gorka Irazu <girazu@esferalia.com>
2011-01-27 12:36:04 +0000

    REMESAS DE PAGOS - Incluir mantenimiento y creacion del fichero AEB 34

--- aon-file-bank/src/main/java/com/code/aon/file/bank/model/CSB34/data/Detail.java
+++ aon-file-bank/src/main/java/com/code/aon/file/bank/model/CSB34/data/Detail.java
@@ -2,130 +2,52 @@ package com.code.aon.file.bank.model.CSB34.data;
 
 import com.code.aon.file.format.core.Account;
 
-
-/**
- * 
- * Detail lines data object
- * 
- * @author Consulting & Development. Iñigo GAyarre - 08/02/2007
- * @since 1.0
- *
- */
 public abstract class Detail {
 
-	/**
-	 * The receiver 
-	 */
 	private Receiver receiver;
-
-	/**
-	 * Receivers account
-	 */
 	private Account account;
-
-	/**
-	 * The amount
-	 */
 	private double amount;
-	
-	/**
-	 * The concept
-	 */
 	private String concept;
-	
-	/**
-	 * The concept´s description
-	 */
-	private String conceptDesc;
-	
-	/**
-	 * @return the account
-	 */
-	public Account getAccount() {
-		return account;
-	}
 
-	/**
-	 * @param account the account to set
-	 */
-	public void setAccount(Account account) {
-		this.account = account;
-	}
+	public abstract String getType();
 
-	
-	/**
-	 * @return the receiver
-	 */
 	public Receiver getReceiver() {
 		return receiver;
 	}
-
-	/**
-	 * @param receiver the receiver to set
-	 */
 	public void setReceiver(Receiver receiver) {
 		this.receiver = receiver;
 	}
 
-	public abstract String getType();
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
 
-	/**
-	 * @return the amount
-	 */
 	public double getAmount() {
 		return amount;
 	}
-
-	/**
-	 * @param amount the amount to set
-	 */
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
 
-	/**
-	 * @return the concept
-	 */
 	public String getConcept() {
 		return concept;
 	}
-
-	/**
-	 * @param concept the concept to set
-	 */
 	public void setConcept(String concept) {
 		this.concept = concept;
 	}
-
-	/**
-	 * @return the conceptDesc
-	 */
-	public String getConceptDesc() {
-		return conceptDesc;
+	public String getConceptPart1() {
+		if (concept.length() > 36)
+			return concept.substring(0, 36);
+		return concept;
 	}
-
-	/**
-	 * @param conceptDesc the conceptDesc to set
-	 */
-	public void setConceptDesc(String conceptDesc) {
-		this.conceptDesc = conceptDesc;
-	}
-
-	/**
-	 * @return the conceptDesc substring
-	 */
-	public String getConceptDescPart1() {
-		if (conceptDesc.length()>36)return conceptDesc.substring(0, 36);
-		return conceptDesc;
-	}
-
-	/**
-	 * @return the conceptDesc substring
-	 */
-	public String getConceptDescPart2() {
-		if (conceptDesc.length()>36){
-			if (conceptDesc.length()>72)return conceptDesc.substring(36,72);
-			return conceptDesc.substring(36);
+	public String getConceptPart2() {
+		if (concept.length() > 36) {
+			if (concept.length() > 72)
+				return concept.substring(36, 72);
+			return concept.substring(36);
 		}
 		return null;
 	}
@@ -140,7 +62,6 @@ public abstract class Detail {
 		description += account == null?"NULL ":"'"+account.getCcc()+"'; ";
 		description += "AMOUNT '"+amount+"'; ";
 		description += "CONCEPT "+concept==null?"NULL":"'"+concept+"'; ";
-		description += "CONCEPT_DESC "+conceptDesc==null?"NULL":"'"+conceptDesc+"'; ";
 		return description;
 	}
 }
--- aon-finance/src/main/java/com/code/aon/finance/enumeration/FinanceBatchType.java
+++ aon-finance/src/main/java/com/code/aon/finance/enumeration/FinanceBatchType.java
@@ -15,29 +15,42 @@ public enum FinanceBatchType implements IResourceable {
 
 
     /** NONE. */
-	NONE,
+	NONE(null),
 
     /** AEB_19. */
-    AEB_19,
+    AEB_19(false),
 
     /** AEB_19_D. */
-    AEB_19_D,
+    AEB_19_D(false),
 
     /** AEB_32. */ 
-    AEB_32,
+    AEB_32(false),
 
     /** AEB_58. */
-    AEB_58, 
+    AEB_58(false), 
     
     /** AEB_58_D. */
-    AEB_58_D; 
+    AEB_58_D(false), 
     
+    /** AEB_34. */ 
+    AEB_34(true);
+
     /** Message file base path. */
     private static final String BASE_NAME = "com.code.aon.finance.i18n.messages";
 
     /** Message key prefix. */
     private static final String MSG_KEY_PREFIX = "aon_enum_financebatchtype_";
     
+    private Boolean payment;
+
+    private FinanceBatchType(Boolean payment) {
+    	this.payment = payment;
+    }
+
+    public Boolean isPayment() {
+    	return payment;
+    }
+
     /**
      * Returns a <code>String</code> with the transalation <code>Locale</code>
      * for the locale.
