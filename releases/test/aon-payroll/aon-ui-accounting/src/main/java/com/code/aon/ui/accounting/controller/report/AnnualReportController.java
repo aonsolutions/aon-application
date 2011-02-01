9aa738425c14c6ba984c51fd0a966dc520faf19c
Raul Trepiana <rtrepiana@esferalia.com>
2011-02-01 18:02:37 +0000

    Esta rota

--- releases/test/aon-payroll/aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/report/AnnualReportController.java
+++
@@ -1,8 +0,0 @@
-package com.code.aon.ui.accounting.controller.report;
-
-import com.code.aon.ui.form.BasicController;
-
-public class AnnualReportController extends BasicController {
-	
-	
-}
--- releases/test/aon-payroll/aon-ui-stat/src/main/java/com/code/aon/ui/stat/controller/IStatConstants.java
+++
@@ -1,9 +0,0 @@
-package com.code.aon.ui.stat.controller;
-
-public interface IStatConstants {
-
-	String EMPTY_STRING = "";
-	
-	String STAT_CONTROLLER_NAME = "stat";
-
-}
