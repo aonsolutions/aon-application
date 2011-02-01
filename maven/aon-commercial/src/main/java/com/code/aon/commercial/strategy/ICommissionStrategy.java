892ddd1bab737ad42e8d541b02efba732b6599d0
Raul Trepiana <rtrepiana@esferalia.com>
2011-02-01 17:33:42 +0000

    Esta rota

--- maven/aon-commercial/src/main/java/com/code/aon/commercial/strategy/ICommissionStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.code.aon.commercial.strategy;
-
-import com.code.aon.commercial.OfferDetail;
-
-public interface ICommissionStrategy {
-	
-	public double getCommission(OfferDetail offerDetail);
-	
-}
