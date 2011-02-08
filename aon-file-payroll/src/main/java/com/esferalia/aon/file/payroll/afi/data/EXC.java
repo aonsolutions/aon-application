7a7c26b7296e6af5934e3a0f6cfb52dad970c25d
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2011-02-08 16:02:51 +0000

    dev

---
+++ aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/afi/data/EXC.java
@@ -0,0 +1,24 @@
+package com.esferalia.aon.file.payroll.afi.data;
+
+/**
+ * explotacion y claves
+ */
+public class EXC {
+	
+	private String tipo;
+	private String denominacion;
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getDenominacion() {
+		return denominacion;
+	}
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+}
---
+++ aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/afi/data/RZS.java
@@ -0,0 +1,39 @@
+package com.esferalia.aon.file.payroll.afi.data;
+
+/**
+ * razon social
+ */
+public class RZS {
+	
+	private String identificador;
+	private String tipoAlfabeticoEmpresario;
+	private String razonSocial;
+	private String autorizacion;
+	
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	public String getTipoAlfabeticoEmpresario() {
+		return tipoAlfabeticoEmpresario;
+	}
+	public void setTipoAlfabeticoEmpresario(String tipoAlfabeticoEmpresario) {
+		this.tipoAlfabeticoEmpresario = tipoAlfabeticoEmpresario;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public String getAutorizacion() {
+		return autorizacion;
+	}
+	public void setAutorizacion(String autorizacion) {
+		this.autorizacion = autorizacion;
+	}
+	
+	
+}
