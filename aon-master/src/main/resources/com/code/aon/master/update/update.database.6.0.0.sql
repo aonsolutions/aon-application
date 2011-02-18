7dfbd09ab8965f7df189c13c47f2011de4e004ee
Gorka Irazu <girazu@esferalia.com>
2011-02-18 12:11:48 +0000

    Version 6.0.1

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.6.0.0.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.6.0.0.sql
@@ -8,6 +8,12 @@
 
 BEGIN;
 
+ALTER TABLE `registry` MODIFY `document_type` tinyint(2) default '0' COMMENT 'Tipo de documento (NIF, CIF...)';
+
+ALTER TABLE `registry` MODIFY `document_country` varchar(2) collate latin1_spanish_ci default 'ES' COMMENT 'Pais del documento';
+
+ALTER TABLE `registry` MODIFY `nationality` varchar(2) collate latin1_spanish_ci default 'ES' COMMENT 'Nacionalidad';
+
 UPDATE `registry` SET `document_type` = 2 WHERE SUBSTR(`document`, 1, 1) IN ('X','Y','Z');
 
 UPDATE `registry` SET `document_type` = 1 WHERE SUBSTR(`document`, 1, 1) BETWEEN 'A' AND 'J' OR SUBSTR(`document`, 1, 1) BETWEEN 'N' AND 'W';
@@ -17,6 +23,10 @@ UPDATE `registry` SET `document_type` = 0 WHERE `document_type` IS NULL;
 UPDATE `registry` SET `document_country` = 'ES';
 
 UPDATE `registry` SET `nationality` = 'ES';
+ 
+UPDATE `registry` SET `type` = 0 WHERE `document_type` != 1; 
+
+UPDATE `registry` SET `type` = 1 WHERE `document_type` = 1; 
 
 
 UPDATE `db_version` SET `version_number` = '6.0.1';
