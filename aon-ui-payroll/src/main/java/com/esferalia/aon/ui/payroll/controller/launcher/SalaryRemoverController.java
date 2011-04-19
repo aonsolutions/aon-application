dfeda52319863d88651b5ce1bf441a1aabd45f7a
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2011-04-19 08:32:29 +0000

    borrado de nominas

---
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/launcher/SalaryRemoverController.java
@@ -0,0 +1,170 @@
+package com.esferalia.aon.ui.payroll.controller.launcher;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
+import com.code.aon.common.dao.sql.DAOException;
+import com.code.aon.ui.form.FormUtil;
+import com.code.aon.ui.form.IController;
+import com.code.aon.ui.util.AonUtil;
+import com.esferalia.aon.payroll.Salary;
+import com.esferalia.aon.payroll.dao.IPayrollAlias;
+import com.esferalia.aon.salary.enumeration.SalaryType;
+import com.esferalia.aon.ui.payroll.controller.IPayrollConstants;
+
+public class SalaryRemoverController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SalaryRemoverController.class.getName());
+	
+	private DataModel model;
+	private SalaryType salaryType;
+	
+	public DataModel getModel() {
+		if (model == null) {
+			model = new ListDataModel();
+		}
+		return model;
+	}
+	public void setModel(DataModel model) {
+		this.model = model;
+	}
+	public SalaryType getSalaryType() {
+		return salaryType;
+	}
+	public void setSalaryType(SalaryType salaryType) {
+		this.salaryType = salaryType;
+	}
+	private List<SelectableSalary> transformList(List<ITransferObject> list) {
+		List<SelectableSalary> rList = new ArrayList<SelectableSalary>();
+		for (ITransferObject to : list) {
+			SelectableSalary r = new SelectableSalary();
+			r.setSalary((Salary) to);
+			rList.add(r);
+		}
+		return rList;
+	}
+	
+	public void initializeModel(List<ITransferObject> list) {
+		setModel(new ListDataModel(transformList(list)));
+	}
+
+	/*
+	 * ACTION LISTENER
+	 */
+	
+	public void onInitialize(ActionEvent event) {
+		setSalaryType(null);
+	}
+	
+	public void onSearch(ActionEvent event) {
+		setModel(null);
+		try {
+			IController controller = FormUtil.getController(IPayrollConstants.SALARY_CONTROLLER);
+			if(getSalaryType()!=null){
+				controller.getCriteria().addEqualExpression(controller.getFieldName(IPayrollAlias.SALARY_TYPE), getSalaryType());
+			}
+			controller.getCriteria().addOrder(controller.getFieldName(IPayrollAlias.SALARY_CONTRACT_WORK_PLACE_ENTERPRISE_ID));
+			controller.getCriteria().addOrder(controller.getFieldName(IPayrollAlias.SALARY_EMPLOYEE_NAME));
+			controller.onSearch(event);
+			initializeModel(controller.getManagerBean().getList(controller.getCriteria()));
+		} catch (ManagerBeanException e) {
+			String msg = "No se pudieron buscar las nóminas";
+			LOGGER.error(msg);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg, e);
+		}
+	}
+	
+	public void onSelectAll(ActionEvent event) {
+		processAll(true);
+	}
+
+	public void onDeselectAll(ActionEvent event) {
+		processAll(false);
+	}
+
+	private void processAll(boolean selected) {
+		for (int i = 0; i < getModel().getRowCount(); i++) {
+			getModel().setRowIndex(i);
+			SelectableSalary r = (SelectableSalary) getModel().getRowData();
+			r.setSelected(selected);
+		}
+	}
+	
+	public void onRemoveSelected(ActionEvent event){
+		boolean mustBeginTransaction = HibernateUtil.mustBeginTransaction();
+		boolean mustCloseSession = HibernateUtil.mustCloseSession();
+		String sessionName = HibernateUtil.getSessionFactoryName();
+		try {
+			try {
+				HibernateUtil.setBeginTransaction(false);
+				HibernateUtil.setCloseSession(false);
+				HibernateUtil.beginTransaction(sessionName);
+				// BEGIN operaciones de la transaccion
+				IManagerBean bean = BeanManager.getManagerBean(Salary.class);
+				for (int i = 0; i < getModel().getRowCount(); i++) {
+					getModel().setRowIndex(i);
+					SelectableSalary r = (SelectableSalary) getModel().getRowData();
+					if(r.isSelected()){
+						bean.remove(r.getSalary());
+					}
+				}
+				// FIN operaciones de la transaccion
+				HibernateUtil.getSession(sessionName).flush();
+				HibernateUtil.commitTransaction(sessionName);
+			} catch (Exception e) {
+				String msg = e.getMessage();
+				try {
+					HibernateUtil.rollbackTransaction(sessionName);
+				} catch (DAOException daoe) {
+					msg = "Unable to rollback transaction! (" + msg + ")";
+				}
+				AonUtil.addErrorMessage(msg);
+				throw new AbortProcessingException(e);
+			} finally {
+				HibernateUtil.closeSession(sessionName);
+			}
+		} finally {
+			HibernateUtil.setCloseSession(mustCloseSession);
+			HibernateUtil.setBeginTransaction(mustBeginTransaction);
+		}
+		onSearch(event);
+	}
+	
+	
+	/*
+	 * INNER CLASS
+	 */
+	public class SelectableSalary{
+		private boolean selected;
+		private Salary salary;
+		
+		public boolean isSelected() {
+			return selected;
+		}
+		public void setSelected(boolean selected) {
+			this.selected = selected;
+		}
+		public Salary getSalary() {
+			return salary;
+		}
+		public void setSalary(Salary salary) {
+			this.salary = salary;
+		}
+	}
+	
+}
