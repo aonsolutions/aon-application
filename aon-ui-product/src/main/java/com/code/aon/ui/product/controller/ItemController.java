3b5930172d3aa9d32ea3e4d89f5ce076fae0a4de
Gorka Irazu <girazu@esferalia.com>
2011-06-10 08:42:10 +0000

    Desglose de la pantalla de Item en Nuevo y Edicion (para lookup y asi mantener el patron de la edicion en los maestros)

--- aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemController.java
+++ aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemController.java
@@ -3,7 +3,6 @@ package com.code.aon.ui.product.controller;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 
-import com.code.aon.common.util.CommonUtil;
 import com.code.aon.product.Item;
 import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.stat.controller.ProductStatEngineController;
@@ -12,7 +11,8 @@ import com.code.aon.ui.util.AonUtil;
 public class ItemController extends BasicController {
 
 	private String selectedTab;
-	
+	private ItemPricesManager pricesManager;
+
 	public String getSelectedTab() {
 		return selectedTab;
 	}
@@ -21,105 +21,27 @@ public class ItemController extends BasicController {
 		this.selectedTab = selectedTab;
 	}
 
-	public void onPurchasePriceChanged(ValueChangeEvent event) {
-		Item item = (Item)this.getTo();
-		if (event.getNewValue() != null	&& !event.getNewValue().toString().equals("")) {
-			double purchasePrice = CommonUtil.round(((Double)event.getNewValue()).doubleValue(), 4);
-			if (purchasePrice == 0) {
-				item.setProfitPercent(0);
-			} else {
-				item.setPrice(getPrice(purchasePrice, item.getProfitPercent()));
-			}
-		} else {
-			item.setPurchasePrice(0);
-			item.setProfitPercent(0);
+	public ItemPricesManager getPricesManager() {
+		if (pricesManager == null) {
+			pricesManager = new ItemPricesManager();
 		}
+		return pricesManager;
+	}
+
+	public void onPurchasePriceChanged(ValueChangeEvent event) {
+		getPricesManager().onPurchasePriceChanged((Item)this.getTo(), event.getNewValue());
 	}
 
 	public void onProfitChanged(ValueChangeEvent event) {
-		Item item = (Item)this.getTo();
-		if (event.getNewValue() != null	&& !event.getNewValue().toString().equals("")) {
-			double profit = CommonUtil.round(((Double)event.getNewValue()).doubleValue(), 3);
-			item.setPrice(getPrice(item.getPurchasePrice(), profit));
-		} else {
-			item.setProfitPercent(0);
-			if (item.getPurchasePrice() != 0) {
-				item.setPrice(item.getPurchasePrice());
-			}
-		}
-	}
-
-	private double getPrice(double purchasePrice, double profit) {
-		double price = 0;
-		for (int i=2; i<=4; i++) {
-			price = CommonUtil.round(purchasePrice * (1 + profit / 100), i);
-			if (profit == CommonUtil.round((price / purchasePrice - 1) * 100, 3)) {
-				return price;
-			} else {
-				price = CommonUtil.truncate(purchasePrice * (1 + profit / 100), i);
-				if (profit == CommonUtil.round((price / purchasePrice - 1) * 100, 3)) {
-					return price;
-				}
-			}
-		}
-		return price;
+		getPricesManager().onProfitChanged((Item)this.getTo(), event.getNewValue());
 	}
 
 	public void onPriceChanged(ValueChangeEvent event) {
-		Item item = (Item)this.getTo();
-		if (event.getNewValue() != null	&& !event.getNewValue().toString().equals("")) {
-			double price = CommonUtil.round(((Double)event.getNewValue()).doubleValue(), 4);
-			if (price == 0) {
-				item.setProfitPercent(0);
-			} else {
-				item.setProfitPercent(getProfit(item.getPurchasePrice(), price));
-			}
-		} else {
-			item.setPrice(0);
-			item.setProfitPercent(0);
-		}
-	}
-
-	private double getProfit(double purchasePrice, double price) {
-		if (purchasePrice == 0) {
-			return 0;
-		}
-		return CommonUtil.round((price / purchasePrice - 1) * 100, 3);
+		getPricesManager().onPriceChanged((Item)this.getTo(), event.getNewValue());
 	}
 
 	public void onSalesPriceChanged(ValueChangeEvent event) {
-		Item item = (Item)this.getTo();
-		if (event.getNewValue() != null	&& !event.getNewValue().toString().equals("")) {
-			double salesPrice = CommonUtil.round(((Double)event.getNewValue()).doubleValue());
-			if (salesPrice == 0) {
-				item.setPrice(0);
-				item.setProfitPercent(0);
-			} else {
-				item.setPrice(getPrice(item, salesPrice));
-				item.setProfitPercent(getProfit(item.getPurchasePrice(), item.getPrice()));
-			}
-		} else {
-			item.setPrice(0);
-			item.setProfitPercent(0);
-		}
-	}
-
-	private double getPrice(Item item, double salesPrice) {
-		double vatPercent = (item.getProduct().getVat() != null) ? item.getProduct().getVat().getPercentage() : 0;
-		double retentionPercent = (item.getProduct().getRetention() != null) ? item.getProduct().getRetention().getPercentage()	: 0;
-		double price = 0;
-		for (int i=2; i<=4; i++) {
-			price = CommonUtil.round(salesPrice / (1 + vatPercent / 100 - retentionPercent / 100), i);
-			if (salesPrice == item.getSalesPrice(price)) {
-				return price;
-			} else {
-				price = CommonUtil.truncate(salesPrice / (1 + vatPercent / 100 - retentionPercent / 100), i);
-				if (salesPrice == item.getSalesPrice(price)) {
-					return price;
-				}
-			}
-		}
-		return price;
+		getPricesManager().onSalesPriceChanged((Item)this.getTo(), event.getNewValue());
 	}
 
 	public void onProductHistory(ActionEvent e){
@@ -127,4 +49,5 @@ public class ItemController extends BasicController {
 		controller.setItem((Item)this.getTo());
 		controller.getProductData();
 	}
+
 }
--- aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemLookup.java
+++ aon-ui-product/src/main/java/com/code/aon/ui/product/controller/ItemLookup.java
@@ -2,30 +2,34 @@ package com.code.aon.ui.product.controller;
 
 import javax.faces.event.ValueChangeEvent;
 
-import com.code.aon.common.util.CommonUtil;
 import com.code.aon.faces.controller.RichLookupBean;
 import com.code.aon.product.Item;
-import com.code.aon.product.Product;
 
 public class ItemLookup extends RichLookupBean {
 
-	public double getSalesPrice() {
-		return ((Item) getTo()).getSalesPrice();
+	private ItemPricesManager pricesManager;
+
+	public ItemPricesManager getPricesManager() {
+		if (pricesManager == null) {
+			pricesManager = new ItemPricesManager();
+		}
+		return pricesManager;
 	}
 
-	public void setSalesPrice(double salesPrice) {
+	public void onPurchasePriceChanged(ValueChangeEvent event) {
+		getPricesManager().onPurchasePriceChanged((Item)this.getTo(), event.getNewValue());
+	}
+
+	public void onProfitChanged(ValueChangeEvent event) {
+		getPricesManager().onProfitChanged((Item)this.getTo(), event.getNewValue());
+	}
+
+	public void onPriceChanged(ValueChangeEvent event) {
+		getPricesManager().onPriceChanged((Item)this.getTo(), event.getNewValue());
 	}
 
 	public void onSalesPriceChanged(ValueChangeEvent event) {
-		double price = 0;
-		if (event.getNewValue() != null && !event.getNewValue().toString().equals("")) {
-			double salesPrice = new Double(event.getNewValue().toString()).doubleValue();
-			Product product = ((Item)this.getTo()).getProduct();
-			double vatPercent = (product.getVat() != null) ? product.getVat().getPercentage() : 0;
-			double retentionPercent = (product.getRetention() != null) ? product.getRetention().getPercentage()	: 0;
-			price = CommonUtil.round(salesPrice	/ (1 + vatPercent / 100 - retentionPercent / 100));
-		}
-		((Item)this.getTo()).setPrice(price);
+		getPricesManager().onSalesPriceChanged((Item)this.getTo(), event.getNewValue());
 	}
-	
+
 }
