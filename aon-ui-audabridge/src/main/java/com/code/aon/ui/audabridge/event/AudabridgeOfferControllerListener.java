87a26d47ced3724fae5c3a8280c83f688002d5a4
Euke Castellano <ecastellano@esferalia.com>
2011-07-07 08:03:51 +0000

    Share project "aon-ui-audabridge" into "http://dev.esferalia.com/repos/aon-application"

---
+++ aon-ui-audabridge/src/main/java/com/code/aon/ui/audabridge/event/AudabridgeOfferControllerListener.java
@@ -0,0 +1,30 @@
+package com.code.aon.ui.audabridge.event;
+
+import com.code.aon.commercial.Offer;
+import com.code.aon.ui.audabridge.IAudaBridgeConstants;
+import com.code.aon.ui.audabridge.controller.AudabridgeOfferController;
+import com.code.aon.ui.form.event.ControllerAdapter;
+import com.code.aon.ui.form.event.ControllerEvent;
+import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.aon.ui.util.AonUtil;
+
+public class AudabridgeOfferControllerListener extends ControllerAdapter {
+
+	
+	@Override
+	public void afterBeanSelected(ControllerEvent event) throws ControllerListenerException {
+		Offer offer = (Offer) event.getController().getTo();
+		AudabridgeOfferController c = (AudabridgeOfferController) 
+			AonUtil.getRegisteredBean(IAudaBridgeConstants.AUDABRIDGE_OFFER_CONTROLLER);
+		c.setOffer(offer);
+	}
+
+	@Override
+	public void afterBeanAdded(ControllerEvent event) throws ControllerListenerException {
+		Offer offer = (Offer) event.getController().getTo();
+		AudabridgeOfferController c = (AudabridgeOfferController) 
+			AonUtil.getRegisteredBean(IAudaBridgeConstants.AUDABRIDGE_OFFER_CONTROLLER);
+		c.setOffer(offer);
+		c.onCreateAssessment(null);
+	}
+}
