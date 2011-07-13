534280deae763e852a934d4302e2f75d857c47d1
Gorka Irazu <girazu@esferalia.com>
2011-07-13 16:15:25 +0000

    Project Name del Project TAS = Serie y Numero de la O.R. + Matricula + Marca y Modelo

--- aon-tas/src/main/java/com/code/aon/tas/event/ProjectTasBeanVetoListener.java
+++ aon-tas/src/main/java/com/code/aon/tas/event/ProjectTasBeanVetoListener.java
@@ -1,5 +1,7 @@
 package com.code.aon.tas.event;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
@@ -10,9 +12,13 @@ public class ProjectTasBeanVetoListener extends ManagerBeanVetoListenerAdapter {
     @Override
     public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
     	ProjectTas to = (ProjectTas)evt.getTo();
-    	to.getProject().setName(to.getReferenceCode() + " - " + to.getTasItem().getModel().getFullName());
-    	to.getProject().setAlias(to.getTasItem().getPublicCode());
+    	to.getProject().setName(obtainProjectTasName(to));
     	to.getProject().setTas(true);
+    }
+
+    private String obtainProjectTasName(ProjectTas to) {
+    	String name = to.getReferenceCode() + " - " + to.getTasItem().getPublicCode() + " (" + to.getTasItem().getModel().getFullName() + ")";
+    	return StringUtils.abbreviate(name, 64);
     }
 
 }
