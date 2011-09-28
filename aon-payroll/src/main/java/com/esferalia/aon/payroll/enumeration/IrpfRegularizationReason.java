c67a4109ce06853e3f61198937eb423630552498
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2011-09-28 09:51:38 +0000

    DEV payroll, el enumerado IrpfRegularizationReason no tenia definido el BASE_NAME de los bundles

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/enumeration/IrpfRegularizationReason.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/enumeration/IrpfRegularizationReason.java
@@ -1,6 +1,7 @@
 package com.esferalia.aon.payroll.enumeration;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import com.code.aon.common.enumeration.IResourceable;
 
@@ -18,10 +19,17 @@ public enum IrpfRegularizationReason implements IResourceable {
 	DEDUCT_HOME_LOAN_OUT,
 	OTHER;
 
+	/** Message file base path. */
+    private static final String BASE_NAME = "com.esferalia.aon.payroll.i18n.messages";
+    
+    /** Message key prefix. */
+    private static final String MSG_KEY_PREFIX = "aon_enum_irpf_regularization_reason_";
+
+	
 	@Override
 	public String getName(Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
 	}
 	
 	public Integer getCausa() {
