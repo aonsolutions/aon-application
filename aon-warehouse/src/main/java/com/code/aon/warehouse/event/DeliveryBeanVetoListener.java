e585a3f7cc5059c534d5841dc944851748023212
Gorka Irazu <girazu@esferalia.com>
2011-10-06 10:13:06 +0000

    En Albaranes de ventas y compras controlar que la fecha este dentro de un rango valido de años.

--- aon-warehouse/src/main/java/com/code/aon/warehouse/event/DeliveryBeanVetoListener.java
+++ aon-warehouse/src/main/java/com/code/aon/warehouse/event/DeliveryBeanVetoListener.java
@@ -1,9 +1,12 @@
 package com.code.aon.warehouse.event;
 
+import java.util.Date;
+
 import com.code.aon.common.enumeration.SecurityLevel;
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
+import com.code.aon.common.util.CommonUtil;
 import com.code.aon.warehouse.Delivery;
 import com.code.aon.warehouse.enumeration.DeliveryStatus;
 
@@ -13,6 +16,13 @@ public class DeliveryBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 	public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
 		Delivery delivery = (Delivery) evt.getTo();
 		setDefaultValues(delivery);
+		checkDelivery(delivery);
+	}
+
+	@Override
+	public void vetoableBeanUpdated(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
+		Delivery delivery = (Delivery) evt.getTo();
+		checkDelivery(delivery);
 	}
 
 	private void setDefaultValues(Delivery delivery) {
@@ -24,6 +34,14 @@ public class DeliveryBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 		}
 		if (delivery.getScope() == null || delivery.getScope().getId() == null) {
 			delivery.setScope(delivery.getCustomer().getScope());
+		}
+	}
+
+	private void checkDelivery(Delivery delivery) throws ManagerBeanVetoListenerException {
+		int thisYear = CommonUtil.getYear(new Date());
+		int deliveryYear = CommonUtil.getYear(delivery.getIssueTime());
+		if (deliveryYear < (thisYear-5) || deliveryYear > (thisYear+1)) {
+			throw new ManagerBeanVetoListenerException("La Fecha del Albaran no es correcta.");
 		}
 	}
 
--- aon-warehouse/src/main/java/com/code/aon/warehouse/event/IncomeBeanVetoListener.java
+++ aon-warehouse/src/main/java/com/code/aon/warehouse/event/IncomeBeanVetoListener.java
@@ -1,9 +1,12 @@
 package com.code.aon.warehouse.event;
 
+import java.util.Date;
+
 import com.code.aon.common.enumeration.SecurityLevel;
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
+import com.code.aon.common.util.CommonUtil;
 import com.code.aon.warehouse.Income;
 import com.code.aon.warehouse.enumeration.IncomeStatus;
 
@@ -13,6 +16,13 @@ public class IncomeBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 	public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
 		Income income = (Income) evt.getTo();
 		setDefaultValues(income);
+		checkIncome(income);
+	}
+
+	@Override
+	public void vetoableBeanUpdated(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
+		Income income = (Income) evt.getTo();
+		checkIncome(income);
 	}
 
 	private void setDefaultValues(Income income) {
@@ -24,6 +34,14 @@ public class IncomeBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 		}
 		if (income.getScope() == null || income.getScope().getId() == null) {
 			income.setScope(income.getSupplier().getScope());
+		}
+	}
+
+	private void checkIncome(Income income) throws ManagerBeanVetoListenerException {
+		int thisYear = CommonUtil.getYear(new Date());
+		int incomeYear = CommonUtil.getYear(income.getIssueTime());
+		if (incomeYear < (thisYear-5) || incomeYear > (thisYear+1)) {
+			throw new ManagerBeanVetoListenerException("La Fecha del Albaran no es correcta.");
 		}
 	}
 
