be93540199b3ffd40b63a3f5f9fc504207214740
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2011-10-06 13:04:46 +0000

    DEV payroll, corrigiendo errores...

--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/salary/draft/SalaryDraftController.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/salary/draft/SalaryDraftController.java
@@ -544,7 +544,10 @@ public class SalaryDraftController extends BasicController {
 			Criteria c = new Criteria();
 			c.addEqualExpression(bean.getFieldName(IPayrollAlias.SALARY_DEDUCTION_SALARY_ID), salary.getId());
 			List<SalaryDeduction> list = new LinkedList<SalaryDeduction>();
-			list.addAll((Collection<? extends SalaryDeduction>) bean.getList(c));
+			for(ITransferObject to: bean.getList(c)){
+				SalaryDeduction d = (SalaryDeduction) to;
+				list.add( d );
+			}
 			list.addAll(getSalaryEmbargos(salary));
 			return (Collection<SalaryDeduction>) list;
 		}
