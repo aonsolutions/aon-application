27af2161d33e57c06ea4d868d1c6eef773e4ae86
Gorka Irazu <girazu@esferalia.com>
2011-10-20 16:34:45 +0000

    Version 6.18.5

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.6.18.4.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.6.18.4.sql
@@ -14,9 +14,13 @@ ALTER TABLE `project_activity` DROP INDEX `IDX_PRJ_ACT_WORKGROUP`;
 
 ALTER TABLE `project_activity` DROP `workgroup` ;
 
-ALTER TABLE `target` ADD `scope` int(4) NOT NULL default '1' COMMENT 'Identificador del Ambito';
+ALTER TABLE `target` ADD `scope` int(4) default NULL COMMENT 'Identificador del Ambito';
 
 UPDATE `target` SET `scope` = (SELECT `scope` FROM `customer` WHERE `registry` = `target`.`registry`);
+
+UPDATE `target` SET `scope` = 1 WHERE `scope` IS NULL OR `scope` = 0;
+
+ALTER TABLE `target` MODIFY `scope` int(4) NOT NULL default '1' COMMENT 'Identificador del Ambito';
 
 ALTER TABLE `target` ADD KEY `IDX_TARGET_SCOPE` (`scope`);
 
