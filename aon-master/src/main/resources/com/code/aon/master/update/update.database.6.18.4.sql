0725954866da6202e5522edd95def6f00b896772
Gorka Irazu <girazu@esferalia.com>
2011-10-20 15:52:05 +0000

    Version 6.18.5

---
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.6.18.4.sql
@@ -0,0 +1,28 @@
+# Database: aon_master
+# Version: Actualizacion de la version 6.18.4 a la version 6.18.5.
+# Created by: girazu
+# Creation Date: 20/10/2011 13:25
+# Comentarios: esta actualización no contiene cambios invalidantes de base de datos.
+
+
+
+BEGIN;
+
+ALTER TABLE `project_activity` DROP FOREIGN KEY `FK_PRJ_ACT_WORKGROUP`;
+
+ALTER TABLE `project_activity` DROP INDEX `IDX_PRJ_ACT_WORKGROUP`;
+
+ALTER TABLE `project_activity` DROP `workgroup` ;
+
+ALTER TABLE `target` ADD `scope` int(4) NOT NULL default '1' COMMENT 'Identificador del Ambito';
+
+UPDATE `target` SET `scope` = (SELECT `scope` FROM `customer` WHERE `registry` = `target`.`registry`);
+
+ALTER TABLE `target` ADD KEY `IDX_TARGET_SCOPE` (`scope`);
+
+ALTER TABLE `target` ADD CONSTRAINT `FK_TARGET_SCOPE` FOREIGN KEY (`scope`) REFERENCES `scope` (`id`);
+
+
+UPDATE `db_version` SET `version_number` = '6.18.5';
+
+COMMIT;
