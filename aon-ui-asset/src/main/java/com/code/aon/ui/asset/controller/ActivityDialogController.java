2e7d7ec54ff291b35806d7198345ec98bea3a7fb
Euke Castellano <ecastellano@esferalia.com>
2012-01-09 08:55:10 +0000

    Merged revisions 17011 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r17011 | atellitu | 2012-01-02 14:06:22 +0100 (lun, 02 ene 2012) | 1 line
      
      Se ha pasado el LoggedUser y DomainResolver a ui-common y se ha añadido la propiedad skipLdap
    ........

--- aon-ui-asset/src/main/java/com/code/aon/ui/asset/controller/ActivityDialogController.java
+++ aon-ui-asset/src/main/java/com/code/aon/ui/asset/controller/ActivityDialogController.java
@@ -1,5 +1,7 @@
 package com.code.aon.ui.asset.controller;
 
+import static com.code.aon.ui.common.ICommonConstants.LOGGED_USER_CONTROLLER_NAME;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -21,7 +23,6 @@ import com.code.aon.asset.Asset;
 import com.code.aon.asset.AssetActivity;
 import com.code.aon.asset.enumeration.ActivityStatus;
 import com.code.aon.bridge.plugin.Utils;
-import com.code.aon.bridge.session.LoggedUser;
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
@@ -31,6 +32,7 @@ import com.code.aon.common.enumeration.WeekDay;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
 import com.code.aon.jaas.auth.AuthPrincipal;
 import com.code.aon.registry.RegistryMedia;
+import com.code.aon.ui.common.controller.LoggedUser;
 import com.code.aon.ui.company.controller.CompanyController;
 import com.code.aon.ui.form.FormUtil;
 import com.code.aon.ui.util.AonUtil;
@@ -289,7 +291,7 @@ public class ActivityDialogController extends EmailParentController{
 	public void onInitializeRequest(ActionEvent event) {
 		onInitialize(event);
 		setRequest(true);
-		String user = ((LoggedUser)AonUtil.getRegisteredBean("loggedUser")).getLoggedUserName();
+		String user = ((LoggedUser)AonUtil.getRegisteredBean(LOGGED_USER_CONTROLLER_NAME)).getLoggedUserName();
 		setWho(user);
 	}
 
