56832caff35132c792245b2a6b7c2560fa493263
Euke Castellano <ecastellano@esferalia.com>
2012-01-10 12:28:43 +0000

    Merged revisions 17096 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r17096 | ecastellano | 2012-01-10 13:02:49 +0100 (mar, 10 ene 2012) | 1 line
      
      Posibilidad de modificar el tipo de la dirección (Principal o Delegación) 
    ........

--- aon-registry/src/main/java/com/code/aon/registry/RegistryAddress.java
+++ aon-registry/src/main/java/com/code/aon/registry/RegistryAddress.java
@@ -36,4 +36,57 @@ public class RegistryAddress extends RegistryAddressDB implements IAddress {
   		return StringUtils.isEmpty(getAlias())?StringUtils.abbreviate(getFullAddress(), 25): getAlias();
     }
 
+    @Transient
+    public boolean isMainAddress() {
+  		return getAddressType() == AddressType.MAIN;
+    }
+
+    @Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (obj.getClass() != getClass()) return false;
+		final RegistryAddress o = (RegistryAddress) obj;
+		if (o.getId() == null && getId() == null) {
+			return new EqualsBuilder()
+				.append(this.address, o.address)
+				.append(this.address2, o.address2)				
+				.append(this.address3, o.address3)
+				.append(this.alias, o.alias)
+				.append(this.city, o.city)				
+				.append(this.geozone, o.geozone)
+				.append(this.number, o.number)
+				.append(this.recipient, o.recipient)
+				.append(this.registry, o.registry)				
+				.append(this.streetType, o.streetType)
+				.append(this.type, o.type)				
+				.append(this.zip, o.zip)
+				.isEquals();
+		}
+		return ObjectUtils.equals(getId(), o.getId());		
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(address)
+			.append(address2)
+			.append(address3)	
+			.append(alias)	
+			.append(city)			
+			.append(geozone)
+			.append(id)
+			.append(number)
+			.append(recipient)	
+			.append(registry)			
+			.append(streetType)
+			.append(type)			
+			.append(zip)
+			.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new PojoToStringBuilder(this).toString();
+	}
 }
