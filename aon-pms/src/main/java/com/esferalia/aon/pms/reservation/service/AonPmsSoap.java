b3181f8c2632af2702f9f07868bb6bdb299356de
Gorka Irazu <girazu@esferalia.com>
2012-01-10 16:19:02 +0000

    Servicio web de recepcion de Reservas

--- aon-pms/src/main/java/com/esferalia/aon/pms/reservation/service/AonPmsSoap.java
+++ aon-pms/src/main/java/com/esferalia/aon/pms/reservation/service/AonPmsSoap.java
@@ -1,30 +1,17 @@
 package com.esferalia.aon.pms.reservation.service;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-
 import javax.jws.WebService;
 
 import com.esferalia.aon.pms.PmsSoap;
+import com.esferalia.aon.pms.reservation.ReservationManager;
 
 @WebService(endpointInterface="com.esferalia.aon.pms.PmsSoap", wsdlLocation="/WEB-INF/wsdl/AonPmsSoapService.wsdl", targetNamespace = "http://com.tradyso.pms.partner.ws_Pms/")
 public class AonPmsSoap implements PmsSoap {
 
 	@Override
 	public String getReservation(String reservation) {
-		File file = new File("/tmp/reservation_" + new Date().getTime() + ".xml");
-		PrintWriter writer = null;
-		try {
-			writer = new PrintWriter(file);
-			writer.write(reservation);
-			writer.flush();
-		} catch (IOException ex) {
-		} finally {
-			writer.close();
-		}
-		return null;
+		ReservationManager reservationManager = new ReservationManager();
+		return reservationManager.processReservation(reservation);
 	}
 
 }
