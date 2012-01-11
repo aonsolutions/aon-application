9286874f6ca34299249d4dae759bb427f1607ba8
Euke Castellano <ecastellano@esferalia.com>
2012-01-11 03:00:13 +0000

    Merged revisions 17149 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r17149 | atellitu | 2012-01-10 19:09:51 +0100 (mar, 10 ene 2012) | 1 line
      
      La funcionalidad del DomainResolver se ha pasado a DomainUtil para poder utlizarlo en el BackDoor
    ........

--- aon-security-bridge/src/main/java/com/code/aon/bridge/session/BackDoorAuthenticationFilter.java
+++ aon-security-bridge/src/main/java/com/code/aon/bridge/session/BackDoorAuthenticationFilter.java
@@ -25,6 +25,7 @@ import com.code.aon.jaas.auth.AuthPrincipal;
 import com.code.aon.jaas.auth.IConstants;
 import com.code.aon.jaas.deployment.DeploymentException;
 import com.code.aon.jaas.valves.BackDoorAuthenticationValve;
+import com.code.aon.ldap.util.DomainUtil;
 
 /**
  * @author Consulting & Development. Iñaki Ayerbe - 05/11/2007
@@ -37,15 +38,18 @@ public class BackDoorAuthenticationFilter implements Filter, IConstants {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		LOGGER.info( "init" );
 	}
 
 	@Override
 	public void destroy() {
+		LOGGER.info( "destroy" );
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
+		LOGGER.info( "doFilter" );		
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String sso = httpRequest.getParameter( BackDoorAuthenticationValve.BACKDOOR_PARAM );
 		if ( sso != null && Boolean.valueOf( sso ) ) {
@@ -72,7 +76,7 @@ public class BackDoorAuthenticationFilter implements Filter, IConstants {
 	}
 	
 	private String getUserName( AuthPrincipal principal, HttpServletRequest httpRequest) {
-		String domain = httpRequest.getServerName();
+		String domain = DomainUtil.getDomain(httpRequest.getServerName());
 		String username = principal.getShortName() + IConstants.IDENTITY_SEPARATOR 
 			+ domain + httpRequest.getContextPath();
 		return username;
