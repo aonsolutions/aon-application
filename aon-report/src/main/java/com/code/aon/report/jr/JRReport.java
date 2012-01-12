1ada18361cb8f19177276c5f427dc85484f3283d
Raul Trepiana <rtrepiana@esferalia.com>
2012-01-12 08:34:44 +0000

    Añadido el prámetro... opcional 'params' al método 'execute' :   execute(OutputStream os, String reportKey, Map<Object,Object>... params) Asi podemos pasar paŕametros opcionales como ZOOM_RATIO para el formato HTML.

--- aon-report/src/main/java/com/code/aon/report/jr/JRReport.java
+++ aon-report/src/main/java/com/code/aon/report/jr/JRReport.java
@@ -227,13 +227,14 @@ public class JRReport {
 	 *            The criteria of the data.
 	 * @param collection
 	 *            The collection of the data.
-	 * 
+	 * @param params 
+	 * 			  Sets export parameters from a specified map.
 	 * @return The outcome.
 	 * @throws ReportException
 	 *             Si se produce algún error. If an error ocurred.
 	 */
 	public String run(OutputFormat outputFormat, OutputStream out,
-			ResourceBundle bundle, Criteria criteria, Collection<?> collection) throws ReportException {
+			ResourceBundle bundle, Criteria criteria, Collection<?> collection, Map<Object,Object>... params) throws ReportException {
 		try {
 			setGeneratedPages( 0 );
 			if (JRExporterFactoryManager.accept(outputFormat)) {
@@ -242,7 +243,10 @@ public class JRReport {
 				factory = JRExporterFactoryManager
 						.getJRExporterFactory(outputFormat);
 				Map<Object, Object> map = new HashMap<Object, Object>();
-
+				
+				for (int i = 0; i < params.length; i++) {
+					map.putAll(params[i]);
+				}
 				factory.fillJRParametersMap(map);
 
 				passDefaultParameters(map);
