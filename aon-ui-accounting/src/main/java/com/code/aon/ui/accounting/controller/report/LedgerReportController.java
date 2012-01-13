34fc943cf23058b762433430c1dc311c205622bd
Raul Trepiana <rtrepiana@esferalia.com>
2012-01-13 09:43:17 +0000

    HotFix 6.20.1 Merged revisions 17082,17163,17172,17174,17178-17181 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r17082 | girazu | 2012-01-09 16:31:33 +0100 (Mon, 09 Jan 2012) | 1 line
      
      Version 6.20.1
    ........
      r17163 | eagirrezabal | 2012-01-11 16:25:03 +0100 (Wed, 11 Jan 2012) | 1 line
      
      datos de empresa - parametros para la impresion de facturas: se muestran a condicion de que no se tenga una plantilla personalizada y que el nuevo parametro definido showSaleInvoiceReportParamas sea true (en aon-gt se pone a false)
    ........
      r17172 | eagirrezabal | 2012-01-12 11:31:34 +0100 (Thu, 12 Jan 2012) | 1 line
      
      nuevo parametro de aon-config showSaleInvoiceReportParams
    ........
      r17174 | eagirrezabal | 2012-01-12 12:10:09 +0100 (Thu, 12 Jan 2012) | 1 line
      
      retoques en las plantillas de impresion de facturas de venta de finance y nuevas plantillas en aon-common-resources (personalizadas)
    ........
      r17178 | ecastellano | 2012-01-12 17:21:45 +0100 (Thu, 12 Jan 2012) | 2 lines
      
      RESOLVED - bug 2596: Listado Mayor de cuentas por número de cuenta 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2596
    ........
      r17179 | ecastellano | 2012-01-12 17:33:58 +0100 (Thu, 12 Jan 2012) | 2 lines
      
      RESOLVED - bug 2610: Gestión de asientos de explotación, cierre.... 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2610
    ........
      r17180 | ecastellano | 2012-01-12 17:55:29 +0100 (Thu, 12 Jan 2012) | 2 lines
      
      RESOLVED - bug 2611: Apuntes. Drop de ejercicio 
      http://bugzilla.esferalia.com/show_bug.cgi?id=2611
    ........
      r17181 | rtrepiana | 2012-01-12 18:20:36 +0100 (Thu, 12 Jan 2012) | 1 line
      
      Añadida la nueva propiedad template????
    ........

--- aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/report/LedgerReportController.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/report/LedgerReportController.java
@@ -169,10 +169,10 @@ public class LedgerReportController extends BasicController implements IAccounti
 						getSecurityLevel());
 			}
 			if (!StringUtils.isBlank(getAccount())) {
-				criteria.addExpression(getFieldName(IAccountingAlias.ACCOUNT_ENTRY_DETAIL_ACCOUNT_ID), getAccount());
+				criteria.addExpression(getFieldName(IAccountingAlias.ACCOUNT_ENTRY_DETAIL_ACCOUNT_CODE), getAccount());
 			}
 			getCriteria().addOrder(
-					getFieldName(IAccountingAlias.ACCOUNT_ENTRY_DETAIL_ACCOUNT_ID));
+					getFieldName(IAccountingAlias.ACCOUNT_ENTRY_DETAIL_ACCOUNT_CODE));
 			if ("1".equals(getOrder()) ) {
 				getCriteria().addOrder(getFieldName(IAccountingAlias.ACCOUNT_ENTRY_DETAIL_ACCOUNT_ENTRY_ENTRY_DATE));
 				getCriteria().addOrder(getFieldName(IAccountingAlias.ACCOUNT_ENTRY_DETAIL_ACCOUNT_ENTRY_ID));
