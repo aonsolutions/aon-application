981930f4230f1daed109f22a0c32e1cb6b09b22a
Euke Castellano <ecastellano@esferalia.com>
2012-01-19 08:13:47 +0000

    Merged revisions 17253 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r17253 | eagirrezabal | 2012-01-18 16:55:33 +0100 (mié, 18 ene 2012) | 1 line
      
      cambios en impresion de facturas
    ........

--- aon-ui-company/src/main/java/com/code/aon/ui/company/controller/CompanyParentController.java
+++ aon-ui-company/src/main/java/com/code/aon/ui/company/controller/CompanyParentController.java
@@ -606,6 +606,10 @@ public class CompanyParentController extends BasicController implements ICompany
 		return saleInvoiceTemplate==null?SaleInvoiceTemplate.DEFAULT.getValue():saleInvoiceTemplate.getValue();
 	}
 	
+	public String getInvoicePrintTemplateValue(){
+		return saleInvoiceTemplate==null?SaleInvoiceTemplate.DEFAULT.getValue():saleInvoiceTemplate.getValue().replaceFirst("saleInvoice", "invoicePrint");
+	}
+	
 	public boolean isPrintLogo() {
 		return printLogo;
 	}
