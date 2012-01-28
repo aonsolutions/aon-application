25cafb974d2a03393db5c0ffb1dcf7da97d3f3a6
Gorka Irazu <girazu@esferalia.com>
2012-01-28 17:45:52 +0000

    Version 7.0.5

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.7.0.4.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.7.0.4.sql
@@ -11,6 +11,8 @@ ALTER TABLE `project_reservation_service_detail` ADD `invoice_detail` int(4) def
 ALTER TABLE `project_reservation_service_detail` ADD KEY `IDX_PROJECT_RESERVATION_SERVICE_DETAIL_INVOICE_DETAIL` (`invoice_detail`);
 ALTER TABLE `project_reservation_service_detail` ADD CONSTRAINT `FK_PROJECT_RESERVATION_SERVICE_DETAIL_INVOICE_DETAIL` FOREIGN KEY (`invoice_detail`) REFERENCES `invoice_detail` (`id`);
 
+ALTER TABLE `invoice_address` MODIFY `geozone` int(4) default NULL COMMENT 'Identificador de la Zona Geografica';
+ALTER TABLE `invoice_address` ADD `province` varchar(45) collate latin1_spanish_ci default NULL COMMENT 'Provincia' AFTER `city`;
 
 UPDATE `db_version` SET `version_number` = '7.0.5';
 
