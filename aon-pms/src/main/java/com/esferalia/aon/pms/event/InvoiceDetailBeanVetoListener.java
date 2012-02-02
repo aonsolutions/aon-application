f02c282ac143fc99c6d11461bc44da2613ebcdfc
Gorka Irazu <girazu@esferalia.com>
2012-02-02 11:23:13 +0000

    Al borrar una Factura de Ventas, que se borren los servicios asociados a ella

--- aon-pms/src/main/java/com/esferalia/aon/pms/event/InvoiceDetailBeanVetoListener.java
+++ aon-pms/src/main/java/com/esferalia/aon/pms/event/InvoiceDetailBeanVetoListener.java
@@ -10,6 +10,7 @@ import com.code.aon.common.event.ManagerBeanVetoListenerException;
 import com.code.aon.finance.InvoiceDetail;
 import com.code.aon.ql.Criteria;
 import com.esferalia.aon.entity.IEntityAlias;
+import com.esferalia.aon.pms.ProjectReservationService;
 import com.esferalia.aon.pms.ProjectReservationServiceDetail;
 
 public class InvoiceDetailBeanVetoListener extends ManagerBeanVetoListenerAdapter {
@@ -26,6 +27,14 @@ public class InvoiceDetailBeanVetoListener extends ManagerBeanVetoListenerAdapte
 	        	for (ITransferObject ito : reservationServiceDetailBean.getList(criteria)) {
 	        		ProjectReservationServiceDetail reservationServiceDetail = (ProjectReservationServiceDetail)ito;
 	        		reservationServiceDetailBean.remove(reservationServiceDetail);
+
+	        		criteria = new Criteria();
+		        	alias = reservationServiceDetailBean.getFieldName(IEntityAlias.PROJECT_RESERVATION_SERVICE_DETAIL_PROJECT_RESERVATION_SERVICE_ID);
+		        	criteria.addEqualExpression(alias, reservationServiceDetail.getProjectReservationService().getId());
+	        		if (reservationServiceDetailBean.getCount(criteria) == 0) {
+	    	    		IManagerBean reservationServiceBean = BeanManager.getManagerBean(ProjectReservationService.class);
+	    	    		reservationServiceBean.remove(reservationServiceDetail.getProjectReservationService());
+	        		}
 	        	}
     		}
     	} catch (ManagerBeanException ex) {
