3523d459ad8ec2ec9381ff28faca57adb2b9172e
Euke Castellano <ecastellano@esferalia.com>
2012-02-09 06:03:55 +0000

    Merged revisions 17439 via svnmerge from http://dev.esferalia.com/repos/aon-application/trunk
    ........
      r17439 | eagirrezabal | 2012-02-08 16:07:40 +0100 (mié, 08 feb 2012) | 1 line
      
      se obtiene el reportKey por defecto para impresion de facturas cuando no hay parametros definidos
    ........

--- aon-ui-company/src/main/java/com/code/aon/ui/company/controller/CompanyParentController.java
+++ aon-ui-company/src/main/java/com/code/aon/ui/company/controller/CompanyParentController.java
@@ -66,6 +66,10 @@ public class CompanyParentController extends BasicController implements ICompany
 	
 	public static final String SALE_INVOICE_REPORT_KEY_PARAM = "REPORT_saleInvoice";
 	
+	public static final String SALE_INVOICE_REPORT_KEY = "saleInvoice";
+	
+	public static final String INVOICE_PRINT_REPORT_KEY = "invoicePrint";
+	
 	/** The attach. */
 	private RegistryAttachment attach;
 	
@@ -607,7 +611,7 @@ public class CompanyParentController extends BasicController implements ICompany
 	}
 	
 	public String getInvoicePrintTemplateValue(){
-		return saleInvoiceTemplate==null?SaleInvoiceTemplate.DEFAULT.getValue():saleInvoiceTemplate.getValue().replaceFirst("saleInvoice", "invoicePrint");
+		return saleInvoiceTemplate==null?INVOICE_PRINT_REPORT_KEY:saleInvoiceTemplate.getValue().replaceFirst(SALE_INVOICE_REPORT_KEY, INVOICE_PRINT_REPORT_KEY);
 	}
 	
 	public boolean isPrintLogo() {
