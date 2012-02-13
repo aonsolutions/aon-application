658546c03e78fa0006a73bef9176cf9c392e252f
Gorka Irazu <girazu@esferalia.com>
2012-02-13 17:38:28 +0000

    Al grabar un WorkPlace desde Company, el Enterprise que viaja unicamente tiene el Id (el de Company), asi que hay que buscarlo para que contenga el Scope que es el que recibe el WorkPlace

--- aon-company/src/main/java/com/code/aon/company/event/WorkPlaceBeanVetoListener.java
+++ aon-company/src/main/java/com/code/aon/company/event/WorkPlaceBeanVetoListener.java
@@ -1,8 +1,11 @@
 package com.code.aon.company.event;
 
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
+import com.code.aon.company.Enterprise;
 import com.code.aon.company.WorkPlace;
 
 public class WorkPlaceBeanVetoListener extends ManagerBeanVetoListenerAdapter {
@@ -10,8 +13,16 @@ public class WorkPlaceBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 	@Override
 	public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
 		WorkPlace workPlace =  (WorkPlace)evt.getTo();
-		if (workPlace.getScope() == null || workPlace.getScope().getId() == null) {
-			workPlace.setScope(workPlace.getEnterprise().getScope());
+		try {
+			if (workPlace.getScope() == null || workPlace.getScope().getId() == null) {
+				Enterprise enterprise = workPlace.getEnterprise();
+				if (enterprise != null && enterprise.getId() != null) {
+					enterprise = (Enterprise)BeanManager.getManagerBean(Enterprise.class).get(enterprise.getId());
+					workPlace.setScope(enterprise.getScope());
+				}
+			}
+		} catch (ManagerBeanException ex) {
+			throw new ManagerBeanVetoListenerException(ex.getMessage(), ex);
 		}
 	}
 
