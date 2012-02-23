ad2c8b3341cf20710532715f8f825f499396d438
Gorka Irazu <girazu@esferalia.com>
2012-02-23 10:35:01 +0000

    Version 7.0.8 corregida

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.7.0.7.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.7.0.7.sql
@@ -9,7 +9,7 @@ BEGIN;
 
 ALTER TABLE `project_reservation` MODIFY `code` varchar(32) collate latin1_spanish_ci NOT NULL COMMENT 'Localizador de la Reserva';
 
-ALTER TABLE `purchase_detail` ADD `proposal_detail` int(4) default NULL COMMENT 'Identificador del Detalle de Solicitud';
+ALTER TABLE `purchase_detail` ADD `proposal_detail` int(4) default NULL COMMENT 'Identificador del Detalle de Solicitud' AFTER `status`;
 ALTER TABLE `purchase_detail` ADD KEY `IDX_PURCHASE_DETAIL_PROPOSAL_DETAIL` (`proposal_detail`);
 ALTER TABLE `purchase_detail` ADD CONSTRAINT `FK_PURCHASE_DETAIL_PROPOSAL_DETAIL` FOREIGN KEY (`proposal_detail`) REFERENCES `proposal_detail` (`id`);
 
