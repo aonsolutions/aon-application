0e17e93894d3d27852f94e5d5552a32a2eb74056
Gorka Irazu <girazu@esferalia.com>
2012-03-01 08:44:26 +0000

    Quitar HQL y usar PROJECTION!!

--- aon-pms/src/main/java/com/esferalia/aon/pms/PosShift.java
+++ aon-pms/src/main/java/com/esferalia/aon/pms/PosShift.java
@@ -9,10 +9,12 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
-
-import com.code.aon.common.dao.hibernate.HibernateUtil;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ql.Projection;
+import com.esferalia.aon.entity.IEntityAlias;
 import com.esferalia.aon.entity.master.PosShiftDB;
 
 @Entity
@@ -38,13 +40,21 @@ public class PosShift extends PosShiftDB {
 	}
 	
 	@Transient
-	public Double getFinalAmount(){
-		String sqlSelect = "SELECT sum(PosShiftCount.amount)"
+	public Double getFinalAmount() throws ManagerBeanException {
+		/*String sqlSelect = "SELECT sum(PosShiftCount.amount)"
 				+ " FROM pos_shift_count as PosShiftCount"
 				+ " WHERE  PosShiftCount.pos_shift = " + getId();
 		Session session = HibernateUtil.getSession(HibernateUtil.getSessionFactoryName());
 		Query sqlQuery = session.createSQLQuery(sqlSelect);
 		finalAmount = (Double)sqlQuery.list().get(0);
+		return finalAmount;*/
+
+		IManagerBean posShiftCountBean = BeanManager.getManagerBean(PosShiftCount.class);
+		Criteria criteria = new Criteria();
+		criteria.addEqualExpression(posShiftCountBean.getFieldName(IEntityAlias.POS_SHIFT_COUNT_POS_SHIFT_ID), getId());
+		Projection projection = Projection.sum(posShiftCountBean.getFieldName(IEntityAlias.POS_SHIFT_COUNT_AMOUNT));
+		Object value = posShiftCountBean.getUniqueResult(projection, criteria);
+		finalAmount = (value == null) ? 0 : (Double)value;
 		return finalAmount;
 	}
 
