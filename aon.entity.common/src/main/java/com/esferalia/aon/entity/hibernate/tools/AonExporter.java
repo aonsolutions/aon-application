e97a288b2b6647d4c193a7198149bebfb987108e
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-03-22 17:10:41 +0000

    path adecuado para ReervationRequest  en el AonExporter

--- aon.entity.common/src/main/java/com/esferalia/aon/entity/hibernate/tools/AonExporter.java
+++ aon.entity.common/src/main/java/com/esferalia/aon/entity/hibernate/tools/AonExporter.java
@@ -433,6 +433,9 @@ public class AonExporter extends GenericExporter{
         map.put("ProjectReservationService","com.esferalia.aon.pms.ProjectReservationService");
         map.put("ProjectReservationServiceDetail","com.esferalia.aon.pms.ProjectReservationServiceDetail");
         map.put("Room","com.esferalia.aon.pms.Room");
+        map.put("ReservationRequest","com.esferalia.aon.pms.ReservationRequest");
+        map.put("ReservationRequestGuest","com.esferalia.aon.pms.ReservationRequestGuest");
+        map.put("ReservationRequestRoom","com.esferalia.aon.pms.ReservationRequestRoom");
         
 		//AON-PURCHASE  
         map.put("Proposal","com.code.aon.purchase.Proposal");
