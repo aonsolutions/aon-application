faf05dad466b1e0a1103974d84d525f30dfb66b7
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-03-22 17:14:54 +0000

    Bug 2720 - Solicitud de Compra. Aunar catálogos Hotel-Departamento

--- aon-ui-purchase/src/main/java/com/code/aon/ui/purchase/controller/ProposalController.java
+++ aon-ui-purchase/src/main/java/com/code/aon/ui/purchase/controller/ProposalController.java
@@ -3,57 +3,110 @@ package com.code.aon.ui.purchase.controller;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.faces.event.AbortProcessingException;
 import javax.faces.model.SelectItem;
 
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.company.Department;
 import com.code.aon.company.WorkplaceDepartment;
 import com.code.aon.product.CatalogueItem;
 import com.code.aon.product.ItemSupplier;
 import com.code.aon.purchase.Proposal;
 import com.code.aon.purchase.enumeration.ProposalStatus;
 import com.code.aon.ql.Criteria;
+import com.code.aon.ql.ast.Expression;
+import com.code.aon.ql.util.ExpressionUtilities;
 import com.code.aon.ui.form.BasicController;
+import com.code.aon.ui.util.AonUtil;
 import com.esferalia.aon.entity.IEntityAlias;
 
 public class ProposalController extends BasicController {
 	
-	public List<SelectItem> getWorkplaceDepartments() throws ManagerBeanException{
+	
+	public List<SelectItem> getDepartments() {
 		List<SelectItem> list = new LinkedList<SelectItem>();
-		IManagerBean bean = BeanManager.getManagerBean(WorkplaceDepartment.class);
-		Criteria criteria = new Criteria();
-		criteria.addEqualExpression(bean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_WORK_PLACE_ID), ((Proposal)getTo()).getWorkPlace().getId());
-		for (ITransferObject ito : bean.getList(criteria)) {
-			WorkplaceDepartment wd = (WorkplaceDepartment)ito;
-			SelectItem item = new SelectItem(wd, wd.getDepartment().getName());
-			list.add(item);
+		try {
+			List<Integer> deptartmentIds = new LinkedList<Integer>();
+			if(getTo()!=null){
+				for (ITransferObject ito : getWorkplaceDepartments()) {
+					WorkplaceDepartment wd = (WorkplaceDepartment)ito;
+					deptartmentIds.add(wd.getDepartment().getId());
+				}
+				if(!deptartmentIds.isEmpty()){
+					IManagerBean dBean = BeanManager.getManagerBean(Department.class);
+					Criteria dCriteria = new Criteria();
+					dCriteria.addInExpression(dBean.getFieldName(IEntityAlias.DEPARTMENT_ID), deptartmentIds);
+					dCriteria.addOrder(dBean.getFieldName(IEntityAlias.DEPARTMENT_NAME));
+					for (ITransferObject ito : dBean.getList(dCriteria)) {
+						Department d = (Department)ito;
+						SelectItem item = new SelectItem(d, d.getName());
+						list.add(item);
+					}
+				}
+			}
+		} catch (ManagerBeanException e) {
+			String msg = "Error al obtener los departamentos";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
 		}
 		return list;
 	}
 	
+	private List<ITransferObject> getWorkplaceDepartments() throws ManagerBeanException {
+		IManagerBean wdBean = BeanManager.getManagerBean(WorkplaceDepartment.class);
+		Criteria wdCriteria = new Criteria();
+		wdCriteria.addEqualExpression(wdBean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_WORK_PLACE_ID), ((Proposal)getTo()).getWorkPlace().getId());
+		wdCriteria.addEqualExpression(wdBean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_ACTIVE), Boolean.TRUE);
+		return wdBean.getList(wdCriteria);
+	}
+	
 	public List<SelectItem> getDepartmentsItems() throws ManagerBeanException{
 		List<SelectItem> list = new LinkedList<SelectItem>();
-		if(((Proposal)getTo()).getWorkplaceDepartment()!=null){
+		Proposal proposal = ((Proposal)getTo()); 
+		if(proposal.getDepartment()!=null){
+			List<Integer> catalogueIds = new LinkedList<Integer>();
+			for(ITransferObject to: getWorkplaceDepartments(proposal)){
+				WorkplaceDepartment wd = (WorkplaceDepartment) to;
+				catalogueIds.add(wd.getCatalogue().getId());
+			}
 			IManagerBean catalogueItemBean = BeanManager.getManagerBean(CatalogueItem.class);
-			Criteria criteria1 = new Criteria();
-			criteria1.addEqualExpression(catalogueItemBean.getFieldName(IEntityAlias.CATALOGUE_ITEM_CATALOGUE_ID), ((Proposal)getTo()).getWorkplaceDepartment().getCatalogue().getId());
+			Criteria deptCriteria = new Criteria();
+			deptCriteria.addInExpression(catalogueItemBean.getFieldName(IEntityAlias.CATALOGUE_ITEM_CATALOGUE_ID), catalogueIds);
 			List<Integer> itemIds = new LinkedList<Integer>();
-			for (ITransferObject ito : catalogueItemBean.getList(criteria1)) {
+			for (ITransferObject ito : catalogueItemBean.getList(deptCriteria)) {
 				itemIds.add(((CatalogueItem)ito).getItem().getId());
 			}
 			IManagerBean itemSupplierBean = BeanManager.getManagerBean(ItemSupplier.class);
-			Criteria criteria2 = new Criteria();
-			criteria2.addNotNullExpression(itemSupplierBean.getFieldName(IEntityAlias.ITEM_SUPPLIER_SUPPLIER_ID));
-			criteria2.addInExpression(itemSupplierBean.getFieldName(IEntityAlias.ITEM_SUPPLIER_ITEM_ID), itemIds);
-			for (ITransferObject ito : itemSupplierBean.getList(criteria2)) {
+			Criteria itemSupCriteria = new Criteria();
+			Expression expr1 = ExpressionUtilities.getEqualExpression(itemSupplierBean.getFieldName(IEntityAlias.ITEM_SUPPLIER_WORK_PLACE_ID), proposal.getWorkPlace().getId());
+			Expression expr2 = ExpressionUtilities.getNullExpression(itemSupplierBean.getFieldName(IEntityAlias.ITEM_SUPPLIER_WORK_PLACE_ID));
+			itemSupCriteria.addExpression(ExpressionUtilities.getOrExpression(expr1, expr2));
+			itemSupCriteria.addNotNullExpression(itemSupplierBean.getFieldName(IEntityAlias.ITEM_SUPPLIER_SUPPLIER_ID));
+			itemSupCriteria.addInExpression(itemSupplierBean.getFieldName(IEntityAlias.ITEM_SUPPLIER_ITEM_ID), itemIds);
+			for (ITransferObject ito : itemSupplierBean.getList(itemSupCriteria)) {
 				ItemSupplier is = (ItemSupplier) ito;
 				SelectItem item = new SelectItem(is.getItem(), is.getItem().getProduct().getCode() + " - " + is.getItem().getProduct().getName());
 				list.add(item);
 			}
 		}
 		return list;
+	}
+	
+	private List<ITransferObject> getWorkplaceDepartments(Proposal proposal){
+		try {
+			IManagerBean bean = BeanManager.getManagerBean(WorkplaceDepartment.class);
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(bean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_WORK_PLACE_ID), proposal.getWorkPlace().getId());
+			criteria.addEqualExpression(bean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_DEPARTMENT_ID), proposal.getDepartment().getId());
+			return bean.getList(criteria);
+		} catch (ManagerBeanException e) {
+			String msg = "Error al obtener los productos";
+			AonUtil.addErrorMessage(msg);
+		}
+		return null;
 	}
 	
 	public boolean isPending() {
--- aon-ui-purchase/src/main/java/com/code/aon/ui/purchase/event/ProposalSearchListener.java
+++ aon-ui-purchase/src/main/java/com/code/aon/ui/purchase/event/ProposalSearchListener.java
@@ -3,6 +3,7 @@ package com.code.aon.ui.purchase.event;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.faces.event.AbortProcessingException;
 import javax.faces.model.SelectItem;
 
 import org.apache.commons.lang.ArrayUtils;
@@ -11,18 +12,20 @@ import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.company.Department;
 import com.code.aon.company.WorkPlace;
 import com.code.aon.company.WorkplaceDepartment;
 import com.code.aon.purchase.enumeration.ProposalStatus;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.ui.util.AonUtil;
 import com.esferalia.aon.entity.IEntityAlias;
 
 public class ProposalSearchListener extends ControllerSearchListener {
 	
 	private WorkPlace workPlace;
-	private WorkplaceDepartment workplaceDepartment;
+	private Department department;
 	private ProposalStatus[] proposalStatuses;
 			
 	public WorkPlace getWorkPlace() {
@@ -33,12 +36,12 @@ public class ProposalSearchListener extends ControllerSearchListener {
 		this.workPlace = workPlace;
 	}
 	
-	public WorkplaceDepartment getWorkplaceDepartment() {
-		return workplaceDepartment;
+	public Department getDepartment() {
+		return department;
 	}
 
-	public void setWorkplaceDepartment(WorkplaceDepartment workplaceDepartment) {
-		this.workplaceDepartment = workplaceDepartment;
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 
 	public ProposalStatus[] getProposalStatuses() {
@@ -53,7 +56,7 @@ public class ProposalSearchListener extends ControllerSearchListener {
 	protected void init() throws ManagerBeanException {
 		super.init();
 		setWorkPlace(new WorkPlace());
-		setWorkplaceDepartment(new WorkplaceDepartment());
+		setDepartment(new Department());
 		ProposalStatus[] defaultProposalStatus = {ProposalStatus.PENDING, ProposalStatus.PARTIAL_PROCESSED};
 		setProposalStatuses(defaultProposalStatus);
 	}
@@ -64,8 +67,8 @@ public class ProposalSearchListener extends ControllerSearchListener {
 		if (getWorkPlace() != null && getWorkPlace().getId() != null) {
 			criteria.addEqualExpression(getFieldName(IEntityAlias.PROPOSAL_WORK_PLACE_ID), getWorkPlace().getId());			
 		}
-		if (getWorkplaceDepartment() != null && getWorkplaceDepartment().getId() != null) {
-			criteria.addEqualExpression(getFieldName(IEntityAlias.PROPOSAL_WORKPLACE_DEPARTMENT_ID), getWorkplaceDepartment().getId());			
+		if (getDepartment() != null && getDepartment().getId() != null) {
+			criteria.addEqualExpression(getFieldName(IEntityAlias.PROPOSAL_DEPARTMENT_ID), getDepartment().getId());			
 		}
 		if (!ArrayUtils.isEmpty(getProposalStatuses())) {
 			String status = getController().resolveAlias(IEntityAlias.PROPOSAL_STATUS);
@@ -73,19 +76,41 @@ public class ProposalSearchListener extends ControllerSearchListener {
 		}
 	}	
 	
-	public List<SelectItem> getWorkplaceDepartments() throws ManagerBeanException{
+	public List<SelectItem> getDepartments() {
 		List<SelectItem> list = new LinkedList<SelectItem>();
-		if(getWorkPlace()!=null && getWorkPlace().getId()!=null){
-			IManagerBean bean = BeanManager.getManagerBean(WorkplaceDepartment.class);
-			Criteria criteria = new Criteria();
-			criteria.addEqualExpression(bean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_WORK_PLACE_ID), getWorkPlace().getId());
-			for (ITransferObject ito : bean.getList(criteria)) {
-				WorkplaceDepartment wd = (WorkplaceDepartment)ito;
-				SelectItem item = new SelectItem(wd, wd.getDepartment().getName());
-				list.add(item);
+		try {
+			List<Integer> deptartmentIds = new LinkedList<Integer>();
+			if(getWorkPlace()!=null && getWorkPlace().getId()!=null){
+				for (ITransferObject ito : getWorkplaceDepartments()) {
+					WorkplaceDepartment wd = (WorkplaceDepartment)ito;
+					deptartmentIds.add(wd.getDepartment().getId());
+				}
+				if(!deptartmentIds.isEmpty()){
+					IManagerBean dBean = BeanManager.getManagerBean(Department.class);
+					Criteria dCriteria = new Criteria();
+					dCriteria.addInExpression(dBean.getFieldName(IEntityAlias.DEPARTMENT_ID), deptartmentIds);
+					dCriteria.addOrder(dBean.getFieldName(IEntityAlias.DEPARTMENT_NAME));
+					for (ITransferObject ito : dBean.getList(dCriteria)) {
+						Department d = (Department)ito;
+						SelectItem item = new SelectItem(d, d.getName());
+						list.add(item);
+					}
+				}
 			}
+		} catch (ManagerBeanException e) {
+			String msg = "Error al obtener los departamentos";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
 		}
 		return list;
+	}
+	
+	private List<ITransferObject> getWorkplaceDepartments() throws ManagerBeanException {
+		IManagerBean wdBean = BeanManager.getManagerBean(WorkplaceDepartment.class);
+		Criteria wdCriteria = new Criteria();
+		wdCriteria.addEqualExpression(wdBean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_WORK_PLACE_ID), getWorkPlace().getId());
+		wdCriteria.addEqualExpression(wdBean.getFieldName(IEntityAlias.WORKPLACE_DEPARTMENT_ACTIVE), Boolean.TRUE);
+		return wdBean.getList(wdCriteria);
 	}
 
 }
