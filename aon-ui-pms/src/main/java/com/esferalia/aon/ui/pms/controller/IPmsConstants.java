38de4a8d763b67866c4a721e364b8ca83ca499c2
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-03-29 15:57:09 +0000

    Bug 2696 - Listado de Llegadas/Salidas. Ordenación Bug 2761 - Listado de Entradas/Salidas. Navegación a Reserva

--- aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/controller/IPmsConstants.java
+++ aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/controller/IPmsConstants.java
@@ -2,24 +2,33 @@ package com.esferalia.aon.ui.pms.controller;
 
 public interface IPmsConstants {
 
-	String COLLECTIONS_CONTROLLER_NAME = "pmsCollections";
-	String RESERVATION_CONTROLLER_NAME = "reservation";
+	String AVAILABLE_ROOM = "availableRoom";
+	
+	String RACK_RESERVATION = "rackReservation";
+	
+	// ************************************************************
+	// BEAN
+	// ************************************************************
 	String ADVANCE_INVOICE_CONTROLLER_NAME = "advanceInvoice";
 	String ADVANCE_INVOICE_CONTROLLER_SEARCH_LISTENER = "advanceInvoiceSearch";
 	String ADVANCE_INVOICE_CONTROLLER_LIST = "advanceInvoice_list";
 	String ADVANCE_INVOICE_CONTROLLER_SEARCH = "advanceInvoice_search";
+	String COLLECTIONS_CONTROLLER_NAME = "pmsCollections";
+	String RACK_CONTROLLER_NAME = "rack";
+	String RACK_SEARCH_LISTENER_NAME = "rackSearch";
+	String RESERVATION_CONTROLLER_NAME = "reservation";
 	String RESERVATION_SEARCH_LISTENER_NAME = "reservationSearch";
 	String RESERVATION_ROOM_CONTROLLER_NAME = "reservationRoom";
 	String RESERVATION_SERVICE_CONTROLLER_NAME = "reservationService";
 	String ROOM_AVAILABILITY_CONTROLLER_NAME = "roomAvailability";
-	String RACK_CONTROLLER_NAME = "rack";
-	String RACK_SEARCH_LISTENER_NAME = "rackSearch";
-
 	String SALE_INVOICE_CONTROLLER_NAME = "saleInvoice";
-
+	
+	// ************************************************************
+	// NAVIGATION
+	// ************************************************************
 	String RACK_LIST_NAME = "rack_list";
+	String RESERVATION_IO_LIST_NAME = "reservationIO_list";
+	
 
-	String AVAILABLE_ROOM = "availableRoom";
-	String RACK_RESERVATION = "rackReservation";
 
 }
