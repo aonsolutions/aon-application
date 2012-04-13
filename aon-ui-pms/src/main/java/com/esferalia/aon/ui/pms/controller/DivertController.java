4be9be14ebdca209aa630aa602d7f85953538b63
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-04-13 09:51:47 +0000

    PMS. solo se pueden desviar reservas que no finalizadas

--- aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/controller/DivertController.java
+++ aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/controller/DivertController.java
@@ -45,7 +45,6 @@ public class DivertController extends BasicController {
 						}
 						controller.getCriteria().addNotEqualExpression(controller.getFieldName(IEntityAlias.PROJECT_RESERVATION_STATUS), ReservationStatus.BLOCKED);
 						controller.getCriteria().addNotEqualExpression(controller.getFieldName(IEntityAlias.PROJECT_RESERVATION_STATUS), ReservationStatus.CANCELLED);
-						controller.getCriteria().addLessThanOrEqualExpression(controller.getFieldName(IEntityAlias.PROJECT_RESERVATION_START_DATE), new Date());
 						controller.getCriteria().addGreaterThanOrEqualExpression(controller.getFieldName(IEntityAlias.PROJECT_RESERVATION_END_DATE), new Date());
 					} catch (ManagerBeanException e) {
 						LOGGER.error("Error filtering reservation", e);
