c58c91530eb59f45d30988d811b4a9d24ad92ccf
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-04-26 14:49:36 +0000

    PMS. Impresion de la solicitud de compra. Traduccion al ingles del bundle de los report

--- aon-purchase/src/main/java/com/code/aon/purchase/Proposal.java
+++ aon-purchase/src/main/java/com/code/aon/purchase/Proposal.java
@@ -1,6 +1,11 @@
 package com.code.aon.purchase;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.esferalia.aon.entity.master.ProposalDB;
@@ -10,5 +15,15 @@ import com.esferalia.aon.entity.master.ProposalDB;
 public class Proposal extends ProposalDB {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Set<ProposalDetail> lines = new HashSet<ProposalDetail>();
+	
+	@OneToMany(mappedBy = "proposal", cascade={CascadeType.REMOVE})
+	public Set<ProposalDetail> getLines() {
+		return this.lines;
+	}
+	public void setLines(Set<ProposalDetail> lines) {
+		this.lines = lines;
+	}
 
 }
