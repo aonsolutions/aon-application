176cc49b4f7717253dbd7dfbe951c58b5423e113
Euke Castellano <ecastellano@esferalia.com>
2012-05-14 15:11:51 +0000

    Un par de imports contra jython

--- aon-web-aio/src/main/java/com/code/aon/aio/servlet/NewDomain.java
+++ aon-web-aio/src/main/java/com/code/aon/aio/servlet/NewDomain.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Enumeration;
-import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.python.core.PyException;
-import org.python.util.PythonInterpreter;
 
 public class NewDomain extends HttpServlet {
 	private static final long serialVersionUID = 1L;
