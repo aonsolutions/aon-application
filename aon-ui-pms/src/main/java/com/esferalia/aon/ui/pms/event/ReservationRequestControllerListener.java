8a471d4a241ce3d4d504a2a1e583eaaa4a791d4e
Gorka Irazu <girazu@esferalia.com>
2012-06-29 11:46:07 +0000

    PMS. Solicitud de reservas. Por defecto al dar nueva solicitud, se marca el titular de reserva Agencia.

--- aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/event/ReservationRequestControllerListener.java
+++ aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/event/ReservationRequestControllerListener.java
@@ -34,7 +34,7 @@ public class ReservationRequestControllerListener extends ControllerAdapter impl
 
 		request.setStartDate(DateUtils.truncate(new Date(), Calendar.DATE));
 		request.setEndDate(DateUtils.addDays(request.getStartDate(), 1));
-		request.setBookingHolder(BookingHolder.GUEST);
+		request.setBookingHolder(BookingHolder.AGENCY);
 		request.setRequestCounter(0);
 		request.setActive(true);
 
