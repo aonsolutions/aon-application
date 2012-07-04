eaa5293389b33c6aa53abd054d8e1b66a9ef738e
Raul Trepiana <rtrepiana@esferalia.com>
2012-07-04 12:43:19 +0000

    Welcome GWT ...

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/bean/GWTPropertiesBean.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/bean/GWTPropertiesBean.java
@@ -6,5 +6,8 @@ public class GWTPropertiesBean {
 		String enabled =  System.getProperty("gwt.enabled", "false");
 		return enabled.equalsIgnoreCase("true");
 	}
-
+	
+	public String getMenuIntegral() {
+		return System.getProperty("gwt.menu.integral", "Integral (default)");
+	}
 }
