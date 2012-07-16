b70fa00000570e5a863a5371ded6ca3e438aafe0
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-07-16 09:18:13 +0000

    Impresion de los cuestionarios en las campañas de marketing

--- aon-ui-marketing/src/main/java/com/code/aon/ui/marketing/print/QuestionValueReport.java
+++ aon-ui-marketing/src/main/java/com/code/aon/ui/marketing/print/QuestionValueReport.java
@@ -1,6 +1,8 @@
 
 package com.code.aon.ui.marketing.print;
 
+import java.util.List;
+
 import org.apache.commons.lang.ObjectUtils;
 
 import com.code.aon.commercial.Question;
@@ -16,9 +18,19 @@ public class QuestionValueReport {
 	
 	private ActionTarget actionTarget;
 	
+	private List<MarketingQuestionValue> questionList;
+	
 	public QuestionValueReport(SurveyResponseDetail to, ActionTarget actionTarget ) {
 		this.to = to;
 		this.actionTarget = actionTarget;
+	}
+	
+	public List<MarketingQuestionValue> getQuestionList() {
+		return questionList;
+	}
+
+	public void setQuestionList(List<MarketingQuestionValue> questionList) {
+		this.questionList = questionList;
 	}
 
 	public SurveyResponseDetail getTo() {
