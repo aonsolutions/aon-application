d07a15e14d1148adb5c94ca01e8f9521ae0e5346
Gorka Irazu <girazu@esferalia.com>
2012-08-09 13:01:27 +0000

    PMS. Permisos de Early CheckOut para User cuando la reserva este InHouse

--- aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/ProjectReservationPermission.java
+++ aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/ProjectReservationPermission.java
@@ -133,7 +133,7 @@ public class ProjectReservationPermission {
 
 	public boolean isEarlyCheckOutAllowed() {
 		Date now = new Date();
-		boolean roleAllowed = (isRoleConfig() && !isBeforeCheckIn(now)) || (isRoleFinance() && isAfterCheckOut(now));
+		boolean roleAllowed = (isInHouse(now)) || (isRoleFinance() && isAfterCheckOut(now));
 		return roleAllowed && reservation.isInvoiced();
 	}
 
