7b7992f45663204e9e10815d57dec1f952e34a00
Gorka Irazu <girazu@esferalia.com>
2012-08-27 10:39:45 +0000

    PMS. Añadido filtro de scope al controlador de Habitaciones.

--- aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/event/RoomSearchListener.java
+++ aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/event/RoomSearchListener.java
@@ -24,7 +24,6 @@ import com.esferalia.aon.ui.pms.controller.RoomController;
 
 public class RoomSearchListener extends ControllerSearchListener {
 
-	private PmsCollectionsController collectionsController;
 	private Hotel hotel;
 	private Item item;
 	
@@ -44,21 +43,11 @@ public class RoomSearchListener extends ControllerSearchListener {
 		this.item = item;
 	}
 	
-	private PmsCollectionsController getCollectionController() {
-		if(collectionsController == null){
-			collectionsController = (PmsCollectionsController) AonUtil.getRegisteredBean(IPmsConstants.COLLECTIONS_CONTROLLER_NAME);
-		}
-		return collectionsController;
-	}
-	
 	@Override
 	protected void init() throws ManagerBeanException {
-		if(getCollectionController().getCurrentUserHotelsCount() == 1){
-			setHotel((Hotel) getCollectionController().getCurrentUserHotelList().get(0));
-		} else {
-			setHotel((Hotel)BeanManager.getManagerBean(Hotel.class).createNewTo());
-		}
+		setHotel((Hotel)BeanManager.getManagerBean(Hotel.class).createNewTo());
 		setItem((Item)BeanManager.getManagerBean(Item.class).createNewTo());
+
 		((RoomController)getController()).clearCheckedRooms();
 	}
 	
@@ -66,7 +55,8 @@ public class RoomSearchListener extends ControllerSearchListener {
 		if (getHotel() != null && getHotel().getId() != null) {
 			return getHotelRoomItems(getHotel());
 		}
-		return getCollectionController().getRoomItems(); 
+		PmsCollectionsController pmsCollections = (PmsCollectionsController)AonUtil.getRegisteredBean(IPmsConstants.COLLECTIONS_CONTROLLER_NAME);
+		return pmsCollections.getRoomItems(); 
 	}
 	
 	private List<SelectItem> getHotelRoomItems(Hotel hotel) throws ManagerBeanException {
