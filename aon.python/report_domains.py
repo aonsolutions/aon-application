e7379c45ffa81e86bee9c941b1ede753d7d7b2ec
Euke Castellano <ecastellano@esferalia.com>
2012-09-12 16:36:54 +0000

    Mejora en la columnas y soporte para academy

--- aon.python/report_domains.py
+++ aon.python/report_domains.py
@@ -21,7 +21,7 @@ class ListDomains(object):
         try: 
             
             system_domain_modules = ("MARKETING","COMERCIAL","GESTION","TESORERIA"
-                               ,"ALMACEN","EXPEDIENTES","CONTABILIDAD","FISCAL","LABORAL","DOCUMENTAL")
+                               ,"ALMACEN","EXPEDIENTES","CONTABILIDAD","FISCAL","LABORAL","DOCUMENTAL","GARAGE","ACADEMY")
             
             conn = self.__arguments.get_connection(nodatabase=True)
             
@@ -32,36 +32,58 @@ class ListDomains(object):
             cur.execute(stmt)
             schemas = cur.fetchall()
             
-            print "DATABASE|ID DOMINIO|DOMINIO PADRE|NOMBRE DOMINIO|ALMACENAMIENTO|USUARIOS|MULTIDOMINIO|EXTENSIONES|",
+            print "DATABASE|ACTIVO|ID DOMINIO|NOMBRE DOMINIO|DOMINIO PADRE|ALMACENAMIENTO|USUARIOS|MULTIDOMINIO|CREADOR|EXTENSIONES|",
             for dm  in system_domain_modules:
                 print dm + "|",
             print
             
             for schema in schemas:
                 dom_cur = conn.cursor()
-                stmt = "SELECT id,name,parent,maxTotalDocumentSize,maxDefinedUsers,domainManagement FROM `"+schema[0]+"`.`domain` ORDER BY `parent`,`name`";
+                stmt = "SELECT id,name,parent,maxTotalDocumentSize,maxDefinedUsers,domainManagement,owner,active FROM `"+schema[0]+"`.`domain` ORDER BY `parent`,`name`";
                 dom_cur.execute(stmt)
                 domains = dom_cur.fetchall()
                 for domain in domains:
                     domain_name = domain[1]
-                    print schema[0] + "|"+ str(domain[0])+ "|" + domain[1] + "|" + str(domain[2])+ "|",
+                    
+                    print schema[0] + "|",
+                    
+                    if domain[7] == None or domain[7] == "1":
+                        print "|",
+                    else:
+                        print "NO" + "|",
+
+                    print str(domain[0])+ "|",
+                    print domain[1] + "|",
+                    
+                    if domain[2] == None:
+                        print "|",
+                    else:
+                        print str(domain[2]) + "|",
+
                     if domain[3] == None:
                         print "0" + "|",
                     else:
                         print str(domain[3]) + "|",
+                        
                     if domain[4] == None:
                         print "0" + "|",
                     else:
                         print str(domain[4]) + "|",
-                    if domain[5] == None:
-                        print "0" + "|",
+                        
+                    if domain[5] == None or domain[5] == "0":
+                        print "NO" + "|",
                     else:
-                        print str(domain[5]) + "|",
+                        print "SI" + "|",
+                        
+                    if domain[6] == None:
+                        print " " + "|",
+                    else:
+                        print str(domain[4]) + "|",
                         
                     mod = conn.cursor()
                     mod.execute("SELECT module FROM `"+schema[0]+"`.domain_application_module WHERE domain = %s",(str(domain[0]),))
                     modules = mod.fetchall()
-                    domain_modules = ["NO","NO","NO","NO","NO","NO","NO","NO","NO","NO"]
+                    domain_modules = ["NO","NO","NO","NO","NO","NO","NO","NO","NO","NO","NO","NO"]
                     for module in modules:
                         domain_modules[module[0]] = "SI"
                     mod.close
