75bff1b6d4e2a4d8a3db9b86e435f21f938d0a2a
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-10-17 08:48:00 +0000

    PAYROLL. Se corrigen errores en la introduccion de percepciones desde el borrador. Se añade el centro de trabajo en la pantalla de busqueda de contratos.

--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/salary/draft/SalaryDraftPaymentController.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/salary/draft/SalaryDraftPaymentController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.enumeration.Month;
 import com.code.aon.common.util.CommonUtil;
@@ -23,16 +24,25 @@ import com.esferalia.aon.payroll.AgreementPayment;
 import com.esferalia.aon.payroll.Contract;
 import com.esferalia.aon.payroll.ContractPayment;
 import com.esferalia.aon.payroll.calculator.IContractPayment;
+import com.esferalia.aon.payroll.enumeration.QuoteType;
+import com.esferalia.aon.payroll.enumeration.TaxationType;
 import com.esferalia.aon.entity.IEntityAlias;
 import com.esferalia.aon.salary.enumeration.SalaryType;
 import com.esferalia.aon.salary.expression.ExpressionScope;
+import com.esferalia.aon.ui.payroll.controller.IPaymentHandler;
 import com.esferalia.aon.ui.payroll.controller.IPayrollConstants;
 import com.esferalia.aon.ui.payroll.controller.contract.ContractDetailVariableController;
 import com.esferalia.aon.ui.payroll.controller.contract.ContractPaymentVariableHandler;
 
-public class SalaryDraftPaymentController extends ContractDetailVariableController{
+public class SalaryDraftPaymentController extends ContractDetailVariableController implements IPaymentHandler{
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(SalaryDraftPaymentController.class.getName());
+	
+	private boolean quoteExpressionEdition;
+	private boolean irpfExpressionEdition;
+	private boolean enableExpressionEdition;
+	private TaxationType taxation;
+	private QuoteType quote;
 
 	public void onPaymentConceptChange(ActionEvent event) {
 		ContractPayment cp = (ContractPayment) getTo();
@@ -43,9 +53,9 @@ public class SalaryDraftPaymentController extends ContractDetailVariableControll
 	
 	@Override
 	public void onEdit(ActionEvent event) {
-		reset(true);
 		setSelectedPayment(event);
 		onReloadExpression(event);
+		reset(true);
 	}
 
 	public void onSave(ActionEvent event) {
@@ -55,7 +65,7 @@ public class SalaryDraftPaymentController extends ContractDetailVariableControll
 		}
 		SalaryDraftController master = (SalaryDraftController) FormUtil.getController(IPayrollConstants.SALARY_DRAFT_CONTROLLER);
 		cp.setContract((Contract) master.getTo());
-		super.onAccept(event);
+		super.accept(event);
 		reset(true);
 	}
 
@@ -81,24 +91,34 @@ public class SalaryDraftPaymentController extends ContractDetailVariableControll
 	private void setSelectedPayment(ActionEvent event){
 		SalaryDraftController controller = (SalaryDraftController) AonUtil.getRegisteredBean(IPayrollConstants.SALARY_DRAFT_CONTROLLER);
 		IContractPayment payment = (IContractPayment) controller.getPaymentsModel().getRowData();
-		if(payment.getScope()==ExpressionScope.CONTRACT){
-			this.setTo((ContractPayment) payment);
-		} else if(payment.getScope()==ExpressionScope.AGREEMENT){
-			super.onReset(event);
-			ContractPayment cp = (ContractPayment) this.getTo();
-			AgreementPayment alp = (AgreementPayment) payment;
-			cp.setContract((Contract) controller.getTo()); 
-			cp.setType(alp.getType()); 
-			cp.setPaymentConcept(alp.getPaymentConcept()); 
-			cp.setDescription(alp.getDescription()); 
-			cp.setExpression(alp.getExpression()); 
-			cp.setIrpfExpression(alp.getIrpfExpression()); 
-			cp.setQuoteExpression(alp.getQuoteExpression()); 
-			cp.setStartDate(alp.getStartDate()); 
-			cp.setEndDate(alp.getEndDate()); 
-			cp.setMonth(alp.getMonth()); 
-			cp.setDescriptionDecorable(alp.isDescriptionDecorable());
-			cp.setSalaryType(alp.getSalaryType());
+		try {
+			if(payment.getScope()==ExpressionScope.CONTRACT){
+				this.select(event, (ITransferObject) payment);
+				onReloadExpression(event);
+			} else if(payment.getScope()==ExpressionScope.AGREEMENT){
+				super.onReset(event);
+				ContractPayment cp = (ContractPayment) this.getTo();
+				AgreementPayment alp = (AgreementPayment) payment;
+				cp.setContract((Contract) controller.getTo()); 
+				cp.setType(alp.getType()); 
+				cp.setPaymentConcept(alp.getPaymentConcept()); 
+				cp.setDescription(alp.getDescription()); 
+				cp.setExpression(alp.getExpression()); 
+				cp.setIrpfExpression(alp.getIrpfExpression()); 
+				cp.setQuoteExpression(alp.getQuoteExpression()); 
+				cp.setStartDate(alp.getStartDate()); 
+				cp.setEndDate(alp.getEndDate()); 
+				cp.setMonth(alp.getMonth()); 
+				cp.setDescriptionDecorable(alp.isDescriptionDecorable());
+				cp.setSalaryType(alp.getSalaryType());
+				
+				onReloadExpression(event);
+			}
+		} catch (ManagerBeanException e) {
+			String msg = "Imposible seleccionar la percepcion";
+			LOGGER.error(msg);
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
 		}
 	}
 	
@@ -165,6 +185,131 @@ public class SalaryDraftPaymentController extends ContractDetailVariableControll
 	@Override
 	public String getExpression(){
 		return ((ContractPayment)getTo()).getExpression()!=null?((ContractPayment)getTo()).getExpression():((ContractPayment)getTo()).getPaymentConcept().getExpression();
+	}
+
+	@Override
+	public String getQuoteExpression() {
+		ContractPayment cp = ((ContractPayment) getTo());
+		return cp.getQuoteExpression();
+	}
+	@Override
+	public String getIrpfExpression() {
+		ContractPayment cp = ((ContractPayment) getTo());
+		return cp.getIrpfExpression();
+	}
+
+	@Override
+	public void setExpression(String expression) {
+		ContractPayment cp = ((ContractPayment) getTo());
+		cp.setExpression(expression);
+	}
+	@Override
+	public void setQuoteExpression(String expression) {
+		ContractPayment cp = ((ContractPayment) getTo());
+		cp.setQuoteExpression(expression);
+	}
+	@Override
+	public void setIrpfExpression(String expression) {
+		ContractPayment cp = ((ContractPayment) getTo());
+		cp.setIrpfExpression(expression);
+	}
+
+	public boolean isEnableExpressionEdition() {
+		return enableExpressionEdition;
+	}
+	public void setEnableExpressionEdition(boolean enableExpressionEdition) {
+		this.enableExpressionEdition = enableExpressionEdition;
+	}
+	public boolean isQuoteExpressionEdition() {
+		return quoteExpressionEdition;
+	}
+	public void setQuoteExpressionEdition(boolean quoteExpressionEdition) {
+		this.quoteExpressionEdition = quoteExpressionEdition;
+	}
+	public boolean isIrpfExpressionEdition() {
+		return irpfExpressionEdition;
+	}
+	public void setIrpfExpressionEdition(boolean irpfExpressionEdition) {
+		this.irpfExpressionEdition = irpfExpressionEdition;
+	}
+
+	@Override
+	public void onSelectExpressionEdition(ActionEvent event){
+		setEnableExpressionEdition( !isEnableExpressionEdition() );
+	}
+
+	@Override
+	public TaxationType getTaxation() {
+		if(taxation==null){
+			taxation = obtainTaxationType();
+		}
+		return taxation;
+	}
+
+	public void setTaxation(TaxationType taxation) {
+		this.taxation = taxation;
+		changeIrpfExpression();
+	}
+
+	@Override
+	public QuoteType getQuote() {
+		if(quote==null){
+			quote = obtainQuoteType();
+		}
+		return quote;
+	}
+
+	public void setQuote(QuoteType quote) {
+		this.quote = quote;
+		changeQuoteExpression();
+	}
+	
+	private void changeQuoteExpression() {
+		if(quote==QuoteType.QUOTE){
+			this.setQuoteExpression(this.getExpression());
+		}else if(quote==QuoteType.NO_QUOTE){
+			this.setQuoteExpression("0");
+		}else if(quote==QuoteType.IPREM_EXCESS){
+			this.setQuoteExpression(IPayrollConstants.IPREM_FORMMULA);
+		} else {
+			this.setQuoteExpression(null);
+		}
+	}
+
+	private void changeIrpfExpression() {
+		if(taxation==TaxationType.TAXED){
+			this.setIrpfExpression(this.getExpression());
+		}else if(taxation==TaxationType.NO_TAXED){
+			this.setIrpfExpression(IPayrollConstants.ZERO_VALUE);
+		} else {
+			this.setIrpfExpression(null);
+		}
+	}
+	
+	private QuoteType obtainQuoteType() {
+		if(this.getQuoteExpression()==null) {
+			return null;
+		} else if(this.getQuoteExpression().equals(this.getExpression())){
+			return QuoteType.QUOTE;
+		} else if(this.getQuoteExpression().equals(IPayrollConstants.ZERO_VALUE)){
+			return QuoteType.NO_QUOTE;
+		} else if(this.getQuoteExpression().equals(IPayrollConstants.IPREM_FORMMULA)){
+			return QuoteType.IPREM_EXCESS;
+		} else {
+			return QuoteType.MANUAL;
+		}
+	}
+	
+	private TaxationType obtainTaxationType() {
+		if(this.getIrpfExpression()==null){
+			return null;
+		} else if(this.getIrpfExpression().equals(this.getExpression())){
+			return TaxationType.TAXED;
+		} else if(this.getIrpfExpression().equals(IPayrollConstants.ZERO_VALUE)){
+			return TaxationType.NO_TAXED;
+		} else {
+			return TaxationType.MANUAL;
+		}
 	}
 		
 }
--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/event/contract/ContractSearchListener.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/event/contract/ContractSearchListener.java
@@ -2,14 +2,21 @@ package com.esferalia.aon.ui.payroll.event.contract;
 
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
 
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.company.Enterprise;
+import com.code.aon.company.WorkPlace;
 import com.code.aon.person.Person;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ui.config.util.UserUtils;
 import com.code.aon.ui.form.event.ControllerSearchListener;
 import com.esferalia.aon.entity.IEntityAlias;
 
@@ -17,6 +24,7 @@ public class ContractSearchListener extends ControllerSearchListener {
 
 	private Person person;
 	private Enterprise enterprise;
+	private WorkPlace workPlace;
 	private Date endDate; 
 	
 	public Date getEndDate() {
@@ -39,6 +47,14 @@ public class ContractSearchListener extends ControllerSearchListener {
 		return enterprise;
 	}
 
+	public WorkPlace getWorkPlace() {
+		return workPlace;
+	}
+
+	public void setWorkPlace(WorkPlace workPlace) {
+		this.workPlace = workPlace;
+	}
+
 	public void setEnterprise(Enterprise enterprise) {
 		this.enterprise = enterprise;
 	}
@@ -49,6 +65,7 @@ public class ContractSearchListener extends ControllerSearchListener {
 		setPerson((Person) personBean.createNewTo());
 		IManagerBean enterpriseBean = BeanManager.getManagerBean(Enterprise.class);
 		setEnterprise((Enterprise) enterpriseBean.createNewTo());
+		setWorkPlace(null);
 	}
 	
 	@Override
@@ -56,12 +73,28 @@ public class ContractSearchListener extends ControllerSearchListener {
 		if ((getPerson() != null) && (getPerson().getId() != null)) {
 			criteria.addEqualExpression(getFieldName(IEntityAlias.CONTRACT_PERSON_ID), getPerson().getId());			
 		}
-		if ((getEnterprise() != null) && (getEnterprise().getId() != null)) {
+		if ((getWorkPlace() != null) && (getWorkPlace().getId() != null)) {
+			criteria.addEqualExpression(getFieldName(IEntityAlias.CONTRACT_WORK_PLACE_ID), getWorkPlace().getId());			
+		} else if ((getEnterprise() != null) && (getEnterprise().getId() != null)) {
 			criteria.addEqualExpression(getFieldName(IEntityAlias.CONTRACT_WORK_PLACE_ENTERPRISE_ID), getEnterprise().getId());			
 		}
-//		Expression expr1 = ExpressionUtilities.getGreaterThanOrEqualExpression(getFieldName(IEntityAlias.CONTRACT_END_DATE), new Date());
-//		Expression expr2 = ExpressionUtilities.getNullExpression(getFieldName(IEntityAlias.CONTRACT_END_DATE));
-//		criteria.addExpression(ExpressionUtilities.getOrExpression(expr1, expr2));			
 	}
+	
+	public List<SelectItem> getWorkPlaces() throws ManagerBeanException {
+		List<SelectItem> workPlaces = new LinkedList<SelectItem>();
+		if ((getEnterprise() != null) && (getEnterprise().getId() != null)) {
+			IManagerBean workPlaceBean = BeanManager.getManagerBean(WorkPlace.class);
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(workPlaceBean.getFieldName(IEntityAlias.WORK_PLACE_ACTIVE), new Boolean(true));
+			criteria.addEqualExpression(workPlaceBean.getFieldName(IEntityAlias.WORK_PLACE_ENTERPRISE_ID), getEnterprise().getId());
+			UserUtils.getInstance().addScopeFilterToCriteria(criteria, workPlaceBean.getFieldName(IEntityAlias.WORK_PLACE_SCOPE_ID));
+			criteria.addOrder(workPlaceBean.getFieldName(IEntityAlias.WORK_PLACE_DESCRIPTION));
+			for(ITransferObject to: workPlaceBean.getList(criteria)){
+				WorkPlace workPlace = (WorkPlace)to;
+				workPlaces.add(new SelectItem(workPlace, workPlace.getDescription()));
+			}
+		}
+		return workPlaces;
+	}	
 
 }
