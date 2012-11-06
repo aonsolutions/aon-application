ce872615d3ad3e8ef270e55015e1f21495e0025f
Gorka Irazu <girazu@esferalia.com>
2012-11-06 09:48:52 +0000

    PMS. Cancelacion de Reserva. Al Cancelar Reserva, si esta en estado Bloqueada, se pasa a estado Cancelada directamente sin atender a la respuesta del CRS.

--- aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/controller/ProjectReservationController.java
+++ aon-ui-pms/src/main/java/com/esferalia/aon/ui/pms/controller/ProjectReservationController.java
@@ -495,7 +495,7 @@ public class ProjectReservationController extends BasicController implements IPm
 		if (isConfirmNoShow()) {
 			reservation.setCheckStatus(ReservationCheckStatus.NO_SHOW);
 		}
-		reservation.setStatus(cancelOk ? ReservationStatus.CANCELLED : ReservationStatus.BLOCKED);
+		reservation.setStatus((cancelOk || reservation.isBlocked()) ? ReservationStatus.CANCELLED : ReservationStatus.BLOCKED);
 		accept(event);
 
 		IController reservationRoomController = (IController)AonUtil.getRegisteredBean(IPmsConstants.RESERVATION_ROOM_CONTROLLER_NAME);
