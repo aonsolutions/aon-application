c6cb3492f605e0f14aebad6b99ebf82f5f7006f3
Aimar Tellitu <atellitu@esferalia.com>
2012-11-15 16:41:23 +0000

    Incomplete - #3082: Añadir AMBITO a cuestionarios y plantillas http://dev.esferalia.com/trac/ticket/3082

--- aon-marketing/src/main/java/com/code/aon/marketing/Survey.java
+++ aon-marketing/src/main/java/com/code/aon/marketing/Survey.java
@@ -8,11 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.code.aon.config.IScopable;
 import com.esferalia.aon.entity.master.SurveyDB;
 
 @Entity
 @Table(name = "survey")
-public class Survey extends SurveyDB {
+public class Survey extends SurveyDB implements IScopable {
 
 	private static final long serialVersionUID = 1L;
 
--- aon-marketing/src/main/java/com/code/aon/marketing/Template.java
+++ aon-marketing/src/main/java/com/code/aon/marketing/Template.java
@@ -3,11 +3,12 @@ package com.code.aon.marketing;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.code.aon.config.IScopable;
 import com.esferalia.aon.entity.master.TemplateDB;
 
 @Entity
 @Table(name = "mk_template")
-public class Template extends TemplateDB  {
+public class Template extends TemplateDB implements IScopable {
 
 	private static final long serialVersionUID = 1L;
 	
