50b22ccafc6f1272c23ffdab2113d71321a4aac7
Euke Castellano <ecastellano@esferalia.com>
2012-11-15 20:41:07 +0000

    #2006 - Realizada la visualización/navegación de las facturas en/desde la ficha de amortización.

--- aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/amortization/AmortizationController.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/amortization/AmortizationController.java
@@ -6,10 +6,9 @@ import java.util.List;
 
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
-import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 
 import com.code.aon.account.Account;
@@ -20,16 +19,25 @@ import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.finance.Invoice;
+import com.code.aon.finance.enumeration.InvoiceType;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ui.accounting.IAccountingConstants;
+import com.code.aon.ui.finance.controller.IFinanceConstants;
+import com.code.aon.ui.finance.controller.InvoiceController;
 import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.util.AonUtil;
 import com.esferalia.aon.entity.IEntityAlias;
 
+/**
+ * @author tyrion
+ *
+ */
 public class AmortizationController extends BasicController {
 
-	private DataModel invoices;
+	private List<ITransferObject> invoices;
+	private Integer invoiceId;
 	
 	private boolean salePanelVisible;
 	private String selectedTab;
@@ -225,15 +233,21 @@ public class AmortizationController extends BasicController {
 		}		
 	}
 	
-	public DataModel getInvoices() {
+	public List<ITransferObject> getInvoices() {
 		if (invoices == null) {
 			resetInvoices();
 		}
 		return invoices;
 	}
-	
-	public void setInvoices(DataModel invoices) {
+	public void setInvoices(List<ITransferObject> invoices) {
 		this.invoices = invoices;
+	}
+	
+	public Integer getInvoiceId() {
+		return invoiceId;
+	}
+	public void setInvoiceId(Integer invoiceId) {
+		this.invoiceId = invoiceId;
 	}
 	
 	public void resetInvoices() {
@@ -244,12 +258,56 @@ public class AmortizationController extends BasicController {
 			Criteria criteria = new Criteria();
 			criteria.addEqualExpression(bean.getFieldName(IEntityAlias.AMORTIZATION_INVOICE_AMORTIZATION_ID), am.getId());
 			List<ITransferObject> list = bean.getList(criteria);
-			setInvoices(new ListDataModel( list ));
+			setInvoices(list);
 		} catch (ManagerBeanException e) {
 			String msg = "No se pudo determinar las facturas vinculadas. [" + e.getMessage() + "]";
 			AonUtil.addErrorMessage(msg);
-			setInvoices(new ListDataModel( ));
+			setInvoices(new LinkedList<ITransferObject>() );
 			throw new AbortProcessingException(msg, e);
 		}		
 	}
+	public boolean isInvoiceNavigationDisabled() {
+		return ( getBackAction() != null);
+	}
+	
+	public String showInvoice() {
+		try {
+			String invoiceViewer = null;
+			Invoice invoice = null;
+			for (ITransferObject to :  getInvoices() ) {
+				AmortizationInvoice inv = (AmortizationInvoice) to;
+				if (ObjectUtils.equals(getInvoiceId(), inv.getInvoice().getId())) {
+					invoice = inv.getInvoice();
+					break;
+				}
+			}
+			if (invoice == null) {
+				String message = "Imposible encontrar laa factura entre las listadas.";
+				AonUtil.addErrorMessage(message);
+				throw new AbortProcessingException(message);
+			}
+			String invoiceControllerName = "";
+			if (invoice.getType() == InvoiceType.SALES) {
+				invoiceControllerName = IFinanceConstants.SALE_INVOICE_CONTROLLER_NAME;
+				invoiceViewer = IFinanceConstants.SALE_INVOICE_FORM_NAME;
+			} else if (invoice.getType() == InvoiceType.PURCHASE) {
+				invoiceControllerName = IFinanceConstants.PURCHASE_INVOICE_CONTROLLER_NAME;
+				invoiceViewer = IFinanceConstants.PURCHASE_INVOICE_FORM_NAME;
+			} else if (invoice.getType() == InvoiceType.EXPENSES) {
+				invoiceControllerName = IFinanceConstants.EXPENSE_INVOICE_CONTROLLER_NAME;
+				invoiceViewer = IFinanceConstants.EXPENSE_INVOICE_FORM_NAME;
+			} else if (invoice.getType() == InvoiceType.UNDEDUCTIBLE) {
+				invoiceControllerName = IFinanceConstants.UNDEDUCTIBLE_INVOICE_CONTROLLER_NAME;
+				invoiceViewer = IFinanceConstants.UNDEDUCTIBLE_INVOICE_FORM_NAME;
+			}
+			InvoiceController invoiceController = (InvoiceController) AonUtil.getRegisteredBean(invoiceControllerName);
+			invoiceController.onLoad(null, invoice.getId(), IAccountingConstants.AMORTIZATION_FORM_NAVKEY, null);
+			return invoiceViewer;
+		} catch (ManagerBeanException e) {
+			String message = "Imposible navegar a la factura";
+			AonUtil.addErrorMessage(message);
+			throw new AbortProcessingException(message,e);
+		}
+	}
+	
 }
