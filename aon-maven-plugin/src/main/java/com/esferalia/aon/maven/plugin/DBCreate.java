ac1d866fcde45a18309ebfcd4c718fe78105bbcb
Euke Castellano <ecastellano@esferalia.com>
2012-11-21 15:56:05 +0000

    Incomplete #3126 (Creación de la base de datos aon_reveng, para la generación de los entities en Jenkins.)

---
+++ aon-maven-plugin/src/main/java/com/esferalia/aon/maven/plugin/DBCreate.java
@@ -0,0 +1,213 @@
+/**
+ * 
+ */
+package com.esferalia.aon.maven.plugin;
+
+/********************************************************************
+* Copyright (c) 2011, esferalia NETWORKS S.A
+*
+* The copyright of the computer program herein is the property 
+* of esferalia NETWORKS.
+*********************************************************************
+* The program may be used and/or copied only with the written 
+* permission of esferalia NETWORKS, or in accordance with the 
+* terms and conditions stipulated in the agreement contract 
+* under which the program has been supplied.
+*********************************************************************
+*/
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+
+import com.code.aon.dbutils.AonSQLException;
+import com.code.aon.master.VersionManager;
+
+/**
+ * @author rtrepiana
+ *
+ * @goal 	create-db
+ * @phase	generate-sources	
+ * @requiresDependencyResolution
+ * @threadSafe
+ *  
+ */
+public class DBCreate extends AbstractMojo {
+	
+	private static final Object LOCK = new Object();
+	
+    // ----------------------------------------------------------------------
+    // Mojo parameters
+    // ----------------------------------------------------------------------
+
+    /**
+     * Specifies the variable name in template's context for tables.
+     * 
+     * @parameter default-value="tables"
+     * @readonly
+     */
+    protected String tablesVariable;
+
+    /**
+     * Specifies the variable name in template's context for targetPackage.
+     * 
+     * @parameter default-value="package"
+     * @readonly
+     */
+    protected String packageVariable;
+
+    /**
+     * Specifies the directory containing template files.
+     * 
+     * @parameter default-value="com/esferalia/aon/master/vm/"
+     * @readonly
+     */
+    protected String sourceDirectory;
+
+    /**
+     * The Maven Project Object
+     * 
+     * @parameter expression="${project}"
+     * @readonly
+     */
+    protected MavenProject project;
+
+    /**
+     * The maven project's helper.
+     * 
+     * @component role="org.apache.maven.project.MavenProjectHelper"
+     * @readonly
+     */
+    private MavenProjectHelper projectHelper;
+
+    // ----------------------------------------------------------------------
+    // aon parameters
+    // ----------------------------------------------------------------------
+
+    /**
+     * Host to connect to.
+     * 
+     * @parameter default-value="127.0.0.1"
+     */
+    private String dbHost ;
+	
+    /**
+     * Port number to use for connection.
+     * 
+     * @parameter default-value="3306"
+     */
+    private String dbPort;
+
+    /**
+     *  
+     * @parameter default-value="aon-master"
+     */
+    private String dbName ;
+    
+    /**
+     * User for login.
+     * 
+     * @parameter default-value="dbuser"
+     */
+    private String dbUser ;
+
+    /**
+     * Password to use when connecting to server.
+     * 
+     * @parameter default-value="serubd2000"
+     */
+    private String dbPasswd ;
+    
+    @Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		synchronized( LOCK) {
+	    	validateParameters();
+	    	
+	    	Connection connection = null;
+	        try {
+	        
+	        	connection = getConnection(null);
+		        dropDataBase( connection , dbName );
+		        VersionManager versionManager = new VersionManager();
+		    	getLog().info("Creating database: " + dbName);
+		        versionManager.createDatabase(connection, dbName);
+		        connection.close();
+				connection = getConnection(dbName);
+		        versionManager.uptodateDatabase(connection);
+		    	getLog().info("Created!");
+			} catch ( SQLException e ) {
+		    	getLog().error(e.getMessage());
+				throw new MojoExecutionException( e.getMessage(), e );
+			} catch ( AonSQLException e ) {
+		    	getLog().error(e.getMessage());
+				throw new MojoExecutionException( e.getMessage(), e );
+			} finally {
+				if ( connection != null ) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+					}
+				}
+			}
+		}
+	}
+    
+    /**
+     * Drops database ..
+     */
+    private void dropDataBase(Connection connection, String dbName) 
+    	throws SQLException {
+    	Statement stmt  = null;
+    	try  {
+	    	stmt = connection.createStatement();
+	    	String sentence = "DROP DATABASE `" + dbName + "`"; 
+	    	getLog().info("Trying to drop database: " + sentence);
+	    	stmt.execute(sentence);
+    	}catch (SQLException e) {
+	    	getLog().warn("Database not dropped: " + e.getMessage());
+		}
+    	finally{
+    		if ( stmt != null )
+    			stmt.close();
+    	}
+    }
+    
+    /**
+     * dbtables is required
+     * 
+     * @throws MojoExecutionException
+     */
+    private void validateParameters() throws MojoExecutionException {
+    	
+    }
+    
+    private Connection getConnection(String dbName) throws MojoExecutionException {
+    	try {
+    		// first of all load JDBC driver
+    		Class.forName("org.gjt.mm.mysql.Driver");
+    	}catch (ClassNotFoundException e) {
+    		throw new MojoExecutionException( e.getMessage(), e );
+		}
+    	String url = String.format("jdbc:mysql://%s:%s/%s", dbHost, dbPort, dbName != null ? dbName : "");
+    	getLog().info("Database creation URL:" + url);
+    	try {
+			Connection c = DriverManager.getConnection(url, dbUser, dbPasswd);
+	    	getLog().info("Connected!");
+	    	return c;
+		} catch (SQLException e) {
+	    	getLog().error(e.getMessage());
+    		throw new MojoExecutionException( e.getMessage(), e );
+		}
+    	
+    }
+
+
+}
