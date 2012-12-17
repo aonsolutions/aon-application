bf7a363ad8b39eb82a7ef08b239d4f90207b6a58
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-12-17 16:38:44 +0000

    PAYROLL. Las incidencias de contrato ahora se guardan en ContractData

--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/contract/ContractCalendarEventController.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/controller/contract/ContractCalendarEventController.java
@@ -1,135 +1,92 @@
 package com.esferalia.aon.ui.payroll.controller.contract;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 
-import com.code.aon.common.dao.hibernate.HibernateUtil;
-import com.code.aon.common.dao.sql.DAOException;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.ast.Expression;
+import com.code.aon.ql.util.ExpressionUtilities;
 import com.code.aon.ui.form.LinesController;
 import com.code.aon.ui.util.AonUtil;
-import com.esferalia.aon.payroll.Contract;
-import com.esferalia.aon.payroll.ContractCalendarEvent;
+import com.esferalia.aon.entity.IEntityAlias;
+import com.esferalia.aon.payroll.enumeration.ContextVariable;
 
 public class ContractCalendarEventController extends LinesController {
-
-//	private static final Logger LOGGER = LoggerFactory.getLogger(ContractTrackingController.class.getName());
-
-	private ContractCalendarEvent event;
-	private boolean fullTime;
-	private Date startDate;
-	private Date endDate;
-	private boolean showMultipleTrackingWindow;
-
-	public Date getStartDate() {
-		return startDate;
+	
+	private boolean forceBackAction;
+	
+	private String forcedBackAction;
+	
+	public boolean isForceBackAction() {
+		return forceBackAction;
 	}
 
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-
-	public boolean isFullTime() {
-		return fullTime;
-	}
-
-	public void setFullTime(boolean fullTime) {
-		this.fullTime = fullTime;
-	}
-
-	public ContractCalendarEvent getEvent() {
-		if(event==null){
-			event = new ContractCalendarEvent();
-		}
-		return event;
-	}
-
-	public void setEvent(ContractCalendarEvent event) {
-		this.event = event;
+	public void setForceBackAction(boolean forceBackAction) {
+		this.forceBackAction = forceBackAction;
 	}
 	
-	public boolean isShowMultipleTrackingWindow() {
-		return showMultipleTrackingWindow;
+	public String getForcedBackAction() {
+		return forcedBackAction;
 	}
 
-	public void setShowMultipleTrackingWindow(boolean showMultipleTrackingWindow) {
-		this.showMultipleTrackingWindow = showMultipleTrackingWindow;
+	public void setForcedBackAction(String forcedBackAction) {
+		this.forcedBackAction = forcedBackAction;
 	}
 
-	public void onSelectFullTime(ActionEvent event){
-		if(isFullTime()){
-			
-		} else{
-			
-		}
+	public List<SelectItem> getCalendarEvents(){
+		List<SelectItem> list = new LinkedList<SelectItem>();
+		SelectItem item = new SelectItem(ContextVariable.HOLIDAYS.toString(), ContextVariable.HOLIDAYS.toString());
+		list.add(item);
+		item = new SelectItem(ContextVariable.SPECIAL_DAYS.toString(), ContextVariable.SPECIAL_DAYS.toString());
+		list.add(item);
+		item = new SelectItem(ContextVariable.SPECIAL_DAYS.toString(), ContextVariable.NO_HOLIDAYS.toString());
+		list.add(item);
+		return list;
 	}
 	
-	public void onMultipleTrackingShow(ActionEvent event)  {
-		setEvent(null);
-		setFullTime(false);
-		setStartDate(null);
-		setEndDate(null);
-	}
-	
-	
-	public void onAcceptMultipleTracking(ActionEvent event) {
-		boolean mustBeginTransaction = HibernateUtil.mustBeginTransaction();
-		boolean mustCloseSession = HibernateUtil.mustCloseSession();
-		String sessionName = HibernateUtil.getSessionFactoryName();
+	@Override
+	public void initializeModel() {
 		try {
-			try {
-				HibernateUtil.setBeginTransaction(false);
-				HibernateUtil.setCloseSession(false);
-				HibernateUtil.beginTransaction(sessionName);
-				// BEGIN operaciones de la transaccion
-				Calendar startDate = new GregorianCalendar();
-				Calendar endDate = new GregorianCalendar();
-				startDate.setTimeInMillis(getStartDate().getTime());
-				endDate.setTimeInMillis(getEndDate().getTime());
-				
-				ContractCalendarEvent ct;
-				while(startDate.before(endDate) || startDate.equals(endDate)){
-					ct = new ContractCalendarEvent();
-					ct.setContract(((Contract)getMasterController().getTo()));
-					ct.setDate(startDate.getTime());
-					ct.setType(getEvent().getType());
-					ct.setDuration(getEvent().getDuration());
-					this.getManagerBean().insert(ct);
-					startDate.add(Calendar.DATE, 1);
-				}
-				// FIN operaciones de la transaccion
-				HibernateUtil.getSession(sessionName).flush();
-				HibernateUtil.commitTransaction(sessionName);
-			} catch (Exception e) {
-				String msg = e.getMessage();
-				try {
-					HibernateUtil.rollbackTransaction(sessionName);
-				} catch (DAOException daoe) {
-					msg = "Unable to rollback transaction! (" + msg + ")";
-				}
-				AonUtil.addErrorMessage(msg);
-				throw new AbortProcessingException(e);
-			} finally {
-				HibernateUtil.closeSession(sessionName);
-			}
-		} finally {
-			HibernateUtil.setCloseSession(mustCloseSession);
-			HibernateUtil.setBeginTransaction(mustBeginTransaction);
+			String alias = this.getManagerBean().getFieldName(IEntityAlias.CONTRACT_DATA_NAME);
+			Expression exp1 = ExpressionUtilities.getEqualExpression(alias, ContextVariable.HOLIDAYS.toString());
+			Expression exp2 = ExpressionUtilities.getEqualExpression(alias, ContextVariable.SPECIAL_DAYS.toString());
+			Expression exp3 = ExpressionUtilities.getEqualExpression(alias, ContextVariable.NO_HOLIDAYS.toString());
+			Expression exp = ExpressionUtilities.getOrExpression(exp1, exp2);
+			exp = ExpressionUtilities.getOrExpression(exp, exp3);
+			getCriteria().addExpression( exp );
+			super.initializeModel();
+		} catch (ManagerBeanException e) {
+			String message = "Hubo un error al cargar las indidencias de contrato.";
+			AonUtil.addErrorMessage(message);
+			throw new AbortProcessingException(message);
 		}
-		onSearch(event);
-		
 	}
-
+	
+	@Override
+	public void onReset(ActionEvent arg0) {
+		if(isForceBackAction()){
+			setForcedBackAction(getBackAction());
+		}
+		super.onReset(arg0);
+		if(isForceBackAction()){
+			setBackAction(getForcedBackAction());
+		}
+	}
+	
+	@Override
+	public void onSelect(ActionEvent event) {
+		if(isForceBackAction()){
+			setForcedBackAction(getBackAction());
+		}
+		super.onSelect(event);
+		if(isForceBackAction()){
+			setBackAction(getForcedBackAction());
+		}
+	}
+	
 	
 }
