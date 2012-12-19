89bd4f6e6b26b99ec9db577c36eb93498ac6ff41
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2012-12-19 16:54:13 +0000

    PAYROLL. Herencia de registros desde el dominio hijo: Pais/Provincia

--- aon-geozone/src/main/java/com/code/aon/geozone/GeoZone.java
+++ aon-geozone/src/main/java/com/code/aon/geozone/GeoZone.java
@@ -3,10 +3,12 @@ package com.code.aon.geozone;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.code.aon.common.annotations.Heritable;
 import com.esferalia.aon.entity.master.GeoZoneDB;
 
 @Entity
 @Table(name="geozone")
+@Heritable
 public class GeoZone extends GeoZoneDB {
 
 	private static final long serialVersionUID = 1L;
