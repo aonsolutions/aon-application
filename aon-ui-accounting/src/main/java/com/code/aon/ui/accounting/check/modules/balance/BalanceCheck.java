8b0a0e2d078a6e15c69c09bb7f4ab98f99a01deb
Euke Castellano <ecastellano@esferalia.com>
2013-01-02 17:11:58 +0000

    Fixeed #3146 Verificación de contabilidad-definición de balances en el padre

--- aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/check/modules/balance/BalanceCheck.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/check/modules/balance/BalanceCheck.java
@@ -3,6 +3,8 @@ package com.code.aon.ui.accounting.check.modules.balance;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.faces.model.SelectItem;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.code.aon.account.Account;
@@ -16,11 +18,14 @@ import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ui.accounting.IAccountingConstants;
 import com.code.aon.ui.accounting.check.AonCheckException;
 import com.code.aon.ui.accounting.check.CheckCategory;
 import com.code.aon.ui.accounting.check.CheckParams;
 import com.code.aon.ui.accounting.check.ICheckEntry;
 import com.code.aon.ui.accounting.check.ICheckModule;
+import com.code.aon.ui.accounting.controller.AccountingCollectionsController;
+import com.code.aon.ui.util.AonUtil;
 
 import com.esferalia.aon.entity.IEntityAlias;
 
@@ -42,68 +47,71 @@ public class BalanceCheck implements ICheckModule{
 			IManagerBean accountBean = BeanManager.getManagerBean(Account.class);
 			String codeAlias = accountBean.getFieldName(IEntityAlias.ACCOUNT_CODE);
 			String entryAlias = accountBean.getFieldName(IEntityAlias.ACCOUNT_ENTRY_ENABLED);
-			IManagerBean balanceBean = BeanManager.getManagerBean(Balance.class);
-			
-			for (ITransferObject to: balanceBean.getList(null)) {
-				Balance balance = (Balance) to;
-				if (balance.getType() != BalanceType.CUSTOM) {
-					if (balance.getLines() != null && balance.getLines().size() > 0) {
-						StringBuilder buf = new StringBuilder(","); 
-						for (BalanceDetail detail: balance.getLines() ) {
-							if (!detail.isInternalCalculation() && !StringUtils.isEmpty( detail.getAccounts())) {
-								if (buf.length() > 0) {
-									buf.append(",");
-								}
-								String accounts = detail.getAccounts();
-								if (StringUtils.isNotEmpty(accounts)) {
-									accounts = accounts.replace("(","");		
-									accounts = accounts.replace(")","");		
-									if (StringUtils.contains(accounts, "?")) {
-										accounts = accounts.replace("?","");
+			AccountingCollectionsController acc = (AccountingCollectionsController) AonUtil.getRegisteredBean(IAccountingConstants.ACCOUNTING_COLLECTIONS_CONTROLLER_NAME);
+			for (BalanceType bt : BalanceType.values()) {
+				List<SelectItem> balances = acc.getBalances(bt);
+				for (SelectItem si: balances) {
+					Balance balance = (Balance) si.getValue();
+					if (balance.getType() != BalanceType.CUSTOM) {
+						if (balance.getLines() != null && balance.getLines().size() > 0) {
+							StringBuilder buf = new StringBuilder(","); 
+							for (BalanceDetail detail: balance.getLines() ) {
+								if (!detail.isInternalCalculation() && !StringUtils.isEmpty( detail.getAccounts())) {
+									if (buf.length() > 0) {
+										buf.append(",");
 									}
-									buf.append( accounts );	
+									String accounts = detail.getAccounts();
+									if (StringUtils.isNotEmpty(accounts)) {
+										accounts = accounts.replace("(","");		
+										accounts = accounts.replace(")","");		
+										if (StringUtils.contains(accounts, "?")) {
+											accounts = accounts.replace("?","");
+										}
+										buf.append( accounts );	
+									}
+									
 								}
-								
 							}
-						}
-						if (buf.length() > 0) {
-							buf.append(",");
-						}
-						Criteria criteria = new Criteria();
-						criteria.addEqualExpression(entryAlias, true);
-						if (balance.getType() == BalanceType.CLOSING) {
-							criteria.addExpression(codeAlias, "1*|2*|3*|4*|5*");
-						} else if (balance.getType() == BalanceType.OPERATING) {
-							criteria.addExpression(codeAlias, "6*|7*");
-						} else if (balance.getType() == BalanceType.PATRIMONY) {
-							criteria.addExpression(codeAlias, "8*|9*");
-						}
-						List<ITransferObject> accounts = accountBean.getList(criteria);
-						for (ITransferObject accountTo: accounts) {
-							Account account = (Account) accountTo;
-							String id1 = "," + account.getCode().substring(0,1) + ",";
-							String id2 = "," + account.getCode().substring(0,2) + ",";
-							String id3 = "," + account.getCode().substring(0,3) + ",";
-							String id4 = "," + account.getCode().substring(0,4) + ",";
-							int count1 = StringUtils.countMatches(buf.toString(), id1);
-							int count2 = StringUtils.countMatches(buf.toString(), id2);
-							int count3 = StringUtils.countMatches(buf.toString(), id3);
-							int count4 = StringUtils.countMatches(buf.toString(), id4);
-							int sum = count1 + count2 + count3 + count4; 
-							if ( sum > 1) {
-								BalanceCheckEntry e = new BalanceCheckEntry();
-								e.setMessage( "[" + account.getCode() + "] Cuenta definida dos veces en el balance '" + balance.getName() + "'.");
-								e.setTo(account);
-								list.add(e);
-							} else if ( sum == 0) {
-								BalanceCheckEntry e = new BalanceCheckEntry();
-								e.setMessage( "[" + account.getCode() + "] Cuenta no reflejada en el balance '" + balance.getName() + "'" );
-								e.setTo(account);
-								list.add(e);
+							if (buf.length() > 0) {
+								buf.append(",");
+							}
+							Criteria criteria = new Criteria();
+							criteria.addEqualExpression(entryAlias, true);
+							if (balance.getType() == BalanceType.CLOSING) {
+								criteria.addExpression(codeAlias, "1*|2*|3*|4*|5*");
+							} else if (balance.getType() == BalanceType.OPERATING) {
+								criteria.addExpression(codeAlias, "6*|7*");
+							} else if (balance.getType() == BalanceType.PATRIMONY) {
+								criteria.addExpression(codeAlias, "8*|9*");
+							}
+							List<ITransferObject> accounts = accountBean.getList(criteria);
+							for (ITransferObject accountTo: accounts) {
+								Account account = (Account) accountTo;
+								String id1 = "," + account.getCode().substring(0,1) + ",";
+								String id2 = "," + account.getCode().substring(0,2) + ",";
+								String id3 = "," + account.getCode().substring(0,3) + ",";
+								String id4 = "," + account.getCode().substring(0,4) + ",";
+								int count1 = StringUtils.countMatches(buf.toString(), id1);
+								int count2 = StringUtils.countMatches(buf.toString(), id2);
+								int count3 = StringUtils.countMatches(buf.toString(), id3);
+								int count4 = StringUtils.countMatches(buf.toString(), id4);
+								int sum = count1 + count2 + count3 + count4; 
+								if ( sum > 1) {
+									BalanceCheckEntry e = new BalanceCheckEntry();
+									e.setMessage( "[" + account.getCode() + "] Cuenta definida dos veces en el balance '" + balance.getName() + "'.");
+									e.setTo(account);
+									list.add(e);
+								} else if ( sum == 0) {
+									BalanceCheckEntry e = new BalanceCheckEntry();
+									e.setMessage( "[" + account.getCode() + "] Cuenta no reflejada en el balance '" + balance.getName() + "'" );
+									e.setTo(account);
+									list.add(e);
+								}
 							}
 						}
 					}
 				}
+			
 			}
 		} catch (ManagerBeanException e) {
 			throw new AonCheckException(e.getMessage(),e);
