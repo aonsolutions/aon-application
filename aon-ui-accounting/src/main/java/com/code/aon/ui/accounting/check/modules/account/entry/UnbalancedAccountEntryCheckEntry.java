ed50a2828798ce6e5c242065d3d32caa786e8919
Euke Castellano <ecastellano@esferalia.com>
2013-02-21 17:58:01 +0000

    Implementación de la navegación desde la verificación de integridad a un apunte descuadrado.

--- aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/check/modules/account/entry/UnbalancedAccountEntryCheckEntry.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/check/modules/account/entry/UnbalancedAccountEntryCheckEntry.java
@@ -1,7 +1,20 @@
 package com.code.aon.ui.accounting.check.modules.account.entry;
 
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+
 import com.code.aon.accounting.AccountEntry;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ui.accounting.IAccountingConstants;
+import com.code.aon.ui.accounting.check.AonCheckException;
 import com.code.aon.ui.accounting.check.CheckEntryAdapter;
+import com.code.aon.ui.accounting.controller.entry.AccountEntryController;
+import com.code.aon.ui.form.FormUtil;
+import com.code.aon.ui.util.AonUtil;
+import com.esferalia.aon.entity.IEntityAlias;
 
 public class UnbalancedAccountEntryCheckEntry extends CheckEntryAdapter {
 
@@ -23,7 +36,33 @@ public class UnbalancedAccountEntryCheckEntry extends CheckEntryAdapter {
 
 	@Override
 	public String getFixActionLabel() {
-		return null;
+		return "Ver";
+	}
+
+	@Override
+	public void onFix(ActionEvent event) throws AonCheckException{
+		AccountEntry entry = (AccountEntry) getTo();
+		try {
+			AccountEntryController entryController = (AccountEntryController) FormUtil
+					.getController(IAccountingConstants.ACCOUNT_ENTRY_CONTROLLER_NAME);
+			Criteria criteria = new Criteria();
+			criteria.addEqualExpression(entryController.getManagerBean().getFieldName(IEntityAlias.ACCOUNT_ENTRY_ID), entry.getId());
+			entryController.setCriteria(criteria);
+			entryController.onSearch(null);
+			entryController.getModel().setRowIndex(0);
+			entryController.onSelect(null);
+			entryController.setBackAction("check_list");
+		} catch (ManagerBeanException e) {
+			String msg = "Error al cargar el apunte.";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		}
+		
+	}
+	
+	@Override
+	public String fixAction() throws AonCheckException {
+		return IAccountingConstants.ACCOUNT_ENTRY_FORM_NAVKEY;
 	}
 
 }
