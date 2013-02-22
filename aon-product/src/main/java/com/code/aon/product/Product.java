e6f3f922e59f7a47324b0b8bb7aaf9408db69f72
Aimar Tellitu <atellitu@esferalia.com>
2013-02-22 07:51:54 +0000

    TPV. Productos. Se ha marcado para que la tabla Producto sea heredable

--- aon-product/src/main/java/com/code/aon/product/Product.java
+++ aon-product/src/main/java/com/code/aon/product/Product.java
@@ -9,10 +9,12 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
+import com.code.aon.common.annotations.Heritable;
 import com.esferalia.aon.entity.master.ProductDB;
 
 @Entity
 @Table(name="product", uniqueConstraints = @UniqueConstraint(columnNames="code"))
+@Heritable
 public class Product extends ProductDB {
 	
 	private static final long serialVersionUID = 1L;
