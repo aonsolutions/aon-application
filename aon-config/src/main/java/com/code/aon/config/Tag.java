4ae47f22badf219f09bf8f93cdc6a17b9b641360
Aimar Tellitu <atellitu@esferalia.com>
2013-02-25 17:18:03 +0000

    La tabla Tag se ha pasado a config

--- aon-config/src/main/java/com/code/aon/config/Tag.java
+++ aon-config/src/main/java/com/code/aon/config/Tag.java
@@ -1,15 +1,15 @@
-package com.code.aon.registry;
+package com.code.aon.config;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.code.aon.common.annotations.Heritable;
-import com.esferalia.aon.entity.master.SegmentDB;
+import com.esferalia.aon.entity.master.TagDB;
 
 @Entity
 @Table(name="tag")
 @Heritable
-public class Tag extends SegmentDB {
+public class Tag extends TagDB {
 
 	private static final long serialVersionUID = 1L;
 	
