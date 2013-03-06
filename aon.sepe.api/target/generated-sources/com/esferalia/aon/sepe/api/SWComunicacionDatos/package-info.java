e53f0f379be0421f793b533839f39a06cbc7ed20
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2013-03-06 10:38:24 +0000

    Version inicial de la api para la comunicacion con SEPE a traves de web services

--- aon.sepe.api/target/generated-sources/com/esferalia/aon/sepe/api/SWComunicacionDatos/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://comunicacion.serviciosWeb.comunicacto", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.esferalia.aon.sepe.api.SWComunicacionDatos;
--- aon.sepe.api/target/generated-sources/com/esferalia/aon/sepe/api/SWConsultaDatos/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://consultaDatos.serviciosWeb.comunicacto", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.esferalia.aon.sepe.api.SWConsultaDatos;
