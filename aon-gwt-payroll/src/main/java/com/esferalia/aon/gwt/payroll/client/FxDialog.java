781ea562280670194519f8b8556bccefd79c099f
Raul Trepiana <rtrepiana@esferalia.com>
2013-03-08 14:09:53 +0000

    Will compile? That's the question .

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/FxDialog.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/FxDialog.java
@@ -9,11 +9,9 @@ import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
-import com.sun.star.beans.GetDirectPropertyTolerantResult;
 
 public class FxDialog extends CustomDialog {
 
