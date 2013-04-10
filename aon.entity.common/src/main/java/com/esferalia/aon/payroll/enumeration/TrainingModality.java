aa15c443189aca58c26083db9d77368191d17cb1
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2013-04-10 15:37:21 +0000

    cambios del hibernate.revenge para los cambios de la BD

---
+++ aon.entity.common/src/main/java/com/esferalia/aon/payroll/enumeration/TrainingModality.java
@@ -0,0 +1,24 @@
+package com.esferalia.aon.payroll.enumeration;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import com.code.aon.common.enumeration.IResourceable;
+
+public enum TrainingModality implements IResourceable {
+
+	DISTANCE,
+	MIX,
+	CLASSROOM,
+	PHONE
+	;      
+	
+    private static final String BASE_NAME = "com.esferalia.aon.payroll.i18n.messages";
+    private static final String MSG_KEY_PREFIX = "aon_enum_training_modality_";
+
+    public String getName(Locale locale) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
+    }
+    
+}
