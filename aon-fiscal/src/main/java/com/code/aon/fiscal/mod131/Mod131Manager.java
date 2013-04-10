e3c68e7b7baa9c0a58d726da86ecc0d1c974b769
Euke Castellano <ecastellano@esferalia.com>
2013-04-10 11:29:02 +0000

    * Error en el cálculo de la retenciones en el modelo 131. Sacaba las retenciones de las facturas de compras, cuando debían ser las de ventas.

--- aon-fiscal/src/main/java/com/code/aon/fiscal/mod131/Mod131Manager.java
+++ aon-fiscal/src/main/java/com/code/aon/fiscal/mod131/Mod131Manager.java
@@ -59,7 +59,7 @@ public class Mod131Manager extends FiscalModelManager {
 			+" INNER JOIN invoice_detail id ON (it.invoice_detail = id.id)" 
 			+" INNER JOIN invoice i ON (id.invoice = i.id)"
 			+" WHERE " + DomainManager.getStaticSQLWhereClause("i.domain")
-			+" AND i.type != 1 " 			// No Ventas
+			+" AND i.type = 1 " 			// Ventas
 			+" AND it.tax_type = 2" 		// IRPF
 			+" AND i.tax_date >= ?"
 			+" AND i.tax_date <= ?";
