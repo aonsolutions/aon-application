07afe33233644b714b9f3e294e9c00381b4b5eef
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2013-05-02 15:46:30 +0000

    Nuevos cambios de BD: Alta de nueva tabla (carrier) y nuevos campos para la direccion de envio de albaranes y pedidos de compra

---
+++ aon.entity.common/src/main/java/com/esferalia/aon/carrier/enumeration/ShipmentStatus.java
@@ -0,0 +1,45 @@
+package com.esferalia.aon.carrier.enumeration;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import com.code.aon.common.enumeration.IResourceable;
+
+/**
+ * Enummeration to identify the different shipments statuses.
+ */
+public enum ShipmentStatus implements IResourceable{
+	
+	PREPARED,
+
+	IN_AGENCY,
+
+	SHIPPING,
+
+	DELIVERED,
+
+	MISSING,
+
+	ERRONEOUS
+	;
+		
+    /** Message file base path. */
+    private static final String BASE_NAME = "com.esferalia.aon.carrier.i18n.messages";
+
+    /** Message key prefix. */
+    private static final String MSG_KEY_PREFIX = "aon_enum_carrier_shipment_status_";
+    
+    /**
+     * Returns a <code>String</code> with the transalation <code>Locale</code>
+     * for the locale.
+     * 
+     * @param locale Required Locale.
+     * 
+     * @return String a <code>String</code>.
+     */
+    public String getName(Locale locale) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
+    }
+
+}
