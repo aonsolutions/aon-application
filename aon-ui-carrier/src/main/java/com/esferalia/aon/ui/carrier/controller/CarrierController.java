a64d62b17864120734b5fddd3818620b7814dd15
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2013-05-02 15:52:52 +0000

    Version inicial de aon-carrier y aon-ui-carrier (agencias de transporte)

---
+++ aon-ui-carrier/src/main/java/com/esferalia/aon/ui/carrier/controller/CarrierController.java
@@ -0,0 +1,8 @@
+package com.esferalia.aon.ui.carrier.controller;
+
+import com.code.aon.ui.registry.controller.RegistryController;
+
+public class CarrierController extends RegistryController {
+	
+	
+}
