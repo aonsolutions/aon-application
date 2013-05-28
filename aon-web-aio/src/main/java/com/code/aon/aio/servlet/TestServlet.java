e10e8792a62da3b1f0be816bd36d1010282eaabd
Euke Castellano <ecastellano@esferalia.com>
2013-05-28 11:02:50 +0000

    Configuración del pool para schemas poco usados.

---
+++ aon-web-aio/src/main/java/com/code/aon/aio/servlet/TestServlet.java
@@ -0,0 +1,37 @@
+package com.code.aon.aio.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.code.aon.pool.AonConnectionException;
+import com.code.aon.ui.company.controller.CompanyDisplay;
+
+public class TestServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 2880941215591419695L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			String host = req.getParameter("aon.domain");
+			if (host == null || "".equals(host)){
+				host = req.getServerName();
+			}
+			CompanyDisplay cd = new CompanyDisplay();
+			cd.init( host );
+			resp.sendError(HttpServletResponse.SC_OK);
+		} catch (AonConnectionException e) {
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		} catch (Throwable e) {
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
+
+}
