09e5338ebc74d05bebad5e661e3d402f594bcc48
Gorka Irazu <girazu@esferalia.com>
2013-05-29 11:08:05 +0000

    TaxDetail Heredable

--- aon-config/src/main/java/com/code/aon/config/TaxDetail.java
+++ aon-config/src/main/java/com/code/aon/config/TaxDetail.java
@@ -3,10 +3,12 @@ package com.code.aon.config;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.code.aon.common.annotations.Heritable;
 import com.esferalia.aon.entity.master.TaxDetailDB;
 
 @Entity
 @Table(name="tax_detail")
+@Heritable
 public class TaxDetail extends TaxDetailDB {
 	
 	private static final long serialVersionUID = 1L;
