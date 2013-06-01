d82168f9389e0f883655da1cac3c720540eec482
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2013-06-01 15:39:18 +0000

    Incomplete. #3278 - Traspasos entre almacenes. Control Scope Impresion en excel de los listados de almacen

--- aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/event/WarehouseTransferSearchListener.java
+++ aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/event/WarehouseTransferSearchListener.java
@@ -1,24 +1,100 @@
 package com.code.aon.ui.warehouse.event;
 
-import com.code.aon.common.BeanManager;
-import com.code.aon.common.ManagerBeanException;
-import com.code.aon.product.Item;
-import com.code.aon.ui.registry.controller.event.RegistrySearchListener;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
-public class WarehouseTransferSearchListener extends RegistrySearchListener {
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
+import com.code.aon.common.ITransferObject;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ql.Criteria;
+import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ui.config.util.UserUtils;
+import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.warehouse.Warehouse;
+import com.code.aon.warehouse.WarehouseTransfer;
+import com.esferalia.aon.entity.IEntityAlias;
+
+public class WarehouseTransferSearchListener extends ControllerSearchListener {
 	
-    private Item item;
-	
-	public Item getItem() {
-		return item;
+    private Warehouse sourceWarehouse;
+    
+    private Warehouse targetWarehouse;
+    
+	public Warehouse getSourceWarehouse() {
+		return sourceWarehouse;
 	}
-	public void setItem(Item item) {
-		this.item = item;
+
+	public void setSourceWarehouse(Warehouse sourceWarehouse) {
+		this.sourceWarehouse = sourceWarehouse;
+	}
+
+	public Warehouse getTargetWarehouse() {
+		return targetWarehouse;
+	}
+
+	public void setTargetWarehouse(Warehouse targetWarehouse) {
+		this.targetWarehouse = targetWarehouse;
+	}
+
+	@Override
+	protected void init() throws ManagerBeanException {
+		setSourceWarehouse((Warehouse)BeanManager.getManagerBean(Warehouse.class).createNewTo());
+		setTargetWarehouse((Warehouse)BeanManager.getManagerBean(Warehouse.class).createNewTo());
 	}
 	
 	@Override
-	protected void init() throws ManagerBeanException {
-		super.init();
-		setItem((Item)BeanManager.getManagerBean(Item.class).createNewTo());
+	protected void completeCriteria(Criteria criteria)
+			throws ManagerBeanException, ExpressionException {
+		if( getSourceWarehouse()!=null && getSourceWarehouse().getId()!=null ){
+			String alias = this.getFieldName(IEntityAlias.WAREHOUSE_TRANSFER_SOURCE_WAREHOUSE_ID);
+			criteria.addEqualExpression(alias, getSourceWarehouse().getId());
+		} else {
+			String alias = this.getFieldName(IEntityAlias.WAREHOUSE_TRANSFER_ID);
+			Collection<Integer> ids = getScopeWarehousesTransferList(IEntityAlias.WAREHOUSE_TRANSFER_SOURCE_WAREHOUSE_WORK_PLACE_SCOPE_ID);
+			ids.addAll(getNullWarehousesTransferList(IEntityAlias.WAREHOUSE_TRANSFER_SOURCE_WAREHOUSE));
+			if(ids.isEmpty()){
+				ids.add(-1);
+			}
+			criteria.addInExpression(alias, ids);
+		}
+		if( getTargetWarehouse()!=null && getTargetWarehouse().getId()!=null ){
+			String alias = this.getFieldName(IEntityAlias.WAREHOUSE_TRANSFER_TARGET_WAREHOUSE_ID);
+			criteria.addEqualExpression(alias, getTargetWarehouse().getId());
+		} else {
+			String alias = this.getFieldName(IEntityAlias.WAREHOUSE_TRANSFER_ID);
+			Collection<Integer> ids = getScopeWarehousesTransferList(IEntityAlias.WAREHOUSE_TRANSFER_TARGET_WAREHOUSE_WORK_PLACE_SCOPE_ID);
+			ids.addAll(getNullWarehousesTransferList(IEntityAlias.WAREHOUSE_TRANSFER_TARGET_WAREHOUSE));
+			if(ids.isEmpty()){
+				ids.add(-1);
+			}
+			criteria.addInExpression(alias, ids);
+		}
 	}
+	
+	private Collection<Integer> getNullWarehousesTransferList(String alias) throws ManagerBeanException {
+		IManagerBean bean = BeanManager.getManagerBean(WarehouseTransfer.class);
+		Criteria criteria = new Criteria();
+		criteria.addNullExpression(bean.getFieldName(alias));
+		List<Integer> ids = new LinkedList<Integer>();
+		for(ITransferObject to: bean.getList(criteria)){
+			WarehouseTransfer wt = (WarehouseTransfer) to;
+			ids.add(wt.getId());
+		}
+		return ids;
+	}
+	
+	private Collection<Integer> getScopeWarehousesTransferList(String alias) throws ManagerBeanException {
+		IManagerBean bean = BeanManager.getManagerBean(WarehouseTransfer.class);
+		Criteria criteria = new Criteria();
+		UserUtils.getInstance().addScopeFilterToCriteria(criteria, bean.getFieldName(alias));
+		List<Integer> ids = new LinkedList<Integer>();
+		for(ITransferObject to: bean.getList(criteria)){
+			WarehouseTransfer wt = (WarehouseTransfer) to;
+			ids.add(wt.getId());
+		}
+		return ids;
+	}
+	
 }
