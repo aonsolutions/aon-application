84985ac5258db295a4a3626ca4528ac0910d6c0e
Euke Castellano <ecastellano@esferalia.com>
2013-06-21 12:53:37 +0000

    Script para el lanzamiento de la libreria de validación de la AEAT.

--- aon.aeat.mipf/src/main/resources/tool/mipf13pdf.sh
+++ aon.aeat.mipf/src/main/resources/tool/mipf13pdf.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cd /usr/share/java/aon.mipf
-pwd
+Xvfb -ac :99 &
+PID=$!
 export DISPLAY=:99
 java -Xmx256m -classpath ./mipf13pdf.jar es.aeat.mipf.mi13.Mipfj $1 $2 $3 $4 $5 $6
+kill -9 $PID
 
 
 
