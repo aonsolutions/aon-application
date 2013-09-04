67dbc54a0e82ffc8711978c404d9e5cc745cc2b5
Raul Trepiana <rtrepiana@esferalia.com>
2013-09-04 10:39:06 +0000

    Imports from eclipse....

--- aon-web-aio/src/main/java/com/code/aon/aio/servlet/OpenIDAuthServlet.java
+++ aon-web-aio/src/main/java/com/code/aon/aio/servlet/OpenIDAuthServlet.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.openid4java.message.*;
 
-import HybridOauth.HybridOauthMessage;
 
 import com.code.aon.jaas.auth.session.AuthenticationLoginException;
 
