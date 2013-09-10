b3d5abf78c9e0da7db8f7614f646a4f5eb5510f7
Gorka Irazu <girazu@esferalia.com>
2013-09-10 14:25:09 +0000

    Series Heredable!

--- aon-config/src/main/java/com/code/aon/config/Series.java
+++ aon-config/src/main/java/com/code/aon/config/Series.java
@@ -3,10 +3,12 @@ package com.code.aon.config;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.code.aon.common.annotations.Heritable;
 import com.esferalia.aon.entity.master.SeriesDB;
 
 @Entity
 @Table(name="series")
+@Heritable
 public class Series extends SeriesDB implements IScopable{
 	
 	private static final long serialVersionUID = 1L;
