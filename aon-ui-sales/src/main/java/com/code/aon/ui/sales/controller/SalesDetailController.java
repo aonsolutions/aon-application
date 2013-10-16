8f7709ec0f124405ebc5df72399095f87e130fe1
Raul Trepiana <rtrepiana@esferalia.com>
2013-10-16 22:36:22 +0000

    Merged revisions 20379,20381 via svnmerge from http://dev.esferalia.net/repos/aon-application/trunk
    ........
      r20379 | atellitu | 2013-10-16 10:37:39 +0200 (mié, 16 oct 2013) | 1 line
      
      Se ha añadido un LoginServlet para permitir conectarse a la aplicación de forma externa
    ........
      r20381 | girazu | 2013-10-16 13:30:51 +0200 (mié, 16 oct 2013) | 1 line
      
      Corregido Error en Pedidos de Venta.
    ........

--- aon-ui-sales/src/main/java/com/code/aon/ui/sales/controller/SalesDetailController.java
+++ aon-ui-sales/src/main/java/com/code/aon/ui/sales/controller/SalesDetailController.java
@@ -167,7 +167,7 @@ public class SalesDetailController extends LinesController implements ISalesCons
 
 	public String getLineStatusInfo() throws ManagerBeanException {
 		StringBuffer info = new StringBuffer(64);
-		DecimalFormat formatter = new DecimalFormat(AonUtil.getMessage(ICommonMessages.BUNDLE_NAME, ICommonMessages.QUANTITY_PATTERN));
+		DecimalFormat formatter = new DecimalFormat(AonUtil.getMessage(ICommonMessages.QUANTITY_PATTERN));
 
 		SalesDetail salesDetail = (SalesDetail)this.getModel().getRowData();
 		IManagerBean deliveryDetailBean = BeanManager.getManagerBean(DeliveryDetail.class);
