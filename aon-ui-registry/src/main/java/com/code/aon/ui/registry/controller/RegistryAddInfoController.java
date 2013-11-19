e737d39745cecd4c0b643be5f0cca4c901e75055
Gorka Irazu <girazu@esferalia.com>
2013-11-19 10:08:00 +0000

    PMS. Parametros de Clientes. En la busqueda y en la lista de parametros se añade el filtro por Clientes activos

--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddInfoController.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddInfoController.java
@@ -3,27 +3,53 @@ package com.code.aon.ui.registry.controller;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.customer.Customer;
+import com.code.aon.customer.enumeration.CustomerStatus;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.Projection;
 import com.code.aon.ql.ProjectionList;
+import com.code.aon.ql.ast.Expression;
+import com.code.aon.ql.util.ExpressionUtilities;
 import com.code.aon.registry.RegistryAddInfo;
 import com.code.aon.ui.form.BasicController;
 import com.esferalia.aon.entity.IEntityAlias;
 
 public class RegistryAddInfoController extends BasicController {
 
+	private boolean customersOnly;
+
+	public boolean isCustomersOnly() {
+		return customersOnly;
+	}
+
+	public void setCustomerMode(ActionEvent event) {
+		customersOnly = true;
+	}
+
+	public Expression getCustomersOnlyExpression() throws ManagerBeanException {
+		IManagerBean customerBean = BeanManager.getManagerBean(Customer.class);			
+		Criteria subCriteria = new Criteria();
+		subCriteria.addEqualExpression(customerBean.getFieldName(IEntityAlias.CUSTOMER_STATUS), CustomerStatus.ACTIVE);
+		ProjectionList projectionList = new ProjectionList(Projection.property(customerBean.getFieldName(IEntityAlias.CUSTOMER_ID)));
+		return ExpressionUtilities.getSubQueryExpression(Customer.class, subCriteria, projectionList);
+	}
+
 	public List<SelectItem> getAddInfoAttributes() throws ManagerBeanException {
 		List<SelectItem> attributes = new LinkedList<SelectItem>();
 		IManagerBean rAddInfoBean = BeanManager.getManagerBean(RegistryAddInfo.class);
 		Criteria criteria = new Criteria();
+		if (isCustomersOnly()) {
+			criteria.addInExpression(getFieldName(IEntityAlias.REGISTRY_ADD_INFO_REGISTRY_ID), getCustomersOnlyExpression());			
+		}
 		criteria.addOrder(rAddInfoBean.getFieldName(IEntityAlias.REGISTRY_ADD_INFO_ATTRIBUTE));
 		Projection projection = Projection.group(rAddInfoBean.getFieldName(IEntityAlias.REGISTRY_ADD_INFO_ATTRIBUTE));
-		for (Object obj : rAddInfoBean.getList(new ProjectionList(projection), null)) {
+		for (Object obj : rAddInfoBean.getList(new ProjectionList(projection), criteria)) {
 			String attribute = (String)obj;
 			attributes.add(new SelectItem(attribute, attribute));
 		}
--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/RegistryAddInfoSearchListener.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/event/RegistryAddInfoSearchListener.java
@@ -6,6 +6,7 @@ import com.code.aon.customer.Customer;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.ui.registry.controller.RegistryAddInfoController;
 import com.esferalia.aon.entity.IEntityAlias;
 
 public class RegistryAddInfoSearchListener extends ControllerSearchListener {
@@ -27,9 +28,13 @@ public class RegistryAddInfoSearchListener extends ControllerSearchListener {
 
 	@Override
 	protected void completeCriteria(Criteria criteria) throws ManagerBeanException, ExpressionException {
-		super.completeCriteria(criteria);
 		if (getCustomer() != null && getCustomer().getId() != null) {
 			criteria.addEqualExpression(getFieldName(IEntityAlias.REGISTRY_ADD_INFO_REGISTRY_ID), getCustomer().getRegistry().getId());
+		} else {
+			RegistryAddInfoController controller = (RegistryAddInfoController)getController();
+			if (controller.isCustomersOnly()) {
+				criteria.addInExpression(getFieldName(IEntityAlias.REGISTRY_ADD_INFO_REGISTRY_ID), controller.getCustomersOnlyExpression());			
+			}
 		}
 	}
 
