3bc1e2a6521463c04db3040d344e8ffff4d8280d
Euke Castellano <ecastellano@esferalia.com>
2014-01-22 14:19:35 +0000

    Model 311.

---
+++ aon-ui-fiscal/src/main/java/com/code/aon/ui/fiscal/controller/mod311/Mod311ControllerListener.java
@@ -0,0 +1,7 @@
+package com.code.aon.ui.fiscal.controller.mod311;
+
+import com.code.aon.ui.fiscal.controller.model.FiscalModelControllerListener;
+
+public class Mod311ControllerListener extends FiscalModelControllerListener {
+
+}
