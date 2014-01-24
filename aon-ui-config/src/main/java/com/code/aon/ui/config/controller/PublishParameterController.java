0ae25f0af02a8c7be7a0d629532ede74892e8ddd
Aimar Tellitu <atellitu@esferalia.com>
2014-01-24 11:23:20 +0000

    Incomplete - # 3540: Mover Parámetros de Configuración (RSS) a Configuración GLobal http://trac.esferalia.net/trac/aon/ticket/3540

--- aon-ui-config/src/main/java/com/code/aon/ui/config/controller/PublishParameterController.java
+++ aon-ui-config/src/main/java/com/code/aon/ui/config/controller/PublishParameterController.java
@@ -10,18 +10,8 @@ public class PublishParameterController {
 
 	private PublishProperties to;
 	
-	private boolean showOnlyFTP;
-	
 	public void onInit( ActionEvent event ) {
 		this.to = getPublishProperties();
-	}
-	
-	public boolean isShowOnlyFTP() {
-		return showOnlyFTP;
-	}
-
-	public void setShowOnlyFTP(boolean showOnlyFTP) {
-		this.showOnlyFTP = showOnlyFTP;
 	}
 
 	public PublishProperties getTo() {
