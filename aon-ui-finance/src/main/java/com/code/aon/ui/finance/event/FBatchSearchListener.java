0b9b02a32ebbdf6bd5387bd116685e5bb17c34cd
Aimar Tellitu <atellitu@esferalia.com>
2014-02-05 15:54:20 +0000

    Se ha corregido las busquedas de Remesas de Pago para que no se mezclen las de tesorería y de Laboral

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/FBatchSearchListener.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/FBatchSearchListener.java
@@ -7,19 +7,42 @@ import com.code.aon.ql.ast.Expression;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ql.util.ExpressionUtilities;
 import com.code.aon.ui.finance.controller.FBatchController;
-import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.ui.form.event.ControllerSearchListenerEx;
 import com.esferalia.aon.entity.IEntityAlias;
 
-public class FBatchSearchListener extends ControllerSearchListener {
+public class FBatchSearchListener extends ControllerSearchListenerEx {
+
+	private FinanceBatchType type;
+	
+	public FinanceBatchType getType() {
+		return type;
+	}
+
+	public void setType(FinanceBatchType type) {
+		this.type = type;
+	}
 
 	@Override
-	protected void completeCriteria(Criteria criteria) throws ManagerBeanException, ExpressionException {
-		criteria.addEqualExpression(getFieldName(IEntityAlias.FINANCE_BATCH_PAYMENT), ((FBatchController)getController()).isPayment());
-		if (((FBatchController)getController()).isPayroll()) {
-			String typeAlias = getFieldName(IEntityAlias.FINANCE_BATCH_FINANCE_BATCH_TYPE);
-			Expression expr1 = ExpressionUtilities.getEqualExpression(typeAlias, FinanceBatchType.AEB_34_N);
-			Expression expr2 = ExpressionUtilities.getEqualExpression(typeAlias, FinanceBatchType.SEPA_34_14_XML);
-			criteria.addExpression(ExpressionUtilities.getOrExpression(expr1, expr2));
+	protected void init() throws ManagerBeanException {
+		setType(null);
+	}	
+	
+	@Override
+	protected void completeCriteria( Criteria criteria ) throws ManagerBeanException, ExpressionException {
+		FBatchController controller = (FBatchController) getController();
+		criteria.addEqualExpression(getFieldName(IEntityAlias.FINANCE_BATCH_PAYMENT), controller.isPayment());
+		String typeAlias = getFieldName(IEntityAlias.FINANCE_BATCH_FINANCE_BATCH_TYPE);		
+		if ( type != null ) {
+			criteria.addEqualExpression(typeAlias, type);
+		} else if ( controller.isPayment() ) {
+			if (controller.isPayroll()) {
+				Expression expr1 = ExpressionUtilities.getEqualExpression(typeAlias, FinanceBatchType.AEB_34_N);
+				Expression expr2 = ExpressionUtilities.getEqualExpression(typeAlias, FinanceBatchType.SEPA_34_14_N_XML);
+				criteria.addExpression(ExpressionUtilities.getOrExpression(expr1, expr2));
+			} else {
+				criteria.addNotEqualExpression(typeAlias, FinanceBatchType.AEB_34_N);
+				criteria.addNotEqualExpression(typeAlias, FinanceBatchType.SEPA_34_14_N_XML);
+			}
 		}
 	}
 
