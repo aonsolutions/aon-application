b8e4ebcdf8630ef7c8a83ad82be06c3424f1ea6c
Raul Trepiana <rtrepiana@esferalia.com>
2014-02-10 17:59:17 +0000

    Fix BUG discovering undefined variables.

--- aon-salary/src/main/java/com/esferalia/aon/salary/expression/ExpressionContext.java
+++ aon-salary/src/main/java/com/esferalia/aon/salary/expression/ExpressionContext.java
@@ -270,6 +270,8 @@ public class ExpressionContext {
 			int len = end - offset + 1;
 			property = new String(expr, offset, len);
 			end = start ;
+			if ( end < 0  )
+				break;
 		} while (!isJavaIdentifier(property) || bindings.containsKey(property));
 
 		return property;
