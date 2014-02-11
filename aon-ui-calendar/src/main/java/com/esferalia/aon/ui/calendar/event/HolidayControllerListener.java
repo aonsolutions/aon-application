9b9b139c97a33d7be3a86b05ac1a5fd1adf86ae6
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2014-02-11 21:19:16 +0000

    incomplete commit

--- aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/event/HolidayControllerListener.java
+++ aon-ui-calendar/src/main/java/com/esferalia/aon/ui/calendar/event/HolidayControllerListener.java
@@ -1,11 +1,22 @@
 package com.esferalia.aon.ui.calendar.event;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import javax.faces.event.AbortProcessingException;
+
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.domain.DomainManager;
+import com.code.aon.dbutils.DatabaseUtil;
+import com.code.aon.pool.AonConnectionException;
 import com.code.aon.ui.form.event.ControllerAdapter;
 import com.code.aon.ui.form.event.ControllerEvent;
 import com.code.aon.ui.form.event.ControllerListenerException;
+import com.code.aon.ui.util.AonUtil;
 import com.esferalia.aon.ui.calendar.controller.HolidayController;
 
 public class HolidayControllerListener extends ControllerAdapter {
@@ -16,6 +27,48 @@ public class HolidayControllerListener extends ControllerAdapter {
 		HolidayController c = (HolidayController) getController();
 		GregorianCalendar cal= new GregorianCalendar();
 		c.setYear(cal.get(Calendar.YEAR));	
+	}
+	
+	@Override
+	public void beforeModelSearched(ControllerEvent event)
+			throws ControllerListenerException {
+		Integer[] ids = {0, DomainManager.getCurrentDomain(), getParentDomainId()};
+		try {
+			event.getController().getCriteria().setSkipDomainFilter( true );
+			event.getController().getCriteria().addInExpression("Holiday.domain", ids);
+		} catch (ManagerBeanException e) {
+			try {
+				event.getController().getCriteria().setSkipDomainFilter( false );
+			} catch (ManagerBeanException e2) {
+				// nada
+			}
+		}
+	}
+	
+	private Integer getParentDomainId() {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			conn = DatabaseUtil.getConnection(AonUtil.getDomainName());
+			String select = "SELECT parent FROM domain WHERE id = " + DomainManager.getCurrentDomain();
+			ps = conn.prepareStatement(select);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				return rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			String msg = "Se ha producido un error al obtener los convenios. ("+ e.getMessage()+")";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} catch (AonConnectionException e) {
+			String msg = "Se ha producido un error al obtener los convenios. ("+ e.getMessage()+")";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} finally {
+			DatabaseUtil.closeQuietly(ps);
+			DatabaseUtil.closeQuietly(conn);
+		}
+		return null;
 	}
 
 }
