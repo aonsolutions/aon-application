b1f0c9a6cbbf06524cfde55955319c3dd79b8c49
Aimar Tellitu <atellitu@esferalia.com>
2014-03-13 16:28:02 +0000

    Exportación apuntes A3. En vez del id de la factura ahora se pone el numero de factura truncado a un maximo de 10 carecteres (maximo admitido por A3). En las lineas falta añadir la descripción del concepto

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/A3Writer.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/A3Writer.java
@@ -160,7 +160,7 @@ public class A3Writer extends BasicExporter {
 			}
 		}
 		// Numero de Factura o Documento
-		setStringRightPad( getMainId().toString(), 58, 10);
+		setStringRightPad( getReferenceCode(), 58, 10);
 		// Linea de apunte (I,M,U)
 		if ( first ) {
 			setString( "I", 68, 1);	
@@ -168,7 +168,7 @@ public class A3Writer extends BasicExporter {
 			setString( getDetails().isEmpty() ? "U" : "M", 68, 1);					
 		}
 		// Descripcion del apunte
-		setStringRightPad( getReferenceCode(), 69, 30);
+		setStringRightPad( aed.getConcept(), 69, 30);
 		// Importe
 		double amount = (aed.getCredit() != 0) ? aed.getCredit() : aed.getDebit();
 		setNumber( amount, 99, 14);
@@ -337,7 +337,9 @@ public class A3Writer extends BasicExporter {
 		// Numero de Factura o Documento
 		setStringLeftPad( getMainId().toString(), 58, 10);
 		// Linea de apunte (M o U)
-		setString( last ? "U" : "M", 68, 1);		
+		setString( last ? "U" : "M", 68, 1);
+		// Descripcion del apunte
+		setStringRightPad( aed.getConcept(), 69, 30);		
 		// Subtipo de factura (01 a 07)
 		setString( getSubtipoDeFactura(VatDeductionType.WITH_RIGHT), 99, 2);	
 		// Base imponible
