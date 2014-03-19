ec16ad1a347dfd12fd5f53bacd0607b03e6277a9
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2014-03-19 12:53:50 +0000

    PAYROLL. Datos de fin de contrato borrados de la pantalla de contrato.

--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/utils/ContractUtils.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/utils/ContractUtils.java
@@ -4,14 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.faces.event.AbortProcessingException;
 
-import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -621,10 +619,6 @@ public class ContractUtils {
 		}
 		try {
 			ContractData contractEndCode = obtainContractData(contract, ContextVariable.CONTRACT_END_CODE.getName());
-//			ContractData advanceNoticeDays = obtainContractData(contract, ContextVariable.ADVANCE_NOTICE_DAYS.getName());
-//			ContractData noHolidays = obtainContractData(contract, ContextVariable.NO_HOLIDAYS.getName());
-			ContractData compensationDays = obtainContractData(contract, ContextVariable.COMPENSATION_DAYS.getName());
-			ContractData workedYears = obtainContractData(contract, ContextVariable.WORKED_YEARS.getName());
 			if(params.getSuspensionCause()!=null){
 				data = contractEndCode!=null?contractEndCode:new ContractData();
 				data.setContract(contract);
@@ -633,55 +627,10 @@ public class ContractUtils {
 				data.setName( ContextVariable.CONTRACT_END_CODE.getName() );
 				data.setExpression("\"" + params.getSuspensionCause().getCode() + "\"");
 				bean.insertOrUpdate(data);
-//				if(params.getSettleAdvanceNoticeDays() != null){
-//					data = advanceNoticeDays!=null?advanceNoticeDays:new ContractData();
-//					data.setContract(contract);
-//					data.setStartDate(contract.getStartDate());
-//					data.setEndDate(contract.getEndDate());
-//					data.setName( ContextVariable.ADVANCE_NOTICE_DAYS.getName() );
-//					data.setExpression(params.getSettleAdvanceNoticeDays().toString());
-//					bean.insertOrUpdate(data);
-//				}
-//				if(params.getSettleNonEnjoyedVacations() != null){
-//					data = noHolidays!=null?noHolidays:new ContractData();
-//					data.setContract(contract);
-//					data.setStartDate(contract.getStartDate());
-//					data.setEndDate(contract.getEndDate());
-//					data.setName( ContextVariable.NO_HOLIDAYS.getName() );
-//					data.setExpression(params.getSettleNonEnjoyedVacations().toString());
-//					bean.insertOrUpdate(data);
-//				}
-				if(params.getSettleCompensationDays() != null){
-					data = compensationDays!=null?compensationDays:new ContractData();
-					data.setContract(contract);
-					data.setStartDate(contract.getStartDate());
-					data.setEndDate(contract.getEndDate());
-					data.setName( ContextVariable.COMPENSATION_DAYS.getName() );
-					data.setExpression(params.getSettleCompensationDays().toString());
-					bean.insertOrUpdate(data);
-				}
-				if(params.getSettleTotalWorkedYears() != null){
-					data = workedYears!=null?workedYears:new ContractData();
-					data.setContract(contract);
-					data.setStartDate(contract.getStartDate());
-					data.setEndDate(contract.getEndDate());
-					data.setName( ContextVariable.WORKED_YEARS.getName() );
-					data.setExpression(params.getSettleTotalWorkedYears().toString());
-					bean.insertOrUpdate(data);
-				}
 			} else {
 				if(contractEndCode != null){
 					bean.remove(contractEndCode);
 				}
-//				if(advanceNoticeDays != null){
-//					bean.remove(advanceNoticeDays);
-//				}
-//				if(noHolidays != null){
-//					bean.remove(noHolidays);
-//				}
-//				if(compensationDays != null){
-//					bean.remove(compensationDays);
-//				}
 			}
 		} catch (ManagerBeanException e) {
 			String msg = "Error al grabar la causa de suspension. (" +e.getMessage() + ")";
@@ -792,25 +741,6 @@ public class ContractUtils {
 		if(map.get(ContextVariable.CONTRACT_END_CODE.getName())!=null){
 			params.setSuspensionCause(TLDCAUSS.getEnumByValue(map.get(ContextVariable.CONTRACT_END_CODE.getName())));
 		}
-		
-//		if(map.get(ContextVariable.ADVANCE_NOTICE_DAYS.getName())!=null && NumberUtils.isDigits(map.get(ContextVariable.ADVANCE_NOTICE_DAYS.getName()))){
-//			Integer days = Integer.parseInt(map.get(ContextVariable.ADVANCE_NOTICE_DAYS.getName()));
-//			Calendar cal = Calendar.getInstance();
-//			cal.setTime(contract.getEndDate());
-//			cal.add(Calendar.DAY_OF_MONTH, -days);
-//			params.setSettleAdvanceNoticeDate(cal.getTime());	
-//		}
-//		if(map.get(ContextVariable.NO_HOLIDAYS.getName())!=null && NumberUtils.isDigits(map.get(ContextVariable.NO_HOLIDAYS.getName()))){
-//			params.setSettleNonEnjoyedVacations(Integer.parseInt(map.get(ContextVariable.NO_HOLIDAYS.getName())));
-//		}
-//		if(map.get(ContextVariable.COMPENSATION_DAYS.getName())!=null && NumberUtils.isDigits(map.get(ContextVariable.COMPENSATION_DAYS.getName()))){
-//			params.setSettleCompensationDays(Integer.parseInt(map.get(ContextVariable.COMPENSATION_DAYS.getName())));
-//		}
-//		if(map.get(ContextVariable.WORKED_YEARS.getName())!=null && NumberUtils.isNumber(map.get(ContextVariable.WORKED_YEARS.getName()))){
-//			Double total = Double.parseDouble(map.get(ContextVariable.WORKED_YEARS.getName()));
-//			params.setSettleWorkedYears(total.intValue());
-//			params.setSettleWorkedMonths((int)((total-params.getSettleWorkedYears())*12));
-//		}
 		
 	}
 	
