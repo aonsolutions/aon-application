c8cba21bd6bbf28991b68367e3878d48d19e9579
Euke Castellano <ecastellano@esferalia.com>
2014-03-31 14:36:35 +0000

    Modelo 303. Régimen Simplificado.

--- aon-ui-fiscal/src/main/java/com/code/aon/ui/fiscal/file/MOD303Writer.java
+++ aon-ui-fiscal/src/main/java/com/code/aon/ui/fiscal/file/MOD303Writer.java
@@ -419,7 +419,7 @@ public class MOD303Writer {
 		
 		double epi = mod303.getEnsuredAmount(Mod303Key.CAC1);
 		epi = ensureEpigraph(epi);
-		sr.setAct1epi( Integer.toString((int) epi));
+		sr.setAct1epi( epi == 0?"":Integer.toString((int) epi));
 		sr.setAct1Uni1(mod303.getEnsuredAmount(Mod303Key.CAC1_M1U));
 		sr.setAct1Imp1(mod303.getEnsuredAmount(Mod303Key.CAC1_M1I));
 		sr.setAct1Uni2(mod303.getEnsuredAmount(Mod303Key.CAC1_M2U));
@@ -460,7 +460,7 @@ public class MOD303Writer {
 
 		epi = mod303.getEnsuredAmount(Mod303Key.CAC2);
 		epi = ensureEpigraph(epi);
-		sr.setAct2epi( Integer.toString((int) epi));
+		sr.setAct2epi( epi == 0?"":Integer.toString((int) epi));
 		sr.setAct2Uni1(mod303.getEnsuredAmount(Mod303Key.CAC2_M1U));
 		sr.setAct2Imp1(mod303.getEnsuredAmount(Mod303Key.CAC2_M1I));
 		sr.setAct2Uni2(mod303.getEnsuredAmount(Mod303Key.CAC2_M2U));
