8c7315c314fa0b5812f564088b6d75e191c25cce
Aimar Tellitu <atellitu@esferalia.com>
2014-05-28 09:20:27 +0000

    InfoWeb. Estaba fallando el Generador Web. Se ha aprovechado para ajustar el ancho de ventanas modales que aparecian cortadas, sobretodo en clientes

--- aon-ui-infoweb/src/main/java/com/code/aon/ui/infoweb/controller/GeneratorController.java
+++ aon-ui-infoweb/src/main/java/com/code/aon/ui/infoweb/controller/GeneratorController.java
@@ -69,7 +69,7 @@ public class GeneratorController implements VelocityConstants  {
 
 	private static final String WEB_INFO_PAGE_RESOURCE_RATTACH_DESCRIPTION = "WebInfoPageResource.rattach.description";
 
-	private static final String WEB_INFO_PAGE_RESOURCE_RATTACH_DATA = "WebInfoPageResource.rattach.data";
+	private static final String WEB_INFO_PAGE_RESOURCE_RATTACH_SIZE = "WebInfoPageResource.rattach.size";
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GeneratorController.class.getName());
 	
@@ -330,7 +330,7 @@ public class GeneratorController implements VelocityConstants  {
 		IManagerBean wiprBean = BeanManager.getManagerBean(WebInfoPageResource.class);
 		Criteria wiprCriteria = new Criteria();
 		wiprCriteria.addEqualExpression(wiprBean.getFieldName(IEntityAlias.WEB_INFO_PAGE_RESOURCE_WEB_INFO_PAGE_ID), wip.getId());
-		wiprCriteria.addNotNullExpression(WEB_INFO_PAGE_RESOURCE_RATTACH_DATA);
+		wiprCriteria.addGreaterThanExpression(WEB_INFO_PAGE_RESOURCE_RATTACH_SIZE,0);
 		List<ITransferObject> wiprList = wiprBean.getList(wiprCriteria);
 
 		for (int i = 0; i < wiprList.size(); i++) {
@@ -353,7 +353,7 @@ public class GeneratorController implements VelocityConstants  {
 		Criteria wiprCriteria = new Criteria();
 		wiprCriteria.addEqualExpression(wiprBean.getFieldName(IEntityAlias.WEB_INFO_PAGE_RESOURCE_WEB_INFO_PAGE_ID), wip.getId());
 		wiprCriteria.addNotNullExpression(WEB_INFO_PAGE_RESOURCE_RATTACH_DESCRIPTION);
-		wiprCriteria.addNotNullExpression(WEB_INFO_PAGE_RESOURCE_RATTACH_DATA);
+		wiprCriteria.addGreaterThanExpression(WEB_INFO_PAGE_RESOURCE_RATTACH_SIZE,0);
 		List<ITransferObject> wiprList = wiprBean.getList(wiprCriteria);
 		String previous_link = "";
 		String next_link = "";
