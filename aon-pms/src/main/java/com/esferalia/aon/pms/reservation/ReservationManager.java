fd8323a0ba40594ef82c01ec225240e644ffc385
Gorka Irazu <girazu@esferalia.com>
2014-06-17 09:07:56 +0000

    PMS. Carga de Reservas. Daba error al intentar grabar un Company en la Reserva

--- aon-pms/src/main/java/com/esferalia/aon/pms/reservation/ReservationManager.java
+++ aon-pms/src/main/java/com/esferalia/aon/pms/reservation/ReservationManager.java
@@ -209,7 +209,7 @@ public class ReservationManager implements IReservationConstants {
 			double agencyCommissionAmount = getReservationUtils().obtainAgencyCommissionAmount(agencyInfo);
 			calculateCommission = agencyCommissionAmount != 0 && getReservationUtils().isAgencyCommission(agency);
 			String agencyRebate = findTpaExtensionsAttribute(reservationType.getTPAExtensions(), DISCOUNT_MODE, null);
-			ProfileInfo companyInfo = findProfileInfo(reservationType.getResGuests().getResGuestArray(), COMPANY_TYPE, null);
+			ProfileInfo companyInfo = findProfileInfo(reservationType.getResGuests().getResGuestArray(), COMPANY_TYPE, SOLRES);
 			String discountPercent = findTpaExtensionsAttribute(reservationType.getTPAExtensions(), DISCOUNT, PERCENT);
 			String discountAmount = findTpaExtensionsAttribute(reservationType.getTPAExtensions(), DISCOUNT, AMOUNT);
 			String bookingHolder = findTpaExtensionsAttribute(reservationType.getTPAExtensions(), BOOKING_HOLDER, null);
