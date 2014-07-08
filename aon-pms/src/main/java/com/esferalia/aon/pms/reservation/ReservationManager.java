2818af5b717598b2a617b0bef5e621e74d34077d
Gorka Irazu <girazu@esferalia.com>
2014-07-08 08:25:00 +0000

    PMS. Carga de Reservas. En las Reservas con descuento, admitir un centimo de diferencia en los descuentos por errores de redondeo en los calculos de Idiso (CRS).

--- aon-pms/src/main/java/com/esferalia/aon/pms/reservation/ReservationManager.java
+++ aon-pms/src/main/java/com/esferalia/aon/pms/reservation/ReservationManager.java
@@ -484,7 +484,8 @@ public class ReservationManager implements IReservationConstants {
 				}
 			}
 
-			if (totalServices == CommonUtil.round(reservation.getTotal() + totalDiscount)) {
+			//Tendria que ser cero, pero se admite un error de +- 1 centimo por error de redondeo en los calculos de Idiso al enviar la Reserva.
+			if (Math.abs(CommonUtil.round(totalServices - reservation.getTotal() - totalDiscount)) <= 0.1) {
 				discountPercent = (1 - reservation.getTotal() / totalServices) * 100;
 			} else {
 				if (calculateCommission) {
