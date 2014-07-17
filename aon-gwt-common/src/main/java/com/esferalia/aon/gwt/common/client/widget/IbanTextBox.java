a2e9cc0b06554bea562a4d3702d965aefcd3c7fc
Euke Castellano <ecastellano@esferalia.com>
2014-07-17 09:32:33 +0000

    Impuesto sobre sociedades. Introduccion IBAN.

--- aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/widget/IbanTextBox.java
+++ aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/widget/IbanTextBox.java
@@ -3,17 +3,25 @@ package com.esferalia.aon.gwt.common.client.widget;
 
 import com.esferalia.aon.gwt.common.client.css.AonResources;
 import com.esferalia.aon.gwt.common.shared.AonUtil;
+import com.esferalia.aon.gwt.common.shared.IIbanContainer;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.SuggestBox;
+import com.google.gwt.user.client.ui.SuggestOracle;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 import com.google.gwt.user.client.ui.TextBox;
 
-public class IbanTextBox extends SimplePanel {
+public class IbanTextBox extends SimplePanel implements HasSelectionHandlers<Suggestion> {
 	
 	private static final AonResources AON_RESOURCES = GWT.create(AonResources.class);
 	
-	TextBox iban1 = new TextBox();
+	SuggestBox iban1;
 	TextBox iban2 = new TextBox();
 	TextBox iban3 = new TextBox();
 	TextBox iban4 = new TextBox();
@@ -21,20 +29,29 @@ public class IbanTextBox extends SimplePanel {
 	TextBox iban6 = new TextBox();
 	
 	private TextBox[] textBoxes = new TextBox[]{
-			iban1,iban2,iban3,iban4,iban5,iban6};
+			iban2,iban3,iban4,iban5,iban6};
 	
-	public IbanTextBox() {
+	public IbanTextBox(SuggestOracle suggestOracle) {
 		super( DOM.createSpan());
 		FlowPanel panel = new FlowPanel();
 		panel.addStyleName(AON_RESOURCES.css().aonNowrap());
 		panel.addStyleName(AON_RESOURCES.css().aonInline());
 		this.add(panel);
+		iban1 = new SuggestBox(suggestOracle);
+		iban1.setWidth("100px");
+		panel.add(iban1);
 		for (TextBox textBox : textBoxes ) {
 			textBox.addStyleName(AON_RESOURCES.css().aonMarginLeft());
 			textBox.setMaxLength(4);
 			textBox.setVisibleLength(5);
 			panel.add(textBox);
 		}
+		FlowPanel panel2 = new FlowPanel();
+		InlineLabel label = new InlineLabel("Comience a escribir para recuperar algun banco de la empresa");
+		label.addStyleName(AON_RESOURCES.css().aonFontSmall());
+		label.addStyleName(AON_RESOURCES.css().aonItalic());
+		panel2.add(label);
+		panel.add(panel2);
 	}
 	
 	public String getValue() {
@@ -51,5 +68,26 @@ public class IbanTextBox extends SimplePanel {
 		iban6.setValue( AonUtil.substring(value, 20,24));
 	}
 	
-	
+	@Override
+	public HandlerRegistration addSelectionHandler(SelectionHandler<Suggestion> handler) {
+		return iban1.addSelectionHandler(handler);
+	}
+
+	public static class IbanSuggestion implements Suggestion {
+		private IIbanContainer ibanContainer;
+
+		public IbanSuggestion(IIbanContainer ibanContainer) {
+			this.ibanContainer = ibanContainer;
+		}
+
+		@Override
+		public String getDisplayString() {
+			return ibanContainer.getDisplay();
+		}
+
+		@Override
+		public String getReplacementString() {
+			return ibanContainer.getIBan();
+		}
+	}
 }
--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/Mod200ServiceAsync.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/Mod200ServiceAsync.java
@@ -1,5 +1,8 @@
 package com.esferalia.aon.gwt.fiscal.client;
 
+import java.util.ArrayList;
+
+import com.esferalia.aon.gwt.common.shared.CompanyBank;
 import com.esferalia.aon.gwt.fiscal.shared.mod200.Mod200;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
@@ -21,6 +24,8 @@ public interface Mod200ServiceAsync {
 	void delete(Mod200 mod200, AsyncCallback<Mod200> callback);
 
 	void dumpAEAT(Mod200 mod200, AsyncCallback<String> callback);
+
+	void getCompanyBanks(int enterprise,AsyncCallback<ArrayList<CompanyBank>> callback);
 
 
 }
