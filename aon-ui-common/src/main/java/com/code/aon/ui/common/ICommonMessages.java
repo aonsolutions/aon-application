23caccf092cdc3a019f3a630ed35f449ac2374ef
Ekain Agirrezabal <eagirrezabal@esferalia.com>
2014-07-30 13:28:09 +0000

    #3728  - Remesar Cobros de Caja. Nueva Remesa. Formato Nombre

--- aon-ui-common/src/main/java/com/code/aon/ui/common/ICommonMessages.java
+++ aon-ui-common/src/main/java/com/code/aon/ui/common/ICommonMessages.java
@@ -35,6 +35,7 @@ public interface ICommonMessages {
 	String DATE_TO = "aon_date_to";
 	String DATE_PATTERN = "aon_date_pattern";
 	String DATE2_PATTERN = "aon_date2_pattern";
+	String DATE3_PATTERN = "aon_date3_pattern";
 	String DAY = "aon_day";
 	String DECIMAL_2_PATTERN = "aon_decimal2_pattern";
 	String DOCUMENT = "aon_document";
--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/PosFinanceController.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/PosFinanceController.java
@@ -1,6 +1,6 @@
 package com.code.aon.ui.finance.controller;
 
-import static com.code.aon.ui.common.ICommonMessages.DATE2_PATTERN;
+import static com.code.aon.ui.common.ICommonMessages.DATE3_PATTERN;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -73,7 +73,7 @@ public class PosFinanceController extends FinanceListController implements IFina
 
 	private FinanceBatch createFinanceBatch() {
 		PosFinanceSearchListener searchListener = (PosFinanceSearchListener)AonUtil.getRegisteredBean(POS_FINANCE_SEARCH_LISTENER_NAME);
-		String date = new SimpleDateFormat(AonUtil.getMessage(DATE2_PATTERN)).format(new Date());
+		String date = new SimpleDateFormat(AonUtil.getMessage(DATE3_PATTERN)).format(new Date());
 		String payMethod = searchListener.getPayMethod().getName();
 		String hotel = searchListener.getWorkPlace().getDescription();
 		if ((payMethod + hotel).length() > 22) {
@@ -84,7 +84,7 @@ public class PosFinanceController extends FinanceListController implements IFina
 		}
 
 		FinanceBatch fBatch = new FinanceBatch();
-		fBatch.setDescription(date + " " + payMethod + " " + hotel);
+		fBatch.setDescription(date + "_" + payMethod + "_" + hotel);
 		fBatch.setIssueDate(new Date());
 		return fBatch;
 	}
