662ff18898c4d0beb90056e60cbc73f2c5f53eaf
Ander Ibanez <aibanez@esferalia.com>
2014-08-13 07:31:15 +0000

    Adición columna size a la tabla domain_gserviceaccount

---
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.0.0.sql
@@ -0,0 +1,9 @@
+# Version: Upgrade from version 7.37.7 to 8.0.0
+# Created by: aibanez
+# Creation Date: 13/08/2014 
+
+
+ALTER TABLE `domain_gserviceaccount` ADD COLUMN `size` FLOAT NULL DEFAULT 0  AFTER `domain` ;
+
+UPDATE `db_version` SET `version_number` = '8.0.1';
+
