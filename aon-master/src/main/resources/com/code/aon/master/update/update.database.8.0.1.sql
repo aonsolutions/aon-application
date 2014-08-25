94b2a14a6aecd71076419f4b9f57bfdf0c8ba744
Ander Ibanez <aibanez@esferalia.com>
2014-08-25 09:34:58 +0000

    Adición columna limit ( limite de tamaño para un dominio) a la tabla domain_gserviceaccount

---
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.0.1.sql
@@ -0,0 +1,9 @@
+# Version: Upgrade from version 8.0.0 to 8.0.1
+# Created by: aibanez
+# Creation Date: 25/08/2014 
+
+
+ALTER TABLE `domain_gserviceaccount` ADD COLUMN `limit` DOUBLE NULL DEFAULT 102400  AFTER `size` ;
+
+UPDATE `db_version` SET `version_number` = '8.0.2';
+
