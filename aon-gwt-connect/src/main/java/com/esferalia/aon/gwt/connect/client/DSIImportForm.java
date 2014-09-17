1928049e1ec26a252f1108b7967770e7abfb9c4d
Alvaro Mtz de Lagos <amtzdelagos@esferalia.com>
2014-09-17 06:03:58 +0000

    Pantalla carga de base de datos.

--- aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/client/DSIImportForm.java
+++ aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/client/DSIImportForm.java
@@ -11,6 +11,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
@@ -23,41 +24,71 @@ public class DSIImportForm implements EntryPoint {
 
 	private static final Binder binder = GWT.create(Binder.class);
 	
-	@UiField 
-	Button importButton;
-
-	@UiField
-	FormPanel importPanel; 
+	@UiField FormPanel uploadFormPanel;
+	@UiField FileUpload fileUpload;
+	@UiField Button sendButton;
 	
+	public DSIImportForm() {
+
+	}
+
 	@Override
 	public void onModuleLoad() {
+		
 		// Inject rich styles.
 		GWT.<GWTResources> create(GWTResources.class).css().ensureInjected();
-		GWT.<AonResources> create(
-				AonResources.class).css().ensureInjected();
-		
+		GWT.<AonResources> create(AonResources.class).css().ensureInjected();
+
 		// Create the UI defined in DSIImportForm.ui.xml.
 		Widget ui = binder.createAndBindUi(this);
+
 		// Add the outer panel to the RootLayoutPanel, so that it will be
 		// displayed.
 		RootLayoutPanel root = RootLayoutPanel.get("rootPanel");
 		root.add(ui);
 		
+		init();
+	}	
+	
+	protected void init () {
+		uploadFormPanel.setAction("/aon_gwt_connect/dsiimport");
+		uploadFormPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
+		uploadFormPanel.setMethod(FormPanel.METHOD_POST);
+
+	}
+	// ------------------------------------------------------------- UiHandlers
+	
+	@UiHandler ("uploadFormPanel")
+	void onSubmit(SubmitEvent event) {
+	
+		if("".equalsIgnoreCase(fileUpload.getFilename()) == false) {
+			Window.alert("Subiendo archivo");
+			//NOW WHAT¿?
+		}
+		
+		else {
+			Window.alert("Subida cancelada");
+			event.cancel();
+		}
+ 	}
+	
+	@UiHandler ("uploadFormPanel")
+	void onSubmitComplete(SubmitCompleteEvent event) {
+		
+		//refresh page??
+		
+		Window.alert("Todo ok");
 	}
 	
-	@UiHandler("importPanel")
-	void onImportSubmit(SubmitEvent event) {
-		Window.alert("onImportSubmit");
-	}
-
-	@UiHandler("importPanel")
-	void onImportSubmitComplete(SubmitCompleteEvent event) {
-		Window.alert("onImportSubmitComplete");
-	}
-
-	@UiHandler("importButton")
-	void onImportClicked(ClickEvent event) {
-		importPanel.submit();
+	@UiHandler ("sendButton")
+	void onClick (ClickEvent event) {
+		
+		 try {
+			 uploadFormPanel.submit();
+		 }catch(Exception ex) {
+			 Window.alert("Error en submit");
+		 }
+		 
 	}
 
 }
