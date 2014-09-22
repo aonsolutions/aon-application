c26f6f4de82040ad7c9ba47bc1e2cf66069424ae
Euke Castellano <ecastellano@esferalia.com>
2014-09-22 07:48:27 +0000

    Listado excel de partes de trabajo.

--- aon-ui-groupware/src/main/java/com/code/aon/ui/groupware/report/DailyTrackingReportController.java
+++ aon-ui-groupware/src/main/java/com/code/aon/ui/groupware/report/DailyTrackingReportController.java
@@ -1,14 +1,21 @@
 package com.code.aon.ui.groupware.report;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.ss.usermodel.Font;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,12 +23,16 @@ import com.code.aon.AonVersion;
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.ICollectionProvider;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.enumeration.MimeType;
 import com.code.aon.groupware.TaskHolder;
 import com.code.aon.groupware.enumeration.DailyTrackingReportType;
+import com.code.aon.groupware.report.dailyTracking.DailyTrackingReport;
 import com.code.aon.groupware.report.dailyTracking.DailyTrackingReportEngine;
 import com.code.aon.groupware.report.dailyTracking.DailyTrackingReportParams;
 import com.code.aon.registry.Registry;
 import com.code.aon.report.OutputFormat;
+import com.code.aon.report.ReportException;
+import com.code.aon.report.poi.ExcelReportExporter;
 import com.code.aon.ui.common.components.LookupChangeEvent;
 import com.code.aon.ui.groupware.GroupwareUtils;
 import com.code.aon.ui.groupware.controller.GroupWareCollectionsController;
@@ -30,6 +41,7 @@ import com.code.aon.ui.project.controller.IProjectConstants;
 import com.code.aon.ui.project.controller.ProjectCollectionsController;
 import com.code.aon.ui.report.controller.ReportManager;
 import com.code.aon.ui.util.AonUtil;
+import com.code.aon.ui.util.DownloadUtil;
 
 public class DailyTrackingReportController implements ICollectionProvider, Serializable {
 	
@@ -174,28 +186,111 @@ public class DailyTrackingReportController implements ICollectionProvider, Seria
 	}
 
 
-	private OutputFormat getOutputFormat() {
-		if (DailyTrackingReportType.REPORT == getParams().getReportType()) {
-			return OutputFormat.XLS;
-		}
-		return OutputFormat.PDF;
+	private boolean isExcelReport() { 
+		return (DailyTrackingReportType.REPORT == getParams().getReportType());
 	}
 
 	public String onReport() {
 		try {
+			String outcome = null;
 			if (!isMonitor()) {
 					getParams().setTaskHolder( getGroupwareUtils().getCurrentTaskHolder() );
 			}
-			ReportManager manager = (ReportManager) AonUtil.getRegisteredBean("report");
-			manager.setReportKey(getParams().getReportType().getReportKey());
-			manager.setOutputFormat(getOutputFormat() == null ? OutputFormat.PDF : getOutputFormat());
-			String outcome = manager.onExecute();
+			if (isExcelReport()) {
+				printExcelReport();
+			} else {
+				ReportManager manager = (ReportManager) AonUtil.getRegisteredBean("report");
+				manager.setReportKey(getParams().getReportType().getReportKey());
+				manager.setOutputFormat(OutputFormat.PDF);
+				outcome = manager.onExecute();
+			}
 			return outcome;
 		} catch (Throwable e) {
 			String msg = "Error al ejecutar el listado. " + e.getMessage();
 			LOGGER.error(msg, e);
 			throw new AbortProcessingException(msg,e);
 		}			
+	}
+
+	private void printExcelReport() {
+		HttpServletResponse response = null;
+		OutputStream out = null;
+		try {
+			String filename = "PartesDeTrabajo";
+			response = DownloadUtil.getResponse();
+			out = DownloadUtil.initDownload(response, filename, MimeType.MIME_MS_EXCEL);
+			ExcelReportExporter exporter = new ExcelReportExporter();
+			exporter.startExport(filename);
+			
+			HSSFFont font = exporter.createFont();
+			font.setFontHeightInPoints((short) 8);
+
+			HSSFFont boldFont = exporter.createFont();
+			boldFont.setFontHeightInPoints((short) 8);
+			boldFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+
+			HSSFCellStyle headerCellStyle = exporter.createCellStyle();
+		    headerCellStyle.setBorderBottom(HSSFCellStyle.BORDER_MEDIUM);
+		    headerCellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER );
+		    headerCellStyle.setFont(boldFont);
+			exporter.addHeaderCell("Cliente", exporter.getWidth(40), headerCellStyle);
+			exporter.addHeaderCell("Usuario", exporter.getWidth(30), headerCellStyle);
+			exporter.addHeaderCell("Fecha", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Tipo Expediente", exporter.getWidth(25), headerCellStyle);
+			exporter.addHeaderCell("Expediente", exporter.getWidth(45), headerCellStyle);
+			exporter.addHeaderCell("Tipo Actividad", exporter.getWidth(25), headerCellStyle);
+			exporter.addHeaderCell("Tipo Trabajo", exporter.getWidth(25), headerCellStyle);
+			exporter.addHeaderCell("Duración (1)", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Duración (2)", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Costo Unitario", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Coste", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Comentarios", exporter.getWidth(100), headerCellStyle);
+			HSSFCellStyle defaultStyle = exporter.createCellStyle();
+			defaultStyle.setFont(font);
+
+			HSSFCellStyle dateStyle = exporter.createCellStyle();
+			dateStyle.setDataFormat( exporter.getDataFormat().getFormat(ExcelReportExporter.DATE_PATTERN));
+			dateStyle.setAlignment(HSSFCellStyle.ALIGN_LEFT );
+			dateStyle.setFont(font);
+
+			HSSFCellStyle amountStyle = exporter.createCellStyle();
+			amountStyle.setFont(font);
+			amountStyle.setAlignment(HSSFCellStyle.ALIGN_RIGHT );
+			amountStyle.setDataFormat( exporter.getDataFormat().getFormat(ExcelReportExporter.DECIMAL_PATTERN));
+			
+			for ( Object obj : getCollection() ) {
+				DailyTrackingReport bs = (DailyTrackingReport) obj;
+				exporter.startLine();
+				exporter.addStringCell( bs.getRegistryName() , defaultStyle );
+				exporter.addStringCell( bs.getUserName() , defaultStyle );
+				exporter.addDateCell( bs.getDate() , dateStyle );
+				exporter.addStringCell( bs.getProjectTypeDescription() , defaultStyle );
+				exporter.addStringCell( bs.getProjectName() , defaultStyle );
+				exporter.addStringCell( bs.getActivityTypeDescription() , defaultStyle );
+				exporter.addStringCell( bs.getJobTypeDescription() , defaultStyle );
+				String hours = (bs.getHours() != 0?(bs.getHours() + " h. "):"") + bs.getMinutes() + " min.";
+				exporter.addStringCell( hours , amountStyle );
+				exporter.addDecimalCell( bs.getDuration() , amountStyle );
+				exporter.addDecimalCell( bs.getCost() , amountStyle );
+				exporter.addDecimalCell( bs.getAmount() , amountStyle );
+				exporter.addStringCell( bs.getComments() , defaultStyle );
+				exporter.endLine();
+			}
+			exporter.endExport(out);
+			
+		} catch (ReportException e) {
+			e.printStackTrace();
+			String msg = "No se pudo generar el listado";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+			String msg = "No se pudo generar el listado";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} finally {
+			DownloadUtil.finishDownload(response, out);
+		}
 	}
 
 }
