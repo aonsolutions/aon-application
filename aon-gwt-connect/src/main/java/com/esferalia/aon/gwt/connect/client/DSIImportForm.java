f65f4e4443f125e30f8f585e90115699637e046b
Alvaro Mtz de Lagos <amtzdelagos@esferalia.com>
2014-09-23 13:55:28 +0000

    Pantalla importar base de datos. Drag and drop activado. Se despliega arbol cuando lista el archivo importado.

--- aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/client/DSIImportForm.java
+++ aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/client/DSIImportForm.java
@@ -4,18 +4,35 @@ import com.esferalia.aon.gwt.common.client.AON;
 import com.esferalia.aon.gwt.common.client.RootLayoutPanel;
 import com.esferalia.aon.gwt.common.client.css.AonResources;
 import com.esferalia.aon.gwt.common.client.css.GWTResources;
+import com.esferalia.aon.gwt.common.client.widget.MinimizePanel;
+import com.esferalia.aon.gwt.common.client.widget.ResultsPanel;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.DisclosurePanel;
 import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteHandler;
 import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.SuggestBox;
+import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.datepicker.client.DateBox;
 
@@ -25,76 +42,180 @@ public class DSIImportForm implements EntryPoint {
 	}
 
 	private static final Binder binder = GWT.create(Binder.class);
-	
-	@UiField DateBox dateBox;
-	@UiField FormPanel uploadFormPanel;
-	@UiField FileUpload fileUpload;
-	@UiField Button sendButton;
+
+	@UiField
+	DateBox dateBox;
+	@UiField
+	FormPanel uploadFormPanel;
+	@UiField
+	TabLayoutPanel footTabPanel;
+	@UiField
+	SplitLayoutPanel splitLayoutPanel;
+	@UiField
+	MinimizePanel footPanel;
+	@UiField
+	FileUpload fileUpload;
+	@UiField
+	Button sendButton;
+	@UiField
+	ScrollPanel sPanel;
+	@UiField
+	VerticalPanel vPanel;
+	@UiField
+	FlexTable layout;
+	@UiField
+	SimplePanel simplePanel;
+
+	ResultsPanel resultsPanel;
 	
 
 	@Override
 	public void onModuleLoad() {
-		
+
 		// Inject rich styles.
 		GWT.<GWTResources> create(GWTResources.class).css().ensureInjected();
 		GWT.<AonResources> create(AonResources.class).css().ensureInjected();
 
-		// Create the UI defined in DSIImportForm.ui.xml.
+		// Create the UI defined in Employee.ui.xml.
 		Widget ui = binder.createAndBindUi(this);
 
 		// Add the outer panel to the RootLayoutPanel, so that it will be
 		// displayed.
 		RootLayoutPanel root = RootLayoutPanel.get("rootPanel");
 		root.add(ui);
-		
+		sPanel.setVisible(false);
+		resultsPanel = new ResultsPanel();
+
 		init();
-	}	
-	
-	protected void init () {
-		
+	}
+
+	protected void init() {
+
 		dateBox.setFormat(new DateBox.DefaultFormat(AON.DATE_FORMAT));
-		
+
 		uploadFormPanel.setAction("/aon_gwt_connect/dsiimport");
 		uploadFormPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
-		uploadFormPanel.setMethod(FormPanel.METHOD_POST);		
-		
+		uploadFormPanel.setMethod(FormPanel.METHOD_POST);
+
 		uploadFormPanel.getElement().setDraggable("DRAGGABLE_TRUE");
-	}
-	// ------------------------------------------------------------- UiHandlers
-	
-	@UiHandler ("uploadFormPanel")
-	void onSubmit(SubmitEvent event) {
-	
-		if("".equalsIgnoreCase(fileUpload.getFilename()) == false) {
-			Window.alert("Subiendo archivo");
-			//NOW WHAT¿?
-		}
 		
+	}
+	
+	private void showEnterprisesList() {
+		
+		sPanel.getElement().getStyle().setBackgroundColor("#FFFFFF");
+		vPanel.add(createAdvancedForm());
+		sPanel.setVisible(true);
+	}
+
+	private Widget createAdvancedForm() {
+		FlexCellFormatter cellFormater = layout.getFlexCellFormatter();
+		layout.clear();		
+		layout.getElement().getStyle().setBackgroundColor("#FFFFFF");		
+
+		// Create some advanced options
+		
+		CheckBox selectAll = new CheckBox();
+		selectAll.setTitle("Seleccionar todas");
+		
+		layout.setWidget(0, 0, selectAll);
+		
+		MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
+		String[] words = {"Vitoria", "Bilbao", "Pamplona", "San Sebastian", "Santander"};
+		oracle.add(words[0]);
+		oracle.add(words[1]);
+		oracle.add(words[2]);
+		oracle.add(words[3]);
+		final SuggestBox suggest = new SuggestBox(oracle);
+		layout.setWidget(0, 1, suggest);
+		Grid advancedOptions = new Grid(12, 2);
+		advancedOptions.setCellSpacing(6);
+		advancedOptions.setWidget(0, 0, new CheckBox());
+		advancedOptions.setHTML(0, 1, "Nombre empresa 1");
+		advancedOptions.setWidget(1, 0, new CheckBox());
+		advancedOptions.setHTML(1, 1, "Nombre empresa 2");
+		advancedOptions.setWidget(2, 0, new CheckBox());
+		advancedOptions.setHTML(2, 1, "Nombre empresa 3");
+		advancedOptions.setWidget(3, 0, new CheckBox());
+		advancedOptions.setHTML(3, 1, "Nombre empresa 4");
+		//****
+		advancedOptions.setWidget(4, 0, new CheckBox());
+		advancedOptions.setHTML(4, 1, "Nombre empresa 5");
+		advancedOptions.setWidget(5, 0, new CheckBox());
+		advancedOptions.setHTML(5, 1, "Nombre empresa 6");
+		advancedOptions.setWidget(6, 0, new CheckBox());
+		advancedOptions.setHTML(6, 1, "Nombre empresa 7");
+		advancedOptions.setWidget(7, 0, new CheckBox());
+		advancedOptions.setHTML(7, 1, "Nombre empresa 8");
+		//****
+		advancedOptions.setWidget(8, 0, new CheckBox());
+		advancedOptions.setHTML(8, 1, "Nombre empresa 9");
+		advancedOptions.setWidget(9, 0, new CheckBox());
+		advancedOptions.setHTML(9, 1, "Nombre empresa 10");
+		advancedOptions.setWidget(10, 0, new CheckBox());
+		advancedOptions.setHTML(10, 1, "Nombre empresa 11");
+		advancedOptions.setWidget(11, 0, new CheckBox());
+		advancedOptions.setHTML(11, 1, "Nombre empresa 12");
+
+		// Add advanced options to form in a disclosure panel
+		
+		DisclosurePanel advancedDisclosure = new DisclosurePanel("Listado de Empresas: ");
+		advancedDisclosure.setOpen(true);
+		advancedDisclosure.setAnimationEnabled(true);
+		advancedDisclosure.setContent(advancedOptions);
+		layout.setWidget(3, 0, advancedDisclosure);
+		cellFormater.setColSpan(3, 0, 2);
+		
+		simplePanel.setWidget(layout);
+		return simplePanel;
+	}
+
+	private void showResultsPanel() {
+
+		InlineLabel resultsTab = new InlineLabel("Resultados");
+		resultsTab.addStyleName(AON.AON_ICON_TIME);
+		resultsTab.addStyleName(AON.AON_ICON_CMD_BUTTON);		
+
+		DSIImportForm.this.footTabPanel.add(DSIImportForm.this.resultsPanel,
+		 resultsTab);
+
+		DSIImportForm.this.splitLayoutPanel.setWidgetSize(
+				DSIImportForm.this.footPanel, Window.getClientHeight() / 4);
+	}
+
+	// ------------------------------------------------------------- UiHandlers
+
+	@UiHandler("uploadFormPanel")
+	void onSubmit(SubmitEvent event) {
+
+		if ("".equalsIgnoreCase(fileUpload.getFilename()) == false) {
+			Window.alert("Subiendo archivo");
+			// NOW WHAT¿?
+		}
+
 		else {
 			Window.alert("Subida cancelada");
 			event.cancel();
 		}
- 	}	
-	
-	@UiHandler ("uploadFormPanel")
-	void onSubmitComplete(SubmitCompleteEvent event) {
-		
-		//refresh page??
-		
-		Window.alert("Todo ok");
-	}
-	
-	@UiHandler ("sendButton")
-	void onClick (ClickEvent event) {
-				
-		 try {
-			 uploadFormPanel.submit();
-		 }catch(Exception ex) {
-			 Window.alert("Error en submit");
-		 }finally {
-			 fileUpload.getElement().setPropertyString("value", "");
-		 }
-		 
 	}
 
+	@UiHandler("uploadFormPanel")
+	void onSubmitComplete(SubmitCompleteEvent event) {
+		// refresh page??
+		Window.alert("Todo ok");
+	}
+
+	@UiHandler("sendButton")
+	void onClick(ClickEvent event) {
+		try {
+			uploadFormPanel.submit();
+			fileUpload.getElement().setPropertyString("value", "");
+			//De momento siempre muestro. 
+			showResultsPanel();
+			showEnterprisesList();			
+		} catch (Exception ex) {
+			Window.alert("Error en submit");
+		}
+	}
+	
 }
