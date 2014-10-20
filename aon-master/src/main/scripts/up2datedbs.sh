ab9e82deb62778990e7869676cba538554036aea
Euke Castellano <ecastellano@esferalia.com>
2014-10-20 11:35:21 +0000

    RPM aon-master

--- aon-master/src/main/scripts/up2datedbs.sh
+++ aon-master/src/main/scripts/up2datedbs.sh
@@ -29,7 +29,6 @@ CLASSPATH=$CLASSPATH:/usr/share/java/commons-dbutils.jar:/usr/share/java/commons
 
 ERR=1
 
-DEPLOYED_XML=/etc/tomcat6/aon.workspace/deployed.xml
 
 function getDBs() {
 	mysql -h $1 -u $2 --password=$3 -sNe "SHOW DATABASES" | \
@@ -40,15 +39,18 @@ function getDBs() {
 
 function getOption() {
 	local OPTION=$1;
-	echo "cat /applications/options/option[@name='$OPTION']/@value" | \
-	xmllint --shell  $DEPLOYED_XML | grep  "value"|  sed -e 's/\s*value="\(.*\)"/\1/';
+	grep -E "^[[:space:]]*$OPTION[[:space:]]*=" /etc/aon-aio/connection | cut -d"=" -f2
 }
 
+#driverClass=org.gjt.mm.mysql.Driver
+#jdbcUrl=jdbc:mysql://sig.cdhc46h3yjvt.eu-west-1.rds.amazonaws.com:3306
+#user=dbuser
+#password=dbpassword
 
 
-URL=`getOption 'hibernate.connection.url'`
-USERNAME=`getOption 'hibernate.connection.username'`
-PASSWORD=`getOption 'hibernate.connection.password'`
+URL=`getOption 'jdbcUrl'`
+USERNAME=`getOption 'user'`
+PASSWORD=`getOption 'password'`
 
 HOST=`echo $URL | sed -e 's/.*\/\/\([^:^,^\/]*\).*/\1/'`
 
@@ -60,5 +62,3 @@ for DB in $DBS; do
         ERR=$(java -classpath $CLASSPATH com.code.aon.master.Up2DateDB $URL/$DB  $USERNAME  $PASSWORD com.mysql.jdbc.Driver 2>&1);
         [ $? -eq 0 ] && echo -e "\\033[1;32mOK\\033[0;39m" || echo -e "\\033[1;31mERROR $ERR\\033[0;39m";
 done
-
-
