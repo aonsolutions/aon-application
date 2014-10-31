a18ef35b0b4b76ad11dc3e7ed1f7d44c7dde8b90
ecastellano <ecastellano@esferalia.com>
2014-10-31 09:41:49 +0100

    Contabilidad. Listado Excel desglosado del extracto de cuentas.

--- aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/report/TrialBalanceController.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/accounting/controller/report/TrialBalanceController.java
@@ -1,16 +1,36 @@
 package com.code.aon.ui.accounting.controller.report;
 
+import static com.esferalia.aon.jooq.tables.Account.ACCOUNT;
+import static com.esferalia.aon.jooq.tables.AccountEntry.ACCOUNT_ENTRY;
+import static com.esferalia.aon.jooq.tables.AccountEntryDetail.ACCOUNT_ENTRY_DETAIL;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Connection;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.DataModel;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.jooq.DSLContext;
+import org.jooq.Record8;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.code.aon.AonVersion;
 import com.code.aon.accounting.Period;
 import com.code.aon.accounting.enumeration.AccountEntryType;
 import com.code.aon.accounting.enumeration.Quarter;
@@ -20,14 +40,20 @@ import com.code.aon.accounting.summary.SummaryProvider;
 import com.code.aon.accounting.summary.SummaryProviderParameters;
 import com.code.aon.accounting.util.AccountingUtil;
 import com.code.aon.accounting.util.Balance;
-import com.code.aon.AonVersion;
 import com.code.aon.common.ICollectionProvider;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.domain.DomainManager;
+import com.code.aon.common.enumeration.MimeType;
 import com.code.aon.common.enumeration.SecurityLevel;
 import com.code.aon.common.util.CommonUtil;
+import com.code.aon.dbutils.DatabaseUtil;
+import com.code.aon.faces.component.util.DownloadUtil;
+import com.code.aon.pool.AonConnectionException;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ql.util.ExpressionException;
 import com.code.aon.ql.util.ExpressionUtilities;
+import com.code.aon.report.ReportException;
+import com.code.aon.report.poi.ExcelReportExporter;
 import com.code.aon.ui.accounting.IAccountingConstants;
 import com.code.aon.ui.accounting.controller.entry.AccountEntryController;
 import com.code.aon.ui.accounting.util.AccountingPeriodUtil;
@@ -224,7 +250,7 @@ public class TrialBalanceController extends DataScrollerState implements ICollec
 				c.getModel().setRowIndex(0);
 				c.onSelect(event);
 			} else {
-				String msg = "No existen cuentas contables con el identiicador " + summary.getCode()
+				String msg = "No existen cuentas contables con el identificador " + summary.getCode()
 						+ "*.";
 				AonUtil.addErrorMessage(msg);
 				throw new AbortProcessingException(msg);
@@ -334,4 +360,152 @@ public class TrialBalanceController extends DataScrollerState implements ICollec
 		getParameters().setRowsPerPage(pageLimit);
 	}
 	
+	public String onExcelReport() {
+		HttpServletResponse response = null;
+		OutputStream out = null;
+		Connection connection = null;
+		try {
+			int domainId = DomainManager.getCurrentDomain();
+			connection = DatabaseUtil.getConnection(AonUtil.getDomainName());
+			DSLContext ctx = DSL.using(connection, AccountingUtil.getDefaultSettings());
+			
+			java.sql.Date fromDate = new java.sql.Date(getParameters().getFromDate().getTime());
+			java.sql.Date toDate = new java.sql.Date(getParameters().getToDate().getTime());
+			
+			String filename = "MayorDeCuentas";
+			response = DownloadUtil.getResponse();
+			out = DownloadUtil.initDownload(response, filename, MimeType.MIME_MS_EXCEL);
+			ExcelReportExporter exporter = new ExcelReportExporter();
+			exporter.startExport(filename);
+			
+			HSSFFont font = exporter.createFont();
+			font.setFontHeightInPoints((short) 8);
+
+			HSSFFont boldFont = exporter.createFont();
+			boldFont.setFontHeightInPoints((short) 8);
+			boldFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+
+			HSSFCellStyle headerCellStyle = exporter.createCellStyle();
+		    headerCellStyle.setBorderBottom(HSSFCellStyle.BORDER_MEDIUM);
+		    headerCellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER );
+		    headerCellStyle.setFont(boldFont);
+		    
+			exporter.addHeaderCell("Cuenta", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Descripción.", exporter.getWidth(40), headerCellStyle);
+			exporter.addHeaderCell("N. Apunte", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Fecha", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Concepto", exporter.getWidth(20), headerCellStyle);
+			exporter.addHeaderCell("Debe", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Haber", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("S. Deudor", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("S. Acreedor", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Contrapartida", exporter.getWidth(10), headerCellStyle);
+			exporter.addHeaderCell("Descripción contrapartida.", exporter.getWidth(20), headerCellStyle);
+			exporter.addHeaderCell("N. Documento", exporter.getWidth(20), headerCellStyle);
+			
+			HSSFFont firstAccountFont = exporter.createFont();
+			firstAccountFont.setFontHeightInPoints((short) 8);
+			firstAccountFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+			firstAccountFont.setColor(new HSSFColor.BLUE().getIndex());
+
+			HSSFCellStyle firstAccountCellStyle = exporter.createCellStyle();
+			firstAccountCellStyle.setFont(firstAccountFont);
+			
+			HSSFCellStyle firstAccountAmountStyle = exporter.createCellStyle();
+			firstAccountAmountStyle.setFont(firstAccountFont);
+			firstAccountAmountStyle.setDataFormat( exporter.getDataFormat().getFormat(ExcelReportExporter.DECIMAL_PATTERN));
+
+			HSSFCellStyle defaultStyle = exporter.createCellStyle();
+			defaultStyle.setFont(font);
+			
+			HSSFCellStyle dateStyle = exporter.createCellStyle();
+			dateStyle.setDataFormat( exporter.getDataFormat().getFormat(ExcelReportExporter.DATE_PATTERN));
+			dateStyle.setFont(font);
+
+			HSSFCellStyle amountStyle = exporter.createCellStyle();
+			amountStyle.setFont(font);
+			amountStyle.setDataFormat( exporter.getDataFormat().getFormat(ExcelReportExporter.DECIMAL_PATTERN));
+			
+			@SuppressWarnings("unchecked")
+			List<Summary> list = (List<Summary>) getModel().getWrappedData();
+			for ( Summary summary : list ) {
+				Result<Record8<Integer, java.sql.Date, String, Double, Double, String, String, String>> 
+					record = ctx.select(ACCOUNT_ENTRY.ID,ACCOUNT_ENTRY.ENTRY_DATE,ACCOUNT_ENTRY_DETAIL.CONCEPT
+								,ACCOUNT_ENTRY_DETAIL.DEBIT,ACCOUNT_ENTRY_DETAIL.CREDIT,
+								ACCOUNT.CODE,ACCOUNT.DESCRIPTION,ACCOUNT_ENTRY_DETAIL.DOCUMENT_NUMBER)
+							.from(ACCOUNT_ENTRY_DETAIL)
+							.join(ACCOUNT_ENTRY).on(ACCOUNT_ENTRY_DETAIL.ACCOUNT_ENTRY.equal(ACCOUNT_ENTRY.ID) )
+							.leftOuterJoin(ACCOUNT).on(ACCOUNT_ENTRY_DETAIL.BALANCING_ACCOUNT.equal(ACCOUNT.ID) )
+							.where(ACCOUNT_ENTRY_DETAIL.DOMAIN.equal(domainId))
+							.and(ACCOUNT_ENTRY_DETAIL.ACCOUNT.equal(summary.getAccountId()))
+							.and(ACCOUNT_ENTRY.ENTRY_DATE.greaterOrEqual(fromDate))
+							.and(ACCOUNT_ENTRY.ENTRY_DATE.lessOrEqual(toDate))
+							.orderBy(ACCOUNT_ENTRY.ENTRY_DATE,ACCOUNT_ENTRY.ID)
+							.fetch();
+				
+				double balance = CommonUtil.round( summary.getInitialUnpaidBalance() - summary.getInitialCreditBalance());
+				
+				exporter.startLine();
+				exporter.addStringCell( summary.getCode() , firstAccountCellStyle );
+				exporter.addStringCell( summary.getDescription() , firstAccountCellStyle );
+				exporter.addStringCell( null, firstAccountCellStyle );
+				exporter.addStringCell( null, firstAccountCellStyle );
+				exporter.addStringCell( "SALDO INICIAL", firstAccountCellStyle );
+				exporter.addStringCell( null, firstAccountCellStyle );
+				exporter.addStringCell( null, firstAccountCellStyle );
+				exporter.addDecimalCell( summary.getInitialUnpaidBalance(), firstAccountAmountStyle );
+				exporter.addDecimalCell( summary.getInitialCreditBalance(), firstAccountAmountStyle );
+				exporter.endLine();
+				
+				for (Record8<Integer, java.sql.Date, String, Double, Double, String, String, String> step : record) {
+					Integer entryId = step.value1();
+					Date entryDate = step.value2();
+					String concept = step.value3();
+					Double debit = step.value4();
+					Double credit = step.value5();
+					balance = CommonUtil.round(balance + debit - credit); 
+					String balAccountCode = step.value6();
+					String balAccountDescription = step.value7();
+					String documentNumber = step.value8();
+					exporter.startLine();
+					
+					exporter.addStringCell( summary.getCode() , defaultStyle );
+					exporter.addStringCell( summary.getDescription() , defaultStyle );
+					exporter.addNumberCell( entryId , defaultStyle );
+					exporter.addDateCell( entryDate, dateStyle);
+					exporter.addStringCell( concept , defaultStyle );
+					exporter.addDecimalCell( debit,amountStyle );
+					exporter.addDecimalCell( credit,amountStyle );
+					exporter.addDecimalCell( balance>0?balance:0.0,amountStyle );
+					exporter.addDecimalCell( balance<0?Math.abs(balance):0.0,amountStyle );
+					exporter.addStringCell( balAccountCode , defaultStyle );
+					exporter.addStringCell( balAccountDescription , defaultStyle );
+					exporter.addStringCell( documentNumber, defaultStyle );
+					
+					exporter.endLine();
+				}
+			}
+			exporter.endExport(out);
+			
+		} catch (ReportException e) {
+			e.printStackTrace();
+			String msg = "No se pudo generar el listado";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+			String msg = "No se pudo generar el listado";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} catch (AonConnectionException e) {
+			e.printStackTrace();
+			String msg = "No se pudo generar el listado";
+			AonUtil.addErrorMessage(msg);
+			throw new AbortProcessingException(msg);
+		} finally {
+			DatabaseUtil.closeQuietly(connection);
+			DownloadUtil.finishDownload(response, out);
+		}
+		return null;
+	}
 }
