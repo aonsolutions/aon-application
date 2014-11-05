5fb8014a5660968473b1d8ed22c563b908ebb2bd
Aimar Tellitu <atellitu@esferalia.com>
2014-11-05 13:32:38 +0100

    TOMCAT 8. Estaba fallando en el Perfil de Cliente y en las respuesta de preguntas porque se estaba utlizando la palabra reservada boolean internamente.

--- aon-marketing/src/main/java/com/code/aon/marketing/SurveyWorkflow.java
+++ aon-marketing/src/main/java/com/code/aon/marketing/SurveyWorkflow.java
@@ -24,18 +24,14 @@ public class SurveyWorkflow extends SurveyWorkflowDB implements IValueHolder{
 	}	
 	
 	@Transient
-	private Boolean getBooleanValue() {
+	public Boolean getBooleanValue() {
 		if ( getNumber() != null ) {
 			return (getNumber() != 0);
 		}
 		return null;
 	}
 	
-	@Transient
-	public boolean isBoolean() {
-		return (getNumber() != null) && (getNumber() != 0);
-	}
-	public void setBoolean(boolean b) {
+	public void setBooleanValue(Boolean b) {
 		setNumber(b ? 1.0 : 0);
 	}
 
