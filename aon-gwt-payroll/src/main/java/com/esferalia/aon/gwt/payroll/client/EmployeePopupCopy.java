7673c4053a52de4d3b9d49b26b552b4a4acf978d
Raúl Trepiana <rtrepiana@gmail.com>
2014-12-17 11:50:42 +0100

    Standard 'CustomDialog'

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeePopupCopy.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeePopupCopy.java
@@ -1,24 +1,22 @@
 package com.esferalia.aon.gwt.payroll.client;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.esferalia.aon.gwt.common.client.AON;
+import com.esferalia.aon.gwt.common.client.widget.CustomDialog;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
-import com.google.gwt.user.client.ui.DecoratedPopupPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.datepicker.client.DateBox;
 
-public class EmployeePopupCopy extends DecoratedPopupPanel {
+public class EmployeePopupCopy extends CustomDialog {
 	
 	interface Listener {
 		void onAcceptButtonClickButton (ClickEvent event);
@@ -28,7 +26,7 @@ public class EmployeePopupCopy extends DecoratedPopupPanel {
 			.create(EmployeePopupCopyUiBinder.class);
 
 	@UiField
-	InlineLabel nameEmployee;
+	Label nameEmployee;
 	@UiField
 	DateBox startDate;
 	@UiField
@@ -49,7 +47,9 @@ public class EmployeePopupCopy extends DecoratedPopupPanel {
 	private String fullName;
 
 	public EmployeePopupCopy() {
-		add(uiBinder.createAndBindUi(this));
+		setCaption("Introduce nuevos datos...");
+
+		setWidget(uiBinder.createAndBindUi(this));
 		
 		this.nameEmployee.setStyleName(AON.AON_BLACK + " " + AON.AON_BOLD);
 		
