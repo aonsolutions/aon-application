d0a958de8478746e38c11013f5eb49271c378691
girazu <girazu@esferalia.com>
2015-01-16 13:19:10 +0100

    Modificación manual del cálculo automático de vtos. en facturas (Fixes #95)

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoiceFinanceControllerListener.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/event/InvoiceFinanceControllerListener.java
@@ -1,11 +1,19 @@
 package com.code.aon.ui.finance.event;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.faces.model.SelectItem;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Hibernate;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 
 import com.code.aon.AonVersion;
 import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.dao.hibernate.HibernateUtil;
 import com.code.aon.config.BankAccount;
 import com.code.aon.config.PayMethod;
 import com.code.aon.finance.Finance;
@@ -39,8 +47,9 @@ public class InvoiceFinanceControllerListener extends ControllerAdapter {
 		finance.setRegistryDocumentType(invoice.getRegistryDocumentType());
 		finance.setRegistryDocumentCountry(invoice.getRegistryDocumentCountry());
         finance.setConcept(invoice.getDocumentNumber()); 
-		finance.setSecurityLevel(invoice.getSecurityLevel());
+		finance.setManual(true);
 		finance.setFinanceStatus(FinanceStatus.PENDING);
+		finance.setSecurityLevel(invoice.getSecurityLevel());
 		try {
 			finance.setAmount(invoiceController.getPendingAmount());
 
@@ -75,6 +84,37 @@ public class InvoiceFinanceControllerListener extends ControllerAdapter {
 			financeController.setShowBankManualInput(financeController.getRegistryBank() == null && StringUtils.isNotEmpty(finance.getBankAccount().getIban()));
 		} catch (ManagerBeanException e) {
 			throw new ControllerListenerException(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public void beforeBeanUpdated(ControllerEvent event) throws ControllerListenerException {
+		Finance finance = (Finance)event.getController().getTo();
+		if (!finance.isManual()) {
+			Date dueDate = finance.getDueDate();
+			Integer payMethod = (finance.getPayMethod() != null) ? finance.getPayMethod().getId() : null;
+			String bankAccount = (finance.getBankAccount() != null) ? finance.getBankAccount().getIban() : null;
+			Double amount = finance.getAmount();
+
+			String select = "SELECT finance.due_date dueDate, finance.pay_method payMethod, " +
+	    						"finance.bank_account bankAccount, finance.amount amount " +
+	    						"FROM finance as finance " +
+	    						"WHERE finance.id = " + finance.getId();
+			Session session = HibernateUtil.getSession(HibernateUtil.getSessionFactoryName());
+			SQLQuery query = session.createSQLQuery(select);
+	        List<?> list = query.addScalar("dueDate", Hibernate.DATE).addScalar("payMethod", Hibernate.INTEGER).
+	        				addScalar("bankAccount", Hibernate.STRING).addScalar("amount", Hibernate.DOUBLE).list();
+	        if (!list.isEmpty()) {
+	        	Object[] obj = (Object[])list.get(0);
+	            Date savedDueDate = (Date)obj[0];
+	            Integer savedPayMethod = (Integer)obj[1];
+	            String savedBankAccount = (String)obj[2];
+	            Double savedAmount = (Double)obj[3];
+	            if (!ObjectUtils.equals(savedDueDate, dueDate) || !ObjectUtils.equals(savedPayMethod, payMethod) ||
+	            		!ObjectUtils.equals(savedBankAccount, bankAccount) || !ObjectUtils.equals(savedAmount, amount)) {
+	            	finance.setManual(true);
+	            }
+	        }
 		}
 	}
 
