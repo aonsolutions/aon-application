1dc454b9122216611134f0a741b54b3f49245057
EkainAgirrezabal <eagirrezabal@esferalia.com>
2015-01-19 15:25:44 +0100

    Fix #98

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/SddMandateObject.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/SddMandateObject.java
@@ -52,7 +52,7 @@ public class SddMandateObject implements ITransferObject, ICollectionProvider, S
 
 	public void setRecurrentPayment(Boolean recurrentPayment) {
 		this.recurrentPayment = recurrentPayment;
-		this.oneOffPayment = this.recurrentPayment!=null?!this.recurrentPayment:null;
+		this.oneOffPayment = this.recurrentPayment!=null && this.recurrentPayment?!this.recurrentPayment:null;
 	}
 
 	public Boolean getOneOffPayment() {
@@ -61,7 +61,7 @@ public class SddMandateObject implements ITransferObject, ICollectionProvider, S
 
 	public void setOneOffPayment(Boolean oneOffPayment) {
 		this.oneOffPayment = oneOffPayment;
-		this.recurrentPayment = !this.oneOffPayment;
+		this.recurrentPayment = this.oneOffPayment!=null && this.oneOffPayment?!this.oneOffPayment:null;
 	}
 
 	@Override
