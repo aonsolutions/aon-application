de2027abaa28b6aa146fad7aa85aadb789c9e62b
Raúl Trepiana <rtrepiana@gmail.com>
2015-02-19 18:14:06 +0100

    The Class 'AonSQLFile' literally, is a sucks . It will be removed tonight , so hotfixes and new versions will be frozen, until this class will be replace by smart one.

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.15.2.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.15.2.sql
@@ -1,37 +1,36 @@
+
+
 # Database: aon_master
 # Version: Actualizacion de la version 8.15.2 a la version 8.16.0.
 # Created by: rtrepiana
 # Creation Date: 11/02/2015 
 
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-
 BEGIN;
 
+DROP PROCEDURE IF EXISTS `UPDATE_8_15_2`;
 
-DELIMITER $$
-CREATE PROCEDURE `UPDATE_8_15_2`(pattern VARCHAR(100))
-BEGIN
-	SET @TO_REMOVE=pattern;
+#DELIMITER $$
+CREATE PROCEDURE `UPDATE_8_15_2`(IN pattern VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_spanish_ci)
+BEGIN	
+	SET @TO_REMOVE=pattern;	/*Skips SQLFile Statement End*/
 
 	UPDATE `payment_concept` SET `expression`=REPLACE(expression, @TO_REMOVE, "" )  
-	WHERE  `expression` LIKE CONCAT('%DIAS_TRABAJADOS%',@TO_REMOVE,'%')
-	OR `expression` LIKE CONCAT('%', @TO_REMOVE,'%DIAS_TRABAJADOS%')
-	;
+	WHERE  `expression` LIKE CONCAT('%DIAS_TRABAJADOS%',@TO_REMOVE,'%') 
+	OR `expression` LIKE CONCAT('%', @TO_REMOVE,'%DIAS_TRABAJADOS%'); /*Skips SQLFile Statement End*/
 
 	UPDATE `agreement_payment` SET `expression`=REPLACE(expression, @TO_REMOVE, "" )  
-	WHERE  `expression` LIKE CONCAT('%DIAS_TRABAJADOS%',@TO_REMOVE,'%')
-	OR `expression` LIKE CONCAT('%', @TO_REMOVE,'%DIAS_TRABAJADOS%')
-	;
+	WHERE  `expression` LIKE CONCAT('%DIAS_TRABAJADOS%',@TO_REMOVE,'%') 
+	OR `expression` LIKE CONCAT('%', @TO_REMOVE,'%DIAS_TRABAJADOS%');  /*Skips SQLFile Statement End*/
+	
+	UPDATE `contract_payment` SET `expression`=REPLACE(expression, @TO_REMOVE, "" ) 
+	WHERE  `expression` LIKE CONCAT('%DIAS_TRABAJADOS%',@TO_REMOVE,'%') 
+	OR `expression` LIKE CONCAT('%', @TO_REMOVE,'%DIAS_TRABAJADOS%'); /*Skips SQLFile Statement End*/
+END
+#$$ DELIMITER 
+; 
 
-	UPDATE `contract_payment` SET `expression`=REPLACE(expression, @TO_REMOVE, "" )  
-	WHERE  `expression` LIKE CONCAT('%DIAS_TRABAJADOS%',@TO_REMOVE,'%')
-	OR `expression` LIKE CONCAT('%', @TO_REMOVE,'%DIAS_TRABAJADOS%')
-	;
-END$$
-DELIMITER ; 
 
-
-CALL UPDATE_8_15_2("* COEFICIENTE_PARCIALIDAD");
+CALL UPDATE_8_15_2("* COEFICIENTE_PARCIALIDAD") ;
 CALL UPDATE_8_15_2("*COEFICIENTE_PARCIALIDAD");
 CALL UPDATE_8_15_2("* ( TIEMPO_COMPLETO ? 1 : (HORAS_SEMANA / HORAS_CONVENIO))");
 CALL UPDATE_8_15_2("*( TIEMPO_COMPLETO ? 1 : (HORAS_SEMANA / HORAS_CONVENIO))");
@@ -46,4 +45,3 @@ UPDATE `db_version` SET `version_number` = '8.16.0';
 
 COMMIT;
 
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
