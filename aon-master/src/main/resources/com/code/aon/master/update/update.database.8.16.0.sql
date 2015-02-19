03dfaf49140ae5f4ecd5c9d8a383373b8b724d90
rtrepiana <rtrepiana@esferalia.com>
2015-02-20 00:21:07 +0100

    Sorry, sorry

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.16.0.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.16.0.sql
@@ -14,27 +14,27 @@ BEGIN;
 INSERT INTO `system_cost` 
 (`domain`	,`start_date`	,`end_date`	,`description`	,`expression`	,`type`	,`code`) VALUES 
 ("-101"		,"2015-01-01"	,NULL		,NULL		,"30.67"	,"0"	,"CGC_E");
-UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="CGC_E"; 
+UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="CGC_E" AND `start_date`="2014-01-01" ; 
 
 INSERT INTO `system_cost` 
 (`domain`	,`start_date`	,`end_date`	,`description`	,`expression`	,`type`	,`code`) VALUES 
 ("-101"		,"2015-01-01"	,NULL		,NULL		,"2.36"		,"1"	,"IT_E");
-UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="IT_E"; 
+UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="IT_E" AND `start_date`="2014-01-01"; 
 
 INSERT INTO `system_cost` 
 (`domain`	,`start_date`	,`end_date`	,`description`	,`expression`	,`type`	,`code`) VALUES 
 ("-101"		,"2015-01-01"	,NULL		,NULL		,"1.86"		,"1"	,"IMS_E");
-UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="IMS_E"; 
+UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="IMS_E" AND `start_date`="2014-01-01"; 
 
 INSERT INTO `system_cost` 
 (`domain`	,`start_date`	,`end_date`	,`description`	,`expression`	,`type`	,`code`) VALUES 
 ("-101"		,"2015-01-01"	,NULL		,NULL		,"2.33"		,"10"	,"FOGASA_E");
-UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="FOGASA_E"; 
+UPDATE `system_cost` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `code`="FOGASA_E" AND `start_date`="2014-01-01"; 
 
 INSERT INTO `system_deduction` 
 (`domain`	,`type`	,`deduction_concept`	,`description`	,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
 ("-101"		,NULL	,"6299"			,NULL		,"0"			,"6.12"		,"2015-01-01"	,NULL		,NULL);
-UPDATE `system_deduction` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `deduction_concept`="6299";
+UPDATE `system_deduction` SET `end_date`="2014-12-31" WHERE `domain`=-101 AND `deduction_concept`="6299" AND `start_date`="2014-01-01";
 
 UPDATE `db_version` SET `version_number` = '8.16.1';
 
