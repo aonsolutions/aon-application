3fb3e3c3404bf08321e0d3356905daeb404263fd
girazu <girazu@esferalia.com>
2015-02-24 14:52:17 +0100

    Eliminar espacios en la grabacion de AddInfos (Fixes #202)

---
+++ aon-product/src/main/java/com/code/aon/product/event/ItemAddInfoBeanVetoListener.java
@@ -0,0 +1,29 @@
+package com.code.aon.product.event;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.code.aon.AonVersion;
+import com.code.aon.common.event.ManagerBeanEvent;
+import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
+import com.code.aon.common.event.ManagerBeanVetoListenerException;
+import com.code.aon.product.ItemAddInfo;
+
+public class ItemAddInfoBeanVetoListener extends ManagerBeanVetoListenerAdapter {
+	
+	private static final long serialVersionUID = AonVersion.SERIAL_VERSION_UID;
+
+    @Override
+    public void vetoableBeanInserted(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
+    	ItemAddInfo to = (ItemAddInfo)evt.getTo();
+    	to.setAttribute(StringUtils.trim(to.getAttribute()));
+    	to.setValue(StringUtils.trim(to.getValue()));
+    }
+
+	@Override
+	public void vetoableBeanUpdated(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
+		ItemAddInfo to = (ItemAddInfo)evt.getTo();
+    	to.setAttribute(StringUtils.trim(to.getAttribute()));
+    	to.setValue(StringUtils.trim(to.getValue()));
+	}
+
+}
