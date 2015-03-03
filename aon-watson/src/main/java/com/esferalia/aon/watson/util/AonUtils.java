c11cd70b9b30c12f0d2eac33e3e5cfbf8012a6d8
Raúl Trepiana <rtrepiana@gmail.com>
2015-03-03 17:10:53 +0100

    Helper date functions ( mainly a wrapper of java.util.Calendar ) and 'null' aware 'equals' & 'hashCode'

---
+++ aon-watson/src/main/java/com/esferalia/aon/watson/util/AonUtils.java
@@ -0,0 +1,47 @@
+package com.esferalia.aon.watson.util;
+
+public class AonUtils {
+
+	/**
+	 * Compares two objects for equality, where either one or both objects may
+	 * be null.
+	 * 
+	 * ObjectUtils.equals(null, null)                  = true
+ 	 * ObjectUtils.equals(null, "")                    = false
+ 	 * ObjectUtils.equals("", null)                    = false
+ 	 * ObjectUtils.equals("", "")                      = true
+ 	 * ObjectUtils.equals(Boolean.TRUE, null)          = false
+ 	 * ObjectUtils.equals(Boolean.TRUE, "true")        = false
+ 	 * ObjectUtils.equals(Boolean.TRUE, Boolean.TRUE)  = true
+ 	 * ObjectUtils.equals(Boolean.TRUE, Boolean.FALSE) = false
+	 * 
+	 * @param obj1
+	 *            the first object, may be null
+	 * @param obj2
+	 *            the second object, may be null
+	 * @return true if the values of both objects are the same
+	 */
+	public static boolean equals(final Object obj1, final Object obj2) {
+		if (obj1 == obj2) {
+			return true;
+		}
+		if (obj1 == null || obj2 == null) {
+			return false;
+		}
+		return obj1.equals(obj2);
+	}
+	
+	/**
+	 * Gets the hash code of an object returning zero when the object is null.
+	 * 
+	 * ObjectUtils.hashCode(null)   = 0
+	 * ObjectUtils.hashCode(obj)    = obj.hashCode()
+	 * 
+	 * @param obj the object to obtain the hash code of, may be null
+	 * @return the hash code of the object, or zero if null
+	 */
+	public static int hashCode(Object obj){
+		return obj == null ? 0 : obj.hashCode();
+	}
+
+}
