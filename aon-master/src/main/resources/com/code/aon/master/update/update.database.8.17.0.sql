fc1945e5be0378c08bd91ef667ab7bbc6d6548e1
Raúl Trepiana <rtrepiana@gmail.com>
2015-03-14 22:59:07 +0100

    Complete MUFACE and ISFAS regimes

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.17.0.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.17.0.sql
@@ -8,6 +8,12 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 BEGIN;
 
 
+DELETE FROM `system_data` WHERE  domain = -8;
+DELETE FROM `system_data` WHERE  domain = -9;
+DELETE FROM `system_deduction` WHERE  domain = -8;
+DELETE FROM `system_deduction` WHERE  domain = -9;
+
+
 INSERT INTO system_data 
 ( domain, name			, expression	, start_date	, end_date		, read_only , comments )  VALUES 
 (-8	, 'PORCENTAJE_ISFAS'	, '1.69'	, '2015-01-01'	, NULL			, 1			, NULL ),
@@ -22,7 +28,7 @@ INSERT INTO system_data
 
 INSERT INTO `system_data` (`domain`,`name`,`expression`,`start_date`,`end_date`,`read_only`,`comments`)
 VALUES ("-8",
-	"HABER_REGULADOR",
+	"BASE_CGC_MIN",
 	"[
 	\"01\":2839.07, 
 	\"02\":2234.42, 
@@ -35,10 +41,26 @@ VALUES ("-8",
 	NULL,
 	"1",
 	NULL);
+
+INSERT INTO system_data 
+( domain, name			, expression	, start_date	, end_date	, read_only 	, comments )  VALUES 
+(-8	, 'BASE_CGC_MAX'	, 'BASE_CGC_MIN', '2015-01-01'	, NULL		, 1		, NULL )
+;
+
+INSERT INTO system_data 
+( domain, name			, expression	, start_date	, end_date	, read_only 	, comments )  VALUES 
+(-8	, 'BASE_CGP_MIN'	, 'BASE_CGC_MIN', '2015-01-01'	, NULL		, 1		, NULL )
+;
+
+INSERT INTO system_data 
+( domain, name			, expression	, start_date	, end_date	, read_only 	, comments )  VALUES 
+(-8	, 'BASE_CGP_MAX'	, 'BASE_CGC_MIN', '2015-01-01'	, NULL		, 1		, NULL )
+;
+
 
 INSERT INTO `system_data` (`domain`,`name`,`expression`,`start_date`,`end_date`,`read_only`,`comments`)
 VALUES ("-9",
-	"HABER_REGULADOR",
+	"BASE_CGC_MIN",
 	"[
 	\"01\":2839.07, 
 	\"02\":2234.42, 
@@ -52,32 +74,56 @@ VALUES ("-9",
 	"1",
 	NULL);
 
-INSERT INTO `system_deduction` 
-(`domain`	,`type`	,`deduction_concept`	,`description`	,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
-("-8"		,10		,NULL					,"ISFAS"		,"0"					,"HABER_REGULADOR * PORCENTAJE_ISFAS / 100.00"	
-																									,"2015-01-01"	,NULL		,NULL);
+INSERT INTO system_data 
+( domain, name			, expression	, start_date	, end_date		, read_only , comments )  VALUES 
+(-9	, 'BASE_CGC_MAX'	, 'BASE_CGC_MIN', '2015-01-01'	, NULL			, 1			, NULL )
+;
+
+INSERT INTO system_data 
+( domain, name			, expression	, start_date	, end_date	, read_only 	, comments )  VALUES 
+(-8	, 'BASE_CGP_MIN'	, 'BASE_CGC_MIN', '2015-01-01'	, NULL		, 1		, NULL )
+;
+
+INSERT INTO system_data 
+( domain, name			, expression	, start_date	, end_date	, read_only 	, comments )  VALUES 
+(-8	, 'BASE_CGP_MAX'	, 'BASE_CGC_MIN', '2015-01-01'	, NULL		, 1		, NULL )
+;
+
 
 INSERT INTO `system_deduction` 
 (`domain`	,`type`	,`deduction_concept`	,`description`	,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
-("-9"		,10		,NULL					,"MUFACE"		,"0"					,"HABER_REGULADOR * PORCENTAJE_ISFAS / 100.00"	
-																									,"2015-01-01"	,NULL		,NULL);
+("-8"		,10		,NULL		,"Instituto Social de las Fuerzas Armadas (ISFAS)"	
+								,"0"			,"BASE_CGC * PORCENTAJE_ISFAS / 100.00"	
+													,"2015-01-01"	,NULL		,NULL);
 
 INSERT INTO `system_deduction` 
-(`domain`	,`type`	,`deduction_concept`	,`description`			,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
-("-8"		,10		,NULL					,"DERECHOS PASIVOS"		,"0"			,"HABER_REGULADOR * PORCENTAJE_DCHOS / 100.00"	
-																											,"2015-01-01"	,NULL		,NULL);
+(`domain`	,`type`	,`deduction_concept`	,`description`	,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
+("-9"		,10		,NULL		,"Administración Civil del Estado (MUFACE)"	
+								,"0"			,"BASE_CGC * PORCENTAJE_ISFAS / 100.00"	
+													,"2015-01-01"	,NULL		,NULL);
 
 INSERT INTO `system_deduction` 
-(`domain`	,`type`	,`deduction_concept`	,`description`			,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
-("-9"		,10		,NULL					,"DERECHOS PASIVOS"		,"0"					,"HABER_REGULADOR * PORCENTAJE_DCHOS / 100.00"	
-																											,"2015-01-01"	,NULL		,NULL);
+(`domain`	,`type`	,`deduction_concept`	,`description`		,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
+("-8"		,10		,NULL		,"Cuota de Derechos Pasivos"		
+									,"0"			,"BASE_CGC * PORCENTAJE_DCHOS / 100.00"	
+																		,"2015-01-01"	,NULL		,NULL);
+
+INSERT INTO `system_deduction` 
+(`domain`	,`type`	,`deduction_concept`	,`description`		,`description_decorable`,`expression`	,`start_date`	,`end_date`	,`month`) VALUES 
+("-9"		,10		,NULL		,"Cuota de Derechos Pasivos"
+									,"0"			,"BASE_CGC * PORCENTAJE_DCHOS / 100.00"	
+																		,"2015-01-01"	,NULL		,NULL);
+INSERT INTO `system_deduction` 
+(`domain`	, `type`, `deduction_concept`	, `description`		, `description_decorable`	, `expression`	, `start_date`, `end_date`, `month`) VALUES 
+(-8		,NULL	,6305			,'@{PORCENTAJE_IRPF} %'	,0				,'BASE_IRPF * PORCENTAJE_IRPF/100','2010-01-01',NULL,NULL);
+
+INSERT INTO `system_deduction` 
+(`domain`	, `type`, `deduction_concept`	, `description`		, `description_decorable`	, `expression`	, `start_date`, `end_date`, `month`) VALUES 
+(-9		,NULL	,6305			,'@{PORCENTAJE_IRPF} %'	,0				,'BASE_IRPF * PORCENTAJE_IRPF/100','2010-01-01',NULL,NULL);
 
 UPDATE `db_version` SET `version_number` = '8.17.1';
 
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-
-
-
 
