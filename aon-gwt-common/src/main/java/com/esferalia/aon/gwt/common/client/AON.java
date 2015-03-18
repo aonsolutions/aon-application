f9c256ed7e63a6e93d3fdf0dcf4ce61b4fccd46d
Raúl Trepiana <rtrepiana@gmail.com>
2015-03-18 14:50:26 +0100

    14x14 cost image missed constant

--- aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/AON.java
+++ aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/AON.java
@@ -157,6 +157,7 @@ public class AON {
 
 	
 	public static final String AON_ICON_CALENDAR = "aon-icon-calendar";
+	public static final String AON_ICON_COST = "aon-icon-cost";
 
 	public static final NumberFormat CURRENCY_FORMAT = NumberFormat
 			.getFormat("#,##0.00");
