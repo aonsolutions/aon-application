03d36c100a41f035bd9fad2061243b827ec2dcab
Raúl Trepiana <rtrepiana@gmail.com>
2015-04-29 19:27:47 +0200

    ECSS % ATEP costs (delegate payment) type to OTHER

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.21.1.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.21.1.sql
@@ -11,14 +11,15 @@ SET @DESCRIPTION=(SELECT description FROM `system_cost` WHERE code='ECSS_E' LIMI
 DELETE FROM `system_cost` WHERE code='ECSS_E';
 
 INSERT INTO `system_cost` ( `domain`, `code`, `type`, `expression`, `description`, `start_date` ) 
-VALUES (0, 'ECSS_E', 0, '-1 * DIAS_ENFERMEDAD_COMUN_16_20 * BASE_REGULADORA * 0.60 ', @DESCRIPTION, '2010-01-01');
+VALUES (0, 'ECSS_E', 8, '-1 * DIAS_ENFERMEDAD_COMUN_16_20 * BASE_REGULADORA * 0.60 ', @DESCRIPTION, '2010-01-01');
 
 INSERT INTO `system_cost` ( `domain`, `code`, `type`, `expression`, `description`, `start_date` ) 
-VALUES (0, 'ECSS_E', 0, '-1 * DIAS_ENFERMEDAD_COMUN_21 * BASE_REGULADORA * 0.75 ', @DESCRIPTION, '2010-01-01');
+VALUES (0, 'ECSS_E', 8, '-1 * DIAS_ENFERMEDAD_COMUN_21 * BASE_REGULADORA * 0.75 ', @DESCRIPTION, '2010-01-01');
 
 
 UPDATE `system_cost` 
-SET expression='-1 * DIAS_ENFERMEDAD_PROFESIONAL * BASE_REGULADORA * 0.75'
+SET expression='-1 * DIAS_ENFERMEDAD_PROFESIONAL * BASE_REGULADORA * 0.75',
+type=8
 WHERE `code`='ATEP_E';
 
 
