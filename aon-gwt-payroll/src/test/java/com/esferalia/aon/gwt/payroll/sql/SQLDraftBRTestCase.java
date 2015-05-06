700f56bcd7806b94f763b62601b94b9dfb1dbcc1
rtrepiana <rtrepiana@esferalia.com>
2015-05-06 22:48:18 +0200

    Test BR with draft context

---
+++ aon-gwt-payroll/src/test/java/com/esferalia/aon/gwt/payroll/sql/SQLDraftBRTestCase.java
@@ -0,0 +1,45 @@
+package com.esferalia.aon.gwt.payroll.sql;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+
+import com.code.aon.ql.Criteria;
+import com.esferalia.aon.gwt.payroll.shared.Salary;
+import com.esferalia.aon.gwt.payroll.shared.SalaryDraft;
+import com.esferalia.aon.payroll.calculator.sql.ISQLContractSalaryCalculatorContext;
+import com.esferalia.aon.payroll.calculator.sql.SQLBRTestCase;
+import com.esferalia.aon.salary.expression.ExpressionException;
+
+public class SQLDraftBRTestCase extends SQLBRTestCase {
+	@Override
+	protected ISQLContractSalaryCalculatorContext getContractSalaryCalculatorContext(
+			Connection connection, Date startDate, Date endDate,
+			Date issueDate, Criteria criteria) throws ExpressionException,
+			SQLException {
+
+		SalaryDraft draft = new SalaryDraft();
+
+		
+//		for ( int i = 0; i <  6; i ++ ) {
+//			com.esferalia.aon.gwt.payroll.shared.Payment draftPayment = new com.esferalia.aon.gwt.payroll.shared.Payment();
+//			draftPayment.setStartDate(startDate);
+//			draftPayment.setEndDate(endDate);
+//			draftPayment.setExpression("66666/6");
+//			draftPayment.setIrpfExpression("_P");
+//			draftPayment.setQuoteExpression("_P");
+//			draftPayment.setSalaryType(Salary.Type.SALARY);
+//			draftPayment
+//					.setDescription("RETRIBUCION NO INCLUIDA EN OTROS APARTADOS");
+//			draftPayment
+//					.setType(com.esferalia.aon.gwt.payroll.shared.Payment.Type.CRA_0001);
+//	
+//			draft.addDraftPayment(draftPayment);
+//		}
+
+
+		SQLSalaryDraftCalculatorContext draftCtx = new SQLSalaryDraftCalculatorContext(
+				draft, connection, startDate, endDate, issueDate, criteria);
+		return draftCtx;
+	}
+}
