dbf9b68f39afd14c7a60b76640a1feb8de85b584
Aimar Tellitu <atellitu@esferalia.com>
2015-06-01 12:55:24 +0200

    Exportación Conect@ Contaplus. Codigo de subcuentas (Fixes #326)

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/ContaPlusWriter.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/ContaPlusWriter.java
@@ -9,7 +9,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -109,13 +108,6 @@ public class ContaPlusWriter extends BasicExporter {
 		return getNextTax(getTaxBreakDowns());
 	}
 	
-	private Account getRegistryAccount() {
-		if ( getRegistryDetail() != null ) {
-			return getRegistryDetail().getAccount();
-		}
-		return null;
-	}
-	
 	private String getSalesAccount() {
 		if ( "T".equals(getInvoiceSeries()) ) {
 			return SALES_T_ACCOUNT;
@@ -129,14 +121,12 @@ public class ContaPlusWriter extends BasicExporter {
 	
 	private String getAccount( Account account ) {
 		String code = account.getCode();
-		if ( ObjectUtils.equals(account, getRegistryAccount()) ) {
-			code = StringUtils.substring(code, 0, 3) + "0" + StringUtils.substring(code, 3); 
-		} else if ( StringUtils.equals(salesAccount, code) ) {
+		if ( StringUtils.equals(salesAccount, code) ) {
 			code = getSalesAccount();
 		} else if ( StringUtils.equals(chargedVat, code) ) {
 			code = CHARGED_VAT_ACCOUNT;
 		} else {
-			code = getAccountCode(code);
+			code = StringUtils.substring(code, 0, 3) + "0" + StringUtils.substring(code, 3);
 		}
 		return getString( code, 12 );
 	}
