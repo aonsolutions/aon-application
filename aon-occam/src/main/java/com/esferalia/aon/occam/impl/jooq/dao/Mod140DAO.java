a32d5516a1ab4ad828aea938ff02e6fb3d1404ed
ecastellano <ecastellano@esferalia.com>
2015-06-02 09:27:29 +0200

    Fiscal. Ajustes Modelo 140. Bizkaia.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/Mod140DAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/Mod140DAO.java
@@ -1,7 +1,9 @@
 package com.esferalia.aon.occam.impl.jooq.dao;
 
+import static com.esferalia.aon.jooq.tables.Account.ACCOUNT;
 import static com.esferalia.aon.jooq.tables.Invoice.INVOICE;
 import static com.esferalia.aon.jooq.tables.InvoiceDetail.INVOICE_DETAIL;
+import static com.esferalia.aon.jooq.tables.InvoiceDetailAccount.INVOICE_DETAIL_ACCOUNT;
 import static com.esferalia.aon.jooq.tables.InvoiceTax.INVOICE_TAX;
 
 import java.io.IOException;
@@ -10,11 +12,13 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 import org.jooq.Condition;
+import org.jooq.Record;
+import org.jooq.Result;
 
 import com.esferalia.aon.occam.api.AONContext;
-import com.esferalia.aon.occam.api.model.finance.Invoice;
-import com.esferalia.aon.occam.api.model.finance.InvoiceVAT;
-import com.esferalia.aon.occam.api.model.finance.InvoiceWithholding;
+import com.esferalia.aon.occam.api.model.fiscal.Mod140;
+import com.esferalia.aon.occam.api.model.fiscal.Mod140.Mod140VAT;
+import com.esferalia.aon.occam.api.model.fiscal.Mod140.Mod140Withholding;
 import com.esferalia.aon.occam.api.model.fiscal.Mod140Context;
 import com.esferalia.aon.occam.api.model.fiscal.Mod140Params;
 import com.esferalia.aon.occam.api.model.type.Country;
@@ -31,15 +35,15 @@ import com.esferalia.aon.watson.mutable.MutableInt;
 import com.esferalia.aon.watson.server.AonDateUtils;
 import com.esferalia.aon.watson.util.AonEnumUtils;
 import com.esferalia.aon.watson.util.AonMathUtils;
+import com.esferalia.aon.watson.util.AonStringUtils;
 
 
 public class Mod140DAO {
 	
 	public static void getInvoices(AONContext ctx, Mod140Context m140ctx ,Mod140Params params, Writer writer  ) {
 		ctx.checkRead();
-		final MutableInt lastInvoice = new MutableInt(Integer.MIN_VALUE);
-		final Invoice invoice = new Invoice();
-		ctx.getDslContext()
+		
+		Result<Record> result = ctx.getDslContext()
 			.select(
 				 INVOICE.ID
 				,INVOICE.DOMAIN
@@ -70,6 +74,8 @@ public class Mod140DAO {
 				
 				,INVOICE_DETAIL.TAXABLE_BASE
 				
+				,ACCOUNT.CODE
+				
 				,INVOICE_TAX.TAX_TYPE
 				,INVOICE_TAX.BASE
 				,INVOICE_TAX.PERCENTAGE
@@ -83,129 +89,140 @@ public class Mod140DAO {
 			.from(INVOICE)
 			.join(INVOICE_DETAIL).on(INVOICE_DETAIL.INVOICE.equal(INVOICE.ID))
 			.join(INVOICE_TAX).on(INVOICE_TAX.INVOICE_DETAIL.equal(INVOICE_DETAIL.ID))
+			.leftOuterJoin(INVOICE_DETAIL_ACCOUNT).on(INVOICE_DETAIL.ID.equal(INVOICE_DETAIL_ACCOUNT.INVOICE_DETAIL))
+			.leftOuterJoin(ACCOUNT).on(INVOICE_DETAIL_ACCOUNT.ACCOUNT.equal(ACCOUNT.ID))
+			
 			.where(getConditions(params))
-			// -------------------------------------- //
-			// --------- FILTER OPTIONS ------------- //
-			// -------------------------------------- //
 			.orderBy(INVOICE.TYPE,INVOICE.SERIES,INVOICE.NUMBER)
-			.fetch()
-			.stream()
-			.forEach( (record -> {
-				Integer id = record.getValue( INVOICE.ID );
-				if (!lastInvoice.getValue().equals(id)) {
-					lastInvoice.setValue(id);
-					if (invoice.getId() != null) {
-						try {
-							Mod140Format.fill(writer, m140ctx , invoice);
-						} catch (IOException e) {
-							throw new AonCoreException(e.getMessage(), e); 
-						}
-					}
-					invoice.initialize();
-					invoice.setId(id);
-					invoice.setDomain(record.getValue( INVOICE.DOMAIN ));
-					invoice.setSeries(record.getValue( INVOICE.SERIES ));
-					invoice.setNumber(record.getValue( INVOICE.NUMBER ));
-					invoice.setEpigraph(m140ctx.getEpigraph());
-					invoice.setReferenceCode(record.getValue( INVOICE.REFERENCE_CODE ));
-					invoice.setIssueDate(record.getValue( INVOICE.ISSUE_DATE ));
-					invoice.setTaxDate(record.getValue( INVOICE.TAX_DATE ));
-					invoice.setRectificationType(
-							AonEnumUtils.enumValue(RectificationType.class, record.getValue( INVOICE.RECTIFICATION_TYPE))
-							);
-					invoice.setRectificationInvoice(record.getValue( INVOICE.RECTIFICATION_INVOICE ));
-					invoice.setRegistryDocument(record.getValue( INVOICE.RDOCUMENT ));
-					invoice.setRegistryDocumentType(
-							AonEnumUtils.enumValue(DocumentType.class, record.getValue( INVOICE.RDOCUMENT_TYPE))
-							);
-					invoice.setRegistryDocumentCountry(Country.safeValueOf(record.getValue( INVOICE.RDOCUMENT_COUNTRY )));
-					invoice.setRegistryName(record.getValue( INVOICE.RNAME ));
-					invoice.setType(
-							AonEnumUtils.enumValue(InvoiceType.class, record.getValue( INVOICE.TYPE ))
-							);
-					invoice.setTransaction(
-							AonEnumUtils.enumValue(InvoiceTransactionType.class, record.getValue( INVOICE.TRANSACTION ))
-							);
-					invoice.setRecorded(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.STATUS))
-							);
-					invoice.setSurcharge(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.SURCHARGE))
-							);
-					invoice.setWithholding(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.WITHHOLDING ))
-							);
-					invoice.setWithholdingFarmer(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.WITHHOLDING_FARMER  ))
-							);
-					invoice.setVatAccrualPayment(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.VAT_ACCRUAL_PAYMENT ))
-							);
-					invoice.setInvestment(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.INVESTMENT ))
-							);
-					invoice.setService(
-							com.esferalia.aon.watson.server.AonEnumUtils.getBoolean(record.getValue( INVOICE.SERVICE ))
-							);
-					invoice.setTaxableBase(record.getValue( INVOICE.TAXABLE_BASE ));
-					invoice.setVatQuota(record.getValue( INVOICE.VAT_QUOTA ));
-					invoice.setRetentionQuota(record.getValue( INVOICE.RETENTION_QUOTA ));
-					invoice.setTotal(record.getValue( INVOICE.TOTAL ));
+			.fetch();
+		
+		final MutableInt lastInvoice = new MutableInt(Integer.MIN_VALUE);
+		final Mod140 inv = new Mod140();
+		
+		for (Record record : result) {
+			Integer id = record.getValue(INVOICE.ID);
+			if (!lastInvoice.getValue().equals(id)) {
+				lastInvoice.setValue(id);
+				if (inv.getId() != null) {
+					fillInvoice(writer, m140ctx, inv);
 				}
-				
-				TaxType taxType = AonEnumUtils.enumValue(TaxType.class, record.getValue( INVOICE_TAX.TAX_TYPE));
-				
-				double base = record.getValue( INVOICE_TAX.BASE );
-				double percentage = record.getValue( INVOICE_TAX.PERCENTAGE );
-				double quota = record.getValue( INVOICE_TAX.QUOTA );
-				
-				if (taxType == TaxType.RETENTION) {
-					if (invoice.getWithholdingData() == null ) {
-						invoice.setWithholdingData( new InvoiceWithholding());
-					}	
-						invoice.getWithholdingData().setBase(AonMathUtils.round(
-								invoice.getWithholdingData().getBase()
+				populateInvoice(record, inv, m140ctx);
+				inv.setId(id);
+			}
+			
+			String account = record.getValue(ACCOUNT.CODE);
+			account = AonStringUtils.defaultString(account, AonStringUtils.EMPTY);
+			account = AonStringUtils.substring(account, 0, 3);
+			account = AonStringUtils.rightPad(account, 3);
+			
+			TaxType taxType = AonEnumUtils.enumValue(TaxType.class,
+					record.getValue(INVOICE_TAX.TAX_TYPE));
+
+			double base = record.getValue(INVOICE_TAX.BASE);
+			double percentage = record.getValue(INVOICE_TAX.PERCENTAGE);
+			double quota = record.getValue(INVOICE_TAX.QUOTA);
+
+			if (taxType == TaxType.RETENTION) {
+				if (inv.getWithholdingData() == null) {
+					inv.setWithholdingData(new Mod140Withholding());
+				}
+				inv.getWithholdingData().setBase(
+						AonMathUtils.round(inv.getWithholdingData().getBase()
 								+ base));
-						invoice.getWithholdingData().setQuota(AonMathUtils.round(
-								invoice.getWithholdingData().getQuota()
+				inv.getWithholdingData().setQuota(
+						AonMathUtils.round(inv.getWithholdingData().getQuota()
 								+ quota));
-						
-						// TODO Si hay mas de un tipo de retención el dato se machaca.
-						invoice.getWithholdingData().setPercentage(percentage);
-						invoice.getWithholdingData().setWithholdingType(
-							AonEnumUtils.enumValue(WithholdingType.class, record.getValue( INVOICE_TAX.WITHHOLDING_TYPE))				
-								);
-						// ------------------------------------------------------------
-				}
-				if (taxType == TaxType.VAT) {
-					double surcharge = record.getValue( INVOICE_TAX.SURCHARGE );
-					double surchargeQuota = record.getValue( INVOICE_TAX.SURCHARGE_QUOTA );
-					double deductibleQuota = record.getValue( INVOICE_TAX.DEDUCTIBLE_QUOTA );
-					InvoiceVAT vat = invoice.ensureInvoiceVAT(percentage,surcharge);
-					vat.setBase(AonMathUtils.round(vat.getBase() + base));
-					vat.setQuota(AonMathUtils.round(
-							vat.getQuota() 
-							+ ((quota == 0.0)
-									?AonMathUtils.round(base * percentage / 100 )
-									:quota)));
-					vat.setSurchargeQuota(AonMathUtils.round(
-							vat.getSurchargeQuota() 
-							+ ((surcharge != 0.0 && surchargeQuota == 0.0)
-									?AonMathUtils.round(base * surcharge / 100 )
-									:surchargeQuota)));
-					vat.setDeductibleQuota(AonMathUtils.round(
-							vat.getSurchargeQuota() 
-							+ ((deductibleQuota == 0.0)
-									?AonMathUtils.round(base * percentage / 100 )
-									:deductibleQuota)));
-					// TODO Si hay mas de un tipo de retención el dato se machaca.
-					vat.setVatDeductionType(
-							AonEnumUtils.enumValue(VatDeductionType.class, record.getValue( INVOICE_TAX.VAT_DEDUCTION_TYPE))				
-								);
-					// -----------------------------------------------------------
-				}
-				
-			}));
+
+				// TODO Si hay mas de un tipo de retención el dato se machaca.
+				inv.getWithholdingData().setPercentage(percentage);
+				inv.getWithholdingData().setWithholdingType(
+						AonEnumUtils.enumValue(WithholdingType.class,
+								record.getValue(INVOICE_TAX.WITHHOLDING_TYPE)));
+				// ------------------------------------------------------------
+			}
+			if (taxType == TaxType.VAT) {
+				double surcharge = record.getValue(INVOICE_TAX.SURCHARGE);
+				double surchargeQuota = record
+						.getValue(INVOICE_TAX.SURCHARGE_QUOTA);
+				double deductibleQuota = record
+						.getValue(INVOICE_TAX.DEDUCTIBLE_QUOTA);
+				Mod140VAT vat = inv.ensureInvoiceVAT(account,percentage, surcharge);
+				vat.setBase(AonMathUtils.round(vat.getBase() + base));
+				vat.setQuota(AonMathUtils.round(vat.getQuota()
+						+ ((quota == 0.0) ? AonMathUtils.round(base
+								* percentage / 100) : quota)));
+				vat.setSurchargeQuota(AonMathUtils.round(vat
+						.getSurchargeQuota()
+						+ ((surcharge != 0.0 && surchargeQuota == 0.0) ? AonMathUtils
+								.round(base * surcharge / 100) : surchargeQuota)));
+				vat.setDeductibleQuota(AonMathUtils.round(vat
+						.getSurchargeQuota()
+						+ ((deductibleQuota == 0.0) ? AonMathUtils.round(base
+								* percentage / 100) : deductibleQuota)));
+				// TODO Si hay mas de un tipo de retención el dato se machaca.
+				vat.setVatDeductionType(AonEnumUtils.enumValue(
+						VatDeductionType.class,
+						record.getValue(INVOICE_TAX.VAT_DEDUCTION_TYPE)));
+				// -----------------------------------------------------------
+			}
+		}
+		if (inv.getId() != null) {
+			fillInvoice(writer, m140ctx , inv);
+		}
+	}
+
+	private static void populateInvoice(Record record, Mod140 inv,
+			Mod140Context m140ctx) {
+		inv.initialize();
+		inv.setDomain(record.getValue(INVOICE.DOMAIN));
+		inv.setSeries(record.getValue(INVOICE.SERIES));
+		inv.setNumber(record.getValue(INVOICE.NUMBER));
+		inv.setEpigraph(m140ctx.getEpigraph());
+		inv.setReferenceCode(record.getValue(INVOICE.REFERENCE_CODE));
+		inv.setIssueDate(record.getValue(INVOICE.ISSUE_DATE));
+		inv.setTaxDate(record.getValue(INVOICE.TAX_DATE));
+		inv.setRectificationType(AonEnumUtils.enumValue(
+				RectificationType.class,
+				record.getValue(INVOICE.RECTIFICATION_TYPE)));
+		inv.setRectificationInvoice(record
+				.getValue(INVOICE.RECTIFICATION_INVOICE));
+		inv.setRegistryDocument(record.getValue(INVOICE.RDOCUMENT));
+		inv.setRegistryDocumentType(AonEnumUtils.enumValue(DocumentType.class,
+				record.getValue(INVOICE.RDOCUMENT_TYPE)));
+		inv.setRegistryDocumentCountry(Country.safeValueOf(record
+				.getValue(INVOICE.RDOCUMENT_COUNTRY)));
+		inv.setRegistryName(record.getValue(INVOICE.RNAME));
+		inv.setType(AonEnumUtils.enumValue(InvoiceType.class,
+				record.getValue(INVOICE.TYPE)));
+		inv.setTransaction(AonEnumUtils.enumValue(InvoiceTransactionType.class,
+				record.getValue(INVOICE.TRANSACTION)));
+		inv.setRecorded(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.STATUS)));
+		inv.setSurcharge(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.SURCHARGE)));
+		inv.setWithholding(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.WITHHOLDING)));
+		inv.setWithholdingFarmer(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.WITHHOLDING_FARMER)));
+		inv.setVatAccrualPayment(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.VAT_ACCRUAL_PAYMENT)));
+		inv.setInvestment(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.INVESTMENT)));
+		inv.setService(com.esferalia.aon.watson.server.AonEnumUtils
+				.getBoolean(record.getValue(INVOICE.SERVICE)));
+		inv.setTaxableBase(record.getValue(INVOICE.TAXABLE_BASE));
+		inv.setVatQuota(record.getValue(INVOICE.VAT_QUOTA));
+		inv.setRetentionQuota(record.getValue(INVOICE.RETENTION_QUOTA));
+		inv.setTotal(record.getValue(INVOICE.TOTAL));
+	}
+
+
+	private static void fillInvoice(Writer writer, Mod140Context m140ctx, Mod140 inv) {
+		try {
+			Mod140Format.fill(writer, m140ctx , inv);
+		} catch (IOException e) {
+			throw new AonCoreException(e.getMessage(), e); 
+		}
 	}
 
 
@@ -227,4 +244,5 @@ public class Mod140DAO {
 		}
 		return list;
 	}
+	
 }
