c99281854aa141eabf524686932763e13d209c89
Aimar Tellitu <atellitu@esferalia.com>
2015-06-02 10:57:33 +0200

    Marketing. Respuestas de Cuestionarios. Buscar por Fecha y respuestas (Fixes #329)

--- aon-ui-marketing/src/main/java/com/code/aon/ui/marketing/event/SurveyResponseSearchListener.java
+++ aon-ui-marketing/src/main/java/com/code/aon/ui/marketing/event/SurveyResponseSearchListener.java
@@ -1,5 +1,9 @@
 package com.code.aon.ui.marketing.event;
 
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
 import com.code.aon.AonVersion;
 import com.code.aon.common.BeanManager;
 import com.code.aon.common.IManagerBean;
@@ -8,9 +12,15 @@ import com.code.aon.config.User;
 import com.code.aon.marketing.MarketingAction;
 import com.code.aon.marketing.Survey;
 import com.code.aon.ql.Criteria;
+import com.code.aon.ql.ast.Expression;
 import com.code.aon.ql.util.ExpressionException;
+import com.code.aon.ql.util.ExpressionUtilities;
+import com.code.aon.registry.Question;
+import com.code.aon.registry.QuestionValue;
 import com.code.aon.registry.Registry;
+import com.code.aon.ui.common.components.LookupChangeEvent;
 import com.code.aon.ui.form.event.ControllerSearchListener;
+import com.code.aon.ui.registry.controller.RegistryCollectionsController;
 import com.esferalia.aon.entity.IEntityAlias;
 
 public class SurveyResponseSearchListener extends ControllerSearchListener {
@@ -24,6 +34,14 @@ public class SurveyResponseSearchListener extends ControllerSearchListener {
 	private MarketingAction action;
 	
 	private User user;
+	
+	private Question question;
+
+	private QuestionValue questionValue;
+	
+	private List<SelectItem> questionValues;
+	
+	private Integer questionValueId;
 	
 	public User getUser() {
 		return user;
@@ -57,6 +75,78 @@ public class SurveyResponseSearchListener extends ControllerSearchListener {
 		this.registry = registry;
 	}
 
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+	
+	public boolean isQuestionResolved() {
+		return (getQuestion() != null) && (getQuestion().getId() != null);
+	}	
+	
+	public Integer getQuestionValueId() {
+		return questionValueId;
+	}
+
+	public void setQuestionValueId(Integer questionValueId) {
+		this.questionValueId = questionValueId;
+	}
+	
+	public QuestionValue getQuestionValue() {
+		return questionValue;
+	}
+
+	public void setQuestionValue(QuestionValue questionValue) {
+		this.questionValue = questionValue;
+	}	
+
+	public List<SelectItem> getQuestionValues() {
+		return questionValues;
+	}
+	
+	public void setQuestionValues(List<SelectItem> questionValues) {
+		this.questionValues = questionValues;
+	}
+
+	public void questionChanged( LookupChangeEvent event ) throws ManagerBeanException {
+		Question question = (Question) event.getNewValue();
+		if ( event.getNewValue() != null ) {
+			QuestionValue qv = new QuestionValue();
+			qv.setQuestion(question);
+			setQuestionValue( qv );
+			questionValues = RegistryCollectionsController.getQuestionValues(question);
+		} else {
+			resetQuestionValue();
+		}
+	}
+	
+	private void resetQuestionValue() {
+		setQuestionValue(null);
+		setQuestionValueId(null);
+		setQuestionValues(null);		
+	}
+
+	private void completeCriteria( Criteria criteria, QuestionValue qv ) {
+		switch ( qv.getQuestion().getType() ) {
+			case BOOLEAN:
+			case NUMBER:
+				criteria.addEqualExpression("SurveyResponse.details.number", qv.getNumber());
+				break;
+			case DATE:
+				criteria.addEqualExpression("SurveyResponse.details.date", qv.getDate());
+				break;
+			case TEXT:
+				Expression exp = ExpressionUtilities.getLikeExpression("SurveyResponse.details.text", "%"+qv.getText()+"%");
+				criteria.addExpression(exp);
+				break;
+			case INFO:
+				break;
+		}		
+	}	
+	
 	@Override
 	protected void init() throws ManagerBeanException {
 		IManagerBean maBean = BeanManager.getManagerBean(MarketingAction.class);
@@ -67,6 +157,9 @@ public class SurveyResponseSearchListener extends ControllerSearchListener {
 		setRegistry( (Registry) registryBean.createNewTo() );
 		IManagerBean userBean = BeanManager.getManagerBean(User.class);
 		setUser( (User) userBean.createNewTo() );
+		IManagerBean questionBean = BeanManager.getManagerBean(Question.class);
+		setQuestion( (Question) questionBean.createNewTo() );
+		resetQuestionValue();
 	}
 	
 	@Override
@@ -87,5 +180,17 @@ public class SurveyResponseSearchListener extends ControllerSearchListener {
 			String alias = getFieldName(IEntityAlias.SURVEY_RESPONSE_USER_ID);
 			criteria.addEqualExpression(alias, getUser().getId());			
 		}
+		if ( isQuestionResolved() ) {
+			criteria.addEqualExpression("SurveyResponse.details.question.id", getQuestion().getId());	
+			if ( getQuestionValueId() != null ) {
+				IManagerBean bean = BeanManager.getManagerBean(QuestionValue.class);
+				QuestionValue qv = (QuestionValue) bean.get(getQuestionValueId());
+				if ( qv != null ) {
+					completeCriteria(criteria, qv);
+				}
+			} else if (! getQuestionValue().isNotFilled() ) {
+				completeCriteria(criteria, getQuestionValue());
+			}
+		}		
 	}	
 }
