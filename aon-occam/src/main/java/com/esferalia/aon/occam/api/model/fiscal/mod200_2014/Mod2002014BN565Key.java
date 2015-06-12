9ed2c16a0059d8201eab322d7d74322141d8b27a
ecastellano <ecastellano@esferalia.com>
2015-06-12 09:32:03 +0200

    Fiscal. Mod 200. Nodo "Liquidación III" completo. (Pag 15,16,17,17B y parte de 18)

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/mod200_2014/Mod2002014BN565Key.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/mod200_2014/Mod2002014BN565Key.java
@@ -13,19 +13,20 @@ public enum Mod2002014BN565Key implements Serializable, IMod200KeysProvider {
 	// ª --> \u00AA º --> \u00BA
 	// ¿ --> \u00BF
 	
-	 C0001(new Mod2002014Key[]{Mod2002014Key.BN929,Mod2002014Key.BN930,null           },"Donac. a ent. sin fines de lucro (Ley 49/2002) 2003") 
-	,C0002(new Mod2002014Key[]{Mod2002014Key.BN942,Mod2002014Key.BN943,Mod2002014Key.BN944},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2004")
-	,C0003(new Mod2002014Key[]{Mod2002014Key.BN294,Mod2002014Key.BN295,Mod2002014Key.BN296},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2005")
-	,C0004(new Mod2002014Key[]{Mod2002014Key.BN066,Mod2002014Key.BN074,Mod2002014Key.BN084},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2006")
-	,C0005(new Mod2002014Key[]{Mod2002014Key.BN008,Mod2002014Key.BN009,Mod2002014Key.BN010},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2007")
-	,C0006(new Mod2002014Key[]{Mod2002014Key.BN034,Mod2002014Key.BN035,Mod2002014Key.BN036},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2008")
-	,C0007(new Mod2002014Key[]{Mod2002014Key.BN201,Mod2002014Key.BN202,Mod2002014Key.BN203},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2009")
-	,C0008(new Mod2002014Key[]{Mod2002014Key.BN904,Mod2002014Key.BN905,Mod2002014Key.BN906},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2010")
-	,C0009(new Mod2002014Key[]{Mod2002014Key.BN990,Mod2002014Key.BN991,Mod2002014Key.BN992},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2011")
-	,C0010(new Mod2002014Key[]{Mod2002014Key.BN997,Mod2002014Key.BN998,Mod2002014Key.BN999},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2012")
-	,C0011(new Mod2002014Key[]{Mod2002014Key.BN993,Mod2002014Key.BN994,Mod2002014Key.BN995},"Donac. a ent. sin fines de lucro (Ley 49/2002) 2013")
-	,C0012(new Mod2002014Key[]{Mod2002014Key.BN598,null           ,Mod2002014Key.BN895},"Total deducciones donac. a ent. sin fines de lucro (Ley 49/2002)")
-	,C0013(new Mod2002014Key[]{Mod2002014Key.BN974,null           ,null           },"Donaciones del per\u00EDodo impositivo efectuadas a entidades sin fines de lucro (Ley 49/2002)")
+	 C0001(new Mod2002014Key[]{Mod2002014Key.BN929,Mod2002014Key.BN930,null               },Mod2002014Key.BN929.getDescription()) 
+	,C0002(new Mod2002014Key[]{Mod2002014Key.BN942,Mod2002014Key.BN943,Mod2002014Key.BN944},Mod2002014Key.BN942.getDescription())
+	,C0003(new Mod2002014Key[]{Mod2002014Key.BN294,Mod2002014Key.BN295,Mod2002014Key.BN296},Mod2002014Key.BN294.getDescription())
+	,C0004(new Mod2002014Key[]{Mod2002014Key.BN066,Mod2002014Key.BN074,Mod2002014Key.BN084},Mod2002014Key.BN066.getDescription())
+	,C0005(new Mod2002014Key[]{Mod2002014Key.BN008,Mod2002014Key.BN009,Mod2002014Key.BN010},Mod2002014Key.BN008.getDescription())
+	,C0006(new Mod2002014Key[]{Mod2002014Key.BN034,Mod2002014Key.BN035,Mod2002014Key.BN036},Mod2002014Key.BN034.getDescription())
+	,C0007(new Mod2002014Key[]{Mod2002014Key.BN201,Mod2002014Key.BN202,Mod2002014Key.BN203},Mod2002014Key.BN201.getDescription())
+	,C0008(new Mod2002014Key[]{Mod2002014Key.BN904,Mod2002014Key.BN905,Mod2002014Key.BN906},Mod2002014Key.BN904.getDescription())
+	,C0009(new Mod2002014Key[]{Mod2002014Key.BN990,Mod2002014Key.BN991,Mod2002014Key.BN992},Mod2002014Key.BN990.getDescription())
+	,C0010(new Mod2002014Key[]{Mod2002014Key.BN997,Mod2002014Key.BN998,Mod2002014Key.BN999},Mod2002014Key.BN997.getDescription())
+	,C0011(new Mod2002014Key[]{Mod2002014Key.BN246,Mod2002014Key.BN247,Mod2002014Key.BN248},Mod2002014Key.BN246.getDescription())
+	,C0012(new Mod2002014Key[]{Mod2002014Key.BN993,Mod2002014Key.BN994,Mod2002014Key.BN995},Mod2002014Key.BN993.getDescription())
+	,C0013(new Mod2002014Key[]{Mod2002014Key.BN598,null               ,Mod2002014Key.BN895},Mod2002014Key.BN598.getDescription())
+	,C0014(new Mod2002014Key[]{Mod2002014Key.BN974,null               ,null               },Mod2002014Key.BN974.getDescription())
 	;
 	 
     private String description;
