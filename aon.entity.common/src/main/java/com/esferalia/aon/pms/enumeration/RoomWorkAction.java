810fbf68887eb185320ab66ad3addf9015025fbe
girazu <girazu@esferalia.com>
2015-06-12 10:20:25 +0200

    NUEVO LISTADO DE PLANIFICACION DE TRABAJO GOBERNANTA (Fixes #244)

--- aon.entity.common/src/main/java/com/esferalia/aon/pms/enumeration/RoomWorkAction.java
+++ aon.entity.common/src/main/java/com/esferalia/aon/pms/enumeration/RoomWorkAction.java
@@ -5,18 +5,18 @@ import java.util.ResourceBundle;
 
 import com.code.aon.common.enumeration.IResourceable;
 
-public enum RoomWorkPlanning implements IResourceable {
+public enum RoomWorkAction implements IResourceable {
 
-	
-	CHECKIN,
-	CHECKOUT,
+	FREE,
+	CHECK_IN,
+	CHECK_OUT,
+	CHECK_OUT_IN,
 	SHEET_CHANGE,
 	CLEANING,
-	FREE,
 	BLOCKED;
     
     /** Message key prefix. */
-    private static final String MSG_KEY_PREFIX = "aon_enum_room_work_planning_";
+    private static final String MSG_KEY_PREFIX = "aon_enum_room_work_action_";
     
     /**
      * Returns a <code>String</code> with the transalation <code>Locale</code>
