9925bc65146488017fba4f8875afa799d5faf899
ecastellano <ecastellano@esferalia.com>
2015-06-24 17:53:56 +0200

    Fiscal. Mod 200. (Fixes #236) Importación ZIP de ficheros oficiales AEAT ejercicio 2013.

--- aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/server/ZippedMod200BOEImportUpload.java
+++ aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/server/ZippedMod200BOEImportUpload.java
@@ -1,10 +1,12 @@
-package com.esferalia.aon.gwt.fiscal.server;
+package com.esferalia.aon.gwt.connect.server;
 
 import gwtupload.server.UploadAction;
 import gwtupload.server.exceptions.UploadActionException;
 import gwtupload.server.exceptions.UploadException;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
@@ -12,17 +14,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
 
-import com.esferalia.aon.occam.api.model.fiscal.mod200_2013.Mod2002013;
-import com.esferalia.aon.occam.server.fiscal.format.mod200.Mod200Reader;
+import com.esferalia.aon.watson.server.io.AonIOUtils;
 
-@WebServlet(name = "Mod200 - 2013 BOE Upload ", urlPatterns = { "/aon_gwt_fiscal/Mod2002013BOEUpload" })
-public class Mod2002013BOEUpload extends UploadAction {
+@WebServlet(name = "Zipped Mod200 - 2013 BOE Upload ", urlPatterns = { "/aon_gwt_connect/ZippedMod200BOEImportUpload" })
+public class ZippedMod200BOEImportUpload extends UploadAction {
+
+	private static final long serialVersionUID = -8711190307608213317L;
+
 
 	@Override
 	public void checkRequest(HttpServletRequest request) {
 		maxSize = 10485760;
 		super.checkRequest(request);
 	}
+
 
 	@Override
 	public String executeAction(HttpServletRequest request,
@@ -31,10 +36,11 @@ public class Mod2002013BOEUpload extends UploadAction {
 		for (FileItem item : sessionFiles) {
 			if (false == item.isFormField()) {
 				try {
-					ByteArrayInputStream input = new ByteArrayInputStream(
-							item.get());
-					Mod2002013 mod200 = Mod200Reader.getMod2002013(input);
-					request.getSession().setAttribute("Mod2002013Import", mod200);
+					ByteArrayInputStream input = new ByteArrayInputStream(item.get());
+					File zipFile = File.createTempFile("aonMod200BOE", ".zip");
+					FileOutputStream output = new FileOutputStream(zipFile); 
+					AonIOUtils.copy(input, output);
+					request.getSession().setAttribute("aonMod200BOEZIP", zipFile);
 				} catch (Exception ex) {
 					throw new UploadException(ex);
 				}
@@ -46,5 +52,5 @@ public class Mod2002013BOEUpload extends UploadAction {
 		// / Send your customized message to the client.
 		return response;
 	}
-
+	
 }
