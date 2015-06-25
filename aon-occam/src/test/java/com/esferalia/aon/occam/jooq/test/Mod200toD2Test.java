1771d4ee52eea345f66279dc21a7e4bf75e83cc5
ecastellano <ecastellano@esferalia.com>
2015-06-25 12:34:09 +0200

    Traspaso balance de sociedades a deposito de cuentas.

--- aon-occam/src/test/java/com/esferalia/aon/occam/jooq/test/Mod200toD2Test.java
+++ aon-occam/src/test/java/com/esferalia/aon/occam/jooq/test/Mod200toD2Test.java
@@ -3,7 +3,9 @@ package com.esferalia.aon.occam.jooq.test;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.text.NumberFormat;
+import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.AfterClass;
@@ -16,9 +18,15 @@ import com.esferalia.aon.occam.api.AONContext;
 import com.esferalia.aon.occam.api.model.fiscal.d2_deposit.D2DepositHeaderKey;
 import com.esferalia.aon.occam.api.model.fiscal.mod200_2014.Mod2002014;
 import com.esferalia.aon.occam.impl.jooq.dao.d2_deposit.Mod2002014toD2;
+import com.esferalia.aon.watson.util.AonStringUtils;
 
 
 public class Mod200toD2Test {
+	
+	private static final NumberFormat F = NumberFormat.getInstance(new Locale("ES"));
+	static {
+		F.setMinimumFractionDigits(2);
+	}
 
 	private static AONContext ctx;
 //	private static String DOMAIN_NAME = "a50111111-masdemar.ecastellano.dev";
@@ -36,16 +44,65 @@ public class Mod200toD2Test {
 	@Test
 	public void testInitialize() throws IOException {
 		Mod2002014 mod200 = AON.getMod2002014ByYear(DOMAIN_NAME, DOMAIN_ID, 2014);
-		Map<D2DepositHeaderKey, Double> ctx = new HashMap<D2DepositHeaderKey, Double>();
-		Mod2002014toD2.fill(ctx, mod200);
-		for (D2DepositHeaderKey key : ctx.keySet() ) {
-			System.out.println( key.getCode()  + " = "  + ctx.get(key));
-		}
-	}
+		Map<D2DepositHeaderKey, Double> ctx = new LinkedHashMap<D2DepositHeaderKey, Double>();
+		Mod2002014toD2.fillBalance(ctx, mod200);
+		System.out.println(AonStringUtils.repeat('-', 104));
+		System.out.println(AonStringUtils.center("BALANCE DE SITUACION", 104));
+		System.out.println(AonStringUtils.repeat('-', 104));
+		toString(ctx);
+		System.out.println(AonStringUtils.repeat('-', 104));
 		
+		System.out.println();
+		System.out.println();
+		
+		ctx = new LinkedHashMap<D2DepositHeaderKey, Double>();
+		Mod2002014toD2.fillPyg(ctx, mod200);
+		System.out.println(AonStringUtils.repeat('-', 104));
+		System.out.println(AonStringUtils.center("BALANCE DE PERDIDAS Y GANANCIAS", 104));
+		System.out.println(AonStringUtils.repeat('-', 104));
+		toString(ctx);
+		System.out.println(AonStringUtils.repeat('-', 104));
+
+		System.out.println();
+		System.out.println();
+		
+		ctx = new LinkedHashMap<D2DepositHeaderKey, Double>();
+		Mod2002014toD2.fillEcpn(ctx, mod200);
+		System.out.println(AonStringUtils.repeat('-', 104));
+		System.out.println(AonStringUtils.center("BALANCE E.C.P.N.", 104));
+		System.out.println(AonStringUtils.repeat('-', 104));
+		toString(ctx);
+		System.out.println(AonStringUtils.repeat('-', 104));
+
+		System.out.println();
+		System.out.println();
+		
+		ctx = new LinkedHashMap<D2DepositHeaderKey, Double>();
+		Mod2002014toD2.fillEcpn2(ctx, mod200);
+		System.out.println(AonStringUtils.repeat('-', 104));
+		System.out.println(AonStringUtils.center("BALANCE E.C.P.N. (II)", 104));
+		System.out.println(AonStringUtils.repeat('-', 104));
+		toString(ctx);
+		System.out.println(AonStringUtils.repeat('-', 104));
+}
+
 	@AfterClass
 	public static void afterClass() {
 		ctx.finalize();
 	}
 	
+	private void toString(Map<D2DepositHeaderKey, Double> ctx) {
+		for (D2DepositHeaderKey key : ctx.keySet() ) {
+			String desc = null;
+			try {
+				desc = D2DepositHeaderKey.valueOf(key.toString() + "98").getDescription();
+			} catch (IllegalArgumentException e ) {
+				desc = key.toString(); 
+			}
+			desc = AonStringUtils.rightPad(AonStringUtils.abbreviate(desc, 77), 80, ".");
+			System.out.println( desc + " (" + key.getCode()  + ") "  +
+					AonStringUtils.leftPad(F.format(ctx.get(key)), 15)
+					);
+		}
+	}
 }
