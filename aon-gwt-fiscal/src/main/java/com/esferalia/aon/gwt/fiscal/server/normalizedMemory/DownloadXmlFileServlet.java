71c72e33405da14971e5d5aad2faca178d80a2b8
Alvaro Martinez de Lagos <amtzdelagos@aonsolutions.es>
2015-06-30 08:40:20 +0200

    Exportar .zip modelo fiscal

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/normalizedMemory/DownloadXmlFileServlet.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/normalizedMemory/DownloadXmlFileServlet.java
@@ -1,12 +1,22 @@
 package com.esferalia.aon.gwt.fiscal.server.normalizedMemory;
 
-import java.io.BufferedInputStream;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,43 +34,117 @@ public class DownloadXmlFileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    protected void doGet(HttpServletRequest p_request, HttpServletResponse p_response)throws ServletException, IOException{
-        String domain_id = p_request.getParameter("domain_id");
-        Integer domainId = Integer.parseInt(domain_id);
-        String domain = AonUtil.getDomainName();
+	protected void doGet(HttpServletRequest p_request,
+			HttpServletResponse p_response) throws ServletException,
+			IOException {
 
+		p_response.addHeader("Content-Disposition",
+				"inline; filename=\"DEPOSITO.zip\"");
+		// p_response.setContentType("application/octet-stream");
+		p_response.setContentType(MimeType.ZIP.getName());
 
-        File f = DBConsults.getXmlFile(domain, domainId) ;
-        
-       
+		String domain_id = p_request.getParameter("domain_id");
+		Integer domainId = Integer.parseInt(domain_id);
+		String domain = AonUtil.getDomainName();
 
+		File f = DBConsults.getXmlFile(domain, domainId);
+		
+		try {
+			
+			ZipOutputStream zos = makeZip(f, p_response.getOutputStream());
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-        long length = f.length();
-        FileInputStream fis = new FileInputStream(f);
-        
-        p_response.addHeader("Content-Disposition","attachment; filename=\"DEPOSITO\"");
-        //p_response.setContentType("application/octet-stream");
-        p_response.setContentType(MimeType.XML.getName());
+		// FileInputStream input = new FileInputStream(zos.toString());
+		//
+		// long length = f.length();
+		// // FileInputStream fis = new FileInputStream(zos);
+		//
+		// if (length > 0 && length <= Integer.MAX_VALUE)
+		// ;
+		// p_response.setContentLength((int) length);
+		// ServletOutputStream out = p_response.getOutputStream();
+		// p_response.setBufferSize(32768);
+		// int bufSize = p_response.getBufferSize();
+		// byte[] buffer = new byte[bufSize];
+		// BufferedInputStream bis = new BufferedInputStream(input, bufSize);
+		// int bytes;
+		// while ((bytes = bis.read(buffer, 0, bufSize)) >= 0)
+		// out.write(buffer, 0, bytes);
 
-        if (length > 0 && length <= Integer.MAX_VALUE);
-            p_response.setContentLength((int)length);
-        ServletOutputStream out = p_response.getOutputStream();
-        p_response.setBufferSize(32768);
-        int bufSize = p_response.getBufferSize();
-        byte[] buffer = new byte[bufSize];
-        BufferedInputStream bis = new BufferedInputStream(fis,bufSize);
-        int bytes;
-        while ((bytes = bis.read(buffer, 0, bufSize)) >= 0)
-            out.write(buffer, 0, bytes);
-        
-        
-        bis.close();
-        fis.close();
-        out.flush();
-        out.close();
-        
-       //TODO probar --->  libro.close();
+		// bis.close();
+		// fis.close();
+		// out.flush();
+		//
+		// // TODO probar ---> libro.close();
 
-    }
-	
 	}
+
+	// *****************************************************************************
+
+	private static ZipOutputStream makeZip (File directory, OutputStream os) throws IOException {
+		
+		URI base = directory.toURI();
+		Deque<File> queue = new LinkedList<File>();
+		queue.push(directory);
+		Closeable res = os;
+		
+		ZipOutputStream zos = null;
+		
+		try {
+			zos = new ZipOutputStream(os);
+			res = zos;
+			while (!queue.isEmpty()) {
+				directory = queue.pop();
+				
+				for (File kid : directory.listFiles()) {
+					
+					String name = base.relativize(kid.toURI()).getPath();
+					if (kid.isDirectory()) {
+						queue.push(kid);
+						name = name.endsWith("/") ? name : name + "/";
+						zos.putNextEntry(new ZipEntry(name));
+						
+					} else {
+						zos.putNextEntry(new ZipEntry(name));
+						copy(kid, zos);
+						zos.closeEntry();
+					}
+					
+					kid.delete();
+				}
+				directory.delete();
+			}
+		} finally {
+			res.close();
+		}
+		
+		return zos;
+	}
+	
+	 private static void copy(File file, OutputStream out) throws IOException {
+		    InputStream in = new FileInputStream(file);
+		    try {
+		      copy(in, out);
+		    } finally {
+		      in.close();
+		    }
+		  }
+	
+	  private static void copy(InputStream in, OutputStream out) throws IOException {
+		    byte[] buffer = new byte[1024];
+		    while (true) {
+		      int readCount = in.read(buffer);
+		      if (readCount < 0) {
+		        break;
+		      }
+		      out.write(buffer, 0, readCount);
+		    }
+		  }
+	// *****************************************************************************
+
+}
--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/d2_deposit/DBConsults.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/d2_deposit/DBConsults.java
@@ -5,10 +5,7 @@ import static com.esferalia.aon.jooq.tables.Rattach.RATTACH;
 import static com.esferalia.aon.jooq.tables.Registry.REGISTRY;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import org.jooq.Record1;
 import org.jooq.Record2;
@@ -18,97 +15,120 @@ import org.jooq.Record7;
 
 import com.esferalia.aon.occam.api.AONContext;
 import com.esferalia.aon.watson.server.io.AonFileUtils;
-import com.esferalia.aon.watson.server.io.AonIOUtils;
 
 public class DBConsults {
 
-	public static Boolean isDigitalDeposit(String domain, Integer domainId){
+	public static Boolean isDigitalDeposit(String domain, Integer domainId) {
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-		
-			Record2<Integer, byte[]> record = ctx.getDslContext()
-				.select(RATTACH.ID,RATTACH.DATA)
-				.from(RATTACH).join(REGISTRY).on(REGISTRY.ID.eq(RATTACH.REGISTRY))
-				.where(RATTACH.TYPE.eq((byte)17).and(RATTACH.DOMAIN.eq(domainId)))
-				.fetchOne();
-			
+
+			Record2<Integer, byte[]> record = ctx
+					.getDslContext()
+					.select(RATTACH.ID, RATTACH.DATA)
+					.from(RATTACH)
+					.join(REGISTRY)
+					.on(REGISTRY.ID.eq(RATTACH.REGISTRY))
+					.where(RATTACH.TYPE.eq((byte) 17).and(
+							RATTACH.DOMAIN.eq(domainId))).fetchOne();
+
 			return record != null && record.value2() != null;
-			
-		}finally {
-			if (ctx != null) ctx.close();
+
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
 	}
-	
-	public static File getXmlFile(String domain, Integer domainId) throws IOException {
+
+	// Devuelve el xml file d2
+
+	public static File getXmlFile(String domain, Integer domainId)
+			throws IOException {
 		AONContext ctx = null;
+		File parent = null;
+
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-				
-				
+
 			// DOMAIN + DOMAIN SON
-			Record7<Integer, String, Byte, String, byte[], String, String> record = ctx.getDslContext()
-						.select(RATTACH.ID, RATTACH.DESCRIPTION,
-								RATTACH.MIMETYPE, RATTACH.DRIVE_ID, RATTACH.DATA,
-								REGISTRY.DOCUMENT, REGISTRY.NAME)
-						.from(RATTACH).join(REGISTRY).on(REGISTRY.ID.eq(RATTACH.REGISTRY))
-						.where(RATTACH.TYPE.eq((byte)17).and(RATTACH.DOMAIN.eq(domainId)))
-						.fetchOne();
-			
-				
-				
-			File f = File.createTempFile("fiscalTmp",".xml");
+			Record7<Integer, String, Byte, String, byte[], String, String> record = ctx
+					.getDslContext()
+					.select(RATTACH.ID, RATTACH.DESCRIPTION, RATTACH.MIMETYPE,
+							RATTACH.DRIVE_ID, RATTACH.DATA, REGISTRY.DOCUMENT,
+							REGISTRY.NAME)
+					.from(RATTACH)
+					.join(REGISTRY)
+					.on(REGISTRY.ID.eq(RATTACH.REGISTRY))
+					.where(RATTACH.TYPE.eq((byte) 17).and(
+							RATTACH.DOMAIN.eq(domainId))).fetchOne();
+
 			byte[] data;
-			if(record != null){
+			if (record != null) {
+
+				parent = new File(record.value2());
+				parent.mkdir();
+				new File(parent, "Documentos").mkdir();
+				new File(parent, "Documentos TMP").mkdir();
+				new File(parent, "Otros Documentos").mkdir();
+				new File(parent, "Otros Documentos TMP").mkdir();
+
+				File f = File.createTempFile("DEPOSITO", ".xml", parent);				
 				data = record.value5();
+
 				try {
-			       AonFileUtils.writeByteArrayToFile(f, data);
+					AonFileUtils.writeByteArrayToFile(f, data);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			return f;
-			
-		}finally {
-			if (ctx != null) ctx.close();
+
+			return parent;
+
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
 	}
-	
-	public static Esquema getDeposit(String domain , Integer domainId){
+
+	public static Esquema getDeposit(String domain, Integer domainId) {
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-				
-				
+
 			// DOMAIN + DOMAIN SON
-			Record7<Integer, String, Byte, String, byte[], String, String> record = ctx.getDslContext()
-						.select(RATTACH.ID, RATTACH.DESCRIPTION,
-								RATTACH.MIMETYPE, RATTACH.DRIVE_ID, RATTACH.DATA,
-								REGISTRY.DOCUMENT, REGISTRY.NAME)
-						.from(RATTACH).join(REGISTRY).on(REGISTRY.ID.eq(RATTACH.REGISTRY))
-						.where(RATTACH.TYPE.eq((byte)17).and(RATTACH.DOMAIN.eq(domainId)))
-						.fetchOne();
-			
-				
-				
-			File f = new File("/tmp/DEPOSITO.xml"); 
+			Record7<Integer, String, Byte, String, byte[], String, String> record = ctx
+					.getDslContext()
+					.select(RATTACH.ID, RATTACH.DESCRIPTION, RATTACH.MIMETYPE,
+							RATTACH.DRIVE_ID, RATTACH.DATA, REGISTRY.DOCUMENT,
+							REGISTRY.NAME)
+					.from(RATTACH)
+					.join(REGISTRY)
+					.on(REGISTRY.ID.eq(RATTACH.REGISTRY))
+					.where(RATTACH.TYPE.eq((byte) 17).and(
+							RATTACH.DOMAIN.eq(domainId))).fetchOne();
+
+			File f = new File("/tmp/DEPOSITO.xml");
 			byte[] data;
 			Esquema schema = null;
-			if(record != null){
+			if (record != null) {
 				data = record.value5();
 				String document = record.value6();
 				String name = record.value7();
-			}
-			else{
-				Record3<Integer, String, String> reg = ctx.getDslContext().select(ENTERPRISE.REGISTRY, REGISTRY.DOCUMENT, REGISTRY.NAME)
-						.from(ENTERPRISE.join(REGISTRY).on(REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
+			} else {
+				Record3<Integer, String, String> reg = ctx
+						.getDslContext()
+						.select(ENTERPRISE.REGISTRY, REGISTRY.DOCUMENT,
+								REGISTRY.NAME)
+						.from(ENTERPRISE.join(REGISTRY).on(
+								REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
 						.where(ENTERPRISE.DOMAIN.eq(domainId)).fetchOne();
 				Integer registry = reg.value1();
 				String document = reg.value2();
-				String name = reg.value3(); 
+				String name = reg.value3();
 				data = Utils.CreateXml(document, name);
-				insertDeposit(ctx, domain, data, domainId, name, document, registry);
-				
+				insertDeposit(ctx, domain, data, domainId, name, document,
+						registry);
+
 			}
 			try {
 				schema = Utils.readXml(data);
@@ -116,128 +136,178 @@ public class DBConsults {
 				e.printStackTrace();
 			}
 			return schema;
-		}finally {
-			if (ctx != null) ctx.close();
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
 	}
-	
-	public static Esquema getDeposit(String domain , Integer domainId, String idstr){
+
+	public static Esquema getDeposit(String domain, Integer domainId,
+			String idstr) {
 		Integer id = Integer.parseInt(idstr);
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-				
-				
+
 			// DOMAIN + DOMAIN SON
-			Record5<Integer, String, Byte, String, byte[]> record = ctx.getDslContext()
-						.select(RATTACH.ID, RATTACH.DESCRIPTION,
-								RATTACH.MIMETYPE, RATTACH.DRIVE_ID, RATTACH.DATA)
-						.from(RATTACH)
-						.where(RATTACH.ID.eq(id))
-						.fetchOne();
-			
-				
-				
-			File f = new File("/tmp/DEPOSITO.xml"); 
+			Record5<Integer, String, Byte, String, byte[]> record = ctx
+					.getDslContext()
+					.select(RATTACH.ID, RATTACH.DESCRIPTION, RATTACH.MIMETYPE,
+							RATTACH.DRIVE_ID, RATTACH.DATA).from(RATTACH)
+					.where(RATTACH.ID.eq(id)).fetchOne();
+
+			File f = new File("/tmp/DEPOSITO.xml");
 			byte[] data;
 			Esquema schema = null;
-		
-				data = record.value5();
-			
+
+			data = record.value5();
+
 			try {
 				schema = Utils.readXml(data);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			return schema;
-		}finally {
-			if (ctx != null) ctx.close();
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
 	}
-	
-	public static String getCIF(String domain, Integer domainId){
+
+	public static String getCIF(String domain, Integer domainId) {
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-		
-			Record1<String> reg = ctx.getDslContext().select(REGISTRY.DOCUMENT)
-				.from(ENTERPRISE.join(REGISTRY).on(REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
-				.where(ENTERPRISE.DOMAIN.eq(domainId)).fetchOne();
-			
+
+			Record1<String> reg = ctx
+					.getDslContext()
+					.select(REGISTRY.DOCUMENT)
+					.from(ENTERPRISE.join(REGISTRY).on(
+							REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
+					.where(ENTERPRISE.DOMAIN.eq(domainId)).fetchOne();
+
 			return reg.value1();
-		}finally {
-			if (ctx != null) ctx.close();
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
-		
+
 	}
 
-	
-	public static Integer insertDeposit(AONContext ctx , String domain, byte[] b,Integer domainId, String name, String document, Integer registry) {
+	public static Integer insertDeposit(AONContext ctx, String domain,
+			byte[] b, Integer domainId, String name, String document,
+			Integer registry) {
 
-			
-			return ctx.getDslContext().insertInto(RATTACH,RATTACH.REGISTRY,RATTACH.DOMAIN,RATTACH.CATEGORY,RATTACH.MIMETYPE,RATTACH.DESCRIPTION,RATTACH.TYPE,RATTACH.SCOPE,RATTACH.SECURITY_LEVEL,RATTACH.ATTACH_DATE,RATTACH.DATA,RATTACH.DRIVE_ID,RATTACH.DPARENT_ID)
-						.values(registry,domainId,null,(byte) com.esferalia.aon.occam.api.model.type.MimeType.XML.ordinal(), name ,(byte) 17,null,(byte)0,null,b,null,null).returning(RATTACH.ID).fetchOne().getId();
-	
+		return ctx
+				.getDslContext()
+				.insertInto(RATTACH, RATTACH.REGISTRY, RATTACH.DOMAIN,
+						RATTACH.CATEGORY, RATTACH.MIMETYPE,
+						RATTACH.DESCRIPTION, RATTACH.TYPE, RATTACH.SCOPE,
+						RATTACH.SECURITY_LEVEL, RATTACH.ATTACH_DATE,
+						RATTACH.DATA, RATTACH.DRIVE_ID, RATTACH.DPARENT_ID)
+				.values(registry,
+						domainId,
+						null,
+						(byte) com.esferalia.aon.occam.api.model.type.MimeType.XML
+								.ordinal(), name, (byte) 17, null, (byte) 0,
+						null, b, null, null).returning(RATTACH.ID).fetchOne()
+				.getId();
+
 	}
-	
-	public static Integer insertDeposit(String domain, byte[] b,Integer domainId) {
+
+	public static Integer insertDeposit(String domain, byte[] b,
+			Integer domainId) {
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-		
-			Record3<Integer, String, String> reg = ctx.getDslContext().select(ENTERPRISE.REGISTRY, REGISTRY.DOCUMENT, REGISTRY.NAME)
-					.from(ENTERPRISE.join(REGISTRY).on(REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
+
+			Record3<Integer, String, String> reg = ctx
+					.getDslContext()
+					.select(ENTERPRISE.REGISTRY, REGISTRY.DOCUMENT,
+							REGISTRY.NAME)
+					.from(ENTERPRISE.join(REGISTRY).on(
+							REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
 					.where(ENTERPRISE.DOMAIN.eq(domainId)).fetchOne();
 			Integer registry = reg.value1();
 			String document = reg.value2();
-			String name = reg.value3(); 
-			
-			ctx.getDslContext().delete(RATTACH).where(RATTACH.DOMAIN.eq(domainId))
-			.and(RATTACH.TYPE.eq((byte)17)).execute();
-		return ctx.getDslContext().insertInto(RATTACH,RATTACH.REGISTRY,RATTACH.DOMAIN,RATTACH.CATEGORY,RATTACH.MIMETYPE,RATTACH.DESCRIPTION,RATTACH.TYPE,RATTACH.SCOPE,RATTACH.SECURITY_LEVEL,RATTACH.ATTACH_DATE,RATTACH.DATA,RATTACH.DRIVE_ID,RATTACH.DPARENT_ID)
-					.values(registry,domainId,null,(byte) com.esferalia.aon.occam.api.model.type.MimeType.XML.ordinal(), name ,(byte) 17,null,(byte)0,null,b,null,null).returning(RATTACH.ID).fetchOne().getId();
-		
-		}finally {
-			if (ctx != null) ctx.close();
+			String name = reg.value3();
+
+			ctx.getDslContext().delete(RATTACH)
+					.where(RATTACH.DOMAIN.eq(domainId))
+					.and(RATTACH.TYPE.eq((byte) 17)).execute();
+			return ctx
+					.getDslContext()
+					.insertInto(RATTACH, RATTACH.REGISTRY, RATTACH.DOMAIN,
+							RATTACH.CATEGORY, RATTACH.MIMETYPE,
+							RATTACH.DESCRIPTION, RATTACH.TYPE, RATTACH.SCOPE,
+							RATTACH.SECURITY_LEVEL, RATTACH.ATTACH_DATE,
+							RATTACH.DATA, RATTACH.DRIVE_ID, RATTACH.DPARENT_ID)
+					.values(registry,
+							domainId,
+							null,
+							(byte) com.esferalia.aon.occam.api.model.type.MimeType.XML
+									.ordinal(), name, (byte) 17, null,
+							(byte) 0, null, b, null, null)
+					.returning(RATTACH.ID).fetchOne().getId();
+
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
-		
+
 	}
-	
-	public static void insertDeposit(String domain, byte[] b,Integer domainId, String idstr) {
-		Integer id  = Integer.parseInt(idstr);
+
+	public static void insertDeposit(String domain, byte[] b, Integer domainId,
+			String idstr) {
+		Integer id = Integer.parseInt(idstr);
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-		
-			ctx.getDslContext().update(RATTACH).set(RATTACH.DATA, b).where(RATTACH.ID.eq(id)).execute();
 
-		}finally {
-			if (ctx != null) ctx.close();
+			ctx.getDslContext().update(RATTACH).set(RATTACH.DATA, b)
+					.where(RATTACH.ID.eq(id)).execute();
+
+		} finally {
+			if (ctx != null)
+				ctx.close();
 		}
-		
+
 	}
 
-	public static Integer insertDepositText(String domain,String name, byte[] b,Integer domainId) {
+	public static Integer insertDepositText(String domain, String name,
+			byte[] b, Integer domainId) {
 		AONContext ctx = null;
 		try {
 			ctx = AONContext.getAONContext(domain, domainId);
-		
-			Record1<Integer> reg = ctx.getDslContext().select(ENTERPRISE.REGISTRY)
-					.from(ENTERPRISE.join(REGISTRY).on(REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
+
+			Record1<Integer> reg = ctx
+					.getDslContext()
+					.select(ENTERPRISE.REGISTRY)
+					.from(ENTERPRISE.join(REGISTRY).on(
+							REGISTRY.ID.eq(ENTERPRISE.REGISTRY)))
 					.where(ENTERPRISE.DOMAIN.eq(domainId)).fetchOne();
 			Integer registry = reg.value1();
-			
-			
-		return ctx.getDslContext().insertInto(RATTACH,RATTACH.REGISTRY,RATTACH.DOMAIN,RATTACH.CATEGORY,RATTACH.MIMETYPE,RATTACH.DESCRIPTION,RATTACH.TYPE,RATTACH.SCOPE,RATTACH.SECURITY_LEVEL,RATTACH.ATTACH_DATE,RATTACH.DATA,RATTACH.DRIVE_ID,RATTACH.DPARENT_ID)
-					.values(registry,domainId,null,(byte) com.esferalia.aon.occam.api.model.type.MimeType.XML.ordinal(), name ,(byte) 17,null,(byte)0,null,b,null,null).returning(RATTACH.ID).fetchOne().getId();
-		
-		}finally {
-			if (ctx != null) ctx.close();
-		}
-		
-	}
-	
-	
 
-	
+			return ctx
+					.getDslContext()
+					.insertInto(RATTACH, RATTACH.REGISTRY, RATTACH.DOMAIN,
+							RATTACH.CATEGORY, RATTACH.MIMETYPE,
+							RATTACH.DESCRIPTION, RATTACH.TYPE, RATTACH.SCOPE,
+							RATTACH.SECURITY_LEVEL, RATTACH.ATTACH_DATE,
+							RATTACH.DATA, RATTACH.DRIVE_ID, RATTACH.DPARENT_ID)
+					.values(registry,
+							domainId,
+							null,
+							(byte) com.esferalia.aon.occam.api.model.type.MimeType.XML
+									.ordinal(), name, (byte) 17, null,
+							(byte) 0, null, b, null, null)
+					.returning(RATTACH.ID).fetchOne().getId();
+
+		} finally {
+			if (ctx != null)
+				ctx.close();
+		}
+
+	}
+
 }
