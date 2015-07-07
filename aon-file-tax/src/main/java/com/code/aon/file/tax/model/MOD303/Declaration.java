f745f090ad7f98211be12e4d24f5848c00c3034e
EukeCastellano <ecastellano@esferalia.com>
2015-07-07 16:35:43 +0200

    Fiscal. Mod 303. (Fixes #421) Reg. General o Reg. Simplificado en fichero.

--- aon-file-tax/src/main/java/com/code/aon/file/tax/model/MOD303/Declaration.java
+++ aon-file-tax/src/main/java/com/code/aon/file/tax/model/MOD303/Declaration.java
@@ -202,7 +202,7 @@ public class Declaration {
 		this.simplRegimeOnly = simplRegimeOnly;
 	}
 	public int getSimplRegimeOnlyNumber() {
-		return isSimplRegimeOnly()?1:2;
+		return isSimplRegimeOnly()?1:3;
 	}
 	public boolean isMergedDeclaration() {
 		return mergedDeclaration;
