f068916f69a305ac2e19824f7033c554abe69cd4
Alvaro Martinez de Lagos <amtzdelagos@aonsolutions.es>
2015-07-22 17:30:27 +0200

    P10 Subvenciones, Donaciones y legados P-A

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/normalizedMemory/PageM10.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/normalizedMemory/PageM10.java
@@ -2,22 +2,32 @@ package com.esferalia.aon.gwt.fiscal.client.normalizedMemory;
 
 import com.esferalia.aon.gwt.common.client.AON;
 import com.esferalia.aon.occam.api.model.Enterprise;
+import com.esferalia.aon.occam.api.model.fiscal.d2_deposit.D2DepositConstants;
 import com.esferalia.aon.occam.api.model.fiscal.d2_deposit.D2DepositKey;
 import com.esferalia.aon.occam.api.model.fiscal.d2_deposit.D2PDepositConstants;
+import com.esferalia.aon.watson.util.AonStringUtils;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class PageM10 extends PageAbs {
 
 	interface PageBinder extends UiBinder<Widget, PageM10> {
 	}
+	
+	private static final String[] PERIODS = new String[] {
+		"Ejercicio 2014" , "Ejercicio 2013"
+	};
 
 	private static final PageBinder pageBinder = GWT.create(PageBinder.class);
 
 
+	@UiField
+	TabPanel tabPanel;
 	
 
 
@@ -39,54 +49,52 @@ public class PageM10 extends PageAbs {
 
 	@Override
 	protected void initializeTable() {
-		table.setWidth("100%");
-		table.setCellSpacing(0);
-		table.getColumnFormatter().setWidth(1, "200px");
-		table.getColumnFormatter().setWidth(2, "200px");
-
-		int row = 0;
-		table.getFlexCellFormatter().addStyleName(row, 0, AON.AON_CSS.aonBold());
-		table.getFlexCellFormatter().addStyleName(row, 0, AON.AON_CSS.aonBorderBottom());
-		table.getFlexCellFormatter().addStyleName(row, 0, AON.AON_CSS.aonTextCenter());
-		table.setWidget(row, 1, new Label("Ejercicio 2014"));
-		table.getFlexCellFormatter().addStyleName(row, 1, AON.AON_CSS.aonBold());
-		table.getFlexCellFormatter().addStyleName(row, 1, AON.AON_CSS.aonBorderBottom());
-		table.getFlexCellFormatter().addStyleName(row, 1, AON.AON_CSS.aonTextCenter());
-		table.setWidget(row, 2, new Label("Ejercicio 2013"));
-		table.getFlexCellFormatter().addStyleName(row, 2, AON.AON_CSS.aonBold());
-		table.getFlexCellFormatter().addStyleName(row, 2, AON.AON_CSS.aonBorderBottom());
-		table.getFlexCellFormatter().addStyleName(row, 2, AON.AON_CSS.aonTextCenter());
 		
-		++row;
+		tabPanel.selectTab(0);
 		
-		//************************ PYMES ***************************		
-		for(Integer i = 0; i< D2PDepositConstants.MP10_ABREVIATE_KEYS.length; i+=2){
-			D2DepositKey[] d2 = new D2DepositKey[]{
-					D2PDepositConstants.MP10_ABREVIATE_KEYS[i],
-					D2PDepositConstants.MP10_ABREVIATE_KEYS[i+1],
-			};
-			row = paintKey(table, d2 , row);
+		if (isPymes()) {
+			defineMRNTable(table, PERIODS, D2PDepositConstants.MP10_ABREVIATE_KEYS);
 		}
-
-		//************************ ABREVIADO ***************************
-//		for(Integer i = 0; i< D2DepositConstants.MA10_ABREVIATE_KEYS.length; i+=2){
-//			D2DepositKey[] d2 = new D2DepositKey[]{
-//					D2DepositConstants.MA10_ABREVIATE_KEYS[i],
-//					D2DepositConstants.MA10_ABREVIATE_KEYS[i+1],
-//			};
-//			row = paintKey(table, d2 , row);
-//		}
-
-		
+		else {
+			defineMRNTable(table, PERIODS, D2DepositConstants.MRN10_ABREVIATE_KEYS);
+		}
 	}
 	
-
-	protected int paintKey(FlexTable tab, D2DepositKey[] keys,  int row) {
-		paintKeyDescription(tab, keys[0], row, 0);
-		for (Integer i = 0; i < keys.length ; i++) {
-			paintKeyField(tab,keys[i],row,i+1);
+	protected void defineMRNTable( FlexTable tab, String[] headers, D2DepositKey[][] keys){
+		
+		tab.setWidth("100%");
+		tab.setCellSpacing(0);
+		int row = 0;
+		int col = 0;
+		tab.getColumnFormatter().addStyleName(col, AON.AON_CSS.aonWidthAuto());
+		tab.getColumnFormatter().addStyleName(col, AON.AON_CSS.aonNowrap());
+		col++;
+		
+		for (String primary : headers) {
+			
+			tab.getColumnFormatter().addStyleName(col, AON.AON_CSS.aonWidth140());
+			tab.setWidget(row, col, new Label(primary));	
+			tab.getFlexCellFormatter().addStyleName(row, col, AON.AON_CSS.aonBold());
+			tab.getFlexCellFormatter().addStyleName(row, col, AON.AON_CSS.aonBorderBottom());
+			tab.getFlexCellFormatter().addStyleName(row, col, AON.AON_CSS.aonTextCenter());
+			++col;
 		}
-		return  ++row;
-	}
+		
+		++row;
+		col = 0;
+		
+		for (D2DepositKey[] innerKeys : keys) {
+			col = 0;
+			paintKeyDescription(tab, innerKeys[0], row, col);
+			tab.getFlexCellFormatter().addStyleName(row, col, AON.AON_CSS.aonNowrap());
+			col++;
+			for (D2DepositKey key : innerKeys ) {
+				
+				paintKeyField(tab,key,row,col,(col==1), key.getCode()); 
+				col++;
+			}
+			++row;
+		}
 
+	}
 }
--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/d2_deposit/D2DepositConstants.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/d2_deposit/D2DepositConstants.java
@@ -403,49 +403,29 @@ public class D2DepositConstants {
 	};
 	
 	
-	public static D2DepositKey[] MA10_ABREVIATE_KEYS = new D2DepositKey[]{
-		D2DepositKey.MA1095000
-	,D2DepositKey.MA10950009
-	,D2DepositKey.MA1095001
-	,D2DepositKey.MA10950019
-	,D2DepositKey.MA1095002
-	,D2DepositKey.MA10950029
-	,D2DepositKey.MA1095003
-	,D2DepositKey.MA10950039
-	,D2DepositKey.MA1095004
-	,D2DepositKey.MA10950049
-	,D2DepositKey.MA1095005
-	,D2DepositKey.MA10950059
-	,D2DepositKey.MA1095006
-	,D2DepositKey.MA10950069
-	,D2DepositKey.MA1095007
-	,D2DepositKey.MA10950079
-	,D2DepositKey.MA1095008
-	,D2DepositKey.MA10950089
-	,D2DepositKey.MA1095009
-	,D2DepositKey.MA10950099
-	,D2DepositKey.MA1095010
-	,D2DepositKey.MA10950109
-	,D2DepositKey.MA1095011
-	,D2DepositKey.MA10950119
-	,D2DepositKey.MA1095012
-	,D2DepositKey.MA10950129
-	,D2DepositKey.MA1095013
-	,D2DepositKey.MA10950139
-	,D2DepositKey.MA1095014
-	,D2DepositKey.MA10950149
-	,D2DepositKey.MA1095015
-	,D2DepositKey.MA10950159
-	,D2DepositKey.MA1095016
-	,D2DepositKey.MA10950169
-	,D2DepositKey.MA1095017
-	,D2DepositKey.MA10950179
-	,D2DepositKey.MA1095018
-	,D2DepositKey.MA10950189
-	,D2DepositKey.MA1095019
-	,D2DepositKey.MA10950199
-	,D2DepositKey.MA1095020
-	,D2DepositKey.MA10950209
+	public static D2DepositKey[][] MRN10_ABREVIATE_KEYS = new D2DepositKey[][]{
+		
+		new D2DepositKey[] {D2DepositKey.MA1095000, D2DepositKey.MA10950009}
+		,new D2DepositKey[] {D2DepositKey.MA1095001, D2DepositKey.MA10950019}
+		,new D2DepositKey[] {D2DepositKey.MA1095002, D2DepositKey.MA10950029}	
+		,new D2DepositKey[] {D2DepositKey.MA1095003, D2DepositKey.MA10950039}
+		,new D2DepositKey[] {D2DepositKey.MA1095004, D2DepositKey.MA10950049}	
+		,new D2DepositKey[] {D2DepositKey.MA1095005, D2DepositKey.MA10950059}
+		,new D2DepositKey[] {D2DepositKey.MA1095006, D2DepositKey.MA10950069}
+		,new D2DepositKey[] {D2DepositKey.MA1095007, D2DepositKey.MA10950079}
+		,new D2DepositKey[] {D2DepositKey.MA1095008, D2DepositKey.MA10950089}
+		,new D2DepositKey[] {D2DepositKey.MA1095009, D2DepositKey.MA10950099}
+		,new D2DepositKey[] {D2DepositKey.MA1095010, D2DepositKey.MA10950109}
+		,new D2DepositKey[] {D2DepositKey.MA1095011, D2DepositKey.MA10950119}
+		,new D2DepositKey[] {D2DepositKey.MA1095012, D2DepositKey.MA10950129}
+		,new D2DepositKey[] {D2DepositKey.MA1095013, D2DepositKey.MA10950139}
+		,new D2DepositKey[] {D2DepositKey.MA1095014, D2DepositKey.MA10950149}
+		,new D2DepositKey[] {D2DepositKey.MA1095015, D2DepositKey.MA10950159}
+		,new D2DepositKey[] {D2DepositKey.MA1095016, D2DepositKey.MA10950169}
+		,new D2DepositKey[] {D2DepositKey.MA1095017, D2DepositKey.MA10950179}
+		,new D2DepositKey[] {D2DepositKey.MA1095018, D2DepositKey.MA10950189}
+		,new D2DepositKey[] {D2DepositKey.MA1095019,D2DepositKey.MA10950199}
+		,new D2DepositKey[] {D2DepositKey.MA1095020, D2DepositKey.MA10950209}
 	};
 	
 	public static D2DepositKey[][] MRN7_ABREVIATE_KEYS_1 = new D2DepositKey[][]{
