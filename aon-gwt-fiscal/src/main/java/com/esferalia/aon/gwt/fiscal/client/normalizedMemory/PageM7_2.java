5ce5b25c7f2c85219dd4b6d8f8c9376f3d635df9
Alvaro Martinez de Lagos <amtzdelagos@aonsolutions.es>
2015-07-23 06:52:38 +0200

    Modelo 200: P7 Refactorizacion Array

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/normalizedMemory/PageM7_2.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/normalizedMemory/PageM7_2.java
@@ -35,7 +35,7 @@ public class PageM7_2 extends PageAbs {
 	};
 	
 	private static final String[] AUXILIARES = new String[] {
-		"Ejercicio 2014" , "Ejercicio 2013"
+		AON.MSG.year2014() , AON.MSG.year2013()
 	};
  
 
