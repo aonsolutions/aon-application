0fa252f3dd2486742443cdc5b010c52c4e81bd90
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2015-08-26 12:36:20 +0200

    Nuevo enumerado para transferencias entre almacenes

---
+++ aon.entity.common/src/main/java/com/code/aon/warehouse/enumeration/WarehouseTransferSource.java
@@ -0,0 +1,21 @@
+package com.code.aon.warehouse.enumeration;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import com.code.aon.common.enumeration.IResourceable;
+
+public enum WarehouseTransferSource implements IResourceable {
+
+	INVENTORY,
+	
+	MANUFACTURING_ORDER;
+	
+    private static final String MSG_KEY_PREFIX = "aon_enum_warehouseTransfer_source_";
+
+    public String getName(Locale locale) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+		return bundle.getString(MSG_KEY_PREFIX + toString());
+    }
+    
+}
