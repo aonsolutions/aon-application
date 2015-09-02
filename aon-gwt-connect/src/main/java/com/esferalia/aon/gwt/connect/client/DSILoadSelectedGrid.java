1696965a2207d5e4738fc98778a7c15bd69ab019
Alvaro Martinez de Lagos <amtzdelagos@aonsolutions.es>
2015-09-02 14:20:25 +0200

    Fix #2395

--- aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/client/DSILoadSelectedGrid.java
+++ aon-gwt-connect/src/main/java/com/esferalia/aon/gwt/connect/client/DSILoadSelectedGrid.java
@@ -64,7 +64,7 @@ public class DSILoadSelectedGrid extends CustomDataGrid<DSILoadSelected>
 		}
 	}
 
-	private static class DefaultDSILoadSelected implements DSILoadSelected {
+	public static class DefaultDSILoadSelected implements DSILoadSelected {
 
 		private JsEmpres empres;
 
