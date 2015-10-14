3d4a40e287a28e05e0438cbec3e6c2db5350b06d
girazu <girazu@esferalia.com>
2015-10-14 12:12:07 +0200

    MASTER 8.31.0. Crear indice y constraint de product despues de hacerlo no nullable

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.29.0.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.29.0.sql
@@ -7,10 +7,10 @@ BEGIN;
 
 ALTER TABLE `catalogue_item` MODIFY `item` int(4) default NULL COMMENT 'Identificador del Articulo';
 ALTER TABLE `catalogue_item` ADD `product` int(4) default NULL COMMENT 'Identificador del Producto' AFTER `catalogue`;
-ALTER TABLE `catalogue_item` ADD KEY `IDX_CATALOGUE_ITEM_PRODUCT` (`product`);
-ALTER TABLE `catalogue_item` ADD CONSTRAINT `FK_CATALOGUE_ITEM_PRODUCT` FOREIGN KEY (`product`) REFERENCES `product` (`id`);
 UPDATE `catalogue_item` SET `product` = (SELECT `product` FROM `item` WHERE `id` = `catalogue_item`.`item`);
 ALTER TABLE `catalogue_item` MODIFY `product` int(4) NOT NULL COMMENT 'Identificador del Producto';
+ALTER TABLE `catalogue_item` ADD KEY `IDX_CATALOGUE_ITEM_PRODUCT` (`product`);
+ALTER TABLE `catalogue_item` ADD CONSTRAINT `FK_CATALOGUE_ITEM_PRODUCT` FOREIGN KEY (`product`) REFERENCES `product` (`id`);
 
 
 UPDATE `db_version` SET `version_number` = '8.31.0';
