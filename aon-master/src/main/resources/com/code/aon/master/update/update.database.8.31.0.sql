631381e9e0e68717e29dff42d1ea9fdd776b0a67
Ander Ibáñez de Gauna <aibanez@aonsolutions.es>
2015-10-19 16:40:16 +0200

    BD. app_param.value from VARCHAR(64) to VARCHAR(86).

---
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.31.0.sql
@@ -0,0 +1,12 @@
+# Database: aon_master
+# Version: Actualizacion de la version 8.31.0 a la version 8.31.1.
+# Created by: aibanez
+# Creation Date: 19/10/2015 16:40
+
+BEGIN;
+
+ALTER TABLE `app_param` CHANGE COLUMN `value` `value` VARCHAR(86) CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci' NULL DEFAULT NULL COMMENT 'Valor del Parametro';
+
+UPDATE `db_version` SET `version_number` = '8.31.1';
+
+COMMIT;
