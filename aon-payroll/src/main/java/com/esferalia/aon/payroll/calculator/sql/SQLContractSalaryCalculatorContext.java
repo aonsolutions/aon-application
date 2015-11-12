b7694c6896ef7f579ee3dc18b75e32f68ab82016
Raúl Trepiana <rtrepiana@gmail.com>
2015-11-12 17:37:48 +0100

    Fix 'HORAS_NOMINA'

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/sql/SQLContractSalaryCalculatorContext.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/sql/SQLContractSalaryCalculatorContext.java
@@ -2968,7 +2968,7 @@ public class SQLContractSalaryCalculatorContext extends
 
 
 	private double getSalaryHours(Period p) {
-		return getWorkedHours(new Period(contractStartDate, contractEndDate));
+		return getWorkedHours(p);
 	}
 
 	private boolean isIndefinite() {
