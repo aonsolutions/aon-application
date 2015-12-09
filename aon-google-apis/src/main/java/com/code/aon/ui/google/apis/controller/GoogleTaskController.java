e0c02ed8edf455b9b35bce7a1261ce62fb804ba8
Ander Ibáñez de Gauna <aibanez@aonsolutions.es>
2015-12-09 10:10:11 +0100

    Inicio de sesión con Google & Amazon en proyecto aon.oauth2 & borrar proyecto aon.amazon.apis

--- aon-google-apis/src/main/java/com/code/aon/ui/google/apis/controller/GoogleTaskController.java
+++ aon-google-apis/src/main/java/com/code/aon/ui/google/apis/controller/GoogleTaskController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 
 import com.code.aon.AonVersion;
 import com.code.aon.google.apis.TaskUtils;
-import com.code.aon.google.apis.sessionInfo.SessionInfo;
+import com.code.aon.oauth2.sessionInfo.SessionInfo;
 import com.code.aon.pool.AonConnectionException;
 import com.code.aon.ui.util.AonUtil;
 import com.google.api.services.tasks.Tasks;
