c461fdad176340d790b33408858228e9049d03cb
Raúl Trepiana <rtrepiana@gmail.com>
2016-01-13 11:47:32 +0100

    Retenciones Ejercicio 2016

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/irpf/IrpfCalculateException.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/irpf/IrpfCalculateException.java
@@ -1,7 +1,7 @@
 package com.esferalia.aon.payroll.irpf;
 
 import com.code.aon.AonVersion;
-import com.esferalia.aon.aeat.jaxb.AEATRetencionesError2015;
+import com.esferalia.aon.aeat.jaxb.AEATRetencionesError2016;
 
 import es.aeat.pret.rw13.jaxb.AEATRetencionesError2013;
 
@@ -10,10 +10,10 @@ public class IrpfCalculateException extends Exception {
 	private static final long serialVersionUID = AonVersion.SERIAL_VERSION_UID;
 
 	private AEATRetencionesError2013 error2013;
-	private AEATRetencionesError2015 error2015;
+	private AEATRetencionesError2016 error2016;
 
-	public IrpfCalculateException(AEATRetencionesError2015 error2015) {
-		this.error2015 = error2015;
+	public IrpfCalculateException(AEATRetencionesError2016 error2016) {
+		this.error2016 = error2016;
 	}
 
 	public IrpfCalculateException(AEATRetencionesError2013 error2013) {
@@ -24,8 +24,8 @@ public class IrpfCalculateException extends Exception {
 		return error2013;
 	}
 	
-	public AEATRetencionesError2015 getAEATRetencionesError2015() {
-		return error2015;
+	public AEATRetencionesError2016 getAEATRetencionesError2016() {
+		return error2016;
 	}
 	
 
