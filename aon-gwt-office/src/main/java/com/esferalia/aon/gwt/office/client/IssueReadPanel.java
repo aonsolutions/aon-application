abdae1677038568c50c14217f81b3d571e9238cd
Alvaro Martinez de Lagos <amtzdelagos@aonsolutions.es>
2016-01-22 12:04:05 +0100

    Visualizacion de issues (en desarrollo)

--- aon-gwt-office/src/main/java/com/esferalia/aon/gwt/office/client/IssueReadPanel.java
+++ aon-gwt-office/src/main/java/com/esferalia/aon/gwt/office/client/IssueReadPanel.java
@@ -2,13 +2,18 @@ package com.esferalia.aon.gwt.office.client;
 
 import java.util.Date;
 
+import com.esferalia.aon.gwt.office.client.models.issues.JsLabel;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class IssueReadPanel extends Composite {
@@ -24,17 +29,40 @@ public class IssueReadPanel extends Composite {
 	@UiField
 	Label dateLabel;
 	@UiField
-	Label titleLabel;	
+	Label statusLabel;
+	@UiField
+	Label titleLabel;
+	@UiField
+	Label typeLabel;
+	@UiField
+	Label userLabel;
+	@UiField
+	HorizontalPanel labelsHPanel;
+	@UiField
+	Label priorityLabel;
 	@UiField
 	TextArea commentTextArea;
+	
+	@UiField
+	Button closeButton;
 
 	public IssueReadPanel(IssueSelected issue) {
 		initWidget(uiBinder.createAndBindUi(this));
 		
 		setNumber(issue.getId());
 		setDate(issue.getCreateAt());
+		setStatusLabel(issue.getState());
 		setAsunto(issue.getTitle());
+		setType(issue.getType());
+		setPriority(issue.getPriority());
+		setUser(issue.getUser().getLogin());
+		setLabel(issue.getLabels());
 		setBody(issue.getBody());
+	}
+	
+	@UiHandler("closeButton")
+	void onCloseButtonClick(ClickEvent event) {
+		
 	}
 	
 	private void setNumber(Integer id) {
@@ -45,8 +73,33 @@ public class IssueReadPanel extends Composite {
 		this.dateLabel.setText(String.valueOf(date));
 	}
 	
+	private void setStatusLabel(String status) {
+		this.statusLabel.setText(status);
+	}
+	
 	private void setAsunto(String asunto) {
 		this.titleLabel.setText(asunto);
+	}
+	
+	private void setType(String type) {
+		this.typeLabel.setText(type);
+	}
+	
+	private void setPriority(String priority) {
+		this.priorityLabel.setText(priority);
+	}
+	
+	private void setUser(String login) {
+		this.userLabel.setText(login);
+	}
+	
+	private void setLabel(JsArray<JsLabel> labels) {
+	
+		if ( labels.length() == 0 )
+			labelsHPanel.add(new Label("Etiquetas no asignadas"));
+		
+		for ( int x = 0 ; x < labels.length(); x++)
+			labelsHPanel.add(new Label(labels.get(x).getName()));
 	}
 	
 	private void setBody(String body) {
