679fa5d35af256baa828d2798ffee879654739be
Alvaro Martinez de Lagos <amtzdelagos@aonsolutions.es>
2016-01-22 14:13:56 +0100

    Enumerado para BD

--- aon.entity.common/src/main/java/com/code/aon/config/enumeration/TagType.java
+++ aon.entity.common/src/main/java/com/code/aon/config/enumeration/TagType.java
@@ -11,50 +11,56 @@ import com.code.aon.common.enumeration.IResourceable;
  * @author Consulting & Development. Eugenio Castellano - 31-ene-2005
  * @since 1.0
  * @version 1.0
- *  
+ * 
  */
 public enum TagType implements IResourceable {
 
-    /**
-     * RATTACH.
-     */
+	/**
+	 * RATTACH.
+	 */
 	RATTACH,
 
-    /**
-     * PRODUCT
-     */
-    PRODUCT,
-	
-    /**
-     * NOTICE
-     */
-    NOTICE,
-    
-    /**
-     * PRIORITY
-     */
-    PRIORITY,
+	/**
+	 * PRODUCT
+	 */
+	PRODUCT,
+
+	/**
+	 * PRIORITY
+	 */
+	PRIORITY,
+
+	/**
+	 * MARKETPLACE
+	 */
+	MARKETPLACE,
 	
 	/**
-	 * MARKETPLACE 
+	 * NOTICE
 	 */
-	MARKETPLACE;
+	OFFICE_NOTICE,
+	
+	OFFICE_PRIORITY,
+	
+	OFFICE_STATUS,
+	
+	OFFICE_TYPE;
 
-    /**
-     * Message key prefix. 
-     */
-    private static final String MSG_KEY_PREFIX = "aon_enum_tagtype_";
+	/**
+	 * Message key prefix.
+	 */
+	private static final String MSG_KEY_PREFIX = "aon_enum_tagtype_";
 
-    /**
-     * Returns a <code>String</code> with the transalation <code>Locale</code>
-     * for the locale.
-     * 
-     * @param locale
-     *            Required Locale.
-     * @return String a <code>String</code>.
-     */
-    public String getName(Locale locale) {
-        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
+	/**
+	 * Returns a <code>String</code> with the transalation <code>Locale</code>
+	 * for the locale.
+	 * 
+	 * @param locale
+	 *            Required Locale.
+	 * @return String a <code>String</code>.
+	 */
+	public String getName(Locale locale) {
+		ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale);
 		return bundle.getString(MSG_KEY_PREFIX + toString());
-    }
+	}
 }
