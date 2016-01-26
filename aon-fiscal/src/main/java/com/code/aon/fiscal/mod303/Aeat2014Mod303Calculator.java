10045f37e25cc336e8fd5a8fcfb8aa8013e779a5
EukeCastellano <ecastellano@aonsolutions.es>
2016-01-26 13:04:15 +0100

    [HFX] Mod 303 R.S. Redondeo de índice de cuota de actividades agrícolas.

--- aon-fiscal/src/main/java/com/code/aon/fiscal/mod303/Aeat2014Mod303Calculator.java
+++ aon-fiscal/src/main/java/com/code/aon/fiscal/mod303/Aeat2014Mod303Calculator.java
@@ -25,7 +25,7 @@ public class Aeat2014Mod303Calculator extends FiscalModelDetailCalculator implem
 			double g1 = mod303.ensureAmount( Mod303Key.CAG1_V1);
 			double g2 = mod303.ensureAmount( Mod303Key.CAG1_V2);
 			if (g2 > 100) {
-				g2 = CommonUtil.round(g2 / 10000);
+				g2 = CommonUtil.round((g2 / 10000),5);
 			}
 			double g3 = CommonUtil.round(g1 * g2);
 			mod303.getDetail( Mod303Key.CAG1_V3).setAmount( g3 );
@@ -44,7 +44,7 @@ public class Aeat2014Mod303Calculator extends FiscalModelDetailCalculator implem
 			double g1 = mod303.ensureAmount( Mod303Key.CAG2_V1);
 			double g2 = mod303.ensureAmount( Mod303Key.CAG2_V2);
 			if (g2 > 100) {
-				g2 = CommonUtil.round(g2 / 10000);
+				g2 = CommonUtil.round((g2 / 10000),4);
 			}
 			double g3 = CommonUtil.round(g1 * g2);
 			mod303.getDetail( Mod303Key.CAG2_V3).setAmount( g3 );
@@ -58,7 +58,7 @@ public class Aeat2014Mod303Calculator extends FiscalModelDetailCalculator implem
 			double g1 = mod303.ensureAmount( Mod303Key.CAG3_V1);
 			double g2 = mod303.ensureAmount( Mod303Key.CAG3_V2);
 			if (g2 > 100) {
-				g2 = CommonUtil.round(g2 / 10000);
+				g2 = CommonUtil.round((g2 / 10000),4);
 			}
 			double g3 = CommonUtil.round(g1 * g2);
 			mod303.getDetail( Mod303Key.CAG3_V3).setAmount( g3 );
@@ -72,7 +72,7 @@ public class Aeat2014Mod303Calculator extends FiscalModelDetailCalculator implem
 			double g1 = mod303.ensureAmount( Mod303Key.CAG4_V1);
 			double g2 = mod303.ensureAmount( Mod303Key.CAG4_V2);
 			if (g2 > 100) {
-				g2 = CommonUtil.round(g2 / 10000);
+				g2 = CommonUtil.round((g2 / 10000),4);
 			}
 			double g3 = CommonUtil.round(g1 * g2);
 			mod303.getDetail( Mod303Key.CAG4_V3).setAmount( g3 );
