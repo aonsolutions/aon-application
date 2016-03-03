d125ce945c8c393b5e402bba189d26ea03e1dce9
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2016-03-03 13:37:33 +0100

    los impuestos no son obligatorios (ref #696)

--- aon-accounting/src/main/java/com/code/aon/accounting/report/OperationReport.java
+++ aon-accounting/src/main/java/com/code/aon/accounting/report/OperationReport.java
@@ -95,12 +95,12 @@ public class OperationReport implements Serializable {
 		return StringUtils.abbreviate(getAccount(), 60);
 	}
 	public Double getTotalBase(){
-		return taxes.stream().filter(o -> o.getBase()!=null).mapToDouble(OperationReportTax::getBase).sum();
+		return taxes==null?0.0:taxes.stream().filter(o -> o.getBase()!=null).mapToDouble(OperationReportTax::getBase).sum();
 	}
 	public Double getTotalQuota(){
-		return taxes.stream().filter(o -> o.getQuota()!=null).mapToDouble(OperationReportTax::getQuota).sum();
+		return taxes==null?0.0:taxes.stream().filter(o -> o.getQuota()!=null).mapToDouble(OperationReportTax::getQuota).sum();
 	}
 	public Double getTotalSurchargeQuota(){
-		return taxes.stream().filter((o) -> o.getSurchargeQuota()!=null).mapToDouble(OperationReportTax::getSurchargeQuota).sum();
+		return taxes==null?0.0:taxes.stream().filter((o) -> o.getSurchargeQuota()!=null).mapToDouble(OperationReportTax::getSurchargeQuota).sum();
 	}
 }
