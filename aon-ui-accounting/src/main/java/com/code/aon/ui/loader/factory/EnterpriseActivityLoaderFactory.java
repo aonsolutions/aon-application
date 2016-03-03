69551a656f682c141b8539a74b52e8431192f007
Felix Florindo <f.florindo@analize.es>
2016-03-03 11:54:00 +0100

    Carga de Datos: Entidad ACT añadir nuevas columnas.

--- aon-ui-accounting/src/main/java/com/code/aon/ui/loader/factory/EnterpriseActivityLoaderFactory.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/loader/factory/EnterpriseActivityLoaderFactory.java
@@ -12,6 +12,9 @@ import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.domain.DomainManager;
 import com.code.aon.company.Enterprise;
+import com.code.aon.config.IAE;
+import com.code.aon.fiscal.enumeration.RetentionRegime;
+import com.code.aon.fiscal.enumeration.VatRegime;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ui.loader.Column;
 import com.code.aon.ui.loader.ILoaderEngine;
@@ -26,7 +29,13 @@ public class EnterpriseActivityLoaderFactory implements ILoaderFactory<ILoadedPo
 	
 	private static final Column[] SUPPORTED_COLUMNS = {
 		 new Column(ACT,"descripcion"	,2,64	,true	,null)             // Descripción
-		,new Column(ACT,"principal"		,0,1	,true	,new int[] {0,1})  // Principal		
+		,new Column(ACT,"principal"		,0,1	,true	,new int[] {0,1})  // Principal
+		,new Column(ACT,"iaeSeccion"	,2,1	,false	,null)             // IAE - Sección
+		,new Column(ACT,"iaeEpigrafe"	,2,8	,false	,null)             // IAE - Epígrafe
+		,new Column(ACT,"recargo"		,0,1	,false	,new int[] {0,1})  // Aplica Recargo de Equivalencia: 0-No, 1-Si
+		,new Column(ACT,"regimenIVA"	,0,1	,false  ,new int[] {0,1})  // Regimen de IVA: 0-General, 1-Simplificado
+		,new Column(ACT,"regimenIRPF"	,0,1	,false	,new int[] {0,1,2})  // Regimen de IRPF: 0-Directa Normal, 1-Directa Simplificada, 2-Objetiva
+		,new Column(ACT,"fechaAlta"	 	,3,10	,false	,null)             // Fecha de Alta
 	};
 	
 	//private ILoaderEngine engine;
@@ -106,6 +115,30 @@ public class EnterpriseActivityLoaderFactory implements ILoaderFactory<ILoadedPo
 		ea.setEnterprise(ep);
 		ea.setDescription(loaded.getDescripcion());
 		ea.setPrincipal(loaded.esPrincipal());
+		
+		// Obtener el IAE		
+		if (StringUtils.isNotBlank(loaded.getIaeSeccion()) &&
+			StringUtils.isNotBlank(loaded.getIaeEpigrafe())) {
+			
+			IManagerBean beanIAE = BeanManager.getManagerBean(IAE.class);
+			
+			Criteria criteriaIAE = new Criteria();            
+	        criteriaIAE.addEqualExpression(beanIAE.getFieldName(IEntityAlias.IAE_SECTION), loaded.getIaeSeccion());
+	        criteriaIAE.addEqualExpression(beanIAE.getFieldName(IEntityAlias.IAE_EPIGRAPH), loaded.getIaeEpigrafe());
+	        List<ITransferObject> listIAE = beanIAE.getList(criteriaIAE);
+	        
+	        if (listIAE!=null && listIAE.size()>0) {
+	        	IAE iae = (IAE)listIAE.get(0);
+	        	ea.setIae(iae);        	
+	        }
+		}
+		
+		ea.setSurcharge(loaded.esRecargo());
+		ea.setVatRegime(VatRegime.values()[loaded.getRegimenIVA()]);
+		ea.setRetentionRegime(RetentionRegime.values()[loaded.getRegimenIRPF()]);	
+		
+		if (loaded.getFechaAlta()!=null)
+			ea.setStartDate(loaded.getFechaAlta());
 		
 		ea = (EnterpriseActivity) bean.insertOrUpdate(ea);
 		return ea.getId();	
--- aon-ui-accounting/src/main/java/com/code/aon/ui/loader/pojo/LoadedEnterpriseActivity.java
+++ aon-ui-accounting/src/main/java/com/code/aon/ui/loader/pojo/LoadedEnterpriseActivity.java
@@ -1,10 +1,19 @@
 package com.code.aon.ui.loader.pojo;
 
+import java.util.Date;
+
 public class LoadedEnterpriseActivity implements ILoadedPojo{
 
 	private String descripcion;	
 	private Integer principal;
-		
+	
+	private String iaeSeccion;
+	private String iaeEpigrafe;
+	private Integer recargo;
+	private Integer regimenIVA;
+	private Integer regimenIRPF;
+	private Date fechaAlta;
+	
 	@Override
 	public String getIdentifier() {
 		return null; 
@@ -28,6 +37,58 @@ public class LoadedEnterpriseActivity implements ILoadedPojo{
 	
 	public boolean esPrincipal() {
 		return (getPrincipal()==1);
+	}
+
+	public String getIaeSeccion() {
+		return iaeSeccion;
+	}
+
+	public void setIaeSeccion(String iaeSeccion) {
+		this.iaeSeccion = iaeSeccion;
+	}
+
+	public String getIaeEpigrafe() {
+		return iaeEpigrafe;
+	}
+
+	public void setIaeEpigrafe(String iaeEpigrafe) {
+		this.iaeEpigrafe = iaeEpigrafe;
+	}
+
+	public Integer getRecargo() {
+		return recargo;
+	}
+
+	public void setRecargo(Integer recargo) {
+		this.recargo = recargo;
+	}
+	
+	public boolean esRecargo() {
+		return (getRecargo()==1);
+	}
+
+	public Integer getRegimenIVA() {
+		return regimenIVA;
+	}
+
+	public void setRegimenIVA(Integer regimenIVA) {
+		this.regimenIVA = regimenIVA;
+	}
+
+	public Integer getRegimenIRPF() {
+		return regimenIRPF;
+	}
+
+	public void setRegimenIRPF(Integer regimenIRPF) {
+		this.regimenIRPF = regimenIRPF;
+	}
+
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
 	}
 		
 }
