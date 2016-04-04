996b0eaade6fa355798f7b32d145460e491c6fc6
EukeCastellano <ecastellano@aonsolutions.es>
2016-04-04 13:14:24 +0200

    Fiscal. Impresión Modelo 131.

---
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/Mod131Print.java
@@ -0,0 +1,60 @@
+package com.esferalia.aon.gwt.fiscal.server;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.esferalia.aon.gwt.common.server.AonServletUtils;
+import com.esferalia.aon.occam.api.FISCAL;
+import com.esferalia.aon.occam.api.model.fiscal.IModelScript;
+import com.esferalia.aon.occam.api.model.fiscal.Mod131;
+import com.esferalia.aon.occam.api.model.fiscal.mod131.Model131ScriptProvider;
+import com.esferalia.aon.occam.api.model.type.MimeType;
+import com.esferalia.aon.occam.api.model.type.Mod131Key;
+import com.esferalia.aon.occam.server.fiscal.format.AonFiscalFileUtils;
+import com.esferalia.aon.watson.server.io.AonIOUtils;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "Mod131 Print", urlPatterns = { "/aon_gwt_fiscal/Model131Print" })
+public class Mod131Print extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		try {
+			int id = Integer.parseInt(req.getParameter("mod131"));
+			String domainName = req.getParameter("domainName");
+			int domainId = Integer.parseInt(req.getParameter("domainId"));
+			String user = AonServletUtils.getLoggedUser();
+			Mod131 mod131 = FISCAL.getMod131(domainName, domainId, user,id);
+
+			Mod131ExcelAction action = new Mod131ExcelAction(mod131);
+			action.initialize(mod131.getModel().getName(mod131.getAdministration(), mod131.getPeriod()));
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			
+			for (IModelScript<Mod131Key> ms : Model131ScriptProvider.obtainScript(mod131)) {
+				action.accept(ms);
+			}
+			action.beforeFinalize();
+			action.finalize(output);
+			ByteArrayInputStream in = new ByteArrayInputStream(output.toByteArray());
+			
+			String fileName = AonFiscalFileUtils.getFileName(mod131);
+			resp.setContentType(MimeType.MS_EXCEL.getName());
+			resp.setHeader("Content-disposition", "attachment; filename=\"" + fileName + "."+ MimeType.MS_EXCEL.getExtension()+ "\";");
+			AonIOUtils.copy(in, resp.getOutputStream());
+			resp.flushBuffer();
+
+		} catch (Throwable e) {
+			throw new ServletException(e);
+		}
+
+	}
+}
