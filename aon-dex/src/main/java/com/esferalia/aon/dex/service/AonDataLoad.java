fc14464208e310425f85e3ea6028a864c9167a1c
girazu <girazu@aonsolutions.es>
2016-04-19 19:12:33 +0200

    NEW PROJECT AON-DEX

---
+++ aon-dex/src/main/java/com/esferalia/aon/dex/service/AonDataLoad.java
@@ -0,0 +1,13 @@
+package com.esferalia.aon.dex.service;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface="com.esferalia.aon.ws.dex.service.IDataLoad", wsdlLocation="/WEB-INF/wsdl/DataLoadService.wsdl", targetNamespace = "http://ws.apache.org/axis2")
+public class AonDataLoad implements IDataLoad {
+
+	@Override
+	public String getDocument(String document) {
+		return "RECIBIDO: " + document;
+	}
+
+}
