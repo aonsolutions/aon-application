55839dc123d0e61d5bf45392315f25ab67674663
girazu <girazu@aonsolutions.es>
2016-04-20 10:59:49 +0200

    NUEVO WEB SERVICE AON-WS_DEX

--- aon-dex/src/main/java/com/esferalia/aon/dex/service/AonDataLoad.java
+++ aon-dex/src/main/java/com/esferalia/aon/dex/service/AonDataLoad.java
@@ -2,7 +2,7 @@ package com.esferalia.aon.dex.service;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface="com.esferalia.aon.ws.dex.service.IDataLoad", wsdlLocation="/WEB-INF/wsdl/DataLoadService.wsdl", targetNamespace = "http://ws.apache.org/axis2")
+@WebService(endpointInterface="com.esferalia.aon.dex.service.IDataLoad", wsdlLocation="/WEB-INF/wsdl/DataLoadService.wsdl", targetNamespace = "http://ws.apache.org/axis2")
 public class AonDataLoad implements IDataLoad {
 
 	@Override
