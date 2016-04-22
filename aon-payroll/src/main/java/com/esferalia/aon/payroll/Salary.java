00373ef327ef120fc48851a42309a93da9ae0fe8
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2016-04-22 15:56:30 +0200

    LABORAL. Impresion de nominas: bases y porcentajes de los costos de empresa

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/Salary.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/Salary.java
@@ -152,6 +152,36 @@ public class Salary extends SalaryDB implements ISalary, ISalaryProxy {
 		return super.getInkindIrpfBase();
 	}
 	
+	@Transient
+	public Double getTotalCommonBase(){
+		Double base = 0.0;
+		try {
+			base = salaryDatas
+					.stream()
+					.filter(o -> (o.getName().equals(ContextVariable.CGC_BASE.getName())))
+					.map(SalaryData::getExpression)
+					.mapToDouble(o -> (NumberUtils.isNumber(o)?Double.valueOf(o):0.0)).sum();
+		} catch (Exception e) {
+			// nada
+		}
+		return base;
+	}
+	
+	@Transient
+	public Double getTotalProfessionalBase(){
+		Double base = 0.0;
+		try {
+			base = salaryDatas
+					.stream()
+					.filter(o -> (o.getName().equals(ContextVariable.CGP_BASE.getName())))
+					.map(SalaryData::getExpression)
+					.mapToDouble(o -> (NumberUtils.isNumber(o)?Double.valueOf(o):0.0)).sum();
+		} catch (Exception e) {
+			// nada
+		}
+		return base;
+	}
+	
 	// *******************************************************
 	// ********************** DATA ***************************
 	// *******************************************************
