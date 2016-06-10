92cbfe33e2fc01a55382a0afa1cced9bb8b03ac0
Raúl Trepiana <rtrepiana@gmail.com>
2016-06-10 10:21:01 +0200

    Database 8.57.0. Fix settles

---
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.56.0.sql
@@ -0,0 +1,29 @@
+# Database: aon_master
+# Version: Actualizacion de la version 8.56.0 a la version 8.57.0.
+# Created by: rtrepiana
+
+BEGIN;
+
+INSERT INTO `payment_concept` 
+(`code`, `domain`, `type`, `description`, `irpf_expression`, `quote_expression` ) 
+(SELECT 'PREAVISO', `domain`, `type`, `description`, `irpf_expression`, `quote_expression` 
+FROM `system_payment` 
+WHERE `salary_type` = 2 
+AND `description` = 'DIAS PREAVISO');
+
+SET @PREAVISO=(SELECT `id` FROM `payment_concept` WHERE `code`='PREAVISO');
+
+
+UPDATE `system_payment` 
+SET 
+`type` = NULL,
+`description`= NULL,
+`irpf_expression` = NULL,
+`quote_expression` = NULL,
+`payment_concept` = @PREAVISO
+WHERE `salary_type` = 2 
+AND `description` = 'DIAS PREAVISO';
+
+UPDATE `db_version` SET `version_number` = '8.57.0';
+
+COMMIT;
