95457cc436d49ae8cde370fcf0c4448f66e0dba5
rtrepiana <rtrepiana@esferalia.com>
2016-07-03 20:39:24 +0200

    Fix Agrarian bases.

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.59.0.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.59.0.sql
@@ -10,9 +10,9 @@ BEGIN;
 
 INSERT INTO system_data 
 ( domain, name				, expression	, start_date	, end_date		, read_only , comments )  VALUES 
-(-107	, 'BASE_CGP_MIN_MES'		, '764.40 * JORNADAS_REALES / 30'			
+(-107	, 'BASE_CGP_MIN_DIA'		, '764.40 * JORNADAS_REALES / 30'			
 							,'2016-01-01'	, NULL			, 1	, NULL ),
-(-107	, 'BASE_CGP_MIN_DIA'		, '( TIEMPO_COMPLETO ) ? 764.40 * ((DIAS_COTIZADOS == DIAS_MES) ? 1 : DIAS_COTIZADOS/30) : 4.60 * HORAS_NOMINA'			
+(-107	, 'BASE_CGP_MIN_MES'		, '( TIEMPO_COMPLETO ) ? 764.40 * ((DIAS_COTIZADOS == DIAS_MES) ? 1 : DIAS_COTIZADOS/30) : 4.60 * HORAS_NOMINA'			
 							,'2016-01-01'	, NULL			, 1	, NULL ),
 (-107	, 'BASE_CGP_MIN'		, '( COTIZACION_MENSUAL ) ? BASE_CGP_MIN_MES : BASE_CGP_MIN_DIA'			
 							,'2016-01-01'	, NULL			, 1	, NULL )
