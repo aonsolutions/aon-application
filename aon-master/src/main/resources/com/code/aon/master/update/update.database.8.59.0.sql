ce083dce1573b460eb8c2bc4f92224bf3935c8a5
rtrepiana <rtrepiana@esferalia.com>
2016-07-03 20:35:48 +0200

    Fix Agrarian bases.

---
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.59.0.sql
@@ -0,0 +1,28 @@
+# Database: aon_master
+# Version: Actualizacion de la version 8.59.0 a la version 8.59.1.
+# Created by: rtrepiana
+# Creation Date: 03/07/2016 
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+
+BEGIN;
+
+
+INSERT INTO system_data 
+( domain, name				, expression	, start_date	, end_date		, read_only , comments )  VALUES 
+(-107	, 'BASE_CGP_MIN_MES'		, '764.40 * JORNADAS_REALES / 30'			
+							,'2016-01-01'	, NULL			, 1	, NULL ),
+(-107	, 'BASE_CGP_MIN_DIA'		, '( TIEMPO_COMPLETO ) ? 764.40 * ((DIAS_COTIZADOS == DIAS_MES) ? 1 : DIAS_COTIZADOS/30) : 4.60 * HORAS_NOMINA'			
+							,'2016-01-01'	, NULL			, 1	, NULL ),
+(-107	, 'BASE_CGP_MIN'		, '( COTIZACION_MENSUAL ) ? BASE_CGP_MIN_MES : BASE_CGP_MIN_DIA'			
+							,'2016-01-01'	, NULL			, 1	, NULL )
+;
+
+
+
+UPDATE `db_version` SET `version_number` = '8.59.1';
+
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+
