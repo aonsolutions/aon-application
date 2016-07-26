4a7062f42bbe821116d7fad0bd7f58270990ba0f
Ander Ibáñez de Gauna <aibanez@aonsolutions.es>
2016-07-26 16:40:30 +0200

    Fiscal - Cuentas Anuales (Fixed #948)

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/deposit/server/normalizedMemory/DownloadXmlFileServlet.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/deposit/server/normalizedMemory/DownloadXmlFileServlet.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
+import java.time.Month;
+import java.util.Calendar;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.zip.ZipEntry;
@@ -19,8 +21,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.esferalia.aon.gwt.common.server.AonServletUtils;
+import com.esferalia.aon.gwt.viewer.server.DriveUtils;
+import com.esferalia.aon.occam.api.AON;
+import com.esferalia.aon.occam.api.model.Domain;
+import com.esferalia.aon.occam.api.model.attachment.Attach;
+import com.esferalia.aon.occam.api.model.attachment.AttachType;
+import com.esferalia.aon.occam.api.model.attachment.RegistryAttachmentType;
+import com.esferalia.aon.occam.api.model.security.User;
 import com.esferalia.aon.occam.api.model.type.MimeType;
-import com.esferalia.aon.occam.impl.jooq.dao.d2_deposit.DBConsults;
+import com.esferalia.aon.watson.server.AonDateUtils;
+import com.esferalia.aon.watson.server.io.AonFileUtils;
 
 @WebServlet(name = "DownloadXml", urlPatterns = { "/aon_gwt_deposit/gwt_download_deposit/*" })
 public class DownloadXmlFileServlet extends HttpServlet {
@@ -28,7 +38,19 @@ public class DownloadXmlFileServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private static final String D2_FILE_MEMORY = "Memoria";
+	private static final String D2_FILE_AUTOCARTERA_MODEL = "Modelo de Autocartera";
+	private static final String D2_FILE_GESTION = "Informe de Gestion";
+	private static final String D2_FILE_AUDIT = "Informe de Auditoria";
+	private static final String D2_FILE_CONVOC = "Anuncios de Convocatoria";
+	private static final String D2_FILE_SICAV = "Certificacion SICAV";
+	
+	private static final String D2_FILE_AUTOCARTERA_MODEL_2015 = "Acciones";
+	private static final String D2_FILE_GESTION_2015 = "Gestion";
+	private static final String D2_FILE_AUDIT_2015 = "Auditoria";
+	private static final String D2_FILE_CONVOC_2015 = "Convocatoria";
+	private static final String D2_FILE_SICAV_2015 = "SICAV";
+	
 	@Override
 	protected void doGet(HttpServletRequest p_request,
 			HttpServletResponse p_response) throws ServletException,
@@ -45,7 +67,8 @@ public class DownloadXmlFileServlet extends HttpServlet {
 		String yearStr = p_request.getParameter("year");
 		Integer year = Integer.parseInt(yearStr);
 		
-		File f = DBConsults.getXmlFile(domain, domainId, year);
+		File f = /*DBConsults.*/getXmlFile(domain, domainId, year);
+		
 		ZipOutputStream zos = null;
 		try {
 			zos = makeZip(f, p_response.getOutputStream());
@@ -183,4 +206,101 @@ public class DownloadXmlFileServlet extends HttpServlet {
 			  return new String(name.substring(0, startPosition));
 		  }
 	  }
- }
+	  
+	  
+	  public static File getXmlFile(String domain, Integer domainId, Integer year)
+				throws IOException {
+			File parent = null;
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.YEAR, year);
+			calendar.set(Calendar.MONTH, Month.DECEMBER.ordinal());
+			calendar.set(Calendar.DAY_OF_MONTH, 31);
+			
+			Attach xml = AON.getAttach(domain, domainId, "",
+					f -> f.getTypeProperty().eq(RegistryAttachmentType.D2_DEPOSIT.value())
+						.and(f.getDomainProperty().eq(domainId)
+						.and(f.getAttachDateProperty().eq(AonDateUtils.toSql(calendar.getTime())))),
+					AttachType.REGISTRY);
+			byte[] data;
+			if (xml != null && xml.getId() != null) {
+				parent = File.createTempFile(xml.getDescription()+"%", "");
+				parent.delete();
+				parent.mkdir();
+				
+				File documents = File.createTempFile("Documentos%", "", parent);
+				documents.delete();
+				documents.mkdir();
+				
+				//**************************
+				getFileDocuments2Zip(domain, domainId, D2_FILE_MEMORY, documents, year);
+				getFileDocuments2Zip(domain, domainId, D2_FILE_AUTOCARTERA_MODEL, documents, year);	
+				getFileDocuments2Zip(domain, domainId, D2_FILE_GESTION, documents, year);			
+				getFileDocuments2Zip(domain, domainId, D2_FILE_AUDIT, documents, year);
+				getFileDocuments2Zip(domain, domainId, D2_FILE_CONVOC, documents, year);
+				getFileDocuments2Zip(domain, domainId, D2_FILE_SICAV, documents, year);
+				//**************************
+				
+				File tmpDocuments = File.createTempFile("Documentos TMP%", "", parent);
+				tmpDocuments.delete();
+				tmpDocuments.mkdir();
+				
+				File otherDocuments =File.createTempFile("Otros Documentos%", "", parent);
+				otherDocuments.delete();
+				otherDocuments.mkdir();
+				
+				File otherTmpDocuments =File.createTempFile("Otros Documentos TMP%", "", parent);
+				otherTmpDocuments.delete();
+				otherTmpDocuments.mkdir();
+
+				File f = File.createTempFile("DEPOSITO%", ".xml", parent);
+				
+				if(xml.getData() != null)
+					data = xml.getData();
+				else if(xml.getDriveId() != null) {
+					Domain dom = AON.getDomain(domain, domainId, "");
+					data = DriveUtils.getByteFile(dom , new User().setLogin(""), xml.getDriveId(), xml.getId(), AttachType.REGISTRY);
+				} else data = null;
+				
+				if(data != null)
+					try {
+						AonFileUtils.writeByteArrayToFile(f, data);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			}
+			
+			return parent;
+	  }
+	  
+		public static File getFileDocuments2Zip(String domain, Integer domainId,final String name2, File parent, Integer year) throws IOException {
+			String name = name2;
+			Attach attach = AON.getAttach(domain, domainId, "",
+					f -> f.getDescriptionProperty().eq(name2)
+						.and(f.getDomainProperty().eq(domainId))
+						.and(f.getTypeProperty().eq((byte)7)),
+					AttachType.REGISTRY);
+
+			byte[] data;				
+			if(attach != null && attach.getId() != null){
+				if(year != null && year > 2014){
+					if(name.equals(D2_FILE_AUDIT)) name = D2_FILE_AUDIT_2015;
+					if(name.equals(D2_FILE_AUTOCARTERA_MODEL)) name = D2_FILE_AUTOCARTERA_MODEL_2015;
+					if(name.equals(D2_FILE_CONVOC)) name = D2_FILE_CONVOC_2015;
+					if(name.equals(D2_FILE_GESTION)) name = D2_FILE_GESTION_2015;
+					if(name.equals(D2_FILE_SICAV)) name = D2_FILE_SICAV_2015;
+				}					
+				String extension = attach.getMimeType().getExtension();
+				File tempFile = File.createTempFile(name.toUpperCase() +"%", "."+extension, parent);				
+				if(attach.getData() != null )
+					data = attach.getData();
+				else if(attach.getDriveId() != null){
+					Domain dom = AON.getDomain(domain, domainId, "");
+					data = DriveUtils.getByteFile(dom, new User().setLogin(""), attach.getDriveId(), attach.getId(), AttachType.REGISTRY);
+				} else data = null;
+				if(data != null)		
+					AonFileUtils.writeByteArrayToFile(tempFile, data);				
+				return tempFile;			
+			}
+			return null;
+		}
+}
