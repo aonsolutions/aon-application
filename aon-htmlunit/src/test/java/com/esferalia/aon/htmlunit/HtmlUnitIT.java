f490812e141ba2e1dc76ba5ab4411b554dbd3c60
Raúl Trepiana <rtrepiana@gmail.com>
2016-10-21 14:30:54 +0200

    Clen imports

--- aon-htmlunit/src/test/java/com/esferalia/aon/htmlunit/HtmlUnitIT.java
+++ aon-htmlunit/src/test/java/com/esferalia/aon/htmlunit/HtmlUnitIT.java
@@ -1,7 +1,5 @@
 package com.esferalia.aon.htmlunit;
 
-import static com.esferalia.aon.htmlunit.HtmlUnitIT.LOGGER;
-
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
