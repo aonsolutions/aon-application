d6253c04944a5e09a9d11f288d5b405ef79d7ae8
rtrepiana <rtrepiana@esferalia.com>
2016-11-15 22:45:00 +0100

    Fix 535 , 635 & 635 concepts

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/tgss/creta/Bases.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/tgss/creta/Bases.java
@@ -1171,7 +1171,7 @@ public class Bases {
 	private static Map<String, CretaData> CONTEXT_VARIABLE_MAP = new HashMap<String, CretaData>() {
 		{
 			put("500", new MandatoryCCretaData(CGC_BASE.getName()));
-			put("535", new MandatoryCCretaData(CGC_BASE.getName()));
+			put("535", new MandatoryCCretaData(MATERNITY_BASE.getName()));
 
 			put("501", new OptionalCCretaData(STRUCTURAL_OVERTIME_BASE.getName()));
 			put("502", new OptionalCCretaData(
@@ -1180,8 +1180,8 @@ public class Bases {
 
 			put("601", new MandatoryCCretaData(CGP_BASE.getName()));
 			put("611", new MandatoryCCretaData(CGP_BASE.getName()));
-			put("635", new MandatoryCCretaData(CGP_BASE.getName()));
-			put("634", new MandatoryCCretaData(CGP_BASE.getName()));
+			put("635", new MandatoryCCretaData(MATERNITY_BASE.getName()));
+			put("634", new MandatoryCCretaData(MATERNITY_BASE.getName()));
 
 			put("663", new MandatoryCCretaData(ContextVariable.PREST_IT));
 
