a841713f21b8a2f0a8c34cabfcaa7e9400a635b8
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2016-11-28 15:27:06 +0100

    Clase navegation: comprobar naegador

---
+++ aon-gwt-dump/src/main/java/com/esferalia/aon/gwt/dump/client/Navegation.java
@@ -0,0 +1,30 @@
+package com.esferalia.aon.gwt.dump.client;
+
+import com.google.gwt.user.client.Window;
+
+public class Navegation {
+	
+	public static String getBrowserName(){
+		return Window.Navigator.getUserAgent().toLowerCase();	
+	}
+	
+	public static boolean isChromeBrowser(){
+		return getBrowserName().contains("chrome");
+	}
+	
+	public static boolean isSafariBrowser(){
+		return getBrowserName().contains("safari");
+	}
+	
+	public static boolean isFireFoxBrowser(){
+		return getBrowserName().contains("firefox");
+	}
+	
+	public static boolean isIEBrowser(){
+		return getBrowserName().contains("msie");
+	}
+	
+	
+}
+
+
