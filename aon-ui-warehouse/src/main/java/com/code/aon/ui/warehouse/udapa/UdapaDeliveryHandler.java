fbf7b5a7e933a60a4154d86ef95c443c5413346b
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2016-12-21 15:19:14 +0100

    UDAPA. (import. y export. EDI) habilitado formato antiguo y connect

--- aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/udapa/UdapaDeliveryHandler.java
+++ aon-ui-warehouse/src/main/java/com/code/aon/ui/warehouse/udapa/UdapaDeliveryHandler.java
@@ -1,10 +1,5 @@
 package com.code.aon.ui.warehouse.udapa;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,29 +10,20 @@ import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.DataModel;
 import javax.faces.model.SelectItem;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.code.aon.AonVersion;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.domain.DomainManager;
-import com.code.aon.faces.component.util.DownloadUtil;
 import com.code.aon.faces.controller.LogPanelController;
-import com.code.aon.file.format.output.FileOutput;
 import com.code.aon.ui.common.serialize.SerializableListDataModel;
-import com.code.aon.ui.company.controller.CompanyController;
-import com.code.aon.ui.company.controller.ICompanyConstants;
-import com.code.aon.ui.customer.controller.CustomerEdiSupportController;
-import com.code.aon.ui.customer.controller.ICustomerConstants;
 import com.code.aon.ui.form.IController;
 import com.code.aon.ui.util.AonUtil;
 import com.code.aon.ui.warehouse.controller.WarehouseCollectionsController;
 import com.code.aon.warehouse.Delivery;
 import com.code.aon.warehouse.Warehouse;
-import com.esferalia.aon.file.seres.util.writer.connect.ConnectDeliveryWriter;
 import com.esferalia.aon.ingenet.IngenetDeliveryManager;
 import com.esferalia.aon.ingenet.SourceSalesNotFoundException;
 
@@ -197,50 +183,6 @@ public class UdapaDeliveryHandler implements Serializable {
 			getLogPanel().error(e.getCause().getMessage());
 		}
 		getLogPanel().info("Proceso finalizado.");
-	}
-	
-	public void onExportUdapaEdiFile(ActionEvent event) {
-		FileOutput output = null;
-		HttpServletResponse response = null;
-		OutputStream out = null;
-		try {
-			Delivery delivery = (Delivery) controller.getTo();
-			CustomerEdiSupportController ediSupport = (CustomerEdiSupportController) AonUtil
-					.getRegisteredBean(ICustomerConstants.CUSTOMER_EDI_SUPPORT_CONTROLLER_NAME);
-			String customerEdiCode = ediSupport.getEdiCodes(
-					delivery.getCustomer().getRegistry(),
-					delivery.getRegistryAddress()).get(
-					CustomerEdiSupportController.ALBARANES);
-			CompanyController company = (CompanyController) AonUtil
-					.getRegisteredBean(ICompanyConstants.COMPANY_CONTROLLER_NAME);
-			String companyEdiCode = company.getEdiCompanyCode();
-
-			// writer file
-			ConnectDeliveryWriter writer = new ConnectDeliveryWriter();
-			output = writer.createFile(delivery, companyEdiCode,
-					customerEdiCode);
-
-			// download file
-			String name = "albaran";
-			String number = delivery.getReferenceCode();
-			byte[] data = output.getContent();
-			int size = data.length;
-			response = DownloadUtil.getResponse();
-			out = DownloadUtil.initDownload(response, name + "." + number,
-					null, size);
-			InputStream fileIn = new BufferedInputStream(
-					new ByteArrayInputStream(data));
-			IOUtils.copy(fileIn, out);
-			IOUtils.closeQuietly(fileIn);
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage());
-			throw new AbortProcessingException(e.getMessage(), e);
-		} catch (Throwable e) {
-			AonUtil.addErrorMessage(e.getMessage());
-			throw new AbortProcessingException(e.getMessage(), e);
-		} finally {
-			DownloadUtil.finishDownload(response, out);
-		}
 	}
 
 	private LogPanelController getLogPanel() {
