ed53689319f5e507751318f5efb10540d8a1ac38
Ander Ibáñez de Gauna <aibanez@aonsolutions.es>
2017-02-01 12:15:16 +0100

    Listado de cuotas - pdf por mes (Fixed #1097)

--- aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/FeePrintController.java
+++ aon-ui-finance/src/main/java/com/code/aon/ui/finance/controller/FeePrintController.java
@@ -1,25 +1,15 @@
 package com.code.aon.ui.finance.controller;
 
-import java.nio.charset.StandardCharsets;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Base64;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import javax.faces.event.ActionEvent;
 
 import com.code.aon.AonVersion;
 import com.code.aon.common.ITransferObject;
 import com.code.aon.common.ManagerBeanException;
-import com.code.aon.common.domain.DomainManager;
 import com.code.aon.ql.Criteria;
 import com.code.aon.ui.form.BasicController;
-import com.code.aon.ui.util.AonUtil;
 import com.esferalia.aon.entity.IEntityAlias;
-import com.esferalia.aon.occam.api.AON;
-import com.esferalia.aon.occam.api.model.Domain;
 
 
 public class FeePrintController extends BasicController {
@@ -42,32 +32,6 @@ public class FeePrintController extends BasicController {
 		criteria.addOrder(getManagerBean().getFieldName(IEntityAlias.CUSTOMER_FEE_BILLING_DATE));
 		criteria.addOrder(getManagerBean().getFieldName(IEntityAlias.CUSTOMER_FEE_CUSTOMER_REGISTRY_NAME));
 		orderedList=getManagerBean().getList(criteria);
-	}
-	
-	public String getOnOrderCustomerFeeByDateLinkExcel() throws ManagerBeanException, ParseException {
-		SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH);
-		SimpleDateFormat format2 = new SimpleDateFormat("dd/MM/yyyy");
-		Integer i = getCriteria().getExpression().toString().indexOf("between ");
-		Integer j  = getCriteria().getExpression().toString().indexOf(" and");
-		String dateStr = getCriteria().getExpression().toString().substring(i+8, j);
-		Date date = format.parse(dateStr);
-		Domain domain = AON.getDomain(AonUtil.getDomainName(), DomainManager.getCurrentDomain(), AonUtil.getRemoteUser());
-		String str = "domain="+ domain.getName() + "&login="+AonUtil.getRemoteUser() + "&date=" + format2.format(date);
-		String base = Base64.getEncoder().encodeToString(str.getBytes(StandardCharsets.UTF_8));
-		return "/download_fee_projection_excel/"+ base;
-	}
-	
-	public String getOnOrderCustomerFeeByDateLinkPdf() throws ManagerBeanException, ParseException {
-		SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH);
-		SimpleDateFormat format2 = new SimpleDateFormat("dd/MM/yyyy");
-		Integer i = getCriteria().getExpression().toString().indexOf("between ");
-		Integer j  = getCriteria().getExpression().toString().indexOf(" and");
-		String dateStr = getCriteria().getExpression().toString().substring(i+8, j);
-		Date date = format.parse(dateStr);
-		Domain domain = AON.getDomain(AonUtil.getDomainName(), DomainManager.getCurrentDomain(), AonUtil.getRemoteUser());
-		String str = "domain="+ domain.getName() + "&login="+AonUtil.getRemoteUser() + "&date=" + format2.format(date);
-		String base = Base64.getEncoder().encodeToString(str.getBytes(StandardCharsets.UTF_8));
-		return "/download_fee_projection_pdf/"+ base;
 	}
 	
 	public void onOrderCustomerFeeByItem(ActionEvent event) throws ManagerBeanException {
