10acd26f278fc1a6fb15ed337b49ae2fa718d56e
girazu <girazu@aonsolutions.es>
2017-02-01 16:26:42 +0100

    FACTURAS. Control de no dejar borrar una Factura que tiene una ficha de Amortizacion asociada

--- aon-account-bridge/src/main/java/com/code/aon/account/bridge/event/InvoiceBeanVetoListener.java
+++ aon-account-bridge/src/main/java/com/code/aon/account/bridge/event/InvoiceBeanVetoListener.java
@@ -1,13 +1,18 @@
 package com.code.aon.account.bridge.event;
 
 import com.code.aon.account.bridge.writer.AccountEntryInvoiceWriter;
+import com.code.aon.accounting.AmortizationInvoice;
 import com.code.aon.AonVersion;
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.IManagerBean;
 import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.event.ManagerBeanEvent;
 import com.code.aon.common.event.ManagerBeanVetoListenerAdapter;
 import com.code.aon.common.event.ManagerBeanVetoListenerException;
 import com.code.aon.finance.Invoice;
 import com.code.aon.finance.enumeration.InvoiceStatus;
+import com.code.aon.ql.Criteria;
+import com.esferalia.aon.entity.IEntityAlias;
 
 public class InvoiceBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 	
@@ -26,12 +31,20 @@ public class InvoiceBeanVetoListener extends ManagerBeanVetoListenerAdapter {
 	public void vetoableBeanRemoved(ManagerBeanEvent evt) throws ManagerBeanVetoListenerException {
 		try {
 			Invoice invoice = (Invoice) evt.getTo();
+			if (invoice.isInvestment()) {
+				IManagerBean amortizationInvoiceBean = BeanManager.getManagerBean(AmortizationInvoice.class);
+				Criteria criteria = new Criteria();
+				criteria.addEqualExpression(amortizationInvoiceBean.getFieldName(IEntityAlias.AMORTIZATION_INVOICE_INVOICE_ID), invoice.getId());
+				if (amortizationInvoiceBean.getCount(criteria) > 0) {
+					throw new ManagerBeanVetoListenerException("La Factura " + invoice.getReferenceCode() + " no se puede borrar. " +
+																 "Tiene asociada una ficha de Amortización.");
+				}
+			}
 			if (invoice.getStatus().equals(InvoiceStatus.SCORED)) {
 				getAccountEntryInvoiceWriter().unrecordInvoice(invoice);
 			}
 		} catch (ManagerBeanException e) {
 			throw new ManagerBeanVetoListenerException(e.getMessage(),e);
-			
 		}
 	}
 }
