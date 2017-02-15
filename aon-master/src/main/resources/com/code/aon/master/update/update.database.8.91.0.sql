1972c74bb422448e265a58d4bf8125068e7b6f5f
Raúl Trepiana <rtrepiana@gmail.com>
2017-02-15 10:34:41 +0100

    Fix 2017 Training & Fellow data

--- aon-master/src/main/resources/com/code/aon/master/update/update.database.8.91.0.sql
+++ aon-master/src/main/resources/com/code/aon/master/update/update.database.8.91.0.sql
@@ -439,8 +439,14 @@ INSERT INTO `system_data`
 ,"1"
 ,NULL);
 
-#
 # 101 & 105 TRAINING & FELLOWS
+
+
+DELETE FROM `system_deduction` 
+WHERE `domain`="-101" AND `start_date`="2017-01-01" AND `deduction_concept` IS NULL;
+
+DELETE FROM `system_deduction` 
+WHERE `domain`="-105" AND `start_date`="2017-01-01" AND `deduction_concept` IS NULL;
 
 SET @CGC_CONCEPT=(SELECT `id` FROM`deduction_concept` WHERE `code`="CGC");
 
@@ -466,7 +472,7 @@ INSERT INTO `system_deduction`
 ,@CGC_CONCEPT
 ,NULL
 ,"0"
-,"6.67"
+,"SI(BASE_CGC > 0.00, 6.67,0.00)"
 ,"2017-01-01"
 ,NULL
 ,NULL);
@@ -494,7 +500,7 @@ VALUES (
 ,@CGC_CONCEPT
 ,NULL
 ,"0"
-,"6.67"
+,"SI(BASE_CGC > 0.00, 6.67,0.00)"
 ,"2017-01-01"
 ,NULL
 ,NULL);
@@ -504,39 +510,45 @@ SET @DESMPL_CONCEPT=(SELECT `id` FROM`deduction_concept` WHERE `code`="DESMPL");
 DELETE FROM `system_deduction` 
 WHERE `domain`="-101" AND `start_date`="2017-01-01" AND `deduction_concept` = @DESMPL_CONCEPT;
 
-UPDATE `system_deduction` 
-SET `end_date` = "2016-12-31" 
-WHERE `domain`="-101" AND `start_date`="2016-01-01" AND `end_date` IS NULL AND `deduction_concept` = @DESMPL_CONCEPT;
+# 
+# UPDATE `system_deduction` 
+# SET `end_date` = "2016-12-31" 
+# WHERE `domain`="-101" AND `start_date`="2016-01-01" AND `end_date` IS NULL AND `deduction_concept` = @DESMPL_CONCEPT;
 
-INSERT INTO `system_deduction` 
-(`domain`
-,`type`
-,`deduction_concept`
-,`description`
-,`description_decorable`
-,`expression`
-,`start_date`
-,`end_date`
-,`month`)
-VALUES 
-("-101"
-,NULL
-,@DESMPL_CONCEPT
-,NULL
-,"0"
-,"1.55/100.00 * 825.60"
-,"2017-01-01"
-,NULL
-,NULL);
+# INSERT INTO `system_deduction` 
+# (`domain`
+# ,`type`
+# ,`deduction_concept`
+# ,`description`
+# ,`description_decorable`
+# ,`expression`
+# ,`start_date`
+# ,`end_date`
+# ,`month`)
+# VALUES 
+# ("-101"
+# ,NULL
+# ,@DESMPL_CONCEPT
+# ,NULL
+# ,"0"
+# ,"1.55/100.00 * BASE_CGP_MIN"
+# ,"2017-01-01"
+# ,NULL
+# ,NULL);
 
 SET @FP_CONCEPT=(SELECT `id` FROM`deduction_concept` WHERE `code`="FP");
 
 DELETE FROM `system_deduction` 
 WHERE `domain`="-101" AND `start_date`="2017-01-01" AND `deduction_concept` = @FP_CONCEPT;
 
+DELETE FROM `system_deduction` 
+WHERE `domain`="-101" AND `start_date`="2014-01-01" AND `deduction_concept` = @FP_CONCEPT 
+AND `expression` IN ('0.15', '0.16');
+
 UPDATE `system_deduction` 
 SET `end_date` = "2016-12-31" 
-WHERE `domain`="-101" AND `start_date`="2016-01-01" AND `end_date` IS NULL AND `deduction_concept` = @FP_CONCEPT;
+WHERE `domain`="-101" AND `start_date` < "2017-01-01" AND `end_date` IS NULL AND `deduction_concept` = @FP_CONCEPT;
+
 
 INSERT INTO `system_deduction` 
 (`domain`
@@ -554,8 +566,8 @@ VALUES
 ,@FP_CONCEPT
 ,NULL
 ,"0"
-,"0.16"
-,"2014-01-01"
+,"SI(BASE_CGP > 0.00, 0.16,0.00)"
+,"2017-01-01"
 ,NULL
 ,NULL);
 
@@ -574,58 +586,40 @@ INSERT INTO `system_cost`
 ,`expression`
 ,`type`
 ,`code`) 
-VALUES ("-101","2017-01-01",NULL,NULL,"33.46","0","CGC_E");
+VALUES ("-101","2017-01-01",NULL,"Contingencias Comunes","SI(BASE_CGC > 0.00, 33.46,0.00)","0","CGC_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-101" AND `code`="IT_E" AND `start_date`="2017-01-01" ;
 
 UPDATE `system_cost` 
-SET `end_date` = "2016-12-31" 
-WHERE `domain`="-101" AND `code`="IT_E" AND `start_date`="2016-01-01" AND `end_date` IS NULL;
+SET `end_date` = NULL 
+WHERE `domain`="-101" AND `code`="IT_E" AND `start_date`="2016-01-01" AND `end_date` = "2016-12-31";
 
-INSERT INTO `system_cost` 
-(`domain`
-,`start_date`
-,`end_date`
-,`description`
-,`expression`
-,`type`
-,`code`) 
-VALUES ("-101","2017-01-01",NULL,NULL,"2.38","1","IT_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-101" AND `code`="IMS_E" AND `start_date`="2017-01-01" ;
 
 UPDATE `system_cost` 
-SET `end_date` = "2016-12-31" 
-WHERE `domain`="-101" AND `code`="IMS_E" AND `start_date`="2016-01-01" AND `end_date` IS NULL;
+SET `end_date` = NULL 
+WHERE `domain`="-101" AND `code`="IMS_E" AND `start_date`="2016-01-01" AND `end_date` = "2016-12-31";
 
-INSERT INTO `system_cost` 
-(`domain`
-,`start_date`
-,`end_date`
-,`description`
-,`expression`
-,`type`
-,`code`) 
-VALUES ("-101","2017-01-01",NULL,NULL,"1.88","1","IMS_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-101" AND `code`="DESMPL_E" AND `start_date`="2017-01-01" ;
 
-UPDATE `system_cost` 
-SET `end_date` = "2016-12-31" 
-WHERE `domain`="-101" AND `code`="DESMPL_E" AND `start_date`="2016-01-01" AND `end_date` IS NULL;
-
-INSERT INTO `system_cost` 
-(`domain`
-,`start_date`
-,`end_date`
-,`description`
-,`expression`
-,`type`
-,`code`) 
-VALUES ("-101","2017-01-01",NULL,NULL,"5.50/100.00 * 825.60","2","DESMPL_E");
+# UPDATE `system_cost` 
+# SET `end_date` = "2016-12-31" 
+# WHERE `domain`="-101" AND `code`="DESMPL_E" AND `start_date`="2016-01-01" AND `end_date` IS NULL;
+# 
+# INSERT INTO `system_cost` 
+# (`domain`
+# ,`start_date`
+# ,`end_date`
+# ,`description`
+# ,`expression`
+# ,`type`
+# ,`code`) 
+# VALUES ("-101","2017-01-01",NULL,NULL,"5.50/100.00 * 825.60","2","DESMPL_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-101" AND `code`="FOGASA_E" AND `start_date`="2017-01-01" ;
@@ -642,7 +636,7 @@ INSERT INTO `system_cost`
 ,`expression`
 ,`type`
 ,`code`) 
-VALUES ("-101","2017-01-01",NULL,NULL,"2.54","10","FOGASA_E");
+VALUES ("-101","2017-01-01",NULL,"FOGASA","SI(BASE_CGP > 0.00, 2.54,0.00)","10","FOGASA_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-101" AND `code`="FP_E" AND `start_date`="2017-01-01" ;
@@ -659,7 +653,7 @@ INSERT INTO `system_cost`
 ,`expression`
 ,`type`
 ,`code`) 
-VALUES ("-101","2017-01-01",NULL,NULL,"1.23","3","FP_E");
+VALUES ("-101","2017-01-01",NULL,"Formación Profesional","SI(BASE_CGP > 0.00, 1.23,0.00)","3","FP_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-105" AND `code`="CGC_E" AND `start_date`="2017-01-01" ;
@@ -676,51 +670,31 @@ INSERT INTO `system_cost`
 ,`expression`
 ,`type`
 ,`code`) 
-VALUES ("-105","2017-01-01",NULL,NULL,"33.46","0","CGC_E");
+VALUES ("-105","2017-01-01",NULL,"Contingencias Comunes","SI(BASE_CGC > 0.00, 33.46,0.00)","0","CGC_E");
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-105" AND `code`="IT_E" AND `start_date`="2017-01-01" ;
 
 UPDATE `system_cost` 
-SET `end_date` = "2016-12-31" 
-WHERE `domain`="-105" AND `code`="IT_E" AND `start_date`="2016-01-01" AND `end_date` IS NULL;
-
-INSERT INTO `system_cost` 
-(`domain`
-,`start_date`
-,`end_date`
-,`description`
-,`expression`
-,`type`
-,`code`) 
-VALUES ("-105","2017-01-01",NULL,NULL,"2.38","1","IT_E");
+SET `end_date` = NULL
+WHERE `domain`="-105" AND `code`="IT_E" AND `start_date`="2016-01-01" AND `end_date` = "2016-12-31" ;
 
 DELETE FROM `system_cost` 
 WHERE `domain`="-105" AND `code`="IMS_E" AND `start_date`="2017-01-01" ;
 
 UPDATE `system_cost` 
-SET `end_date` = "2016-12-31" 
-WHERE `domain`="-105" AND `code`="IMS_E" AND `start_date`="2016-01-01" AND `end_date` IS NULL;
-
-INSERT INTO `system_cost` 
-(`domain`
-,`start_date`
-,`end_date`
-,`description`
-,`expression`
-,`type`
-,`code`) 
-VALUES ("-105","2017-01-01",NULL,NULL,"1.88","1","IMS_E");
-
+SET `end_date` = NULL
+WHERE `domain`="-105" AND `code`="IMS_E" AND `start_date`="2016-01-01" AND `end_date` = "2016-12-31" ;
 
 DELETE FROM `system_data` WHERE `domain`=-101 AND `name` = 'BASE_CGP_MIN' AND `start_date` = '2017-01-01';
+
 INSERT INTO `system_data` ( `domain`, `name`, `expression`, `start_date` ) 
 VALUES ( -101, 'BASE_CGP_MIN', '825.60', '2017-01-01' ); 
 
 DELETE FROM `system_data` WHERE `domain`=-105 AND `name` = 'BASE_CGP_MIN' AND `start_date` = '2017-01-01';
+
 INSERT INTO `system_data` ( `domain`, `name`, `expression`, `start_date` ) 
 VALUES ( -105, 'BASE_CGP_MIN', '825.60', '2017-01-01' ); 
-
 
 
 UPDATE `db_version` SET `version_number` = '8.91.1';
