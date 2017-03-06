9785a2c4decb2acdaf6b824ab450fd12bd9fae4e
Ander Ibáñez de Gauna <aibanez@aonsolutions.es>
2017-03-06 09:41:41 +0100

    Packing list. arreglos.

--- aon-gwt-api/src/main/java/com/esferalia/aon/gwt/api/client/warehouse/Warehouse.java
+++ aon-gwt-api/src/main/java/com/esferalia/aon/gwt/api/client/warehouse/Warehouse.java
@@ -3,24 +3,20 @@ package com.esferalia.aon.gwt.api.client.warehouse;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-import com.esferalia.aon.gwt.api.client.IApi;
-import com.esferalia.aon.gwt.api.client.IApiAsync;
 import com.esferalia.aon.gwt.api.client.JSON;
 import com.esferalia.aon.gwt.api.client.Methods;
 import com.esferalia.aon.gwt.api.client.incidence.JsObject;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class Warehouse extends Methods{
-
-	final IApiAsync impl = GWT.create(IApi.class);
 
 	public Warehouse(String url, String accesToken, String domainName, String userName) {
 		this.url = url;
 		this.accessToken = accesToken;
 		this.domainName = domainName;
 		this.userName = userName;
+		this.scheme = url.contains("https") ? "https" : "http";
 	}
 	
 	public void getCarrierPacking(HashMap<String, LinkedList<String>> filterMap, AsyncCallback<JSON<JsCarrierPacking>> callback){
