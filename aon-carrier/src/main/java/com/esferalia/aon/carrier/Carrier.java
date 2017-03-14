cb0c8bcfc1ba7e32d6a4f41f2483907ae77d5326
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-03-14 14:35:14 +0100

    ALMACEN. listados de agencias de transporte

--- aon-carrier/src/main/java/com/esferalia/aon/carrier/Carrier.java
+++ aon-carrier/src/main/java/com/esferalia/aon/carrier/Carrier.java
@@ -1,21 +1,13 @@
 package com.esferalia.aon.carrier;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import com.code.aon.AonVersion;
 import com.code.aon.config.IScopable;
 import com.code.aon.registry.IRegistry;
-import com.code.aon.registry.RegistryAttachment;
-import com.code.aon.seller.enumeration.SellerStatus;
 import com.esferalia.aon.entity.master.CarrierDB;
-import com.esferalia.aon.entity.master.SellerDB;
 
 @Entity
 @Table(name="carrier")
