099b459dedd61b685b0bed50cfac7ca4afccdde0
EukeCastellano <ecastellano@aonsolutions.es>
2017-03-15 17:04:26 +0100

    nueva entrada de apuntes - extracto de cuentas, contrapartida [Fixes #1194]

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/accounting/panel/AccountStatementPanel.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/accounting/panel/AccountStatementPanel.java
@@ -103,7 +103,9 @@ public class AccountStatementPanel extends ScrollPanel implements HasSelectionHa
 						filter.addClickHandler(new ClickHandler() {
 							@Override
 							public void onClick(ClickEvent event) {
-								if (!dateTo.getValue().before(dateFrom.getValue())) {
+								if ( (dateTo.getValue() == null && dateFrom.getValue() != null) 
+								  || (dateTo.getValue() != null && dateFrom.getValue() == null)
+								  || (!dateTo.getValue().before(dateFrom.getValue()))) {
 									AccountStatementPanel.this.show(
 											new AccountStatementParams()
 											.setAccount( result.getAccount().getId())
@@ -152,8 +154,8 @@ public class AccountStatementPanel extends ScrollPanel implements HasSelectionHa
 							}
 							header.add(label);
 						}
-						Label header1 = new Label("    DIARIO    FECHA   CONCEPTO                                      "
-								+ "DEBE            HABER     SALDO DEUDOR    SALDO ACREDOR CONTRAPR. NUM.DOCUMENTO");
+						Label header1 = new Label(" DIARIO    FECHA   CONCEPTO                                      "
+								+ "DEBE            HABER     SALDO DEUDOR    SALDO ACREDOR CONTRAPARTIDA         NUM.DOCUMENTO");
 						header1.setStyleName(AON.AON_CSS.aonBold());
 						header1.addStyleName(AON.AON_CSS.aonMarginTop());
 						header1.addStyleName(AON.AON_CSS.aonBorderTop());
@@ -166,14 +168,17 @@ public class AccountStatementPanel extends ScrollPanel implements HasSelectionHa
 						} else {
 							for (final AccountStatement as : result.getDetails()) {
 								final Label label = new Label(
-										AonStringUtils.leftPad(AonStringUtils.defaultIfEmpty(AonNumberUtils.toString(as.getJournal()),AonStringUtils.SPACE), 10)
+										AonStringUtils.leftPad(AonStringUtils.defaultString(AonNumberUtils.toString(as.getJournal())), 7)
 										+AonStringUtils.center(AON.DATE_FORMAT.format(as.getEntryDate()),12)
 										+AonStringUtils.rightPad(AonStringUtils.abbreviate(as.getConcept(),32), 33)
 										+AonStringUtils.leftPad(AonMathUtils.isZero(as.getDebit())?AonStringUtils.SPACE:AON.FMT.format(as.getDebit()),17)		
 										+AonStringUtils.leftPad(AonMathUtils.isZero(as.getCredit())?AonStringUtils.SPACE:AON.FMT.format(as.getCredit()),17)
 										+AonStringUtils.leftPad(AonMathUtils.isZero(as.getDebitBalance())?AonStringUtils.SPACE:AON.FMT.format(as.getDebitBalance()),17)		
 										+AonStringUtils.leftPad(AonMathUtils.isZero(as.getUnpaidBalance())?AonStringUtils.SPACE:AON.FMT.format(as.getUnpaidBalance()),17)
-										+AonStringUtils.center(AonStringUtils.defaultString(as.getBalancingAccountCode()),11)
+										+AonStringUtils.SPACE
+										+AonStringUtils.rightPad(AonStringUtils.defaultString(as.getBalancingAccountCode()),10)
+										+AonStringUtils.rightPad(AonStringUtils.defaultString(AonStringUtils.abbreviate(as.getBalancingAccountDescription(),11)),12)
+										+AonStringUtils.SPACE
 										+AonStringUtils.rightPad(AonStringUtils.defaultString(as.getDocumentNumber()), 33));
 								label.addClickHandler(new ClickHandler() {
 									
@@ -186,6 +191,7 @@ public class AccountStatementPanel extends ScrollPanel implements HasSelectionHa
 									@Override
 									public void onMouseOver(MouseOverEvent event) {
 										label.addStyleName(AON.AON_CSS.aonBold());
+										label.addStyleName(AON.AON_CSS.aonBorderBottom());
 									}
 								});
 								label.addMouseOutHandler(new MouseOutHandler() {
@@ -193,6 +199,7 @@ public class AccountStatementPanel extends ScrollPanel implements HasSelectionHa
 									@Override
 									public void onMouseOut(MouseOutEvent event) {
 										label.removeStyleName(AON.AON_CSS.aonBold());
+										label.removeStyleName(AON.AON_CSS.aonBorderBottom());
 									}
 								});
 								label.addStyleName(AON.AON_CSS.aonClickable());
