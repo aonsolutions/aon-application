813541bd90b4d807dd0de92681583c02594dbe3a
girazu <girazu@aonsolutions.es>
2017-03-15 18:23:12 +0100

    PMS. Reservas. Fecha de penalizacion. Tener en cuenta el startTime en lugar del startDate.

--- aon-pms/src/main/java/com/esferalia/aon/pms/reservation/ReservationUtils.java
+++ aon-pms/src/main/java/com/esferalia/aon/pms/reservation/ReservationUtils.java
@@ -1449,7 +1449,7 @@ public class ReservationUtils implements IReservationConstants, Serializable {
 		if (StringUtils.contains(penaltyStr, "#")) {
 			String dueHours = penaltyStr.substring(penaltyStr.indexOf("#") + 1);
 			if (NumberUtils.isNumber(dueHours)) {
-				return DateUtils.addHours(DateUtils.truncate(reservation.getStartDate(), Calendar.DATE), 0-Integer.parseInt(dueHours));
+				return DateUtils.addHours(DateUtils.truncate(reservation.getStartTime(), Calendar.DATE), 0-Integer.parseInt(dueHours));
 			}
 		}
 		return new Date();
