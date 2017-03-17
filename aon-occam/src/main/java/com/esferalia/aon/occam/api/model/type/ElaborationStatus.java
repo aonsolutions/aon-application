c48f62b983374973a8747e0534653828d9fe200d
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-03-17 14:15:37 +0100

    INGENET. nuevo estado REOPEN para elaboraciones

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/type/ElaborationStatus.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/type/ElaborationStatus.java
@@ -1,11 +1,13 @@
 package com.esferalia.aon.occam.api.model.type;
 
+
 public enum ElaborationStatus {
 
     PENDING("Pendiente"),
     CLOSED("Cerrado"),
     IN_PROGRESS("En progreso"),
     FAIL("Fallido"),
+    REOPEN("Reabierto"),
     ;
 	
 	private String name;
@@ -14,12 +16,22 @@ public enum ElaborationStatus {
 		this.name = name;
 	}
 	
-	public byte value(){
-		return (byte) this.ordinal();
-	}
-	
     public String getName() {
 		return name;
     }
+    
+	public byte value() {
+		return (byte) this.ordinal();
+	}
+	
+	public static ElaborationStatus safeValueOf( Byte i ) {
+		if (i == null) return null;
+		return safeValueOf( i.intValue() ); 
+	}
+	public static ElaborationStatus safeValueOf( Integer i ) {
+		if (i == null) return null;
+		if (i < 0 || i >= ElaborationStatus.values().length) return null;
+		return ElaborationStatus.values()[i];
+	}
 
 }
