33df515d3808bda1a9b1a0307853dfbffc2841e4
girazu <girazu@aonsolutions.es>
2017-04-04 14:06:49 +0200

    Configuracion Global. Funcionalidad contratada. Cambios en pantalla ConexFlow. Añadir Forma de Pago, arreglos para que funcione correctamente en Internet Explorer, y otros arreglos.

--- aon-ui-admin/src/main/java/com/code/aon/ui/admin/controller/ConexFlowController.java
+++ aon-ui-admin/src/main/java/com/code/aon/ui/admin/controller/ConexFlowController.java
@@ -2,7 +2,12 @@ package com.code.aon.ui.admin.controller;
 
 import javax.faces.event.ActionEvent;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.code.aon.common.BeanManager;
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.common.domain.DomainManager;
+import com.code.aon.config.PayMethod;
 import com.code.aon.config.util.AppParamUtil;
 import com.code.aon.ui.form.BasicController;
 import com.code.aon.ui.util.AonUtil;
@@ -14,81 +19,26 @@ import com.esferalia.aon.occam.api.model.type.AppParam;
 import com.esferalia.aon.occam.api.model.type.DomainType;
 import com.esferalia.aon.occam.impl.jooq.dao.AppParamDAO;
 
-
 public class ConexFlowController extends BasicController {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-	
+	boolean conexFlow;
 	boolean showConexFlowWindow;
 	String server;
 	String serverAck;
 	String user;
 	String keyA;
 	String keyB;
-	boolean conexFlow;
+	PayMethod payMethod;
 	
 	public ConexFlowController() {
 		conexFlow = getConexFlowAux();
 	}
 	
-	public String getServer() {
-		return server;
-	}
-
-	public void setServer(String server) {
-		if(server == null) server = "";
-		this.server = server;
-	}
-
-	public String getServerAck() {
-		return serverAck;
-	}
-
-	public void setServerAck(String serverAck) {
-		if(serverAck == null) serverAck = "";
-		this.serverAck = serverAck;
-	}
-	
-	public String getKeyA() {
-		return keyA;
-	}
-
-	public void setKeyA(String keyA) {
-		this.keyA = keyA;
-	}
-
-	public String getKeyB() {
-		return keyB;
-	}
-
-	public void setKeyB(String keyB) {
-		this.keyB = keyB;
-	}
-
-	public boolean getConexFlowAux(){
-		AONContext ctx = null;
-		try {
-			String domainName = AonUtil.getDomainName();
-			Integer domainId = DomainManager.getCurrentDomain();
-			ctx = AONContext.getAONContext(domainName, domainId, AonUtil.getRemoteUser());
-			ApplicationParameter aux1 = AppParamDAO.fetchOne(ctx,
-					AppParam.PMS_CONEXFLOW_SERVER_PARAM);
-			return (aux1 != null && aux1.getValue() != null && !aux1.getValue()
-					.equals("Null"));
-		} finally {
-			if (ctx != null)
-				ctx.close();
-		}
-	}
-	
 	public boolean isConexFlow() {
 		return conexFlow;
 	}
-
 	public void setConexFlow(boolean conexFlow) {
 		this.conexFlow = conexFlow;
 	}
@@ -96,18 +46,65 @@ public class ConexFlowController extends BasicController {
 	public boolean isShowConexFlowWindow() {
 		return showConexFlowWindow;
 	}
-
 	public void setShowConexFlowWindow(boolean showConexFlowWindow) {
 		this.showConexFlowWindow = showConexFlowWindow;
 	}
 	
+	public String getServer() {
+		return server;
+	}
+	public void setServer(String server) {
+		this.server = (server != null) ? server : StringUtils.EMPTY;
+	}
+
+	public String getServerAck() {
+		return serverAck;
+	}
+	public void setServerAck(String serverAck) {
+		this.serverAck = (serverAck != null) ? serverAck : StringUtils.EMPTY;
+	}
+
 	public String getUser() {
 		return user;
 	}
-
 	public void setUser(String user) {
-		if(user == null) user = "";
-		this.user = user;
+		this.user = (user != null) ? user : StringUtils.EMPTY;
+	}
+
+	public String getKeyA() {
+		return keyA;
+	}
+	public void setKeyA(String keyA) {
+		this.keyA = keyA;
+	}
+
+	public String getKeyB() {
+		return keyB;
+	}
+	public void setKeyB(String keyB) {
+		this.keyB = keyB;
+	}
+
+	public PayMethod getPayMethod() {
+		return payMethod;
+	}
+	public void setPayMethod(PayMethod payMethod) {
+		this.payMethod = payMethod;
+	}
+
+	public boolean getConexFlowAux(){
+		AONContext ctx = null;
+		try {
+			ctx = AONContext.getAONContext(AonUtil.getDomainName(), DomainManager.getCurrentDomain(), AonUtil.getRemoteUser());
+			ApplicationParameter aux = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_SERVER_PARAM);
+			return (aux != null && aux.getValue() != null);
+		} finally {
+			if (ctx != null) ctx.close();
+		}
+	}
+
+	public boolean isShowConexflow(){
+		return isPlayasol() && isHotel();
 	}
 
 	public boolean isHotel(){
@@ -117,16 +114,12 @@ public class ConexFlowController extends BasicController {
 		Domain domain = AON.getDomain(domainName, domainId, user);
 		return domain.getDomainType().equals(DomainType.HOTEL);
 	}
-	
+
 	public boolean isPlayasol(){
 		String domainName = AonUtil.getDomainName();
 		return domainName.contains("playasol");
 	}	
-	
-	public boolean isShowConexflow(){
-		return isPlayasol() && isHotel();
-	}
-	
+
 	public void onConexFlowShow(){
 		AONContext ctx = null;
 		try {
@@ -134,51 +127,53 @@ public class ConexFlowController extends BasicController {
 			domain.setName(AonUtil.getDomainName());
 			domain.setId(AonUtil.getAuthPrincipal().getDomainId());
 			ctx = AONContext.getAONContext(domain.getName(), domain.getId(), AonUtil.getRemoteUser());
-			ApplicationParameter aux1 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_SERVER_PARAM);
-			setServer((aux1 != null && !aux1.getValue().equals("Null")) ? aux1.getValue() : "");			
-			ApplicationParameter aux2 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_SERVER_ACK_PARAM);
-			setServerAck((aux2 != null && !aux2.getValue().equals("Null")) ? aux2.getValue() : "");
-			ApplicationParameter aux3 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_USER);
-			setUser((aux3 != null && !aux3.getValue().equals("Null")) ? aux3.getValue() : "");
-			
-			ApplicationParameter aux4 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_KEY_A);
-			setKeyA((aux4 != null && !aux4.getValue().equals("Null")) ? aux4.getValue() : "");
-			ApplicationParameter aux5 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_KEY_B);
-			setKeyB((aux5 != null && !aux5.getValue().equals("Null")) ? aux5.getValue() : "");
 
-		}finally {
+			ApplicationParameter aux1 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_SERVER_PARAM);
+			setServer((aux1 != null && aux1.getValue() != null) ? aux1.getValue() : "");
+			ApplicationParameter aux2 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_SERVER_ACK_PARAM);
+			setServerAck((aux2 != null && aux2.getValue() != null) ? aux2.getValue() : "");
+			ApplicationParameter aux3 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_USER);
+			setUser((aux3 != null && aux3.getValue() != null) ? aux3.getValue() : "");
+			ApplicationParameter aux4 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_KEY_A);
+			setKeyA((aux4 != null && aux4.getValue() != null) ? aux4.getValue() : "");
+			ApplicationParameter aux5 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_KEY_B);
+			setKeyB((aux5 != null && aux5.getValue() != null) ? aux5.getValue() : "");
+			ApplicationParameter aux6 = AppParamDAO.fetchOne(ctx, AppParam.PMS_CONEXFLOW_PAY_METHOD);
+			setPayMethod((aux6 != null && aux6.getValue() != null) ? (PayMethod)BeanManager.getManagerBean(PayMethod.class).get(Integer.parseInt(aux6.getValue())) : null);
+		} catch (ManagerBeanException ex) {
+			setPayMethod(null);
+		} finally {
 			if (ctx != null) ctx.close();
 		}
 	}
-	
-	
+
 	public void onSaveConexFlow(ActionEvent event){
 		AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_SERVER_PARAM, getServer());
 		AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_SERVER_ACK_PARAM, getServerAck());
 		AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_USER, getUser());
 		AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_KEY_A, getKeyA());
 		AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_KEY_B, getKeyB());
-
+		AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_PAY_METHOD, getPayMethod().getId());
 	}
-	
-	public void onChangeConexFlowStatus(ActionEvent event) {
 
-		if(isConexFlow()){
+	public void onChangeConexFlowStatus(ActionEvent event) {
+		if (isConexFlow()) {
 			onConexFlowShow();
 			setShowConexFlowWindow(true);
 		}
 		else{
-			String nullString = "Null";
-			AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_SERVER_PARAM, nullString);
-			AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_SERVER_ACK_PARAM, nullString);
-			AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_USER, nullString);
-			AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_KEY_A, nullString);
-			AppParamUtil.insertParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_KEY_B, nullString);
+			AppParamUtil.removeParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_SERVER_PARAM);
+			AppParamUtil.removeParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_SERVER_ACK_PARAM);
+			AppParamUtil.removeParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_USER);
+			AppParamUtil.removeParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_KEY_A);
+			AppParamUtil.removeParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_KEY_B);
+			AppParamUtil.removeParameter(com.code.aon.common.enumeration.AppParam.PMS_CONEXFLOW_PAY_METHOD);
 		}	
 	}
-	
+
 	public void onClose(ActionEvent event){
 		setShowConexFlowWindow(false);
 		setConexFlow(getConexFlowAux());
 	}
+
 }
