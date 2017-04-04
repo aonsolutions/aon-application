cdead322fa49a122a752aef1e595fd7d8b6291b7
EukeCastellano <ecastellano@aonsolutions.es>
2017-04-04 13:14:09 +0200

    [HFX] ERROR 202 - No se genera el fichero [Fixes #1280]

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/Mod202File.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/Mod202File.java
@@ -1,7 +1,11 @@
 package com.esferalia.aon.gwt.fiscal.server;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,7 +17,9 @@ import com.esferalia.aon.gwt.common.server.AonServletUtils;
 import com.esferalia.aon.occam.api.FISCAL;
 import com.esferalia.aon.occam.api.model.fiscal.Mod202;
 import com.esferalia.aon.occam.api.model.type.MimeType;
+import com.esferalia.aon.occam.server.fiscal.format.AonFiscalFileUtils;
 import com.esferalia.aon.occam.server.fiscal.format.mod202.Mod202Writer;
+import com.esferalia.aon.watson.server.io.AonIOUtils;
 
 @SuppressWarnings("serial")
 @WebServlet(name = "Mod202 File download", urlPatterns = { "/aon_gwt_fiscal/Model202File" })
@@ -22,32 +28,30 @@ public class Mod202File extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
 		try {
 			int id = Integer.parseInt(req.getParameter("mod202"));
 			String domainName = req.getParameter("domainName");
 			int domainId = Integer.parseInt(req.getParameter("domainId"));
-			Mod202 mod202 = FISCAL.getMod202(domainName, domainId,AonServletUtils.getLoggedUser(), id);
+			String user = AonServletUtils.getLoggedUser(); 
+			Mod202 mod202 = FISCAL.getMod202(domainName, domainId, user, id);
 			
-			String s = mod202.getName();
-		    StringBuilder sb = new StringBuilder();
-		    if(!Character.isJavaIdentifierStart(s.charAt(0))) {
-		        sb.append("_");
-		    }
-		    for (char c : s.toCharArray()) {
-		        if(Character.isJavaIdentifierPart(c)) {
-		            sb.append(c);
-		        }
-		    }		
-
-		    String fileName = "Mod202" 
-					+ "_" + mod202.getYear() 
-					+ "_" + sb.toString();
-			resp.setContentType(MimeType.TXT.getName());
-			resp.setHeader("Content-disposition", "attachment; filename=\"" + fileName + ".txt\";");
-
-			OutputStreamWriter writer = new OutputStreamWriter( resp.getOutputStream() , "ISO-8859-15" );
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			OutputStreamWriter wr = null;
+			try {
+				wr = new OutputStreamWriter(output,"ISO-8859-1");
+			} catch (UnsupportedEncodingException e) {
+				wr = new OutputStreamWriter(output);
+			}
+			PrintWriter writer = new PrintWriter(wr);
 			Mod202Writer.fillWriter(mod202, writer);
+			ByteArrayInputStream in = new ByteArrayInputStream(output.toByteArray());
+
+		    String fileName = AonFiscalFileUtils.getFileName(mod202);
+		    MimeType mime = MimeType.TXT;
+		    resp.setCharacterEncoding("ISO-8859-1");
+			resp.setContentType(mime.getName());
+			resp.setHeader("Content-disposition", "attachment; filename=\"" + fileName + "." + mime.getExtension()+ "\";");
+			AonIOUtils.copy(in, resp.getOutputStream());
 			resp.flushBuffer();
 			
 		} catch (Throwable e) {
@@ -55,4 +59,5 @@ public class Mod202File extends HttpServlet {
 		}
 
 	}
+
 }
