fec58bdeb1c91fa1dd329ff640fdb346a7795b64
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-04-10 15:09:48 +0200

    GESTION. (ventas) impedir elaborar o borrar pedidos en elaboracion

--- aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesControllerListener.java
+++ aon-ui-sales/src/main/java/com/code/aon/ui/sales/event/SalesControllerListener.java
@@ -134,7 +134,7 @@ public class SalesControllerListener extends ControllerAdapter implements ISales
 				f -> f.getSourceProperty().eq(ElaborationSource.SALES.value())
 						.and(f.getSourceIdProperty().in(ids)));
 		if(list!=null && list.size()>0){
-			throw new ControllerListenerException("No se puede borrar, hay productos que están en elaboración.");
+			throw new ControllerListenerException("No se puede borrar, hay productos que están en proceso de elaboración.");
 		}
 	}
 
