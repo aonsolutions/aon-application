257226537215a835831b88240d2c31516e272394
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2017-05-10 12:19:00 +0200

    Creacion y gestion tabla (Gestor Incidencias Empleado)

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeEventsDraft.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeEventsDraft.java
@@ -2,20 +2,32 @@ package com.esferalia.aon.gwt.payroll.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DoubleBox;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.OrderedMultiSelectionModel;
 
 public class EmployeeEventsDraft extends Composite {
 
 	private static EmployeeEventsDraftUiBinder uiBinder = GWT.create(EmployeeEventsDraftUiBinder.class);
 
 	interface EmployeeEventsDraftUiBinder extends UiBinder<Widget, EmployeeEventsDraft> {
+	}
+	
+	@UiField
+	MyStyle style;
+
+	interface MyStyle extends CssResource {
+		String cellFormat();
+		String cellOddFormat();
+		String isSelectedCell();
 	}
 	
 	@UiField
@@ -30,11 +42,11 @@ public class EmployeeEventsDraft extends Composite {
 	@UiField
 	Grid eventsGrid;
 	
+	private OrderedMultiSelectionModel<Integer> selectedPositions = new OrderedMultiSelectionModel<Integer>();
 	
 	public EmployeeEventsDraft() {
 		//Inicializamos la vista del gestor de incidencias
 		initWidget(uiBinder.createAndBindUi(this));
-		
 		fillCellsEvents();
 	}
 
@@ -52,6 +64,48 @@ public class EmployeeEventsDraft extends Composite {
 		changeYear(1);
 	}
 	
+	@UiHandler("eventsGrid")
+	public void onEventsGridClick(ClickEvent event) {
+		
+		event.preventDefault();
+		
+		int row = eventsGrid.getCellForEvent(event).getRowIndex();
+		int col = eventsGrid.getCellForEvent(event).getCellIndex();
+		
+		if (col != 0){
+			if(event.isControlKeyDown()){ //Cambiar por CTRL
+				Integer previusPos = -1;
+				
+				if(!selectedPositions.getSelectedList().isEmpty()){
+					previusPos = selectedPositions.getSelectedList().get(0);
+					int previusRow = calculateRow(previusPos);
+					if (row == previusRow)
+						selectPosition(row,col);
+				}
+			}else{
+				//Borrar selecciones anteriores
+				eraseSelectedPositions();
+				selectPosition(row,col);
+			}
+			
+		}else{
+			//Borrar selecciones anteriores
+			eraseSelectedPositions();
+			
+			//Aplicar estilo seleccionado a una fila entera
+			for (int j = 1; j < 13; j++){
+				//Guardar en SelectionModel
+				int selectPos = (row * 13) + j;
+				selectedPositions.setSelected(selectPos, true);
+				
+				//Aplicar estilo seleccion a posicion
+				eventsGrid.getWidget(row, j).setStyleName(style.isSelectedCell());
+			
+			}
+		}
+		
+	}
+
 
 	/**
 	 * METEDOS AUXILIARES
@@ -60,8 +114,12 @@ public class EmployeeEventsDraft extends Composite {
 	private void fillCellsEvents() {
 		for (int row = 1; row < 17; row ++)
 			for (int col = 1; col < 13; col ++){
-				Label labelEvent = new Label("333.333");
-				eventsGrid.setWidget(row, col, labelEvent);
+				DoubleBox eventValue = new DoubleBox();
+				eventValue.setStyleName(style.cellFormat());
+				if (row % 2 == 1)
+					eventValue.setStyleName(style.cellOddFormat());
+				eventValue.setValue(row*col*1.00);
+				eventsGrid.setWidget(row, col, eventValue);
 			}	
 	}
 	
@@ -69,6 +127,40 @@ public class EmployeeEventsDraft extends Composite {
 		int actualYear = Integer.parseInt(yearLabel.getText());
 		int newYear = actualYear + change;
 		yearLabel.setText(Integer.toString(newYear));
+	}
+	
+	private void selectPosition(int row, int col) {
+		//Guardar en SelectionModel
+		int selectPos = (row * 13) + col;
+		selectedPositions.setSelected(selectPos, true);
+		
+		//Aplicar estilo seleccion a posicion
+		eventsGrid.getWidget(row, col).setStyleName(style.isSelectedCell());	
+	}
+	
+	private void eraseSelectedPositions() {
+		for (Integer position : selectedPositions.getSelectedList()){
+			int row = calculateRow(position);
+			int col = calculateCol(position);
+			
+			//Borrar estilos
+			eventsGrid.getWidget(row, col).removeStyleName(style.isSelectedCell());
+			
+			//Aplicar estilo base
+			eventsGrid.getWidget(row, col).setStyleName(style.cellFormat());
+			if (row % 2 == 1)
+				eventsGrid.getWidget(row, col).setStyleName(style.cellOddFormat());
+			
+		}
+		selectedPositions.clear();		
+	}
+
+	private int calculateCol(Integer position) {
+		return position % 13;
+	}
+
+	private int calculateRow(Integer position) {
+		return position / 13;
 	}
 	
 }
