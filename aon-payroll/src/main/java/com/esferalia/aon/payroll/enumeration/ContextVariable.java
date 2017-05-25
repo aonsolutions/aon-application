a776f52443d529df3764014d4872e11bd6734e4b
Raúl Trepiana <rtrepiana@gmail.com>
2017-05-25 16:39:17 +0200

    New functions 'FRACCIONAR' for dummies.

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/enumeration/ContextVariable.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/enumeration/ContextVariable.java
@@ -285,6 +285,7 @@ public enum ContextVariable implements IResourceable{
 	public static final String CHECK_VAR = "CHECK_VAR";
 	public static final String REDEFINE = "REDEFINE";
 	public static final String PRORATION = "PRORRATEAR";
+	public static final String FRACTIONATE = "FRACCIONAR";
 	
 	public static final String OLDD = "ANTIGUEDAD";
 	public static final String PREST_IT = "PREST_IT";
