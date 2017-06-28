ad6a393848962f402eef32b73e8c2628a6620377
EukeCastellano <ecastellano@aonsolutions.es>
2017-06-28 18:48:53 +0200

    Mod 200. PADIS hasta [00592] "Cuota íntegra ajustada positiva"

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/mod200_2016/Mod2002016BN571Key.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/mod200_2016/Mod2002016BN571Key.java
@@ -16,7 +16,7 @@ public enum Mod2002016BN571Key implements Serializable, IMod200KeysProvider {
 	 C01(new Mod2002016Key[]{Mod2002016Key.BN1054,Mod2002016Key.BN1050,Mod2002016Key.BN1051,Mod2002016Key.BN1052,Mod2002016Key.BN1053},"DI internacional 2015")
 	,C02(new Mod2002016Key[]{Mod2002016Key.BN1348,Mod2002016Key.BN1349,Mod2002016Key.BN1350,Mod2002016Key.BN1351,Mod2002016Key.BN1352},"DI internacional 2016(*)")
 	,C03(new Mod2002016Key[]{Mod2002016Key.BN131 ,null 				  ,Mod2002016Key.BN132 ,null /* BN571 */    ,Mod2002016Key.BN133 },"Total")
-	,C04(new Mod2002016Key[]{null  				 ,Mod2002016Key.BN103 ,null  			   ,null  				,null  				 },"Tipo de gravamen 2016")
+	,C04(new Mod2002016Key[]{null  				 ,Mod2002016Key.BN103D,null  			   ,null  				,null  				 },"Tipo de gravamen 2016")
 	;
 	 
     private String description;
