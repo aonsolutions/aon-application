72756ede3810c33801d321c597df86295d6e47b4
EukeCastellano <ecastellano@aonsolutions.es>
2017-06-30 10:51:06 +0200

    Mod 200. Deshabilitar casillas Pagina 16.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod200/e2016/Page16.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod200/e2016/Page16.java
@@ -1,11 +1,7 @@
 package com.esferalia.aon.gwt.fiscal.client.mod200.e2016;
 
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
-
 import com.esferalia.aon.gwt.common.client.AON;
 import com.esferalia.aon.gwt.fiscal.client.mod200.e2016.Model2002016.Model200PageCallback;
-import com.esferalia.aon.occam.api.model.fiscal.mod200_2016.Mod2002016Behaviour;
 import com.esferalia.aon.occam.api.model.fiscal.mod200_2016.Mod2002016Constants;
 import com.esferalia.aon.occam.api.model.fiscal.mod200_2016.Mod2002016Key;
 import com.google.gwt.user.client.ui.FlexTable;
@@ -62,18 +58,6 @@ public class Page16 extends PageAbs {
 		addHeaderCell(table,row, 3,AON.MSG.canCol3());
 		addHeaderCell(table,row, 4,AON.MSG.canCol4());
 		
-		// Desactivar las casillas 20 y 21 si no está marcado el caracter 45
-		if (callback.getMod200Object().getMod200().isChecked(Mod2002016Key.C0045))
-		{
-			Mod2002016Behaviour.BEHAVIOUR_KEYS_MAP.put(Mod2002016Key.RC020,new Boolean[]{FALSE,FALSE});
-			Mod2002016Behaviour.BEHAVIOUR_KEYS_MAP.put(Mod2002016Key.RC021,new Boolean[]{FALSE,FALSE});
-		}
-		else
-		{
-			Mod2002016Behaviour.BEHAVIOUR_KEYS_MAP.put(Mod2002016Key.RC020,new Boolean[]{FALSE,TRUE});
-			Mod2002016Behaviour.BEHAVIOUR_KEYS_MAP.put(Mod2002016Key.RC021,new Boolean[]{FALSE,TRUE});
-		}	
-		
 		++row;
 		for (int i = 0; i < Mod2002016Constants.CANARIAS_KEYS.length; i++) {
 			Mod2002016Key[] keys = Mod2002016Constants.CANARIAS_KEYS[i];
@@ -106,4 +90,13 @@ public class Page16 extends PageAbs {
   		  && (callback.getMod200Object().getMod200().isChecked(Mod2002016Key.C0029));
 		return av;
 	}
+	
+	@Override
+	protected boolean isDisabled(Mod2002016Key key) {
+		if (key == Mod2002016Key.RC020 || key == Mod2002016Key.RC021)  {
+			return callback.getMod200Object().getMod200().isNotChecked(Mod2002016Key.C0045);
+		}
+		return super.isDisabled(key);
+	}
+	
 }
