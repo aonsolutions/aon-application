3cdf1676d07abb9a57064071f0d884743616b32f
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-07-05 15:03:59 +0200

    ALMACEN. (elaboraciones) correccion del filtro y del scroll

--- aon-gwt-warehouse/src/main/java/net/aonsolutions/aon/gwt/warehouse/client/elaboration/Elaboration.java
+++ aon-gwt-warehouse/src/main/java/net/aonsolutions/aon/gwt/warehouse/client/elaboration/Elaboration.java
@@ -34,7 +34,7 @@ public class Elaboration extends AonTemplate2 {
 //	}
 
 	public Elaboration(AonData aonData) {
-//		filterMap = new HashMap<>();
+		filterMap = new HashMap<>();
 		this.API = new API(GWT.getModuleBaseURL(), aonData.getMd5(), aonData.getDomain().getName(),
 				aonData.getUser().getLogin());
 	}
