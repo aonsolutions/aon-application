f9bc22e1a0bf5d903cfc627abf5c7a15f09bc928
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2017-07-21 10:14:23 +0200

    Actualizacion Test (Gestor Incidencias Empleado)

--- aon-htmlunit/src/test/java/com/esferalia/aon/htmlunit/payroll/GeneralIntegralTest.java
+++ aon-htmlunit/src/test/java/com/esferalia/aon/htmlunit/payroll/GeneralIntegralTest.java
@@ -754,7 +754,7 @@ public class GeneralIntegralTest extends BaseIntegralTestCase {
 	public void TestComplementoIncidencias() throws Exception {
 
 		open("incidencias");
-		wait4Id("COMPLEMENTO_I, INCIDENCIA");
+		wait4Id("COMPLEMENTO_I,_INCIDENCIA");
 		
 	}
 	
