926071182b1defc79885d2424adc58fa186b94c6
aibanez <aibanez@aonsolutions.es>
2017-07-24 15:44:40 +0200

    SII. error en cuota deducible redondeo.

--- aon-sii/src/main/java/net/aonsolutions/aon/sii/SIIBuilt.java
+++ aon-sii/src/main/java/net/aonsolutions/aon/sii/SIIBuilt.java
@@ -763,7 +763,7 @@ public class SIIBuilt {
 			}			
 
 			// CUOTA DEDUCIBLE
-			frt.setCuotaDeducible(contextList.stream().filter(a -> a.getInvoice().equals(invoice)).mapToDouble(a -> a.getDeductibleQuota()).sum() + ""); // TODO
+			frt.setCuotaDeducible(AonMathUtils.round(contextList.stream().filter(a -> a.getInvoice().equals(invoice)).mapToDouble(a -> a.getDeductibleQuota()).sum()) + ""); // TODO
 
 			// DESCRIPCION OPERACION
 			AccountingInvoice ai = ACCOUNTING.getAccountingInvoiceFromInvoice(domain.getName(), domain.getId(), login, invoice);
