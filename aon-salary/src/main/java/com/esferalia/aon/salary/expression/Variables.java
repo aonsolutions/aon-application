c8d7e8be5f525a98e16faf2c4cbc78a12405603e
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2017-08-02 11:28:47 +0200

    Arreglado fallo que devolvia mal las ITimedVaribales en el metodo getContext (Gestor Incidencias Empleado)

--- aon-salary/src/main/java/com/esferalia/aon/salary/expression/Variables.java
+++ aon-salary/src/main/java/com/esferalia/aon/salary/expression/Variables.java
@@ -506,8 +506,14 @@ public class Variables implements Comparator<ITimedVariable<?>> {
 			Period intersect = var.getPeriod().intersect(p);
 			if (intersect == null)
 				continue;
+			try {
+				var.getValue(intersect);
+				ret.add(new WrapTimedVariable<T>(intersect, (ITimedVariable<T>) var));
+			} catch (Exception e) {
+				continue;
+			}
 
-			ret.add(new WrapTimedVariable<T>(intersect, (ITimedVariable<T>) var));
+//			ret.add(new WrapTimedVariable<T>(intersect, (ITimedVariable<T>) var));
 		}
 
 		return ret;
