efed56beb1f237fa9a7313374368f1075ec81598
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-08-25 15:22:40 +0200

    CONFIG. (direcciones) navegar al mapa de la direccion

--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressController.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressController.java
@@ -1,5 +1,6 @@
 package com.code.aon.ui.registry.controller;
 
+import java.net.IDN;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -11,6 +12,7 @@ import javax.faces.model.SelectItem;
 import org.apache.commons.lang.StringUtils;
 
 import com.code.aon.AonVersion;
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.registry.RegistryAddress;
 import com.code.aon.ui.common.ICommonConstants;
 import com.code.aon.ui.form.LinesController;
@@ -43,6 +45,28 @@ public class RegistryAddressController extends LinesController {
 		if(address!=null && address.getZip()!=null) {
 			address.loadGeoZoneByZip();
 		}
+	}
+	
+	public String getCurrentURL() throws ManagerBeanException {
+		if ( getModel().isRowAvailable() ) {
+			RegistryAddress ra = (RegistryAddress) getSelectedTO();
+			String value = IDN.toASCII("http://www.google.es/maps/place/");
+			value += IDN.toASCII(ra.getAddress().toLowerCase().replaceAll("s/n", ""));
+			if(ra.getNumber()!=null && !"".equals(ra.getNumber())){
+				value += IDN.toASCII(",+"+ra.getNumber());
+			}
+			if(ra.getZip()!=null && !"".equals(ra.getZip())){
+				value += IDN.toASCII(",+"+ra.getZip());
+			}
+			if(ra.getCity()!=null && !"".equals(ra.getCity())){
+				value += IDN.toASCII(",+"+ra.getCity());
+			}
+			if(ra.getGeozone()!=null && ra.getGeozone().getId()!=null){
+				value += IDN.toASCII(",+"+ra.getGeozone().getName());
+			}
+			return value;
+		}
+		return null;
 	}
 	
 
--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressLinesController.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressLinesController.java
@@ -1,8 +1,11 @@
 package com.code.aon.ui.registry.controller;
 
+import java.net.IDN;
+
 import javax.faces.event.ActionEvent;
 
 import com.code.aon.AonVersion;
+import com.code.aon.common.ManagerBeanException;
 import com.code.aon.registry.RegistryAddress;
 import com.code.aon.ui.form.LinesController;
 
@@ -15,6 +18,28 @@ public class RegistryAddressLinesController extends LinesController {
 		if(address!=null && address.getZip()!=null) {
 			address.loadGeoZoneByZip();
 		}
+	}
+	
+	public String getCurrentURL() throws ManagerBeanException {
+		if ( getModel().isRowAvailable() ) {
+			RegistryAddress ra = (RegistryAddress) getSelectedTO();
+			String value = IDN.toASCII("http://www.google.es/maps/place/");
+			value += IDN.toASCII(ra.getAddress().toLowerCase().replaceAll("s/n", "").trim());
+			if(ra.getNumber()!=null && !"".equals(ra.getNumber())){
+				value += IDN.toASCII(",+"+ra.getNumber());
+			}
+			if(ra.getZip()!=null && !"".equals(ra.getZip())){
+				value += IDN.toASCII(",+"+ra.getZip());
+			}
+			if(ra.getCity()!=null && !"".equals(ra.getCity())){
+				value += IDN.toASCII(",+"+ra.getCity());
+			}
+			if(ra.getGeozone()!=null && ra.getGeozone().getId()!=null){
+				value += IDN.toASCII(",+"+ra.getGeozone().getName());
+			}
+			return value;
+		}
+		return null;
 	}
 
 }
