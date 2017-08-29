542a8572bd191250e706d4e25e5d6ca2858f455d
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-08-29 13:18:01 +0200

    CONFIG. (direcciones) cambios en la url del mapa de la direccion

--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressController.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressController.java
@@ -1,23 +1,19 @@
 package com.code.aon.ui.registry.controller;
 
-import java.net.IDN;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.TreeSet;
 
-import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
 import org.apache.commons.lang.StringUtils;
 
 import com.code.aon.AonVersion;
-import com.code.aon.common.ManagerBeanException;
 import com.code.aon.registry.RegistryAddress;
 import com.code.aon.ui.common.ICommonConstants;
-import com.code.aon.ui.form.LinesController;
 
-public class RegistryAddressController extends LinesController {
+public class RegistryAddressController extends RegistryAddressLinesController {
 	
 	private static final long serialVersionUID = AonVersion.SERIAL_VERSION_UID;
 
@@ -39,35 +35,5 @@ public class RegistryAddressController extends LinesController {
 		}
 		return municipalities;
 	}
-	
-	public void onLoadGeozone(ActionEvent event){
-		RegistryAddress address = (RegistryAddress) getTo();
-		if(address!=null && address.getZip()!=null) {
-			address.loadGeoZoneByZip();
-		}
-	}
-	
-	public String getCurrentURL() throws ManagerBeanException {
-		if ( getModel().isRowAvailable() ) {
-			RegistryAddress ra = (RegistryAddress) getSelectedTO();
-			String value = IDN.toASCII("http://www.google.es/maps/place/");
-			value += IDN.toASCII(ra.getAddress().toLowerCase().replaceAll("s/n", ""));
-			if(ra.getNumber()!=null && !"".equals(ra.getNumber())){
-				value += IDN.toASCII(",+"+ra.getNumber());
-			}
-			if(ra.getZip()!=null && !"".equals(ra.getZip())){
-				value += IDN.toASCII(",+"+ra.getZip());
-			}
-			if(ra.getCity()!=null && !"".equals(ra.getCity())){
-				value += IDN.toASCII(",+"+ra.getCity());
-			}
-			if(ra.getGeozone()!=null && ra.getGeozone().getId()!=null){
-				value += IDN.toASCII(",+"+ra.getGeozone().getName());
-			}
-			return value;
-		}
-		return null;
-	}
-	
 
 }
