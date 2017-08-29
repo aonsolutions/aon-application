19c6fe6bd116e015d1dd404c3666bace9593b9d6
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2017-08-29 13:18:01 +0200

    CONFIG. (direcciones) cambios en la url del mapa de la direccion

--- aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressLinesController.java
+++ aon-ui-registry/src/main/java/com/code/aon/ui/registry/controller/RegistryAddressLinesController.java
@@ -1,6 +1,9 @@
 package com.code.aon.ui.registry.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.net.IDN;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import javax.faces.event.ActionEvent;
 
@@ -12,6 +15,8 @@ import com.code.aon.ui.form.LinesController;
 public class RegistryAddressLinesController extends LinesController {
 	
 	private static final long serialVersionUID = AonVersion.SERIAL_VERSION_UID;
+	
+	public final static String GOOGLE_MAPS_SEARCH_URL = "http://www.google.com/maps/search/";
 
 	public void onLoadGeozone(ActionEvent event){
 		RegistryAddress address = (RegistryAddress) getTo();
@@ -19,25 +24,26 @@ public class RegistryAddressLinesController extends LinesController {
 			address.loadGeoZoneByZip();
 		}
 	}
-	
-	public String getCurrentURL() throws ManagerBeanException {
+		
+	public String getGoogleMapsURL() throws ManagerBeanException {
 		if ( getModel().isRowAvailable() ) {
 			RegistryAddress ra = (RegistryAddress) getSelectedTO();
-			String value = IDN.toASCII("http://www.google.es/maps/place/");
-			value += IDN.toASCII(ra.getAddress().toLowerCase().replaceAll("s/n", "").trim());
-			if(ra.getNumber()!=null && !"".equals(ra.getNumber())){
-				value += IDN.toASCII(",+"+ra.getNumber());
+			String params = "";
+			try {
+				params = ra.getAddress().trim();
+				if(ra.getNumber()!=null && !"".equals(ra.getNumber()))
+					params += " " + ra.getNumber().trim();
+				if(ra.getZip()!=null && !"".equals(ra.getZip()))
+					params += " " + ra.getZip().trim();
+				if(ra.getCity()!=null && !"".equals(ra.getCity()))
+					params += " " + ra.getCity().trim();
+				if(ra.getGeozone()!=null && ra.getGeozone().getId()!=null)
+					params += " " + ra.getGeozone().getName();
+				params = URLEncoder.encode(params, StandardCharsets.UTF_8.name());
+			} catch (UnsupportedEncodingException e) {
+				params = "";
 			}
-			if(ra.getZip()!=null && !"".equals(ra.getZip())){
-				value += IDN.toASCII(",+"+ra.getZip());
-			}
-			if(ra.getCity()!=null && !"".equals(ra.getCity())){
-				value += IDN.toASCII(",+"+ra.getCity());
-			}
-			if(ra.getGeozone()!=null && ra.getGeozone().getId()!=null){
-				value += IDN.toASCII(",+"+ra.getGeozone().getName());
-			}
-			return value;
+			return IDN.toASCII( GOOGLE_MAPS_SEARCH_URL ) + params;
 		}
 		return null;
 	}
