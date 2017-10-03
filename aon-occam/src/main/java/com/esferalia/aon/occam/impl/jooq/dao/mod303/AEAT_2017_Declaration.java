732d18a39ce8d049954791098f96c5d13155b708
EukeCastellano <ecastellano@aonsolutions.es>
2017-10-03 17:59:11 +0200

    [HFX] Nuevo 303 - cuotas a compensar con declaraciones importadas [Fixes #1536]

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/mod303/AEAT_2017_Declaration.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/mod303/AEAT_2017_Declaration.java
@@ -1572,7 +1572,7 @@ public class AEAT_2017_Declaration extends Mod303Declaration {
 				add( Mod303Key.CT_C67, mod, 
 					Mod303DAO.getLastPeriodModels(ctx, mod)
 					.filter(fm -> AonObjectUtils.equals( fm.getDescription(Mod303Key.CM_004),FiscalModelDeclarationType.COMPENSATE.getValue()))
-					.mapToDouble(fm -> fm.getAmount(Mod303Key.CT_C71))
+					.mapToDouble(fm -> AonMathUtils.round(fm.getAmount(Mod303Key.CT_C71) * (-1)))
 					.findFirst()
 					.orElse(0.0));						
 			}
