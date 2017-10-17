5ef4d3c0827d12d3f65896b4398e1884870ae633
EukeCastellano <ecastellano@aonsolutions.es>
2017-10-17 17:06:04 +0200

    Nuevo modelo 303 - importes diferencias con complementarias [Fixes #1547]

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/mod303/Model3032017ARABAScript2.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/fiscal/mod303/Model3032017ARABAScript2.java
@@ -6,32 +6,36 @@ import static com.esferalia.aon.occam.api.model.type.FiscalModelKeyInfo.INVOICE;
 import static com.esferalia.aon.occam.api.model.type.FiscalModelKeyInfo.TITLE;
 
 import com.esferalia.aon.occam.api.model.fiscal.IModelScript;
+import com.esferalia.aon.occam.api.model.fiscal.KeyTypes;
 import com.esferalia.aon.occam.api.model.type.FiscalModelKeyInfo;
 import com.esferalia.aon.occam.api.model.type.Mod303Key;
 
 public enum Model3032017ARABAScript2 implements IModelScript<Mod303Key> {
 	
-	 DED01 ("IVA deducible"						,null								,TITLE)
-	,DED02 (Mod303Key.AR_C030.getDescription()	,new Mod303Key[]{Mod303Key.AR_C030}	,INVOICE,DIFF_INVOICE)
-	,DED03 (Mod303Key.AR_C031.getDescription()	,new Mod303Key[]{Mod303Key.AR_C031}	,INVOICE,DIFF_INVOICE)
-	,DED04 (Mod303Key.AR_C032.getDescription()	,new Mod303Key[]{Mod303Key.AR_C032}	,INVOICE,DIFF_INVOICE)
-	,DED05 (Mod303Key.AR_C033.getDescription()	,new Mod303Key[]{Mod303Key.AR_C033}	,INVOICE,DIFF_INVOICE)
-	,DED06 (Mod303Key.AR_C034.getDescription()	,new Mod303Key[]{Mod303Key.AR_C034}	,INVOICE,DIFF_INVOICE)
-	,DED07 (Mod303Key.AR_C035.getDescription()	,new Mod303Key[]{Mod303Key.AR_C035}	,INVOICE,DIFF_INVOICE)
-	,DED08 (Mod303Key.AR_C036.getDescription()	,new Mod303Key[]{Mod303Key.AR_C036}	,INVOICE,DIFF_INVOICE)
-	,DED09 (Mod303Key.AR_C037.getDescription()	,new Mod303Key[]{Mod303Key.AR_C037}	,INVOICE,DIFF_INVOICE)
-	,DED10 (Mod303Key.AR_C038.getDescription()	,new Mod303Key[]{Mod303Key.AR_C038}	,COMPUTE)
-	,DED11 (Mod303Key.AR_C039.getDescription()	,new Mod303Key[]{Mod303Key.AR_C039}	,COMPUTE)
+	 DED01 ("IVA deducible"						,null								,null,TITLE)
+	,DED02 (Mod303Key.AR_C030.getDescription()	,new Mod303Key[]{Mod303Key.AR_C030}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	,DED03 (Mod303Key.AR_C031.getDescription()	,new Mod303Key[]{Mod303Key.AR_C031}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	,DED04 (Mod303Key.AR_C032.getDescription()	,new Mod303Key[]{Mod303Key.AR_C032}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	,DED05 (Mod303Key.AR_C033.getDescription()	,new Mod303Key[]{Mod303Key.AR_C033}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	,DED06 (Mod303Key.AR_C034.getDescription()	,new Mod303Key[]{Mod303Key.AR_C034}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	,DED07 (Mod303Key.AR_C035.getDescription()	,new Mod303Key[]{Mod303Key.AR_C035}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	,DED08 (Mod303Key.AR_C036.getDescription()	,new Mod303Key[]{Mod303Key.AR_C036}	,new KeyTypes[]{KeyTypes.DEDUCTIBLE_QUOTA},INVOICE,DIFF_INVOICE)
+	
+	,DED09 (Mod303Key.AR_C037.getDescription()	,new Mod303Key[]{Mod303Key.AR_C037}	,null,INVOICE,DIFF_INVOICE)
+	,DED10 (Mod303Key.AR_C038.getDescription()	,new Mod303Key[]{Mod303Key.AR_C038}	,null,COMPUTE)
+	,DED11 (Mod303Key.AR_C039.getDescription()	,new Mod303Key[]{Mod303Key.AR_C039}	,null,COMPUTE)
 	;
 	
 	private String label;
 	private Mod303Key[] keys;
 	private FiscalModelKeyInfo[] infoKeys;
+	private KeyTypes[] keyTypes;
 	
-	private Model3032017ARABAScript2(String label, Mod303Key[] keys,FiscalModelKeyInfo ... infoKeys) {
+	private Model3032017ARABAScript2(String label, Mod303Key[] keys,KeyTypes[] keyTypes,FiscalModelKeyInfo ... infoKeys) {
 		this.label = label;
 		this.keys = keys;
 		this.infoKeys = infoKeys;
+		this.keyTypes = keyTypes;  
 	}
 
 	@Override
@@ -64,5 +68,10 @@ public enum Model3032017ARABAScript2 implements IModelScript<Mod303Key> {
 	public boolean paintHeaderBefore() {
 		return false;
 	};
+	
+	@Override
+	public KeyTypes[] getKeyTypes() {
+		return this.keyTypes;
+	}
 	
 }
