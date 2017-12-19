6d1b9f9509bb73d0f4eb1b9da4ce8e14fc6fad3a
Sergio Valdepeñas <sergio@MacBook-Pro-de-Sergio.local>
2017-12-19 10:46:57 +0100

    Cambio en el proyecto aon-file-payroll por el cambio en la tabla Contract

--- aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/contract/pdf/model/LearningModel.java
+++ aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/contract/pdf/model/LearningModel.java
@@ -286,11 +286,12 @@ public class LearningModel extends AbstractContractModel {
 			} else {
 				if(contrata!=null && contrata.getCno()!=null ){
 					setPdfFieldValue(PdfFieldLearning.ACTIVITY_EMPLOYEE_PROFFESION.getValue(), contrata.getCno().getTitle());
-					if(contract.getAgreementLevelCategory()!=null && contract.getAgreementLevelCategory().getId()!=null){
-						setPdfFieldValue(PdfFieldLearning.ACTIVITY_EMPLOYEE_CATEGORY.getValue(), contract.getAgreementLevelCategory().getDescription());
-					} else {
-						setPdfFieldValue(PdfFieldLearning.ACTIVITY_EMPLOYEE_CATEGORY.getValue(), contract.getCategoryDescription());
-					}
+					setPdfFieldValue(PdfFieldLearning.ACTIVITY_EMPLOYEE_CATEGORY.getValue(), contract.getCategoryDescription());
+//					if(contract.getAgreementLevelCategory()!=null && contract.getAgreementLevelCategory().getId()!=null){
+//						setPdfFieldValue(PdfFieldLearning.ACTIVITY_EMPLOYEE_CATEGORY.getValue(), contract.getAgreementLevelCategory().getDescription());
+//					} else {
+//						setPdfFieldValue(PdfFieldLearning.ACTIVITY_EMPLOYEE_CATEGORY.getValue(), contract.getCategoryDescription());
+//					}
 					
 					String cno = contrata.getCno().getCode();
 					if( !StringUtils.isEmpty(cno) ){
