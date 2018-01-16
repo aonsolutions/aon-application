3fc0a9ab1058f0178e1752017b99724412a80761
EukeCastellano <ecastellano@aonsolutions.es>
2018-01-16 16:00:37 +0100

    error modelo 303 simplificado con epigrafe 722 [Fixes #1711]

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/mod303/AEAT_2017_Declaration.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/mod303/AEAT_2017_Declaration.java
@@ -687,7 +687,7 @@ public class AEAT_2017_Declaration extends Mod303Declaration {
 			,mod -> ensureActivity(mod,0).setIng(mod.isLastPeriod()?0.0:mod.getAmount(Mod303Key.CT_S121))
 			,true)
 		// (1) Actividades en régimen simplificado. 1% de la cuota devengada por operaciones corrientes
-		,CT_S12X(Mod303Key.CT_S12X,null,null,null,"isLastPeriod()?(CT_S117 * 1 / 100):0.0"
+		,CT_S12X(Mod303Key.CT_S12X,null,null,null,"isLastPeriod()?round(CT_S117 * 1 / 100):0.0"
 			,null
 			,mod -> mod.putAmount(Mod303Key.CT_S12X,ensureActivity(mod,0).getSopx())
 			,mod -> ensureActivity(mod,0).setSopx(mod.getAmount(Mod303Key.CT_S12X))
@@ -947,7 +947,7 @@ public class AEAT_2017_Declaration extends Mod303Declaration {
 			,mod -> ensureActivity(mod,1).setIng(mod.isLastPeriod()?0.0:mod.getAmount(Mod303Key.CT_S221))
 			,true)
 		// (1) Actividades en régimen simplificado. 1% de la cuota devengada por operaciones corrientes
-		,CT_S22X(Mod303Key.CT_S22X,null,null,null,"isLastPeriod()?(CT_S217 * 1 / 100):0.0"
+		,CT_S22X(Mod303Key.CT_S22X,null,null,null,"isLastPeriod()?round(CT_S217 * 1 / 100):0.0"
 			,null
 			,mod -> mod.putAmount(Mod303Key.CT_S22X,ensureActivity(mod,1).getSopx())
 			,mod -> ensureActivity(mod,1).setSopx(mod.getAmount(Mod303Key.CT_S22X))
@@ -1207,7 +1207,7 @@ public class AEAT_2017_Declaration extends Mod303Declaration {
 			,mod -> ensureActivity(mod,2).setIng(mod.getAmount(Mod303Key.CT_S321))
 			,true)
 		// (1) Actividades en régimen simplificado. 1% de la cuota devengada por operaciones corrientes
-		,CT_S32X(Mod303Key.CT_S32X,null,null,null,"isLastPeriod()?(CT_S317 * 1 / 100):0.0"
+		,CT_S32X(Mod303Key.CT_S32X,null,null,null,"isLastPeriod()?round(CT_S317 * 1 / 100):0.0"
 			,null
 			,mod -> mod.putAmount(Mod303Key.CT_S32X,ensureActivity(mod,2).getSopx())
 			,mod -> ensureActivity(mod,2).setSopx(mod.getAmount(Mod303Key.CT_S32X))
@@ -1467,7 +1467,7 @@ public class AEAT_2017_Declaration extends Mod303Declaration {
 			,mod -> ensureActivity(mod,3).setIng(mod.getAmount(Mod303Key.CT_S421))
 			,true)
 		// (1) Actividades en régimen simplificado. 1% de la cuota devengada por operaciones corrientes
-		,CT_S42X(Mod303Key.CT_S42X,null,null,null,"isLastPeriod()?(CT_S417 * 1 / 100):0.0"
+		,CT_S42X(Mod303Key.CT_S42X,null,null,null,"isLastPeriod()?round(CT_S417 * 1 / 100):0.0"
 			,null
 			,mod -> mod.putAmount(Mod303Key.CT_S42X,ensureActivity(mod,3).getSopx())
 			,mod -> ensureActivity(mod,3).setSopx(mod.getAmount(Mod303Key.CT_S42X))
