9c1ff9a35b990d534a1624ed920b209995ab5cb7
EukeCastellano <ecastellano@aonsolutions.es>
2018-02-07 16:29:30 +0100

    Mod347. Unknown ordinal value for enum class com.code.aon.fiscal.enumeration.Mod347Status [Fixes #1772]

--- aon.entity.common/src/main/java/com/code/aon/fiscal/enumeration/Mod347Status.java
+++ aon.entity.common/src/main/java/com/code/aon/fiscal/enumeration/Mod347Status.java
@@ -12,7 +12,12 @@ import com.code.aon.common.enumeration.IResourceable;
 public enum Mod347Status implements IResourceable {
 	
 	PENDING,
-	FINISHED;
+	FINISHED,
+	BATCHED, 
+	BLOCKED,
+	SENT,
+	MISSING
+	;
 	
     /** Message key prefix. */
     private static final String MSG_KEY_PREFIX = "aon_enum_mod347_status_";
--- aon.entity.common/src/main/java/com/code/aon/fiscal/enumeration/Mod349Status.java
+++ aon.entity.common/src/main/java/com/code/aon/fiscal/enumeration/Mod349Status.java
@@ -12,7 +12,12 @@ import com.code.aon.common.enumeration.IResourceable;
 public enum Mod349Status implements IResourceable {
 	
 	PENDING,
-	FINISHED;
+	FINISHED,
+	BATCHED, 
+	BLOCKED,
+	SENT,
+	MISSING
+	;
 	
     /** Message key prefix. */
     private static final String MSG_KEY_PREFIX = "aon_enum_mod349_status_";
