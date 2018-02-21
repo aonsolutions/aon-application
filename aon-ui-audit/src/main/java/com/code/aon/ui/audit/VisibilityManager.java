6217ef83d34c6441dd08211fa9099e8678ef70d0
girazu <girazu@archlinux>
2018-02-21 11:35:24 +0100

    Menu gestion duplicado, en caso de tener contratado el portal conecta (Fixes #1799)

--- aon-ui-audit/src/main/java/com/code/aon/ui/audit/VisibilityManager.java
+++ aon-ui-audit/src/main/java/com/code/aon/ui/audit/VisibilityManager.java
@@ -104,7 +104,7 @@ public class VisibilityManager extends BasicVisibilityManager {
 				enabledModules.remove(Module.PAYROLL);
 			}
 		}
-		if (parentDomainId == null || enabledModules.contains(Module.MANAGEMENT) || enabledModules.contains(Module.AON_ONE)) {
+		if (parentDomainId == null || enabledModules.contains(Module.MANAGEMENT) || enabledModules.contains(Module.AON_ONE) || enabledModules.contains(Module.AON_FINANCE)) {
 			enabledModules.remove(Module.FINANCE_PORTAL);
 		}
 		return enabledModules;
