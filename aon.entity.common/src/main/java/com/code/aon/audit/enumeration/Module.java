97031a0609aa6fee98bc9d59699633f27ee7cde7
girazu <girazu@archlinux>
2018-03-15 17:31:03 +0100

    MODULES. Marketing y Tesoreria Deprecados!

--- aon.entity.common/src/main/java/com/code/aon/audit/enumeration/Module.java
+++ aon.entity.common/src/main/java/com/code/aon/audit/enumeration/Module.java
@@ -15,76 +15,31 @@ import com.code.aon.common.enumeration.IResourceable;
  */
 public enum Module implements IResourceable {
 
-	/** MARKETING. */
+	@Deprecated
 	MARKETING( "marketing" ),
-    
-	/** CRM. */
 	CRM( "crm" ),
-    
-    /** MANAGEMENT. */
 	MANAGEMENT( "management" ),
-    
-    /** TREASURY. */
+	@Deprecated
 	TREASURY( "treasury" ),
-
-    /** WAREHOUSE. */
 	WAREHOUSE( "warehouse" ),
-
-    /** GROUPWARE. */
 	GROUPWARE( "groupware" ),
-
-    /** ACCOUNTING. */
 	ACCOUNTING( "accounting" ),
-
-    /** FISCAL. */
 	FISCAL( "fiscal" ),
-
-    /** PAYROLL. */
 	PAYROLL( "payroll" ),
-
-    /** DOCUMENT. */
 	DOCUMENT( "document" ),
-	
-    /** GARAGE. */
 	GARAGE( "garage" ),
-	
-    /** ACADEMY. */
 	ACADEMY( "academy" ),
-
-    /** HOTEL. */
 	HOTEL( "hotel" ),
-	
-    /** INFOWEB. */
 	INFOWEB( "infoweb" ),
-	
-    /** PAYROLL_PORTAL. */
 	PAYROLL_PORTAL( "payroll_portal" ),
-
-    /** DOCUMENT_PORTAL. */
 	DOCUMENT_PORTAL( "document_portal" ),
-	
-    /** POS. */
 	POS( "pos" ),
-	
-    /** CONTRAT@. */
 	CONTRATA( "contrata" ),
-
-    /** CONFIGURATION. */
 	CONFIGURATION( "configuration" ),
-
-    /** AON_ONE. */
 	AON_ONE( "aonOne" ),
-
-    /** ECOMMERCE. */
 	ECOMMERCE( "eCommerce" ),
-	
-    /** CALL_CENTER. */
 	CALL_CENTER( "call_center" ),
-	
-    /** FINANCE_PORTAL. */
 	FINANCE_PORTAL( "finance_portal" ),
-
-    /** AON_FINANCE. */
 	AON_FINANCE( "aonFinance" );
 
     /** Message key prefix. */
