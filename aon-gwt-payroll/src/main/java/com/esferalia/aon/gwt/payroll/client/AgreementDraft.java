65272c68c594ac385c85fe30ddded098a7845ec6
rtrepiana <rtrepiana@aonsolutions.es>
2018-04-11 08:44:57 +0200

    Revert "(PARCHE) Simular guardar cuando se crea un convenio desde cero"
    This reverts commit f5f6ca619bccdbcca72dc9f1205ee1f06da177f2.

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/AgreementDraft.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/AgreementDraft.java
@@ -826,7 +826,6 @@ public class AgreementDraft extends ResizeComposite implements CalculateCallback
 								agreementDraftObject.setStartDate(month);
 								agreementDraftObject.setEndDate(DateUtils.getLastDayOfMonth(month));
 								calculate();
-								agreementDraftObject.save(AgreementDraft.this);
 							}
 						};
 						
