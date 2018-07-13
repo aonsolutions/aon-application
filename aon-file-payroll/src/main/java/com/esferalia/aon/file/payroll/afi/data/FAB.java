fba96d73ecd12688f6c1a3d99361d1057c31cd50
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2018-07-13 13:45:23 +0200

    LABORAL. (afi) grado de discapacidad opcional

--- aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/afi/data/FAB.java
+++ aon-file-payroll/src/main/java/com/esferalia/aon/file/payroll/afi/data/FAB.java
@@ -10,7 +10,7 @@ public class FAB {
 	private Integer fechaReal;
 	private Integer grupoCotizacion;
 	private String indicativoGrupoCotizacionDiario;
-	private Integer gradoDiscapacidad;
+	private String gradoDiscapacidad;
 	private Integer claveContrato;
 	private String condicionDesempleado;
 	private String mujerSubrepresentada;
@@ -61,10 +61,10 @@ public class FAB {
 	public void setIndicativoGrupoCotizacionDiario(String indicativoGrupoCotizacionDiario) {
 		this.indicativoGrupoCotizacionDiario = indicativoGrupoCotizacionDiario;
 	}
-	public Integer getGradoDiscapacidad() {
+	public String getGradoDiscapacidad() {
 		return gradoDiscapacidad;
 	}
-	public void setGradoDiscapacidad(Integer gradoDiscapacidad) {
+	public void setGradoDiscapacidad(String gradoDiscapacidad) {
 		this.gradoDiscapacidad = gradoDiscapacidad;
 	}
 	public Integer getClaveContrato() {
