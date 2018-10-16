c6ef3ff256b42788680cd1147300c4682da38814
rtrepiana <rtrepiana@aonsolutions.es>
2018-10-16 09:16:28 +0200

    Sick of Hibernate

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/report/StatelessReportManager.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/report/StatelessReportManager.java
@@ -3,6 +3,7 @@ package com.esferalia.aon.gwt.payroll.report;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -10,6 +11,11 @@ import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
 
+import com.code.aon.common.ICollectionProvider;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.common.dao.sql.DAOException;
+import com.code.aon.ql.Criteria;
+import com.code.aon.report.IReportDynamicParamsProvider;
 import com.code.aon.report.OutputFormat;
 import com.code.aon.report.ReportException;
 import com.code.aon.report.config.ReportConfig;
@@ -18,6 +24,7 @@ import com.code.aon.report.config.ReportConfigurationParser;
 import com.code.aon.report.jr.JRReport;
 import com.code.aon.report.jr.JRReportFactory;
 import com.code.aon.ui.report.controller.ReportManager;
+import com.code.aon.ui.util.AonUtil;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperReport;
@@ -43,9 +50,94 @@ public class StatelessReportManager extends ReportManager {
 		ReportConfig config = rcm.getReport(reportKey);
 		JRReportFactory.register(reportKey, new StatelessJRReport(config, params));
 		
-		return super.execute(os, reportKey, params);
+		return executeImpl(os, reportKey, params);
 	}
 	
+	/**
+	 * Runs the report. Obtains a
+	 * <code>com.code.aon.ui.report.jr.JRReport</code> calling the
+	 * <code>JRReportFactory.getJRReport(getReportKey())</code> method. Also,
+	 * finalizes the reponse calling the
+	 * <code>FacesContext.getCurrentInstance().responseComplete()</code>.
+	 * 
+	 * @return The outcome (- null - because this method finalizes the reponse).
+	 * @throws ReportException
+	 *             If an error ocurred.
+	 * @throws DAOException
+	 */
+	@SuppressWarnings("unchecked")
+	public String executeImpl(OutputStream os, String reportKey, Map<Object,Object>... params) throws ReportException {
+
+		JRReport report = null;
+		try {
+			report = JRReportFactory.getJRReport(reportKey);
+			Criteria criteria = null;
+			Collection<?> collection = getCollection(report);
+
+			String dp = report.getReportConfig().getDynamicParamsProvider();
+			if (dp != null) {
+				IReportDynamicParamsProvider dpp = (IReportDynamicParamsProvider) AonUtil
+						.getRegisteredBean(dp);
+				Map<String, Object> dynParams = dpp.getDynamicParamsMap();
+				report.setDynamicParams(dynParams);
+			}
+
+			String out = report.run(getOutputFormat(), os, getBundle(), getLocale(), criteria, collection, params);
+			if (out == null) {
+				out = Integer.toString(report.getGeneratedPages());
+			}
+			return out;
+		} catch (Throwable t) {
+			t.printStackTrace();
+			AonUtil.addFatalMessage("Report Error:" + t.getMessage());
+			if (t instanceof ReportException) {
+				throw (ReportException) t;
+			}
+			throw new ReportException(t.getMessage(), t);
+		} finally {
+			if ( report != null ) {
+				report.setCustomParams(null);	
+			}			
+		}
+	}
+	
+	
+	/**
+	 * Obtains the <code>java.util.Collection</code> that will be passed to
+	 * method <code>com.code.aon.common.IFinderBean.getList()</code>.
+	 * Otherwise the litaral must be a valid ICollectionProvider implementation
+	 * class.
+	 * 
+	 * @param report
+	 *            The report to be executed.
+	 * @return The collection.
+	 * @throws ReportException
+	 *             If an error ocurred.
+	 */
+	private Collection<?> getCollection(JRReport report) throws ReportException {
+		Collection<?> collection = null;
+		ReportConfig config = report.getReportConfig();
+		String provider = config.getCollectionProvider();
+		ICollectionProvider collectionProvider = getCollectionProvider();
+		if ((collectionProvider == null) && (provider != null)) {
+			try {
+				Class<?> collectionProviderClass = Class.forName(provider);
+				collectionProvider = (ICollectionProvider) collectionProviderClass
+						.newInstance();
+			} catch (Throwable th) {
+				throw new ReportException(th.getMessage(), th);
+			}
+		}
+		if ( (collection == null) && (collectionProvider != null) ) {
+			try {
+				collection = collectionProvider.getCollection(config.isForceRefresh());
+			} catch (ManagerBeanException e) {
+				throw new ReportException(e.getMessage(), e);
+			}
+		}
+		return collection;
+	}
+
 	
 	private static class StatelessJRReport extends JRReport {
 
