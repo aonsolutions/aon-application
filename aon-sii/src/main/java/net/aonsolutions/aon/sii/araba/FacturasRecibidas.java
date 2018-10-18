4dff9453922c5405c6be2b8dadd46d72f64c3e54
anderibz <aibanez@aonsolutions.es>
2018-10-18 11:54:21 +0200

    SII. error fact. intracomunitaria.

--- aon-sii/src/main/java/net/aonsolutions/aon/sii/araba/FacturasRecibidas.java
+++ aon-sii/src/main/java/net/aonsolutions/aon/sii/araba/FacturasRecibidas.java
@@ -608,7 +608,7 @@ public class FacturasRecibidas extends SIIBuilt{
 			otro.setCodigoPais(CountryType2.valueOf(vat.getRegistryDocumentCountry().getIso2()));
 
 			String document = vat.getRegistryDocument();
-			if(!document.substring(0,2).equals(vat.getRegistryDocumentCountry().getIso2())) {
+			if(!document.substring(0,2).equalsIgnoreCase(vat.getRegistryDocumentCountry().getIso2())) {
 				document = vat.getRegistryDocumentCountry().getIso2() + document;
 			}
 			otro.setID(document);		
