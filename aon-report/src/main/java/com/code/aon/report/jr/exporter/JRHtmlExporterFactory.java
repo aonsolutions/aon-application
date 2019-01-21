b3b402e23d6b2736bc8735f331c803dc080a717c
rtrepiana <rtrepiana@aonsolutions.es>
2019-01-21 18:12:00 +0100

    Upgarde jasperreport to 6.7.0

--- aon-report/src/main/java/com/code/aon/report/jr/exporter/JRHtmlExporterFactory.java
+++ aon-report/src/main/java/com/code/aon/report/jr/exporter/JRHtmlExporterFactory.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import net.sf.jasperreports.engine.JRExporter;
 import net.sf.jasperreports.engine.JRExporterParameter;
-import net.sf.jasperreports.engine.export.JRHtmlExporter;
+import net.sf.jasperreports.engine.export.HtmlExporter;
 import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
 
 import com.code.aon.report.IReportConstants;
@@ -12,21 +12,21 @@ import com.code.aon.report.ReportException;
 
 /**
  * Returns an exporter for the HTML format.
- * 
+ *
  * @author Consulting & Development. Eugenio Castellano - 05-may-2005
  * @since 1.0
- * 
+ *
  */
 public class JRHtmlExporterFactory implements IJRExporterFactory {
 
 	@Override
 	public JRExporter getJRExporter() {
-		return new JRHtmlExporter();
+		return new HtmlExporter();
 	}
 
 	@Override
 	public void fillJRParametersMap(Map<String,Object> fillMap, Map<JRExporterParameter,Object> exporterMap) throws ReportException {
-		exporterMap.put(JRHtmlExporterParameter.IS_USING_IMAGES_TO_ALIGN,Boolean.FALSE);
+		//exporterMap.put(JRHtmlExporterParameter.IS_USING_IMAGES_TO_ALIGN,Boolean.FALSE);
 		exporterMap.put(JRHtmlExporterParameter.HTML_HEADER, getHtmlHeader());
 		exporterMap.put(JRHtmlExporterParameter.HTML_FOOTER, getHtmlFooter());
 		exporterMap.put(JRHtmlExporterParameter.BETWEEN_PAGES_HTML, "");
