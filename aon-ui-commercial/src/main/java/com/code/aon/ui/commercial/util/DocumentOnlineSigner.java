5d7565fb7c91d91c702cbf23d6b7facf106bef90
EkainAgirrezabal <eagirrezabal@aonsolutions.es>
2019-07-15 18:52:44 +0200

    EVICERTIA. solicitud de datos previo al envio

---
+++ aon-ui-commercial/src/main/java/com/code/aon/ui/commercial/util/DocumentOnlineSigner.java
@@ -0,0 +1,18 @@
+package com.code.aon.ui.commercial.util;
+
+import java.io.Serializable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.code.aon.AonVersion;
+
+public class DocumentOnlineSigner implements Serializable {
+	
+	private static final long serialVersionUID = AonVersion.SERIAL_VERSION_UID;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DocumentOnlineSigner.class.getName());
+	
+		
+	
+}
