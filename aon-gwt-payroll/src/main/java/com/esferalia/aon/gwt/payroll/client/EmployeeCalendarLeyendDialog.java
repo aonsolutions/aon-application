3165c65b602b68ad8e489d3286b65376a7663f5a
svaldepenas <svaldepenas@aonsolutions.es>
2020-01-07 13:55:26 +0100

    Employee calendar leyend color days clean code

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeCalendarLeyendDialog.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeCalendarLeyendDialog.java
@@ -2,47 +2,30 @@ package com.esferalia.aon.gwt.payroll.client;
 
 import com.esferalia.aon.gwt.common.client.widget.CustomDialog;
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Widget;
 
-public abstract class EmployeeCalendarLeyendDialog extends CustomDialog {
+public class EmployeeCalendarLeyendDialog extends CustomDialog {
 
-	interface Binder extends UiBinder<Widget, EmployeeCalendarLeyendDialog> {
-
-	}
+	interface Binder extends UiBinder<Widget, EmployeeCalendarLeyendDialog> {}
 
 	private static final Binder binder = GWT.create(Binder.class);
 	
 	@UiField
 	MyStyle style;
 
-	interface MyStyle extends CssResource {
-		
-	}
+	interface MyStyle extends CssResource {}
 	
-	@UiField
-	Button cancelButton;
+	// -------------------------------------------------------------------------------
+	// --------------------------------- MAIN CLASS ----------------------------------
+	// -------------------------------------------------------------------------------
 
 	public EmployeeCalendarLeyendDialog() {
 		setCaption("Leyenda");
 		
-		setWidget(binder.createAndBindUi(this));		
-		
-		cancelButton.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				hide();
-				
-			}
-		});
-		
+		setWidget(binder.createAndBindUi(this));
 	}
-
-	protected abstract void onAccept();
 
 }
