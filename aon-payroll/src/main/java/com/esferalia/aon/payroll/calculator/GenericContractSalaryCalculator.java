72e78bd6b5100c70c3efb365c7f5039b212fd182
rtrepiana <rtrepiana@aonsolutions.es>
2020-02-05 19:15:11 +0100

    Fix 'contract' vars

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/GenericContractSalaryCalculator.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/GenericContractSalaryCalculator.java
@@ -1520,18 +1520,14 @@ public class GenericContractSalaryCalculator<T extends ISalary, C extends ISalar
 	protected void fillData(IContractSalaryCalculatorContext ctx, String  names []) throws SalaryException {
 		ExpressionContext expressionContext = ctx.getExpressionContext();
 		for (String name : names) {
-			try {
-				ITimedVariable<?> data = expressionContext.getVariable(name, ctx.getStartDate(), ctx.getEndDate());
-				try {
-					salaryBuilder.addData(name, data);
-					continue;
-				} catch (Throwable t) {
-				}
-			}catch (Throwable t) {
-			}
 			
 			try {
-				for (ITimedVariable<Object> data :expressionContext.eval(name, ctx.getStartDate(), ctx.getEndDate())){
+				for (ITimedVariable<?> data :expressionContext.eval(name, ctx.getStartDate(), ctx.getEndDate())){
+					try {
+						data = ((IExpressionVariable<?>) data).getContext().getOrDefault(name, data);
+					} catch (Throwable t) {
+					}
+					
 					try {
 						salaryBuilder.addData(name, data);
 					} catch (Throwable t) {
