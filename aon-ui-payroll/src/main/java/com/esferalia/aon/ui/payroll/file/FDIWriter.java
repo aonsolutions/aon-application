a34c97c211541a8ee26b3f10f3ab7d398ebb554b
AON SOLUTIONS <aonsolutions@Mac-mini-de-AON.local>
2020-03-19 10:42:54 +0100

    Adds new IT 'Enfermedad Común, Prestación Profesional (COVID-19)'

--- aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/file/FDIWriter.java
+++ aon-ui-payroll/src/main/java/com/esferalia/aon/ui/payroll/file/FDIWriter.java
@@ -270,6 +270,7 @@ public class FDIWriter implements Serializable {
 			dit.setCausa("00");
 		}
 		if (detail.getContractLeave().getType() == LeaveType.COMMON_DISEASE || 
+				detail.getContractLeave().getType() == LeaveType.COMMON_OCCUPATIONAL_DISEASE || 
 				detail.getContractLeave().getType() == LeaveType.COMMON_DISEASE_AT_LACK) {
 //			1	Enfermedad común		
 			dit.setContingencia(T35.T35_1.getCode());
