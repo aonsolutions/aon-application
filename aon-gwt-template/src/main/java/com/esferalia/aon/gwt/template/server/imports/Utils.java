4809bf3442c00e0f6b84fd177c91d56443110c03
anderibz <aibanez@aonsolutions.es>
2020-04-13 17:14:14 +0200

    Error importacion pgc.

--- aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/server/imports/Utils.java
+++ aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/server/imports/Utils.java
@@ -33,7 +33,7 @@ public class Utils {
 	public static String calculateAccount(String acc, Integer pos) {
 		if(acc.length() > 9) {
 			return acc.substring(0,pos) + acc.substring((acc.length() - 9) + pos);
-		} else if(acc.length() < 9) {
+		} else if(acc.length() > pos && acc.length() < 9) {
 			return acc.substring(0, pos) + generateZeros(9 - acc.length()) + acc.substring(pos);
 		}
 		return acc;
