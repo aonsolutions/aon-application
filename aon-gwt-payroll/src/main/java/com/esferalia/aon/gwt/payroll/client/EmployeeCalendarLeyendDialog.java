fc13215993990b6db514e1b4aa03f1bc09690519
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2020-04-18 13:59:49 +0200

    New employee calendar leyend dialog

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeCalendarLeyendDialog.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeCalendarLeyendDialog.java
@@ -2,9 +2,7 @@ package com.esferalia.aon.gwt.payroll.client;
 
 import com.esferalia.aon.gwt.common.client.widget.CustomDialog;
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 
 public class EmployeeCalendarLeyendDialog extends CustomDialog {
@@ -12,11 +10,6 @@ public class EmployeeCalendarLeyendDialog extends CustomDialog {
 	interface Binder extends UiBinder<Widget, EmployeeCalendarLeyendDialog> {}
 
 	private static final Binder binder = GWT.create(Binder.class);
-	
-	@UiField
-	MyStyle style;
-
-	interface MyStyle extends CssResource {}
 	
 	// -------------------------------------------------------------------------------
 	// --------------------------------- MAIN CLASS ----------------------------------
