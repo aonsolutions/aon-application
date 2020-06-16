26ee738097f3004568bca9ebda11873a3e90c614
AON SOLUTIONS <aonsolutions@Mac-mini-de-AON.local>
2020-06-16 23:36:23 +0200

    Fix 'HORAS_NOMINA'. Sometimes with 31 days months 'HORAS_NOMINA' were too large

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -20,6 +20,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import net.aonsolutions.db.up2date.security.AuthCreation;
+import net.aonsolutions.db.up2date.tgss.SalaryHoursUpdateMonthDays;
 
 public class Up2Date {
 
@@ -122,7 +123,8 @@ public class Up2Date {
     		//RealDecreeLaw182020Update.REALDECREELAW182020UPDATE
     		//DefaultAgreementDeleteWarn.DEFAULTAGREEMENTDELETEWARN
     		//RealDecreeLaw182020UpdateII.REALDECREELAW182020UPDATEII
-    		AuthCreation.AUTH_CREATION
+    		//AuthCreation.AUTH_CREATION
+    		SalaryHoursUpdateMonthDays.SALARYHOURSUPDATEMONTHDAYS
     };
 
 
---
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/tgss/SalaryHoursUpdateMonthDays.java
@@ -0,0 +1,80 @@
+package net.aonsolutions.db.up2date.tgss;
+
+import static com.esferalia.aon.jooq.tables.SystemData.SYSTEM_DATA;
+import static com.esferalia.aon.jooq.tables.SystemDeduction.SYSTEM_DEDUCTION;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.Calendar;
+
+import org.jooq.DSLContext;
+import org.jooq.DeleteConditionStep;
+import org.jooq.InsertSetMoreStep;
+import org.jooq.SQLDialect;
+import org.jooq.UpdateConditionStep;
+import org.jooq.conf.ParamType;
+import org.jooq.conf.Settings;
+import org.jooq.impl.DSL;
+
+import com.esferalia.aon.jooq.tables.SystemDeduction;
+import com.esferalia.aon.jooq.tables.records.SystemDataRecord;
+import com.esferalia.aon.jooq.tables.records.SystemDeductionRecord;
+
+import net.aonsolutions.db.up2date.Update;
+
+public class SalaryHoursUpdateMonthDays implements Update {
+
+	public static SalaryHoursUpdateMonthDays SALARYHOURSUPDATEMONTHDAYS = new SalaryHoursUpdateMonthDays();
+
+	private static final String SALARY_HOURS = "HORAS_NOMINA";
+	
+	private SalaryHoursUpdateMonthDays() {
+		super();
+	}
+
+	@Override
+	public void upgrade(Connection connection) {
+		Settings settings ; 
+		DSLContext dslContext;
+		
+		settings = new Settings();
+		settings.setRenderSchema(false);
+		settings.setParamType(ParamType.INLINED);
+
+		// Establish context
+		dslContext = DSL.using(connection, SQLDialect.MARIADB, settings);
+
+		boolean upgraded =
+		dslContext.fetchCount(
+		dslContext.select()
+		.from(SYSTEM_DATA)
+		.where(SYSTEM_DATA.DOMAIN.eq(0))
+		.and(SYSTEM_DATA.NAME.eq(SALARY_HOURS))
+		.and(SYSTEM_DATA.EXPRESSION.contains("DIAS_NOMINA/DIAS_MES"))) == 1;
+
+		if ( upgraded ) 
+			return;
+
+		// DOMAIN = 0 , GENERAL
+		UpdateConditionStep<SystemDataRecord> updateMonthDays = 
+		dslContext
+		.update(SYSTEM_DATA)
+		.set(SYSTEM_DATA.EXPRESSION, 
+		DSL.replace(SYSTEM_DATA.EXPRESSION, "DIAS_NOMINA/30", "DIAS_NOMINA/DIAS_MES"))
+		.where(SYSTEM_DATA.DOMAIN.eq(0))
+		.and(SYSTEM_DATA.NAME.eq(SALARY_HOURS))
+		;
+		
+		dslContext.transaction( (config) -> {
+			
+			dslContext.execute("SET FOREIGN_KEY_CHECKS=0;");
+			
+			updateMonthDays.execute();
+			
+			dslContext.execute("SET FOREIGN_KEY_CHECKS=1;");
+
+		});
+		
+	}
+
+}
