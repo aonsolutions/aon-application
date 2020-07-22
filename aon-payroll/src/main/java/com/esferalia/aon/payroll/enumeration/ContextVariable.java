93b9433c68522c8fda78639e3bf2359fcfb75475
rtrepiana <rtrepiana@aonsolutions.es>
2020-07-22 10:04:40 +0200

    HOTFIX I'm so sorry, this is embarrased. I forgot this in my last commit :-(

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/enumeration/ContextVariable.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/enumeration/ContextVariable.java
@@ -155,6 +155,7 @@ public enum ContextVariable implements IResourceable {
 	PATERNITY_FACTOR("COEFICIENTE_PATERNIDAD", VariableType.DOUBLE, false),
 	MATERNITY_FACTOR("COEFICIENTE_MATERNIDAD", VariableType.DOUBLE, false),
 	WORKED_FACTOR("COEFICIENTE_TRABAJADO", VariableType.STRING, false),
+	DROP_FACTOR("COEFICIENTE_AUSENCIA", VariableType.DOUBLE, false),
 
 	// Bajas, Incapacidad Temporal
 	ERE("ERE", VariableType.BOOLEAN), 
