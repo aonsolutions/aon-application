e9dc70bbd20df4b47a93006bd2671d8009259525
anderibz <aibanez@aonsolutions.es>
2020-08-31 08:53:05 +0200

    Example servlet...

---
+++ aon-api/src/main/java/net/aonsolutions/aon/api/servlet/ExampleServlet.java
@@ -0,0 +1,64 @@
+package net.aonsolutions.aon.api.servlet;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.esferalia.aon.occam.api.AON;
+import com.esferalia.aon.occam.api.model.Domain;
+import com.esferalia.aon.watson.util.AonNumberUtils;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "LoginServlet", urlPatterns = {"/ms/api/example/*"})
+public class ExampleServlet extends HttpServlet{
+		
+	private static final Logger LOGGER  = Logger.getLogger(ExampleServlet.class.getName());
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		LOGGER.info("AON EXAMPLE SERVLET - GET METHOD");
+		String token = req.getHeader("session_id");
+		String domainName = req.getHeader("domain_name");
+		Integer domainId = !"null".equalsIgnoreCase(req.getHeader("domain_id")) && AonNumberUtils.toInteger(req.getHeader("domain_id")) != null 
+				? AonNumberUtils.toInteger(req.getHeader("domain_id")) : 0;
+		Domain domain = AON.getDomain(domainName, domainId, "", f -> f.getNameProperty().eq(domainName));
+		String[] pathInfo = req.getPathInfo()!= null || "null".equalsIgnoreCase(req.getPathInfo()) ? req.getPathInfo().split("/") : null;
+
+		JSONObject json = new JSONObject();
+		if(pathInfo  != null) {
+			// Condicional Pathinfo... 
+			if("app".equalsIgnoreCase(pathInfo[1])) {
+				
+			}
+		}
+		
+		Utils.addCorsHeader(resp);
+		Utils.giveBack(req, resp, json, new JSONObject());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		LOGGER.info("USER SERVLET - POST METHOD");
+		String token = req.getHeader("session_id");
+				String domainName = req.getHeader("domain_name");
+		Integer domainId = !"null".equalsIgnoreCase(req.getHeader("domain_id")) && AonNumberUtils.toInteger(req.getHeader("domain_id")) != null 
+				? AonNumberUtils.toInteger(req.getHeader("domain_id")) : 0;
+		Domain domain = AON.getDomain(domainName, domainId, "", f -> f.getNameProperty().eq(domainName));
+		String[] pathInfo = req.getPathInfo()!= null || "null".equalsIgnoreCase(req.getPathInfo()) ? req.getPathInfo().split("/") : null;
+		
+		JSONObject json = Utils.getRequestJSON(req);
+		if(pathInfo != null) {
+			if("app".equalsIgnoreCase(pathInfo[1])) {
+				// Ejecutar funcionalidad del api
+			}
+		}
+		Utils.addCorsHeader(resp);
+		Utils.giveBack(req, resp, json, new JSONObject());
+	}
+}
