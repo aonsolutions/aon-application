57b26100bbd19f47a2697ad3eb513b4498fad30f
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2020-09-08 21:20:43 +0200

    Add method StringUtils

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/tgss/cra/StringUtils.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/tgss/cra/StringUtils.java
@@ -30,5 +30,18 @@ public class StringUtils {
 		
 		return outputStr;
 	}
+	
+	public static boolean isBlank(String str) {
+		int strLen;
+		if (str == null || (strLen = str.length()) == 0) {
+			return true;
+		}
+		for (int i = 0; i < strLen; i++) {
+			if ((Character.isSpace(str.charAt(i)) == false)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
