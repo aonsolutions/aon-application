43190193cb7d66a09c7d4e944231454052ff35be
anderibz <aibanez@aonsolutions.es>
2020-09-11 12:09:08 +0200

    new aon solutions. Aon Icon.

---
+++ aon-solutions/src/main/webapp/components/aon-icon.js
@@ -0,0 +1,73 @@
+import {icons} from '../assets/icons/icons.js';
+
+class AonIcon extends HTMLElement {
+
+  static get observedAttributes() {
+    return ['icon', 'color', 'size'];
+  }
+
+  get id() {
+    return this.getAttribute('id');
+  }
+
+  set id(id) {
+    this.setAttribute('id', id);
+  }
+
+  get icon() {
+    return this.getAttribute('icon');
+  }
+
+  set icon(icon) {
+    this.setAttribute('icon', icon);
+  }
+
+  get color() {
+    return this.getAttribute('color');
+  }
+
+  set color(color) {
+    this.setAttribute('color', color);
+  }
+
+  get size() {
+    return this.getAttribute('size');
+  }
+
+  set size(size) {
+    this.setAttribute('size', size);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if('icon' === name){
+
+    }
+
+    if('color' === name){
+
+    }
+
+    if('size' === name){
+
+    }
+  }
+
+  constructor () {
+		super();
+	}
+
+	connectedCallback () {
+  	this.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"
+          width="${this.hasAttribute('size') ? this.getAttribute('size') : '24px'}"
+          height="${this.hasAttribute('size') ? this.getAttribute('size') : '24px'}">
+        <g id="${this.getAttribute('icon')}">
+          <path d="${icons[this.getAttribute('icon')]}" style="fill:${this.hasAttribute('color') ? this.getAttribute('color'): '#5f6368'};"/>
+        </g>
+      </svg>
+		`;
+  }
+
+}
+
+window.customElements.define('aon-icon', AonIcon);
