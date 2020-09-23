2d3b7f65828c1f039bc87a3cc41e0c0038ad9b7c
Jose M Partida <josempartida@ayudat.es>
2020-09-23 08:14:29 +0200

    Added aonSolutionsxAYUDA T Suite folder

---
+++ aon-suite/README.md
@@ -0,0 +1,23 @@
+# aon Suite
+
+## Dependencias
+- **Bootstrap 4.5.2**
+- **jQuery 3.5.1**
+- **Chart.js 2.9.3**: Librería JavaScript para la visualización de gráficos con animaciones e interactivos, responsive y con soporte hasta IE11+
+
+## Dependencias de desarrollo
+- **gulp 4.0.2**: Gestor de tareas de desarrollo
+- **gulp-concat**: Necesario para compilar el JS
+- **browser-sync**: Necesario para recargar automáticamente el navegador cuando se guarda algún archivo
+
+    - Al ejecutar la tarea `gulp watch` se nos abrirá una nueva pestaña en el navegador con la dirección `localhost:9000`, cualquier cambio que hagamos en ficheros .scss, .js o .html que se encuentren en los directorios configurados para ello, hará que se actualice el navegador automáticamente
+
+    - Si tras ejecutar `gulp watch` accedemos a la dirección `localhost:9001`, podremos ver una interfaz que proporciona el paquete Browsersync con algunas herramientas de desarrollo, por ejemplo si desde el menú de esta interfaz pulsamos en la opción "Remote Debug" del menú de la izquierda, podremos activar "CSS Outlining", lo que añadirá bordes a todos los elementos de la página
+
+    - Browsersync ofrece también la posibilidad de acceder a la app desde cualquier dispositivo que se encuentre conectado a nuestra red. Esta opción está desactivada por defecto, para activarla debemos comentar una línea del archivo `gulpfile.js`, al hacerlo nos aparecerá en la terminal una IP junto a "External URL:" y podremos utilizar dicha IP para acceder a la app desde otros dispositivos, la línea a comentar es la siguiente (se encuentra dentro de la función `sync`):
+
+        ```js
+        listen: 'localhost'
+        ```
+
+        Nota: para que este cambio surta efecto debemos terminar el proceso de gulp en la terminal, y volver a ejecutar `gulp watch`
---
+++ aon-suite/gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+
+// Requires browser-sync for live reloading the browser and creates a Browsersync instance
+var browserSync = require('browser-sync').create();
+
+// Requires the gulp-sass plugin
+var sassPlugin = require('gulp-sass');
+var concat = require('gulp-concat');
+
+// Watch files for changes
+function watchFiles() {
+    gulp.watch('assets/scss/*.scss', sass);
+    gulp.watch('public/js/*.js').on('change', browserSync.reload); // Triggers a full reload on the brower
+    gulp.watch('public/**/*.html').on('change', browserSync.reload); // Triggers a full reload on the brower
+}
+
+// Compiles Sass to CSS
+function sass() {
+    return gulp.src('assets/scss/app.scss')
+        .pipe(sassPlugin()) // Transforms Sass to CSS using the gulp-sass plugin
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.stream()); // Stream changes to the browser (without a full reload)
+}
+
+// Añadir al array los módulos JS para que se compilen todos juntos
+function scripts() {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/js/'))
+    //.pipe(rename({suffix: '.min'})) // Cuando vayamos a poner el .min
+    //.pipe(uglify())
+}
+
+function sync(done) {
+    browserSync.init({
+        server: {
+            baseDir: 'public' // Base directory from where our app will be served
+        },
+        port: 9000, // Port used by the app
+        ui: {
+            port: 9001 // Port used by the Browsersync UI
+        },
+        listen: 'localhost', // Comment this line to get an external URL, which can be accessed from other devices on the same network
+        notify: false // If true, shows a notificacion saying Browsersync is connected
+    });
+
+    done();
+}
+
+// First compiles Sass to CSS if there are changes, then starts watching files and Browsersync
+var watch = gulp.series(sass, gulp.parallel(watchFiles, sync));
+
+exports.sass = sass;
+exports.scripts = scripts;
+exports.watch = watch;
