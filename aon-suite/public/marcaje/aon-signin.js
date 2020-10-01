514cb618502b7e00eef323aefbe6ca9f9915fe4e
anderibz <aibanez@aonsolutions.es>
2020-10-01 11:55:28 +0200

    AON SUITE. signin (marcaje) & messenger (ticket).

---
+++ aon-suite/public/marcaje/aon-signin.js
@@ -0,0 +1,53 @@
+class AonSignin extends HTMLElement {
+
+	constructor () {
+		super();
+	}
+
+	connectedCallback () {
+		this.innerHTML = `
+			<aon-application id="aonSignin" title="CONTROL DE HORARIO"></aon-application>
+		`;
+    this.build();
+ 	}
+
+ 	build() {
+		let aonSignin = document.getElementById('aonSignin');
+
+		let options = [
+			{
+				name: 'Administradores',
+				icon: 'admin_panel_settings',
+				fn: () => this.loadAdmin()
+			},
+			{
+				name: 'Empleados',
+				icon: 'people',
+				fn: () => this.loadEmployee()
+			},
+			{
+				name: 'Historial',
+				icon: 'history',
+				fn: () => this.loadHistory()
+			}
+		];
+		aonSignin.addSidenavOptions('OPCIONES', options);
+		this.loadAdmin();
+	}
+
+	loadAdmin() {
+		let aonSignin = document.getElementById('aonSignin');
+		aonSignin.setContentHTML('<iframe src="./administrador.html" style="width:100%;height:100%;border:none;"></iframe>');
+	}
+
+	loadEmployee() {
+		let aonSignin = document.getElementById('aonSignin');
+		aonSignin.setContentHTML('<iframe src="./empleado.html" style="width:100%;height:100%;border:none;"></iframe>');
+	}
+
+	loadHistory() {
+		let aonSignin = document.getElementById('aonSignin');
+		aonSignin.setContentHTML('<iframe src="./historial.html" style="width:100%;height:100%;border:none;"></iframe>');
+	}
+}
+window.customElements.define('aon-signin', AonSignin);
---
+++ aon-suite/public/ticket/aon-messenger.js
@@ -0,0 +1,53 @@
+class AonMessenger extends HTMLElement {
+
+	constructor () {
+		super();
+	}
+
+	connectedCallback () {
+		this.innerHTML = `
+			<aon-application id="aonMessenger" title="MESSENGER"></aon-application>
+		`;
+		this.build();
+ 	}
+
+ 	build() {
+		let aonMessenger = document.getElementById('aonMessenger');
+
+		let options = [
+			{
+				name: 'Index',
+				icon: 'message',
+				fn: () => this.loadIndex()
+			},
+			{
+				name: 'Create',
+				icon: 'message',
+				fn: () => this.loadCreate()
+			},
+			{
+				name: 'Show',
+				icon: 'message',
+				fn: () => this.loadShow()
+			}
+		];
+		aonMessenger.addSidenavOptions('OPCIONES', options);
+		this.loadIndex();
+	}
+
+	loadIndex() {
+		let aonMessenger = document.getElementById('aonMessenger');
+		aonMessenger.setContentHTML('<iframe src="./index.html" style="width:100%;height:100%;border:none;"></iframe>');
+	}
+
+	loadCreate() {
+		let aonMessenger = document.getElementById('aonMessenger');
+		aonMessenger.setContentHTML('<iframe src="./create.html" style="width:100%;height:100%;border:none;"></iframe>');
+	}
+
+	loadShow() {
+		let aonMessenger = document.getElementById('aonMessenger');
+		aonMessenger.setContentHTML('<iframe src="./show.html" style="width:100%;height:100%;border:none;"></iframe>');
+	}
+}
+window.customElements.define('aon-messenger', AonMessenger);
