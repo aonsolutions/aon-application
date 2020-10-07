daf7d1d53e63eac2137919109f8dff75f9ecd651
VictorRodFra <victorrodriguez@ayudatpymes.com>
2020-10-07 09:36:00 +0200

    Apartados laboral y contable

--- aon-suite/public/contable/aon-contable.js
+++ aon-suite/public/contable/aon-contable.js
@@ -1,69 +1,67 @@
 class AonContable extends HTMLElement {
+    constructor () {
+        super();
+    }
 
-	constructor () {
-		super();
-	}
+    connectedCallback () {
+        this.innerHTML = `
+            <aon-application id="aonContable" title="CONTABLE"></aon-application>
+        `;
+        this.build();
+    }
 
-	connectedCallback () {
-		this.innerHTML = `
-			<aon-application id="aonContable" title="CONTABLE"></aon-application>
-		`;
-    this.build();
- 	}
+    build() {
+        let aonContable = document.getElementById('aonContable');
 
- 	build() {
-		let aonContable = document.getElementById('aonContable');
+        let documentOptions = [
+            {
+                name: 'Recientes',
+                icon: 'access_time',
+                fn: () => this.loadIndex()
+            },
+            {
+                name: 'Ver todos',
+                icon: 'text_snippet',
+                fn: () => this.loadIndex()
+            }
+        ];
+        aonContable.addSidenavOptions('DOCUMENTOS', documentOptions);
 
-		aonContable.addToolbarOption('Add', 'add', () => {alert('Add Example')});
+        let categoryOptions = [
+            {
+                name: 'Resumen',
+                icon: 'folder',
+                fn: () => this.loadIndex()
+            },
+            {
+                name: 'PPyGG',
+                icon: 'folder',
+                fn: () => this.loadPPYGG()
+            },
+            {
+                name: 'Balance',
+                icon: 'folder',
+                fn: () => this.loadBalance()
+            }
+        ];
 
-		let documentOptions = [
-			{
-				name: 'Recientes',
-				icon: 'access_time',
-				fn: () => this.loadIndex()
-			},
-			{
-				name: 'Pendientes',
-				icon: 'inbox',
-				fn: () => this.loadIndex()
-			}
-		];
-		aonContable.addSidenavOptions('DOCUMENTOS', documentOptions);
+        aonContable.addSidenavOptions('CATEGORIAS', categoryOptions);
+        this.loadIndex();
+    }
 
-		let categoryOptions = [
-		{
-				name: 'Resumen',
-				icon: 'folder',
-				fn: () => this.loadIndex()
-			},
-      {
-				name: 'PPyGG',
-				icon: 'folder',
-				fn: () => this.loadPPYGG()
-			},
+    loadIndex() {
+        let aonContable = document.getElementById('aonContable');
+        aonContable.setContentHTML('<iframe src="./index.html" style="width:100%;height:100%;border:none;"></iframe>');
+    }
 
-      {
-				name: 'Balance',
-				icon: 'folder',
-				fn: () => this.loadBalance()
-			}
-		];
-		aonContable.addSidenavOptions('CATEGORIAS', categoryOptions);
-	}
+    loadPPYGG() {
+        let aonContable = document.getElementById('aonContable');
+        aonContable.setContentHTML('<iframe src="./ppygg.html" style="width:100%;height:100%;border:none;"></iframe>');
+    }
 
-	loadIndex() {
-		let aonContable = document.getElementById('aonContable');
-		aonContable.setContentHTML('<iframe src="./index.html" style="width:100%;height:100%;border:none;"></iframe>');
-	}
-
-	loadPPYGG() {
-		let aonContable = document.getElementById('aonContable');
-		aonContable.setContentHTML('<iframe src="./ppygg.html" style="width:100%;height:100%;border:none;"></iframe>');
-	}
-
-	loadBalance() {
-		let aonContable = document.getElementById('aonContable');
-		aonContable.setContentHTML('<iframe src="./balance.html" style="width:100%;height:100%;border:none;"></iframe>');
-	}
+    loadBalance() {
+        let aonContable = document.getElementById('aonContable');
+        aonContable.setContentHTML('<iframe src="./balance.html" style="width:100%;height:100%;border:none;"></iframe>');
+    }
 }
 window.customElements.define('aon-contable', AonContable);
--- aon-suite/public/laboral/aon-laboral.js
+++ aon-suite/public/laboral/aon-laboral.js
@@ -1,49 +1,74 @@
 class AonLaboral extends HTMLElement {
+    constructor () {
+        super();
+    }
 
-	constructor () {
-		super();
-	}
+    connectedCallback () {
+        this.innerHTML = `
+                <aon-application id="aonLaboral" title="LABORAL"></aon-application>
+        `;
+        this.build();
+    }
 
-	connectedCallback () {
-		this.innerHTML = `
-			<aon-application id="aonLaboral" title="LABORAL"></aon-application>
-		`;
-    this.build();
- 	}
+    build() {
+        let aonLaboral = document.getElementById('aonLaboral');
+        
+        aonLaboral.addToolbarOption('Alta', 'person_add_alt_1', () => this.loadNew('alta'));
+        aonLaboral.addToolbarOption('Baja', 'person_remove_alt_1', () => this.loadNew('baja'));
 
- 	build() {
-		let aonLaboral = document.getElementById('aonLaboral');
+        let employerOptions = [
+            {
+                name: 'Recientes',
+                icon: 'access_time',
+                fn: () => this.loadIndex()
+            },
+            {
+                name: 'Altas',
+                icon: 'arrow_drop_up',
+                fn: () => this.loadIndex()
+            },
+            {
+                name: 'Bajas',
+                icon: 'arrow_drop_down',
+                fn: () => this.loadIndex()
+            },
+            {
+                name: 'Nóminas',
+                icon: 'text_snippet',
+                fn: () => this.loadNominas()
+            }
+        ];
+        aonLaboral.addSidenavOptions('EMPLEADOS', employerOptions);
 
-		aonLaboral.addToolbarOption('Add', 'add', () => {alert('Add Example')});
+        let actionsOptions = [
+            {
+                name: 'Crear alta',
+                icon: 'event_available',
+                fn: () => this.loadNew('alta')
+            },
+            {
+                name: 'Crear baja',
+                icon: 'event_busy',
+                fn: () => this.loadNew('baja')
+            }
+        ];
+        aonLaboral.addSidenavOptions('ACCIONES', actionsOptions);
+        this.loadIndex();
+    }
 
-		let documentOptions = [
-			{
-				name: 'Recientes',
-				icon: 'access_time',
-				fn: () => this.loadIndex()
-			},
-			{
-				name: 'Pendientes',
-				icon: 'inbox',
-				fn: () => this.loadIndex()
-			}
-		];
-		aonLaboral.addSidenavOptions('DOCUMENTOS', documentOptions);
-
-		let categoryOptions = [
-		{
-				name: 'Resumen',
-				icon: 'folder',
-				fn: () => this.loadIndex()
-			}
-		];
-		aonLaboral.addSidenavOptions('CATEGORIAS', categoryOptions);
-	}
-
-	loadIndex() {
-		let aonLaboral = document.getElementById('aonLaboral');
-		aonLaboral.setContentHTML('<iframe src="./index.html" style="width:100%;height:100%;border:none;"></iframe>');
-	}
-
+    loadIndex() {
+        let aonLaboral = document.getElementById('aonLaboral');
+        aonLaboral.setContentHTML('<iframe src="./index.html" style="width:100%;height:100%;border:none;"></iframe>');
+    }
+    
+    loadNominas() {
+        let aonLaboral = document.getElementById('aonLaboral');
+        aonLaboral.setContentHTML('<iframe src="./nominas.html" style="width:100%;height:100%;border:none;"></iframe>');
+    }
+    
+    loadNew(type) {
+        let aonLaboral = document.getElementById('aonLaboral');
+        aonLaboral.setContentHTML('<iframe src="./new.html?id='+type+'" style="width:100%;height:100%;border:none;"></iframe>');
+    }
 }
 window.customElements.define('aon-laboral', AonLaboral);
