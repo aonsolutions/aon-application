1d402d76e41d709ecad135dabec19c6251372aee
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2020-10-14 19:59:27 +0200

    Fix CRA page

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/MainCRANew.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/MainCRANew.java
@@ -746,8 +746,8 @@ public class MainCRANew extends MainEntryPoint {
 	    
 	    // Add the data to the data provider, which automatically pushes it to the
 	    // widget.
+	    dataProvider.getList().clear();
 	    List<CCCInfo> cccList = dataProvider.getList();
-	    cccList.clear();
 	    
 	    this.cccs = this.mainCRAObjectNew.getEnterpriseCCCs();
 	    
@@ -1097,7 +1097,7 @@ public class MainCRANew extends MainEntryPoint {
 	
 	@UiHandler("newCRAButton")
 	public void onNewCRAButton(ClickEvent event) {
-		this.mainCRAObjectNew.resetEnterpriseCCCList();
+//		this.mainCRAObjectNew.resetEnterpriseCCCList();
 		
 		showCCCs();
 		clearSelectionModel();
