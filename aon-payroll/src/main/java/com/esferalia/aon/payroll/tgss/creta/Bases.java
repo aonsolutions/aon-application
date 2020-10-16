0a18c59e45c497bac271b05ed15d330436be9d42
rtrepiana <rtrepiana@aonsolutions.es>
2020-10-16 11:53:33 +0200

    Fix BUG with ITs and ERTEs

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/tgss/creta/Bases.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/tgss/creta/Bases.java
@@ -1951,9 +1951,9 @@ public class Bases {
 			
 			put("603", new NonNegativeCompositeCCretaData()
 					.add(MATERNITY_BASE)
-					.add(ERE_BASES )
 					.add(DIRECT_BASE)		//   
 					.add(CGP_BASE)
+					.add(ERE_BASES )
 					.add(CGC_BASE_ENTERPRISE)	  
 					);
 			put("613", new NonNegativeCompositeCCretaData()
