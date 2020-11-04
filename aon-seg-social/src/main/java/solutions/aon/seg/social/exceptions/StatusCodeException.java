2f5c52ac0f2e206076908265471a3f11318f1cde
akrck02 <aketza.vazquez@gmail.com>
2020-11-04 18:19:11 +0100

    StatusCodeException handler exception

--- aon-seg-social/src/main/java/solutions/aon/seg/social/exceptions/StatusCodeException.java
+++ aon-seg-social/src/main/java/solutions/aon/seg/social/exceptions/StatusCodeException.java
@@ -1,5 +1,17 @@
 package solutions.aon.seg.social.exceptions;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+
 public class StatusCodeException extends SegSocialException{
 
+	public static void HandleStatusCodeException(FailingHttpStatusCodeException e) throws StatusCodeException 
+	{
+		switch (e.getStatusCode()) {
+		case 403:
+			throw new ForbiddenException();
+		default:
+			throw new StatusCodeException();
+		}
+	}
+	
 }
