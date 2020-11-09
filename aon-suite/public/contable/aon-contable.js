8924d29499e5a563bbc114d87cb0a1160527176a
JL <joseluisjimenez@ayudatpymes.com>
2020-11-09 12:59:53 +0100

    Agregar apartado vista clásica en toda las opciones

--- aon-suite/public/contable/aon-contable.js
+++ aon-suite/public/contable/aon-contable.js
@@ -1,3 +1,5 @@
+import {loginbidoq} from  '../components/bidoq.js';
+
 class AonContable extends HTMLElement {
     constructor () {
         super();
@@ -47,6 +49,8 @@ class AonContable extends HTMLElement {
 
         aonContable.addSidenavOptions('CATEGORIAS', categoryOptions);
         this.loadIndex();
+
+        this.suiteOld();
     }
 
     loadIndex() {
@@ -62,6 +66,58 @@ class AonContable extends HTMLElement {
     loadBalance() {
         let aonContable = document.getElementById('aonContable');
         aonContable.setContentHTML('<iframe src="./balance.html" style="width:100%;height:100%;border:none;"></iframe>');
+    }
+    
+	suiteOld() {
+        let aonContable       = document.getElementById('aonContable');
+        // Datos de mientras de pruebas
+        const aon_domain_name = 'altai-G90317447-ayudat.aonsolutions.net';
+        const aon_session_id  = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJzY2hlbWFcIjpcImF5dWRhdC1hb25zb2x1dGlvbnMtbmV0XCIsXCJzY2hlbWFfZmlyc3RfZG9tYWluXCI6XCIwMDIyNDIwMzllLWF5dWRhdC5hb25zb2x1dGlvbnMubmV0XCIsXCJ1dWlkXCI6XCJFNkFGMjg1NEI2NjYxMUVBODMyMzA2QTBCREQ3MkE0NlwifSIsImlzcyI6ImF1dGgwIiwiaWF0IjoxNjAwNzkzNDgyfQ.4O-z1Hldqz1WAmX7kcsBkRlb0zy64ucYXQIoLnDL7mA';
+   
+        let options = [
+            {
+                name: 'aonSolutions',
+                img : '../img/aon.png',
+                fn: () => open('https://' + aon_domain_name + '/login?token=' + aon_session_id, '_blank')
+            },
+            {
+                name: 'Bidoq',
+                img : '../img/bidoq.png',
+                fn  : () => this.loadBidoq()
+            }
+        ];
+        aonContable.addSidenavOptions('VISTA CLÁSICA', options);
+        this.loadIndex();
+    }
+
+    async loadBidoq(){
+        // Ponemos un cargando mientras
+        const aonContable = document.getElementById('aonContable');
+        aonContable.setContentHTML('<center class="lds-padding-top"><div class="lds-ripple"><div></div><div></div></div></center>');
+
+        try {
+            // Llamamos a Bidoq para hacer el Login
+            const loginBidoq    = await loginbidoq();
+            const response      = JSON.parse(loginBidoq);
+            
+            if (typeof response !== 'undefined') {
+                if (typeof response.code !== 'undefined' && response.code === 0 && response.datos.ruta) {
+                    // Abrir BIDOQ
+                    window.open(response.datos.respuesta, '_blank');
+                    aonContable.setContentHTML('');
+                } else {
+                    console.log(response);
+                    alert(response.message);
+                    aonContable.setContentHTML('');
+                }
+            } else {
+                console.error('Ocurrió un error al intentar abrir BIDOQ');
+                aonContable.setContentHTML('');
+            }
+        } catch (error) {
+            console.error('Ocurrió un error: ' + error.message);
+            aonContable.setContentHTML('');
+        }
     }
 }
 window.customElements.define('aon-contable', AonContable);
