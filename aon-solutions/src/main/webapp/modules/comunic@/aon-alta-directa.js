737a5707134e0e3376c64a39c76ef4d633044d4c
RAY VASQUEZ <anrto_94_5@hotmail.com>
2020-11-10 15:16:15 +0100

    Form alta directa

--- aon-solutions/src/main/webapp/modules/comunic@/aon-alta-directa.js
+++ aon-solutions/src/main/webapp/modules/comunic@/aon-alta-directa.js
@@ -1,7 +1,10 @@
 import {AonElement} from '../../components/AonElement.js';
 import '../../components/aon-card.js';
 import '../../components/aon-input.js';
+import '../../components/aon-date.js';
 import {serializeForm} from '../../services/utils.js'
+import {getPersonas} from '../../services/service.js'
+import '../../components/aon-suggestion.js';
 
 export class AonAltaDirecta extends AonElement {
 
@@ -31,7 +34,7 @@ export class AonAltaDirecta extends AonElement {
                 <div class="aonRow">
                     <div class="aonContainer">
                         <div class="aonCol aonS12 aonM6">
-                            <aon-input name="dni" id="dni" description="DNI/NIE" type="text" required="true"></aon-input>
+                            <div id="aonAltaDirectaDivDni"></div>
                         </div>
                         <div class="aonCol aonS12 aonM6">
                             <aon-input name="naf" id="naf" description="Número de afiliación" type="text"></aon-input>
@@ -40,7 +43,7 @@ export class AonAltaDirecta extends AonElement {
                             <aon-input name="nombre" id="nombre" description="Nombre" type="text"></aon-input>
                         </div>
                         <div class="aonCol aon6 aonM6">
-                            <aon-input name="apellido" id="apellido" description="1er Apellido" type="text"></aon-input>
+                            <aon-input name="apellido" id="apellido1" description="1er Apellido" type="text"></aon-input>
                         </div>
                         <div class="aonCol aon6 aonM6">
                             <aon-input name="apellido2" id="apellido2" description="2do Apellido" type="text"></aon-input>
@@ -52,7 +55,7 @@ export class AonAltaDirecta extends AonElement {
                             <aon-input name="modalidad_contrato" id="modalidad_contrato" description="Modalidad" type="text"></aon-input>
                         </div>
                         <div class="aonCol aonS12 aonM6">
-                            <aon-input name="fecha" id="fecha" description="Fecha" type="date"></aon-input>
+                            <aon-date name="fecha" id="fecha" title="Fecha"></aon-date>
                         </div> 
                         <div class="aonCol aonS12 aonM6">
                             <aon-input name="grupo_cotizacion" id="grupo_cotizacion" description="Grupo de cotización" type="text"></aon-input>
@@ -75,12 +78,41 @@ export class AonAltaDirecta extends AonElement {
             </form>
         `);
 
-        let form = this.getElement('aonAltaDirectaForm');
-        form.addEventListener('submit', (e)=>{
-            e.preventDefault(); 
-            let formJson = serializeForm(e.target);
-            console.log(formJson);
-        })
+        let form = this.getElement(`aonAltaDirectaForm`);
+        form.addEventListener('submit',this.formSubmit);
+
+        let aonAltaDirectaDni = this.getElement(`aonAltaDirectaDivDni`);
+        aonAltaDirectaDni.innerHTML = `<aon-suggestion id="aonAltaDirectaDni" title="DNI/NIE"></aon-suggestion>`;
+
+		let searchSuggestion = this.getElement(`aonAltaDirectaDni`);
+		searchSuggestion.addEventListener('keyup', ({target:{value}}) => {
+            let valor = value.toString().toUpperCase();
+			if(valor.length > 2) {
+                getPersonas().then( resp => {
+                    let filters = resp.filter(f=>f.dni.indexOf(valor) >= 0).map(m=> {
+                        return {...m, value:m.dni, name:m.dni};
+                    });
+                    searchSuggestion.buildOptions(filters);
+                });
+			} else {
+				searchSuggestion.closeOptions();
+			}
+		});
+		searchSuggestion.addEventListener('select', ({detail}) => {
+            if(detail){
+                this.getElement('nombre').setAttribute('value', detail.nombre);
+                this.getElement('apellido1').setAttribute('value', detail.last_name1);
+                this.getElement('apellido2').setAttribute('value', detail.last_name2);
+            }
+		});
+    }
+
+
+
+    formSubmit(e){
+        e.preventDefault(); 
+        let formJson = serializeForm(e.target);
+        console.log(formJson);
     }
 }
 window.customElements.define('aon-alta-directa', AonAltaDirecta);
--- aon-solutions/src/main/webapp/services/comunicaService.js
+++ aon-solutions/src/main/webapp/services/comunicaService.js
@@ -2,8 +2,8 @@
 const result = [
     {
         id: 1,
-        name: 'NOMBRE',
-        last_name: 'APELLIDOS', 
+        nombre: 'NOMBRE',
+        last_nombre: 'APELLIDOS', 
         dni: '0Y777777X',
         mov: 'Alta',
         naf: '123123131',
@@ -12,8 +12,8 @@ const result = [
     },
     {
         id: 2,
-        name: 'NOMBRE',
-        last_name: 'APELLIDOS', 
+        nombre: 'NOMBRE',
+        last_nombre: 'APELLIDOS', 
         dni: '0Y777777X',
         mov: 'Alta',
         naf: '123123131',
@@ -22,8 +22,8 @@ const result = [
     },
     {
         id: 3,
-        name: 'NOMBRE',
-        last_name: 'APELLIDOS', 
+        nombre: 'NOMBRE',
+        last_nombre: 'APELLIDOS', 
         dni: '0Y777777X',
         mov: 'Alta',
         naf: '123123131',
@@ -32,8 +32,8 @@ const result = [
     },
     {
         id: 4,
-        name: 'NOMBRE',
-        last_name: 'APELLIDOS', 
+        nombre: 'NOMBRE',
+        last_nombre: 'APELLIDOS', 
         dni: '0Y777777X',
         mov: 'Alta',
         naf: '123123131',
@@ -42,7 +42,7 @@ const result = [
     },
     {
         id: 5,
-        name: 'NOMBRE',
+        nombre: 'NOMBRE',
         last_name: 'APELLIDOS', 
         dni: '0Y777777X',
         mov: 'Alta',
@@ -54,4 +54,68 @@ const result = [
 
 export const getMovements = () =>  new Promise((resolve) => {
     resolve(result);
+});
+
+
+
+const personas = [
+    {
+        id: 1,
+        nombre: 'NOMBRE',
+        last_name1: 'APELLIDO1', 
+        last_name2: 'APELLIDO2', 
+        dni: 'Y777777X',
+        mov: 'Alta',
+        naf: '123123131',
+        tipo_contrato: 'Jornada Parcial',
+        fecha: '21-01-2020'
+    },
+    {
+        id: 2,
+        nombre: 'RAY',
+        last_name1: 'VASQUEZ', 
+        last_name2: 'BEAUPERTHUY', 
+        dni: 'Y7514970X',
+        mov: 'Alta',
+        naf: '123123131',
+        tipo_contrato: 'Jornada completa',
+        fecha: '22-01-2020'
+    },
+    {
+        id: 3,
+        nombre: 'NOMBRE',
+        last_name1: 'APELLIDO1', 
+        last_name2: 'APELLIDO2', 
+        dni: 'Y777777X',
+        mov: 'Alta',
+        naf: '123123131',
+        tipo_contrato: 'Jornada Parcial',
+        fecha: '23-01-2020'
+    },
+    {
+        id: 4,
+        nombre: 'NOMBRE',
+        last_name1: 'APELLIDO1', 
+        last_name2: 'APELLIDO2', 
+        dni: 'Y777777X',
+        mov: 'Alta',
+        naf: '123123131',
+        tipo_contrato: 'Jornada completa',
+        fecha: '25-01-2020'
+    },
+    {
+        id: 5,
+        nombre: 'NOMBRE',
+        last_name1: 'APELLIDO1', 
+        last_name2: 'APELLIDO2', 
+        dni: 'Y777777X',
+        mov: 'Alta',
+        naf: '123123131',
+        tipo_contrato: 'Jornada completa',
+        fecha: '25-01-2020'
+    },
+];
+
+export const getPersonas = (dni) =>  new Promise((resolve) => {
+    resolve(personas);
 });
