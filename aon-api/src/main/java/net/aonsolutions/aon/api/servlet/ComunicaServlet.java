b7c88795d6225ccc70f52aa56fdffccc4b63bdf5
RAY VASQUEZ <anrto_94_5@hotmail.com>
2020-11-13 16:54:53 +0100

    Add aon-seg social api

---
+++ aon-api/src/main/java/net/aonsolutions/aon/api/servlet/ComunicaServlet.java
@@ -0,0 +1,100 @@
+package net.aonsolutions.aon.api.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+//import java.io.PrintStream;
+import java.util.Collection;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.esferalia.aon.occam.api.AON;
+//import com.esferalia.aon.occam.api.AONContext;
+import com.esferalia.aon.occam.api.model.Domain;
+import com.esferalia.aon.watson.util.AonNumberUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import solutions.aon.seg.social.objects.Employee;
+import solutions.aon.seg.social.SistemaRED;
+import solutions.aon.seg.social.exceptions.SegSocialException;
+import solutions.aon.seg.social.exceptions.statusCode.StatusCodeException;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "ComunicaServlet", urlPatterns = {"/ms/api/comunica/*"})
+public class ComunicaServlet extends HttpServlet{
+		
+	private static final Logger LOGGER  = Logger.getLogger(ComunicaServlet.class.getName());
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		LOGGER.info("AON EXAMPLE SERVLET - GET METHOD");
+		String token = req.getHeader("session_id");
+		String domainName = req.getHeader("domain_name");
+		byte content [] = null;
+		Integer domainId = !"null".equalsIgnoreCase(req.getHeader("domain_id")) && AonNumberUtils.toInteger(req.getHeader("domain_id")) != null 
+				? AonNumberUtils.toInteger(req.getHeader("domain_id")) : 0;
+
+				
+		Domain domain = AON.getDomain(domainName, domainId, "", f -> f.getNameProperty().eq(domainName));
+		
+		Gson gjson = new GsonBuilder().setDateFormat("YYYY-MM-dd").create();
+		OutputStream os = resp.getOutputStream();
+		try {
+
+			String[] pathInfo = req.getPathInfo()!= null || "null".equalsIgnoreCase(req.getPathInfo()) ? req.getPathInfo().split("/") : null;
+	
+		
+			if(pathInfo  != null) {
+				// Condicional Pathinfo... 
+				if("/movements".equalsIgnoreCase(pathInfo[1])) {
+		
+				}
+			}
+		
+			Utils.addCorsHeader(resp);
+			
+			resp.setStatus(HttpServletResponse.SC_OK);
+			
+//		    try(final InputStream certificateInputStream=TestPaternity.class.getResourceAsStream("FNMT.p12")){
+		    final InputStream certificateInputStream= ComunicaServlet.class.getResourceAsStream("FNMT.p12");
+			Collection<Employee> Employees = SistemaRED.getEmployees(certificateInputStream, "jg@FNMT", "pkcs12", "0111", "01105360062");	
+			//Collection<Employee> Employees = SistemaRED.getFullEmployees(certificateInputStream, "jg@FNMT", "pkcs12", "0111", "01105360062");				
+			content = gjson.toJson(Employees).getBytes();
+		} 
+		catch (Exception e) {
+//            e.printStackTrace(); 
+			resp.setStatus(500);
+			content = gjson.toJson(e.getMessage()).getBytes();
+		}
+		resp.setContentType("text/html");
+		resp.setContentLength(content.length);
+		os.write(content);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		LOGGER.info("USER SERVLET - POST METHOD");
+		String token = req.getHeader("session_id");
+				String domainName = req.getHeader("domain_name");
+		Integer domainId = !"null".equalsIgnoreCase(req.getHeader("domain_id")) && AonNumberUtils.toInteger(req.getHeader("domain_id")) != null 
+				? AonNumberUtils.toInteger(req.getHeader("domain_id")) : 0;
+		Domain domain = AON.getDomain(domainName, domainId, "", f -> f.getNameProperty().eq(domainName));
+		String[] pathInfo = req.getPathInfo()!= null || "null".equalsIgnoreCase(req.getPathInfo()) ? req.getPathInfo().split("/") : null;
+		
+		JSONObject json = Utils.getRequestJSON(req);
+		if(pathInfo != null) {
+			if("app".equalsIgnoreCase(pathInfo[1])) {
+				// Ejecutar funcionalidad del api
+			}
+		}
+		Utils.addCorsHeader(resp);
+		Utils.giveBack(req, resp, json, new JSONObject());
+	}
+}
