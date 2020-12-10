5ad056bb81ea721275641f142adacfc930e88b18
ecastellano <ecastellano@archlinux>
2020-12-10 19:18:08 +0100

    Fiscal. Mod 184 - 2020. AEAT.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod184/Model184.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod184/Model184.java
@@ -197,7 +197,7 @@ public class Model184 extends MainEntryPoint {
 
 	private void newModel() {
 		cleanErrorPanel();
-		SERVICE.initializeMod184(getCurrentDomainName(),getCurrentUser(),getCurrentDomain(), 2019,
+		SERVICE.initializeMod184(getCurrentDomainName(),getCurrentUser(),getCurrentDomain(), 2020,
 				new AsyncCallback<Mod184>() {
 					@Override
 					public void onSuccess(Mod184 m184) {
