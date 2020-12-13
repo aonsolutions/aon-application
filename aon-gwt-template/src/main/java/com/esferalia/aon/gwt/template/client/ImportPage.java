d47f0e3d58724faf94242710a21ae96afd53f691
Ander Ibáñez de Gauna <aibanez@aonsolutions.es>
2020-12-13 12:13:08 +0100

    Importaciones excel. pdf de ayuda.

--- aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/client/ImportPage.java
+++ aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/client/ImportPage.java
@@ -97,7 +97,7 @@ public class ImportPage extends AonTemplate2{
 	}
 	
 	private void info() {
-		Window.open("https://drive.google.com/file/d/1tw-LHm-NnGqMZoEHNWR1kNR1enAp2Unq/view?usp=sharing", "_blank", null);
+		Window.open("https://drive.google.com/file/d/1FP06gvmneA18j0TmY8Z4_cX6-psxPgmC/view?usp=sharing", "_blank", null);
 	}
 	
 	private void content() {
