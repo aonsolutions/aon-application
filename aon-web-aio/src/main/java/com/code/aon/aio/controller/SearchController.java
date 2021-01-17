da1d62d6ec32e3f2a13d8c23672a143154f855f9
rtrepiana <rtrepiana@aonsolutions.es>
2021-01-17 23:45:43 +0100

    Try to clean filter ????

--- aon-web-aio/src/main/java/com/code/aon/aio/controller/SearchController.java
+++ aon-web-aio/src/main/java/com/code/aon/aio/controller/SearchController.java
@@ -5,7 +5,12 @@ import static com.code.aon.ui.config.controller.ConfigConstants.DOMAIN_SWITCHER;
 
 import java.io.Serializable;
 
+import javax.faces.event.ActionEvent;
+
 import com.code.aon.AonVersion;
+import com.code.aon.common.ManagerBeanException;
+import com.code.aon.ui.admin.controller.DomainsController;
+import com.code.aon.ui.admin.controller.IAdminConstants;
 import com.code.aon.ui.config.controller.ContractSwitcher;
 import com.code.aon.ui.config.controller.DomainSwitcher;
 import com.code.aon.ui.util.AonUtil;
@@ -16,12 +21,25 @@ public class SearchController implements Serializable {
 
 
 	private String filter; 
+	private String beanName;
 	
 	
 	public String getFilter() {
 		return filter;
 	}
 	
+	public void onEditSearch(ActionEvent event) {
+		setFilter(null);
+	}
+
+	public String getBeanName() {
+		return beanName;
+	}
+
+	public void setBeanName(String beanName) {
+		this.beanName = beanName;
+	}
+
 	public void setFilter(String filter) {
 		this.filter = filter;
 		getDomainSwitcher().setFilter(filter);
@@ -32,9 +50,26 @@ public class SearchController implements Serializable {
 		return (DomainSwitcher) AonUtil.getRegisteredBean(DOMAIN_SWITCHER);
 	}
 	
+	public DomainsController getDomainsController() {
+		return (DomainsController) AonUtil.getRegisteredBean(IAdminConstants.DOMAINS_CONTROLLER_NAME);
+	}
+	
 	public ContractSwitcher getContractSwitcher() {
 		return (ContractSwitcher) AonUtil.getRegisteredBean(CONTRACT_SWITCHER);
 	}
 	
+	public void onSelectChildDomain( ActionEvent event) throws ManagerBeanException {
+		getDomainsController().onSelectChildDomain(event);
+		resetFilter();
+	}
+	
+	public void onSelectContractDomain( ActionEvent event) throws ManagerBeanException {
+		getDomainsController().onSelectContractDomain(event);
+		resetFilter();
+	}
+	
+	private void resetFilter() {
+		filter = null;
+	}
 	
 }
