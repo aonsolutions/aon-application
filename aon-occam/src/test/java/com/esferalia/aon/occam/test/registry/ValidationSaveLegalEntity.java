994bb806773e8a32c2a197f6f6c3717ebf404871
ecastellano <ecastellano@aonsolutions.es>
2021-02-08 11:44:40 +0100

    OCCAM Test. Fix ValidationSaveLegalEntity.

--- aon-occam/src/test/java/com/esferalia/aon/occam/test/registry/ValidationSaveLegalEntity.java
+++ aon-occam/src/test/java/com/esferalia/aon/occam/test/registry/ValidationSaveLegalEntity.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.esferalia.aon.occam.api.model.registry.Registry;
+import com.esferalia.aon.occam.api.model.type.Country;
 import com.esferalia.aon.occam.impl.jooq.dao.RegistryDAO;
 import com.esferalia.aon.occam.test.AbstractOccamTest;
 import com.esferalia.aon.occam.test.faker.AonFaker;
@@ -16,9 +17,11 @@ public class ValidationSaveLegalEntity extends AbstractOccamTest {
 	public void test() {
 		Registry registry = AonFaker.getRegistry( ctx );
 		boolean entity = AonDocumentUtil.isEntity( registry.getDocument() );
+		registry.setDocumentCountry( Country.ES );
 		registry.setLegalPerson( !entity );
 		registry = RegistryDAO.insert(ctx, registry);
-		assertEquals(entity, registry.isLegalPerson() );
+		String msg = "Doc: " + registry.getDocument() + " must be "  + AonDocumentUtil.isEntity( registry.getDocument() ) +  " --> " + registry.isLegalPerson(); 
+		assertEquals(msg,entity, registry.isLegalPerson() );
 	}
 
 }
