4da9ec2f47be0adf3f0241f71e4b9b128535ebce
rtrepiana <rtrepiana@aonsolutions.es>
2021-02-08 21:20:53 +0100

    Fix 'PEC 16 - Bonificación SPEE Prog Fomento de Empleo - Cuantía'

--- aon-in-payroll/src/main/java/com/esferalia/aon/in/payroll/tgss/idc/BonusListener.java
+++ aon-in-payroll/src/main/java/com/esferalia/aon/in/payroll/tgss/idc/BonusListener.java
@@ -67,7 +67,7 @@ class BonusListener  implements IdcListener {
 	static final Map<String, String> PEC_EXPRESSION_MAP = new HashMap<String, String>() {
 		{
 			put("01", "( %s ) * %.2f / 100.00"); 															// BONIFICACIÓN INEM
-			put("16", "%2$.2f"); 															// 
+			put("16",  String.format(Locale.ROOT,"%%2$.2f * %s * %s / %s", ContextVariable.PARTIAL_FACTOR, ContextVariable.QUOTE_DAYS, ContextVariable.MONTH_DAYS )); 															// 
 			put("15", String.format(Locale.ROOT,"(%%s) * %%.2f / 100.00 * %1$s",ContextVariable.ERE_FACTOR_FORCE_OFF, ContextVariable.ERE_FACTOR_FORCE, ContextVariable.ERE_FACTOR )); 	// EXONERACIÓN E.R.E. FUERZA MAYOR. TIEMPO PARCIAL
 			put("37", "( %s ) * %.2f / 100.00");
 			put("41", "( %s ) * %.2f / 100.00");
