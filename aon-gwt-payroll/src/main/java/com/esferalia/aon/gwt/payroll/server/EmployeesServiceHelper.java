d2f725e6f745c74e92c9c432c5aac55d3ad89365
rtrepiana <rtrepiana@aonsolutions.es>
2021-02-15 20:13:56 +0100

    Fix compile errors, again :-(

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/server/EmployeesServiceHelper.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/server/EmployeesServiceHelper.java
@@ -9,6 +9,7 @@ import static com.esferalia.aon.payroll.sql.SQLConstants.CONTRACT;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
