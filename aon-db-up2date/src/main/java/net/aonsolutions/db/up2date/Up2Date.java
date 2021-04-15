e13d27a2f22ed3aa441bd753f4c9b033e01d947d
rtrepiana <rtrepiana@aonsolutions.es>
2021-04-15 18:55:04 +0200

    Revert "create table notification, notification_receiver"
    This reverts commit 000dd1ecf150277466d63cc5734311c0f38e6b61.

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,8 +19,6 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import net.aonsolutions.db.up2date.notification.NotificationCreation;
-import net.aonsolutions.db.up2date.notification.NotificationReceiverCreation;
 import net.aonsolutions.db.up2date.payroll.IntegrityFix;
 import net.aonsolutions.db.up2date.payroll.RetirementInsert;
 import net.aonsolutions.db.up2date.security.AuthDeviceUpdate;
@@ -153,9 +151,7 @@ public class Up2Date {
     		//Bases2021Update.BASES2021UPDATE
     		//AuthDeviceUpdate.AUTH_DEVICE_UPDATE,
     		IntegrityFix.INTEGRITYFIX,
-    		RetirementInsert.RETIREMENTINSERT,
-    		NotificationCreation.NOTIFICATION_CREATION,
-    		NotificationReceiverCreation.NOTIFICATION_RECEIVER_CREATION
+    		RetirementInsert.RETIREMENTINSERT
     };
 
 
