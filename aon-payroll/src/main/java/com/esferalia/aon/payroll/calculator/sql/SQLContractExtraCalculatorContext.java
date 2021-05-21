51a1eebd5fbc6a3e5a60b9fdfd8ac79da228afb0
rtrepiana <rtrepiana@aonsolutions.es>
2021-05-21 13:49:09 +0200

    Revert "Protect against EXTRAS editor, soo many errors at this point. :-("
    This reverts commit be772115dfc361aa68289ac5798b8f444ba2d1cb.

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/sql/SQLContractExtraCalculatorContext.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/sql/SQLContractExtraCalculatorContext.java
@@ -125,7 +125,7 @@ public class SQLContractExtraCalculatorContext extends SQLContractSalaryCalculat
 				getExtraPaymentFilter(), 
 				new CompositePayments<IContractPayment>(monthlyQuotedPayments, super.getContractPayments(),getWarnPayment(monthlyQuotedPayments)));
 
-		return getExtraPayments(extraPayments);
+		return extraPayments;
 	}
 
 	@Override
@@ -687,25 +687,6 @@ public class SQLContractExtraCalculatorContext extends SQLContractSalaryCalculat
 		public void sqlContractExtraCalculatorContext(IContractSalaryCalculatorContext ctx) throws AonException;
 	}
 
-	private static final class DelegateExtraPayment extends DelegateContractPayment {
-		private DelegateExtraPayment(IContractPayment contractPayment) {
-			super(contractPayment);
-		}
-
-		@Override
-		public SalaryType getSalaryType() {
-			return SalaryType.EXTRA;
-		}
-	}
-
-	private static Collection<IContractPayment> getExtraPayments(Collection<IContractPayment> payments){
-		List<IContractPayment> extraPayments = new ArrayList<IContractPayment>();
-		
-		payments.forEach( p -> extraPayments.add(new DelegateExtraPayment(p)));
-		
-		return extraPayments;
-	}
-	
 	private static class ExtraPaymentFilter implements FilterCollection.Filter<IContractPayment> {
 		
 		private Month month;
