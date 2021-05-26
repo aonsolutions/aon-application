08056684cb04f8c5e561311c545ea18825dbe645
RAY VASQUEZ <anrto_94_5@hotmail.com>
2021-05-26 10:15:17 +0200

    NEW SUITE. iframe embed maps not free :-(

--- aon-solutions/src/main/webapp/environments/constants.js
+++ aon-solutions/src/main/webapp/environments/constants.js
@@ -222,6 +222,7 @@ export const WITHHOLDING_FARMER = 'withholdingFarmer';
 export const API_KEY_MAP = 'AIzaSyAr2rzslqS8G6uy5IWEczcPFHpaXOfVjO4';
 
 export const URL_MAP = `https://maps.googleapis.com/maps/api/js?key=${API_KEY_MAP}&hl=es&callback=showNewMap`;
+export const URL_MAP_EMBED = `https://www.google.com/maps/embed/v1/place?key=${API_KEY_MAP}`;
 
 
 
