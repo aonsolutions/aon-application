8f0e8f74fdce0f8647cd076f727578745e9dcaff
nintzatekeen <09iker05@gmail.com>
2021-05-31 17:39:12 +0200

    Trying stuff with before and after

--- aon-appium/src/test/java/com/esferalia/aon/appium/timeControl/TimeControlTestCase.java
+++ aon-appium/src/test/java/com/esferalia/aon/appium/timeControl/TimeControlTestCase.java
@@ -1,10 +1,15 @@
 package com.esferalia.aon.appium.timeControl;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.esferalia.aon.appium.AbstractTestCase;
 
@@ -15,23 +20,53 @@ public class TimeControlTestCase extends AbstractTestCase {
 		setUpTestDefaultData();
 		setUpTestCase(apk, username, password, driver);
 	}
-	
-	@Before
-	public void openApp() {
-		//app.activateApp("aon APP");
-		
+
+//	@Before
+//	public void backToHome() {
+//		WebDriverWait wait = new WebDriverWait(app, 10);
+//		app.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+//		
+//		WebElement homeIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("aonMobileMenuHomeButtonIcon")));
+//		homeIcon.click();
+//	}
+
+	@After
+	public void closeAtEnd() {
+		app.closeApp();
 	}
-	
+
+	@Before
+	public void openOnStart() {
+		app.launchApp();
+	}
+
 	@Test
 	public void signInTest() {
-		console.info("HOLIIIII");	
+
 	}
 
-	
 	@Test
-	public void signInMultipleClickTest() {
-		console.info("QUE TAAAAL");	
+	public void filterTest() {
+		try {
+			WebDriverWait wait = new WebDriverWait(app, 10);
+//		app.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			
+			WebElement timeElem = wait.until(ExpectedConditions
+					.presenceOfElementLocated(By.cssSelector("*[id='aonMobileMenuControl HorarioButtonIcon']")));
+			timeElem.click();
+
+//		app.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+			WebElement filterElem = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.id("aonSigninToolbarHeaderToolSectionfilterButtonIcon")));
+			filterElem.click();
+
+			WebElement closeElement = wait.until(
+					ExpectedConditions.visibilityOfElementLocated(By.id("aonFilterDialogMenuFilterDialogClick")));
+			closeElement.click();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	
 }
