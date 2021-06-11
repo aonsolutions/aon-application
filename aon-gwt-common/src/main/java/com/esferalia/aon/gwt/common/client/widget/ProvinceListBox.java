c25d17d62c63ef538fd9813033189d5ba619f0fe
ecastellano <ecastellano@aonsolutions.es>
2021-06-11 12:58:53 +0200

    Fiscal. Iconos, CSS. Preparando el camino.

--- aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/widget/ProvinceListBox.java
+++ aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/widget/ProvinceListBox.java
@@ -1,19 +1,15 @@
 package com.esferalia.aon.gwt.common.client.widget;
 
 
-import com.esferalia.aon.gwt.common.client.i18n.CommonMessages;
 import com.esferalia.aon.occam.api.model.type.Province;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.ListBox;
 
 public class ProvinceListBox extends ListBox {
 
-	private static final CommonMessages MSGS = GWT.create(CommonMessages.class);
-	
 	public ProvinceListBox() {
 		setWidth("120px");
 		for (Province p : Province.values()) {
-			this.addItem( MSGS.provinceName( p ));	
+			this.addItem( p.getName() );	
 		}
 	}
 	
