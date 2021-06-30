25956d76c40468cf4ea9adb14c771dea05ff578e
RAY VASQUEZ <anrto_94_5@hotmail.com>
2021-06-30 18:03:20 +0200

    Servlet taskHolder for workgroup, TaskServlet

--- aon-api/src/main/java/net/aonsolutions/aon/api/servlet/task/TaskServlet.java
+++ aon-api/src/main/java/net/aonsolutions/aon/api/servlet/task/TaskServlet.java
@@ -4,13 +4,11 @@ import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import com.esferalia.aon.occam.api.AON;
-import com.esferalia.aon.occam.api.model.Domain;
-
+import com.esferalia.aon.occam.api.AON_SOLUTIONS;
+import com.esferalia.aon.occam.api.json.IJsonNames;
+import com.esferalia.aon.occam.api.json.JsonUtils;
+import com.esferalia.aon.occam.api.json.TaskJSON;
+import com.esferalia.aon.occam.api.model.task.Task;
 import net.aonsolutions.aon.api.ewok.AonApiData;
 import net.aonsolutions.aon.api.servlet.AonApiHttpServlet;
 
@@ -46,7 +44,7 @@ public class TaskServlet extends AonApiHttpServlet{
 			AonApiData api = initialize(req, resp);
 			switch (api.getPath()) {
 				case "/":
-					response(req, resp, new JSONObject());
+					response(req, resp, saveTask(api));
 				break;
 				default:
 					throw new Exception("La ruta introducida es incorrecta.");
@@ -58,45 +56,22 @@ public class TaskServlet extends AonApiHttpServlet{
 	}
 
 	private Object getTasks(AonApiData api) {
-		Domain domain = api.getDomain();
-		JSONArray array = new JSONArray();
-		AON.getTaskStream(domain.getName(), domain.getId(), "",
-				f-> f.getDomainProperty().eq(domain.getId())
-//				f->f.getIdProperty().isNotNull()
-		)
-		.forEach(task->{
-			JSONObject json = new JSONObject();
-			json.put("id", task.getId());
-			json.put("domain", task.getDomain());
-			json.put("description", task.getDescription());
-			json.put("priority", task.getPriority());
-			json.put("status", task.getStatus());
-			json.put("percent", task.getPercent());
-			json.put("taskHolder", task.getTaskHolder());
-			json.put("workgroup", task.getWorkgroup());
-			json.put("source", task.getSource());
-			json.put("sourceId", task.getSourceId());
-			json.put("project", task.getProject());
-			json.put("registry", task.getRegistry());
-			json.put("activityType", task.getActivityType());
-			json.put("sender", task.getSender());
-			json.put("comments", task.getComments());
-			json.put("repeatPeriod", task.getRepeatPeriod());
-			json.put("gtaskId", task.getGtaskId());
-			json.put("gtasklistId", task.getGtasklistId());
-			json.put("number", task.getNumber());
-			json.put("creationUser", task.getCreationUser());
-			json.put("number", task.getNumber());
-			json.put("modificationUser", task.getModificationUser());
-			json.put("parent", task.getParent());
-			json.put("creationDate", task.getCreationDate()!=null ? task.getCreationDate().getTime() : null);
-			json.put("modificationDate", task.getModificationDate()!=null ? task.getModificationDate().getTime() : null);
-			json.put("startDate", task.getStartDate()!=null ? task.getStartDate().getTime() : null);
-			json.put("endDate",  task.getEndDate()!=null ? task.getEndDate().getTime() : null);
-			json.put("dueDate", task.getDueDate()!=null ? task.getDueDate().getTime(): null);
-			json.put("dueDate", task.getDueDate()!=null ? task.getDueDate().getTime(): null);
-			array.put(json);
-		});
-		return array;
+		return TaskJSON.toJSON(
+				AON_SOLUTIONS.getTaskStream(
+						api.getDomain(), api.getUser(), 
+						f-> f.getDomainProperty().eq(api.getDomain().getId())
+				)
+		);
+	}
+	
+	private Object saveTask(AonApiData api) {
+		Task task = TaskJSON.fromJSON(api.getData());
+		addWorkflow(api, task); //ADD WORKFLOW
+		return TaskJSON.toJSON(AON_SOLUTIONS.saveTask(api.getDomain(), api.getUser(), task));
+	}
+	
+	private void addWorkflow(AonApiData api, Task task) {
+		String comment = api.getData().getString("comment");
+		System.out.println(comment);
 	}
 }
