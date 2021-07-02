fedc27a7ba35ffba5a17a21414a3b7e1d3a636e9
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2021-07-02 14:33:26 +0200

    JooqContrataContract and JooqEmployee changes

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqContrataContract.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqContrataContract.java
@@ -2,6 +2,7 @@ package com.esferalia.aon.gwt.payroll.jooq;
 
 import static com.esferalia.aon.jooq.tables.Agreement.AGREEMENT;
 import static com.esferalia.aon.jooq.tables.AgreementLevel.AGREEMENT_LEVEL;
+import static com.esferalia.aon.jooq.tables.Certifica2BatchDetail.CERTIFICA2_BATCH_DETAIL;
 import static com.esferalia.aon.jooq.tables.Cno.CNO;
 import static com.esferalia.aon.jooq.tables.Contract.CONTRACT;
 import static com.esferalia.aon.jooq.tables.ContractAttach.CONTRACT_ATTACH;
@@ -22,6 +23,7 @@ import static com.esferalia.aon.jooq.tables.Rmedia.RMEDIA;
 import static com.esferalia.aon.jooq.tables.Rpaymethod.RPAYMETHOD;
 import static com.esferalia.aon.jooq.tables.Salary.SALARY;
 import static com.esferalia.aon.jooq.tables.Scope.SCOPE;
+import static com.esferalia.aon.jooq.tables.SepeBatchAttach.SEPE_BATCH_ATTACH;
 import static com.esferalia.aon.jooq.tables.Workplace.WORKPLACE;
 
 import java.io.ByteArrayOutputStream;
@@ -41,6 +43,7 @@ import javax.xml.bind.JAXBException;
 
 import org.jooq.DSLContext;
 import org.jooq.Record;
+import org.jooq.Record1;
 import org.jooq.Record10;
 import org.jooq.Record2;
 import org.jooq.Result;
@@ -1793,6 +1796,32 @@ public class JooqContrataContract {
 		}
 		
 		contractData.setContractJourneyDuration(journies);
+		
+		// ---------------------------------------------- CheckSettle and Certifica2
+		
+		Result<Record> settlementRecords = dslContext.select().from(SALARY)
+				.where(SALARY.CONTRACT.eq(contractId))
+				.and(SALARY.TYPE.eq((byte)2))
+				.orderBy(SALARY.ID.desc())
+				.fetch();
+		
+		contractData.setHasSettle(settlementRecords.isNotEmpty());
+		
+		List<Integer> certifca2BatachIds = dslContext.select(CERTIFICA2_BATCH_DETAIL.CERTIFICA2_BATCH)
+				.from(CERTIFICA2_BATCH_DETAIL)
+				.where(CERTIFICA2_BATCH_DETAIL.CONTRACT.eq(contractId))
+				.fetch(CERTIFICA2_BATCH_DETAIL.CERTIFICA2_BATCH);
+		
+		if(certifca2BatachIds.isEmpty()) {
+			contractData.setHasCertifica2(false);
+		} else {
+			Record1<Integer> sepeBatchAttachRecord = dslContext.select(SEPE_BATCH_ATTACH.ID).from(SEPE_BATCH_ATTACH)
+				.where(SEPE_BATCH_ATTACH.SOURCE_BATCH.eq(certifca2BatachIds.get(0)))
+				.and(SEPE_BATCH_ATTACH.MIMETYPE.eq((byte)5))
+				.fetchOne();
+			
+			contractData.setHasCertifica2(sepeBatchAttachRecord != null);
+		}
 		
 		employeeContractInfo.setEmployeeInfo(employeeData);
 		employeeContractInfo.setContractInfo(contractData);
