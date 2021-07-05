de03e72fe76f2e4c7bf0675084f556f057c55f17
nintzatekeen <09iker05@gmail.com>
2021-07-05 12:25:35 +0200

    Improved amount checking method

--- aon-selenium/src/test/java/solutions/aon/selenium/aio/NetAndGrossTestCase.java
+++ aon-selenium/src/test/java/solutions/aon/selenium/aio/NetAndGrossTestCase.java
@@ -25,7 +25,12 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -42,6 +47,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import solutions.aon.selenium.tools.Logger;
+import solutions.aon.selenium.tools.Logger.Status;
 import solutions.aon.selenium.tools.SeleniumTools;
 
 public class NetAndGrossTestCase extends AioBaseTestCase {
@@ -104,7 +110,7 @@ public class NetAndGrossTestCase extends AioBaseTestCase {
       
         if (!SeleniumTools.waitUntilElementContains(driver, By.id("gwt-debug-monthListBox-item0"), df.format(new Date())))
         	fail("Didn't find month");
-        
+        Thread.sleep(2000);
         
         String chimboSelector = "#rootPanel table td:nth-child(3) td:nth-child(2) div span";
       
@@ -121,6 +127,7 @@ public class NetAndGrossTestCase extends AioBaseTestCase {
         
         Double diff =  safeDouble(payments,0.00) - safeDouble(deductions,0.00);
         assertEquals(diff, total);
+        checkFieldsV2(driver);
         log(SUCCESS, "DONE.");
         
         
@@ -151,39 +158,61 @@ public class NetAndGrossTestCase extends AioBaseTestCase {
                 
         diff =  safeDouble(payments,0.00) - safeDouble(deductions,0.00);
         assertEquals(diff, total);
+        
+        retryingFindClick(driver, By.id("gwt-debug-monthListBox"));
+		
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.MONTH, -1);
+		Thread.sleep(1000);
+		selectMonth(driver, c.getTime());
+        Thread.sleep(2000);
+		
+        
+        
+        
+        
         log(SUCCESS, "DONE.");
 	}
 	
+	
+	//IGNORED BECAUSE IT FAILS -> THE DRAFT HAS A PROBLEM WHEN ROUNDING AMOUNTS
+//	@Ignore
 	@Test
 	public void netTest() throws InterruptedException {
 		
 //		ENTER 'INTEGRAL DE NÓMINAS'
+		log(CLICK, "Entering \"Laboral\"");
 		retryingFindClick(driver, By.cssSelector("a[id='aonContent:mainMenuForm:menu_payroll']"));
 		Thread.sleep(1000);
+		log(CLICK, "Entering \"Integral de Nóminas\"");
 		retryingFindClick(driver, By.cssSelector("*[id='aonContent:payrollMenu:gwt_employee']"));
 		
 		String dummiesId = "gwt-debug-4dummies";
 		By dummies = By.cssSelector("#" + dummiesId + " > table > tbody > tr > td:nth-of-type(1)");
 		wait.until(ExpectedConditions.elementToBeClickable(dummies));
 		Thread.sleep(500);
+		log(CLICK, "Deploying \"4DUMMIES\"");
 		retryingFindClick(driver, dummies);
 		
 		String netConstantId = "gwt-debug-constante,_neto";
 		Thread.sleep(500);
+		log(CLICK, "Deploying \"CONSTANTE, BRUTO\"");
 		retryingFindClick(driver, By.cssSelector("div [id='" + netConstantId + "'] > table tr td:nth-of-type(1)"));
 		
 		String draftId = "gwt-debug-constante,_neto-draft-content";
 		Thread.sleep(500);
+		log(CLICK, "Entering \"Borrador\"");
 		retryingFindClick(driver, By.id(draftId));
 		Thread.sleep(500);
+		log(CLICK, "Selecting previous month");
 		retryingFindClick(driver, By.id("gwt-debug-monthListBox"));
 		
 		Calendar c = Calendar.getInstance();
 		c.add(Calendar.MONTH, -1);
 		Thread.sleep(1000);
 		selectMonth(driver, c.getTime());
-		
-		checkFields(driver);
+		log(Status.COMPARE, "Checking fields");
+		checkFieldsV2(driver);
 		
 		//CHANGE NET
 		
@@ -196,7 +225,7 @@ public class NetAndGrossTestCase extends AioBaseTestCase {
 			netInput.sendKeys(Keys.BACK_SPACE);
 			netInput.sendKeys(Keys.ARROW_RIGHT);
 		}
-		
+		log(Status.INPUT, "Changing net");
 		netInput.sendKeys("2500");
 		
 		
@@ -204,8 +233,9 @@ public class NetAndGrossTestCase extends AioBaseTestCase {
 		
 		
 		Thread.sleep(2000);
-		checkFields(driver);
-		
+		log(Status.COMPARE, "Checking fields after changing net");
+		checkFieldsV2(driver);
+		log(SUCCESS, "Net test succeded");
 	}
 	
 	
@@ -271,5 +301,126 @@ public class NetAndGrossTestCase extends AioBaseTestCase {
 		Double actualTotalDeductions = getAmount(driver, By.cssSelector("#rootPanel  table  td:nth-child(3)  td:nth-child(2) div.aon-text-center > span:nth-child(1)"));
 		
 		assertEquals(actualTotalDeductions, totalDeductions);
+	}
+	
+	private static List<Double> getPayments(WebDriver driver) {
+		List<WebElement> paymentElems = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//tr[contains(@id, 'gwt-debug-payment-row')]//input[contains(@id, 'gwt-debug-db-amount-label')]")));
+		
+		HashSet<String> paymentIds = new HashSet<String>();
+		
+		for (WebElement elem : paymentElems) {
+			paymentIds.add(elem.getAttribute("id"));
+		}
+		
+        List<Double> paymentsList = new LinkedList<Double>();
+        
+        for (String id : paymentIds) {
+        	paymentsList.add(getAmount(driver, By.id(id)));
+        }
+		
+        return paymentsList;
+        
+        
+	}
+	
+	private static Map<String, Double> getDeductions(WebDriver driver) {
+		List<WebElement> deductionElems = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("#gwt-debug-paymentsTable :not(tr[style='display: none;']) tr tr span[id^='gwt-debug']")));
+		
+		HashSet<String> deductionIds = new HashSet<String>();
+		
+		for (WebElement elem : deductionElems) {
+			
+			deductionIds.add(elem.getAttribute("id"));
+			
+		}
+        
+        HashMap<String, Double> deductionMap = new HashMap<String, Double>();
+        
+        
+        int noIdNumber = 0;
+        for (String id : deductionIds) {
+        	
+        	String key = id != null ? id : "no_id_"+noIdNumber++; 
+        	
+			deductionMap.put(key, getAmount(driver, By.id(id)));
+        }
+        
+        return deductionMap;
+	}
+	
+
+	private static void checkFieldsV2(WebDriver driver) {
+		String netLiquidId = "gwt-debug-totalLiquidLabel";
+		
+		Double gross = getAmount(driver, By.id("gwt-debug-totalPaymentsLabel"));
+				
+//		String paymentsSelector = "input.gwt-TextBox.aon-text-right[disabled]";
+		
+		Map<String, Double> deductions = getDeductions(driver);
+		
+		Double totalDeductions = deductions.values().stream().reduce((acc, value) -> acc + value).orElse(0d);
+		totalDeductions = SeleniumTools.unmessDouble(totalDeductions);
+		
+		
+		Double actualTotalDeductions = getAmount(driver, By.cssSelector("#rootPanel  table  td:nth-child(3)  td:nth-child(2) div.aon-text-center > span:nth-child(1)"));
+		
+		assertEquals(actualTotalDeductions, totalDeductions);
+		
+		List<Double> payments = getPayments(driver);
+		
+		By netLiquid = By.id(netLiquidId);
+		Double net = getAmount(driver, netLiquid);
+		Double totalPayment = getAmount(driver, By.id("gwt-debug-totalPaymentLabel"));
+		
+		Double expectedGross = payments.stream().reduce((acc, val) -> acc + val).orElse(0d);
+		
+		assertEquals(expectedGross, totalPayment);
+		
+		Double expectedNet = SeleniumTools.unmessDouble(totalPayment - totalDeductions);
+		
+		assertEquals(expectedNet, net);
+		
+		//-----CHECKING PERCENTS-----
+		
+		log(Status.COMPARE, "Checking percentages");
+		
+		Double ccBase = getAmount(driver, By.id("gwt-debug-cgcBaseLabel"));
+		Double atEpBase = getAmount(driver, By.id("gwt-debug-cgpBaseLabel"));
+		Double irpfBase = getAmount(driver, By.xpath("//*[div[text()='BASE IRPF']]//span[1]"));
+		
+		deductions.keySet().stream().filter(key -> !key.contains("no_id")).forEach(id -> {
+			
+			Double base = 0d;
+			
+			switch (id) {
+				case "gwt-debug-irpf":
+					base = irpfBase;
+					break;
+				case "gwt-debug-common_contingency":
+				case "gwt-debug-unemployment":
+				case "gwt-debug-job_training":
+					base = ccBase;
+					break;
+				default:
+					base = ccBase;
+					
+			}
+			
+			Double percent = getAmount(driver, By.xpath("//*[@id='" + id + "']/../../../../../../td[2]"));
+			percent = percent != null ? percent : getAmount(driver, By.xpath("//*[@id='" + id + "']/../../../../../../td[2] //input"));
+			
+			Double actualAmount = deductions.get(id);
+			
+			Double expectedAmount = SeleniumTools.unmessDouble(base * percent / 100);
+			
+			assertEquals(expectedAmount, actualAmount);
+			
+		});
+		log(Status.END, "Percentage check end");
+		
+		//-----------------------------
+		
+		
+		
 	}
 }
