eb085796ddb09e029969848ee4c1967ec82ba373
nintzatekeen <09iker05@gmail.com>
2021-07-08 13:01:28 +0200

    More tests and scrolling month improved

--- aon-selenium/src/test/java/solutions/aon/selenium/aio/GeneralIntegralTest.java
+++ aon-selenium/src/test/java/solutions/aon/selenium/aio/GeneralIntegralTest.java
@@ -4,14 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static solutions.aon.selenium.aio.id.AonHeaderId.LABORAL_BUTTON;
 import static solutions.aon.selenium.aio.id.LaboralId.INTEGRAL_DE_NOMINAS;
-import static solutions.aon.selenium.tools.Logger.log;
-import static solutions.aon.selenium.tools.Logger.Status.CLICK;
 import static solutions.aon.selenium.tools.SeleniumTools.retryingFindClick;
 
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.regex.Pattern;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -122,8 +119,6 @@ public class GeneralIntegralTest extends AioBaseTestCase {
 		assertEquals("CONCEPTO ANTIGUEDAD, DESCRIPCION", elementText);
 	}
 	
-	
-	//TODO: Pasar esto a Selenium
 	@Test
 	public void TestAtrasos() throws Exception {
 		
@@ -167,6 +162,7 @@ public class GeneralIntegralTest extends AioBaseTestCase {
 			Assert.fail("'redo' button not hidden");
 		
 		SeleniumTools.selectPayrollType(driver, SALARY_TYPE.SALARY);
+		SeleniumTools.selectMonthScrollingV2(driver, calendar.getTime());
 		
 		SeleniumTools.safeInput(driver, "#gwt-debug-description-box-new-payment", "[3]ATRASOS");
 		WebElement input = wait.until(ExpectedConditions.elementToBeClickable(By.id("gwt-debug-description-box-new-payment")));
@@ -176,6 +172,134 @@ public class GeneralIntegralTest extends AioBaseTestCase {
 		input.sendKeys(Keys.TAB);
 		boolean checkInput = SeleniumTools.changingElementAssert(driver, By.id("gwt-debug-description-box-3"), "value", "[3]ATRASOS");
 		assertTrue("[3]ATRASOS not found", checkInput);
+		
+		SeleniumTools.acceptDraft(driver);
+		
+		By deleteButton = By.id("gwt-debug-delete-button-3");
+		wait.until(ExpectedConditions.elementToBeClickable(deleteButton));
+		retryingFindClick(driver, deleteButton);
+		SeleniumTools.acceptDraft(driver);
+		
+		SeleniumTools.selectPayrollType(driver, SALARY_TYPE.SALARY);
+		SeleniumTools.selectMonthScrollingV2(driver, calendar.getTime());
+		
+		SeleniumTools.safeInput(driver, "#gwt-debug-description-box-new-payment", "[3]ATRASOS");
+		input = wait.until(ExpectedConditions.elementToBeClickable(By.id("gwt-debug-description-box-new-payment")));
+		input.sendKeys(Keys.TAB);
+		SeleniumTools.safeInput(driver, "#gwt-debug-amount-box-new-payment", "100");
+		input = wait.until(ExpectedConditions.elementToBeClickable(By.id("gwt-debug-amount-box-new-payment")));
+		input.sendKeys(Keys.TAB);
+		checkInput = SeleniumTools.changingElementAssert(driver, By.id("gwt-debug-description-box-3"), "value", "[3]ATRASOS");
+		assertTrue("[3]ATRASOS not found", checkInput);
+		
+		SeleniumTools.delay(driver, calendar.getTime(), new Date());
+		Double ccBase = SeleniumTools.getAmount(driver, By.id("gwt-debug-cgcBaseLabel"));
+		assertEquals((Double)0.00d, ccBase);
+		Double payments = SeleniumTools.getAmount(driver, By.id("gwt-debug-totalPaymentsLabel"));
+		assertEquals((Double)0.00d, payments);
+		Double liquid = SeleniumTools.getAmount(driver, By.id("gwt-debug-totalLiquidLabel"));
+		assertEquals((Double)0.00d, liquid);
+	}
+	
+	@Test
+	public void TestBasesMaximasYMinimas() throws Exception {
+		
+//		ENTER 'INTEGRAL DE NÓMINAS'
+		SeleniumTools.integralFromIndex(driver);
+//		OPEN 'BASES MÁXIMAS Y MÍNIMAS' WORKPLACE
+		SeleniumTools.openWorkplace(driver, "gwt-debug-bases_maximas_y_minimas");
+		
+		SeleniumTools.draft(driver, "BASE, MÁXIMA ( GRUPO 01 )");
+		checkCgcAndCgpByDate(2016, Calendar.DECEMBER, 3642.00, 3642.00);
+		checkCgcAndCgpByDate(2017, Calendar.JANUARY, 3751.20, 3751.20);
+		checkCgcAndCgpByDate(2018, Calendar.JANUARY, 3751.20, 3751.20);
+		checkCgcAndCgpByDate(2018, Calendar.JULY, 3751.20, 3751.20);
+		checkCgcAndCgpByDate(2018, Calendar.AUGUST, 3803.70, 3803.70);
+		checkCgcAndCgpByDate(2019, Calendar.AUGUST, 4070.10, 4070.10);
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA ( GRUPO 01 )");
+		checkCgcAndCgpByDate(2016, Calendar.DECEMBER, 1067.40, 764.40);
+		checkCgcAndCgpByDate(2017, Calendar.JANUARY, 1152.90, 825.60);
+		checkCgcAndCgpByDate(2018, Calendar.JANUARY, 1199.10, 858.60);
+		checkCgcAndCgpByDate(2019, Calendar.JANUARY, 1466.40, 1050.00);
+		
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA ( GRUPO 02 )");
+		checkCgcAndCgpByDate(2018, Calendar.JANUARY, 994.20, 858.60);
+		checkCgcAndCgpByDate(2019, Calendar.JANUARY, 1215.90, 1050.00);
+
+		SeleniumTools.draft(driver, "BASE, MÍNIMA ( GRUPO 03 )");
+		checkCgcAndCgpByDate(2018, Calendar.JANUARY, 864.90, 858.60);
+		checkCgcAndCgpByDate(2019, Calendar.JANUARY, 1057.80, 1050.00);
+
+		SeleniumTools.draft(driver, "BASE, MÍNIMA ( GRUPO 04 )");
+		checkCgcAndCgpByDate(2018, Calendar.JANUARY, 858.60, 858.60);
+		checkCgcAndCgpByDate(2019, Calendar.JANUARY, 1050.00, 1050.00);
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA ( GRUPO 09 )");
+		checkCgcAndCgpByDate(2019, Calendar.AUGUST, 1050.00, 1050.00);
+		checkCgcAndCgpByDate(2019, Calendar.SEPTEMBER, 1050.00, 1050.00);
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA ( GRUPO 10 )");
+		checkCgcAndCgpByDate(2019, Calendar.AUGUST, 35.00 * 31, 1050.00);
+		checkCgcAndCgpByDate(2019, Calendar.SEPTEMBER, 1050.00, 1050.00);
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA IT ( GRUPO 01 )");
+		checkCgcByDate(2016, Calendar.JUNE, 1067.40);
+		checkCgcByDate(2016, Calendar.JULY, 1067.40);
+		checkCgcByDate(2017, Calendar.JANUARY, 1152.90);
+		checkCgcByDate(2018, Calendar.JANUARY, 1199.10);
+		checkCgcByDate(2019, Calendar.JANUARY, 1466.40);
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA PARCIAL ( HORAS )");
+		checkCgcByDate(2019, Calendar.AUGUST, 6.33 * 44.00);
+		checkCgcAndCgpByDate(2019, Calendar.SEPTEMBER, 6.33 * 42.00, 6.33 * 42.00);
+		
+		SeleniumTools.draft(driver, "BASE, MÍNIMA PARCIAL ( MENSUAL )");
+		checkCgcByDate(2019, Calendar.AUGUST, 1050.00 * 0.25);
+		checkCgcByDate(2019, Calendar.SEPTEMBER, 1050.00 * 0.25);
+		
+	}
+	
+	private void checkCgcAndCgpByDate(int year, int month, Double expectedCgcBase, Double expectedCgpBase) throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		SeleniumTools.resetCalendar(calendar);
+		
+		SeleniumTools.selectMonthScrollingV2(driver, calendar.getTime());
+		Thread.sleep(500);
+		SeleniumTools.checkSalaryPeriod(driver, calendar.getTime());
+		Double cgcBase = SeleniumTools.getAmount(driver, By.id("gwt-debug-cgcBaseLabel"));
+		assertEquals(expectedCgcBase, cgcBase);
+		Double cgpBase = SeleniumTools.getAmount(driver, By.id("gwt-debug-cgpBaseLabel"));
+		assertEquals(expectedCgpBase, cgpBase);
+	}
+	
+	private void checkCgcByDate(int year, int month, Double expectedCgcBase) throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		SeleniumTools.resetCalendar(calendar);
+		
+		SeleniumTools.selectMonthScrollingV2(driver, calendar.getTime());
+		Thread.sleep(500);
+		SeleniumTools.checkSalaryPeriod(driver, calendar.getTime());
+		Double cgcBase = SeleniumTools.getAmount(driver, By.id("gwt-debug-cgcBaseLabel"));
+		assertEquals(expectedCgcBase, cgcBase);
+	}
+	
+	private void checkCgpByDate(int year, int month, Double expectedCgpBase) throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		SeleniumTools.resetCalendar(calendar);
+		
+		SeleniumTools.selectMonthScrollingV2(driver, calendar.getTime());
+		Thread.sleep(500);
+		SeleniumTools.checkSalaryPeriod(driver, calendar.getTime());
+		Double cgpBase = SeleniumTools.getAmount(driver, By.id("gwt-debug-cgpBaseLabel"));
+		assertEquals(expectedCgpBase, cgpBase);
 	}
 	
 }
