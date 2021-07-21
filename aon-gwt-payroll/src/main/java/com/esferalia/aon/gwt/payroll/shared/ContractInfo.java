a38da09cfa7b5b061df35fbd216470c69374d249
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2021-07-21 11:05:04 +0200

    Add movPrevDelete and altaConsolidadaDelete methods to ContrataEmployee

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/shared/ContractInfo.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/shared/ContractInfo.java
@@ -69,6 +69,8 @@ public class ContractInfo implements Serializable{
 	private boolean hasSettle;
 	private boolean hasCertifica2;
 	
+	private boolean isTGSSActive;
+	
 	public ContractInfo() {
 		super();
 		this.contractId = null;
@@ -470,6 +472,14 @@ public class ContractInfo implements Serializable{
 
 	public void setHasCertifica2(boolean hasCertifica2) {
 		this.hasCertifica2 = hasCertifica2;
+	}
+	
+	public boolean isTGSSActive() {
+		return this.isTGSSActive;
+	}
+	
+	public void setIsTGSSActive(boolean isTGSSActive) {
+		this.isTGSSActive = isTGSSActive;
 	}
 
 	public String toString(){
