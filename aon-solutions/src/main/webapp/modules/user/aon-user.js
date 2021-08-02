e0022900fc1f608c5eab2a88d6d5fbe333724793
anderibz <aibanez@aonsolutions.es>
2021-08-02 12:30:24 +0200

    New Suite. user next & previous fix.

--- aon-solutions/src/main/webapp/modules/user/aon-user.js
+++ aon-solutions/src/main/webapp/modules/user/aon-user.js
@@ -17,7 +17,7 @@ import { MSG, MATERIAL_ICONS, CONSTANT } from '../../environments/environments.j
 import * as ACTION from '../actions.js';
 import { AonMobileUserList } from './aon-mobile-user-list.js';
 import { AonUserList } from './aon-user-list.js';
-import { getNextUser, getPreviousUser, getUsers } from './UserCache.js';
+import { getNextUser, getPreviousUser, getUsers, updateUser } from './UserCache.js';
 
 export class AonUser extends AonElement {
 
@@ -439,6 +439,7 @@ export class AonUser extends AonElement {
 	}
 
 	save() {
+		updateUser(this._user);
 		if(!this._user.portal && (!this._user.roles || this._user.roles.length == 0)) {
 			this._user.portal = true;
 			this._user.roles = [Role.ENTERPRISE];
