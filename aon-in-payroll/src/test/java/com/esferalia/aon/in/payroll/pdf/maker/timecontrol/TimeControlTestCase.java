1dca3371295991d2384887fe666c07dccc688bba
nintzatekeen <09iker05@gmail.com>
2021-08-17 12:05:10 +0200

    Time control PDF

---
+++ aon-in-payroll/src/test/java/com/esferalia/aon/in/payroll/pdf/maker/timecontrol/TimeControlTestCase.java
@@ -0,0 +1,64 @@
+package com.esferalia.aon.in.payroll.pdf.maker.timecontrol;
+
+import static org.junit.Assert.fail;
+
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Locale;
+
+import org.junit.Test;
+
+import com.esferalia.aon.in.payroll.pdf.maker.exception.CanNotCreatePdfException;
+import com.esferalia.aon.in.payroll.pdf.maker.timecontrol.TimeControlTemplate;
+import com.esferalia.aon.in.payroll.pdf.maker.timecontrol.bean.EmployeeData;
+import com.github.javafaker.Faker;
+
+public class TimeControlTestCase {
+	
+	@Test
+	public void test() throws FileNotFoundException {
+		int repeats = Faker.instance().number().numberBetween(5, 50);
+		for (int i=0; i<repeats; i++) {
+			singleTest();
+		}
+	}
+
+	private void singleTest() throws FileNotFoundException {
+		Faker faker = Faker.instance(new Locale("es", "ES"));
+		int pages = faker.number().numberBetween(5, 20);
+		String enterprise = nullProbability(faker.company().name(), 15);
+		String cif = nullProbability(String.valueOf(faker.number().numberBetween(1000000000, 999999999)), 15);
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MONTH, Calendar.JANUARY);
+		Date from = cal.getTime();
+		cal.set(Calendar.MONTH, Calendar.DECEMBER);
+		Date period = faker.date().between(from, cal.getTime());
+		LinkedList<EmployeeData> employees = new LinkedList<EmployeeData>();
+		for (int i=0; i< pages; i++) {
+			EmployeeData emp = new EmployeeData();
+			emp.setContract(nullProbability(String.valueOf(faker.number().numberBetween(100, 999)), 15));
+			emp.setDni(nullProbability(faker.code().isbn10(false), 15));
+			emp.setNaf(nullProbability(faker.code().isbn10(false), 15));
+			emp.setEmployeeName(nullProbability(faker.gameOfThrones().character(), 15));
+			employees.add(emp);
+		}
+		try {
+//			OutputStream os = new FileOutputStream("/home/igonzalez/Escritorio/pedefes/faker.pdf");
+			TimeControlTemplate.print(OutputStream.nullOutputStream(), enterprise, cif, employees, period);
+		} catch (CanNotCreatePdfException e) {
+			fail();
+		}
+	}
+	
+	private static <E> E nullProbability (E item, int percentage) {
+		int rand = (int)(Math.random() * 100) + 1;
+		if (rand < percentage)
+			return null;
+		else
+			return item;
+	}
+	
+}
