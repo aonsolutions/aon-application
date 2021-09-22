889b6f4d84f991e8de2537aa4c65454ea2e010f9
anderibz <aibanez@aonsolutions.es>
2021-09-22 17:00:22 +0200

    SII. periodo de liquidación.

--- aon-sii/src/main/java/net/aonsolutions/aon/sii/bizkaia/FacturasRecibidas.java
+++ aon-sii/src/main/java/net/aonsolutions/aon/sii/bizkaia/FacturasRecibidas.java
@@ -186,9 +186,6 @@ public class FacturasRecibidas extends SIIBuilt {
 
 		LRFacturasRecibidasType factura = new LRFacturasRecibidasType();
 
-		// PeriodoLiquidacion || PeriodoImpositivo
-		factura.setPeriodoLiquidacion(periodoLiquidacion(vat, false));
-
 		// IDFactura
 		IDFacturaRecibidaType f = new IDFacturaRecibidaType();
 
@@ -242,6 +239,8 @@ public class FacturasRecibidas extends SIIBuilt {
 				AppParam.FS_MODEL_CFG_SII);
 		Boolean isRegistro = "R".equals(ap.getValue());
 		Date opDate = isRegistro ? vat.getCreationDate() : vat.getTaxDate();
+		// PeriodoLiquidacion || PeriodoImpositivo
+		factura.setPeriodoLiquidacion(periodoLiquidacion(opDate, false));
 		
 		ApplicationParameter ap2 = AON.getApplicationParameter(domain.getName(), domain.getId(), login,
 				AppParam.SII_INCLUDE_DATE);
