513a09d8d34d85f6aeace8cb49e01b65c7ab07fa
Sergio <svaldepenas@aonsolutions.es>
2021-09-24 20:16:44 +0200

    Download clauses to contract on creation

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqEmployee.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqEmployee.java
@@ -5,6 +5,7 @@ import static com.esferalia.aon.jooq.tables.AgreementLevel.AGREEMENT_LEVEL;
 import static com.esferalia.aon.jooq.tables.Certifica2BatchDetail.CERTIFICA2_BATCH_DETAIL;
 import static com.esferalia.aon.jooq.tables.Contract.CONTRACT;
 import static com.esferalia.aon.jooq.tables.ContractBonus.CONTRACT_BONUS;
+import static com.esferalia.aon.jooq.tables.ContractClause.CONTRACT_CLAUSE;
 import static com.esferalia.aon.jooq.tables.ContractData.CONTRACT_DATA;
 import static com.esferalia.aon.jooq.tables.ContractDeduction.CONTRACT_DEDUCTION;
 import static com.esferalia.aon.jooq.tables.ContractEmbargo.CONTRACT_EMBARGO;
@@ -524,11 +525,34 @@ public class JooqEmployee {
 		contractData.setContractId(contractId);
 		employeeContractData.setContractInfo(contractData);
 		
+		downloadClausesToContract(dslContext, contractId, domain);
+		
 		System.out.println("GUARDADO -> CONTRACT : " + contractId);
 		
 		return employeeContractData;
 	}
 	
+	private static void downloadClausesToContract(DSLContext dslContext, Integer contractId, Integer domainId) {
+		Integer parentDomainId = dslContext.select(DOMAIN.PARENT).from(DOMAIN).where(DOMAIN.ID.eq(domainId)).fetchOne(DOMAIN.PARENT);
+	
+		Result<Record> clauseRecords = dslContext.select().from(CONTRACT_CLAUSE)
+				.where(CONTRACT_CLAUSE.CONTRACT.isNull())
+				.and(CONTRACT_CLAUSE.DOMAIN.eq(domainId)
+					.or(CONTRACT_CLAUSE.DOMAIN.eq(parentDomainId))
+				).fetch();
+		
+		for(Record clauseRecord : clauseRecords) {
+			dslContext.insertInto(CONTRACT_CLAUSE)
+				.set(CONTRACT_CLAUSE.DOMAIN, domainId)
+				.set(CONTRACT_CLAUSE.CONTRACT, contractId)
+				.set(CONTRACT_CLAUSE.LINE, clauseRecord.get(CONTRACT_CLAUSE.LINE))
+				.set(CONTRACT_CLAUSE.NAME, clauseRecord.get(CONTRACT_CLAUSE.NAME))
+				.set(CONTRACT_CLAUSE.DESCRIPTION, clauseRecord.get(CONTRACT_CLAUSE.DESCRIPTION))
+				.set(CONTRACT_CLAUSE.GENERAL, clauseRecord.get(CONTRACT_CLAUSE.GENERAL))
+				.execute();
+		}
+	}
+
 	private static Record getEmployeeRecord(DSLContext dslContext, Integer contractId) {
 		return 
 		dslContext
