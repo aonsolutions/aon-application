290ce243f672889bcc9fa04605ead1f32d863815
Sergio <svaldepenas@aonsolutions.es>
2021-10-07 16:36:46 +0200

    Fix RETA load employee

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqEmployee.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqEmployee.java
@@ -1013,12 +1013,17 @@ public class JooqEmployee {
 		
 		// ---------------------------------------------- Contract Extension
 		
-		Integer contractTypeValue = Integer.parseInt(contractData.getContractType());
 		Boolean hasExtension = false;
-		if(contractTypeValue >= 400) {
-			Result<Record> extensionRecords = dslContext.select().from(CONTRACT_ATTACH).where(CONTRACT_ATTACH.CONTRACT.eq(contractData.getContractId())).and(CONTRACT_ATTACH.TYPE.eq((byte)13)).fetch();
-			if(extensionRecords.isNotEmpty()) hasExtension = true;
-		}
+		
+		try {
+			Integer contractTypeValue = Integer.parseInt(contractData.getContractType());
+			
+			if(contractTypeValue >= 400) {
+				Result<Record> extensionRecords = dslContext.select().from(CONTRACT_ATTACH).where(CONTRACT_ATTACH.CONTRACT.eq(contractData.getContractId())).and(CONTRACT_ATTACH.TYPE.eq((byte)13)).fetch();
+				if(extensionRecords.isNotEmpty()) hasExtension = true;
+			}
+		} catch (NumberFormatException e) {}
+		
 		contractData.setHasExtension(hasExtension);
 		
 		employeeContractInfo.setEmployeeInfo(employeeData);
