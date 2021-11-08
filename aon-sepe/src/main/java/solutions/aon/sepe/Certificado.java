1dc8994dc0602094fd23eb59a1a55670541f38e6
Sergio <svaldepenas@aonsolutions.es>
2021-11-08 12:33:45 +0100

    Certific@2 remove replace method

--- aon-sepe/src/main/java/solutions/aon/sepe/Certificado.java
+++ aon-sepe/src/main/java/solutions/aon/sepe/Certificado.java
@@ -206,18 +206,20 @@ public class Certificado {
 				{//DATA CTZ
 					for(QuoteData qdata: certificates.getQuoteData()) {
 						System.out.println(qdata.getBccc().get() +" "+ qdata.getBccc().get());
+						System.out.println(decimalFormat.format(qdata.getBccc().get()) +" "+ 
+								decimalFormat.format(qdata.getBcd().get()));
 						form =  (HtmlForm) HtmlUnitToolkit.wait4(htmlPage, p -> p.querySelector("#BeanMecanizacionOLIPre")).orElseThrow();
 						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionPre.srAnyoCotizacion").setValueAttribute(qdata.getAnio().toString());
 						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionPre.srMesCotizacion").setValueAttribute(qdata.getMonth().toString());
 						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionPre.srDiasCotizacion").setValueAttribute(qdata.getDays().toString());
 						if(qdata.getBccc().isPresent()) {
 							form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionPre.srBaseContingenciasComunes")
-							.setValueAttribute(qdata.getBccc().get());
+							.setValueAttribute(decimalFormat.format(qdata.getBccc().get()));
 						}
 						
 						if(qdata.getBcd().isPresent()) {
 							form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionPre.srBaseContingenciasDesempleo")
-							.setValueAttribute(qdata.getBcd().get());
+							.setValueAttribute(decimalFormat.format(qdata.getBcd().get()));
 						}
 						htmlPage = ((HtmlSubmitInput)htmlPage.querySelector("form[name=BeanMecanizacionOLIPre] input[name=btAnadir]")).click();
 					}
@@ -229,9 +231,9 @@ public class Certificado {
 					if(certificates.getDaysCtzVc()!=null)
 						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionVacacionesPre.srDiasCotizacion").setValueAttribute(certificates.getDaysCtzVc().toString());
 					if(certificates.getBcccVc()!=null)
-						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionVacacionesPre.srBaseContingenciasComunes").setValueAttribute(certificates.getBcccVc().replace(".",","));
+						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionVacacionesPre.srBaseContingenciasComunes").setValueAttribute(decimalFormat.format(certificates.getBcccVc().get()));
 					if(certificates.getBcdVc()!=null)
-						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionVacacionesPre.srBaseContingenciasDesempleo").setValueAttribute(certificates.getBcdVc().replace(".",","));
+						form.getInputByName("orDatosTrabajador.orDatosInsercionCotizacionVacacionesPre.srBaseContingenciasDesempleo").setValueAttribute(decimalFormat.format(certificates.getBcdVc().get()));
 					htmlPage = ((HtmlSubmitInput)htmlPage.querySelector("form[name=BeanMecanizacionOLIPre] input[name=btActualizarTotales]")).click();
 					handleSepeExceptions(htmlPage);
 				}
