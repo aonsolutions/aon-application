22fb08d750122f217d2fa46ffb4d8b17fe71995d
anderibz <aibanez@aonsolutions.es>
2021-11-17 22:16:08 +0100

    New Suite/F. Emitidas: Aplicar IRPF en facturas a Canarias, Ceuta y Melilla [Fix #2682]

--- aon-solutions/src/main/webapp/modules/invoice/aon-invoice.js
+++ aon-solutions/src/main/webapp/modules/invoice/aon-invoice.js
@@ -418,6 +418,7 @@ export class AonInvoice extends AonElement {
 
 		if(hasComment) {
 			let ul = this.createElement(TAG.UL);
+			ul.classList.add(CSS.AON_UL);
 			ul.style.width = '100%';
 			card.setContent(ul);
 			this.invoice.comments.forEach((item, i) => {
@@ -804,8 +805,12 @@ export class AonInvoice extends AonElement {
 		taxesTable.id = this.TAX_TABLE2;
 		card.addContent(taxesTable);
 
-		if(this.invoice.isEmitida() && !this.invoice.isNacional()) {
+		if(!this.invoice.isNacional() && !this.invoice.isCcm()) {
 			this.invoice.taxes = [];
+		}
+
+		if(this.invoice.isCcm()) {
+			this.invoice.taxes = this.invoice.taxes.filter(f => TaxType.IRPF === f.tax);
 		}
 
 		for(let i = 0; i < this.invoice.taxes.length; i++) {
@@ -835,7 +840,7 @@ export class AonInvoice extends AonElement {
 			addButton.icon = MATERIAL_ICONS.ADD;
 
 			addButton.addEventListener('click', () => {
-				if(this.invoice.isEmitida() && !this.invoice.isNacional()) {
+				if(!this.invoice.isNacional()) {
 					// TODO
 				} else { 
 					this.setFocus(this.TAX_TYPE + this.invoice.taxes.length);
@@ -845,7 +850,7 @@ export class AonInvoice extends AonElement {
 				}
 			});
 			div.appendChild(addButton);
-			if(this.invoice.isEmitida() && !this.invoice.isNacional()) {
+			if(!this.invoice.isNacional()) {
 				addButton.setDisabled(true);
 			}
 		}
@@ -866,7 +871,7 @@ export class AonInvoice extends AonElement {
 			if(this.autosave) this.save();
 		});
 		div.appendChild(irpf);
-		if(this.invoice.isEmitida() && !this.invoice.isNacional()) {
+		if(!this.invoice.isNacional() && !this.invoice.isCcm()) {
 			this.invoice.setWithholding(false);
 			irpf.setDisabled(true);
 		}
