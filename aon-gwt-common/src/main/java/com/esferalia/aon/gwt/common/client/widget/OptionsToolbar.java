2b7a6ce99e5a5ca98a5c6262b8599cc5e8fb417b
rtrepiana <rtrepiana@aonsolutions.es>
2021-12-08 21:32:44 +0100

    Remove 'EMPLEADOS' StackPanel.

--- aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/widget/OptionsToolbar.java
+++ aon-gwt-common/src/main/java/com/esferalia/aon/gwt/common/client/widget/OptionsToolbar.java
@@ -14,6 +14,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -56,6 +57,9 @@ public class OptionsToolbar extends Composite {
 	
 	@UiField
 	TextBox searchTextBox;
+	
+	@UiField
+	FlowPanel buttonBar;
 
 	private List<Listener> listeners;
 
@@ -63,6 +67,12 @@ public class OptionsToolbar extends Composite {
 		initWidget(uiBinder.createAndBindUi(this));
 		
 		this.listeners = new ArrayList<Listener>();
+	}
+	
+	@Override
+	public void setHeight(String height) {
+		super.setHeight(height);
+		buttonBar.setHeight(height);
 	}
 
 	public void setVisibleSearchTextBox(boolean visible) {
