c9ea5d5d6dba2bfb758e3db228da857719c455c3
rtrepiana <rtrepiana@aonsolutions.es>
2021-12-19 20:40:44 +0100

    Fix 'bypass' , unknown error.

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/Employees.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/Employees.java
@@ -82,6 +82,8 @@ import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.Widget;
 
+import sun.util.logging.resources.logging;
+
 public class Employees extends ResizeComposite implements OpenHandler<TreeItem>, SelectionHandler<TreeItem>,
 		ScrollHandler, ContextMenuHandler, KeyDownHandler, LoadHandler, OptionsToolbar.Listener {
 
@@ -1951,7 +1953,6 @@ public class Employees extends ResizeComposite implements OpenHandler<TreeItem>,
 	 */
 
 	private void changeVisibleEmployees() {
-
 		TreeItem enterpriseItem = tree.getItem(0);
 		int childCount = enterpriseItem.getChildCount();
 		int workplacesOffset = getWorkplacesOffset(enterpriseItem);
@@ -1959,13 +1960,15 @@ public class Employees extends ResizeComposite implements OpenHandler<TreeItem>,
 			TreeItem workplaceItem = enterpriseItem.getChild(i);
 			boolean inViewport = elementInViewport(workplaceItem.getElement());
 			boolean opened = workplaceItem.getState();
-			workplaceItem.setState(false); // close workplace
+			try {
+				workplaceItem.setState(false); // close workplace
+			}catch ( Exception e ) {
+			}
 			removeEmployeeItems(workplaceItem);
 			if (inViewport & opened) {
 				workplaceItem.setState(true);
 			}
 		}
-
 	}
 
 	private void changeVisibleWorkplaces() {
@@ -2568,4 +2571,10 @@ public class Employees extends ResizeComposite implements OpenHandler<TreeItem>,
 
 		return null;
 	}
+
+	private static native void log (String message ) /*-{
+		console.log(message);
+	}-*/
+;
+	
 }
