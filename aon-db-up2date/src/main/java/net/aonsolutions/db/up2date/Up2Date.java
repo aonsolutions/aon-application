184d8505ae42fba4ac26e6291d1baabac6026519
RAY VASQUEZ <anrto_94_5@hotmail.com>
2021-12-21 12:55:08 +0100

    dbUpdate workplace

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -20,6 +20,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import net.aonsolutions.db.up2date.data.DataRequestUpdate;
+import net.aonsolutions.db.up2date.payroll.WorkplaceUpdate;
 
 
 public class Up2Date {
@@ -193,7 +194,8 @@ public class Up2Date {
     		//AgriculturalBases2021UpdateIII.AGRICULTURALBASES2021UPDATEIII,
     		//TrainingPercentages2021Update.TRAINNINGPERCENTAGES2021UPDATE,
     		//FellowsPercentages2021Update.FELLOWSPERCENTAGES2021UPDATE,
-    		DataRequestUpdate.DATA_REQUEST_UPDATE
+    		DataRequestUpdate.DATA_REQUEST_UPDATE,
+    		WorkplaceUpdate.WORKPLACE_UPDATE
     };
 
 
