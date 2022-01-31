bd5a6455f99b0db54b475fbbedfad432bb080ebd
RAY VASQUEZ <anrto_94_5@hotmail.com>
2022-01-31 17:54:40 +0100

    timecontrol lastDate fix, employeeITDAO edit

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/EmployeeITDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/EmployeeITDAO.java
@@ -103,9 +103,7 @@ public class EmployeeITDAO {
 			return new EmployeeIT[0];
 		
 		for (EmployeeIT employeeIT : employeeIts) {
-
 			ContractLeaveRecord contractLeaveRecord = setContractLeave(dslContext, employeeIT);
-			
 			setContractLeaveDetail(dslContext, contractLeaveRecord, employeeIT.getITsParts());
 		}
 		
@@ -126,9 +124,30 @@ public class EmployeeITDAO {
 			}
 
 			employeeIt.getEndDate().ifPresent(end-> condition.and(CONTRACT_LEAVE.END_DATE.le(toSql(end)) ) );
-		
-			ContractLeaveRecord contractLeaveRecord  = condition.fetchOptionalInto(CONTRACT_LEAVE)
-			.orElseGet( () ->{
+			
+			Optional<ContractLeaveRecord> exists = condition.fetchStreamInto(CONTRACT_LEAVE).findFirst();
+			
+			ContractLeaveRecord contractLeaveRecord;
+	
+			if(exists.isPresent()) {
+				contractLeaveRecord = exists.get();
+				employeeIt.getDescription().ifPresent(d-> contractLeaveRecord.set(CONTRACT_LEAVE.DESCRIPTION, d) );
+
+				employeeIt.getEndDate().ifPresent(d-> contractLeaveRecord.set(CONTRACT_LEAVE.END_DATE, toSql(d)) );
+				
+				employeeIt.getParent().ifPresent(d-> contractLeaveRecord.set(CONTRACT_LEAVE.PARENT, d) );
+				
+				employeeIt.getDailyCgcBase().ifPresent(d-> contractLeaveRecord.set(CONTRACT_LEAVE.DAILY_CGC_BASE, d) );
+
+				employeeIt.getDailyCgpBase().ifPresent(d-> contractLeaveRecord.set(CONTRACT_LEAVE.DAILY_CGP_BASE, d) );
+
+				employeeIt.getDailyRegBase().ifPresent(d-> contractLeaveRecord.set(CONTRACT_LEAVE.DAILY_REG_BASE, d) );
+				
+				 if(null!=employeeIt.getDischargeCause()) 
+					 contractLeaveRecord.set(CONTRACT_LEAVE.DISCHARGE_CAUSE, employeeIt.getDischargeCause().value());
+				 
+				 contractLeaveRecord.update();
+			} else {
 				InsertSetMoreStep<ContractLeaveRecord> sets = dslContext
 				.insertInto(CONTRACT_LEAVE)
 				.set(CONTRACT_LEAVE.DOMAIN, employeeIt.getDomain())
@@ -149,18 +168,16 @@ public class EmployeeITDAO {
 
 				employeeIt.getDailyRegBase().ifPresent(d-> sets.set(CONTRACT_LEAVE.DAILY_REG_BASE, d) );
 				
-				return sets.returning().fetchOne();
-			} );
-		 
-		 if(null!=employeeIt.getDischargeCause()) {
-				contractLeaveRecord.set(CONTRACT_LEAVE.DISCHARGE_CAUSE, employeeIt.getDischargeCause().value());
-				contractLeaveRecord.update();
-		 }
+				 if(null!=employeeIt.getDischargeCause()) 
+					 sets.set(CONTRACT_LEAVE.DISCHARGE_CAUSE, employeeIt.getDischargeCause().value());
+		
+				contractLeaveRecord = sets.returning().fetchOne();
+			}		 
+
 		 return contractLeaveRecord;
 	}
 	
 	private static void setContractLeaveDetail(DSLContext dslContext, ContractLeaveRecord contractLeaveRecord, List<EmployeeITPart> its) {
-
 
 		InsertSetMoreStep<ContractLeaveDetailRecord> insertContractLeaveDetail = null;
 		
@@ -179,11 +196,12 @@ public class EmployeeITDAO {
 				.and(CONTRACT_LEAVE_DETAIL.DATE.eq(toSql(it.getDate())))
 				.and(CONTRACT_LEAVE_DETAIL.TYPE.eq(it.getType().value()));
 			}
-			
 			Optional<ContractLeaveDetailRecord> detailLeave = contextDetail.fetchStreamInto(CONTRACT_LEAVE_DETAIL).findFirst();
 			
 			if(detailLeave.isPresent()) {
+	
 				ContractLeaveDetailRecord contractLeaveDetailRecord = detailLeave.get();
+				
 				it.getCollegeNumber().ifPresent(c-> contractLeaveDetailRecord.set(CONTRACT_LEAVE_DETAIL.COLLEGE_NUMBER, c) );
 				
 				it.getConfirmOrder().ifPresent(c-> contractLeaveDetailRecord.set(CONTRACT_LEAVE_DETAIL.CONFIRM_ORDER, c) );
