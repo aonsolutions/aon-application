d39f2ae55c6f186f25bcf154876dd4138a9378b8
Sergio <svaldepenas@aonsolutions.es>
2022-02-02 21:45:06 +0100

    Up2Date database for agreement owner

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,6 +19,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import net.aonsolutions.db.up2date.payroll.AgreementOwnerUpdate;
 import net.aonsolutions.db.up2date.payroll.AlterAgreement4Owner;
 import net.aonsolutions.db.up2date.payroll.ContractCleanUpdate;
 import net.aonsolutions.db.up2date.data.DataRequestUpdate;
@@ -229,7 +230,8 @@ public class Up2Date {
     		
     		IrpfQuotasInsert.IRPFQUOTASINSERT,
     		IrpfQuotasCheck.IRPFQUOTASCHECK,
-    		AlterAgreement4Owner.ALTER_AGREEMENT_OWNER
+    		AlterAgreement4Owner.ALTER_AGREEMENT_OWNER,
+    		AgreementOwnerUpdate.AGREEMENTOWNERUPDATE
     };
     
 
