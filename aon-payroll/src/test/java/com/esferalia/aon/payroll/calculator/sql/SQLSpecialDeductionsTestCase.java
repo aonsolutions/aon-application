4e41607aadb8ed2de8bb1ee1b7ea170ee4e88012
EukeCastellano <ecastellano@aonsolutions.es>
2022-03-03 18:46:16 +0100

    Compilation error.

--- aon-payroll/src/test/java/com/esferalia/aon/payroll/calculator/sql/SQLSpecialDeductionsTestCase.java
+++ aon-payroll/src/test/java/com/esferalia/aon/payroll/calculator/sql/SQLSpecialDeductionsTestCase.java
@@ -258,7 +258,7 @@ public class SQLSpecialDeductionsTestCase extends AbstractSQLTestCase {
 		expected = 1750.00 * workedDays / monthDays;
 		Assert.assertEquals(String.format("%s :", ContextVariable.TOTAL_PAYMENT), expected, salary.getTotalPayment());
 		Assert.assertEquals(String.format("%s :", ContextVariable.CGC_BASE), expected , salary.getCommonBase());
-		Assert.assertEquals(String.format("%s :", ContextVariable.TOTAL_LIQUID), expected - ( expected * 6.35/100.00), salary.getTotalLiquid().DELTA);
+		Assert.assertEquals(String.format("%s :", ContextVariable.TOTAL_LIQUID), expected - ( expected * 6.35/100.00), salary.getTotalLiquid(), DELTA);
 	}
 
 	// ------------------------------------------------------------------------
