8dc0ccc15bc091fc6e9814fe3ad99f58044fd690
EukeCastellano <ecastellano@aonsolutions.es>
2022-03-18 09:50:22 +0100

    Merge

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/fiscal/MODEL111Impl.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/fiscal/MODEL111Impl.java
@@ -86,7 +86,8 @@ public class MODEL111Impl implements IMODEL111 {
 
 	@Override
 	public Mod111 initialize(AONContext ctx, Mod111 mod111) {
-		return Mod111DAO.initialize(ctx,mod111);
+		return ctx.getDslContext().transactionResult(
+				configuration -> Mod111DAO.initialize(ctx,mod111));
 	}
 
 	@Override
