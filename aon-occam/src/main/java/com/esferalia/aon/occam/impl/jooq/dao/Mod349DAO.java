ecd7a412a11e787b38ccb1e2880cf5843d093776
EukeCastellano <ecastellano@aonsolutions.es>
2022-03-24 14:36:22 +0100

    Mod349 change.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/Mod349DAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/Mod349DAO.java
@@ -144,6 +144,7 @@ public class Mod349DAO {
 			}
 			return getById(ctx, mod349.getId());			 
 		} catch (Throwable t) {
+			t.printStackTrace();
 			throw new AonCoreException(t.getCause()!=null?t.getCause().getMessage():t.getMessage());
 		}
 	}
@@ -669,7 +670,10 @@ public class Mod349DAO {
 					// para poder guardarlo posteriormente como rectificacion en el modelo 349
 					int rectificateYear = 0;
 					Period rectificatePeriod = null;
-					if (vat.isInsidePeriod() && vat.getRectificationType() != null && (vat.getRectificationType() == RectificationType.NORMAL_RECTIFIER || vat.getRectificationType() == RectificationType.SPECIAL_RECTIFIER)) {
+					if (vat.isInsidePeriod() 
+						&& vat.getRectificationType() != null 
+						&& (vat.getRectificationType() == RectificationType.NORMAL_RECTIFIER || vat.getRectificationType() == RectificationType.SPECIAL_RECTIFIER)
+						&& vat.getRectificateInvoiceTaxDate() != null) {
 						// Buscar posible periodo donde se declaro la factura rectificada (mensual, trimestral), según su fecha de IVA, si es distinto del que se está declarando, si el periodo es el mismo no se crea linea para la rectificacion en el modelo 349
 						rectificateYear = AonDateUtils.getYear(vat.getRectificateInvoiceTaxDate());
 						// Primero probamos con posible periodo mensual
