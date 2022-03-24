5652289c75f366de8b2d898ed251bcbe1ea8a701
akrck02 <aketza.vazquez@gmail.com>
2022-03-24 11:50:34 +0100

    Side menu bar for help view

---
+++ aon-help/src/main/java/com/code/aon/web/help/service/drive/SmartSearch.java
@@ -0,0 +1,34 @@
+package com.code.aon.web.help.service.drive;
+
+public class SmartSearch {
+
+	
+	public static void searchVideosMatching(String name) {
+		
+		connection = DriveService
+		
+		
+	}
+	
+	public static void searchPdfMatching(String name) {
+		
+		
+		
+		
+	}
+	
+	public static void searchPdfContaining(String name) {
+		
+		
+		
+	}
+	
+
+	/**
+	 *  EXECUTION MAIN
+	 * --------------------------------------
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
