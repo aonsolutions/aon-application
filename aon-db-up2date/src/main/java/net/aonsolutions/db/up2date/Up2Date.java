db58dd21105fd1af8da0e1d09127d34f1486f5f5
EukeCastellano <ecastellano@aonsolutions.es>
2022-04-04 20:46:14 +0200

    More ...

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -20,8 +20,6 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import net.aonsolutions.db.up2date.fiscal.RefreshMod303Result;
-import net.aonsolutions.db.up2date.payroll.AgreementClean;
-import net.aonsolutions.db.up2date.payroll.CertificatesClean;
 import net.aonsolutions.db.up2date.tgss.Artist2022Update;
 import net.aonsolutions.db.up2date.tgss.Bases2022UpdateII;
 import net.aonsolutions.db.up2date.tgss.FellowsPercentages2022Update;
@@ -220,7 +218,6 @@ public class Up2Date {
 //    		CertificateDomainFix.CERTIFICATEDOMAINFIX,
 //    		SalaryHoursUpdateDaily.SALARYHOURSUPDATEDAILY,
 //    		AgreementClean.AGREEMENTCLEAN,
-//			CertificatesClean.CERTIFICATESCLEAN
 //    		CertificatesClean.CERTIFICATESCLEAN
     		Bases2022UpdateII.BASES2022UPDATEIII,
     		HomeBases2022Update.HOMEBASES2022UPDATE,
