78af628baa6ff531346654e2427e0e107665df22
Sergio <svaldepenas@aonsolutions.es>
2022-04-05 17:49:12 +0200

    MainIT, EnterpriseIT and WorkplaceIT changes, add list view, and other changes

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/MainContrataITObject.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/MainContrataITObject.java
@@ -1,6 +1,8 @@
 package com.esferalia.aon.gwt.payroll.client;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.SortedSet;
@@ -599,6 +601,15 @@ public class MainContrataITObject {
 		for(ITEmployee employee : employeesList)
 			for(IT it : employee.getIts())
 				itsList.add(it);
+		
+		itsList.sort(new Comparator<IT>() {
+			@Override
+			public int compare(IT it1, IT it2) {
+				return it1.getStartDate().compareTo(it2.getStartDate());
+			}
+		});
+		
+		Collections.reverse(itsList);
 	}
 	
 	public List<ITEmployee> getEmployeesList(boolean contractsWithIT, Date startDate, Date endDate){
