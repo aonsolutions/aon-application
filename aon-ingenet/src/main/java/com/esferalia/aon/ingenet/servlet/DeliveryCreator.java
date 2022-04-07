e1f50a64b18d40824a9f84d05269f574620cbb37
anderibz <aibanez@aonsolutions.es>
2022-04-07 11:57:57 +0200

    Ingenet not update sales¿?

--- aon-ingenet/src/main/java/com/esferalia/aon/ingenet/servlet/DeliveryCreator.java
+++ aon-ingenet/src/main/java/com/esferalia/aon/ingenet/servlet/DeliveryCreator.java
@@ -946,7 +946,7 @@ public class DeliveryCreator implements Serializable {
 	}
 	private void manageSalesDetail(List<Delivery> deliveryList){
 		deliveryList.forEach(delivery->{			
-			AON.getDeliveryDetails(getDomain(), getDomainId(), getUser(),
+			AON.getDeliveryDetailStream(getDomain(), getDomainId(), getUser(),
 					f->f.getIdProperty().eq(delivery.getId()))
 			.filter(d->d.getSalesDetail()!=null)
 			.collect(Collectors.groupingBy(DeliveryDetail::getSalesDetail,
