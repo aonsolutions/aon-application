cc4aae53e68193a572a26609ac4039cf17383be2
akrck02 <aketza.vazquez@gmail.com>
2022-05-24 12:57:53 +0200

    domain URLs can be copied now with style ;)

--- aon-ui-common/src/main/java/com/code/aon/ui/common/controller/ConfigurationController.java
+++ aon-ui-common/src/main/java/com/code/aon/ui/common/controller/ConfigurationController.java
@@ -267,7 +267,7 @@ public class ConfigurationController implements Serializable {
 	 * @return the help url
 	 */
 	public String getHelpURL() {
-		return "http://faqs.aonsolutions.es";//getURL(this.currentAction);
+		return "http://faqs.aonsolutions.es"; //getURL(this.currentAction);
 	}
 	
 	public LocaleElement[] getLocales() {
