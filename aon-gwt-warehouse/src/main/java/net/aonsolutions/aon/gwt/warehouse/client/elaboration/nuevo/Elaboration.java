89983bab7ebc11befd7f42f1e654ac7eaeb9e9a5
anderibz <aibanez@aonsolutions.es>
2022-06-14 13:20:37 +0200

    New Elaboration in progress...

---
+++ aon-gwt-warehouse/src/main/java/net/aonsolutions/aon/gwt/warehouse/client/elaboration/nuevo/Elaboration.java
@@ -0,0 +1,39 @@
+package net.aonsolutions.aon.gwt.warehouse.client.elaboration.nuevo;
+
+import com.esferalia.aon.gwt.common.client.polymer.AonTemplate2;
+import com.esferalia.aon.gwt.common.client.widget.solutions.AonToolbar;
+import com.esferalia.aon.gwt.common.client.widget.solutions.AonToolbarSearchBox;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+public class Elaboration extends AonTemplate2 {
+
+	@Override
+	public void onModuleLoad() {
+		super.onModuleLoad();
+		load();
+	}
+
+	private void load() {
+		loadToolbar();
+		loadContent();
+	}
+
+	private void loadToolbar() {
+		getDockLayoutPanel().setWidgetSize(getToolbar(), 50);
+		AonToolbar toolbar = new AonToolbar("Elaboraciones");
+		
+		AonToolbarSearchBox searchBox = new AonToolbarSearchBox() {
+			
+			@Override
+			public void onValueChange(String value) {
+
+			}
+		};
+		toolbar.showSearchPanel(searchBox);
+		setToolbar(toolbar);
+	}
+
+	public void loadContent() {
+		setContent(new SimplePanel());
+	}
+}
