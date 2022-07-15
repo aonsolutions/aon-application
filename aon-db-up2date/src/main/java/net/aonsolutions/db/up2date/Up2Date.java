ab7809137863dca0265696494629870b7411c024
anderibz <aibanez@aonsolutions.es>
2022-07-15 14:27:09 +0200

    jacal fix. update invoice reference.

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,13 +19,8 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import net.aonsolutions.db.up2date.config.InsertIAE863;
-import net.aonsolutions.db.up2date.payroll.AlterAgreement4Log;
-import net.aonsolutions.db.up2date.tgss.Bases2022FixHourly;
+import net.aonsolutions.db.up2date.finance.JacalInvoiceUpdateFix;
 import net.aonsolutions.db.up2date.tgss.FellowsITIMS2022UpdateII;
-import net.aonsolutions.db.up2date.tgss.RealDecreeLaw322021Art151Fix;
-import net.aonsolutions.db.up2date.tgss.SalaryHoursFix;
-import net.aonsolutions.db.up2date.tgss.SalaryHoursFixII;
 
 public class Up2Date {
 
@@ -257,7 +252,8 @@ public class Up2Date {
     		//SalaryHoursFixII.SALARYHOURSFIXII
     		//AlterAgreement4Log.ALTERAGREEMENTLOG,
     		//RealDecreeLaw322021Art151Fix.REALDECREELAW322021ART151FIX,
-    		FellowsITIMS2022UpdateII.FELLOWSITIMS2022UPDATEII
+    		FellowsITIMS2022UpdateII.FELLOWSITIMS2022UPDATEII,
+    		JacalInvoiceUpdateFix.JACA_INVOICE_UPDATE
     };
     
 
