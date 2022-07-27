4c501662ee33b29c6cff41abe605ed3bad22400e
rtrepiana <rtrepiana@aonsolutions.es>
2022-07-27 13:46:56 +0200

    Load DEFAULT index.pdf

--- aon-ui-help/src/main/java/com/code/aon/ui/help/pdf/PdfSearcher.java
+++ aon-ui-help/src/main/java/com/code/aon/ui/help/pdf/PdfSearcher.java
@@ -1,67 +1,93 @@
 package com.code.aon.ui.help.pdf;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
 
+import com.esferalia.aon.watson.util.AonStringUtils;
+
 public class PdfSearcher {
 
-	
-	
+	private static final List<PDOutlineItem> INDEX = getIndex();
+
+	public static List<PDOutlineItem> search(Predicate<PDOutlineItem> filter) {
+		return INDEX.stream().filter(filter)
+				.collect(Collectors.toList());
+	}
+
+	public static List<PDOutlineItem> search(String name) {
+		return search(outline -> AonStringUtils.containsIgnoreCase(outline.getTitle(), name));
+	}
+
 	/**
-	 * Search all entries for given name
-	 * @param document The index 
-	 * @param name The name to search for
+	 * Search index entries for given name
+	 * 
+	 * @param document The index
+	 * @param name     The name to search for
 	 * @return The found entries
 	 */
 	public static ArrayList<PDOutlineItem> search(PDDocument document, String name) {
-		
-		ArrayList<PDOutlineItem> results = new ArrayList<PDOutlineItem>(); 
-				
-		
-			PDDocumentCatalog catalogue = document.getDocumentCatalog();
-			
-			if(catalogue == null) {
-				return results;
+
+		ArrayList<PDOutlineItem> results = new ArrayList<PDOutlineItem>();
+
+		PDDocumentCatalog catalogue = document.getDocumentCatalog();
+
+		if (catalogue == null) {
+			return results;
+		}
+
+		PDDocumentOutline outline = catalogue.getDocumentOutline();
+
+		if (outline == null) {
+			return results;
+		}
+
+		// For each outline item search sub-items
+
+		for (PDOutlineItem item = outline.getFirstChild(); item != null; item = item.getNextSibling()) {
+			if (item.getTitle().toUpperCase().contains(name.toUpperCase())) {
+				results.add(item);
 			}
-			
-			PDDocumentOutline outline = catalogue.getDocumentOutline();
-			
-			if(outline == null) {
-				return results;
-			}
-			
-			// For each outline item search sub-items
-			
-			for(PDOutlineItem item = outline.getFirstChild(); item != null; item = item.getNextSibling()) {
-				if(item.getTitle().toUpperCase().contains(name.toUpperCase())) {
-					results.add(item);
-				}
-				searchInside(item, name , results);
-			}
-				
+			searchInside(item, name, results);
+		}
+
 		return results;
 	}
-	
+
 	/**
-	 * Search inside the given item 
-	 * @param parent The item to search in
-	 * @param name The name to search
+	 * Search inside the given item
+	 * 
+	 * @param parent  The item to search in
+	 * @param name    The name to search
 	 * @param results The results
 	 */
 	private static void searchInside(PDOutlineItem parent, String name, ArrayList<PDOutlineItem> results) {
-		
-		for(PDOutlineItem item = parent.getFirstChild(); item != null; item = item.getNextSibling()) {
-			if(item.getTitle().toUpperCase().contains(name.toUpperCase())) {
+
+		for (PDOutlineItem item = parent.getFirstChild(); item != null; item = item.getNextSibling()) {
+			if (item.getTitle().toUpperCase().contains(name.toUpperCase())) {
 				results.add(item);
 			}
-			searchInside(item,name,results);
+			searchInside(item, name, results);
 		}
-				
+
 	}
-	
-	
+
+	private static List<PDOutlineItem> getIndex() {
+		try (InputStream is = PdfSearcher.class.getResourceAsStream("index.pdf");
+				PDDocument doc = Loader.loadPDF(is)) {
+			return search(doc, "");
+		} catch (Exception e) {
+			return Collections.emptyList();
+		}
+	}
+
 }
