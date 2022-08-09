4176eff7b8d99889c71ae9736275e023c79e266e
anderibz <aibanez@aonsolutions.es>
2022-08-09 14:07:42 +0200

    New suite. elaboration in progress.. II

--- aon-solutions/src/main/webapp/modules/warehouse/elaboration/aon-mobile-package-list.js
+++ aon-solutions/src/main/webapp/modules/warehouse/elaboration/aon-mobile-package-list.js
@@ -1,11 +1,10 @@
 import { AonMobileList } from '../../../components/aon-mobile-list.js';
 import { MATERIAL_ICONS, TAG } from '../../../environments/environments.js';
-import { getElaborationPackages } from '../../../services/warehouseService.js';
 import { AonMobilePackage } from './aon-mobile-package.js';
 
 export class AonMobilePackageList extends AonMobileList {
 
-    more;
+    elaborationPackages;
 
     constructor () {
         super();
@@ -14,51 +13,25 @@ export class AonMobilePackageList extends AonMobileList {
     connectedCallback () {
         this.initialize();
         this.init();
-        this.addEventListener('more', () => {
-    		if(this.more)
-    			this.loadMore()
-    	});
     }
 
     initialize() {
-        this.more = false;
-        this.filter = {
-            page:1,
-            perPage:30
-        }
-    }
 
-    loadMore() {
-        let filter = this.getFilter();
-        if(filter.page) {
-            filter.page = filter.page + 1;
-            this.setFilter(filter);
-            getElaborationPackages(filter).then(packages => {
-                if(packages.length == 0)
-                    this.more = false;
-                packages.forEach((packaging, i) => this.addRow(packaging, i));
-            });
-        }
     }
 
     init() {
         this.build();
-        getElaborationPackages({
-            page:1,
-            perPage:30
-        }).then(packages => {
-            if(packages.length == 0){   
-                this.empty();
-            }
-            packages.forEach((packaging, i) => this.addRow(packaging, i));
-        });        
+        if(this.elaborationPackages.length <= 0){
+            this.empty();
+        }
+        this.elaborationPackages.forEach((packaging, i) => this.addRow(packaging, i));
     }
 
     addRow(packaging, i) {
         let liValue = {
             icon: MATERIAL_ICONS.PALLET,
-            title: packaging.reference,
-            subtitle: packaging.description
+            title: packaging.item.name,
+            subtitle: packaging.item.serialNumber
         }
         this.addLi(liValue, i, () => this.aonPackage(packaging, i));
     }
@@ -70,15 +43,10 @@ export class AonMobilePackageList extends AonMobileList {
         this.clearElement(div);
         div.appendChild(aonPackage);
     }
-    
-    getFilter() {
-		return this.filter || {};
-	}
 
-    setFilter(filter) {		
-		this.filter = JSON.stringify(filter);
-        this.init();
-	}
+    setElaborationPackages(elaborationPackages){
+        this.elaborationPackages = elaborationPackages;
+    }
 }
 if(!window.customElements.get(TAG.AON_MOBILE_PACKAGE_LIST)){
     window.customElements.define(TAG.AON_MOBILE_PACKAGE_LIST, AonMobilePackageList);
