5c19bf9a37bab3eaa92a2334522fa10a6668c3ec
RAY VASQUEZ <anrto_94_5@hotmail.com>
2022-08-23 14:49:57 +0200

    NewsServlet

---
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/json/NewsJSON.java
@@ -0,0 +1,83 @@
+package com.esferalia.aon.occam.api.json;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.esferalia.aon.occam.api.model.IJsonNames;
+import com.esferalia.aon.occam.api.model.news.News;
+import com.esferalia.aon.occam.api.model.news.NewsType;
+import com.esferalia.aon.occam.api.model.registry.Category;
+import com.esferalia.aon.occam.api.model.security.Scope;
+import com.esferalia.aon.watson.server.AonDateUtils;
+
+public class NewsJSON {
+	private static final String FORMAT_DATE = "yyyy-MM-dd"; 
+	public static LinkedList<News> fromJSON(JSONArray json) {
+		LinkedList<News> list = new LinkedList<>();
+		for(Integer i = 0; i < json.length(); i++) {
+			list.add(fromJSON(json.getJSONObject(i)));
+		}
+ 		return list;
+	}
+	
+	public static News fromJSON(JSONObject json) {
+		return new News()
+			.setId(json.optInt(IJsonNames.ID))
+			.setDomain(DomainJSON.fromJSON(json.optJSONObject(IJsonNames.DOMAIN)))
+			.setTitle(json.optString(IJsonNames.TITLE))
+			.setDescription(json.optString(IJsonNames.DESCRIPTION))
+			.setContent(json.optString(IJsonNames.CONTENT))
+			.setUrl(json.optString(IJsonNames.URL))
+			.setActive(json.optBoolean(IJsonNames.ACTIVE))
+			.setRss(json.optBoolean(IJsonNames.RSS))
+			.setInitDate(!json.optString("init_date").isEmpty() ? AonDateUtils.parse(json.optString("init_date"), FORMAT_DATE) : null)
+			.setEndDate(!json.optString("end_date").isEmpty() ? AonDateUtils.parse(json.optString("end_date"), FORMAT_DATE) : null)
+			.setType( NewsType.safeValueOf(json.optString(IJsonNames.TYPE))) 
+			.setCategory(json.optInt(IJsonNames.CATEGORY)!=0 ? new Category().setId(json.optInt(IJsonNames.CATEGORY)) : null)
+			.setScope(json.optInt(IJsonNames.SCOPE)!=0 ? new Scope().setId(json.optInt(IJsonNames.SCOPE)) : null )
+			;
+	}
+	
+	public static JSONArray toJSON(LinkedList<News> news) {
+		return toJSON(news.stream());
+	}
+	
+	public static JSONArray toJSON(List<News> news) {
+		JSONArray array = new JSONArray();
+		news.forEach(t -> array.put(toJSON(t)));
+		return array;
+	}
+	
+	public static JSONArray toJSON(Stream<News> news) {
+		JSONArray array = new JSONArray();
+		news.forEach(t -> array.put(toJSON(t)));
+		return array;
+	}
+	
+	public static JSONObject toJSON(News news) {
+		if(news==null) return new JSONObject();
+		JSONObject json = new JSONObject()
+			.put(IJsonNames.ID, news.getId())
+			.put(IJsonNames.DOMAIN, DomainJSON.toJSON(news.getDomain()))
+			.put(IJsonNames.TITLE, news.getTitle())
+			.put(IJsonNames.DESCRIPTION, news.getDescription())
+			.put(IJsonNames.CONTENT, news.getContent())
+			.put(IJsonNames.URL, news.getUrl())
+			.put(IJsonNames.ACTIVE, news.isActive())
+			.put(IJsonNames.RSS,  news.isRss())
+			.put(IJsonNames.CATEGORY, news.getCategory()!=null ? CategoryJSON.toJSON(news.getCategory()) : null)
+			.put(IJsonNames.TYPE, news.getType()!=null ? news.getType().getName() : null)
+			.put(IJsonNames.SCOPE, news.getScope()!=null ? ScopeJSON.toJSON(news.getScope()) : null)
+			;
+		
+		news.getInitDate().ifPresent(d-> json.put("init_date", d.getTime()));
+		news.getEndDate().ifPresent(d-> json.put("end_date", d.getTime()));
+		
+		return json;
+	}
+
+}
