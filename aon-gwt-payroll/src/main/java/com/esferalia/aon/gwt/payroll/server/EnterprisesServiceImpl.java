c398f1694743e52fee9b1877620078139b8ab85f
Sergio <svaldepenas@aonsolutions.es>
2022-09-21 18:21:42 +0200

    Fix contract transform comunication date

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/server/EnterprisesServiceImpl.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/server/EnterprisesServiceImpl.java
@@ -3440,7 +3440,8 @@ public class EnterprisesServiceImpl extends AonRemoteServiceServlet implements
 			Integer userId = AonServletUtils.getUserID(connection, login, domainId, parentDomainId);
 			
 			return withParent ? AON.getCertificatesWithParent(domain, domainId, parentDomainId, login, userId) : AON.getCertificates(domain, domainId, login, userId);
-		} catch (SQLException e) {
+		} catch (Exception e) {
+			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		}
 	}
