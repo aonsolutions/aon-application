29b6f1c925777e9dc689c0a45536d4851e5ea59b
anderibz <aibanez@aonsolutions.es>
2022-10-05 15:15:22 +0200

    UDAPA. desadv eroski.

--- aon-seres/src/main/java/com/esferalia/aon/seres/writer/connect2/ConnectDeliveryWriterOccam.java
+++ aon-seres/src/main/java/com/esferalia/aon/seres/writer/connect2/ConnectDeliveryWriterOccam.java
@@ -348,7 +348,11 @@ public class ConnectDeliveryWriterOccam  implements Serializable {
 
 	private List<SEH1B> createSEH1BList(Delivery delivery) {
 		List<SEH1B> list = new ArrayList<>();
-		createSEH1BRecord(delivery);
+		if(isEroski(delivery.getCustomer().getDocument())) {
+			for (DeliveryDetail detail : delivery.getDetails()) {
+				list.add(createSEH1BRecord(detail));
+			}
+		}
 		return list;
 	}
 
@@ -559,24 +563,23 @@ public class ConnectDeliveryWriterOccam  implements Serializable {
 	/**
 	 * Información de lotes
 	 */
-	private SEH1B createSEH1BRecord(Delivery delivery) {
-		// TODO createSEH1BRecord
+	private SEH1B createSEH1BRecord(DeliveryDetail deliveryDetail) {
+		String date = SeresUtils.dateTimeFormat().format(deliveryDetail.getItem().getSerialDate());
 		SEH1B record = new SEH1B();
-		record.setCodigoInstrucciones(null);
+		record.setCodigoInstrucciones("10");
 		record.setMarcasDeEnvio(null);
 		record.setFechaDeCaducidad_36__102_203_(null);
 		record.setFecha_horaRecepcionDeLaMercancia_50__102_203_(null);
 		record.setConsumirAntesDeFecha_361__102_203_(null);
 		record.setCalificadorDeCantidad_11_12_(null);
 		record.setCantidad(null);
-		record.setCalificadorNumeroIdentidad(null);
-		record.setNumeroIdentidad(null);
-		record.setFechaDeEnvasadoOEmpaquetado_365__102_203_(null);
+		record.setCalificadorNumeroIdentidad("BX");
+		record.setNumeroIdentidad(deliveryDetail.getItem().getSerialNumber());
+		record.setFechaDeEnvasadoOEmpaquetado_365__102_203_(date);
 		record.setFechaProduccion_fabricacion_94__102_203_(null);
 		return record;
 	}
-	
-	
+
 	/////////////////////////////////////////////////
 	/////////////////////////////////////////////////
 	/////////////////////////////////////////////////
@@ -915,6 +918,10 @@ public class ConnectDeliveryWriterOccam  implements Serializable {
 
 	private boolean isECI(String document) {
 		return "A28017895".equalsIgnoreCase(document);
+	}
+	
+	private boolean isEroski(String document) {
+		return "F20033361".equalsIgnoreCase(document);
 	}
 
 }
