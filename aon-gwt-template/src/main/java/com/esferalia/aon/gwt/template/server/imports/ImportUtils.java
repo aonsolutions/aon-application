2199b029d58011fc36ff3e8ea739b46ff2b22ff2
anderibz <aibanez@aonsolutions.es>
2022-10-20 12:45:38 +0200

    Imports fix.

---
+++ aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/server/imports/ImportUtils.java
@@ -0,0 +1,27 @@
+package com.esferalia.aon.gwt.template.server.imports;
+
+import java.text.Collator;
+import java.util.Locale;
+
+import com.esferalia.aon.watson.util.AonStringUtils;
+
+public class ImportUtils {
+	
+	public boolean compare(String value1, String value2) {
+		if(value1 == null || value2 == null) return false;
+		Collator c = Collator.getInstance(new Locale("es"));
+		c.setStrength(Collator.PRIMARY);
+		return c.equals(value1, value2);
+	}
+	
+	public boolean compare(String value, String... values) {
+		if(value == null || values == null || values.length == 0) return false;
+		Collator c = Collator.getInstance(new Locale("es"));
+		c.setStrength(Collator.PRIMARY);
+		for (String  value2 : values) {
+			if(value2 != null && c.equals(value, value2))
+				return true;
+		}
+		return false;
+	}
+}
