e27e7c86f45a4eddf8cf1baaf671d12c69b48c25
anderibz <aibanez@aonsolutions.es>
2022-11-02 11:07:41 +0100

    Compile error...

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/IJsonNames.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/IJsonNames.java
@@ -33,6 +33,7 @@ public final class IJsonNames {
 	public static final String ADMINISTRATION = "administration";
 	public static final String ADMINISTRATIVE = "administrative";
 	public static final String ALIAS = "alias";
+	public static final String ALLOW_CONCURRENT = "allowConcurrent";
 	public static final String AMORTIZATION_DESCRIPTION = "amortizationDescription";
 	public static final String AMORTIZATION_PERCENT = "amortizationPercent";
 	public static final String AMORTIZATION_INITIAL_DATE = "amortizationInitialDate";
@@ -185,6 +186,7 @@ public final class IJsonNames {
 	public static final String EXPENSES_PERCENT = "expensesPercent";
 	public static final String EXPENSES_RATIO = "expensesRatio";
 	public static final String EXPIRATION_DATE = "expirationDate";
+	public static final String EXPIRE = "expire";
 	public static final String EXPRESSION = "expression";
 	// ---------------------------------------------------------------------------------------------	[F] 
 	public static final String FARMER_REGIME = "farmerRegime";
@@ -235,6 +237,7 @@ public final class IJsonNames {
 	public static final String INACTIVE = "inactive";
 	public static final String INCREASE_PERCENT = "increasePercent";
 	public static final String INGENET = "ingenet";
+	public static final String INIT_ACTION = "initAction";
 	public static final String INITIATION_DATE = "initiationDate";
 	public static final String INSIDE_PERIOD = "insidePeriod";
 	public static final String INTE = "inte";
@@ -270,6 +273,7 @@ public final class IJsonNames {
 	public static final String KIND = "kind";
 	// ---------------------------------------------------------------------------------------------	[L] 
 	public static final String LANGUAGE = "language";
+    public static final String LAST_ACCESS = "lastAccess";
 	public static final String LAST_ACCESS_DATE = "lastAccessDate";
 	public static final String LAST_ACCESS_USER = "lastAccessUser";
 	public static final String LEDGER_ACCOUNT = "ledgerAccount";
@@ -280,6 +284,8 @@ public final class IJsonNames {
 	public static final String LEVEL = "level";
 	public static final String LIMIT = "limit";
 	public static final String LINE = "line";
+	public static final String LINES_PAGE_LIMIT = "linesPageLimit";
+	public static final String LOCALE = "locale";
 	public static final String LOGIN = "login";
 	public static final String LOGO = "logo";
 	public static final String LOTABLE = "lotable";
@@ -343,6 +349,7 @@ public final class IJsonNames {
 	public static final String PACK_UNITS = "packUnits";
 	public static final String PACK_UNITS_TAG = "packUnitsTag";
 	public static final String PAGE = "page";
+	public static final String PAGE_LIMIT = "pageLimit";
 	public static final String PAGE_OFFSET = "pageOffset";
 	public static final String PAGE_OFFSET_TEXT = "pageOffsetText";
 	public static final String PAID = "paid";
@@ -440,6 +447,7 @@ public final class IJsonNames {
 	public static final String SALES_DETAIL = "salesDetail";
 	public static final String SALES_RATIO = "salesRatio";
 	public static final String SCHEMA = "schema";
+	public static final String SCHEMA_FIRST_DOMAIN = "schema_first_domain";
 	public static final String SCOPE = "scope";
 	public static final String SECTION = "section";
 	public static final String SECURITY_LEVEL = "securityLevel";
@@ -499,6 +507,7 @@ public final class IJsonNames {
 	public static final String TAS = "tas";
 	public static final String TASK = "task";
 	public static final String TASK_HOLDER = "task_holder";
+	public static final String TASK_HOLDERS = "taskHolders";
 	public static final String TAXES = "taxes";
 	public static final String TAX = "tax";
 	public static final String TAG = "tag";
@@ -514,6 +523,7 @@ public final class IJsonNames {
 	public static final String TO_DATE = "toDate";
 	public static final String TO_DUE_DATE = "toDueDate";
 	public static final String TO_NUMBER = "toNumber";
+	public static final String TOOLBAR = "toolbar";
 	public static final String TOTAL = "total";
 	public static final String TOTAL_BALANCE = "totalBalance";
 	public static final String TOTAL_PACKAGES = "totalPackages";
