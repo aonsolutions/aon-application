dba2f1866a93bf7b6d4b343c1d01669b483a4249
EukeCastellano <ecastellano@aonsolutions.es>
2022-11-16 11:37:02 +0100

    Mod180. Ej 2022. Leve actualiz<ción & refactor.

--- aon-occam/src/main/java/com/esferalia/aon/occam/server/fiscal/format/mod180/Mod180Writer.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/server/fiscal/format/mod180/Mod180Writer.java
@@ -1,19 +1,54 @@
-package com.esferalia.aon.occam.server.fiscal.format;
+package com.esferalia.aon.occam.server.fiscal.format.mod180;
 
 import java.io.IOException;
 import java.io.Writer;
 
 import com.esferalia.aon.occam.api.model.fiscal.Mod180;
 import com.esferalia.aon.occam.api.model.fiscal.Mod180Detail;
-import com.esferalia.aon.watson.util.AonStringUtils;
+import com.esferalia.aon.watson.error.AonCoreException;
 
 public class Mod180Writer {
 
 	@FunctionalInterface
-	private interface IPropertyFiller {
+	interface IPropertyFiller {
 		public void propertyFill(Writer writer, Mod180 mod180, Mod180Detail detail) throws IOException;
 	}
+	private enum Writers {
+		ALL_2014 {
 
+			@Override
+			void fill(Mod180 mod180, Writer wr) throws IOException {
+				Mod180File2014.fill(mod180, wr);
+			}
+			
+			@Override
+			boolean accept(Mod180 mod180) {
+				return true;				
+			}
+		},
+		;
+
+		abstract boolean accept(Mod180 mod180);
+		abstract void fill(Mod180 mod180, Writer wr) throws IOException;
+	}
+	
+	public static void fillWriter(Mod180 mod180, Writer wr) throws IOException {
+		boolean filled = false;
+		for (Writers writer : Writers.values()) {
+			if (writer.accept(mod180)) {
+				writer.fill(mod180, wr);
+				filled = true;
+			}
+		}
+		wr.flush();
+		if (!filled) {
+			throw new AonCoreException("La generaci\u00F3n de el modelo no est\u00E1 soportada.");
+		}
+	}
+
+/*	
+	
+	
 	private enum Mod180File2014 {
 		// REGISTRO DE DECLARANTE.
 		TYPE_1 (new IPropertyFiller[] { 
@@ -114,4 +149,5 @@ public class Mod180Writer {
 		}
 		writer.flush();
 	}
+*/
 }
