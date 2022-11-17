f3f819ab0733856636bc21238909b6a50efe04c4
EukeCastellano <ecastellano@aonsolutions.es>
2022-11-17 16:45:01 +0100

    Mod 190. Ej 2022 & Refactor.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod190/Model190.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod190/Model190.java
@@ -78,7 +78,7 @@ public class Model190 extends MainEntryPoint {
 
 		@Override
 		public void onNew() {
-			newModel(getOptions(), 2021);
+			newModel(getOptions(), 2022);
 		}
 
 		@Override
--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/FiscalMenuDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/FiscalMenuDAO.java
@@ -30,6 +30,7 @@ import com.esferalia.aon.occam.api.model.type.Period;
 import com.esferalia.aon.occam.impl.jooq.dao.fiscal.FiscalModelDAO;
 import com.esferalia.aon.occam.impl.jooq.dao.fiscal.mod180.Mod180DAO;
 import com.esferalia.aon.occam.impl.jooq.dao.fiscal.mod184.Mod184DAO;
+import com.esferalia.aon.occam.impl.jooq.dao.fiscal.mod190.Mod190DAO;
 import com.esferalia.aon.occam.impl.jooq.dao.fiscal.mod193.Mod193DAO;
 import com.esferalia.aon.watson.util.AonEnumUtils;
 import com.esferalia.aon.watson.util.AonStringUtils;
