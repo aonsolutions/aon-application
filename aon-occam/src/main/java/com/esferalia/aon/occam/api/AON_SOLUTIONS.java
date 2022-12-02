69fd7f72a98eaa0506f25ae715a0475826a32f75
RAY VASQUEZ <anrto_94_5@hotmail.com>
2022-12-02 12:05:22 +0100

    taskDAO change get ids

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/AON_SOLUTIONS.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/AON_SOLUTIONS.java
@@ -947,9 +947,9 @@ public class AON_SOLUTIONS {
 		}
 	}
 	
-	public static Stream<Task> getTaskParentOrChildStream(Domain domain, User user, TaskFilter filter, boolean excludeDescription) {
+	public static Stream<Task> getTaskParentOrChildStream(Domain domain, User user, TaskFilter filter) {
 		try (CloseableAONContext ctx = AONContext.getAONContext(domain, user)){
-			return getTask2().getTaskParentOrChildStream(ctx, filter, excludeDescription);
+			return getTask2().getTaskParentOrChildStream(ctx, filter);
 		}
 	}
 	
