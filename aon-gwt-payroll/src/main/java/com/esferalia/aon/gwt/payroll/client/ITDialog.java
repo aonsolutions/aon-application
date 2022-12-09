1a490daa86c8444b560e7d0114dee368349a6497
Sergio <svaldepenas@aonsolutions.es>
2022-12-09 11:09:28 +0100

    'Periodo de Observacion por Enfermedad Profesional' replaces old 'Enfermedad comun periodo de carencia'

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/ITDialog.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/ITDialog.java
@@ -457,9 +457,9 @@ public abstract class ITDialog extends AonCustomDialog {
 		causeLowPart.addItem("Riesgo para el embarazo", "4");
 		causeLowPart.addItem("Riesgo durante la lactancia", "5");
 		causeLowPart.addItem("Accidente no laboral", "6");
-		causeLowPart.addItem("Enfermedad com\u00Fan periodo de carencia", "7");
+//		causeLowPart.addItem("Enfermedad com\u00Fan periodo de carencia", "7");
+		causeLowPart.addItem("Periodo de Observaci\u00f3n por Enfermedad Profesional", "7");
 		causeLowPart.addItem("Enfermedad com\u00Fan, prestaci\u00F3n profesional (COVID-19)", "8");
-		causeLowPart.addItem("Periodo de Observaci\u00f3n por Enfermedad Profesional", "9");
 		
 		causeHighPart.clear();
 		causeHighPart.addItem("-", "-1");
