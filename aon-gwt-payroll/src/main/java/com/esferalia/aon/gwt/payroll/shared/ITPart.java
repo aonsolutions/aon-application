eb7d66956f8c213c2c372a3697d68da516313adc
Sergio Valdepeñas <svaldepenas@MacBook-Pro-Sergio.local>
2023-01-09 14:01:38 +0100

    ITDialg, fix collegeNumber, fix indexOutRangeException

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/shared/ITPart.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/shared/ITPart.java
@@ -20,6 +20,9 @@ public class ITPart implements Serializable {
 	private String date;
 	private Byte status;
 	
+	private boolean delete = false;
+	private boolean modify = false;
+	
 	public ITPart() {
 		super();
 	}
@@ -66,6 +69,7 @@ public class ITPart implements Serializable {
 
 	public ITPart setCollegeNumber(String collegeNumber) {
 		this.collegeNumber = collegeNumber;
+		setModify(true);
 		return this;
 	}
 
@@ -75,6 +79,7 @@ public class ITPart implements Serializable {
 
 	public ITPart setConfirmOrderNumber(Byte confirmOrderNumber) {
 		this.confirmOrderNumber = confirmOrderNumber;
+		setModify(true);
 		return this;
 	}
 
@@ -84,6 +89,7 @@ public class ITPart implements Serializable {
 
 	public ITPart setCias(String cias) {
 		this.cias = cias;
+		setModify(true);
 		return this;
 	}
 
@@ -93,6 +99,7 @@ public class ITPart implements Serializable {
 
 	public ITPart setDate(Date date) {
 		this.date = format(date);
+		setModify(true);
 		return this;
 	}
 
@@ -105,6 +112,24 @@ public class ITPart implements Serializable {
 		return this;
 	}
 	
+	public boolean isDelete() {
+		return delete;
+	}
+
+	public ITPart setDelete(boolean delete) {
+		this.delete = delete;
+		return this;
+	}
+
+	public boolean isModify() {
+		return modify;
+	}
+
+	public ITPart setModify(boolean modify) {
+		this.modify = modify;
+		return this;
+	}
+
 	@Override
     public String toString() {
         return "ITPart{"
@@ -115,7 +140,9 @@ public class ITPart implements Serializable {
     		+ "collegeNumber=" + collegeNumber +","
     		+ "cias=" + cias +","
     		+ "date=" + date +","
-    		+ "status=" + status +"}";
+    		+ "status=" + status +","
+    		+ "status=" + delete +","
+    		+ "status=" + modify +"}";
     }
 		
 }
