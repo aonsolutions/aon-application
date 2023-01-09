3f509b75cddd6f9f8759723eb14c25b924a9c969
EukeCastellano <ecastellano@aonsolutions.es>
2023-01-09 20:35:15 +0100

    WithholdingType in Faces.

--- aon.entity.common/src/main/java/com/code/aon/config/enumeration/WithholdingType.java
+++ aon.entity.common/src/main/java/com/code/aon/config/enumeration/WithholdingType.java
@@ -1,31 +1,50 @@
 package com.code.aon.config.enumeration;
 
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.CAPITAL_INMOBILIARIO;
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.CAPITAL_MOBILIARIO;
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.DERECHOS_IMAGEN;
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.GANANCIAS_PATRIMONIALES;
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.OTRAS;
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.PROFESIONAL;
+import static com.code.aon.config.enumeration.WithholdingTypeGroup.TRABAJO;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 import com.code.aon.common.enumeration.IResourceable;
 
-/**
- * Enummeration to identify the different types of an Invoice.
- * 
- */
 public enum WithholdingType implements IResourceable {
-
-	/** PROFESIONALES */
-	PROFESSIONAL,
+	PROFESSIONAL(PROFESIONAL)
+	,RENTING(CAPITAL_INMOBILIARIO)
+	,MOVABLE_CAPITAL(CAPITAL_MOBILIARIO)
+	,FARMER(OTRAS)
+	,TRANSPORT_OPERATOR(OTRAS)
+	,M190_G_02	(PROFESIONAL)
+	,M190_G_03	(PROFESIONAL)
+	,M190_H_02 	(OTRAS)
+	,M190_H_03	(OTRAS)
+	,M190_I_01	(OTRAS)
+	,M190_I_02	(OTRAS)
+	,M190_J		(DERECHOS_IMAGEN)
+	,M190_K_01	(GANANCIAS_PATRIMONIALES)
+	,M190_K_03	(GANANCIAS_PATRIMONIALES)
+	,M190_K_02	(GANANCIAS_PATRIMONIALES)
+	,M193_C1(CAPITAL_MOBILIARIO)
+	,M193_C2(CAPITAL_MOBILIARIO)
+	,M193_C3(CAPITAL_MOBILIARIO)
+	,M190_F_01(TRABAJO)
+	,M190_F_02_1(TRABAJO)
+	,M190_F_02_2(TRABAJO)
+	;
+	private WithholdingTypeGroup group;
 	
-	/** ARRENDAMIENTO */
-	RENTING,
+	private WithholdingType(WithholdingTypeGroup group){
+		this.group = group;
+	}
 	
-	/** CAPITAL MOBILIARIO */
-	MOVABLE_CAPITAL,
-	
-	/** AGRICULTOR */
-	FARMER,
-	
-	/** TRANSPORTISTAS Y ASIMILADOS */
-	TRANSPORT_OPERATOR;
-
+	public WithholdingTypeGroup getGroup() {
+		return group;
+	}
     /** Message key prefix. */
     private static final String MSG_KEY_PREFIX = "aon_enum_withholding_type_";
 
@@ -41,4 +60,23 @@ public enum WithholdingType implements IResourceable {
         ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale); 
 		return bundle.getString(MSG_KEY_PREFIX + toString());
     }
+    
+	public static WithholdingType[] ORDERED_VALUES = new WithholdingType[] { 
+			 // PROFESIONAL 
+		PROFESSIONAL,M190_G_02,M190_G_03
+		// CAPITAL_INMOBILIARIO
+		,RENTING
+		//  OTRAS
+		,FARMER,M190_H_02,M190_H_03,TRANSPORT_OPERATOR,M190_I_01,M190_I_02
+		// TRABAJO
+		,M190_F_01,M190_F_02_1,M190_F_02_2
+		// CAPITAL_MOBILIARIO
+		,MOVABLE_CAPITAL,M193_C1,M193_C2,M193_C3
+		// DERECHOS_IMAGEN
+		,M190_J
+		// GANANCIAS_PATRIMONIALES
+		,M190_K_01,M190_K_03,M190_K_02
+	};
+
+    
 }
