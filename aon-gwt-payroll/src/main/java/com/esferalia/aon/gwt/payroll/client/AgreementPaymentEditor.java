93f761013e43e5c73f01890896b3f2e32e0d8446
Sergio Valdepeñas <svaldepenas@MacBook-Pro-Sergio.local>
2023-01-25 23:04:07 +0100

    AgreementPaymentEditor get associated extra by issueDate & expression

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/AgreementPaymentEditor.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/AgreementPaymentEditor.java
@@ -753,13 +753,40 @@ public abstract class AgreementPaymentEditor extends AonCustomDialog {
 	}
 
 	private AgreementExtra findSummerExtra() {
-		Optional<AgreementExtra> extraOpt = allExtras.stream().filter(extraIt -> AonStringUtils.containsIgnoreCase(extraIt.getIssueDate(), "6") || AonStringUtils.containsIgnoreCase(extraIt.getIssueDate(), "7")).findFirst();
-		return extraOpt.isPresent() ? extraOpt.get() : null;
+		String expression = payment.getExpression();
+		if(expression.contains(" ")) {
+			String[] expressions = expression.split(" ");
+			expression = "";
+			for(int i=0; i<expressions.length; i++)
+				if(i!=0) expression+= expressions[i] + " ";
+		}
+		
+		for(AgreementExtra extraIt : allExtras)
+			if((AonStringUtils.containsIgnoreCase(extraIt.getIssueDate(), "6") || AonStringUtils.containsIgnoreCase(extraIt.getIssueDate(), "7")) && isSameExpression(extraIt, expression))
+				return extraIt;
+		
+		return null;
 	}
 
 	private AgreementExtra findWinterExtra() {
-		Optional<AgreementExtra> extraOpt = allExtras.stream().filter(extraIt -> AonStringUtils.containsIgnoreCase(extraIt.getIssueDate(), "12")).findFirst();
-		return extraOpt.isPresent() ? extraOpt.get() : null;
+		String expression = payment.getExpression();
+		if(expression.contains(" ")) {
+			String[] expressions = expression.split(" ");
+			expression = "";
+			for(int i=0; i<expressions.length; i++)
+				if(i!=0) expression+= expressions[i] + " ";
+		}
+		
+		for(AgreementExtra extraIt : allExtras)
+			if(AonStringUtils.containsIgnoreCase(extraIt.getIssueDate(), "12") && isSameExpression(extraIt, expression))
+				return extraIt;
+		
+		return null;
+	}
+
+	private boolean isSameExpression(AgreementExtra extraIt, String expression) {
+		Optional<Payment> paymentIt = allPayments.stream().filter(payment -> payment.getId().equals(extraIt.getAgreementPayment())).findFirst();
+		return paymentIt.isPresent() && AonStringUtils.containsIgnoreCase(paymentIt.get().getExpression(), expression.trim()) ? true : false;
 	}
 
 	private String getExtraType(AgreementExtra extra) {
