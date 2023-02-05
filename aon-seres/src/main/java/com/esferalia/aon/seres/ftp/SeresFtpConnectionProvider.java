ea58dd9fc57b08ace30d485d87dc530cc8bec62a
anderibz <aibanez@aonsolutions.es>
2023-02-05 14:10:30 +0100

    Seres udapa. SFTP.

--- aon-seres/src/main/java/com/esferalia/aon/seres/ftp/SeresFtpConnectionProvider.java
+++ aon-seres/src/main/java/com/esferalia/aon/seres/ftp/SeresFtpConnectionProvider.java
@@ -1,25 +1,20 @@
 package com.esferalia.aon.seres.ftp;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.commons.io.output.ByteArrayOutputStream;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.time.DateUtils;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPFileFilter;
-import org.apache.commons.net.ftp.FTPReply;
 
-import com.code.aon.common.ILogger;
-import com.esferalia.aon.seres.ftp.SeresFtpConnectionProvider.StoreResult;
+import com.esferalia.aon.occam.api.model.seres.SeresInfo;
+import com.esferalia.aon.occam.api.model.seres.SeresPath;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.SftpException;
 
 public class SeresFtpConnectionProvider implements Serializable {
 
@@ -32,15 +27,12 @@ public class SeresFtpConnectionProvider implements Serializable {
 	
 	
 	public static boolean checkLogin(String server, Integer port, String user,
-			String passwd) throws FtpLoginException, FtpException {
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException {
-				// FtpLoginException is throwed if login failed
-			}
-		};
-		ftp.connect(server, port, user, passwd);
-		return true;
+			String passwd) throws JSchException {
+		SeresInfo info = new SeresInfo()
+				.setServer(server)
+				.setUser(user)
+				.setPassword(passwd);
+		return SeresSftpConnectionProvider.checkLogin(info);
 	}
 	
 	static class StoreResult {
@@ -49,110 +41,80 @@ public class SeresFtpConnectionProvider implements Serializable {
 	}
 	public static boolean storeFile(String remotePath, String fileName,
 			InputStream localInputStream, String server, Integer port,
-			String user, String passwd) throws FtpLoginException, FtpException {
-		StoreResult result = new StoreResult();
-		result.completed = false;
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException, FtpException {
-				this.storeFile(remotePath, fileName, localInputStream,
-						result);
-				localInputStream.close();
-			}
-		};
-		ftp.connect(server, port, user, passwd);
-		return result.completed;
+			String user, String passwd) throws JSchException, SftpException  {
+		SeresInfo info = new SeresInfo()
+				.setServer(server)
+				.setUser(user)
+				.setPassword(passwd)
+				.setSeresPath(SeresPath.safeValueOf(remotePath));
+		SeresSftpConnectionProvider.storeFile(info, fileName, localInputStream);
+		return true;
 	}
 	
 	public static byte[] retrieveFile(String remotePath, String remoteFile,
-			String server, Integer port, String user, String passwd)
-			throws FtpLoginException, FtpException {
-
-		ByteArrayOutputStream localOutputStream = new ByteArrayOutputStream();
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException {
-				boolean success = this.retrieveFile(remotePath, remoteFile,
-						localOutputStream);
-				if (!success) {
-					System.out
-							.println("There was some problem retrieving file.");
-					return;
-				}
-				// TODO log me
-//				System.out.println("File was downloaded!");
-			}
-		};
-		try {
-			ftp.connect(server, port, user, passwd);
-			return localOutputStream.toByteArray();
-		} finally {
-			try {
-				localOutputStream.close();
-			} catch (IOException e) {
-				// nothing
-			}
-		}
+			String server, Integer port, String user, String passwd) throws SftpException, JSchException {
+		SeresInfo info = new SeresInfo()
+				.setServer(server)
+				.setUser(user)
+				.setPassword(passwd)
+				.setSeresPath(SeresPath.safeValueOf(remotePath));
+		
+		return SeresSftpConnectionProvider.retrieveFile(info, remoteFile);
 	}
 	
 	public static List<String> retrieveDirectoryList(String remotePath,
 			String server, Integer port, String user, String passwd)
-			throws FtpLoginException, FtpException {
-		List<String> fileList = new LinkedList<>();
-
-//		printFileTree("/", "", 0);
-
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException {
-				this.fillDirectoryList(remotePath, fileList);
-			}
-		};
-
-		ftp.connect(server, port, user, passwd);
-
-		return fileList;
+			throws JSchException, SftpException {
+		SeresInfo info = new SeresInfo()
+				.setServer(server)
+				.setUser(user)
+				.setPassword(passwd)
+				.setSeresPath(SeresPath.safeValueOf(remotePath));
+		
+		List<LsEntry> list = SeresSftpConnectionProvider.retrieveDirectoryList(info);
+		return list.stream()
+				.filter(f -> !f.getFilename().equals(".") && !f.getFilename().equals(".."))
+				.map(r -> remotePath + "/" + r.getFilename())
+				.collect(Collectors.toCollection(LinkedList::new));
 	}
 	
 	public static List<FtpFile> retrieveFileList(String remotePath, Date start,
 			Date end, String server, Integer port, String user, String passwd)
-			throws FtpLoginException, FtpException {
-		List<FtpFile> fileList = new LinkedList<>();
+			throws JSchException, SftpException {
+		SeresInfo info = new SeresInfo()
+				.setServer(server)
+				.setUser(user)
+				.setPassword(passwd)
+				.setSeresPath(SeresPath.safeValueOf(remotePath));
+		
+		List<LsEntry> list = SeresSftpConnectionProvider.retrieveDirectoryList(info);
 
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException, FtpException {
-				if (changeWorkingDirectory(remotePath)) {
-					this.fillFileList(remotePath, start, end, fileList);
-				} else {
-					System.out.println("Could not change directory!");
-				}
-			}
-		};
-
-		ftp.connect(server, port, user, passwd);
-
-		return fileList;
+		return list.stream()
+		.filter(f -> !f.getFilename().equals(".") && !f.getFilename().equals("..") 
+			&& start.before(new Date(f.getAttrs().getMTime() * 1000L))
+			&& end.after(new Date(f.getAttrs().getMTime() * 1000L)))
+		.map(r -> {
+			FTPFile file = new FTPFile();
+			file.setName(r.getFilename());
+		
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(r.getAttrs().getMTime() * 1000L);
+			file.setTimestamp(calendar);
+			
+			return new FtpFile(file);
+		}).collect(Collectors.toCollection(LinkedList::new));
 	}
 	
 	public static boolean deleteFile(String remotePath,
 			String server, Integer port, String user, String passwd)
-					throws FtpLoginException, FtpException {
-		boolean completed = false;
-		
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException, FtpException {
-				this.deleteFile(remotePath, completed);
-			}
-		};
-		
-		ftp.connect(server, port, user, passwd);
-		
-		return completed;
+					throws SftpException, JSchException {
+		SeresInfo info = new SeresInfo()
+				.setServer(server)
+				.setUser(user)
+				.setPassword(passwd);
+		SeresSftpConnectionProvider.deleteFile(info, remotePath);
+		return true;
 	}
-	
-	
 	
 }
 
@@ -162,274 +124,3 @@ public class SeresFtpConnectionProvider implements Serializable {
 
 
 
-abstract class FtpConnector {
-	
-	private List<String> fileTree;
-	
-	private FTPClient ftp = new FTPClient();
-	
-	public abstract void onSuccess() throws IOException, FtpException;
-	
-	
-	public void connect(String server, Integer port, String user, String passwd)
-			throws FtpLoginException, FtpException {
-		assignJvmSystemProperies();
-		try {
-			boolean success = login(server, port, user, passwd);
-			if (success) {
-				onSuccess();
-			}
-		} catch (IOException ex) {
-			System.out.println("Error: " + ex.getMessage());
-			ex.printStackTrace();
-		} finally {
-			restoreJvmSystemProperies();
-			disconnect();
-		}
-	}
-	
-	private boolean login(String server, Integer port, String user,
-			String passwd) throws IOException, FtpLoginException, FtpException {
-		try {
-			ftp.connect(server, port);
-		} catch (Exception e) {
-//			System.out.println("Connection failed! (check hostname and port)");
-			throw new FtpException("Connection failed! (check hostname and port)", e);
-		}
-
-		// TODO log me
-//		String replyString = ftp.getReplyString();
-//		System.out.println("REPLY: " + replyString);
-
-		int replyCode = ftp.getReplyCode();
-		if (!FTPReply.isPositiveCompletion(replyCode)) {
-			System.err.println("Some error!");
-			return false;
-		}
-		ftp.enterLocalPassiveMode();
-		// TODO log me
-//		System.out.print("FTP LOGIN: " + server
-//				+ (port != null ? ":" + port : "") + "@" + user
-//				+ " (using password "
-//				+ (passwd != null && !"".equals(passwd) ? "YES" : "NO") + ")");
-		boolean success = ftp.login(user, passwd);
-		if (success) {
-			// TODO log me
-//			System.out.println(" -> SUCCESS!");
-		} else {
-			// TODO log me
-//			System.out.println(" -> FAILED! (check username and password)");
-			throw new FtpLoginException(
-					"Login failed! (check username and password)");
-		}
-		return success;
-	}
-	
-	private void disconnect() {
-		try {
-			if (ftp.isConnected()) {
-				ftp.logout();
-				ftp.disconnect();
-			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	protected boolean changeWorkingDirectory(String path) throws IOException{
-		boolean success = ftp.changeWorkingDirectory(path);
-		if(success){
-			// TODO log me
-//			System.out.print("WorkingDirectory changed to ");
-//			System.out.println("'"+ftp.printWorkingDirectory()+"'");
-		}
-		return success;
-	}
-	
-		
-	protected void printFileTree(String parentDir, String currentDir, int level)
-			throws IOException {
-		buildFullTree(parentDir, currentDir, level);
-		fileTree.forEach(System.out::println);
-	}
-	
-	protected void buildFullTree(String parentDir, String currentDir,
-			int level) throws IOException {
-		String dirToList = parentDir!=null?parentDir:"/";
-		
-		if (currentDir==null || currentDir.equals("")) {
-			fileTree = new LinkedList<>();
-		}
-		if (!currentDir.equals("")) {
-			dirToList += "/" + currentDir;
-		}
-		
-		FTPFile[] subFiles = ftp.listFiles(dirToList);
-		if (subFiles != null && subFiles.length > 0) {
-			for (FTPFile aFile : subFiles) {
-				String currentFileName = aFile.getName();
-				if (currentFileName.equals(".")
-						|| currentFileName.equals("..")) {
-					continue;
-				}
-				if (aFile.isDirectory()) {
-					fileTree.add(String.format("%0" + (level + 1) + "d", 0)
-							.replace("0", "\t")
-							+ "[" + currentFileName + "]");
-					buildFullTree(dirToList, currentFileName, level + 1);
-				} else {
-					fileTree.add(String.format("%0" + (level + 1) + "d", 0)
-							.replace("0", "\t") + currentFileName);
-				}
-			}
-		}
-	}
-	
-	protected void fillDirectoryList(String directoryPath, List<String> list) throws IOException {
-		if (list == null) {
-			list = new LinkedList<>();
-		}
-		FTPFile[] subDirectories = null;
-		try {
-			if(directoryPath!=null){
-				subDirectories = ftp.listFiles(directoryPath);
-			} else {
-				subDirectories = ftp.listFiles();
-			}
-		} catch (IOException e) {
-			subDirectories = ftp.listFiles();
-		}
-		if (subDirectories != null && subDirectories.length > 0) {
-			for (FTPFile f : subDirectories) {
-				String currentFileName = f.getName();
-				if (currentFileName.equals(".")
-						|| currentFileName.equals("..")) {
-					continue;
-				}
-				if (f.isDirectory()) {
-					list.add((directoryPath!=null?directoryPath:"")+"/"+f.getName());
-					fillDirectoryList((directoryPath!=null?directoryPath:"")+"/"+f.getName(), list);
-				}
-			}
-		}
-	}
-	
-	protected void fillFileList(String directoryPath, Date start, Date end,
-			List<FtpFile> list) throws IOException, FtpException {
-		if (list == null) {
-			list = new LinkedList<>();
-		}
-		FTPFile[] subFiles = null;
-		try {
-			FTPFileFilter filter = null;
-			if (start != null || end != null) {
-				filter = new FTPFileFilter() {
-					@Override
-					public boolean accept(FTPFile arg0) {
-						return (start == null ? true : DateUtils.ceiling(
-								arg0.getTimestamp().getTime(),
-								Calendar.DAY_OF_MONTH)
-								.compareTo(
-										DateUtils.ceiling(start,
-												Calendar.DAY_OF_MONTH)) >= 0)
-								&& (end == null ? true : DateUtils.ceiling(
-										arg0.getTimestamp().getTime(),
-										Calendar.DAY_OF_MONTH).compareTo(
-										DateUtils.ceiling(end,
-												Calendar.DAY_OF_MONTH)) <= 0);
-					}
-				};
-			}
-			if (directoryPath != null && filter != null) {
-				subFiles = ftp.listFiles(directoryPath, filter);
-			} else if (directoryPath != null) {
-				subFiles = ftp.listFiles(directoryPath);
-			} else {
-				subFiles = ftp.listFiles();
-			}
-		} catch (IOException e) {
-			throw new FtpException(e);
-		}
-		if (subFiles != null && subFiles.length > 0) {
-			for (FTPFile f : subFiles) {
-				String currentFileName = f.getName();
-				if (currentFileName.equals(".") || currentFileName.equals("..")) {
-					continue;
-				}
-				if (f.isFile()) {
-					list.add(new FtpFile(f));
-				}
-			}
-		}
-	}
-
-	protected boolean retrieveFile(String remotePath, String remoteFile, OutputStream localOutputStream) throws IOException {
-		boolean success = changeWorkingDirectory(remotePath);
-		if (success) {
-			ftp.setFileType(FTP.BINARY_FILE_TYPE);
-			success = ftp.retrieveFile(remoteFile, localOutputStream);
-		}
-		return success;
-	}
-	
-	protected void storeFile(String remotePath, String fileName, InputStream localInputStream, StoreResult result) throws IOException, FtpException {
-		boolean success = changeWorkingDirectory(remotePath);
-		if (success) {
-			ftp.setFileType(FTP.BINARY_FILE_TYPE);
-			result.completed =  ftp.storeFile(fileName, localInputStream);
-			if (!result.completed) {
-				// TODO log me
-				result.message = "ERROR: the file is not uploaded successfully.";
-				System.out.println("ERROR: the file is not uploaded successfully.");
-				throw new FtpException("Ha ocurrido un error en la transmision del fichero");
-			}
-//			completed = ftp.completePendingCommand();
-		}
-	}
-	
-	protected boolean deleteFile(String remotePath, boolean completed) throws IOException {
-		ftp.setFileType(FTP.BINARY_FILE_TYPE);
-		completed = ftp.deleteFile(remotePath);
-		return completed;
-	}
-	
-	
-	// **************************************************
-	// JVM system properties
-	// **************************************************
-	private String preferIPv4Stack = null;
-	private final static String PROPERTY_preferIPv4Stack = "java.net.preferIPv4Stack";
-	
-	private void assignJvmSystemProperies() {
-		preferIPv4Stack = System.getProperty(PROPERTY_preferIPv4Stack);
-		System.setProperty(PROPERTY_preferIPv4Stack, "true");
-	}
-
-	private void restoreJvmSystemProperies() {
-		if(StringUtils.isBlank(preferIPv4Stack)){
-			System.clearProperty(PROPERTY_preferIPv4Stack);
-		} else {
-			System.setProperty(PROPERTY_preferIPv4Stack, preferIPv4Stack);
-		}
-	}
-
-	
-	class DefaultLogger implements ILogger {
-
-		@Override
-		public void error(String arg0) {
-			System.err.println("# FTP ERROR: " + arg0);
-		}
-
-		@Override
-		public void info(String arg0) {
-			System.out.println("# FTP: " + arg0);
-		}
-
-		@Override
-		public void warn(String arg0) {
-			System.out.println("# FTP WARN: " + arg0);
-		}
-		
-	}
-}
--- aon-seres/src/main/java/com/esferalia/aon/seres/ftp/SeresSftpConnectionProvider.java
+++ aon-seres/src/main/java/com/esferalia/aon/seres/ftp/SeresSftpConnectionProvider.java
@@ -1,17 +1,18 @@
 package com.esferalia.aon.seres.ftp;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
-import org.apache.commons.net.ftp.FTPFile;
 
+import com.esferalia.aon.occam.api.model.seres.SeresInfo;
+import com.esferalia.aon.occam.api.model.seres.SeresPath;
+import com.esferalia.aon.watson.server.AonDateUtils;
 import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
@@ -26,40 +27,33 @@ public class SeresSftpConnectionProvider implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	
-	public static boolean checkLogin(String server, Integer port, String user,
-			String passwd) throws JSchException {
+	public static boolean checkLogin(SeresInfo info) throws JSchException {
 		JSch jsch = new JSch();
-	    Session jschSession = jsch.getSession(user, server);
-	    jschSession.setPassword(passwd);
-	    jschSession.connect();	    
-	    
+	    Session jschSession = jsch.getSession(info.getUser(), info.getServer());
+	    jschSession.setPassword(info.getPassword());
+	    jschSession.connect();
+	    jschSession.disconnect();
 		return true;
 	}
 	
-	public static Session connect(String server, Integer port, String user,
-			String passwd) throws JSchException {
+	public static Session connect(SeresInfo info) throws JSchException {
+		java.util.Properties config = new java.util.Properties(); 
+		config.put("StrictHostKeyChecking", "no");
+		
 		JSch jsch = new JSch();
-	    Session jschSession = jsch.getSession(user, server);
-	    jschSession.setPassword(passwd);
+	    Session jschSession = jsch.getSession(info.getUser(), info.getServer(), info.getPort());
+	    jschSession.setPassword(info.getPassword());
+	    jschSession.setConfig(config);
 	    jschSession.connect();
 	    return jschSession;
 	}
 	
-	
-	
-	static class StoreResult {
-		boolean completed;
-		String message;
-	}
-	
-	public static boolean storeFile(String remotePath, String fileName,
-			InputStream localInputStream, String server, Integer port,
-			String user, String passwd) throws JSchException, SftpException {
-		Session session = connect(server, port, user, passwd);
+	public static boolean storeFile(SeresInfo info, String fileName, InputStream localInputStream) throws JSchException, SftpException {
+		Session session = connect(info);
 		ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
 		
 		channel.connect();
-		channel.cd(remotePath);		
+		channel.cd(info.getSeresPath().getPath());		
 		channel.put(localInputStream, fileName);
 		channel.exit();
 		channel.disconnect();
@@ -67,14 +61,13 @@ public class SeresSftpConnectionProvider implements Serializable {
 		return true;
 	}
 	
-	public static byte[] retrieveFile(String remotePath, String remoteFile,
-			String server, Integer port, String user, String passwd)
+	public static byte[] retrieveFile(SeresInfo info, String remoteFile)
 			throws SftpException, JSchException {
-		Session session = connect(server, port, user, passwd);
+		Session session = connect(info);
 		ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
 		
 		channel.connect();
-		channel.cd(remotePath);
+		channel.cd(info.getSeresPath().getPath());
 		ByteArrayOutputStream localOutputStream = new ByteArrayOutputStream();
 		channel.get(remoteFile, localOutputStream);
 		channel.exit();
@@ -83,54 +76,60 @@ public class SeresSftpConnectionProvider implements Serializable {
 		return localOutputStream.toByteArray();
 	}
 		
-	public static List<String> retrieveDirectoryList(String remotePath,
-			String server, Integer port, String user, String passwd)
+	public static List<ChannelSftp.LsEntry> retrieveDirectoryList(SeresInfo info)
 			throws JSchException, SftpException {
-		Session session = connect(server, port, user, passwd);
+		Session session = connect(info);
 		ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
 	
 		channel.connect();
-		channel.cd(remotePath);
-		Vector<String> vector = channel.ls(remotePath);
+		channel.cd(info.getSeresPath().getPath());
+		channel.ls(info.getSeresPath().getPath());
+	
+		Vector<ChannelSftp.LsEntry> vector =  channel.ls(info.getSeresPath().getPath());
 		channel.exit();
 		channel.disconnect();
 		session.disconnect();
 		return vector;
 	}
 	
-	public static List<FtpFile> retrieveFileList(String remotePath, Date start,
-			Date end, String server, Integer port, String user, String passwd)
-			throws FtpLoginException, FtpException {
-		FTPFile file = new FTPFile();
-		
-		List<FtpFile> fileList = new LinkedList<>();
-
-		FtpConnector ftp = new FtpConnector() {
-			@Override
-			public void onSuccess() throws IOException, FtpException {
-				if (changeWorkingDirectory(remotePath)) {
-					this.fillFileList(remotePath, start, end, fileList);
-				} else {
-					System.out.println("Could not change directory!");
-				}
-			}
-		};
-
-		ftp.connect(server, port, user, passwd);
-		return fileList;
-	}
-	
-	public static boolean deleteFile(String remotePath,
-			String server, Integer port, String user, String passwd)
+	public static boolean deleteFile(SeresInfo info, String fileName)
 					throws SftpException, JSchException {
-		Session session = connect(server, port, user, passwd);
+		Session session = connect(info);
 		ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
 		channel.connect();
-		channel.rm(remotePath);
+//		channel.rm(info.getSeresPath().getPath() + "/" + fileName);
+		channel.rm(fileName);
 		channel.exit();
 		channel.disconnect();
 		session.disconnect();
 		return true;
+	}
+	
+	public static void main(String[] args) throws JSchException, SftpException {
+		String a = "/recepcion/orders_d96a";
+		SeresPath path = SeresPath.safeValueOf(a);
+		System.out.println(path.getPath());
+		
+//		SeresInfo info = new SeresInfo()
+//				.setServer("webconnect.seresnet.com")
+//				.setPort(22)
+//				.setUser("ftp1251")
+//				.setPassword("x1xx0wub")
+//				.setSeresPath(SeresPath.RECEPCION_ORDERS_D96A);
+//		
+//		System.out.println("***** START");		List<LsEntry> list = retrieveDirectoryList(info);
+//		Date start = AonDateUtils.getDate(2023, 0, 1);
+//		Date end = AonDateUtils.getDate(2023, 0, 15);
+//		System.out.println(start);
+//		list.stream()
+//		.filter(f -> !f.getFilename().equals(".") && !f.getFilename().equals("..") 
+//			&& start.before(new Date(f.getAttrs().getMTime() * 1000L))
+//			&& end.after(new Date(f.getAttrs().getMTime() * 1000L)))
+//		.forEach(r ->  {
+//			System.out.println( r.getFilename());
+//			System.out.println(r.getAttrs().getMtimeString());
+//		});
+//		System.out.println("***** END");
 	}
 	
 	
