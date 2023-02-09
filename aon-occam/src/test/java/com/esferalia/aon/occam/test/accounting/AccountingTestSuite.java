e2177a7b333414e172347b3c52cabce6342f79cf
EukeCastellano <ecastellano@aonsolutions.es>
2023-02-09 10:42:42 +0100

    Accounting occam tests.

--- aon-occam/src/test/java/com/esferalia/aon/occam/test/accounting/AccountingTestSuite.java
+++ aon-occam/src/test/java/com/esferalia/aon/occam/test/accounting/AccountingTestSuite.java
@@ -11,7 +11,8 @@ import com.esferalia.aon.occam.test.accounting.period.AccountPeriodTestSuite;
 @RunWith(Suite.class)
 @SuiteClasses({
 	AccountPeriodTestSuite.class,
-	AccountEntryTestSuite.class
+	AccountEntryTestSuite.class,
+	// BalanceTestSuite.class
 })
 public class AccountingTestSuite {
 
