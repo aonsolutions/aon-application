da69a4baa000f2b8bd3ca9cb60d0318e9cd04a6f
Raúl Trepiana <rtrepiana@aonsolutions.es>
2023-02-21 13:12:46 +0100

    Redo Fix/Update Embargar for twor or more embargos.

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,10 +19,9 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import net.aonsolutions.db.up2date.tgss.BasesMin2023Guess;
-import net.aonsolutions.db.up2date.tgss.IPREM2023Update;
+import net.aonsolutions.db.up2date.payroll.Embargar4Many;
+import net.aonsolutions.db.up2date.payroll.EmbargoDescriptionUpdate;
 import net.aonsolutions.db.up2date.tgss.MEITrainingRemoveUndo;
-import net.aonsolutions.db.up2date.tgss.SMI2023Update;
 import net.aonsolutions.db.up2date.tgss.SiltraRattachOldDelete;
 
 public class Up2Date {
@@ -314,12 +313,13 @@ public class Up2Date {
 	    	//Embargar4Many.BIEMBARGAR4MANY,
 		//FinanceTrackingAmountFix.FINANCE_TRACKING_AMOUNT_FIX,
 	    	//OcupationITIMSFix.OCUPATIONITIMSFIX,
-	    	//Embargar4Many.BIEMBARGAR4MANY,
+	    	Embargar4Many.BIEMBARGAR4MANY,
 	    	//BasesMin2023Update.BASESMIN2023UPDATE,
 	    	//AlterAlcatrazFinanceTracking.ALTER_ALCATRAZ_FINANCE_TRACKING,
 	    	//SMI2023Update.SMI20223UPDATE,
 	    	//IPREM2023Update.IPREM2022UPDATE,
 	    	//BasesMin2023Guess.BASESMIN2023GUESS,
+	    	EmbargoDescriptionUpdate.EMBARGODESCRIPTIONUPDATE,
 	    	
 	    	
 	    	//AgreementEmptyTrash.AGREEMENTEMPTYTRASH,
