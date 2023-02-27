78a64715ce4133f9971a039c599f5a32f70c0a10
ikgonzalez <igonzalez@aonsolutions.es>
2023-02-27 17:59:32 +0100

    Agreement payment start date update (commented)

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,6 +19,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import net.aonsolutions.db.up2date.payroll.AgreementPaymentStartDateUpdate;
 import net.aonsolutions.db.up2date.payroll.Embargar4Many;
 import net.aonsolutions.db.up2date.payroll.EmbargoDescriptionUpdate;
 import net.aonsolutions.db.up2date.tgss.MEITrainingRemoveUndo;
@@ -324,7 +325,9 @@ public class Up2Date {
 	    	
 	    	//AgreementEmptyTrash.AGREEMENTEMPTYTRASH,
     		//Important, not remove
-    		SiltraRattachOldDelete.SILTRARATTACHOLDDELETE
+    		SiltraRattachOldDelete.SILTRARATTACHOLDDELETE,
+    		
+//    		AgreementPaymentStartDateUpdate.AGREEMENT_PAYMENT_START_DATE_UPDATE
     		
     };
 
