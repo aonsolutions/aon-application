c07cf785a1ab4846bece9942b405370add17c4be
Sergio Valdepeñas <svaldepenas@MacBook-Pro-Sergio.local>
2023-03-01 19:00:48 +0100

    ComunicaActivityCccTest ignore to let machine compile update host

--- aon-htmlunit/src/test/java/com/esferalia/aon/htmlunit/payroll/ComunicaActivityCccTest.java
+++ aon-htmlunit/src/test/java/com/esferalia/aon/htmlunit/payroll/ComunicaActivityCccTest.java
@@ -17,6 +17,7 @@ import java.util.logging.Level;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
@@ -34,6 +35,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.javascript.JavaScriptErrorListener;
 import com.gargoylesoftware.htmlunit.util.WebConnectionWrapper;
 
+@Ignore
 public class ComunicaActivityCccTest {
 
 	public static final String INTEGRATION_TGSS_URL = "integration.test.tgss.payroll.url";
