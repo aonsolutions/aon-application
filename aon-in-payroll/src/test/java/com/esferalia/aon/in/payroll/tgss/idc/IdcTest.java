268c132e85a19b3dd5481e20aeff1a0608bcbb02
Raúl Trepiana <rtrepiana@aonsolutions.es>
2023-03-09 20:25:40 +0100

    Fix '16 BON.P.F.EMPL.CUANTIA  62,50 03 CONT.COMUN-C.EMPR 1625 DESEM.12/18 MES.M 0206 RDL 8/2019'

--- aon-in-payroll/src/test/java/com/esferalia/aon/in/payroll/tgss/idc/IdcTest.java
+++ aon-in-payroll/src/test/java/com/esferalia/aon/in/payroll/tgss/idc/IdcTest.java
@@ -3745,6 +3745,67 @@ public class IdcTest extends AbstractSQLTestCase {
 		}
 	}
 
+	@Test
+	public void testIdcplcccTrabajadoresTramosXXIII()
+			throws com.esferalia.aon.in.payroll.pdf.UnknownPDFException, IOException, JAXBException {
+		try (InputStream is = IdcTest.class.getResourceAsStream("idcplcccXXIII.pdf")) {
+			TrabajadoresTramos trabajadoresTramos = Idcplccc.geTrabajadoresTramos(is,
+				new TrabajadoresTramosCallback() {
+			    		@Override
+			    		public boolean isPartTimeEmployee(String ssNum, String ccc, Date start, Date end) {
+			    		    return true;
+			    		}
+				});
+	
+			marshal(trabajadoresTramos, System.out);
+	
+			Liquidacion liquidacion = trabajadoresTramos.getLiquidacion();
+	
+			assertEquals("0111", liquidacion.getCcc().getRegimen());
+			assertEquals("41", liquidacion.getCcc().getProvincia());
+			assertEquals("111822622", liquidacion.getCcc().getNumero());
+	
+			assertEquals("02", liquidacion.getPeriodoDesde().getMes());
+			assertEquals("2023", liquidacion.getPeriodoDesde().getAnho());
+			assertEquals("02", liquidacion.getPeriodoHasta().getMes());
+			assertEquals("2023", liquidacion.getPeriodoHasta().getAnho());
+	
+			assertEquals(1, liquidacion.getLiquidacionMes().size());
+	
+			LiquidacionMes liquidacionesMes = liquidacion.getLiquidacionMes().get(0);
+			assertEquals("02", liquidacionesMes.getMesLiquidativo().getMes());
+			assertEquals("2023", liquidacionesMes.getMesLiquidativo().getAnho());
+	
+			Trabajadores trabajadores = liquidacionesMes.getTrabajadores();
+			assertEquals(1, trabajadores.getTrabajador().size());
+	
+			for (Trabajador trabajador : trabajadores.getTrabajador()) {
+			    	assertEquals("411003115722", trabajador.getNaf());
+				assertEquals(2, trabajador.getTramos().getTramo().size());
+	
+				Tramo tramo = trabajador.getTramos().getTramo().get(0);
+				assertEquals("08", tramo.getInformacionAfiliacion().getGrupoCotizacion());
+				assertEquals("01", tramo.getFechaDesde().getDia());
+				assertEquals("02", tramo.getFechaDesde().getMes());
+				assertEquals("2023", tramo.getFechaDesde().getAnho());
+				assertEquals("19", tramo.getFechaHasta().getDia());
+				assertEquals("02", tramo.getFechaHasta().getMes());
+				assertEquals("2023", tramo.getFechaHasta().getAnho());
+				assertTramoTiempoParcial(tramo);	
+
+				tramo = trabajador.getTramos().getTramo().get(1);
+				assertEquals("08", tramo.getInformacionAfiliacion().getGrupoCotizacion());
+				assertEquals("20", tramo.getFechaDesde().getDia());
+				assertEquals("02", tramo.getFechaDesde().getMes());
+				assertEquals("2023", tramo.getFechaDesde().getAnho());
+				assertEquals("28", tramo.getFechaHasta().getDia());
+				assertEquals("02", tramo.getFechaHasta().getMes());
+				assertEquals("2023", tramo.getFechaHasta().getAnho());
+				assertTramoTiempoParcial(tramo);	
+			}
+	
+		}
+	}
 
 	@Test
 	public void testIdcSyncI() throws IOException, UnknownPDFException {
