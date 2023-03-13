ff5b271894f526c079b92527b20f7da8d0ed8660
EukeCastellano <ecastellano@aonsolutions.es>
2023-03-13 12:56:32 +0100

    Desglose de cuentas en Balances. Saldos cuenta 5525 y 551 [SOL #30110]

--- aon-occam/src/main/java/com/esferalia/aon/occam/server/accounting/BalanceScript.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/server/accounting/BalanceScript.java
@@ -1,11 +1,14 @@
 package com.esferalia.aon.occam.server.accounting;
 
+import java.io.Serializable;
 import java.util.List;
 
 import com.esferalia.aon.occam.api.model.accounting.IAccMiningKeyAccept;
 
-public abstract class BalanceScript {
+public abstract class BalanceScript implements Serializable {
 
+	private static final long serialVersionUID = 6703644150354316745L;
+	
 	public abstract  IAccMiningKeyAccept getAccepter();
 	public abstract List<? extends IBalanceKey> getKeyList();
 	
