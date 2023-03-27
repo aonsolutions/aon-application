17bcc433752e09a52d902228ed932ab6e70d6616
ikgonzalez <igonzalez@aonsolutions.es>
2023-03-27 13:20:06 +0200

    dbupdate checks

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/domain/DomainAonCustomer.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/domain/DomainAonCustomer.java
@@ -1,6 +1,10 @@
 package net.aonsolutions.db.up2date.domain;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
@@ -28,13 +32,47 @@ public class DomainAonCustomer implements Update {
 
 		dslContext = DSL.using(connection, SQLDialect.MYSQL, settings);
 		
+		boolean columnExists = false;
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		
 		try {
-			dslContext
-			.execute("ALTER TABLE `domain` ADD `aonCustomer` int DEFAULT NULL COMMENT 'Referencia al customer en Aon'");
-			System.out.println("\tDomainAonCustomer. aonCustomer ADDED!");
-		} catch ( Exception e ) {
-			System.out.println("\tDomainAonCustomer. aonCustomer NOT ADDED!");
-			e.printStackTrace();
+			stmt = connection.createStatement();
+			rs = stmt.executeQuery("select * from domain limit 1");
+			ResultSetMetaData rsmd = rs.getMetaData();
+			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+				String name = rsmd.getColumnName(i);
+				if ("aonCustomer".equals(name)) {
+					columnExists = true;
+				}
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			if (stmt != null)
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+				}
+			;
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			;
+		}
+		
+		if (!columnExists) {			
+			try {
+				dslContext
+				.execute("ALTER TABLE `domain` ADD `aonCustomer` int DEFAULT NULL COMMENT 'Referencia al customer en Aon'");
+				System.out.println("\tDomainAonCustomer. aonCustomer ADDED!");
+			} catch ( Exception e ) {
+				System.out.println("\tDomainAonCustomer. aonCustomer NOT ADDED!");
+				e.printStackTrace();
+			}
 		}
 	}
 
