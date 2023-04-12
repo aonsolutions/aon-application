f0d9314f05ac0b7b7418d5af58dd4e1859587be1
EukeCastellano <ecastellano@aonsolutions.es>
2023-04-12 10:19:21 +0200

    Mod 130. Avoid NullPointer.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod130/Model130Table.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/mod130/Model130Table.java
@@ -198,7 +198,7 @@ class Model130Table extends SimpleLayoutPanel implements HasSelectionHandlers<Mo
 				.addCell( sust , AON.CSS.aonTextCenter())
 				.addCell( new InlineLabel(mod130.getDocument()))
 				.addCell( new InlineLabel(mod130.getFullName()))
-				.addCell( new InlineLabel(AON.FMT.format(mod130.getDeclarationResult())), AON.CSS.aonTextRight())
+				.addCell( new InlineLabel(mod130.getDeclarationResult()==null?"":AON.FMT.format(mod130.getDeclarationResult())), AON.CSS.aonTextRight())
 				.addCell( new InlineLabel(mod130.getDeclarationResultType() == null ? "" : mod130.getDeclarationResultType().getDescription()))
 				.addCell( new InlineLabel(
 						(mod130.getFinance() != null && mod130.getFinance().getFinanceStatus() != null)
