45d3473453f70fb987c5a3061c1d18e16c246a9a
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-04-18 08:40:07 +0200

    re #9787. First version guard control

--- aon-solutions2/src/app/modules/auth/auth.component.ts
+++ aon-solutions2/src/app/modules/auth/auth.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+
 })
+
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  title :string      = "Inicia Sesión";
+  subtitle :string   ="Entra en tu cuenta de AON";
+  login :string      ="INICIAR SESIÓN";
+  orLogin :string    = "O INICIA SESIÓN";
+  noPassword :string ="SIN CONTRASEÑA";
+  user :string       ="";
+  password :string;
+  hidePassword :boolean = true;
+
+  constructor() {
+    this.user     = "";
+    this.password = "";
+  }
 
   ngOnInit(): void {
   }
 
+  clean(){
+    this.user ="";
+    console.log('user');
+  }
+
+  getUser(){
+    console.log('User: ', this.user);
+    console.log('password: ', this.password);
+  }
 }
