913884b1a729a29b9af9e2ef4168c62d1e8154a4
ikgonzalez <igonzalez@aonsolutions.es>
2023-04-24 18:17:04 +0200

    Occam changes for domain link

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/DomainCompany.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/DomainCompany.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class DomainCompany implements Serializable {
 	
 	private static final long serialVersionUID = 6809124449291099756L;
-	
+	private String schema;
 	private Domain domain;
 	private Company company;
 	
@@ -21,6 +21,13 @@ public class DomainCompany implements Serializable {
 	}
 	public DomainCompany setCompany(Company company) {
 		this.company = company;
+		return this;
+	}
+	public String getSchema() {
+		return schema;
+	}
+	public DomainCompany setSchema(String schema) {
+		this.schema = schema;
 		return this;
 	}
 
