6f07182f9eea79c23f86913c1fd6fdaaa964586c
anderibz <aibanez@aonsolutions.es>
2023-04-26 22:36:02 +0200

    Finance paymethod json changes.

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/json/invoice/FinanceJSON.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/json/invoice/FinanceJSON.java
@@ -38,7 +38,8 @@ public class FinanceJSON {
 				.setId(JsonUtils.getInteger(json, IJsonNames.ID))
 				.setDomain(JsonUtils.getInteger(json, IJsonNames.DOMAIN))
 				.setPayMethod(paymethod)
-				.setPayMethodType(PayMethodType.safeValueOf(json.optString(IJsonNames.PAYMETHOD)))
+				.setPayMethodName(JsonUtils.getString(json, IJsonNames.PAYMETHOD_NAME))
+				.setPayMethodType(PayMethodType.safeValueOf(JsonUtils.getString(json, IJsonNames.PAYMETHOD_TYPE)))
 				.setBankAccount(new BankAccount(json.optString(IJsonNames.BANK_ACCOUNT)))
 				.setAmount(JsonUtils.getdouble(json, IJsonNames.AMOUNT))
 				.setDueDate(date);
@@ -57,6 +58,8 @@ public class FinanceJSON {
 			.put(IJsonNames.DOMAIN, finance.getDomain())
 			.put(IJsonNames.DUE_DATE, AonDateUtils.format(finance.getDueDate(), "yyyy-MM-dd"))
 			.put(IJsonNames.PAYMETHOD, finance.getPayMethod())
+			.put(IJsonNames.PAYMETHOD_NAME, finance.getPayMethodName())
+			.put(IJsonNames.PAYMETHOD_TYPE, finance.getPayMethodType().name())
 			.put(IJsonNames.BANK_ACCOUNT, finance.getBankAccount() != null
 				? finance.getBankAccount().getIban() : null)
 			.put(IJsonNames.AMOUNT, finance.getAmount());
