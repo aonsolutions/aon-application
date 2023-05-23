18ebed06f08c765ea0f96446d5ae051084ca937f
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2023-05-23 12:31:00 +0200

    re #15424. Update implementation of dropdownMenu component.

--- aon-solutions2/src/app/modules/home/components/home/home.component.ts
+++ aon-solutions2/src/app/modules/home/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DropdownMenuComponent } from 'src/app/shared/components/dropdown-menu/dropdown-menu.component';
-import { MenuItem } from 'src/app/shared/components/dropdown-menu/dropdown-menu.component';
+import { MenuItem } from 'src/app/core/models/interface/menu-item';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,9 +14,18 @@ export class HomeComponent implements OnInit {
   myValue: number = 0;
 
   menuItem: MenuItem [] = [
-    {root:true, text:'first', children: [{root:false, text:'first-a', routerlink:'/inbox'},{root:false, text:'first-b', value:1234}]},
-    {root:true, text:'third', value:123},
-    {root:true, text:'second', children: [{root:false, text:'second-a'},{root:false, text:'second-b'}]},
+    {root:true, text:'first', children: [
+      {root:false, text:'first-a', routerlink:'/inbox'},
+      {root:false, text:'first-b', click:() => this.setValue('1234')}
+    ]},
+    {root:true, text:'third', click:() => this.log('123')},
+    {root:true, text:'second', children: [
+      {root:false, text:'second-a'},
+      {root:false, text:'second-b', children: [
+        {root:false, text:'second-a-x'},
+        {root:false, text:'second-a-z'},
+      ]}
+    ]},
   ]
 
   constructor() {
@@ -25,8 +34,12 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  log($event:any){
-    console.log('asd');
-    console.log($event);
+  log(val:string){
+    console.log(val);
   }
+
+  setValue(val:any){
+    this.myValue = val;
+  }
+  
 }
--- aon-solutions2/src/app/shared/components/dropdown-menu/dropdown-menu.component.ts
+++ aon-solutions2/src/app/shared/components/dropdown-menu/dropdown-menu.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
-
-export interface MenuItem {
-  root: boolean;
-  text: string;
-  icon?: string;
-  colorIcon?: string;
-  routerlink?: string;
-  value?: number;
-  children?: MenuItem[];
-}
-
+import { MenuItem } from 'src/app/core/models/interface/menu-item';
 @Component({
   selector: 'app-dropdown-menu',
   templateUrl: './dropdown-menu.component.html',
@@ -19,22 +9,21 @@ export interface MenuItem {
 export class DropdownMenuComponent implements OnInit {
 
   @Input() submenu: MenuItem = {root:true, text:''}
-  @Output() getValue = new EventEmitter<number>();
   @Input() menuItem: MenuItem [] = [{root:false,text:''}];
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger | undefined;
 
-  someMethod() {
+  openDropdownMenu() {
     this.trigger?.openMenu();
+  }
+
+  closeDropdownMenu(){
+    this.trigger?.closeMenu();
   }
 
   constructor() {
   }
 
   ngOnInit(): void {
-  }
-
-  sendValue(value: number) {
-    this.getValue.emit(value);
   }
 
 }
