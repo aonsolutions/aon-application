86f2ed883d618393bfa5b6de7762d0615af536c4
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2023-05-31 18:01:28 +0200

    CustomerFee add 'no segment' option

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/FeeDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/FeeDAO.java
@@ -116,7 +116,10 @@ public class FeeDAO {
 				.leftOuterJoin(SELLER_ALIAS).on(SELLER.REGISTRY.eq(SELLER_ALIAS.ID))
 				.leftOuterJoin(INVOICING_GROUP).on(INVOICING_GROUP.ID.eq(CUSTOMER_FEE.INVOICING_GROUP));
 		if (customerFeeParams != null && customerFeeParams.getSegment() != null) {
-			fromCustomerRecords = fromCustomerRecords 	
+			if(customerFeeParams.getSegment() == -1)
+				fromCustomerRecords = fromCustomerRecords 	
+				.leftJoin(RSEGMENT).on(RSEGMENT.REGISTRY.eq(CUSTOMER.REGISTRY));
+			else fromCustomerRecords = fromCustomerRecords 	
 				.join(RSEGMENT).on(RSEGMENT.REGISTRY.eq(CUSTOMER.REGISTRY));
 		}
 				
@@ -163,7 +166,7 @@ public class FeeDAO {
 		if(null != customerFeeParams.getCustomerStatus())
 			condition = condition.and(CUSTOMER.STATUS.eq(customerFeeParams.getCustomerStatus()));
 		
-		if(null != customerFeeParams.getSegment()) 
+		if(null != customerFeeParams.getSegment() && customerFeeParams.getSegment() != -1) 
 			condition = condition.and(RSEGMENT.SEGMENT.eq(customerFeeParams.getSegment()));
 		
 		if(null != customerFeeParams.getStartDate()){
@@ -711,7 +714,10 @@ public class FeeDAO {
 				.leftOuterJoin(SELLER_ALIAS).on(SELLER.REGISTRY.eq(SELLER_ALIAS.ID))
 				.leftOuterJoin(INVOICING_GROUP).on(INVOICING_GROUP.ID.eq(CUSTOMER_FEE.INVOICING_GROUP));
 		if (customerFeeParams != null && customerFeeParams.getSegment() != null) {
-			fromCustomerRecords = fromCustomerRecords 	
+			if(customerFeeParams.getSegment() == -1)
+				fromCustomerRecords = fromCustomerRecords 	
+				.leftJoin(RSEGMENT).on(RSEGMENT.REGISTRY.eq(CUSTOMER.REGISTRY));
+			else fromCustomerRecords = fromCustomerRecords 	
 				.join(RSEGMENT).on(RSEGMENT.REGISTRY.eq(CUSTOMER.REGISTRY));
 		}
 		Result<Record1<Integer>> customerFeeRecords = fromCustomerRecords	
@@ -770,7 +776,10 @@ public class FeeDAO {
 				.leftOuterJoin(SELLER_ALIAS).on(SELLER.REGISTRY.eq(SELLER_ALIAS.ID))
 				.leftOuterJoin(INVOICING_GROUP).on(INVOICING_GROUP.ID.eq(CUSTOMER_FEE.INVOICING_GROUP));
 		if (customerFeeParams != null && customerFeeParams.getSegment() != null) {
-			fromCustomerRecords = fromCustomerRecords 	
+			if(customerFeeParams.getSegment() == -1)
+				fromCustomerRecords = fromCustomerRecords 	
+				.leftJoin(RSEGMENT).on(RSEGMENT.REGISTRY.eq(CUSTOMER.REGISTRY));
+			else fromCustomerRecords = fromCustomerRecords 	
 				.join(RSEGMENT).on(RSEGMENT.REGISTRY.eq(CUSTOMER.REGISTRY));
 		}
 		Result<Record1<Integer>> customerRecords = fromCustomerRecords 
