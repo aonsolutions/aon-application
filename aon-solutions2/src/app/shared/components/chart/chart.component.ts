536c90b356b25ecb12e5abac540bd5b6b1774742
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-06-20 13:46:43 +0200

    re #15713. Initial chart component

--- aon-solutions2/src/app/shared/components/chart/chart.component.ts
+++ aon-solutions2/src/app/shared/components/chart/chart.component.ts
@@ -9,16 +9,15 @@ import { MultiDataSet, Label, Color } from 'ng2-charts';
 })
 export class ChartComponent implements OnInit {
 
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartLabels: Label[] = ['Lorem', 'Ipsum', 'Dolor'];
   public doughnutChartData: MultiDataSet = [
     [350, 450, 100],
     [50, 150, 120],
     [250, 130, 70],
   ];
   public doughnutChartType: ChartType = 'doughnut';
-  public colors:Color[]=[
-    {
-      backgroundColor:[
+  public colors: Color[] = [{
+      backgroundColor: [
         '#6CF58E','#FF624E','#FA64B1'
       ]
     }
