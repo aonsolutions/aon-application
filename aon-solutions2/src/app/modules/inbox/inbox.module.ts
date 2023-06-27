2a5c0692a0b244d063fb63c32759b03feb067ea5
Rocio <rocioleiva@aonsolutions.es>
2023-06-27 14:01:37 +0200

    re#16479 added chart component at home

--- aon-solutions2/src/app/modules/inbox/inbox.module.ts
+++ aon-solutions2/src/app/modules/inbox/inbox.module.ts
@@ -3,19 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { InboxComponent } from './components/inbox/inbox.component';
-import { ChartComponent } from 'src/app/shared/components/chart/chart.component';
-import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [
     InboxComponent,
-    ChartComponent
   ],
   imports: [
     CommonModule,
     InboxRoutingModule,
-    ChartsModule
   ]
 })
 export class InboxModule { }
