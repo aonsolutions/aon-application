1fd0be7f2a44318c342210a5885e55c098ff283a
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-07-05 14:44:56 +0200

    re #16213 improvement grid in documentation

--- aon-solutions2/src/app/modules/documentation/components/files/files.component.ts
+++ aon-solutions2/src/app/modules/documentation/components/files/files.component.ts
@@ -4,6 +4,7 @@ import { Document } from 'src/app/core/models/class/document';
 import { DocumentService } from 'src/app/core/services/document.service';
 import { Folder } from 'src/app/core/models/class/folder';
 import { FolderService } from 'src/app/core/services/folder.service';
+import { MenuButton } from 'src/app/core/models/interface/menu-button';
 
 @Component({
   selector: 'app-files',
@@ -16,8 +17,17 @@ export class FilesComponent implements OnInit {
 
   documentationListFolders: Folder[] = [];
   documentsList: Document[] = [];
+  fileOptions: MenuButton[] = [
+    {routerlink: '#', shape: 'eye', text: 'Vista previa', class: '', selected: 'false'},
+    {routerlink: '#', shape: 'edit', text: 'Cambiar nombre', class: '', selected: 'false'},
+    {routerlink: '#', shape: 'bookmark', text: 'Etiquetar como', class: '', selected: 'false'},
+    {routerlink: '#', shape: 'cloud_download', text: 'Descargar', class: '', selected: 'false'},
+    {routerlink: '#', shape: 'delete', text: 'Eliminar', class: '', selected: 'false'}
+  ];
+
 
   constructor(folderService: FolderService, documentService: DocumentService) {
+
 
     documentService.getDocumentList().then(documentsList => {
       this.documentsList = documentsList;
