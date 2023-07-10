4927f460fa07f82492e13c853a770546b6c447a4
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2023-07-10 12:36:53 +0200

    Create module taxPanel.

---
+++ aon-solutions2/src/app/modules/tax-panel/tax-panel.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaxPanelRoutingModule } from './tax-panel-routing.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    TaxPanelRoutingModule
+  ]
+})
+export class TaxPanelModule { }
