46dbbea1f1c7cc30b4d091a8bca012d72a046537
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-07-17 13:04:08 +0200

    re#19084 Add ngxtransalte to dependencies project

--- aon-solutions2/src/app/app.component.ts
+++ aon-solutions2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aon-solutions2';
+  selectedLanguage = 'es';
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.setDefaultLang(this.selectedLanguage);
+    this.translateService.use(this.selectedLanguage);
+  }
+
+  selectLanguage(language: string) {
+    this.translateService.use(language);
+  }
 }
