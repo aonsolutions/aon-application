d35cab7c20e9d37c478b72278e89efb1559c0c01
Raúl Trepiana <rtrepiana@aonsolutions.es>
2023-07-21 14:51:12 +0200

    Fix BUG with day date & irpf II .

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/GenericContractSalaryCalculator.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/GenericContractSalaryCalculator.java
@@ -1020,7 +1020,7 @@ public class GenericContractSalaryCalculator<T extends ISalary, C extends ISalar
 						    MONEY_IRPF_BASE } ) {
 
 						     irpfExpressionContext.getVariables(irpfVar.getName()).stream()
-						     .filter( v -> v.getPeriod().intersects(irpfPeriod))
+						    .filter( v -> v.getPeriod().getEnd().compareTo(irpfPeriod.getStart()) <= 0)
         					    .sorted( (v1, v2) -> v2.getPeriod().compareTo(v1.getPeriod()))
         					    .map( v -> new ITimedVariable<Object>() {
         						public Period getPeriod() {
