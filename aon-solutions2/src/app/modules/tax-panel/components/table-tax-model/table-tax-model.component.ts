ebd80f1e199a0a292e0561bb4ab6b09da99d4f94
Rocio <rocioleiva@aonsolutions.es>
2023-08-02 13:35:52 +0200

    re #20170 filter by trimester and all trimester

--- aon-solutions2/src/app/modules/tax-panel/components/table-tax-model/table-tax-model.component.ts
+++ aon-solutions2/src/app/modules/tax-panel/components/table-tax-model/table-tax-model.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { TaxModelService } from 'src/app/core/services/tax-model.service';
 import { ModalEditTaxModelComponent } from '../modal-edit-tax-model/modal-edit-tax-model.component';
 import { ModalPaymentComponent } from '../modal-payment/modal-payment.component';
@@ -11,7 +17,6 @@ import { FilterBuilder } from 'libraries/AonSDK/aon';
   styleUrls: ['./table-tax-model.component.scss'],
 })
 export class TableTaxModelComponent implements OnInit {
-
   @Input() trimester: number = 0;
   bodyTable: any = [];
   headerTable: any = {
@@ -38,63 +43,65 @@ export class TableTaxModelComponent implements OnInit {
 
   private updateTableData() {
     let filterBuilder = new FilterBuilder();
-    filterBuilder.addField('trimester', this.trimester);
-    this.taxModelService.getTaxModelList(filterBuilder.getFilter()).then((response) => {
-      console.log(response);
+    if (this.trimester !== 0) {
+      filterBuilder.addField('trimester', this.trimester);
+    }
+    this.taxModelService
+      .getTaxModelList(filterBuilder.getFilter())
+      .then((response) => {
+        // Tax
+        let tableRow: any = [];
+        response.forEach(function (tax, taxKey) {
+          let column: any = {};
 
-      // Tax
-      let tableRow: any = [];
-      response.forEach(function (tax, taxKey) {
-        let column: any = {};
-
-        // clone object
-        column = Object.assign({}, tax);
-        // Object Tax
-        // Predefinimos la key de la fila
-        column.key = taxKey;
-        // Cargamos datos en la tabla
-        column.name =
-          "<div class='orange'>MODELO " +
-          tax.Name +
-          '</div>' +
-          "<span class='griss'>" +
-          tax.TaxType +
-          '</span>';
-        column.result = tax.Result + ' &euro;';
-        column.status =
-          tax.Status === 'pendiente'
-            ? {
-                icon: [{ watch_later: 'orange' }],
-                text:
-                  "<span class='background-text-orange'>" +
-                  tax.Status +
-                  '</span>',
-              }
-            : "<span class='background-text-orange-light margin-left-2'>" +
-              tax.Status +
-              '</span>';
-        // Add date Actions ( buttons )
-        // La referencia tendra que ser por ID, ya que el texto puede cambiar dependiendo del idioma
-        switch (tax.Status) {
-          case 'pendiente':
-            column.actions = ['eye', 'done_all', 'edit'];
-            break;
-          case 'confirmado':
-            column.actions = ['eye'];
-            break;
-          case 'presentado':
-            column.actions = ['picture_as_pdf'];
-            break;
-          default:
-            column.actions = [];
-            break;
-        }
-        // Add object date table
-        tableRow.push(column);
+          // clone object
+          column = Object.assign({}, tax);
+          // Object Tax
+          // Predefinimos la key de la fila
+          column.key = taxKey;
+          // Cargamos datos en la tabla
+          column.name =
+            "<div class='orange'>MODELO " +
+            tax.Name +
+            '</div>' +
+            "<span class='griss'>" +
+            tax.TaxType +
+            '</span>';
+          column.result = tax.Result + ' &euro;';
+          column.status =
+            tax.Status === 'pendiente'
+              ? {
+                  icon: [{ watch_later: 'orange' }],
+                  text:
+                    "<span class='background-text-orange'>" +
+                    tax.Status +
+                    '</span>',
+                }
+              : "<span class='background-text-orange-light margin-left-2'>" +
+                tax.Status +
+                '</span>';
+          // Add date Actions ( buttons )
+          // La referencia tendra que ser por ID, ya que el texto puede cambiar dependiendo del idioma
+          switch (tax.Status) {
+            case 'pendiente':
+              column.actions = ['eye', 'done_all', 'edit'];
+              break;
+            case 'confirmado':
+              column.actions = ['eye'];
+              break;
+            case 'presentado':
+              column.actions = ['picture_as_pdf'];
+              break;
+            default:
+              column.actions = [];
+              break;
+          }
+          // Add object date table
+          tableRow.push(column);
+        });
+        // Tax date format for table
+        this.bodyTable = tableRow;
       });
-      // Tax date format for table
-      this.bodyTable = tableRow;
-    });
   }
 
   @ViewChild('modalEdit') modalComponentEdit: any = '';
