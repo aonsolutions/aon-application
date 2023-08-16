e9ec4159a8381b10e36db81a24a56686ca6a54d1
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-08-16 09:13:20 +0200

    re #16066 new employee service

---
+++ aon-solutions2/src/app/core/services/employee.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { EmployeeFactory, ICollection, IEmployee, IFilter } from 'libraries/AonSDK/aon';
+import { ErrorService } from './error.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private singleObjectCrud = new EmployeeFactory().createSingleObjectCrud();
+  private multipleObjectCrud = new EmployeeFactory().createMultipleObjectCrud();
+
+  constructor(private errorService: ErrorService) { }
+
+  async getEmployeeList(filter?: IFilter): Promise<ICollection<IEmployee>> {
+    return (await this.multipleObjectCrud.getCollection(filter)).result;
+  }
+
+  async updateEmployee(employee: IEmployee): Promise<IEmployee> {
+    return (await this.singleObjectCrud.updateElement(employee)).result;
+  }
+
+  async createEmployee(employee: IEmployee): Promise<IEmployee> {
+    return (await this.singleObjectCrud.createElement(employee)).result;
+  }
+
+  async deleteEmployee(pkey: any): Promise<boolean> {
+    return (await this.singleObjectCrud.deleteElement(pkey)).result;
+  }
+
+  async toggleEmployeeStatus(employeeId: string):Promise<IEmployee> {
+    const employeeToUpdate: IEmployee = (await this.singleObjectCrud.getElement(employeeId)).result;
+
+    if (employeeToUpdate) {
+      employeeToUpdate.Active = !employeeToUpdate.Active;
+
+      return this.updateEmployee(employeeToUpdate);
+    }
+
+    // Error al tratar de acceder al elemento
+    const errorEmployee = this.errorService.getError('0206');
+    throw errorEmployee;
+
+  }
+}
