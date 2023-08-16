3673c57600fe94b3b6068b46d99b659daabe01bf
jljimenez <joseluisjimenez@ayudatpymes.com>
2023-08-16 13:14:34 +0200

    re #20936. edit documentation

--- aon-solutions2/src/app/shared/components/button/button.component.ts
+++ aon-solutions2/src/app/shared/components/button/button.component.ts
@@ -6,20 +6,19 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-
-  @Input() width : string = '11.6rem';
-  @Input() height : string = '2.4rem';
-  @Input() color : string = '';
-  @Input() backgroundColor : string = '';
-  @Input() border : string = '';
-  @Input() borderRadius : string = '';
-  @Input() padding : string = '0.948rem';
+  @Input() width                : string = '11.6rem';
+  @Input() height               : string = '2.4rem';
+  @Input() color                : string = '';
+  @Input() backgroundColor      : string = '';
+  @Input() border               : string = '';
+  @Input() borderRadius         : string = '';
+  @Input() padding              : string = '0.948rem';
   @Input() hoverBackgroundColor : string = '';
-  @Input() hoverBorderColor : string = '';
-  @Input() hoverColor : string = '';
-  @Input() fontSize : string = '';
-  @Input() align : string = 'center';
-  @Input() class : string = '';
+  @Input() hoverBorderColor     : string = '';
+  @Input() hoverColor           : string = '';
+  @Input() fontSize             : string = '';
+  @Input() align                : string = 'center';
+  @Input() class                : string = '';
   @HostBinding('style.--widthHost') widthHost = '';
   @HostBinding('style.--heightHost') heightHost = '';
 
@@ -27,7 +26,7 @@ export class ButtonComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.widthHost = this.width;
+    this.widthHost  = this.width;
     this.heightHost = this.height;
   }
 
--- aon-solutions2/src/app/shared/components/dropdown-menu/dropdown-menu.component.ts
+++ aon-solutions2/src/app/shared/components/dropdown-menu/dropdown-menu.component.ts
@@ -2,14 +2,13 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { MenuItem } from 'src/app/core/models/interface/menu-item';
 @Component({
-  selector: 'app-dropdown-menu',
-  templateUrl: './dropdown-menu.component.html',
-  styleUrls: ['./dropdown-menu.component.scss']
+  selector    : 'app-dropdown-menu',
+  templateUrl : './dropdown-menu.component.html',
+  styleUrls   : ['./dropdown-menu.component.scss']
 })
 export class DropdownMenuComponent implements OnInit {
-
-  @Input() submenu: MenuItem = {root:true, text:''}
-  @Input() menuItem: MenuItem [] = [{root:false,text:''}];
+  @Input() submenu  : MenuItem    = {root:true, text:''}
+  @Input() menuItem : MenuItem [] = [{root:false,text:''}];
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger | undefined;
 
   openDropdownMenu() {
