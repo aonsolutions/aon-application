32178963e4d20fcc6c9312a6da5d8513e630c211
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-08-22 12:48:46 +0200

    re #16066 employee service and contract

---
+++ aon-solutions2/src/app/core/services/contract.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { ContractFactory, ICollection, IContract, IFilter } from 'libraries/AonSDK/aon';
+import { ErrorService } from './error.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContractService {
+
+  private singleObjectCrud = new ContractFactory().createSingleObjectCrud();
+  private multipleObjectCrud = new ContractFactory().createMultipleObjectCrud();
+
+  constructor(private errorService: ErrorService) { }
+
+  async getContractList(filter?: IFilter): Promise<ICollection<IContract>> {
+    return (await this.multipleObjectCrud.getCollection(filter)).result;
+  }
+
+  async getContract(pkey: any): Promise<IContract> {
+    return (await this.singleObjectCrud.getElement(pkey)).result;
+  }
+
+  async createContract(contract: IContract): Promise<IContract> {
+    return (await this.singleObjectCrud.createElement(contract)).result;
+  }
+
+  async updateContract(contract: IContract): Promise<IContract> {
+    return (await this.singleObjectCrud.updateElement(contract)).result;
+  }
+
+  async deleteContract(pkey: any): Promise<boolean> {
+    return (await this.singleObjectCrud.deleteElement(pkey)).result;
+  }
+
+  async extendContract(contractId: string, newEndDate: Date): Promise<IContract> {
+    const contract: IContract = (await this.singleObjectCrud.getElement(contractId)).result;
+
+    if (contract) {
+      contract.EndDate = newEndDate;
+      this.updateContract(contract);
+    }
+
+    // Error al tratar de acceder al elemento
+    const errorEmployee = this.errorService.getError('0206');
+    throw errorEmployee;
+  }
+
+  async changeContractType(contractId: string, newType: string): Promise<IContract> {
+    const contract: IContract = (await this.singleObjectCrud.getElement(contractId)).result;
+
+    if (contract) {
+      contract.Type = newType;
+      this.updateContract(contract);
+    }
+
+    // Error al tratar de acceder al elemento
+    const errorEmployee = this.errorService.getError('0206');
+    throw errorEmployee;
+  }
+
+}
