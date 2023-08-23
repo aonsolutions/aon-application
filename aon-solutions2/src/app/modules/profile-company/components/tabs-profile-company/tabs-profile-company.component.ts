ad5616168b842c5b6e9ea27f0014f20bee0ca016
Rocio <rocioleiva@aonsolutions.es>
2023-08-23 11:07:12 +0200

    re #19720. translate

--- aon-solutions2/src/app/modules/profile-company/components/tabs-profile-company/tabs-profile-company.component.ts
+++ aon-solutions2/src/app/modules/profile-company/components/tabs-profile-company/tabs-profile-company.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 
 export interface Tabs {
   name: string;
 }
+
 @Component({
   selector: 'app-tabs-profile-company',
   templateUrl: './tabs-profile-company.component.html',
   styleUrls: ['./tabs-profile-company.component.scss']
 })
-export class TabsProfileCompanyComponent implements OnInit {
-  tabIndex:number = 0
-  constructor() { }
 
-  tabs: Tabs[] = [
-    { name: 'Datos personales' },
-    { name: 'Datos empresa' },
-    { name: 'Datos registrales' },
-    { name: 'Certificados' }
-  ];
-  ngOnInit(): void {
+export class TabsProfileCompanyComponent implements OnInit {
+  tabIndex: number = 0;
+  tabs: Tabs[] = [];
+
+  constructor(
+    private translateService: TranslateService
+  ) {
+    this.translateService
+      .get([
+        'PROFILE.PERSONAL_INFORMATION',
+        'PROFILE.COMPANY_INFORMATION',
+        'PROFILE.REGISTRATION_INFORMATION',
+        'PROFILE.CERTIFICATES'
+      ])
+      .subscribe((result) => {
+        this.tabs = [
+          { name: result['PROFILE.PERSONAL_INFORMATION'] },
+          { name: result['PROFILE.COMPANY_INFORMATION'] },
+          { name: result['PROFILE.REGISTRATION_INFORMATION'] },
+          { name: result['PROFILE.CERTIFICATES'] },
+        ];
+      });
   }
 
+  ngOnInit(): void {
+  }
 }
