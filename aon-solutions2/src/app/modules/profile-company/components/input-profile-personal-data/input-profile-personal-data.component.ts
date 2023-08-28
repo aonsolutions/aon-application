dbb34b3eb64e8ca3bef9ce9b6d8c0a377818330c
Rocio <rocioleiva@aonsolutions.es>
2023-08-28 10:09:30 +0200

    re #21560. userService

--- aon-solutions2/src/app/modules/profile-company/components/input-profile-personal-data/input-profile-personal-data.component.ts
+++ aon-solutions2/src/app/modules/profile-company/components/input-profile-personal-data/input-profile-personal-data.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../core/services/user.service';
 
 @Component({
   selector: 'app-input-profile-personal-data',
   templateUrl: './input-profile-personal-data.component.html',
-  styleUrls: ['./input-profile-personal-data.component.scss']
+  styleUrls: ['./input-profile-personal-data.component.scss'],
 })
 export class InputProfilePersonalDataComponent implements OnInit {
+  users: any[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.userService.getUser('94385657M').then(user => {
+      this.users.push(user);
+      this.logUserNames();
+    });
   }
 
+  logUserNames(): void {
+    for (const user of this.users) {
+      console.log(user.Name);
+    }
+  }
 }
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { UserService } from '../../../../core/services/user.service';
+
+// @Component({
+//   selector: 'app-input-profile-personal-data',
+//   templateUrl: './input-profile-personal-data.component.html',
+//   styleUrls: ['./input-profile-personal-data.component.scss'],
+// })
+// export class InputProfilePersonalDataComponent implements OnInit {
+//   users: any[] = [];
+
+//   constructor(private userService: UserService) {}
+
+//   ngOnInit(): void {
+//     // let user = this.userService.getUser('94385657M');
+//     // console.log(user);
+
+//     this.userService.getUserList().then(userCollection => {
+//       this.users = userCollection.toArray();
+//       this.logUserNames();
+//     });
+//   }
+
+//   logUserNames(): void {
+//     for (const user of this.users) {
+//       console.log(user.Name);
+//     }
+//   }
+// }
