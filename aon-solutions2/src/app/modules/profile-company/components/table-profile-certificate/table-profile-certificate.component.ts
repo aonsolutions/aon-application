906295faaea30f99fe5e4b574a8e8bf066e4661d
Rocio <rocioleiva@aonsolutions.es>
2023-08-29 11:41:49 +0200

    merge aongate

--- aon-solutions2/src/app/modules/profile-company/components/table-profile-certificate/table-profile-certificate.component.ts
+++ aon-solutions2/src/app/modules/profile-company/components/table-profile-certificate/table-profile-certificate.component.ts
@@ -15,19 +15,19 @@ import { CertificateService } from '../../../../core/services/certificate.servic
   styleUrls: ['./table-profile-certificate.component.scss'],
 })
 export class TableProfileCertificateComponent implements OnInit {
+  displayedColumns: string[] = [
+    'name',
+    'representationType',
+    'expirationDate',
+    'alias',
+    'type',
+    'tgss',
+    'sepe',
+    'aeat',
+    'actions',
+  ];
   headerTable: any = {};
   bodyTable: any = [];
-  displayedColumns: string[] = [
-    'Holder',
-    'Representation',
-    'Expiration Date',
-    'Alias',
-    'Type',
-    'TGSS',
-    'SEPE',
-    'AEAT',
-    'Actions',
-  ];
 
 //type: class Certificate { constructor(name, representationType, expirationDate, alias, type, tgss, sepe, aeat) }
 
@@ -36,64 +36,52 @@ export class TableProfileCertificateComponent implements OnInit {
     ) {
       this.translateService
       .get([
-        'PROFILE.HOLDER',
-        'PROFILE.REPRESENTATION',
-        'PROFILE.EXPIRATION_DATE',
-        'PROFILE.ALIAS',
-        'PROFILE.TYPE',
-        'PROFILE.TGSS',
-        'PROFILE.SEPE',
-        'PROFILE.AEAT',
-        'PROFILE.ACTIONS'
+        'PROFILE.HOLDER', 'PROFILE.REPRESENTATION', 'PROFILE.EXPIRATION_DATE',
+        'PROFILE.ALIAS', 'PROFILE.TYPE', 'PROFILE.TGSS', 'PROFILE.SEPE',
+        'PROFILE.AEAT', 'PROFILE.ACTIONS'
       ])
       .subscribe((result) => {
         this.headerTable = {
-          holder: result['PROFILE.HOLDER'] ,
-          representation: result['PROFILE.REPRESENTATION'] ,
+          name: result['PROFILE.HOLDER'] ,
+          representationType: result['PROFILE.REPRESENTATION'] ,
           expirationDate: result['PROFILE.EXPIRATION_DATE'] ,
           alias: result['PROFILE.ALIAS'],
           type: result['PROFILE.TYPE'],
           tgss: result['PROFILE.TGSS'],
           sepe: result['PROFILE.SEPE'],
           aeat: result['PROFILE.AEAT'],
-          actions: result['PROFILE.TYPE']
+          actions: result['PROFILE.ACTIONS'],
         };
+
+        let tableRow: any = [];
+        let column: any = {};
+
+        certificateService.getCertificateList().then((response) => {
+          response.forEach(function (certificate, certificateKey) {
+            column = Object.assign({}, certificate);
+            column.key = certificateKey;
+            column.name = certificate.Name;
+            column.representationType = certificate.RepresentationType ;
+            column.expirationDate = certificate.ExpirationDate;
+            column.alias = certificate.Alias;
+            column.type = certificate.Type;
+            column.tgss = certificate.Tgss;
+            column.sepe = certificate.Sepe;
+            column.aeat = certificate.Aeat;
+            column.actions = ['verified_user', 'delete'];
+
+            tableRow.push(column);
+            console.log(column)
+          });
+
+          this.bodyTable = tableRow;
+        });
       });
-
-    let tableRow: any = [];
-    let column: any = {};
-
-    certificateService.getCertificateList().then((response) => {
-
-      response.forEach(function (certificate, certificateKey) {
-
-        column = Object.assign({}, certificate);
-
-        column.key = certificateKey;
-
-        column.name = certificate.Name;
-        column.representationType = certificate.RepresentationType;
-        column.expirationDate = certificate.ExpirationDate;
-        column.alias = certificate.Alias;
-        column.type = certificate.Type;
-        column.tgss = certificate.Tgss;
-        column.sepe = certificate.Sepe;
-        column.aeat = certificate.Aeat;
-        column.actions = ['verified_user', 'delete'];
-
-
-        tableRow.push(column);
-        console.log(certificateService)
-      });
-
-      this.bodyTable = tableRow;
-    });
   }
 
   ngOnInit(): void {}
 
   modalClick(object: any) {
-
     this.certificateService.getCertificate(object.key).then((response) => {
       console.log(response);
     });
