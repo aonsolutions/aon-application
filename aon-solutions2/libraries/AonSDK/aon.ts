e50e6409ae96c669ba0a41ce134591bcc273fb30
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-09-01 13:50:34 +0200

    re #21643. fix key registryEnterprise

--- aon-solutions2/libraries/AonSDK/aon.ts
+++ aon-solutions2/libraries/AonSDK/aon.ts
@@ -2015,7 +2015,7 @@ class RegistryEnterprise implements IRegistryEnterprise, IModel {
     this.notary = notary || '';
     this.protocol = protocol || '';
     this.inscription = inscription || '';
-    this.key = this.id;
+    this.key = this.idEnterprise;
   }
   public get Id(): string {
     return this.id;
@@ -2090,7 +2090,7 @@ class RegistryEnterprise implements IRegistryEnterprise, IModel {
   }
 
   getKey(): string {
-    return this.id;
+    return this.idEnterprise;
   }
 
   getFilterableFields(): Map<string, any> {
