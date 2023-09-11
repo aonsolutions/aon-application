a09f123677e9857d89211c57f7fdad8f997d817d
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2023-09-11 19:54:49 +0200

    Add debug methods

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/registry/BookingCustomer.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/registry/BookingCustomer.java
@@ -46,6 +46,7 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DeckPanel;
@@ -1416,8 +1417,16 @@ public class BookingCustomer extends HTMLPanel {
 		            		for(String error : errors)
 		            			errorMessage += error + "\n";
 		            		AonMessagePanel.showError(messagePanel, errorMessage);
-		            	} else
+		            	} else {
 		            		AonMessagePanel.showSuccess(messagePanel, "La sincronizaci\u00f3n del dominio " + domainCompany.getDomain().getDescription() + " se ha realizado correctamente");
+		            		Timer timer = new Timer() {
+			           		     @Override
+			           		     public void run() {
+			           		    	setBookingCustomer(customer, customerDomains);
+			           		     }
+			           		};
+			           		timer.schedule(2500);
+		            	}
 		            } else {
 		            	AonMessagePanel.showError(messagePanel, response.getText());
 		            }
