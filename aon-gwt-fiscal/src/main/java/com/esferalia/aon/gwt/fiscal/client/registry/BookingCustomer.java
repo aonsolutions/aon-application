706ac07daaae5f041867c31a987f971047de136e
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2023-09-15 15:43:50 +0200

    BookingCustomer change get conect@ users from localhost to aon.solutions

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/registry/BookingCustomer.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/registry/BookingCustomer.java
@@ -79,8 +79,8 @@ public class BookingCustomer extends HTMLPanel {
 			// Create a URL builder and add query parameters
 			UrlBuilder urlBuilder = new UrlBuilder();
 			urlBuilder.setProtocol(Window.Location.getProtocol()); // Use the current protocol
-			urlBuilder.setHost("localhost:8080");
-//			urlBuilder.setHost("aon.solutions"); 
+//			urlBuilder.setHost("localhost:8080");
+			urlBuilder.setHost("aon.solutions"); 
 			urlBuilder.setPath(baseUrl);
 			
 			urlBuilder.setParameter("customer", customer.getId().toString());
