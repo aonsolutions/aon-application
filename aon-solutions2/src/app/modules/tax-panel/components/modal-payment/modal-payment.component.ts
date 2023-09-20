a349dc59dba1b204453c59dd0aa809c6c0f2422c
Rocio <rocioleiva@aonsolutions.es>
2023-09-20 09:56:27 +0200

    re #22945 modal payment

--- aon-solutions2/src/app/modules/tax-panel/components/modal-payment/modal-payment.component.ts
+++ aon-solutions2/src/app/modules/tax-panel/components/modal-payment/modal-payment.component.ts
@@ -1,15 +1,16 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-modal-payment',
   templateUrl: './modal-payment.component.html',
   styleUrls: ['./modal-payment.component.scss'],
 })
 export class ModalPaymentComponent implements OnInit {
-  selectedOption: string = '';
+  selectedOption: string = '1';
   showText: boolean = false;
+  showCreateBank: boolean = false;
+  showConfirmation: boolean = false;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -17,11 +18,14 @@ export class ModalPaymentComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  toggleCreateBank() {
+    this.showCreateBank = !this.showCreateBank;
+  }
+
+  showConfirmationContent() {
+    this.showConfirmation = true;
+  }
 }
-
-
-
-
-
 
 
