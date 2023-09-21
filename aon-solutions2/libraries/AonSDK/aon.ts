951b602f1e440ae015958c214e6ab31b70ca4565
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2023-09-21 13:54:31 +0200

    re #23096. Fixed parse incoming taxModel yo SDK's taxModel

--- aon-solutions2/libraries/AonSDK/aon.ts
+++ aon-solutions2/libraries/AonSDK/aon.ts
@@ -4630,7 +4630,8 @@ class ApiTaxModel extends TaxModel implements IApiModel {
         tax.Result = data.result ? data.result : '';
         tax.Status = data.status ? data.status : '';
         tax.TaxType = '';
-        tax.Trimester = data.period ? data.period : '';
+        tax.Trimester = data.period ? +data.period.replace('T',""): 1;
+        console.log(data.period, tax.Trimester)
         tax.Year = data.year ? data.year : '';
         return tax;
     }
