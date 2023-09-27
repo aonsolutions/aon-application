c4592ba0728bcccded11390268ed15442a2ef402
anderibz <aibanez@aonsolutions.es>
2023-09-27 13:12:49 +0200

    Insert iae 474 - ARTES GRAFICAS.

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -20,10 +20,9 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import net.aonsolutions.db.up2date.config.InsertIAE1516;
+import net.aonsolutions.db.up2date.config.InsertIAE474;
 import net.aonsolutions.db.up2date.config.InsertIAE575576;
-import net.aonsolutions.db.up2date.payroll.NoteInsert;
 import net.aonsolutions.db.up2date.security.UdpateAonPacks;
-import net.aonsolutions.db.up2date.tgss.SuspendJobAndSalaryDaysInsert;
 import net.aonsolutions.db.up2date.warehouse.DeliveryPackagingCreation;
 import net.aonsolutions.db.up2date.warehouse.SalesInfoCreation;
 
@@ -394,7 +393,8 @@ public class Up2Date {
 	    DeliveryPackagingCreation.DELIVERY_PACKAGING_CREATION,
 	    SalesInfoCreation.SALES_INFO_CREATION,
 	    InsertIAE1516.INSERT_IAE_1516,
-	    UdpateAonPacks.UPDATE_AON_PACKS
+	    UdpateAonPacks.UPDATE_AON_PACKS,
+	    InsertIAE474.INSERT_IAE_474
 	};
 
     // ------------------------------------------------------------------------
