6348b015e6912aee8fa4650272d9d634e6633ef1
Maria <mwoodruff@aonsolutions.es>
2023-10-04 11:44:24 +0200

    Excel Question Button

---
+++ aon-gwt-marketing/src/main/java/com/esferalia/aon/gwt/marketing/server/AbsExcelAction.java
@@ -0,0 +1,246 @@
+package com.esferalia.aon.gwt.marketing.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.util.TempFile;
+import org.apache.poi.util.TempFileCreationStrategy;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+
+import com.esferalia.aon.watson.util.AonStringUtils;
+
+public abstract class AbsExcelAction  {
+	
+	protected static final String FONT_FAMILY = "DejaVu Sans Mono";
+	protected static final String DATE_PATTERN = "dd/MM/yyyy";
+	protected static final String DECIMAL_PATTERN = "#,##0.00";
+	protected static final String NUMBER_PATTERN = "#,###";
+	protected static final XSSFColor AON_BLUE = new XSSFColor(new java.awt.Color(0,114,207));
+	protected static final XSSFColor AON_LIGHT_GRAY = new XSSFColor(new java.awt.Color(240,240,240));
+	
+	protected SXSSFWorkbook workbook;
+	protected SXSSFSheet sheet;
+	protected Row row;
+	protected int rowCount;
+	protected int cellCount;
+	protected DataFormat dataFormat;
+	protected CellStyle dateStyle;
+	protected CellStyle smallDateStyle;
+	protected CellStyle decimalStyle;
+	protected CellStyle numberStyle;
+	protected CellStyle centerCellStyle;
+	protected XSSFCellStyle headerCellStyle;
+	protected Font boldFont;
+	protected Font defaulFont;	
+	protected Font smallFont;
+	protected Font smallBoldFont;
+	protected Font italicSmallFont;
+	
+	public void initialize(String name) {
+		initialize(name, true);
+	}
+    
+	public void initialize(String name, boolean printHeaders) {
+		workbook = new SXSSFWorkbook(1);
+		
+		
+	    sheet = (SXSSFSheet) workbook.createSheet(name);
+	    dataFormat = workbook.getCreationHelper().createDataFormat();
+	    rowCount = 0;
+	    cellCount = 0;
+	    dateStyle = workbook.createCellStyle();
+	    dateStyle.setDataFormat(dataFormat.getFormat(DATE_PATTERN));
+	    dateStyle.setAlignment( HorizontalAlignment.CENTER );
+	    
+	    numberStyle = workbook.createCellStyle();
+	    numberStyle.setDataFormat(dataFormat.getFormat(NUMBER_PATTERN));
+	    numberStyle.setAlignment( HorizontalAlignment.CENTER );
+	     
+	    decimalStyle = workbook.createCellStyle();
+	    decimalStyle.setDataFormat(dataFormat.getFormat(DECIMAL_PATTERN));
+	    decimalStyle.setAlignment( HorizontalAlignment.RIGHT );
+	    
+		centerCellStyle = workbook.createCellStyle();
+		centerCellStyle.setAlignment( HorizontalAlignment.CENTER );
+
+		defaulFont= workbook.createFont();
+		defaulFont.setFontHeightInPoints((short) 9);
+		
+		smallFont = workbook.createFont();
+		smallFont.setFontHeightInPoints((short) 8);
+
+		smallBoldFont = workbook.createFont();
+		smallBoldFont.setFontHeightInPoints((short) 8);
+		smallBoldFont.setBold(true);
+
+		italicSmallFont = workbook.createFont();
+		italicSmallFont.setFontHeightInPoints((short) 8);
+		italicSmallFont.setItalic(true);
+		
+		smallDateStyle = workbook.createCellStyle();
+	    smallDateStyle.setDataFormat(dataFormat.getFormat(DATE_PATTERN));
+	    smallDateStyle.setAlignment( HorizontalAlignment.CENTER );
+	    smallDateStyle.setFont( smallFont );
+
+	    boldFont= workbook.createFont();
+		boldFont.setFontHeightInPoints((short) 9);
+		boldFont.setBold(true);
+
+		Font headerFont= workbook.createFont();
+		headerFont.setBold(true);
+		headerFont.setColor( IndexedColors.WHITE.index );
+
+		headerCellStyle = (XSSFCellStyle) workbook.createCellStyle();
+		headerCellStyle.setAlignment( HorizontalAlignment.CENTER );
+		headerCellStyle.setVerticalAlignment( VerticalAlignment.CENTER);
+	    headerCellStyle.setBorderBottom(BorderStyle.MEDIUM);
+	    headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	    headerCellStyle.setFillForegroundColor(AON_BLUE);
+	    headerCellStyle.setFont(headerFont);
+	    if (printHeaders) {
+	    	headerRow();
+	    }
+	}
+	
+	protected Cell alignCenter(Cell cell) {
+		cell.setCellStyle( centerCellStyle );
+		return cell;
+	}
+	protected Cell addEmptyCell() {
+		return addCell("");
+	}
+	protected Cell addCell(String value) {
+		Cell cell = row.createCell(cellCount++);
+		cell.setCellValue(AonStringUtils.trimToEmpty( value ) );
+		cell.setCellType(CellType.STRING);
+		return cell;
+	}
+
+	protected Cell addCell(Enum<?> value) {
+		Cell cell = row.createCell(cellCount++);
+		if ( value != null ) {
+			cell.setCellValue(value.toString());
+		}
+		cell.setCellType(CellType.STRING);
+		return cell;
+	}
+
+	protected Cell addCell(Short value) {
+		Cell cell = row.createCell(cellCount++);
+		cell.setCellStyle(numberStyle);
+		if ( value != null ) {
+			cell.setCellValue(value);
+		}
+		cell.setCellType(CellType.NUMERIC);
+		return cell;
+	}
+	
+	protected Cell addCell(Integer value) {
+		Cell cell = row.createCell(cellCount++);
+		cell.setCellStyle(numberStyle);
+		if ( value != null ) {
+			cell.setCellValue(value);
+		}
+		cell.setCellType(CellType.NUMERIC);
+		return cell;
+	}
+
+	protected Cell addCell(Date value) {
+		Cell cell = row.createCell(cellCount++);
+		cell.setCellStyle(dateStyle);
+		if ( value != null ) {
+			cell.setCellValue(value);
+		}
+		return cell;
+	}
+
+	protected Cell addCell(Double number) {
+		Cell cell = row.createCell(cellCount++);
+		cell.setCellStyle(decimalStyle);
+		cell.setCellValue(number!=null?number:0.0);
+		cell.setCellType(CellType.NUMERIC);
+		return cell;
+	}
+
+	protected Cell addCell(boolean bool) {
+		Cell cell = row.createCell(cellCount++);
+		cell.setCellValue(bool?"SI":"NO");
+		cell.setCellType(CellType.STRING);
+		return cell;
+	}
+
+	public void finalize(OutputStream out) throws IOException {
+		workbook.write(out);
+		
+		// Note that SXSSF allocates temporary files that you 
+		// must always clean up explicitly, by calling the dispose method.
+		//
+		// http://poi.apache.org/spreadsheet/how-to.html#sxssf
+		//
+		workbook.dispose();
+	}
+
+	protected abstract void headerRow();
+	
+	private static class AONTempFileCreationStrategy implements TempFileCreationStrategy {
+
+        /** The directory where the temporary files will be created (<code>null</code> to use the default directory). */
+        private File dir;
+
+        @Override
+		public File createTempFile(String prefix, String suffix) throws IOException {
+            // Identify and create our temp dir, if needed
+        	
+            if (dir == null || !dir.canWrite()) {
+                dir = new File(System.getProperty("java.io.tmpdir"), "poifiles");
+                dir.mkdir();
+                if (System.getProperty("poi.keep.tmp.files") == null)
+                    dir.deleteOnExit();
+            }
+
+            // Generate a unique new filename 
+            File newFile = File.createTempFile(prefix, suffix, dir);
+
+            // Set the delete on exit flag, unless explicitly disabled
+            if (System.getProperty("poi.keep.tmp.files") == null)
+                newFile.deleteOnExit();
+
+            // All done
+            return newFile;
+		}
+        
+        @Override
+        public File createTempDirectory(String prefix) throws IOException {
+        	// TODO Auto-generated method stub
+            File dir = new File(System.getProperty("java.io.tmpdir"), prefix);
+            dir.mkdir();
+            if (System.getProperty("poi.keep.tmp.files") == null)
+                dir.deleteOnExit();
+            return dir;
+        }
+		
+	}
+	
+	static {
+		TempFile.setTempFileCreationStrategy(new AONTempFileCreationStrategy());
+	}
+
+
+	 
+}
---
+++ aon-gwt-marketing/src/main/java/com/esferalia/aon/gwt/marketing/server/QuestionReportExcelPrint.java
@@ -0,0 +1,153 @@
+package com.esferalia.aon.gwt.marketing.server;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Footer;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.PrintSetup;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.util.CellUtil;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+
+import com.esferalia.aon.gwt.marketing.client.MainEntryPoint;
+import com.esferalia.aon.gwt.marketing.client.marketing.MarketingModuleOptions;
+import com.esferalia.aon.gwt.marketing.client.marketing.QuestionModule;
+import com.esferalia.aon.occam.api.AON;
+import com.esferalia.aon.occam.api.model.Question;
+import com.esferalia.aon.occam.api.model.QuestionParams;
+import com.esferalia.aon.occam.api.model.type.MimeType;
+import com.esferalia.aon.watson.error.AonCoreException;
+import com.esferalia.gwt.marketing.shared.IRequestParamsNames;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "QuestionReport Excel Print", urlPatterns = { "/aon_gwt_marketing/roms/QuestionReportExcelPrint"})
+public class QuestionReportExcelPrint extends HttpServlet {
+	
+	private static final long serialVersionUID = 2828445311492662344L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try  {
+			ExcelAction action = new ExcelAction();
+			action.initialize("Preguntas");
+			
+			QuestionParams questionParams = new QuestionParams()
+					.setDomain(Integer.parseInt(req.getParameter(IRequestParamsNames.DOMAIN_ID)))
+					.setDomainName(req.getParameter(IRequestParamsNames.DOMAIN_NAME))
+					.setUser(req.getParameter(IRequestParamsNames.USER))
+					;
+					
+			List<Question> list = AON.getQuestionList(questionParams);
+			list.stream().forEach(action);
+
+			resp.setContentType(MimeType.MS_EXCEL.getName());
+			String balName = "Preguntas";
+			resp.setHeader("Content-disposition",
+					"attachment; filename=\"" + balName + "." + MimeType.MS_EXCEL_2007.getExtension() + "\";");
+			action.finalize(resp.getOutputStream());
+			
+			resp.flushBuffer();
+			list.stream().close();
+		} catch (Throwable e) {
+			throw new ServletException(e);
+		}
+	}
+	
+	private class ExcelAction extends AbsExcelAction implements Consumer<Question> {
+		private XSSFCellStyle entryHeaderStyle;
+	
+		@Override
+		protected void headerRow() {
+			PrintSetup printSetup = sheet.getPrintSetup();
+			printSetup.setLandscape(true);
+			sheet.setMargin(Sheet.LeftMargin, 0.3);
+			sheet.setMargin(Sheet.RightMargin, 0.3);
+			sheet.setMargin(Sheet.TopMargin, 0.3);
+			Footer footer = sheet.getFooter();
+			footer.setLeft("Preguntas ");
+			footer.setRight("P\u00E1g: &P/&N");
+	
+			row = sheet.createRow(rowCount);
+			CellStyle defaultStyle = workbook.createCellStyle();
+			defaultStyle.setFont(smallFont);
+	
+			decimalStyle.setFont(smallFont);
+	
+			Font journalHeaderFont = workbook.createFont();
+			journalHeaderFont.setBold(true);
+			journalHeaderFont.setFontHeightInPoints((short) 8);
+	
+			XSSFCellStyle headerStyle = (XSSFCellStyle) workbook.createCellStyle();
+			headerStyle.setAlignment(HorizontalAlignment.CENTER);
+			headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+			headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+			headerStyle.setFillForegroundColor(AON_LIGHT_GRAY);
+			headerStyle.setFont(journalHeaderFont);
+			headerStyle.setBorderTop(BorderStyle.THIN);
+			headerStyle.setBorderRight(BorderStyle.THIN);
+			headerStyle.setBorderLeft(BorderStyle.THIN);
+			headerStyle.setBorderBottom(BorderStyle.THIN);
+	
+			entryHeaderStyle = (XSSFCellStyle) workbook.createCellStyle();
+			entryHeaderStyle.setAlignment(HorizontalAlignment.CENTER);
+			entryHeaderStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+			entryHeaderStyle.setBorderBottom(BorderStyle.THIN);
+			entryHeaderStyle.setFont(defaulFont);
+	
+			XSSFCellStyle rightHeaderCellStyle = (XSSFCellStyle) headerStyle.clone();
+			rightHeaderCellStyle.setAlignment(HorizontalAlignment.RIGHT);
+	
+			row = sheet.createRow(rowCount++);
+			cellCount = 0;
+	
+			// cells
+			CellUtil.createCell(row, cellCount, "ALIAS", headerStyle);
+			sheet.setDefaultColumnStyle(cellCount, defaultStyle);
+			sheet.setColumnWidth(cellCount++, 25 * 256);
+	
+			CellUtil.createCell(row, cellCount, "DESCRIPCI\u00D3N", headerStyle);
+			sheet.setDefaultColumnStyle(cellCount, defaultStyle);
+			sheet.setColumnWidth(cellCount++, 70 * 256);
+	
+			CellUtil.createCell(row, cellCount, "TIPO", headerStyle);
+			sheet.setDefaultColumnStyle(cellCount, defaultStyle);
+			sheet.setColumnWidth(cellCount++, 15 * 256);
+	
+			CellUtil.createCell(row, cellCount, "ACTIVA", headerStyle);
+			sheet.setDefaultColumnStyle(cellCount, defaultStyle);
+			sheet.setColumnWidth(cellCount++, 15 * 256);
+	
+		}
+	
+		@Override
+		public void accept(Question question) {
+			row = sheet.createRow(rowCount++);
+			cellCount = 0;
+	
+			addCell(question.getAlias());
+			addCell(question.getText());
+			addCell(question.getType());
+			addCell(question.isActive());
+			
+			try {
+				if (rowCount % 100 == 0)
+					sheet.flushRows();
+			} catch (IOException e) {
+				throw new AonCoreException(e);
+			}
+		}
+	}
+}
