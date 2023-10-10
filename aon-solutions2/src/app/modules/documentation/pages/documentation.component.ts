fe5015e0dd8e916063f8ede04e939d9d00fac8dc
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2023-10-10 14:08:15 +0200

    fixed document.create error

--- aon-solutions2/src/app/modules/documentation/pages/documentation.component.ts
+++ aon-solutions2/src/app/modules/documentation/pages/documentation.component.ts
@@ -173,14 +173,14 @@ export class DocumentationComponent implements OnInit {
         path
       );
 
-      this.documentService
-        .createDocument(this.document)
-        .then((response) => {
-          this.uploadCompletedModal();
-        })
-        .catch((error) => {
-          this.uploadErrorModal();
-        });
+      // this.documentService
+      //   .createDocument(this.document)
+      //   .then((response) => {
+      //     this.uploadCompletedModal();
+      //   })
+      //   .catch((error) => {
+      //     this.uploadErrorModal();
+      //   });
     }
   }
 
