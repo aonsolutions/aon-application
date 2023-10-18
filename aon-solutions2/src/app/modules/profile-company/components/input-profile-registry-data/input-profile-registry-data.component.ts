93b5e012379c57032d5a79c567690bfd53bf5b5f
David Moreno Buj <davidmoreno@ayudatpymes.com>
2023-10-18 12:30:45 +0200

    re #24283 saved company registerData after logic upgrade

--- aon-solutions2/src/app/modules/profile-company/components/input-profile-registry-data/input-profile-registry-data.component.ts
+++ aon-solutions2/src/app/modules/profile-company/components/input-profile-registry-data/input-profile-registry-data.component.ts
@@ -9,7 +9,7 @@ import { IRegistryEnterprise } from 'libraries/AonSDK/src/aon';
 export class InputProfileRegistryDataComponent implements OnInit {
   documentEnterprise: string = '';
   @Input() registryEnterprise: IRegistryEnterprise | null = null;
-  @Output() registryEnterpriseChange: EventEmitter<any> = new EventEmitter<any>();
+  // @Output() registryEnterpriseChange: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {
   }
@@ -17,20 +17,15 @@ export class InputProfileRegistryDataComponent implements OnInit {
     // Actualiza el valor correspondiente en el objeto registryEnterprise
     registryEnterprise[propertyName] = newValue;
 
-    localStorage.setItem('registryEnterprise', JSON.stringify(this.registryEnterprise));
+    // localStorage.setItem('registryEnterprise', JSON.stringify(this.registryEnterprise));
     // Emite el evento con los datos actualizados
-    this.registryEnterpriseChange.emit(this.registryEnterprise);
+    // this.registryEnterpriseChange.emit(this.registryEnterprise);
 
   }
 
 
 
   ngOnInit(): void {
-    // Recuperar el objeto registryEnterprise desde el LocalStorage al inicializar el componente
-    const storedRegistryEnterprise = localStorage.getItem('registryEnterprise');
-    if (storedRegistryEnterprise) {
-      this.registryEnterprise = JSON.parse(storedRegistryEnterprise);
-    }
   }
 
 }
