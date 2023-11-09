4bd654ea8282eecc9a5a8253576ee28adf01d760
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2023-11-09 22:45:21 +0100

    Fix aon-bank-card

--- aon-solutions/src/main/webapp/modules/accounting/aon-bank-card.js
+++ aon-solutions/src/main/webapp/modules/accounting/aon-bank-card.js
@@ -209,15 +209,15 @@ export class AonBankCard extends AonElement {
     sixDaysAgo.setDate(today.getDate() - 7);
   
     if (this.isSameDay(inputDate, today)) {
-      return `Hoy, ${this.getDayName(inputDate)} a las ${this.formatTime(inputDate)}`;
+      return `Actualizado hoy, ${this.getDayName(inputDate)} a las ${this.formatTime(inputDate)}`;
     } else if (this.isSameDay(inputDate, yesterday)) {
-      return `Ayer, ${this.getDayName(inputDate)}`;
+      return `Actualizado ayer, ${this.getDayName(inputDate)}`;
     } else if (inputDate > sixDaysAgo) {
       return this.getDayName(inputDate);
     } else {
       const dayDiff = Math.floor((today - inputDate) / (1000 * 60 * 60 * 24));
       const formattedDate = `${this.padWithZero(inputDate.getDate())} ${this.getMonthName(inputDate)}`;
-      return `${formattedDate} (Hace ${dayDiff} días)`;
+      return `Actualizado ${formattedDate} (Hace ${dayDiff} días)`;
     }
   }
   
@@ -230,8 +230,8 @@ export class AonBankCard extends AonElement {
   }
   
   formatTime(date) {
-    const hours = padWithZero(date.getHours());
-    const minutes = padWithZero(date.getMinutes());
+    const hours = this.padWithZero(date.getHours());
+    const minutes = this.padWithZero(date.getMinutes());
     return `${hours}:${minutes}`;
   }
   
