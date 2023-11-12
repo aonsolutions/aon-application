ec98105155fe4a31d22081d29002d253e4845542
Raúl Trepiana <rtrepiana@aonsolutions.es>
2023-11-12 21:06:09 +0100

    Import 'INFORME DE VIDA LABORAL DE UN CÓDIGO CUENTA DE COTIZACIÓN'.

---
+++ aon-in-payroll/src/main/java/com/esferalia/aon/in/payroll/tgss/ivl/Ivlccc.java
@@ -0,0 +1,11 @@
+package com.esferalia.aon.in.payroll.tgss.ivl;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.esferalia.aon.in.payroll.pdf.UnknownPDFException;
+import com.esferalia.aon.in.payroll.tgss.idc.Idc.IdcListener;
+
+public class Ivlccc {
+}
