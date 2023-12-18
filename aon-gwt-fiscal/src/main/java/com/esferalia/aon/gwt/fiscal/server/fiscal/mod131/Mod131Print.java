58defd117a2845192e052187bbcec8d49abc37eb
EukeCastellano <ecastellano@aonsolutions.es>
2023-12-18 12:43:14 +0100

    Fiscal. Mod131 Scripts refactor.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/fiscal/mod131/Mod131Print.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/server/fiscal/mod131/Mod131Print.java
@@ -4,22 +4,22 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import com.esferalia.aon.gwt.fiscal.client.FiscalModelUtils;
+import com.esferalia.aon.occam.api.fiscal.MODEL131;
+import com.esferalia.aon.occam.api.model.Occam;
+import com.esferalia.aon.occam.api.model.fiscal.IModelScript;
+import com.esferalia.aon.occam.api.model.fiscal.Mod131;
+import com.esferalia.aon.occam.api.model.fiscal.mod131.Model131ScriptProvider;
+import com.esferalia.aon.occam.api.model.type.MimeType;
+import com.esferalia.aon.occam.api.model.type.Mod131Key;
+import com.esferalia.aon.occam.server.fiscal.format.AonFiscalFileUtils;
+import com.esferalia.aon.watson.server.io.AonIOUtils;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import com.esferalia.aon.gwt.fiscal.client.FiscalModelUtils;
-import com.esferalia.aon.gwt.fiscal.shared.mod131.Model131ScriptProvider;
-import com.esferalia.aon.occam.api.fiscal.MODEL131;
-import com.esferalia.aon.occam.api.model.Occam;
-import com.esferalia.aon.occam.api.model.fiscal.IModelScript;
-import com.esferalia.aon.occam.api.model.fiscal.Mod131;
-import com.esferalia.aon.occam.api.model.type.MimeType;
-import com.esferalia.aon.occam.api.model.type.Mod131Key;
-import com.esferalia.aon.occam.server.fiscal.format.AonFiscalFileUtils;
-import com.esferalia.aon.watson.server.io.AonIOUtils;
 
 @WebServlet(name = "Mod131 Print", urlPatterns = { "/aon_gwt_fiscal/ms/Model131Print" })
 public class Mod131Print extends HttpServlet {
