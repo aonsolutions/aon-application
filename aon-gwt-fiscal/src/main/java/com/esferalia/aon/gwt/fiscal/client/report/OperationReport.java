7cc61436837a3e664e04c4eb466dfe0984f9293f
Félix Florindo <f.florindo@analize.es>
2024-02-14 09:55:49 +0100

    Contabilidad: Panel CGVI añadir "Todas" en filtro actividad.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/report/OperationReport.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/report/OperationReport.java
@@ -116,6 +116,7 @@ public class OperationReport extends MainEntryPoint {
 		tabLayout.addSelectionHandler( event -> {
 			if (event.getSelectedItem() == IVA_TAB) ivaContent.clear();
 			else if (event.getSelectedItem() == IRPF_TAB) irpfContent.clear();
+			onSearch();
 		});
 		ivaContent = new SimpleLayoutPanel();
 		tabLayout.add(ivaContent, "Listado IVA");
@@ -139,6 +140,7 @@ public class OperationReport extends MainEntryPoint {
 
 		final AonToolbarButton export = new AonToolbarButton(AON.MSG.export(),AON.CSS.aonIconExcel());
 		export.addClickHandler(event -> submitForm(OPERATION_EXCEL_REPORT_PRINT, getWidgetParams()));
+		export.setVisible(false); // Ya no se utiliza esta opción, solo los libros requerimientos AEAT
 		toolbarPanel.add(export);
 		
 		diskForm = new FormPanel("_blank");
@@ -250,17 +252,18 @@ public class OperationReport extends MainEntryPoint {
 			activity = new ListBox();
 			activity.addStyleName(AON.CSS.aonMarginLeft());
 			activity.setWidth("300px");
-			int i = 0;
-			for (EnterpriseActivity ea : options.getConfiguration().getAllActivities()) {
-				
+			activity.addItem("-- Todas --", "");
+			int i = 1;
+			for (EnterpriseActivity ea : options.getConfiguration().getAllActivities()) {				
 				activity.addItem(ea.getDescription() + (ea.getIae().isEmpty()?"":(" ("+ea.getEpigraph()+")")), AonNumberUtils.toString( ea.getId()));
 				if (ea.isPrincipal()) {
 					activity.setItemText(i, ea.getDescription() + AonStringUtils.ASTERISK);
-					indexMainActivity = i; // Se quedará marcada la actividad principal, por defecto
+					indexMainActivity = i; // Se quedará marcada la actividad principal, por defecto (Ahora no, ahora se queda marcada "Todas"
 				}
 				i++;
 			}
-			activity.setSelectedIndex(indexMainActivity);
+			//activity.setSelectedIndex(indexMainActivity);
+			activity.setSelectedIndex(0);
 		}
 
 		FlexTable tab = new FlexTable();
@@ -356,7 +359,8 @@ public class OperationReport extends MainEntryPoint {
 		period.setSelectedIndex(0);
 		fillDates();
 		if (options.getConfiguration() != null && options.getConfiguration().hasAllActivities()) {
-			activity.setSelectedIndex(indexMainActivity);
+			//activity.setSelectedIndex(indexMainActivity);
+			activity.setSelectedIndex(0);
 		}
 //		 onSearch(); Disable initial search
 	}
@@ -380,8 +384,13 @@ public class OperationReport extends MainEntryPoint {
 			.setExpenses(type.getSelectedIndex() == 0)
 			;
 		if (options.getConfiguration() != null && options.getConfiguration().hasAllActivities() ) {
-			params.setActivity( AonNumberUtils.toInteger( activity.getSelectedValue()));
-			params.setActivityDescription( activity.getSelectedItemText() == null ? "" : activity.getSelectedItemText().replace("*",""));
+			if (activity.getSelectedIndex() > 0) {
+				params.setActivity( AonNumberUtils.toInteger( activity.getSelectedValue()));
+				params.setActivityDescription( activity.getSelectedItemText() == null ? "" : activity.getSelectedItemText().replace("*",""));
+			} else {
+				params.setActivity(null);
+				params.setActivityDescription("");			
+			}
 		}
 		return params;
 	}
--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/AccountingOperationDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/AccountingOperationDAO.java
@@ -216,8 +216,11 @@ public class AccountingOperationDAO {
                 		.and(INVOICE_TAX_ACCOUNT.ACCOUNT.eq(OP_DETAIL_ACC_ID))
                 		)
                 .leftOuterJoin(retInvoiceTax).on(retInvoiceTax.INVOICE_DETAIL.equal(INVOICE_DETAIL.ID).and(retInvoiceTax.TAX_TYPE.equal((byte)2)))  // Retención IRPF
-                .leftOuterJoin(ENTERPRISE_ACTIVITY).on(ENTERPRISE_ACTIVITY.ID.equal(params.getActivity()))
+                
+                //.leftOuterJoin(ENTERPRISE_ACTIVITY).on(ENTERPRISE_ACTIVITY.ID.equal(params.getActivity()))
+                .leftOuterJoin(ENTERPRISE_ACTIVITY).on(ENTERPRISE_ACTIVITY.ID.equal( OP_ACTIVITY ))
                 .leftOuterJoin(IAE).on(IAE.ID.equal(ENTERPRISE_ACTIVITY.IAE))
+                
                 .leftOuterJoin(INVOICE_DUA).on(INVOICE_DUA.INVOICE_IMPORT.equal(INVOICE.ID))
                 
                 .where(OP_DOMAIN.equal(domain))		                
@@ -249,8 +252,10 @@ public class AccountingOperationDAO {
 						.setPercent(Objects.requireNonNullElse(rec.getValue(vatInvoiceTax.PERCENTAGE), 0.0))
 						.setSurchargePercent(Objects.requireNonNullElse(rec.getValue(vatInvoiceTax.SURCHARGE),0.0))
 						// Nuevos datos para el Libro Registro AEAT
+						
 						.setActivityType(getActivityType(rec.getValue(IAE.SECTION),rec.getValue(IAE.EPIGRAPH))) // Actividad - Tipo							
-						.setActivityIAE(AonStringUtils.trimToEmpty(rec.getValue(IAE.EPIGRAPH)))					// Actividad - Epígrafe							
+						.setActivityIAE(AonStringUtils.trimToEmpty(rec.getValue(IAE.EPIGRAPH)))					// Actividad - Epígrafe
+						
 						.setInvoiceType(getInvoiceType( rec ))                 // Tipo de Factura									
 						.setConceptType(rec.getValue(conceptType))   // Concepto de Ingreso/Gasto
 						.setInvoiceSeries(rec.getValue(INVOICE.SERIES))  // Factura - Serie							 
