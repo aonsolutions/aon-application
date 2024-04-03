dd450c213e694440540d191fdc66c4e87c711296
anderibz <aibanez@aonsolutions.es>
2024-04-03 18:47:24 +0200

    PORTAL. pending invoices opened.

--- aon-solutions/src/main/webapp/modules/invoice/InvoiceOptions.js
+++ aon-solutions/src/main/webapp/modules/invoice/InvoiceOptions.js
@@ -85,6 +85,7 @@ import * as UA from '../../services/userAgentService.js';
     name: MSG.PENDINGS,
     icon: MATERIAL_ICONS.INBOX,
     clickable: true,
+    opened: true,
     options: [RAWDOC_INBOX_ISSUED, RAWDOC_INBOX_RECEIVED, RAWDOC_INBOX_TICKET]
   }
 
