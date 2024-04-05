f136ad7940920e5db76d58a2302db6aad09f6497
anderibz <aibanez@aonsolutions.es>
2024-04-05 15:35:03 +0200

    PORTAL. invoice counter changes.

--- aon-solutions/src/main/webapp/modules/invoice/aon-invoice-panel.js
+++ aon-solutions/src/main/webapp/modules/invoice/aon-invoice-panel.js
@@ -35,6 +35,8 @@ import './aon-invoice-print.js';
 import '../../components/aon-application.js';
 import '../../components/aon-dialog-menu.js';
 
+import { getCounter, addCounter, clearCounter } from './InvoiceCounter.js';
+
 export class AonInvoicePanel extends AonElement {
 
 	selectedOption;
@@ -190,36 +192,79 @@ export class AonInvoicePanel extends AonElement {
 		this.getApplication().addEventListener(EVENT.SELECT_OPTION, (e) => {
 			this.selectOption(e.detail);
 		});
-		let data = {
-			invofox: this.getDur().isInvofox()
-		};
-		getRawdocCount(data).then(r => {
-			this.counter = r;
-			OPTION.getOptions(this.getDur()).forEach(option => {
-				option.app = INVOICE;
-				option.count = this.getCounter(option, r);
-				if(option.options)
-					option.options.forEach((o1, i) =>{
-						option.options[i].count = this.getCounter(o1, r);
-						if(option.options[i].options)
-							option.options[i].options.forEach((o2, j) => {
-								option.options[i].options[j].count = this.getCounter(o2, r);
-							});
-					})
-				this.getApplication().addSidenavOptions3(option);
-			});
-		});
 
+		OPTION.getOptions(this.getDur()).forEach(option => {
+			option.app = INVOICE;
+			this.getApplication().addSidenavOptions3(option);
+		});
+		this.buildCounter();
+	}
+
+	buildCounter() {
+		clearCounter();
+		this.invoiceCounter();
 		if(this.getDur().isInvofox()) {
 			this.invofoxCounter();
 		}
+	}
+
+	invoiceCounter() {
+		getRawdocCount({}).then(r => {
+			if(r.invoice && r.invoice.emitida && r.invoice.emitida > 0){
+				addCounter(OPTION.INVOICE_ISSUED, r.invoice.emitida);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX_ISSUED);
+			}
+
+			if(r.invoice && r.invoice.recibida && r.invoice.recibida > 0){
+				addCounter(OPTION.INVOICE_RECEIVED, r.invoice.recibida);
+				this.updateCounterSpan(OPTION.INVOICE_RECEIVED);
+			}
+
+			if(r.invoice && r.invoice.ticket && r.invoice.ticket > 0){
+				addCounter(OPTION.INVOICE_TICKET, r.invoice.ticket);
+				this.updateCounterSpan(OPTION.INVOICE_TICKET);
+			}
+
+			if(r && r.rawdoc && r.rawdoc.inbox && r.rawdoc.inbox.count && r.rawdoc.inbox.count > 0){
+				addCounter(OPTION.INVOICE_PENDINGS, r.rawdoc.inbox.count);
+				this.updateCounterSpan(OPTION.INVOICE_PENDINGS);
+			}
+
+			if(r && r.rawdoc && r.rawdoc.inbox && r.rawdoc.inbox.OUTPUT && r.rawdoc.inbox.OUTPUT > 0){
+				addCounter(OPTION.RAWDOC_INBOX_ISSUED, r.rawdoc.inbox.OUTPUT);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX_ISSUED);
+			}
+
+			if(r && r.rawdoc && r.rawdoc.inbox && r.rawdoc.inbox.INPUT && r.rawdoc.inbox.INPUT > 0){
+				addCounter(OPTION.RAWDOC_INBOX_RECEIVED, r.rawdoc.inbox.INPUT);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX_RECEIVED);
+			}
+
+			// if(r && r.rawdoc && r.rawdoc.inbox && r.rawdoc.inbox.TICKET && r.rawdoc.inbox.TICKET > 0){
+			// 	addCounter(OPTION.RAWDOC_INBOX_TICKET, r.rawdoc.inbox.TICKET);
+			// }
+
+			if(r && r.rawdoc && r.rawdoc.rejected && r.rawdoc.rejected.count && r.rawdoc.rejected.count > 0){
+				addCounter(OPTION.RAWDOC_REJECT, r.rawdoc.rejected.count);
+				this.updateCounterSpan(OPTION.RAWDOC_REJECT);
+			}
+
+			if(r && r.rawdoc && r.rawdoc.draft && r.rawdoc.draft.count && r.rawdoc.draft.count > 0){
+				addCounter(OPTION.RAWDOC_DRAFT, r.rawdoc.draft.count);
+				this.updateCounterSpan(OPTION.RAWDOC_DRAFT);
+			}
+		});
 	}
 
 	invofoxCounter() {
 		let issuedFilter = {publicStatus:['approved', 'pendingCorrection'], type:['invoice'], companyActsLike:'issuer' };	
 		getInvofoxCount(issuedFilter).then(r => {
 			if(r && r.count && r.count > 0) {
-				this.updateCounterSpan(OPTION.RAWDOC_INBOX_ISSUED, r.count);
+				addCounter(OPTION.INVOICE_PENDINGS, r.count);
+				this.updateCounterSpan(OPTION.INVOICE_PENDINGS);
+
+				addCounter(OPTION.RAWDOC_INBOX_ISSUED, r.count);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX_ISSUED);
 			}
 		});
 
@@ -227,7 +272,11 @@ export class AonInvoicePanel extends AonElement {
 		let receivedFilter = {publicStatus:['approved', 'pendingCorrection'], type: ['invoice'], companyActsLike:'recipient'};	
 		getInvofoxCount(receivedFilter).then(r => {
 			if(r && r.count && r.count > 0) {
-				this.updateCounterSpan(OPTION.RAWDOC_INBOX_RECEIVED, r.count);
+				addCounter(OPTION.INVOICE_PENDINGS, r.count);
+				this.updateCounterSpan(OPTION.INVOICE_PENDINGS);
+
+				addCounter(OPTION.RAWDOC_INBOX_RECEIVED, r.count);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX_RECEIVED);
 			}
 		});
 
@@ -235,7 +284,11 @@ export class AonInvoicePanel extends AonElement {
 		let ticketFilter = {publicStatus:['approved', 'pendingCorrection'], type: ['ticket'], companyActsLike:'recipient'};	
 		getInvofoxCount(ticketFilter).then(r => {
 			if(r && r.count && r.count > 0) {
-				this.updateCounterSpan(OPTION.RAWDOC_INBOX_TICKET, r.count);
+				addCounter(OPTION.RAWDOC_INBOX, r.count);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX);
+
+				addCounter(OPTION.RAWDOC_INBOX_TICKET, r.count);
+				this.updateCounterSpan(OPTION.RAWDOC_INBOX_TICKET);
 			}
 		});
 
@@ -243,60 +296,26 @@ export class AonInvoicePanel extends AonElement {
 		let rejectedFilter = {publicStatus:['approved', 'pendingCorrection'], type: ['invoice', 'deliveryNote', 'promissoryNote', 'supplyNote'], companyActsLike:'unknown'};	
 		getInvofoxCount(rejectedFilter).then(r => {
 			if(r && r.count && r.count > 0) {
-				this.updateCounterSpan(OPTION.RAWDOC_REJECT, r.count);
+				addCounter(OPTION.RAWDOC_REJECT, r.count);
+				this.updateCounterSpan(OPTION.RAWDOC_REJECT);
 			}
 		});
 
 		let trashFilter = {publicStatus:['discarded', 'rejected', 'error' ]};	
 		getInvofoxCount(trashFilter).then(r => {
 			if(r && r.count && r.count > 0) {
-				this.updateCounterSpan(OPTION.RAWDOC_DRAFT, r.count);
+				addCounter(OPTION.RAWDOC_DRAFT, r.count);
+				this.updateCounterSpan(OPTION.RAWDOC_DRAFT);
 			}
 		});
 	}
 
-	updateCounterSpan(option, count) {	
+	updateCounterSpan(option) {	
 		let span = document.getElementById("aonMenuItemSpan" + option.id);
 		if(span){
-			span.innerHTML = span.innerHTML.includes(")") 
-				? span.innerHTML.replace(")", "") + "+" + count + ")" 
-				: span.innerHTML + " (" + count + ")";
+			span.innerHTML = option.name + " (" + getCounter()[option.id] + ")";
 			span.style.fontWeight = "bold";
-		} else setTimeout(this.updateCounterSpan, 100, option, count);
-	}
-
-	getCounter(option, r) {
-		switch(option.id){
-			case OPTION.INVOICE_PENDINGS.id:
-				return (r && r.rawdoc && r.rawdoc.inbox) 
-					? r.rawdoc.inbox.count : undefined;
-			case OPTION.RAWDOC_INBOX_ISSUED.id:
-				return (r && r.rawdoc && r.rawdoc.inbox) 
-					? r.rawdoc.inbox.OUTPUT : undefined;
-			case OPTION.RAWDOC_INBOX_RECEIVED.id:
-				return (r && r.rawdoc && r.rawdoc.inbox) 
-					? r.rawdoc.inbox.INPUT : undefined;
-			case OPTION.RAWDOC_INBOX_TICKET.id:
-				return (r && r.rawdoc && r.rawdoc.inbox) 
-					? undefined : undefined; // TODO
-			case OPTION.RAWDOC_REJECT.id:
-				return (r && r.rawdoc && r.rawdoc.rejected) 
-					? r.rawdoc.rejected.count : undefined;
-			case OPTION.RAWDOC_DRAFT.id:
-				return (r && r.rawdoc && r.rawdoc.draft) 
-					? r.rawdoc.draft.count : undefined;
-			case OPTION.INVOICE_ISSUED.id:
-				return (r && r.invoice) 
-					? r.invoice.emitida : undefined;
-			case OPTION.INVOICE_RECEIVED.id:
-				return (r && r.invoice) 
-					? r.invoice.recibida : undefined;
-			case OPTION.INVOICE_TICKET.id:
-				return (r && r.invoice) 
-					? r.invoice.ticket : undefined;		
-					
-			default: return undefined;
-		}
+		} else setTimeout(this.updateCounterSpan, 100, option);
 	}
 
 	search(value) {
