7b25b6400e1cb9ad8a7f63119ca885022613e667
anderibz <aibanez@aonsolutions.es>
2024-04-08 11:48:06 +0200

    invoice counter fix.

--- aon-solutions/src/main/webapp/modules/invoice/aon-invoice-panel.js
+++ aon-solutions/src/main/webapp/modules/invoice/aon-invoice-panel.js
@@ -243,7 +243,7 @@ export class AonInvoicePanel extends AonElement {
 		getRawdocCount({}).then(r => {
 			if(r.invoice && r.invoice.emitida && r.invoice.emitida > 0){
 				addCounter(OPTION.INVOICE_ISSUED, r.invoice.emitida);
-				this.updateCounterSpan(OPTION.RAWDOC_INBOX_ISSUED);
+				this.updateCounterSpan(OPTION.INVOICE_ISSUED);
 			}
 
 			if(r.invoice && r.invoice.recibida && r.invoice.recibida > 0){
