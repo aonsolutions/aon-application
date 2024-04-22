6a63afc74ea16fafdf10ef0c5f1a8d5ddb1862c9
Raúl Trepiana <rtrepiana@aonsolutions.es>
2024-04-22 09:42:01 +0200

    Up2Date already executed .

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -444,7 +444,7 @@ public class Up2Date {
 		// FellowsPercentages2024Update.FELLOWSPERCENTAGES2023UPDATE,
 		// RefreshMod131Result.REFRESH_MOD131_RESULT
     		
-    	AgriculturalBases2024Update.AGRICULTURALBASES2024UPDATE	
+    	// AgriculturalBases2024Update.AGRICULTURALBASES2024UPDATE	
 	};
 
     // ------------------------------------------------------------------------
