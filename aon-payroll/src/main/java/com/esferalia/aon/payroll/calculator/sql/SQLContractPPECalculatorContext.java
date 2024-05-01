3a230c5b3126afedb7f1fd93b7d3d638f3ec3dc3
Raúl Trepiana <rtrepiana@aonsolutions.es>
2024-05-01 21:38:59 +0200

    Fix HORAS_NOMINA for 'ATR. for PPE' . Now we read SALARY_HOURS form DB salaries.

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/sql/SQLContractPPECalculatorContext.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/calculator/sql/SQLContractPPECalculatorContext.java
@@ -12,13 +12,14 @@ import java.util.stream.Collectors;
 
 import com.code.aon.common.AonException;
 import com.code.aon.ql.Criteria;
+import com.esferalia.aon.occam.api.AON;
+import com.esferalia.aon.occam.api.AONContext;
 import com.esferalia.aon.payroll.DelegateContractPayment;
 import com.esferalia.aon.payroll.calculator.IContractBonus;
 import com.esferalia.aon.payroll.calculator.IContractPayment;
 import com.esferalia.aon.payroll.enumeration.ContextVariable;
 import com.esferalia.aon.salary.enumeration.PaymentType;
 import com.esferalia.aon.salary.enumeration.SalaryType;
-import com.esferalia.aon.salary.expression.DeferredExpressionVariable;
 import com.esferalia.aon.salary.expression.ExpressionContext;
 import com.esferalia.aon.salary.expression.ExpressionException;
 import com.esferalia.aon.salary.expression.ExpressionImpl;
@@ -26,7 +27,6 @@ import com.esferalia.aon.salary.expression.ExpressionScope;
 import com.esferalia.aon.salary.expression.ExpressionVariable;
 import com.esferalia.aon.salary.expression.IExpression;
 import com.esferalia.aon.salary.expression.ITimedObject;
-import com.esferalia.aon.salary.expression.ITimedResult;
 import com.esferalia.aon.salary.expression.ITimedVariable;
 import com.esferalia.aon.salary.expression.Period;
 
@@ -41,7 +41,14 @@ public class SQLContractPPECalculatorContext extends SQLContractSalaryCalculator
 			throws SQLException, ExpressionException {
 		super(connection, startDate, endDate, issueDate, chargeDate, criteria);
 	}
-
+	
+	
+	@Override
+	protected void initContractExpressionCtx(NextHook hook) throws SQLException, ExpressionException {
+		super.initContractExpressionCtx(hook);
+		overrideSalaryHours(getExpressionContext());
+	}
+	
 	@Override
 	public SalaryType getSalaryType() {
 		return SalaryType.DELAY;
@@ -83,6 +90,13 @@ public class SQLContractPPECalculatorContext extends SQLContractSalaryCalculator
 			}
 		};
 	}
+	
+	@Override
+	protected Collection<ITimedObject<IExpression>> loadContractData(ExpressionContext ctx, Date startDate,
+			Date endDate) throws SQLException {
+		// TODO Auto-generated method stub
+		return super.loadContractData(ctx, startDate, endDate);
+	}
 
 	private void readDelayCause() {
 
@@ -98,6 +112,47 @@ public class SQLContractPPECalculatorContext extends SQLContractSalaryCalculator
 		}
 		getExpressionContext().readVariable(ContextVariable.DELAY_CAUSE.getName(), getStartDate(), getEndDate(),
 				PaymentType.class);
+	}
+	
+	private void overrideSalaryHours(ExpressionContext ctx) {
+		
+		for ( Period period : ctx.getPeriods(ContextVariable.SALARY_HOURS.getName())) {
+			ctx.putVariable(ContextVariable.SALARY_HOURS.getName(), new ITimedVariable<Double>() {
+				
+				@Override
+				public Period getPeriod() {
+					return period;
+				}
+				
+				@Override
+				public Double getValue(Period period) {
+					try {
+						return getDBSalaryHours(period);
+					} catch ( Exception e ) {
+						try {
+							return ctx.eval(ContextVariable.WORKED_HOURS.getName(), period.getStart(), period.getEnd(), Number.class)
+									.stream().collect(Collectors.summingDouble( r -> r.getValue().doubleValue() ));
+						} catch (Exception e1) {
+							return 0.00;
+						}
+					}
+				}
+			});
+		}
+	}
+	
+	private Double getDBSalaryHours(Period period) {
+		AONContext aonContext = new AONContext(getConnection());
+		Double salaryHours = AON.getSalaryData(aonContext, 
+		p -> p.getContractProperty().eq(this.getId())
+		.and(p.getIsSalaryProperty().eq(Boolean.TRUE))
+		.and(p.getEndDateProperty().ge(period.getStart())
+		.and(p.getStartDateProperty().le(period.getEnd()))))
+		.flatMap( s -> s.getContextData().get(ContextVariable.SALARY_HOURS.getName()).stream() )
+		.collect(Collectors.summingDouble( d -> Double.parseDouble(d.getExpression())));
+		if ( salaryHours == null || salaryHours == 0.00 )
+			throw new NullPointerException();
+		return salaryHours;
 	}
 
 }
