12b176bc04f6db1274b70fa12dd7cb6bb9835cb8
asolaun <ander.solaun@aonsolutions.es>
2024-05-06 09:33:22 +0200

    SESRequesHandler fixed

--- aon-in-issues/src/main/java/aon/solutions/SESRequestHandler.java
+++ aon-in-issues/src/main/java/aon/solutions/SESRequestHandler.java
@@ -181,7 +181,7 @@ public class SESRequestHandler<T> implements RequestHandler<Map<String, T>, APIG
 							.setCreationDate(new Date())
 							.setCreationUser(user.getLogin())
 							.setDomain(company.getDomain())
-							.setTitle(subject)
+							.setTitle(subject.replace("Asunto de la tarea:", ""))
 							.setTaskHolder(user.getTaskHolders().get(0))
 							.setSender(user.getTaskHolders().get(0))
 							.setRegistry(user.getRegistry())
