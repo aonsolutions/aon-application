6b323333204ce2bf8831ea5e1e87418a215b81fa
Raúl Trepiana <rtrepiana@aonsolutions.es>
2024-05-28 14:46:24 +0200

    Revert "Listado XLS de clientes sin terminar"
    This reverts commit 7fe73f5c0ed2c78cdc7fe5e8ab8ff829b0a8b10b.

--- aon-ui-customer/src/main/java/com/code/aon/ui/customer/controller/CustomerController.java
+++ aon-ui-customer/src/main/java/com/code/aon/ui/customer/controller/CustomerController.java
@@ -59,7 +59,6 @@ import com.esferalia.aon.watson.util.AonCollectionUtils;
 
 import jakarta.servlet.http.HttpServletResponse;
 import net.aonsolutions.aon.customer.report.CustomerReportPDF;
-import net.aonsolutions.aon.customer.report.CustomerReportXLS;
 import net.aonsolutions.aon.report.AonReportException;
 
 public class CustomerController extends CustomerListController implements ICustomerConstants, IAuditableController {
@@ -352,30 +351,6 @@ public class CustomerController extends CustomerListController implements ICusto
 			context.responseComplete();
 			return null;
 		} catch (IOException | AonReportException e) {
-			throw new ManagerBeanException( e ); 
-		}
-	}
-	
-	public String onNewReportXLS() throws ManagerBeanException {
-		try {
-			FacesContext context = FacesContext.getCurrentInstance();
-			HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
-			OutputStream out = response.getOutputStream();
-			Occam occam = new Occam()
-					.setDomainName(  AonUtil.getDomainName() )
-					.setDomain(  DomainManager.getCurrentDomain() )
-					.setUser(  AonUtil.getRemoteUser() )
-					;
-			new CustomerReportXLS( occam )
-				.print(out,AonCollectionUtils.stream(getManagerBean().getList(getCriteria()))
-					.map(to -> (Customer) to)
-					.map(this::toCustomerFull ));
-			response.flushBuffer();
-			context.responseComplete();
-			return null;
-		} catch (IOException | AonReportException e) {
-			throw new ManagerBeanException( e ); 
-		} catch (ManagerBeanException e) {
 			throw new ManagerBeanException( e ); 
 		}
 	}
