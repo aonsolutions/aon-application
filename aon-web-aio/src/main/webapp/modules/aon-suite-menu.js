7d3f29a702bb7517e640603d15aa7a818b105eac
amillan <alejandromillanmolinero@gmail.com>
2024-05-29 14:05:53 +0200

    Changes on suite menu

--- aon-web-aio/src/main/webapp/modules/aon-suite-menu.js
+++ aon-web-aio/src/main/webapp/modules/aon-suite-menu.js
@@ -1,13 +1,16 @@
 import { AonElement } from 'aonsolutions/components/AonElement.js';
 import { MSG, CSS, EVENT, TAG } from 'aonsolutions/environments/environments.js'; 
 import { AonIconButton } from 'aonsolutions/components/aon-icon-button.js';
+import { AonButton } from 'aonsolutions/components/aon-button.js';
 import { AonCard } from 'aonsolutions/components/aon-card.js';
 export class AonSuiteMenu extends AonElement {
 
 	SIDE_MENU;
     CONTENT;
     TITLE;
-	
+    NEW_BUTTON;
+    UPLOAD_BUTTON;
+    CONF_BUTTON;
     options; 
 
 	constructor () {
@@ -25,6 +28,9 @@ export class AonSuiteMenu extends AonElement {
 		this.SIDE_MENU = this.id+ 'SideMenu';
 		this.CONTENT = this.id+ 'Content';
         this.TITLE = this.id + 'Title';
+        this.NEW_BUTTON = this.id + 'NewButton';
+        this.UPLOAD_BUTTON = this.id + 'UploadButton';
+        this.CONF_BUTTON = this.id + 'ConfButton';
 	}
 
 	build() {
@@ -41,27 +47,100 @@ export class AonSuiteMenu extends AonElement {
         sideMenu.style.borderRight = "1px solid rgba(0, 0, 0, 0.1)";
         divFlex.appendChild(sideMenu);
 
-        let title = this.createDiv();
-        title.id = this.TITLE;
-        title.className = "aonSidenavTitleBeta";
-        /*
-        title.style.fontSize = "16px";
-        title.style.marginBottom = "10px";
-        title.style.fontWeight = "bold";
-        title.style.marginLeft = "10px";
-        title.innerHTML = "Contabilidad";
-        */
-        sideMenu.appendChild(title);
+        let newButton = new AonButton();
+        newButton.id = this.NEW_BUTTON;
+        newButton.icon = "add";
+        newButton.title = "Agregar nuevo";
+        newButton.style.display = "block";
+        newButton.color = "transparent";
+        newButton.style.width = "222px";
+        newButton.style.border = "1px solid var(--aonBlue)";
+        newButton.style.borderRadius = "5px";
+        newButton.style.marginLeft = "14px";
+        newButton.style.marginTop = "20px";
+        sideMenu.appendChild(newButton);  
+        let newBtText = this.getElement(newButton.TEXT);
+        let newBtIcon = this.getElement(newButton.ICON);
+        let newBtBt = this.getElement(newButton.BUTTON);
+        newBtText.style.color = "var(--aonBlue)";
+        newBtText.style.fontWeight = "normal";
+        newBtIcon.style.color = "var(--aonBlue)";
+        newBtBt.style.boxShadow = "none";
 
+
+        newButton.addEventListener("mouseover", () => {
+           newButton.style.backgroundColor = "rgba(0,36,105,0.1)";
+        });
+          
+          newButton.addEventListener("mouseleave", () => {
+            newButton.style.backgroundColor = "transparent";
+        });
+
+        let sideNavTitle = this.createDiv();
+        sideNavTitle.className = "aonSidenavTitleBeta";
+        sideNavTitle.innerHTML = "Accesos rápidos";
+        sideMenu.appendChild(sideNavTitle);
+
+        sideMenu.appendChild(this.buildSideNavRow("Todos","stacks"));
+        sideMenu.appendChild(this.buildSideNavRow("Abierto recientemente","schedule"));
+
+
+        let utilidades = this.createDiv();
+        utilidades.className = "aonSidenavTitleBeta";
+        utilidades.innerHTML = "Utilidades";
+        sideMenu.appendChild(utilidades);
+
+        sideMenu.appendChild(this.buildSideNavRow("Configuración", "folder_managed"));
+
+        this.buildSideNavCard(sideMenu, "Documentos","1");
+        this.buildSideNavCard2(sideMenu, "Auditoria facturas","2");
+
+        let uploadButton = new AonButton();
+        uploadButton.id = this.UPLOAD_BUTTON;
+        uploadButton.icon = "publish";
+        uploadButton.title = "Subir archivo";
+        uploadButton.color = "transparent";
+        uploadButton.style.display = "block";
+        uploadButton.style.width = "222px";
+        uploadButton.style.border = "2px dashed rgba(0, 0, 0, 0.1)";
+        uploadButton.style.borderRadius = "5px";
+        uploadButton.style.marginLeft = "12px";
+        uploadButton.style.marginTop = "20px";
+        sideMenu.appendChild(uploadButton);
+        let text = this.getElement(uploadButton.TEXT);
+        let icon = this.getElement(uploadButton.ICON);
+        let button = this.getElement(uploadButton.BUTTON);
+        text.style.color = "var(--aonBlue)";
+        text.style.fontWeight = "normal";
+        icon.style.color = "var(--aonBlue)";
+        button.style.boxShadow = "none";
+
+
+        uploadButton.addEventListener("mouseover", () => {
+            uploadButton.style.backgroundColor = "rgba(0,36,105,0.1)";
+        });
+           
+        uploadButton.addEventListener("mouseleave", () => {
+            uploadButton.style.backgroundColor = "transparent";
+        });
+              
         let content = this.createDiv();
         content.id = this.CONTENT;
         content.style.height = "822px";
         content.style.width = "100%";
         content.style.display = "flex";
         content.style.backgroundColor = "rgb(250, 249, 248)";
-        content.style.gap="1rem";
-        content.style.flexWrap="wrap";
+        content.style.gap = "1rem";
+        content.style.flexWrap = "wrap";
         divFlex.appendChild(content);
+
+        let title = this.createDiv();
+        title.id = this.TITLE;
+        title.className = "aonSidenavTitleBeta";
+        title.style.position = "absolute";
+        title.style.marginLeft = "22px";
+        title.style.marginTop = "30px";
+        content.appendChild(title);
 
         this.options.forEach((opt, i) => {
             this.buildCard(opt, i);
@@ -77,6 +156,60 @@ export class AonSuiteMenu extends AonElement {
         }
     }
 
+
+    buildSideNavCard(sideMenu,title,id){
+        let card = new AonCard();
+        card.id = id;
+		card.title = title;
+		card.style.minWidth = "230px";
+
+        card.style.display = "flex";
+        card.style.minHeight = "20px";
+        card.style.marginTop = "10px";
+        sideMenu.appendChild(card);
+		
+        let cardDiv = this.getElement(card.CARD);
+		cardDiv.style.boxShadow = '0 2px 4px rgba(0,0,0,.1)';
+		cardDiv.style.borderRadius = '2px';
+        cardDiv.style.minWidth = "230px";
+        
+        let divGeneral = this.createDiv();
+        divGeneral.appendChild(this.buildCardData("999 Pendientes"));
+        divGeneral.appendChild(this.buildCardData("999 Rechazados"));
+        divGeneral.appendChild(this.buildCardData("999 Fras sin contabilizar"));
+        
+	
+		card.setContent(divGeneral);
+        
+    }
+
+    buildSideNavCard2(sideMenu,title,id){
+        let card = new AonCard();
+        card.id = id;
+		card.title = title;
+		card.style.minWidth = "230px";
+
+        card.style.display = "flex";
+        card.style.minHeight = "20px";
+        card.style.marginTop = "10px";
+        sideMenu.appendChild(card);
+		
+        let cardDiv = this.getElement(card.CARD);
+		cardDiv.style.boxShadow = '0 2px 4px rgba(0,0,0,.1)';
+		cardDiv.style.borderRadius = '2px';
+        cardDiv.style.minWidth = "230px";
+        
+        let divGeneral = this.createDiv();
+        divGeneral.appendChild(this.buildCardData("999 Con IRPF Profesional"));
+        divGeneral.appendChild(this.buildCardData("999 Con IRPF Alquiler"));
+        divGeneral.appendChild(this.buildCardData("999 Intracomunitarios"));
+        divGeneral.appendChild(this.buildCardData("999 Extracomunitarios"));
+        
+	
+		card.setContent(divGeneral);
+        
+    }
+
     buildCard(opt, i){
         let card = new AonCard();
 		card.id = "card" + i;
@@ -86,6 +219,7 @@ export class AonSuiteMenu extends AonElement {
         card.style.display = "flex";
         card.style.height = "300px";
 		card.style.marginLeft = "10px";
+        card.style.marginTop = "60px";
         this.getContent().appendChild(card);
 		
         let cardDiv = this.getElement(card.CARD);
@@ -104,7 +238,7 @@ export class AonSuiteMenu extends AonElement {
     buildCardData(value) {
 		let div = this.createDiv();
 		div.style.marginTop = '10px';
-		div.style.title = value;
+		div.title = value;
         div.style.cursor = "pointer";
 		div.style.display = "block";
         div.style.padding = "2px";
@@ -112,10 +246,10 @@ export class AonSuiteMenu extends AonElement {
 		let span = this.createDiv();
 		span.className = CSS.AON_CARD_TEXT;
 		span.innerHTML = value;
+        span.style.cursor = "pointer";
         span.style.color = "var(--aonBlue)"
 		div.appendChild(span);
 
-        // Agrega los event listeners para cambiar el estilo al pasar el mouse
         span.addEventListener('mouseover', function() {
             span.style.color = 'var(--aonBlue)';
             span.style.textDecoration = 'underline';
@@ -123,6 +257,42 @@ export class AonSuiteMenu extends AonElement {
         span.addEventListener('mouseout', function() {
             span.style.color = 'var(--aonBlue)';
             span.style.textDecoration = '';
+        });
+
+		return div;
+	}
+
+    
+
+    buildSideNavRow(value, icon) {
+		let div = this.createSpan();
+		div.style.paddingTop = '5px';
+		div.style.title = value;
+        div.style.paddingLeft = "30px";
+        div.style.borderRadius = "5px";
+        div.style.paddingBottom = "5px";
+        div.style.cursor = "pointer";
+		div.style.display = "flex";
+
+		let i = this.createElement(TAG.I);
+		i.className = CSS.MATERIAL_ICONS;
+		i.style.marginRight = '5px';
+		i.style.verticalAlign = "middle";
+		i.innerHTML= icon;
+		div.appendChild(i);
+
+		let span = this.createDiv();
+		span.className = CSS.AON_CARD_TEXT;
+        span.style.marginTop = "2px";
+		span.innerHTML = value;
+		div.appendChild(span);
+
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "rgba(0,36,105,0.1)";
+        });
+           
+        div.addEventListener("mouseleave", () => {
+            div.style.backgroundColor = "transparent";
         });
 
 		return div;
