f7930895d2bbe7f85b9236e630da6a3bcc297c75
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2024-05-30 19:29:52 +0200

    Up2Date DB AddColumnMarketingActionTag

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -20,6 +20,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import net.aonsolutions.db.up2date.finance.AnnuledInvoiceCreation;
+import net.aonsolutions.db.up2date.marketing.AddColumnMarketingActionTag;
 import net.aonsolutions.db.up2date.payroll.CleanDuplicatesPayrollWorkplace;
 import net.aonsolutions.db.up2date.tgss.MEIFellows2024Remove;
 
@@ -456,7 +457,8 @@ public class Up2Date {
         //AddColumnSellerTaskHolder.ADD_COLUMN_SELLER_TASK_HOLDER,
 		
         CleanDuplicatesPayrollWorkplace.CLEAN_DUPLICATE_PAYROLL_WORKPLACE, 
-        AnnuledInvoiceCreation.ANNULED_INVOICE_CREATION
+        AnnuledInvoiceCreation.ANNULED_INVOICE_CREATION,
+        AddColumnMarketingActionTag.ADD_COLUMN_MARKETING_ACTION_TAG
 	};
 
     // ------------------------------------------------------------------------
