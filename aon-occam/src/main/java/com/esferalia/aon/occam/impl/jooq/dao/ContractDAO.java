6db4aa27c95748457dc09d29a9a271556ef9bac5
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2024-06-04 13:06:48 +0200

    re #37367. Improve performance of getContractList method.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/ContractDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/ContractDAO.java
@@ -36,7 +36,6 @@ import static com.esferalia.aon.jooq.tables.SalaryEmbargo.SALARY_EMBARGO;
 import static com.esferalia.aon.jooq.tables.SalaryPayment.SALARY_PAYMENT;
 import static com.esferalia.aon.jooq.tables.Workplace.WORKPLACE;
 
-import java.sql.Date;
 import java.util.stream.Stream;
 
 import org.jooq.*;
@@ -109,24 +108,28 @@ public class ContractDAO {
 		ctx.checkRead();
 		Field<Integer> dateMiliseconds = DSL.field("UNIX_TIMESTAMP(date)", Integer.class);
 		Field<Integer> totalTime = DSL.field("total_time", Integer.class);
-		Field<Date> salaryMaxDate = DSL.field("max_date", Date.class);
-		Field<Date> cdMaxDate = DSL.field("cd_max_date", Date.class);
 		Field<String> rDocument = DSL.field("rDocument", String.class);
+		Field<Integer> rnSalary = DSL.field("rnSalary", Integer.class);
+		Field<Integer> idSalary = DSL.field("idSalary", Integer.class);
+		Field<Double> salaryAmount = DSL.field("salaryAmount", Double.class);
+		Field<Integer> rnContractData = DSL.field("rnContractData", Integer.class);
+		Field<Integer> idContractData = DSL.field("idContractData", Integer.class);
+		Field<String> expressionCD = DSL.field("expressionCD", String.class);
 		Table<?> registryTable = REGISTRY.as("registryTable");
 		Table<?> salary = SALARY.as("s");
 		Table<?> contractData = CONTRACT_DATA.as("cd");
 		Table<?> tm = Timecontrol.TIMECONTROL.as("tm");
 		
-		Select<?> subQ1 = DSL.select(DSL.max(SALARY.END_DATE).as(salaryMaxDate))
-				.select(SALARY.CONTRACT)
-				.from(SALARY)
-				.groupBy(SALARY.CONTRACT);
+		Select<?> subQ1 = DSL.select(SALARY.CONTRACT.as(idSalary))
+				.select(SALARY.CGC_BASE.as(salaryAmount))
+				.select(DSL.rowNumber().over(DSL.partitionBy(SALARY.CONTRACT).orderBy(SALARY.END_DATE.desc())).as(rnSalary))
+				.from(SALARY);
 		
-		Select<?> subQ2 = DSL.select(DSL.max(CONTRACT_DATA.START_DATE).as(cdMaxDate))
-				.select(CONTRACT_DATA.CONTRACT)
+		Select<?> subQ2 = DSL.select(CONTRACT_DATA.CONTRACT.as(idContractData))
+				.select(CONTRACT_DATA.EXPRESSION.as(expressionCD))
+				.select(DSL.rowNumber().over(DSL.partitionBy(CONTRACT_DATA.CONTRACT).orderBy(CONTRACT_DATA.START_DATE.desc())).as(rnContractData))
 				.from(CONTRACT_DATA)
-				.where(CONTRACT_DATA.NAME.like("TC2"))
-				.groupBy(CONTRACT_DATA.CONTRACT);
+				.where(CONTRACT_DATA.NAME.eq("TC2"));
 		
 		Select<?> subQ3 = DSL.select(
 					DSL.coalesce(
@@ -149,28 +152,20 @@ public class ContractDAO {
 				.select(CONTRACT.START_DATE)
 				.select(CONTRACT.END_DATE)
 				.select(REGISTRY.DOCUMENT)
-				.select(CONTRACT_DATA.EXPRESSION.as(CONTRACT_TYPE))
+				.select(subQ2.asTable().as(contractData).field(expressionCD).as(CONTRACT_TYPE))
 				.select(CONTRACT.DOMAIN)
 				.select(CONTRACT.PERSON)
 				.select(CONTRACT.WORKPLACE)
 				.select(WORKPLACE.DESCRIPTION)
-				.select(DSL.sum(SALARY.CGC_BASE).cast(Double.class).as(SALARY_CGC_BASE))
+				.select(subQ1.asTable().as(salary).field(salaryAmount).as(SALARY_CGC_BASE))
 				.select(subQ3.asTable().as(tm).field(totalTime).as(MARK_TOTAL_TIME))
 				.from(CONTRACT)
 				.join(WORKPLACE).onKey()
 				.join(REGISTRY).on(REGISTRY.ID.eq(CONTRACT.PERSON))
 				.leftJoin(subQ1.asTable().as(salary))
-						.on(CONTRACT.ID.eq(salary.field(SALARY.CONTRACT)))
-				.leftJoin(SALARY)
-						.on(SALARY.CONTRACT.eq(CONTRACT.ID)
-						.and(DSL.year(salaryMaxDate).eq(DSL.year(SALARY.END_DATE)))
-						.and(DSL.month(salaryMaxDate).eq(DSL.month(SALARY.END_DATE))))
+					.on(CONTRACT.ID.eq(subQ1.asTable().as(salary).field(idSalary)).and(subQ1.asTable().as(salary).field(rnSalary).eq(1)))
 				.leftJoin(subQ2.asTable().as(contractData))
-						.on(subQ2.asTable().as(contractData).field(CONTRACT_DATA.CONTRACT).eq(CONTRACT.ID))
-				.leftJoin(CONTRACT_DATA)
-						.on(CONTRACT_DATA.CONTRACT.eq(CONTRACT.ID)
-						.and(subQ2.asTable().as(contractData).field(cdMaxDate).eq(CONTRACT_DATA.START_DATE))
-						.and(CONTRACT_DATA.NAME.eq("TC2")))
+					.on(CONTRACT.ID.eq(subQ2.asTable().as(contractData).field(idContractData)).and(subQ2.asTable().as(contractData).field(rnContractData).eq(1)))
 				.leftJoin(subQ3.asTable().as(tm)).on(subQ3.asTable().as(tm).field(rDocument).eq(REGISTRY.DOCUMENT))
 				.groupBy(CONTRACT.ID)
 				.having(CONTRACT_EXTENDED_DATA_PROPERTIES.getConditions(filter))
