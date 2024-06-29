86cad35659126f47e3db203860a15b6346dd0978
anderibz <aibanez@aonsolutions.es>
2024-06-29 21:08:41 +0200

    Revert "skip test"
    This reverts commit c7a592feeed246d55b29a9276660a42e2497e106.

--- aon-payroll/src/test/java/com/esferalia/aon/payroll/calculator/sql/SQLWorkedHoursTestCase.java
+++ aon-payroll/src/test/java/com/esferalia/aon/payroll/calculator/sql/SQLWorkedHoursTestCase.java
@@ -725,7 +725,6 @@ public class SQLWorkedHoursTestCase extends AbstractSQLTestCase {
 	}
 
 	@Test
-	@Ignore
 	public void testHolidaysWorkHoursI()
 			throws ExpressionException, SQLException, SalaryException {
 		Connection connection = getConnection();
