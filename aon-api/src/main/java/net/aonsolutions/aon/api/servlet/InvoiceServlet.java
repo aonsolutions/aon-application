b8666496282d3eab0eb897f8252e5b71bc3b3fc0
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2024-07-01 10:00:13 +0200

    Add remarks and comments to invoice fill and parse.

--- aon-api/src/main/java/net/aonsolutions/aon/api/servlet/InvoiceServlet.java
+++ aon-api/src/main/java/net/aonsolutions/aon/api/servlet/InvoiceServlet.java
@@ -561,7 +561,7 @@ public class InvoiceServlet extends AonApiHttpServlet{
 		json.put(IJsonNames.NUMBER, jsonInvoice.optString(IJsonNames.NUMBER));
 		json.put(IJsonNames.SERIE, jsonInvoice.optString(IJsonNames.SERIE));
 		json.put(IJsonNames.EMAIL, jsonInvoice.optBoolean(IJsonNames.EMAIL));
-		json.put(IJsonNames.DOCUMENT, jsonInvoice.optBoolean(IJsonNames.DOCUMENT));
+		json.put(IJsonNames.DOCUMENT, jsonInvoice.optJSONObject(IJsonNames.RECEIVER).optString(IJsonNames.DOCUMENT));
 		if(rawdoc.getMimeType() != null){
 	        JSONObject data = new JSONObject();
 	        data.put("domain_name", api.getDomain().getName());
--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/InvoiceDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/InvoiceDAO.java
@@ -643,6 +643,7 @@ public class InvoiceDAO {
 				.setRetentionQuota(r.getValue(INVOICE.RETENTION_QUOTA))	
 				.setTotal(r.getValue(INVOICE.TOTAL))	
 				.setComments(r.getValue(INVOICE.COMMENTS))
+				.setRemarks(r.getValue(INVOICE.REMARKS))
 				.setFiscal(checkField(r, INVOICE_FISCAL.INVOICE)
 						? InvoiceFiscalDAO.InvoiceFiscalFiller.buildInvoiceFiscal(r)
 						: new InvoiceFiscal())
