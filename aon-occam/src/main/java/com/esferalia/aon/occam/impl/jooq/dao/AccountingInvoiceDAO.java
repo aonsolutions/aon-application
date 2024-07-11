9fe86c60ef62927f9cf633eeec042694a77f4bed
EukeCastellano <ecastellano@aonsolutions.es>
2024-07-11 10:34:25 +0200

    Move fillBreakdown() method from AccountingInvoiceDAO to InvoiceDAO.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/AccountingInvoiceDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/AccountingInvoiceDAO.java
@@ -231,7 +231,7 @@ public class AccountingInvoiceDAO {
 				}
 				
 				if ( !ai.isAccountSource() ) {
-					fillBreakdown(ctx, ai.getInvoice());
+					InvoiceDAO.fillBreakdown(ctx, ai.getInvoice());
 				}
 				ai.getInvoice().setFinances(FinanceDAO.getInvoiceFinances(ctx, invoiceId));
 				
@@ -392,70 +392,6 @@ public class AccountingInvoiceDAO {
 			.setSurchargeQuotaEdited( false  )
 			.setDeductibleQuotaEdited( false  )
 		);
-	}
-
-	public static void fillBreakdown(AONContext ctx, Invoice invoice) {
-		fillBreakdown(ctx, invoice, false);
-	}
-	
-	public static void fillBreakdown(AONContext ctx, Invoice invoice, boolean skipVatExempt) {
-		if (invoice.getBreakdown() == null) {
-			invoice.setBreakdown(new LinkedList<>());
-		}
-		
-		boolean vatExempt = 
-				invoice.isSales()  && !invoice.isNational() && !skipVatExempt		// VENTA NO NACIONAL
-			;
-
-		ctx.getDslContext()
-			.select( 
-				INVOICE_TAX.TAX_TYPE,
-				INVOICE_TAX.BASE,
-				INVOICE_TAX.PERCENTAGE,
-				INVOICE_TAX.QUOTA,
-				INVOICE_TAX.SURCHARGE,
-				INVOICE_TAX.SURCHARGE_QUOTA,
-				INVOICE_TAX.WITHHOLDING_TYPE
-					) 
-		.from( INVOICE_DETAIL )
-		.innerJoin( INVOICE_TAX ).on( INVOICE_TAX.INVOICE_DETAIL.eq(INVOICE_DETAIL.ID))
-		.where(INVOICE_DETAIL.INVOICE.eq(invoice.getId()))
-		.and( !vatExempt ? DSL.trueCondition(): INVOICE_TAX.TAX_TYPE.ne(TaxType.VAT.value()) )
-		.fetch()
-		.stream()
-		.map( tax -> new InvoiceBreakdown()
-			.setTaxType( TaxType.safeValueOf(tax.getValue(INVOICE_TAX.TAX_TYPE) ))
-			.setBase(tax.getValue(INVOICE_TAX.BASE))
-			.setPercentage(tax.getValue(INVOICE_TAX.PERCENTAGE))
-			.setQuota(tax.getValue(INVOICE_TAX.QUOTA))
-			.setSurcharge(tax.getValue(INVOICE_TAX.SURCHARGE))
-			.setSurchargeQuota(tax.getValue(INVOICE_TAX.SURCHARGE_QUOTA))
-			.setWithholdingType(WithholdingType.safeValueOf(tax.getValue(INVOICE_TAX.WITHHOLDING_TYPE) )))
-		.forEach( br -> {
-			boolean added = false;
-			for (InvoiceBreakdown invBr : invoice.getBreakdown()) {
-				if ( invBr.getTaxType() == br.getTaxType() && AonNumberUtils.equals(invBr.getPercentage(), br.getPercentage())) {
-					invBr.setBase(AonMathUtils.round( invBr.getBase() + br.getBase(), 4));
-					invBr.setQuota(AonMathUtils.round( invBr.getQuota() + br.getQuota(), 4));
-					added = true;
-				} 
-			}
-			if (!added) {
-				invoice.getBreakdown().add(br);		
-			}
-		});
-		for (InvoiceBreakdown br : invoice.getBreakdown()) {
-			if (AonMathUtils.isZero( br.getQuota() )) {
-				br.setQuota( AonMathUtils.round( br.getBase() * br.getPercentage() / 100 ) );
-			}
-			if (AonMathUtils.isNotZero(br.getSurcharge()) && AonMathUtils.isZero( br.getSurchargeQuota() )) {
-				br.setSurchargeQuota( AonMathUtils.round( br.getBase() * br.getSurcharge() / 100 ) );
-			}
-		}
-		invoice.getBreakdown().sort((b1, b2) -> Comparator
-			.comparing(InvoiceBreakdown::getTaxType)
-			.thenComparing(InvoiceBreakdown::getPercentage)		
-			.compare(b1, b2));
 	}
 
 	public static AccountingInvoice initializeInvoice(AONContext ctx, InvoiceType type, Integer registryId, AccountingInvoice ai, boolean preserveData) {
