cd04b7841b419257490292a39a456ad7dd53ab67
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2024-07-19 12:19:56 +0200

    TaskHolders list only for employee.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/TaskImpl.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/TaskImpl.java
@@ -293,6 +293,12 @@ public class TaskImpl implements ITask {
 	}
 	
 	@Override
+	public Stream<TaskHolder> getTaskHolderEmployee(AONContext ctx, TaskHolderFilter filter, Integer page, Integer perPage) {
+		return ctx.getDslContext().transactionResult(
+				configuration -> TaskOldDAO.getTaskHolderEmployee(ctx, filter, page, perPage));
+	}
+	
+	@Override
 	public Stream<TaskHolder> getTaskHolderStream(AONContext ctx, byte[] auth) {
 		return ctx.getDslContext().transactionResult(
 				configuration -> TaskOldDAO.getTaskHolderStream(ctx, auth));
