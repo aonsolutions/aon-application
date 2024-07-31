2053387d0af3b521f5ad84d65dc60b15f2857432
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2024-07-31 15:22:52 +0200

    Portal AonSepa. Changes AonSepaList

--- aon-solutions/src/main/webapp/modules/documental/aon-sepa-list.js
+++ aon-solutions/src/main/webapp/modules/documental/aon-sepa-list.js
@@ -1,28 +1,42 @@
-import {AonElement} from '../../components/AonElement.js';
-import {getSepaDocuments, getDocuments, downloadDocuments, sendDocumentMail, updateFiles, deleteFile, getDomainUserRoles, downloadSepaDocument, downloadDocument} from '../../services/service.js';
-import {DomainUserRoles} from '../../models/DomainUserRoles.js';
+import { AonElement } from "../../components/AonElement.js";
+import {
+  getSepaDocuments,
+  getDocuments,
+  downloadDocuments,
+  sendDocumentMail,
+  updateFiles,
+  deleteFile,
+  getDomainUserRoles,
+  downloadSepaDocument,
+  downloadDocument,
+} from "../../services/service.js";
+import { DomainUserRoles } from "../../models/DomainUserRoles.js";
 
-import '../../components/aon-table.js';
+import "../../components/aon-table.js";
 
-import { CONSTANT, MATERIAL_ICONS, MSG } from '../../environments/environments.js';
-import * as ACTION from '../actions.js';
-import * as LS from '../../services/localStorageService.js';
-import { DOCUMENTAL } from '../../services/app.js';
-import { AonTable } from '../../components/aon-table.js';
-import { formatNumber } from '../../services/utils.js';
+import {
+  CONSTANT,
+  MATERIAL_ICONS,
+  MSG,
+} from "../../environments/environments.js";
+import * as ACTION from "../actions.js";
+import * as LS from "../../services/localStorageService.js";
+import { DOCUMENTAL } from "../../services/app.js";
+import { AonTable } from "../../components/aon-table.js";
+import { formatNumber } from "../../services/utils.js";
+import { AonSepa } from "./aon-sepa.js";
 
 export class AonSepaList extends AonElement {
+  more;
+  _roles;
 
-	more;
-	_roles;
+  TABLE;
 
-	TABLE;
+  static get observedAttributes() {
+    return [];
+  }
 
-	static get observedAttributes() {
-		return [];
-	}
-
-	get filter() {
+  get filter() {
     return this.getAttribute(CONSTANT.FILTER);
   }
 
@@ -30,151 +44,155 @@ export class AonSepaList extends AonElement {
     this.setAttribute(CONSTANT.FILTER, filter);
   }
 
-	attributeChangedCallback(name, oldValue, newValue) {
-		this.initialize();
-	}
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.initialize();
+  }
 
-	constructor () {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	connectedCallback () {
-		this.initialize();
-		getDomainUserRoles({}).then(r => {
-			this._roles = new DomainUserRoles(r);
-			this.build();
-		});
- 	}
+  connectedCallback() {
+    this.initialize();
+    getDomainUserRoles({}).then((r) => {
+      this._roles = new DomainUserRoles(r);
+      this.build();
+    });
+  }
 
-	initialize() {
-		this.more = true;
-		this.TABLE = 'aonDocumentalSepaTable';
-	}
+  initialize() {
+    this.more = true;
+    this.TABLE = "aonDocumentalSepaTable";
+  }
 
- 	build() {
-		let aonDocumentalSepaTable =  new AonTable();
-		aonDocumentalSepaTable.id = this.TABLE;
-		aonDocumentalSepaTable.setApp(DOCUMENTAL);
-		this.appendChild(aonDocumentalSepaTable);
+  build() {
+    let aonDocumentalSepaTable = new AonTable();
+    aonDocumentalSepaTable.id = this.TABLE;
+    aonDocumentalSepaTable.setApp(DOCUMENTAL);
+    this.appendChild(aonDocumentalSepaTable);
 
-		aonDocumentalSepaTable.addColumn(MSG.DATE, 'date', 'issueDate', '20%');
-		aonDocumentalSepaTable.addColumn(MSG.NAME, 'string', 'description', '40%');
-		aonDocumentalSepaTable.addColumn(MSG.AMOUNT, 'number', 'amount', '15%');
-		aonDocumentalSepaTable.addColumn("Nóminas", 'string', 'details', '15%');
-		aonDocumentalSepaTable.addColumn("", "icons", "icons", "10%");
+    aonDocumentalSepaTable.addColumn(MSG.DATE, "date", "issueDate", "20%");
+    aonDocumentalSepaTable.addColumn(MSG.NAME, "string", "description", "40%");
+    aonDocumentalSepaTable.addColumn(MSG.AMOUNT, "number", "amount", "15%");
+    aonDocumentalSepaTable.addColumn("Nóminas", "string", "details", "15%");
+    aonDocumentalSepaTable.addColumn("", "icons", "icons", "10%");
 
-		this.init();
+    this.init();
 
-		aonDocumentalSepaTable.addEventListener('more', () => {
-			if(this.more)
-				this.loadMore()
-		});
-	}
+    aonDocumentalSepaTable.addEventListener("more", () => {
+      if (this.more) this.loadMore();
+    });
+  }
 
-	async loadMore() {
-		let aonDocumentalSepaTable = this.getElement(this.TABLE);
-		let filter = this.getFilter();
+  async loadMore() {
+    let aonDocumentalSepaTable = this.getElement(this.TABLE);
+    let filter = this.getFilter();
 
-		if(aonDocumentalSepaTable && filter.page) {
-			filter.page = filter.page + 1;
-			this.setFilter(filter);
+    if (aonDocumentalSepaTable && filter.page) {
+      filter.page = filter.page + 1;
+      this.setFilter(filter);
 
-			let sepaDocuments = await getSepaDocuments(this.getFilter());
+      let sepaDocuments = await getSepaDocuments(this.getFilter());
 
-			let sepaDocumentsFormat = sepaDocuments.map(sepaDoc => {
-				return {
-					...sepaDoc,
-					amount : this.getAmount(sepaDoc),
-					details: !sepaDoc.fbatch_details ? 0 : sepaDoc.fbatch_details.length,
-					icons : this.buildIcons(sepaDoc)
-				}
-			});
+      let sepaDocumentsFormat = sepaDocuments.map((sepaDoc) => {
+        return {
+          ...sepaDoc,
+          amount: this.getAmount(sepaDoc),
+          details: !sepaDoc.fbatch_details ? 0 : sepaDoc.fbatch_details.length,
+          icons: this.buildIcons(sepaDoc),
+        };
+      });
 
-			if(sepaDocumentsFormat.length == 0)
-				this.more = false;
-			sepaDocumentsFormat.forEach((doc, i) => {
-				aonDocumentalSepaTable.addRow(doc, () => this.aonDocument(doc, i), (e) => this.aonDocumentContextMenu(e, doc, i));
-			});
-		}
-	}
+      if (sepaDocumentsFormat.length == 0) this.more = false;
+      sepaDocumentsFormat.forEach((doc, i) => {
+        aonDocumentalSepaTable.addRow(
+          doc,
+          () => this.getApplication().setContent(new AonSepa(doc))
+        );
+      });
+    }
+  }
 
-	async init() {
-		this.more = true;
-		let aonDocumentalSepaTable = this.getElement(this.TABLE);
-		if(aonDocumentalSepaTable) {
+  async init() {
+    this.more = true;
+    let aonDocumentalSepaTable = this.getElement(this.TABLE);
+    if (aonDocumentalSepaTable) {
+      let sepaDocuments = await getSepaDocuments(this.getFilter());
 
-			let sepaDocuments = await getSepaDocuments(this.getFilter());
+      let sepaDocumentsFormat = sepaDocuments.map((sepaDoc) => {
+        return {
+          ...sepaDoc,
+          amount: this.getAmount(sepaDoc),
+          details: !sepaDoc.fbatch_details ? 0 : sepaDoc.fbatch_details.length,
+          icons: this.buildIcons(sepaDoc),
+        };
+      });
 
-			let sepaDocumentsFormat = sepaDocuments.map(sepaDoc => {
-				return {
-					...sepaDoc,
-					amount : this.getAmount(sepaDoc),
-					details: !sepaDoc.fbatch_details ? 0 : sepaDoc.fbatch_details.length,
-					icons : this.buildIcons(sepaDoc)
-				}
-			});
+      aonDocumentalSepaTable.removeRows();
+      sepaDocumentsFormat.forEach((doc, i) => {
+        aonDocumentalSepaTable.addRow(
+          doc,
+          () => this.getApplication().setContent(new AonSepa(doc))
+        );
+      });
+    }
+  }
 
-			console.log(sepaDocumentsFormat);
-			aonDocumentalSepaTable.removeRows();
-			sepaDocumentsFormat.forEach((doc, i) => {
-				aonDocumentalSepaTable.addRow(doc, () => this.aonDocument(doc, i), (e) => this.aonDocumentContextMenu(e, doc, i));
-			});
-		}
-	}
+  getAmount(sepaDoc) {
+    if (!sepaDoc.fbatch_details || sepaDoc.fbatch_details.length === 0) {
+      return formatNumber(0, 2, "EUR");
+    }
 
-	getAmount(sepaDoc) {
-		if(!sepaDoc.fbatch_details || sepaDoc.fbatch_details.length === 0){
-			return formatNumber(0, 2, "EUR");
-		}
+    let total = sepaDoc.fbatch_details.reduce(
+      (t, detail) => t + detail.amount,
+      0
+    );
+    return formatNumber(total, 2, "EUR");
+  }
 
-		let total = sepaDoc.fbatch_details.reduce((t, detail) => t + detail.amount, 0);
-		return formatNumber(total, 2, "EUR");
-	}
+  buildIcons(sepaDoc) {
+	if(!sepaDoc.rattach) return;
 
-	buildIcons(sepaDoc) {
-		return [
-			{
-				icon: MATERIAL_ICONS.VISIBILITY,
-				color : "grey",
-				title : "Ver nóminas",
-				fn : () => console.log("Ver nóminas : " + sepaDoc.description)
-			},
-			{
-				icon: MATERIAL_ICONS.FILE_DOWNLOAD,
-				color : "grey",
-				title : "Descargar",
-				fn : () => {
-					console.log("Descargar fichero Sepa");
-					this.downloadSepaFile(sepaDoc.rattach)
-				}
-			}
-		];
-	}
+    return [
+    //   {
+    //     icon: MATERIAL_ICONS.VISIBILITY,
+    //     color: "grey",
+    //     title: "Ver nóminas",
+    //     fn: () => this.getApplication().setContent(new AonSepa(doc)),
+    //   },
+      {
+        icon: MATERIAL_ICONS.FILE_DOWNLOAD,
+        color: "grey",
+        title: "Descargar",
+        fn: () => this.downloadSepaFile(sepaDoc.rattach)
+      },
+    ];
+  }
 
-	downloadSepaFile(rattach) {
-		let data = {
-			domainId: LS.getDomainId(),
-			domainName: LS.getDomainName(),
-			domainLogin: LS.getDomainLogin(),
-			rattach: rattach,
-			type: "registry"
-		};
+  downloadSepaFile(rattach) {
+    let data = {
+      domainId: LS.getDomainId(),
+      domainName: LS.getDomainName(),
+      domainLogin: LS.getDomainLogin(),
+      rattach: rattach,
+      type: "registry",
+    };
 
-		let json = btoa(JSON.stringify(data));
-		downloadDocument(json);
-	}
+    let json = btoa(JSON.stringify(data));
+    downloadDocument(json);
+  }
 
-	getFilter() {
-		return this.hasAttribute('filter')
- 			? JSON.parse(this.getAttribute('filter'))
-			: {
-				page: 1,
-				per_page: 50
-			};
-	}
+  getFilter() {
+    return this.hasAttribute("filter")
+      ? JSON.parse(this.getAttribute("filter"))
+      : {
+          page: 1,
+          per_page: 50,
+        };
+  }
 
-	setFilter(filter) {
-		return this.setAttribute('filter', JSON.stringify(filter));
-	}
+  setFilter(filter) {
+    return this.setAttribute("filter", JSON.stringify(filter));
+  }
 }
-window.customElements.define('aon-sepa-list', AonSepaList);
+window.customElements.define("aon-sepa-list", AonSepaList);
