03592ab9a8819271f72fc1ff4de377b7424c1a31
Raúl Trepiana Zarate <rtrepiana@aonsolutions.es>
2024-09-18 17:35:19 +0200

    Revert "Svaldepenas"

--- aon-solutions/src/main/webapp/modules/documental/aon-documental-card.js
+++ aon-solutions/src/main/webapp/modules/documental/aon-documental-card.js
@@ -48,9 +48,7 @@ export class AonDocumentalCard extends AonElement {
     this.style.height = "100%";
 
     let cardContent = this.createElement(TAG.DIV);
-    cardContent.style.display = "flex";
-    cardContent.style.flexDirection = "column";
-    cardContent.style.justifyContent = "center";
+    cardContent.className = CSS.AON_FLEX_COLUMN;
     cardContent.id = "documentalCardTable";
     this.appendChild(cardContent);
 
@@ -80,27 +78,24 @@ export class AonDocumentalCard extends AonElement {
       let documentalCard = this.getElement("documentalCard");
       documentalCard.style.display = "none";
     } else {
-      let maxLength = documents.length > 7 ? 7 : documents.length;
+      let maxLength = documents.length > 5 ? 5 : documents.length;
 
       for (let index = 0; index < maxLength; index++) {
         const document = documents[index];
         
         let row = this.createElement(TAG.DIV);
-        row.className = CSS.AON_DOCUMENTAL_CARD_ROW_CONTENT;
+        row.className = CSS.AON_DOCUMENTAL_CARD_ROW;
         row.addEventListener(EVENT.CLICK, () => {
           this.goDocumentalDocument(document);
         });
 
         let leftContent = this.createElement(TAG.DIV);
         leftContent.className = CSS.AON_FLEX;
-        leftContent.style.alignItems = "center";
-        leftContent.style.flexGrow = "1";
-        leftContent.style.minWidth = "0";
-        leftContent.style.marginRight = ".5rem";
-        leftContent.style.gap = ".5rem";
+        leftContent.style.alignContent = "center";
+        leftContent.style.flexDirection = "column";
 
         let description = this.createElement(TAG.SPAN);
-        description.className = CSS.AON_ELLIPSIS;
+        description.className = CSS.AON_DOCUMENTAL_ELLIPSIS;
         description.style.fontSize = ".9rem";
         description.style.color = "var(--aonDocumental)";
         description.style.fontWeight = "500";
@@ -109,11 +104,9 @@ export class AonDocumentalCard extends AonElement {
         leftContent.appendChild(description);
 
         let rightContent = this.createElement(TAG.DIV);
-        rightContent.style.display = "flex";
-        rightContent.style.flexDirection = "column";
-        rightContent.style.width = "5.5rem";
-        rightContent.style.textAlign = "right";
-        rightContent.style.flexShrink = "0";
+        rightContent.className = CSS.AON_FLEX_COLUMN;
+        rightContent.style.alignItems = "end";
+        rightContent.style.gap = "0";
 
         let date = this.createElement(TAG.SPAN);
         date.classList.add("aonDocumentalCardDate");
--- aon-solutions/src/main/webapp/modules/messenger/aon-messenger-card.js
+++ aon-solutions/src/main/webapp/modules/messenger/aon-messenger-card.js
@@ -59,7 +59,6 @@ export class AonMessengerCard extends AonElement {
     let tableContent = this.createElement(TAG.DIV);
     tableContent.className = CSS.AON_FLEX_COLUMN;
     tableContent.style.width = "100%";
-    tableContent.style.gap = "0";
     tableContent.id = "messengerCardTable";
 
     let showAllMessage = this.createElement(TAG.DIV);
@@ -127,14 +126,12 @@ export class AonMessengerCard extends AonElement {
   getDataDesktop(datos) {
     try {
       const documents = this.getDocuments();
-
       return datos.map((dato, idx) => ({
         ...dato,
-        rightContent: this.getDateParseNew(dato.date),
-        senderRecipt : this.getSenderRecipt(dato),
-        newTitle: this.getTitleDesktop(dato),
+        dateParse: this.getDateParseNew(dato.date),
+        newTitle: this.getTitleDesktop(dato, documents.document, documents.documentTh),
         assigned: TaskListUtils.getAssignedHtml(dato, documents.domainId),
-        lettersHtml: TaskListUtils.getIcon(dato, "24"),
+        lettersHtml: TaskListUtils.getIcon(dato),
         fn: () => this.goMessengerChat(dato, idx)
       }));
     } catch (e) {
@@ -149,49 +146,51 @@ export class AonMessengerCard extends AonElement {
       this.rootPanel(aonComponent);
     }
   }
+
+  // getDateParseNew(date) {
+  //   return window.innerWidth < 768 ? AonDateUtils.getDayMonthOrFull(date) : AonDateUtils.setDateTpDay(date);
+  // }
   
   getDateParseNew(date) {
-    let rightContent = this.createElement(TAG.DIV);
-    rightContent.style.display = "flex";
-    rightContent.style.flexDirection = "column";
-    rightContent.style.width = "4.5rem";
-    rightContent.style.textAlign = "right";
-    rightContent.style.flexShrink = "0";
-
     if(!date) {
-      console.log("Formating Err");
-      return rightContent;
+      return "Formating Err"
     }
-    
-    let dateDescription = this.createElement(TAG.SPAN);
-    dateDescription.classList.add("aonMessengerCardDate");
-    
-    let dateTime = this.createElement(TAG.SPAN);
-    dateTime.classList.add("aonMessengerCardDate");
     
     var today = new Date();
     today.setHours(0,0,0,0);
     var yesterday = new Date(today);
     yesterday.setDate(today.getDate() - 1);
+    var week = new Date(today);
+    week.setDate(today.getDate() - 7);
+    var month = new Date(today);
+    month.setDate(today.getDate() - 30);
+    var quarter = new Date(today);
+    quarter.setDate(today.getDate() - 90);
+    var halfYear = new Date(today);
+    halfYear.setDate(today.getDate() - 180);
+    var year = new Date(today);
+    year.setDate(today.getDate() - 365);
 
     if (date >= today) {
-        dateDescription.innerHTML = "Hoy";
-        dateTime.innerHTML = AonDateUtils.timeParserHHMM(date);
-        rightContent.appendChild(dateDescription);
-        rightContent.appendChild(dateTime);
+        return 'Hoy';
     } else if (date >= yesterday) {
-        dateDescription.innerHTML = "Ayer";
-        dateTime.innerHTML = AonDateUtils.timeParserHHMM(date);
-        rightContent.appendChild(dateDescription);
-        rightContent.appendChild(dateTime);
+        return 'Ayer';
+    // } else if (date >= week) {
+    //     return 'Última semana';
+    // } else if (date >= month) {
+    //     return 'Último mes';
+    // } else if (date >= quarter) {
+    //     return 'Último trimestre';
+    // } else if (date >= halfYear) {
+    //     return 'Último semestre';
+    // } else if (date >= year) {
+    //     return 'Último año';
+    // } else {
+    //     return 'Más de un año';
+    // }
     } else {
-        dateDescription.innerHTML = AonDateUtils.getDayStr(new Date(date));
-        dateTime.innerHTML = AonDateUtils.getDayMonthOrFullShort(date);
-        rightContent.appendChild(dateDescription);
-        rightContent.appendChild(dateTime);
+        return AonDateUtils.getDayMonthOrFull(date);
     }
-
-    return rightContent;
   }
 
   getDocuments() {
@@ -207,30 +206,26 @@ export class AonMessengerCard extends AonElement {
     };
   }
 
-  getSenderRecipt(messenger) {
+  getTitleDesktop(messenger, document, documentTh) {
     let div = this.createElement(TAG.DIV);
-    div.classList = CSS.AON_ELLIPSIS;
-    div.style.fontSize = ".7rem";
-
-    let sender = this.getSender(messenger);
-    let receipt = this.getReceipt(messenger);
-
-    div.appendChild(sender);
-    div.appendChild(document.createTextNode(" " + receipt));
-
-    return div;
-  }
-
-  getTitleDesktop(messenger) {
-    let div = this.createElement(TAG.DIV);
-    div.style.display = "flex";
-    div.style.flexDirection = "column";
-    div.style.flexGrow = "1";
-    div.style.minWidth = "0";
+    div.className = CSS.AON_FLEX_COLUMN;
+    div.style.alignItems = "flex-start";
     div.style.gap = ".2rem";
 
+    let sender = this.getSender(messenger, document, documentTh);
+
+    let title = this.createElement(TAG.SPAN);
+    title.innerHTML = sender;
+    title.title = sender;
+    title.className = CSS.AON_ELLIPSIS;
+    title.style.fontSize = ".9rem";
+    title.style.color = "var(--aonMessenger)";
+    title.style.fontWeight = "bold";
+
+    div.appendChild(title);
+
     let content = this.createElement(TAG.SPAN);
-    content.classList = CSS.AON_ELLIPSIS;
+    content.className = CSS.AON_ELLIPSIS;
     content.style.fontSize = ".7rem";
     content.style.fontWeight = "bold";
     content.innerHTML = messenger.title;
@@ -245,7 +240,7 @@ export class AonMessengerCard extends AonElement {
     if (description) description = description.replace(/<[^>]+>|&nbsp;|\n/g, " ");
 
     let descriptionContent = this.createElement(TAG.SPAN);
-    descriptionContent.classList = CSS.AON_ELLIPSIS;
+    descriptionContent.className = CSS.AON_ELLIPSIS;
     descriptionContent.style.fontSize = ".7rem";
     descriptionContent.innerHTML = `(${messenger.newNumber}) ${description}`;
     descriptionContent.title = description;
@@ -254,78 +249,66 @@ export class AonMessengerCard extends AonElement {
     return div;
   }
 
-  getSender(res) {
-    let sender = this.createElement(TAG.SPAN);
-    sender.style.fontSize = ".8rem";
-    sender.style.color = "var(--aonMessenger)";
-    sender.style.fontWeight = "bold";
-
-    let senderText = "";
-    if (res.registry && res.registry.name) {
-      senderText = `${res.registry.name}`;
-    } else if (res.sender && res.sender.name) {
-      senderText = `${res.sender.name}`;
+  getSender(res, document, documentTh) {
+    let sender = "";
+    if (res.registry && res.registry.name && document !== res.registry.document) {
+      sender = `${res.registry.name} ${sender}`;
+    } else if (res.sender && res.sender.name && documentTh !== res.sender.document) {
+      sender = `${res.sender.name} ${sender}`;
+    } else if (res.workgroup && res.workgroup.description) {
+      // GRUPO ASIGNADO
+      sender = res.workgroup.description;
     } else {
-      senderText = "SYSTEM";
+      sender = "SIN GRUPO ASIGNADO";
     }
-
-    sender.innerHTML = senderText;
-    sender.title = senderText;
   
     return sender;
   };
 
-  getReceipt(res) {
-    let receipt = "";
-
-    if(res.task_holder && res.task_holder.name){
-      receipt = `para ${res.task_holder.name}`;
-    } else if(res.workgroup && res.workgroup.description){
-      receipt = `para ${res.workgroup.description}`;
-    }
-  
-    return receipt;
-  };
-
   getTable(messengers) {
     let content = this.getElement("messengerCardTable");
-    let maxIndex = messengers.length > 4 ? 4 : messengers.length;
+    let maxIndex = messengers.length > 3 ? 3 : messengers.length;
     
     for (let index = 0; index < maxIndex; index++) {
       const messenger = messengers[index];
-
+      
       let row = this.createElement(TAG.DIV);
       row.classList = CSS.AON_MESSENGER_CARD_ROW;
       row.addEventListener(EVENT.CLICK, () => {
         messenger.fn();
         // () => messenger.fn;
       });
-      
-      let senderRecipt = messenger.senderRecipt;
-      row.appendChild(senderRecipt);
-
-      let rowContent = this.createElement(TAG.DIV);
-      rowContent.classList = CSS.AON_MESSENGER_CARD_ROW_CONTENT;
-      row.appendChild(rowContent);
 
       let leftContent = this.createElement(TAG.DIV);
       leftContent.className = CSS.AON_FLEX;
-
+      leftContent.style.gap = ".8rem";
+      leftContent.style.alignContent = "start";
       leftContent.style.alignItems = "center";
-      leftContent.style.flexGrow = "1";
-      leftContent.style.minWidth = "0";
-      leftContent.style.marginRight = ".5rem";
-      leftContent.style.gap = ".5rem";
 
       let icon = messenger.lettersHtml;
       icon.firstElementChild.style.color = "var(--aonMessenger)";
+      icon.firstElementChild.firstElementChild.style.fontSize = "30px";
       leftContent.appendChild(icon);
 
       let title = messenger.newTitle;
       leftContent.appendChild(title);
 
-      rowContent.appendChild(leftContent);
-      rowContent.appendChild(messenger.rightContent);
+      let rightContent = this.createElement(TAG.DIV);
+      rightContent.className = CSS.AON_FLEX;
+      rightContent.style.alignItems = "center";
+      rightContent.style.gap = ".5rem";
+
+      // let assigned = messenger.assigned;
+      // rightContent.appendChild(assigned);
+
+      let date = this.createElement(TAG.SPAN);
+      date.classList.add("aonMessengerCardDate");
+      date.innerHTML = messenger.dateParse;
+
+      rightContent.appendChild(date);
+
+      row.appendChild(leftContent);
+      row.appendChild(rightContent);
 
       content.appendChild(row);
     }
