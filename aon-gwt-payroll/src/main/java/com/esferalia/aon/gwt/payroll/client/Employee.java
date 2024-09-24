86caf167db92d2467578fc70384b384b3f1292f4
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2024-09-24 11:03:02 +0200

    Employee allow change second surname

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/Employee.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/Employee.java
@@ -530,8 +530,7 @@ public abstract class Employee extends ResizeComposite {
 	@UiHandler("secondSurname")
 	void onSecondSurnameChangeValue(ChangeEvent event) {
 		String secondSurnameStr = this.secondSurname.getValue().trim();
-		if (AonStringUtils.isNotBlank(secondSurnameStr))
-			onEmployeeSecondSurnameChange(secondSurnameStr);
+		onEmployeeSecondSurnameChange(secondSurnameStr);
 	}
 
 	@UiHandler("ssRegimeType")
