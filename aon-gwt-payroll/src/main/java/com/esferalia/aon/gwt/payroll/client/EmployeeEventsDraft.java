4f572e139186e3769c2b32fc0939e171e97e9595
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2024-10-16 18:54:49 +0200

    EmployeeEventsDraft dates from start contract to next year

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeEventsDraft.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/EmployeeEventsDraft.java
@@ -522,25 +522,21 @@ public abstract class EmployeeEventsDraft extends Composite implements ContextMe
 	}
 	
 	public void initializeYearLB(ListBox yearLB) {
-		year = DateUtils.getYear();
+		Integer iteratorYear = DateUtils.getYear() + 1;
+		Integer contractStartYear = DateUtils.getYear(employeeEventsDraft.getContractStartDate());
 		
 		yearLB.clear();
 		
-		Integer yearAux = DateUtils.getYear();
-		Integer previusYearIII = year - 3;
-		Integer previusYearII = year - 2;
-		Integer previusYear = year - 1;
-		Integer nextYear = year + 1;
-		
-		yearLB.addItem(nextYear.toString(), nextYear.toString());
-		yearLB.addItem(yearAux.toString(), yearAux.toString());
-		yearLB.addItem(previusYear.toString(), previusYear.toString());
-		yearLB.addItem(previusYearII.toString(), previusYearII.toString());
-		yearLB.addItem(previusYearIII.toString(), previusYearIII.toString());
+		while (iteratorYear >= contractStartYear) {
+			yearLB.addItem(iteratorYear.toString(), iteratorYear.toString());
+			iteratorYear--;
+		}
 		
 		yearLB.addChangeHandler(e -> changeYear());
 		
-		setSelectedValueLB(yearLB, year+"");
+		setSelectedValueLB(yearLB, DateUtils.getYear()+"");
+		
+		year = DateUtils.getYear();
 		year = year - 1900;
 		
 	}
