b0ab564399e4643f377993af566575f39928fb03
anderibz <aibanez@aonsolutions.es>
2024-10-22 18:45:53 +0200

    invoice list changes.

--- aon-solutions/src/main/webapp/modules/invoice/aon-invoice-list.js
+++ aon-solutions/src/main/webapp/modules/invoice/aon-invoice-list.js
@@ -8,7 +8,7 @@ import { Invoice, getDocumentNumber } from './Invoice.js';
 
 import {addInvoices, setInvoices, setIndex} from './InvoiceCache.js';
 
-import { CONSTANT, MATERIAL_ICONS, MSG, TAG } from '../../environments/environments.js';
+import { COLORS, CONSTANT, MATERIAL_ICONS, MSG, TAG } from '../../environments/environments.js';
 
 import * as ACTION from '../actions.js';
 import { formatNumber, isBase64 } from '../../services/utils.js';
@@ -58,7 +58,7 @@ export class AonInvoiceList extends AonElement {
 			aonInvoiceTable.addColumn(MSG.INVOICE_NUMBER, 'string', 'reference', '150px');
 			aonInvoiceTable.addColumn(MSG.HOLDER, 'string', 'name', 'auto');
 			aonInvoiceTable.addColumn(MSG.AMOUNT, 'number', 'totalParse', '100px');
-			aonInvoiceTable.addColumn('', 'icons', 'icons', '100px');
+			aonInvoiceTable.addColumn('', 'icons', 'icons', this.isInvoice() ? '100px' : '140px');
 	
 			this.init();
 			aonInvoiceTable.addEventListener('more', () => {
@@ -197,6 +197,15 @@ export class AonInvoiceList extends AonElement {
 				icon: this.getOcrInvoiceStatusIcon(),
 				title: this.getOcrInvoiceStatusIconTitle(invoice),
 				color: this.getOcrInvoiceStatusIconColor(invoice)
+			};
+			icons.push(icon);
+		}
+
+		if(inv.isRawdoc()) {
+			let icon = {
+				icon: MATERIAL_ICONS.INFO,
+				title: invoice.creation_date + ' - ' + invoice.creation_user,
+				color: COLORS.AON_LIGHT_GRAY
 			};
 			icons.push(icon);
 		}
@@ -457,6 +466,10 @@ export class AonInvoiceList extends AonElement {
 
 	isProcessing() {
 		return this.getFilter().status &&  this.getFilter().status === 'processing';
+	}
+
+	isInvoice() {
+		return this.getFilter().status &&  this.getFilter().status === 'accounting';
 	}
 
 	getPaymethod(paymethod) {
