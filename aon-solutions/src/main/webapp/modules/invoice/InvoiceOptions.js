2cc2eefb102bf090d4c7ff44b9a058ece75a71b7
anderibz <aibanez@aonsolutions.es>
2024-10-29 11:44:05 +0100

    gwt dialog in portal fix.

--- aon-solutions/src/main/webapp/modules/invoice/InvoiceOptions.js
+++ aon-solutions/src/main/webapp/modules/invoice/InvoiceOptions.js
@@ -3,6 +3,7 @@ import * as GWT from "../../gwt/gwt.js";
 
   export const gwtLoad = (option) => {
     let application = document.querySelector(TAG.AON_APPLICATION);
+    document.body.classList.add('gwt-Selector');
     GWT.load(option, application.CONTENT);
   }
 
