2128ff302851f15c7e7bc0d88c0dad85f28d1569
anderibz <aibanez@aonsolutions.es>
2024-11-26 12:18:54 +0100

    SES send mail fix.

--- aon-aws/src/main/java/solutions/aon/aws/ses/SES.java
+++ aon-aws/src/main/java/solutions/aon/aws/ses/SES.java
@@ -24,12 +24,8 @@ import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sesv2.SesV2Client;
-import software.amazon.awssdk.services.sesv2.model.EmailContent;
-import software.amazon.awssdk.services.sesv2.model.GetEmailIdentityRequest;
 import software.amazon.awssdk.services.sesv2.model.GetEmailIdentityResponse;
-import software.amazon.awssdk.services.sesv2.model.RawMessage;
 import software.amazon.awssdk.services.sesv2.model.SendEmailRequest;
-import software.amazon.awssdk.services.sesv2.model.VerificationStatus;
 
 public class SES {
 
@@ -78,6 +74,63 @@ public class SES {
 			// Create a new MimeMessage object.
 			MimeMessage message = new MimeMessage(session);
 
+			// Add subject, from and to lines.
+        	message.setSubject(msg.getSubject(), "UTF-8");
+        
+        	if(!isVerifiedForSendingStatus(msg.getFrom())) {
+        		msg.setFrom(null);
+        	}
+        	
+        	message.setFrom(new InternetAddress(msg.getAliasFrom()));
+        
+        	String to =  String.join(",", msg.getTo().toArray(String[]::new));
+        	message.setRecipients(jakarta.mail.Message.RecipientType.TO, InternetAddress.parse(to));
+        	
+        	String bcc =  String.join(",", msg.getBcc().toArray(String[]::new));
+        	message.setRecipients(jakarta.mail.Message.RecipientType.BCC, InternetAddress.parse(bcc));
+
+        	String cc =  String.join(",", msg.getCc().toArray(String[]::new));
+        	message.setRecipients(jakarta.mail.Message.RecipientType.CC, InternetAddress.parse(cc));
+        	
+        	if(msg.isReplyTo()) {
+            	message.setReplyTo(InternetAddress.parse(msg.getReplyTo()));
+        	}
+        	
+        	// Create a multipart/alternative child container.
+        	MimeMultipart msgBody = new MimeMultipart("alternative");
+        
+        	// Create a wrapper for the HTML and text parts.        
+        	MimeBodyPart wrap = new MimeBodyPart();
+                
+        	// Define the HTML part.
+        	MimeBodyPart htmlPart = new MimeBodyPart();
+        	htmlPart.setContent(msg.getBody(),"text/html; charset=UTF-8");
+                
+        	// Add the text and HTML parts to the child container.
+        	msgBody.addBodyPart(htmlPart);
+        
+        	// Add the child container to the wrapper object.
+        	wrap.setContent(msgBody);
+        
+        	// Create a multipart/mixed parent container.
+        	MimeMultipart multipart = new MimeMultipart("mixed");
+        
+        	// Add the parent container to the message.
+        	message.setContent(multipart);
+        
+        	// Add the multipart/alternative part to the message.
+        	multipart.addBodyPart(wrap);
+        
+        	for (File file : msg.getFiles()) {
+        		// Define the attachment
+        		MimeBodyPart att = new MimeBodyPart(); 
+        		DataSource bds = new FileDataSource(file);
+        		att.setDataHandler(new DataHandler(bds)); 
+        		att.setFileName(bds.getName());             
+        		// Add the attachment to the message.
+        		multipart.addBodyPart(att);
+        	}
+			
             // Instantiate an Amazon SES client, which will make the service 
             // call with the supplied AWS credentials.
             
