dd84d3d82f00f261b63d65f264e1223ee8f0e199
Raúl Trepiana <rtrepiana@aonsolutions.es>
2024-12-27 20:47:39 +0100

    2025 MEI 0.80 ( 0.13 & 0.67 )

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -24,6 +24,7 @@ import net.aonsolutions.db.up2date.fiscal.AlterFsMod193Detail2024;
 import net.aonsolutions.db.up2date.fiscal.FsModel369Creation;
 import net.aonsolutions.db.up2date.payroll.LackPayInsert;
 import net.aonsolutions.db.up2date.tgss.FPPercentage2024Fix;
+import net.aonsolutions.db.up2date.tgss.MEI2025Insert;
 import net.aonsolutions.db.up2date.tgss.SolidarityInsert;
 import net.aonsolutions.db.up2date.tgss.TrainingExcessDeductions2024Insert;
 
@@ -491,9 +492,11 @@ public class Up2Date {
     		
     	//LackPayInsert.LACKPAYINSERT
     		
-    	TrainingExcessDeductions2024Insert.TRAININGEXCESSDEDUCTIONS2024INSERT,
-    	FPPercentage2024Fix.FPPERCENTAGE2024FIX,
-    	AlterFsMod190Detail2024.ALTER_FS_MODEL_190_DETAIL_2024
+    	//TrainingExcessDeductions2024Insert.TRAININGEXCESSDEDUCTIONS2024INSERT,
+    	//FPPercentage2024Fix.FPPERCENTAGE2024FIX,
+    	//AlterFsMod190Detail2024.ALTER_FS_MODEL_190_DETAIL_2024
+    		
+    	MEI2025Insert.MEI2025INSERT
 
 	};
 
