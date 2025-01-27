17736aaddedd197b157366589deb8106febaac4a
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-01-27 15:46:46 +0100

    Cost servlet protect against not end date

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/server/CostExcelServlet.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/server/CostExcelServlet.java
@@ -60,8 +60,8 @@ public class CostExcelServlet extends HttpServlet {
 		Integer month;
 		Integer year;
 		
-		Integer monthEnd;
-		Integer yearEnd;
+		Integer monthEnd = null;
+		Integer yearEnd = null;
 		
 		String domainName;
 		String user;
@@ -89,9 +89,11 @@ public class CostExcelServlet extends HttpServlet {
 			month = Integer.parseInt(req.getParameter(MONTH.getName()));
 			year = Integer.parseInt(req.getParameter(YEAR.getName()));
 			
-			monthEnd = Integer.parseInt(req.getParameter(MONTH.getName() + "End"));
-			yearEnd = Integer.parseInt(req.getParameter(YEAR.getName() + "End"));
-			
+			try {
+				monthEnd = Integer.parseInt(req.getParameter(MONTH.getName() + "End"));
+				yearEnd = Integer.parseInt(req.getParameter(YEAR.getName() + "End"));
+			} catch (Exception e) {}
+				
 			excelType = com.esferalia.aon.in.payroll.excel.ExcelType.valueOf(req.getParameter(EXCEL_TYPE.getName()));
 			
 			domainName = req.getParameter(DOMAIN.getName());
@@ -115,14 +117,19 @@ public class CostExcelServlet extends HttpServlet {
 		calendar.set(Calendar.SECOND, 0);
 		Date startDate = calendar.getTime();
 		
-		Calendar calendarEnd = Calendar.getInstance();
-		calendarEnd.set(Calendar.YEAR, yearEnd);
-		calendarEnd.set(Calendar.MONTH, monthEnd);
-		calendarEnd.set(Calendar.DAY_OF_MONTH, 1); // The first day of the month has value 1.
-		calendarEnd.set(Calendar.HOUR, 0);
-		calendarEnd.set(Calendar.MINUTE, 0);
-		calendarEnd.set(Calendar.SECOND, 0);
-		Date endDate = AonDateUtils.getMonthLastDay(calendarEnd.getTime());
+		Date endDate;
+		if(null == yearEnd) {
+			endDate = AonDateUtils.getMonthLastDay(startDate);
+		} else {
+			Calendar calendarEnd = Calendar.getInstance();
+			calendarEnd.set(Calendar.YEAR, yearEnd);
+			calendarEnd.set(Calendar.MONTH, monthEnd);
+			calendarEnd.set(Calendar.DAY_OF_MONTH, 1); // The first day of the month has value 1.
+			calendarEnd.set(Calendar.HOUR, 0);
+			calendarEnd.set(Calendar.MINUTE, 0);
+			calendarEnd.set(Calendar.SECOND, 0);
+			endDate = AonDateUtils.getMonthLastDay(calendarEnd.getTime());
+		}
 		
 		resp.setContentType(MimeType.MS_EXCEL.getName());
 		String fileName = "Costes_" + enterpriseName + "_" + (AonStringUtils.isBlank(workplaceName) ? "" : workplaceName + "_") + formatter.format(startDate) + "." +  MimeType.MS_EXCEL_2007.getExtension();
