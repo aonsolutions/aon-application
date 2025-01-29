bcb83b0a61680242ecc9843412b45b3df1075bbe
EukeCastellano <ecastellano@aonsolutions.es>
2025-01-29 15:19:18 +0100

    [DBUpdate] Remake Takara Fix

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,6 +19,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import net.aonsolutions.db.up2date.finance.RemakeTakaraFix;
 import net.aonsolutions.db.up2date.fiscal.AlterFsMod190Detail2024;
 import net.aonsolutions.db.up2date.fiscal.AlterFsMod193Detail2024;
 import net.aonsolutions.db.up2date.fiscal.FsModel369Creation;
@@ -506,7 +507,8 @@ public class Up2Date {
 			
 			// SalaryHoursFixIII.SALARYHOURSFIXIII
 			
-			MEI2025Rollback.MEI2025ROLLBACK
+			// MEI2025Rollback.MEI2025ROLLBACK,
+			RemakeTakaraFix.REMAKETAKARAFIX
 	};
 
 	// ------------------------------------------------------------------------
