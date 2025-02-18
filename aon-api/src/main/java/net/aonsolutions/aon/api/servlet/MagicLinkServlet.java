da80d60a604cbd025aadbe7d45ad4c96849f8391
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-02-18 19:45:27 +0100

    Keep MagicLinkServlet simple

--- aon-api/src/main/java/net/aonsolutions/aon/api/servlet/MagicLinkServlet.java
+++ aon-api/src/main/java/net/aonsolutions/aon/api/servlet/MagicLinkServlet.java
@@ -1,15 +1,7 @@
 package net.aonsolutions.aon.api.servlet;
 
-import static com.esferalia.aon.occam.api.model.attachment.AttachType.REGISTRY;
-import static com.esferalia.aon.occam.api.model.attachment.RegistryAttachmentType.LOGO;
-import static com.esferalia.aon.occam.api.model.attachment.RegistryAttachmentType.SIGNATURE;
-
 import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Base64;
 import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.velocity.Template;
@@ -19,24 +11,12 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.json.JSONObject;
 
-import com.esferalia.aon.occam.api.AON;
-import com.esferalia.aon.occam.api.AONContext;
-import com.esferalia.aon.occam.api.AONContext.CloseableAONContext;
 import com.esferalia.aon.occam.api.AON_SOLUTIONS;
-import com.esferalia.aon.occam.api.SECURITY;
 import com.esferalia.aon.occam.api.json.JsonUtils;
-import com.esferalia.aon.occam.api.model.Company;
-import com.esferalia.aon.occam.api.model.Domain;
 import com.esferalia.aon.occam.api.model.IJsonNames;
 import com.esferalia.aon.occam.api.model.aonsolutions.AonToken;
-import com.esferalia.aon.occam.api.model.aonsolutions.DomainUserRoles;
-import com.esferalia.aon.occam.api.model.attachment.Attach;
-import com.esferalia.aon.occam.api.model.registry.RegistryMedia;
 import com.esferalia.aon.occam.api.model.security.Auth;
-import com.esferalia.aon.occam.api.model.security.User;
-import com.esferalia.aon.occam.impl.jooq.dao.DomainDAO;
 import com.esferalia.aon.watson.server.AonDateUtils;
-import com.esferalia.aon.watson.util.AonStringUtils;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -90,47 +70,17 @@ public class MagicLinkServlet extends AonApiHttpServlet {
 			String token = AonToken.build(auth, expireDate);
 			String magicLink = "https://" + url + "?token=" + token; 
 			
-			
-			List<User> users = getUsersByEmail(email);
-			if(!users.isEmpty() && users.size() == 1) {
-				User user = users.get(0);
-				Company cp = AON.getCompany(user.getDomain().getName(), user.getDomain().getId(), "", 
-						f -> f.getDomainProperty().eq(user.getDomain().getId()));
-				String companyName = cp.getName().toUpperCase();
+			sendGmail(auth, magicLink, expireDate);
 				
-				sendGmail(auth, magicLink, expireDate, companyName, user);
-			} else
-				sendGmail(auth, magicLink, expireDate);
 		} else throw new AonApiException(AonApiError.NOT_VALID_EMAIL.getMessage());
-	}
-	
-	private List<User> getUsersByEmail(String email){
-		List<User> users = new ArrayList<User>();
-		List<String> schemas = AONContext.getSchemas();
-		
-		for(String schema : schemas) {
-			String domain = AONContext.getSchemaFirstDomain(schema);
-			if(!AonStringUtils.isBlank(domain)){
-				users.addAll( AON_SOLUTIONS.getUsersByEmail(domain, 0, email) );
-			}
-		}
-		
-		return users;
 	}
 	
 	public void sendGmail(Auth auth, String magicLink, Date expireDate) {
 		SESMessage msg = new SESMessage()
 			.setTo(auth.getEmail())
+			.setFrom(AON_FROM)
 			.setSubject("MAGIC LINK | " + "AON SOLUTIONS")
 			.setBody(getContent(auth, magicLink, expireDate));
-		SES.sendEmail(msg);
-	}
-	
-	public void sendGmail(Auth auth, String magicLink, Date expireDate, String companyName, User user ) {
-		SESMessage msg = new SESMessage()
-			.setTo(auth.getEmail())
-			.setSubject("MAGIC LINK | " + companyName)
-			.setBody(getContent(user, auth, magicLink, expireDate));
 		SES.sendEmail(msg);
 	}
 	
@@ -140,15 +90,13 @@ public class MagicLinkServlet extends AonApiHttpServlet {
         engine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
         engine.init();
         
-        String from = AON_FROM;
-        
         VelocityContext context = new VelocityContext();
         context.put("logo", AON_LOGO);
 		context.put("parentName", "AON SOLUTIONS");
 		context.put("name", auth.getName());
         context.put("expireDate", AonDateUtils.format(expireDate, "dd/MM/yyyy HH:mm"));
         context.put("magicLink", magicLink);
-		context.put("contact", AonStringUtils.isBlank(from) ? "booking@aonsolutions.es" : from);
+		context.put("contact", "booking@aonsolutions.es");
         
         Template template = engine.getTemplate("/net/aonsolutions/aon/api/servlet/templates/magic_link.vm");
         
@@ -157,83 +105,4 @@ public class MagicLinkServlet extends AonApiHttpServlet {
 
         return writer.toString();
     }
-	
-	private String getContent(User user, Auth auth, String magicLink, Date expireDate) {
-        VelocityEngine engine = new VelocityEngine();
-        engine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        engine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
-        engine.init();
-        
-        String from = getFromMessage(user);
-        
-        VelocityContext context = new VelocityContext();
-		context.put("logo", getLogoUrl(user.getDomain()));
-		context.put("parentName", user.getDomain().getDescription());
-		context.put("name", auth.getName());
-        context.put("expireDate", AonDateUtils.format(expireDate, "dd/MM/yyyy HH:mm"));
-        context.put("magicLink", magicLink);
-		context.put("contact", AonStringUtils.isBlank(from) ? "booking@aonsolutions.es" : from);
-        
-        Template template = engine.getTemplate("/net/aonsolutions/aon/api/servlet/templates/magic_link.vm");
-        
-        StringWriter writer = new StringWriter();
-        template.merge(context, writer);
-
-        return writer.toString();
-    }
-	
-	private static String getLogoUrl(Domain domain) {
-		String logoUrl = AON_LOGO;
-		
-		try (CloseableAONContext aonContext = AONContext.getAONContext(domain.getName(), 0, "")) {
-			Company company = AON.getCompany(domain, new User().setLogin(""), f -> f.getDomainProperty().eq(domain.getId()));
-
-			Attach attach = getLogoAttach(aonContext, company.getId());
-
-			String str = "domain=" + attach.getDomain().getId() + "&id=" + attach.getId() + "&attach_type=registry";
-			String result = Base64.getEncoder().encodeToString(str.getBytes(StandardCharsets.UTF_8));
-
-			Domain attachDomain = DomainDAO.getDomain(aonContext, attach.getDomain().getId());
-			logoUrl = "https://" + domain.getName() + "/ms/download_attachment/" + attachDomain.getName() + "/" + attach.getCreationUser() + "/"
-					+ result;
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			logoUrl = AON_LOGO;
-		}
-
-		return logoUrl;
-	}
-
-	private static Attach getLogoAttach(AONContext aonContext, Integer enterpriseId) {
-		Attach attach1 = AON.getAttach(aonContext.getDomainName(), aonContext.getDomainId(), aonContext.getUser(),
-				f -> f.getTypeProperty().eq(SIGNATURE.value()).and(f.getAttachModuleProperty().eq(enterpriseId)),
-				REGISTRY);
-
-		if (attach1 == null || attach1.getData() == null)
-			attach1 = AON.getAttach(aonContext.getDomainName(), aonContext.getDomainId(), aonContext.getUser(),
-					f -> f.getTypeProperty().eq(LOGO.value()).and(f.getAttachModuleProperty().eq(enterpriseId)),
-					REGISTRY);
-
-		return attach1;
-	}
-
-	private static String getFromMessage(User user) {
-		String from = null;
-		
-		DomainUserRoles dur = SECURITY.getDomainUserRoles(user.getDomain(), user.getLogin(), user.getId());
-
-		Domain parentDomain = dur.getDomain().isParent() ? dur.getDomain() : dur.getParentDomain();
-		
-		if(dur.hasCustomView() || dur.hasParentCustomView()) {
-			RegistryMedia emailMedia = AON.getRegistryMedia(user.getDomain(), user,
-					f -> f.getDomainProperty().eq(parentDomain.getId()).and(f.getMediaProperty().eq((byte) 4)));
-			if (null != emailMedia && AonStringUtils.isNotBlank(emailMedia.getValue()))
-				from = emailMedia.getValue();
-		}
-		if(AonStringUtils.isBlank(from)) {
-			from = AON_FROM;
-		}
-		return from;
-	}
 }
