cad2e7b51f88f4d78d66037f13fcfdafa27cdba6
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-02-19 17:44:09 +0100

    User reset password email set BCC, catch posible exceptions, returns from email

--- aon-solutions/src/main/webapp/modules/user/aon-user.js
+++ aon-solutions/src/main/webapp/modules/user/aon-user.js
@@ -529,10 +529,11 @@ export class AonUser extends AonElement {
 		d.setTitle(MSG.SEND);
 		d.setContentHTML('Al notificar los datos de usuario se generará una nueva contraseña.');
 		d.addAcceptAction(() => {
-			sendUserInfoEmail(this.user).then(() => {
-				this.showToast({message: "Se ha enviado un mail a " + this.user.email + " con la contraseña nueva", type: CONSTANT.SUCCESS, delay: 3000});
+			sendUserInfoEmail(this.user).then(success => {
+				this.showToast({message: "Se ha enviado un mail a " + this.user.email + ", desde la cuenta " + success.fromEmail + ", con la contraseña nueva", type: CONSTANT.SUCCESS, delay: 3000});
 			  }, err => {
-				this.showError(err);
+				let jsonError = JSON.parse(err);
+				this.showToast({message: jsonError.message, type: CONSTANT.ERROR, delay: 5000});
 			  });
 		});
 		d.open();
