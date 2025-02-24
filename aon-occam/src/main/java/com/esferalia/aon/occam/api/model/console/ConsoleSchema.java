bb2e690cc739c7655c28297f3b143f5fdf744e8a
EukeCastellano <ecastellano@aonsolutions.es>
2025-02-24 11:03:31 +0100

    CONSOLE. aonsolutions.org added.

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/console/ConsoleSchema.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/console/ConsoleSchema.java
@@ -17,6 +17,7 @@ public enum ConsoleSchema {
 	,ETL			("ETL"		,"etl.aonsolutions.net","console-etl.aonsolutions.net","jgarcia")
 	,UDAPA			("UDAPA"	,"grupo-udapa-aonsolutions-net","console-udapa.aonsolutions.net","jgarcia")
 	,DEMO			("DEMO"		,"demo-aonsolutions-net","console-demos.aonsolutions.net","jgarcia")
+	,SNAPSHOT		("SNAPSHOT" ,"sig-grupo-esferalia","console.aonsolutions.org","jgarcia")
 	
 	// -------------------------------------------
 	// BASES DE DATOS DE DESARROLLO EN MI MÒAQUINA
