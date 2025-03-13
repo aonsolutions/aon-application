7356aa451046277d486860a42bbf7168d06d4fda
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-03-13 10:35:05 +0100

    Fix 'new' compile error. Pass a parameter either like primitive type ( byte, short...) or null it's an error. It's safe pass parameter either like Short, Byte ... or null

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/fiscal/RefreshMod123Result.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/fiscal/RefreshMod123Result.java
@@ -93,7 +93,7 @@ public class RefreshMod123Result implements Update {
 							if (result != null) {
 								dslContext.update(FS_MODEL)
 									.set(FS_MODEL.RESULT,result)
-									.set(FS_MODEL.DECLARATION_TYPE, (declarationType == null? null : declarationType.value()))
+									.set(FS_MODEL.DECLARATION_TYPE, (declarationType == null? null : (Byte)declarationType.value()))
 									.where(FS_MODEL.ID.eq(id))
 									.execute();
 							}
