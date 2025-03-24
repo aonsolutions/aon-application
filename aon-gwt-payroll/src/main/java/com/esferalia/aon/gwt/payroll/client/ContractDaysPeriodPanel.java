fae2022dc0be62373070610f69feeba18e3b85a7
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-03-24 18:09:43 +0100

    ContractDaysPeriodPanel changes, use AonCustomMonthSelect

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/ContractDaysPeriodPanel.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/client/ContractDaysPeriodPanel.java
@@ -3,13 +3,11 @@ package com.esferalia.aon.gwt.payroll.client;
 import java.util.Date;
 
 import com.esferalia.aon.gwt.common.client.AON;
-import com.esferalia.aon.gwt.common.client.widget.MonthListBox;
 import com.esferalia.aon.gwt.common.client.widget.solutions.AonCustomCheckBox;
-import com.esferalia.aon.gwt.common.client.widget.solutions.AonCustomIntegerBox;
 import com.esferalia.aon.gwt.common.client.widget.solutions.AonCustomListBox;
+import com.esferalia.aon.gwt.common.client.widget.solutions.AonCustomMonthSelect;
 import com.esferalia.aon.gwt.common.client.widget.solutions.AonMessagePanel;
 import com.esferalia.aon.gwt.common.shared.DateUtils;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
@@ -20,11 +18,11 @@ public class ContractDaysPeriodPanel extends FlowPanel {
 		void onAccept(Date start, Date end, Boolean extended, Boolean totals);
 	}
 
-	private AonCustomListBox monthStartListBox = new AonCustomListBox("Mes Inicio");
-	private AonCustomIntegerBox yearStartBox = new AonCustomIntegerBox("A\u00f1o Inicio");
+	private AonCustomMonthSelect monthStartListBox = new AonCustomMonthSelect("F. Inicio");
+//	private AonCustomIntegerBox yearStartBox = new AonCustomIntegerBox("A\u00f1o Inicio");
 	
-	private AonCustomListBox monthEndListBox = new AonCustomListBox("Mes Fin");
-	private AonCustomIntegerBox yearEndBox = new AonCustomIntegerBox("A\u00f1o Fin");
+	private AonCustomMonthSelect monthEndListBox = new AonCustomMonthSelect("F. Fin");
+//	private AonCustomIntegerBox yearEndBox = new AonCustomIntegerBox("A\u00f1o Fin");
 	
 	private AonCustomListBox typeListBox = new AonCustomListBox("Tipo");
 	private AonCustomCheckBox totalsCB = new AonCustomCheckBox("Mostrar Totales");
@@ -43,33 +41,17 @@ public class ContractDaysPeriodPanel extends FlowPanel {
 		rootPanel.addStyleName(AON.CSS.aonFlexColumn());
 		rootPanel.setWidth("90%");
 		
-		FlowPanel startPanel = new FlowPanel();
-		startPanel.setStyleName(AON.CSS.aonItemFlex());
-		startPanel.setWidth("100%");
+		Date firstMonth = DateUtils.addYears2Date(DateUtils.getFirstDayOfMonth(new Date()), -3);
+		Date lastMonth = DateUtils.addYears2Date(DateUtils.getFirstDayOfMonth(new Date()), 3);
+		Date defaultDate = DateUtils.getFirstDayOfMonth(new Date());
 		
-		fillMothsListBox(monthStartListBox);
+		monthStartListBox.getAonCustomMonthListBox().setFirstMonth(firstMonth);
+		monthStartListBox.getAonCustomMonthListBox().setLastMonth(lastMonth);
+		monthStartListBox.getAonCustomMonthListBox().setSelectedMonth(defaultDate);
 		
-		yearStartBox.setWidth("10rem");
-		yearStartBox.hideNearBy();
-		yearStartBox.getNumberBox().setMaxLength(4);
-		yearStartBox.getNumberBox().getElement().setPropertyString("placeholder", "A\u00f1o");
-		
-		startPanel.add(monthStartListBox);
-		startPanel.add(yearStartBox);
-		
-		FlowPanel endPanel = new FlowPanel();
-		endPanel.setStyleName(AON.CSS.aonItemFlex());
-		endPanel.setWidth("100%");
-		
-		fillMothsListBox(monthEndListBox);
-		
-		yearEndBox.setWidth("10rem");
-		yearEndBox.hideNearBy();
-		yearEndBox.getNumberBox().setMaxLength(4);
-		yearEndBox.getNumberBox().getElement().setPropertyString("placeholder", "A\u00f1o");
-		
-		endPanel.add(monthEndListBox);
-		endPanel.add(yearEndBox);
+		monthEndListBox.getAonCustomMonthListBox().setFirstMonth(firstMonth);
+		monthEndListBox.getAonCustomMonthListBox().setLastMonth(lastMonth);
+		monthEndListBox.getAonCustomMonthListBox().setSelectedMonth(defaultDate);
 		
 		FlowPanel typePanel = new FlowPanel();
 		typePanel.setStyleName(AON.CSS.aonItemFlex());
@@ -84,8 +66,8 @@ public class ContractDaysPeriodPanel extends FlowPanel {
 		typePanel.add(typeListBox);
 		typePanel.add(totalsCB);
 		
-		rootPanel.add(startPanel);
-		rootPanel.add(endPanel);
+		rootPanel.add(monthStartListBox);
+		rootPanel.add(monthEndListBox);
 		rootPanel.add(typePanel);
 		
 		FlowPanel buttons = new FlowPanel();
@@ -111,35 +93,21 @@ public class ContractDaysPeriodPanel extends FlowPanel {
 		
     	add(rootPanel);	
 	}
-
-	private void fillMothsListBox(AonCustomListBox listBox) {
-		listBox.clearItems();
-		listBox.addItem("Enero", "0");
-		listBox.addItem("Febrero", "1");
-		listBox.addItem("Marzo", "2");
-		listBox.addItem("Abril", "3");
-		listBox.addItem("Mayo", "4");
-		listBox.addItem("Junio", "5");
-		listBox.addItem("Julio", "6");
-		listBox.addItem("Agosto", "7");
-		listBox.addItem("Septiembre", "8");
-		listBox.addItem("Octubre", "9");
-		listBox.addItem("Noviembre", "10");
-		listBox.addItem("Diciembre", "11");	
-	}
 	
 	private Date getStartDate() {
-		if(null == yearStartBox.getValue()) return null;
-		try {
-			return DateUtils.getFirstDayOfMonth( DateUtils.getDate(Integer.parseInt(monthStartListBox.getValue()), yearStartBox.getValue()) );
-		} catch (Exception e) { return null; }
+		return monthStartListBox.getAonCustomMonthListBox().getSelected();
+//		if(null == yearStartBox.getValue()) return null;
+//		try {
+//			return DateUtils.getFirstDayOfMonth( DateUtils.getDate(Integer.parseInt(monthStartListBox.getValue()), yearStartBox.getValue()) );
+//		} catch (Exception e) { return null; }
 	}
 	
 	private Date getEndDate() {
-		if(null == yearEndBox.getValue()) return null;
-		try {
-			return DateUtils.getLastDayOfMonth( DateUtils.getDate(Integer.parseInt(monthEndListBox.getValue()), yearEndBox.getValue()) );
-		} catch (Exception e) { return null; }
+		return null == monthEndListBox.getAonCustomMonthListBox().getSelected() ? null : DateUtils.getLastDayOfMonth(monthEndListBox.getAonCustomMonthListBox().getSelected());
+//		if(null == yearEndBox.getValue()) return null;
+//		try {
+//			return DateUtils.getLastDayOfMonth( DateUtils.getDate(Integer.parseInt(monthEndListBox.getValue()), yearEndBox.getValue()) );
+//		} catch (Exception e) { return null; }
 	}
 
 }
