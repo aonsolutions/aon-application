b920abd3f4b7b8668bff66e8aa62bc1d95fd019e
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-03-24 11:35:50 +0100

    Update some dependencies to latest releases.

--- aon-payroll/src/main/java/com/esferalia/aon/payroll/agreement/AgreementParser.java
+++ aon-payroll/src/main/java/com/esferalia/aon/payroll/agreement/AgreementParser.java
@@ -1205,7 +1205,7 @@ public class AgreementParser {
 			
 			
 			String agreementCode = cmd.getOptionValue(agreementCodeOpt.getLongOpt());
-			String domainIdStr = cmd.getOptionValue(domainOpt.getLongOpt(), null);
+			String domainIdStr = cmd.getOptionValue(domainOpt.getLongOpt(), (String) null);
 			
 			if(null != domainIdStr)
 				domainId = Integer.parseInt(domainIdStr);
