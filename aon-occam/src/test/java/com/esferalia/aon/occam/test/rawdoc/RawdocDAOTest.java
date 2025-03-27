8a8b60dc2897444835d577e29e3d2c4af9040f53
EukeCastellano <ecastellano@aonsolutions.es>
2025-03-27 10:55:35 +0100

    Fix rawdoc tests.

--- aon-occam/src/test/java/com/esferalia/aon/occam/test/rawdoc/RawdocDAOTest.java
+++
@@ -1,5 +0,0 @@
-package com.esferalia.aon.occam.test.rawdoc;
-
-public class RawdocDAOTest {
-
-}
--- aon-occam/src/test/java/com/esferalia/aon/occam/test/rawdoc/RawdocTestSuite.java
+++ aon-occam/src/test/java/com/esferalia/aon/occam/test/rawdoc/RawdocTestSuite.java
@@ -7,7 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	RawdocValidationTest.class,
-	RawdocDAOTest.class,
 })
 public class RawdocTestSuite {
 
