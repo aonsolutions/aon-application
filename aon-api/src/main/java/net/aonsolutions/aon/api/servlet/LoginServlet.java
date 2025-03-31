8062c53bda15baf56129b086ef32f4cb3d3949b4
anderibz <aibanez@aonsolutions.es>
2025-03-31 13:36:21 +0200

    magic link fix.

--- aon-api/src/main/java/net/aonsolutions/aon/api/servlet/LoginServlet.java
+++ aon-api/src/main/java/net/aonsolutions/aon/api/servlet/LoginServlet.java
@@ -133,11 +133,9 @@ public class LoginServlet extends AonApiHttpServlet{
 				} 
 				ok = true;
 			} else {
-				//String domainName = req.getServerName();
-				
-				// Ya que tenemos el primer dominio del schema, lo usamos
-				String domainName = aonToken.getSchemaFirstDomain();
+				String domainName = req.getServerName();
 				Domain domain = AON_SOLUTIONS.getDomain(domainName);
+				
 				if ( domain != null && Objects.equals(domain.getName(), domainName )) {
 					User user = AON_SOLUTIONS.getUser(domain, token);
 					ok = user != null && AonStringUtils.equalsIgnoreCase(user.getAuth().getUuid(), aonToken.getUuid());
@@ -237,6 +235,16 @@ public class LoginServlet extends AonApiHttpServlet{
 	
 	private static Integer[] arrayOf( Integer ...ts ) {
 		return Arrays.stream(ts).filter(Objects::nonNull).toArray(Integer[]::new);
+	}
+	
+	public static void main(String[] args) {
+		String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInN1YiI6IntcInNjaGVtYVwiOlwic3VpdGUtYW9uc29sdXRpb25zLW9yZ1wiLFwic2NoZW1hX2ZpcnN0X2RvbWFpblwiOlwiMTYxOTA4NjlsLmFvbnNvbHV0aW9ucy5vcmdcIixcInV1aWRcIjpcIjRENkNGOEExNkRERjExRUJCMzAwMDJCOUY2QzVDNzhEXCJ9IiwiaWF0IjoxNzQzNDE3ODYxLCJleHAiOjE3NDM0MTg3NjF9._m_GPR9NzbdYttGwdq_LfhuDDQppuEnuz3oLF_EHuIk";
+		String token2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoMCIsInN1YiI6IntcInV1aWRcIjpcIjRENkNGOEExNkRERjExRUJCMzAwMDJCOUY2QzVDNzhEXCJ9IiwiaWF0IjoxNzQzNDE4MTA1LCJleHAiOjE3NTEyODA1MDV9.tj6RNNf77pWhVAao4yzKUi7vIxLZZm3a0XVrER9K6qI";
+		JSONObject json = SECURITY.decodeJWT(token, AonSecret.getAonSecret());
+		JSONObject json2 = SECURITY.decodeJWT(token2, AonSecret.getAonSecret());
+		System.out.println(json.toString());
+		System.out.println(json2.toString());
+		
 	}
 	
 
