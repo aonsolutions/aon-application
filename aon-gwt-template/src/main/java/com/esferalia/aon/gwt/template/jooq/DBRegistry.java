270d6e0ad64c94102aaf91eafd5dcdb4ffb6f4b2
anderibz <aibanez@aonsolutions.es>
2025-04-15 11:08:13 +0200

    registry excel add zip, province & country.

--- aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/jooq/DBRegistry.java
+++ aon-gwt-template/src/main/java/com/esferalia/aon/gwt/template/jooq/DBRegistry.java
@@ -1,7 +1,9 @@
 package com.esferalia.aon.gwt.template.jooq;
 
-import static com.esferalia.aon.jooq.tables.Customer.CUSTOMER;
 import static com.esferalia.aon.jooq.tables.Creditor.CREDITOR;
+import static com.esferalia.aon.jooq.tables.Customer.CUSTOMER;
+import static com.esferalia.aon.jooq.tables.Geotree.GEOTREE;
+import static com.esferalia.aon.jooq.tables.Geozone.GEOZONE;
 import static com.esferalia.aon.jooq.tables.Raddress.RADDRESS;
 import static com.esferalia.aon.jooq.tables.Registry.REGISTRY;
 import static com.esferalia.aon.jooq.tables.Supplier.SUPPLIER;
@@ -22,6 +24,7 @@ import com.esferalia.aon.occam.api.model.registry.Supplier;
 import com.esferalia.aon.occam.api.model.registry.SupplierFull;
 import com.esferalia.aon.occam.api.model.security.User;
 import com.esferalia.aon.occam.api.model.type.StreetType;
+import com.esferalia.aon.occam.impl.jooq.dao.GeoZoneDAO.GeoZoneFiller;
 
 public class DBRegistry {
 	
@@ -31,11 +34,17 @@ public class DBRegistry {
 	
 	public static List<CustomerFull> getCustomers(Domain domain, User user) {
 		try (CloseableAONContext ctx = AONContext.getAONContext(domain, user)){
+			com.esferalia.aon.jooq.tables.Geozone parent = GEOZONE.as("parentGeozone");
+			com.esferalia.aon.jooq.tables.Geozone child = GEOZONE.as("childGeozone");
+			
 			HashMap<Integer, CustomerFull> map = new HashMap<>();
 			ctx.getDslContext().select()
 				.from(REGISTRY)
 				.join(CUSTOMER).on(REGISTRY.ID.eq(CUSTOMER.REGISTRY))
 				.join(RADDRESS).on(REGISTRY.ID.eq(RADDRESS.REGISTRY))
+				.leftOuterJoin(child).on(child.ID.eq(RADDRESS.GEOZONE))
+				.leftOuterJoin(GEOTREE).on(GEOTREE.CHILD.eq(RADDRESS.GEOZONE))
+				.leftOuterJoin(parent).on(parent.ID.eq(GEOTREE.PARENT))
 				.where(CUSTOMER.DOMAIN.eq(domain.getId()))
 				.fetch().stream().forEach(r -> {
 					
@@ -66,8 +75,12 @@ public class DBRegistry {
 							.setZip(r.getValue(RADDRESS.ZIP))
 							.setCity(r.getValue(RADDRESS.CITY))
 							.setGeozone(r.getValue(RADDRESS.GEOZONE))
+							.setGeozoneCode(r.getValue(child.CODE))
+							.setGeozoneName(r.getValue(child.NAME))
+							.setChild(GeoZoneFiller.build(r, child))
+							.setParent(GeoZoneFiller.build(r, parent))						
 							.setAlias(r.getValue(RADDRESS.ALIAS))
-							.setMunicipalityCode(r.getValue(RADDRESS.MUNICIPALITY_CODE));
+							.setMunicipalityCode(r.getValue(RADDRESS.MUNICIPALITY_CODE));					
 					
 					map.get(id).addAddress(address);
 				});
@@ -78,11 +91,17 @@ public class DBRegistry {
 	
 	public static List<SupplierFull> getSuppliers(Domain domain, User user) {
 		try (CloseableAONContext ctx = AONContext.getAONContext(domain, user)){
+			com.esferalia.aon.jooq.tables.Geozone parent = GEOZONE.as("parentGeozone");
+			com.esferalia.aon.jooq.tables.Geozone child = GEOZONE.as("childGeozone");
+			
 			HashMap<Integer, SupplierFull> map = new HashMap<>();
 			ctx.getDslContext().select()
 				.from(REGISTRY)
 				.join(SUPPLIER).on(REGISTRY.ID.eq(SUPPLIER.REGISTRY))
 				.join(RADDRESS).on(REGISTRY.ID.eq(RADDRESS.REGISTRY))
+				.leftOuterJoin(child).on(child.ID.eq(RADDRESS.GEOZONE))
+				.leftOuterJoin(GEOTREE).on(GEOTREE.CHILD.eq(RADDRESS.GEOZONE))
+				.leftOuterJoin(parent).on(parent.ID.eq(GEOTREE.PARENT))
 				.where(SUPPLIER.DOMAIN.eq(domain.getId()))
 				.fetch().stream().forEach(r -> {
 					
@@ -113,6 +132,10 @@ public class DBRegistry {
 							.setZip(r.getValue(RADDRESS.ZIP))
 							.setCity(r.getValue(RADDRESS.CITY))
 							.setGeozone(r.getValue(RADDRESS.GEOZONE))
+							.setGeozoneCode(r.getValue(child.CODE))
+							.setGeozoneName(r.getValue(child.NAME))
+							.setChild(GeoZoneFiller.build(r, child))
+							.setParent(GeoZoneFiller.build(r, parent))
 							.setAlias(r.getValue(RADDRESS.ALIAS))
 							.setMunicipalityCode(r.getValue(RADDRESS.MUNICIPALITY_CODE));
 					
@@ -125,11 +148,17 @@ public class DBRegistry {
 	
 	public static List<CreditorFull> getCreditors(Domain domain, User user) {
 		try (CloseableAONContext ctx = AONContext.getAONContext(domain, user)){
+			com.esferalia.aon.jooq.tables.Geozone parent = GEOZONE.as("parentGeozone");
+			com.esferalia.aon.jooq.tables.Geozone child = GEOZONE.as("childGeozone");
+			
 			HashMap<Integer, CreditorFull> map = new HashMap<>();
 			ctx.getDslContext().select()
 				.from(REGISTRY)
 				.join(CREDITOR).on(REGISTRY.ID.eq(CREDITOR.REGISTRY))
 				.join(RADDRESS).on(REGISTRY.ID.eq(RADDRESS.REGISTRY))
+				.leftOuterJoin(child).on(child.ID.eq(RADDRESS.GEOZONE))
+				.leftOuterJoin(GEOTREE).on(GEOTREE.CHILD.eq(RADDRESS.GEOZONE))
+				.leftOuterJoin(parent).on(parent.ID.eq(GEOTREE.PARENT))
 				.where(CREDITOR.DOMAIN.eq(domain.getId()))
 				.fetch().stream().forEach(r -> {
 					
@@ -160,6 +189,10 @@ public class DBRegistry {
 							.setZip(r.getValue(RADDRESS.ZIP))
 							.setCity(r.getValue(RADDRESS.CITY))
 							.setGeozone(r.getValue(RADDRESS.GEOZONE))
+							.setGeozoneCode(r.getValue(child.CODE))
+							.setGeozoneName(r.getValue(child.NAME))
+							.setChild(GeoZoneFiller.build(r, child))
+							.setParent(GeoZoneFiller.build(r, parent))
 							.setAlias(r.getValue(RADDRESS.ALIAS))
 							.setMunicipalityCode(r.getValue(RADDRESS.MUNICIPALITY_CODE));
 					
