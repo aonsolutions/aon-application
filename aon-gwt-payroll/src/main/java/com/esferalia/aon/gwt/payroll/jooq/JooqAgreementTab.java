6862922ac3dc156099f4e29f18ed7fb502ff5034
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-05-01 21:28:06 +0200

    JooqAgreementIntegrity & JooqAgreementTab if agreement_payment or contract_payment same info as payment_concept set null to agreement_payment or contract_payment

--- aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqAgreementTab.java
+++ aon-gwt-payroll/src/main/java/com/esferalia/aon/gwt/payroll/jooq/JooqAgreementTab.java
@@ -684,6 +684,9 @@ public class JooqAgreementTab {
 					.set(AGREEMENT_PAYMENT.MONTH, AonNumberUtils.toByte(payment.getMonth()))
 					.set(AGREEMENT_PAYMENT.SALARY_TYPE, AonEnumUtils.getByte(payment.getSalaryType()))
 					.set(AGREEMENT_PAYMENT.TYPE, DSL.castNull(AGREEMENT_PAYMENT.TYPE))
+//					.set(AGREEMENT_PAYMENT.DESCRIPTION, DSL.castNull(AGREEMENT_PAYMENT.DESCRIPTION))
+					.set(AGREEMENT_PAYMENT.IRPF_EXPRESSION, DSL.castNull(AGREEMENT_PAYMENT.IRPF_EXPRESSION))
+					.set(AGREEMENT_PAYMENT.QUOTE_EXPRESSION, DSL.castNull(AGREEMENT_PAYMENT.IRPF_EXPRESSION))
 					;
 				
 				if(AonStringUtils.isNotBlank(payment.getExpression()) && AonStringUtils.containsIgnoreCase(payment.getExpression(), "DISABLE"))
