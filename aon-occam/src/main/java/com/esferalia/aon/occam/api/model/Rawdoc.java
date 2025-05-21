8d3868743d6a1b2b1c326c1de5a997e95c3508c7
EukeCastellano <ecastellano@aonsolutions.es>
2025-05-21 18:45:26 +0200

    Rawdoc Module. Processed folder & basic filter.

--- aon-occam/src/main/java/com/esferalia/aon/occam/api/model/Rawdoc.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/api/model/Rawdoc.java
@@ -201,8 +201,8 @@ public class Rawdoc implements Serializable, HasAudit {
 	}
 	
 	public boolean isInbox() 		{return (this.getStatus() == RawdocStatus.INBOX);}
-	public boolean isProcessed() 		{return (this.getStatus() == RawdocStatus.PROCESSED);}
-	public boolean isRecordable() 	{return (this.isInbox() || isProcessed());}	
+	public boolean isProcessed() 	{return (this.getStatus() == RawdocStatus.PROCESSED);}
+	public boolean isRecordable() 	{return isInbox() || isProcessed();}	
 	public boolean isRejected() 	{return (this.getStatus() == RawdocStatus.REJECTED);}
 	public boolean isDraft() 		{return (this.getStatus() == RawdocStatus.DRAFT);}
 }
