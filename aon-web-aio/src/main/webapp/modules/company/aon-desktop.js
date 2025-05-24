dd11f51be38eb2253467779e421080e24da13c3b
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-05-25 01:37:15 +0200

    Limit JSF graphics & new roles only for Beta environments. Sometimes are failing :-(

--- aon-web-aio/src/main/webapp/modules/company/aon-desktop.js
+++ aon-web-aio/src/main/webapp/modules/company/aon-desktop.js
@@ -519,7 +519,7 @@ export class AonDesktop extends AonElement {
 		// Accounting
 		// ------------------------------------------------
 		let aonJsfAccountingGraphCard ;
-		if (this.getDur().isAccountingManager()) {
+		if (this.getDur().isAccountingManager() && this.isBeta() ) {
 			let accountingGraphCard = new AonCard();
 			accountingGraphCard.classList.add(CSS.AON_DASHBOARD_CARD);
 			accountingGraphCard.id = "accountingGraphCard";
@@ -571,7 +571,7 @@ export class AonDesktop extends AonElement {
 		// ------------------------------------------------
 		// Payroll
 		// ------------------------------------------------
-		if (this.getDur().isPayrollManager()) {
+		if (this.getDur().isPayrollManager() && this.isBeta()  ) {
 			let contractGraphCard = new AonCard();
 			let payrollGraphCard = new AonCard();
 			payrollGraphCard.classList.add(CSS.AON_DASHBOARD_CARD);
