94a604a96f850c2d529305ff811659d9c4068ff8
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-06-06 13:35:44 +0200

    Only for try & setup  Selenium Grid [skip ci]

--- aon-selenium/src/test/java/solutions/aon/selenium/AbstractTestCase.java
+++ aon-selenium/src/test/java/solutions/aon/selenium/AbstractTestCase.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AbstractTestCase {
 
+
 	protected static String getUrl() {
 		return System.getProperty("url", "http://127.0.0.1:8080/app");
 	}
@@ -28,6 +29,10 @@ public class AbstractTestCase {
 
 	protected static String getPassword() {
 		return System.getProperty("password", "123456");
+	}
+
+	protected static String getSelenium() {
+		return System.getProperty("selenium", "http://selenium:4444");
 	}
 
 	protected static WebDriver newChromeDriver() {
@@ -64,7 +69,7 @@ public class AbstractTestCase {
 		
 		ChromeOptions chromeOptions = new ChromeOptions();
 		
-		RemoteWebDriver driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(), chromeOptions);
+		RemoteWebDriver driver = new RemoteWebDriver(new URI(getSelenium()).toURL(), chromeOptions);
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	    
