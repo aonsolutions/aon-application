6e1d82200fb6dd8d536a69dcc9ac61cf2037632c
EukeCastellano <ecastellano@aonsolutions.es>
2025-06-10 18:49:05 +0200

    Impersonate user GWT widget & faces user screen.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/console/ConsoleDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/console/ConsoleDAO.java
@@ -346,6 +346,19 @@ public class ConsoleDAO {
 		
 	}
 
+	public static boolean enableRemoteAccess(AONContext ctx, Integer domainId) {
+		if (!isRemoteAccessEnabled(ctx, domainId)) {
+			int count = ctx.getDslContext()
+				.insertInto(APP_PARAM)
+				.set(APP_PARAM.DOMAIN, domainId)
+				.set(APP_PARAM.NAME, AppParam.AON_SUPPORT_ENABLED.toString())
+				.set(APP_PARAM.VALUE, String.valueOf(new Date().getTime()))
+				.execute();
+			ctx.log().info("Remote Access Change: ON " + domainId + "(" + count + " rows)");
+		}
+		return true;
+	}
+
 	@SuppressWarnings("unchecked")
 	private static TableField<?, Integer> getPkField(String tableName) {
 		return (TableField<?, Integer>) AON_MASTER.getTable(tableName).getPrimaryKey().getFields().get(0);
