669ab49f82e9e8a9a6b7c563b559bd89467aafa8
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-06-11 16:21:14 +0200

    Protect against no/undefined 'aon_domain_name'. [skip ci]

--- aon-web-aio/src/main/webapp/components/AonElement.js
+++ aon-web-aio/src/main/webapp/components/AonElement.js
@@ -189,7 +189,7 @@ export class AonElement extends HTMLElement{
   
   isAyudaTorInfoautonomos(){
 	const href = localStorage.getItem("aon_domain_name")
-    return href?.includes('ayudat') || href.includes('infoautonomos')
+    return href?.includes('ayudat') || href?.includes('infoautonomos')
   }
   
   isAyudaT(){
