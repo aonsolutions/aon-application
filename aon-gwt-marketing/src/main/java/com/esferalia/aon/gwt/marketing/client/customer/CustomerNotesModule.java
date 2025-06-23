006215763667e4894f7349f757027fc9db63bc86
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-06-23 11:50:38 +0200

    CustomerNotesModule remove question marks for title

--- aon-gwt-marketing/src/main/java/com/esferalia/aon/gwt/marketing/client/customer/CustomerNotesModule.java
+++ aon-gwt-marketing/src/main/java/com/esferalia/aon/gwt/marketing/client/customer/CustomerNotesModule.java
@@ -431,8 +431,8 @@ public class CustomerNotesModule extends MainEntryPoint {
 	private void deleteNote(RegistryNote note, AonTableButton btn, Consumer<Void> deletion) {
 		AonCustomDeleteTooltip aonCustomDeleteTooltip = new AonCustomDeleteTooltip(
 				note.getNoteType().equals(NoteType.MESSAGE)
-	            ? "\u00bfEliminar nota?"
-	            : "\u00bfEliminar observaci\u00f3n?",
+	            ? "Eliminar nota"
+	            : "Eliminar observaci\u00f3n",
 	            note.getNoteType().equals(NoteType.MESSAGE)
 	            ? "\u00bfSeguro que quiere eliminar la nota?"
 	            : "\u00bfSeguro que quiere eliminar la observaci\u00f3n?",
