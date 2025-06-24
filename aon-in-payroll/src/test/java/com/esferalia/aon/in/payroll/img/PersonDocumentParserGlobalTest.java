bc9929171357e7b0c532071836313e619f3150ed
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-06-24 22:13:24 +0200

    Revert "personDocumentParserPDFOldFormatTest skip, not loading DNI name PDF"
    This reverts commit c62dcd5a20a00ee8e14c3fc059ffdb997957b58f.

--- aon-in-payroll/src/test/java/com/esferalia/aon/in/payroll/img/PersonDocumentParserGlobalTest.java
+++ aon-in-payroll/src/test/java/com/esferalia/aon/in/payroll/img/PersonDocumentParserGlobalTest.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
