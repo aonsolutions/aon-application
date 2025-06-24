403bff8cfcb3780d707b76f7d12b02189473c59a
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-06-24 23:39:07 +0200

    Fix error/none company logo.

--- aon-web-aio/src/main/webapp/modules/aon-parent.js
+++ aon-web-aio/src/main/webapp/modules/aon-parent.js
@@ -308,6 +308,7 @@ export class AonParent extends AonElement {
 		this.getWelcomeImage().then( img => welcomeImg.src = img );
 		this.getWelcomeMessage().then( msg  => welcomeImg.title = msg );
 		welcomeImg.classList.add(CSS.AON_WELCOME_LOGO);
+		welcomeImg.onerror = () => 	welcomeImg.style.display = 'none'; // Hide image if it fails to load		
 		welcomeDiv.appendChild(welcomeImg);
 
 
