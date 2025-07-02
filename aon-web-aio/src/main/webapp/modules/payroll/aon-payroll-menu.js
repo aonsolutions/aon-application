228d6e738769d06ce6237853ba7e1e13e062a1bf
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-07-02 20:06:32 +0200

    Remove new view on payroll, fiscal & accounting menu if its domainManagement

--- aon-web-aio/src/main/webapp/modules/payroll/aon-payroll-menu.js
+++ aon-web-aio/src/main/webapp/modules/payroll/aon-payroll-menu.js
@@ -26,7 +26,7 @@ export class AonPayrollMenu extends AonSuiteMenu {
 	        this.initialize();
 	        this.build();
 	        this.setTitle("Opciones de laboral");
-			if (this.isBeta()) {
+			if (this.isBeta() && !this.isDomainManagementAvailable()) {
 			   	let newViewButton = new AonIconButton();
 			   	newViewButton.id = this.id + "NewViewBtn";
 				newViewButton.icon = "open_in_new";
