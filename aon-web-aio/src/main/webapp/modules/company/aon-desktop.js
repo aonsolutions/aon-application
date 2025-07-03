7f64ce5fd7bac64ca0f3716a092dbbdd1b2b093a
anderibz <aibanez@aonsolutions.es>
2025-07-03 20:17:23 +0200

    upload invoice file with activity. without beta.

--- aon-web-aio/src/main/webapp/modules/company/aon-desktop.js
+++ aon-web-aio/src/main/webapp/modules/company/aon-desktop.js
@@ -366,7 +366,7 @@ export class AonDesktop extends AonElement {
 	uploadInvoiceDesktop(input, files) {
 		getCompanyActivities({}).then(activities => {
 			let data = { uploaded: 0 };
-			if(this.isBeta() && activities.length > 1) {
+			if(activities.length > 1) {
 				let activity =  createSelect(this.ACTIVITY, MSG.ACTIVITY);
 				activity.default = true;
 				activity.setAlias("id", "description");
@@ -397,7 +397,7 @@ export class AonDesktop extends AonElement {
 				});
 				d.open();
 			} else {
-				if(this.isBeta() && activities.length > 0) {
+				if(activities.length > 0) {
 					data.activity = activities[0].id;
 				}
 
