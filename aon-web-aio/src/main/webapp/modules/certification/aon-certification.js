2bc78f3493bcf4695f5b80ccaffc08eb04d8acb4
joseluisjimenez <joseluisjimenez@ayudatpymes.com>
2025-07-29 15:25:56 +0200

    modificar vista de certificaciones cargando imagenes

--- aon-web-aio/src/main/webapp/modules/certification/aon-certification.js
+++ aon-web-aio/src/main/webapp/modules/certification/aon-certification.js
@@ -14,116 +14,17 @@ export class AonCertification extends AonElement {
   }
 
   build () {
-    /*
-    const container = document.createElement('div');
-    container.id    = "certifications";
-    container.innerHTML = `
- 
-            <div class="cert-card">
-                <h3> <img src="../../images/apps/aeat.png" class="logo"> AEAT</h3>
-                <ul>
-                    <li>Homologado para la DigitalizaciÃ³n Certificada de Facturas<br> NÂº HomologaciÃ³n: RGE405069592024</li>
-                    <li>Software integrado con Veri*Factu</li>
-                    <li>Registrado como Colaborador Social</li>
-                    <li>Comunic@ / AEAT:
-                        <ul>
-                            <li>ComunicaciÃ³n Online en tiempo real de modelos fiscales</li>
-                            <li>Consulta y descarga de modelos fiscales presentados</li>
-                        </ul>
-                    </li>
-                </ul>
-                <br>
-                <div class="link-inline">
-                    <a class="link" href="https://sede.agenciatributaria.gob.es/static_files/Sede/Procedimiento_ayuda/FZ01/RSoftHomologado_doc_base.pdf" target="_blank" rel="noopener noreferrer" >Ver software homologados</a>
-                    <span> (PÃ¡gina 11) </span>
-                </div>	
-            </div>
-
-            <div class="cert-card">
-                <div class="logo-row">
-                    <img src="../../images/apps/aeat.png" class="logo left">
-                    <img src="../../images/apps/verifactu.png" class="logo center">
-                    <img src="../../images/apps/atc.png" class="logo right">
-                </div>                    
-                <h4> Software Integrado con Veri*Factu</h4>
-                <p>
-                    Nuestro software cumple con todos los requisitos tÃ©cnicos y legales establecidos por la Agencia Tributaria. 
-                    Ayuda asesorÃ­as, Pymes y autÃ³nomos a adaptarse de manera sencilla al nuevo marco de facturaciÃ³n electrÃ³nica 
-                    con total seguridad y respaldo normativo.
-                </p>
-                <a class="link2" href="https://drive.google.com/file/d/1xObCAXybwhoOEa339DgFy9h3KL97CYJ5/view?usp=sharing" target="_blank" rel="noopener noreferrer" >Ver DeclaraciÃ³n Responsable</a>
-            </div>
-
-            <div class="cert-card">
-                <h3> <img src="../../images/apps/haciendasForales.png" class="logo"> Haciendas Forales</h3>
-                <p>
-                    SoluciÃ³n cloud homologada por el Gobierno Vasco como software garante del cumplimiento legal de âTicket BAIâ
-                    para las tres Haciendas Forales:
-                </p>
-                <ul>
-                    <li>Hacienda Foral de ÃLAVA/ARABA</li>
-                    <li>Hacienda Foral de BIZKAIA</li>
-                    <li>Hacienda Foral de GIPUZKOA</li>
-                </ul>
-                <br>
-                <a class="link2" href="https://www.batuz.eus/es/registro-de-software?q=aonSolutions" target="_blank" rel="noopener noreferrer" >Ver software registrados</a>
-            </div>
-
-            <div class="cert-card">
-                <h3> <img src="../../images/apps/atc.png" class="logo"> Agencia Tributaria Canaria</h3>
-                <ul>
-                    <li>Software con soporte del IGIC</li>
-                    <li>Software integrado con Veri*Factu</li>
-                    <li>Registrado como Colaborador Social</li>
-                </ul>
-            </div>
-
-            <div class="cert-card">
-                <h3> <img src="../../images/apps/tgss.png" class="logo"> TGSS</h3>
-                <ul>
-                    <li>Software de nÃ³minas registrado</li>
-                    <li>CÃ³digo Proveedor de nÃ³minas nÂº 558</li>
-                    <li>Comunic@ / TGSS:
-                        <ul>
-                            <li>ComunicaciÃ³n Online en tiempo real de ALTAS previas, mensajes AFI y FIE</li>
-                            <li>Consulta de Certificado de empresa, Vida Laboral, IDC y FIE</li>
-                            <li>SincronizaciÃ³n de datos entre TGSS y AON</li>
-                        </ul>
-                    </li>
-                </ul>
-                <br>
-                <div class="link-inline">
-                    <a class="link" href="https://www.seg-social.es/wps/wcm/connect/wss/7ada0194-1392-4dca-bc7f-e2496ec372b8/T86-Proveedor+de+n%C3%B3mina+2018-07.pdf?MOD=AJPERES&CVID=" target="_blank" rel="noopener noreferrer">Ver listado proveedores</a>
-                    <span>(PÃ¡gina 4)</span>
-                </div>				
-            </div>
-
-            <div class="cert-card">
-                <h3> <img src="../../images/apps/sepe.png" class="logo"> SEPE</h3>
-                <ul>
-                    <li>Comunic@ / TGSS:
-                        <ul>
-                            <li>ComunicaciÃ³n Online en tiempo real de Contrat@, Copia bÃ¡sica, prÃ³rrogas y Certific@2</li>
-                            <li>Consulta de Contratos y Copia bÃ¡sica</li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-    `;
-    //this.appendChild(container);
-*/
     //
     // AEAT
     const aeatCard     = new AonCard();
     aeatCard.id        = "aeat-card";
     aeatCard.title     = "AEAT";
-    aeatCard.className = "notification-card";
     this.appendChild(aeatCard);
     // Contenido
       // imagen
-      const img     = this.createElement(TAG.IMG);
-      img.className = "card-img";
-      aeatCard.setContent(img);
+      const imgAeat     = this.createElement(TAG.DIV);
+      imgAeat.className = "card-img-aeat";
+      aeatCard.setContent(imgAeat);
       // textos
       const optionsAeat = [
         "Homologado para la Digitalización Certificada de Facturas Nº Homologación: RGE405069592024",
@@ -155,9 +56,13 @@ export class AonCertification extends AonElement {
     const verifactuCard     = new AonCard();
     verifactuCard.id        = "verifactu-card";
     verifactuCard.title     = "Software Integrado con Veri*Factu";
-    verifactuCard.className = "notification-card";
     this.appendChild(verifactuCard);
     // Contenido
+      // imagen
+      const imgVeri = this.createElement(TAG.DIV);
+      imgVeri.className = "card-img-veri";
+      verifactuCard.setContent(imgVeri);
+      // textos
       const verifactuText       = this.createElement(TAG.P);
       verifactuText.textContent = `Nuestro software cumple con todos los requisitos técnicos y legales establecidos por la Agencia Tributaria. 
 Ayuda asesorías, Pymes y autónomos a adaptarse de manera sencilla al nuevo marco de facturación electrónica 
@@ -178,9 +83,13 @@ con total seguridad y respaldo normativo.`;
     const haciendasCard     = new AonCard();
     haciendasCard.id        = "haciendas-card";
     haciendasCard.title     = "Haciendas Forales";
-    haciendasCard.className = "notification-card";
     this.appendChild(haciendasCard);
     // Contenido
+      // imagen
+      const imgHaciendas = this.createElement(TAG.DIV);
+      imgHaciendas.className = "card-img-haciendas";
+      haciendasCard.setContent(imgHaciendas);
+      // textos
       const haciendasText       = this.createElement(TAG.P);
       haciendasText.textContent = `Solución cloud homologada por el Gobierno Vasco como software garante del cumplimiento legal de “Ticket BAI” para las tres Haciendas Forales:`;
 
@@ -206,10 +115,15 @@ con total seguridad y respaldo normativo.`;
     // AGENCIA TRIBUTARIA CANARIA
     const atcCard     = new AonCard();
     atcCard.id        = "atc-card";
+    atcCard.className = "no-space";
     atcCard.title     = "Agencia Tributaria Canaria";
-    atcCard.className = "notification-card";
     this.appendChild(atcCard);
     // Contenido
+      // imagen
+      const imgAtc = this.createElement(TAG.DIV);
+      imgAtc.className = "card-img-atc";
+      atcCard.setContent(imgAtc);
+      // textos
       const atcOptions = [
         "Software con soporte del IGIC",
         "Software integrado con Veri*Factu",
@@ -226,9 +140,13 @@ con total seguridad y respaldo normativo.`;
     const tgssCard     = new AonCard();
     tgssCard.id        = "tgss-card";
     tgssCard.title     = "TGSS";
-    tgssCard.className = "notification-card";
     this.appendChild(tgssCard);
     // Contenido
+      // imagen
+      const imgTgss = this.createElement(TAG.DIV);
+      imgTgss.className = "card-img-tgss";
+      tgssCard.setContent(imgTgss);
+      // textos
       const tgssOptions = [
         "Software de nóminas registrado",
         "Código Proveedor de nóminas nº 558",
@@ -259,9 +177,14 @@ con total seguridad y respaldo normativo.`;
     const sepeCard     = new AonCard();
     sepeCard.id        = "sepe-card";
     sepeCard.title     = "SEPE";
-    sepeCard.className = "notification-card";
+    sepeCard.className = "no-space";
     this.appendChild(sepeCard);
     // Contenido
+      // imagen
+      const imgSepe = this.createElement(TAG.DIV);
+      imgSepe.className = "card-img-sepe";
+      sepeCard.setContent(imgSepe);
+      // textos
       const sepeOptions = [
         {
           title   : "Comunic@ / TGSS:",
