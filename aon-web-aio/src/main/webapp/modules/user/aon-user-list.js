d3c8275ae36d495529754b9188e91a3c264bde6f
anderibz <aibanez@aonsolutions.es>
2025-10-14 02:51:10 +0200

    only in beta or ayudat.

--- aon-web-aio/src/main/webapp/modules/user/aon-user-list.js
+++ aon-web-aio/src/main/webapp/modules/user/aon-user-list.js
@@ -187,7 +187,7 @@ export class AonUserList extends AonElement {
 		setFilter(this.filter);
 		getUserRoles({user: user.id}, this.sessionData).then(roles => {
 			user.roles = roles;
-			let aonUser = new AonNewUser();
+			let aonUser = this.isBeta() || this.isAyudaTorInfoautonomos() ? new AonNewUser() : new AonUser();
 			aonUser.sessionData = this.sessionData;
 			aonUser.parent = this.parent;
 			aonUser.id = 'aonUser-' + user.id;
