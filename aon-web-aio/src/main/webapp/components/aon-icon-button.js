f8a746d9f8e698337730e232e63417e4d3f80d95
Guillermo Jesús Bernal Pérez <guillermobernal@ayudatpymes.es>
2025-10-17 10:08:09 +0200

    fix messenger config inputs events

--- aon-web-aio/src/main/webapp/components/aon-icon-button.js
+++ aon-web-aio/src/main/webapp/components/aon-icon-button.js
@@ -156,6 +156,7 @@ export class AonIconButton extends AonElement {
     let background = this.hasAttribute("background") ? this.getAttribute("background")  : "transparent";
 
     this.getButton().className = "aonIconButton";
+	this.getButton().type = "button";
     this.getButton().style.color = this.getAttribute("color") ? this.getAttribute("color") : "#5f6368";
     this.getButton().style.backgroundColor = background;
     if (this.hasAttribute(CONSTANT.TITLE)) {
