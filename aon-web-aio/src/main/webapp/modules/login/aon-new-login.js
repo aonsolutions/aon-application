8edf0f23ddbb0dcb94ff2057954963b6beeefa40
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-10-22 12:33:18 +0200

    Revert "Login using 'aon-jaas'"
    This reverts commit e1f4d93231c8de5af6bcef6a80a4854323d0b183.

--- aon-web-aio/src/main/webapp/modules/login/aon-new-login.js
+++ aon-web-aio/src/main/webapp/modules/login/aon-new-login.js
@@ -510,7 +510,7 @@ export class AonNewLogin extends AonElement {
     let loader = this.getElement("aonLoginLoader");
     loader.style.display = "";
     loader.start();
-    signin(data)
+    (this.useJaas()?signin:login)(data)
       .then(() => {
         // document.body.style.background = 'transparent';
         loader.stop();
@@ -614,6 +614,13 @@ export class AonNewLogin extends AonElement {
     });
   }
   
+	useJaas() {
+		const queryString = window.location.search;
+		const searchParams = new URLSearchParams(queryString);
+		return searchParams.get('jaas') != null;
+	
+	}
+	
 	isConsole(company) {
 		return company.type == 'ADMIN' && company.id === 0;
 	}
