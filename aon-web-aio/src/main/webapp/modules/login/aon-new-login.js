c1a34a8a7906af1bf93c29dd2cab42a20aff055f
Sergio Valdepeñas <svaldepenas@aonsolutions.es>
2025-12-19 12:45:49 +0100

    Future CSS

--- aon-web-aio/src/main/webapp/modules/login/aon-new-login.js
+++ aon-web-aio/src/main/webapp/modules/login/aon-new-login.js
@@ -88,17 +88,17 @@ export class AonNewLogin extends AonElement {
     loginContent.style.height = "100%";
     loginContent.style.justifyContent = "center";
     
+    let loginDivForm = this.createElement(TAG.DIV);
+    loginDivForm.id = "loginDivForm";
+    loginDivForm.className = CSS.AON_LOGIN_FORM;
+    loginContent.appendChild(loginDivForm);
+    
     let welcomeImg = this.createElement(TAG.IMG);
 	welcomeImg.onerror = () => 	welcomeImg.style.display = 'none'; // Hide image if it fails to load
 	this.getWelcomeImage().then( img => welcomeImg.src = img );
 	this.getWelcomeMessage().then( msg  => welcomeImg.title = msg );
 	welcomeImg.classList.add(CSS.AON_WELCOME_LOGO_LOGIN);
-	loginContent.appendChild(welcomeImg);
-
-    let loginDivForm = this.createElement(TAG.DIV);
-    loginDivForm.id = "loginDivForm";
-    loginDivForm.className = CSS.AON_LOGIN_FORM;
-    loginContent.appendChild(loginDivForm);
+	loginDivForm.appendChild(welcomeImg);
 
     this.createTitlePanel(loginDivForm, MSG.LOGIN, MSG.LOGIN_SUBTITLE);
 
