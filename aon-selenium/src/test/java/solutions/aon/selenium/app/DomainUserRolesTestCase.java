f5324db7189af9ce076a530b232bb006ef3efb2e
Raúl Trepiana <rtrepiana@aonsolutions.es>
2025-12-28 21:14:07 +0100

    Test 'Índice Contenidos' for Portal users.

--- aon-selenium/src/test/java/solutions/aon/selenium/app/DomainUserRolesTestCase.java
+++ aon-selenium/src/test/java/solutions/aon/selenium/app/DomainUserRolesTestCase.java
@@ -10,6 +10,7 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -147,6 +148,41 @@ public class DomainUserRolesTestCase extends AppBaseTestCase {
 		}
 	}
 	
+	@Test
+	public void testPortalUserMain() throws MalformedURLException, URISyntaxException {
+		String url = System.getProperty("integration.test.env.app.url",
+				"http://general-payroll-test.aonsolutions.org:8080/?jass");
+		String email = System.getProperty("integration.test.env.app.auth", "portal@general-payroll-test.aonsolutions.org");
+		String password = System.getProperty("integration.test.env.app.password", "portal");
+
+		WebDriver webDriver = null;
+		try {
+			webDriver = newWebDriver();
+
+			login(webDriver, url, email, password);
+
+			WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("aonHeaderHelp"))).click();
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("contentIndexHelp"))).click();
+
+			WebElement aonJsfAppFrame = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("aonJsfAppFrame")));
+			webDriver.switchTo().frame(aonJsfAppFrame);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("aonContent:driveContentForm")));
+			
+			Assert.assertEquals(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".breadcrumb span.link"))).getText(), "Portal");
+			
+			
+
+		} catch (Exception e) {
+			throw new AssertionError("Error during test execution: " + e.getMessage(), e);
+		} finally {
+			if (webDriver != null) {
+				webDriver.close();
+				webDriver.quit();
+			}
+		}
+	}
 	
 	
 
