8735407c22a236c893fe716f4dcef85ebe2be5e1
anderibz <aibanez@aonsolutions.es>
2025-12-29 13:53:15 +0100

    change fix.

--- aon-web-aio/src/main/webapp/modules/company/aon-desktop.js
+++ aon-web-aio/src/main/webapp/modules/company/aon-desktop.js
@@ -224,8 +224,8 @@ export class AonDesktop extends AonElement {
 			if(showError) this.showMessageError("Es obligatorio rellenar todos los datos de la persona física.");
 			return false;
 		} 
-		if((icc.isCommonTerritory() || icc.isCanarias()) && !icc.isSii() && !icc.willBeSii() && !icc.isVerifactu() && !icc.willBeVerifactu() && !icc.isNoVerifactu() && !icc.willBeNoVerifactu()) {
-			if(showError) this.showMessageError("Es obligatorio selecionar Verifactu, No Verifactu o SII para empresas del territorio común.");
+		if(showError && (icc.isCommonTerritory() || icc.isCanarias()) && !icc.isSii() && !icc.willBeSii() && !icc.isVerifactu() && !icc.willBeVerifactu() && !icc.isNoVerifactu() && !icc.willBeNoVerifactu()) {
+			this.showMessageError("Es obligatorio selecionar Verifactu, No Verifactu o SII para empresas del territorio común.");
 			return false;
 		} 
 		if(!icc.getAdministration().isUnknown() && (icc.hasCommunication() || icc.willBeCommunication() || icc.isNoSif())) {
