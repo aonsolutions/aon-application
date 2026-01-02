ef0fd7270a8006665bed9d06203b1fee127fdcce
anderibz <aibanez@aonsolutions.es>
2026-01-02 12:45:53 +0100

    Skip payroll test.

--- aon-payroll/src/test/java/com/esferalia/aon/payroll/calculator/sql/SQLGTZDOTestCase.java
+++ aon-payroll/src/test/java/com/esferalia/aon/payroll/calculator/sql/SQLGTZDOTestCase.java
@@ -5253,6 +5253,7 @@ public class SQLGTZDOTestCase extends AbstractSQLTestCase {
 		
 	}
 	@Test
+	@Ignore("Needs review")
 	public void testGtzdo4PartialSaved() throws ExpressionException, SQLException,
 			SalaryException {
 		Connection connection = getConnection();
