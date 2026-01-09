51552a2a68dc17444364708fef9074d2e0237f52
EukeCastellano <ecastellano@aonsolutions.es>
2026-01-09 09:50:03 +0100

    InvoiceRegistryBox. Gwt StatFilter updates.

---
+++ aon-gwt-stat/src/main/java/com/esferalia/aon/gwt/stat/client/StatModuleOptions.java
@@ -0,0 +1,9 @@
+package com.esferalia.aon.gwt.stat.client;
+
+import com.esferalia.aon.gwt.common.client.widget.solutions.AonModuleOptions;
+
+public class StatModuleOptions extends AonModuleOptions<StatModuleOptions> {
+
+	private static final long serialVersionUID = 4952254781382054646L;
+	
+}
