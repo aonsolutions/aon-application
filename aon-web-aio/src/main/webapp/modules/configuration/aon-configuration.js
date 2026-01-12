9a83dbaaced4cb2a1ff34f61a97e7dccaf12d8fc
jmortega <jmortega@aonsolutions.es>
2026-01-12 14:48:21 +0100

    remove icon for client file

--- aon-web-aio/src/main/webapp/modules/configuration/aon-configuration.js
+++ aon-web-aio/src/main/webapp/modules/configuration/aon-configuration.js
@@ -112,7 +112,7 @@ export class AonConfiguration extends AonElement {
 				if (relationshipCompany.rrelationship) {
 					officeOptions.push({
 						name: MSG.CLIENT_FILE,
-						icon: MATERIAL_ICONS.CONTACTS,
+						// icon: MATERIAL_ICONS.CONTACTS,
 						fn: () => this.buildCustomerList(),
 					});
 					
