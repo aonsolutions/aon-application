028c596a768558b33900e20528f841854f5b91a9
Raúl Trepiana <rtrepiana@aonsolutions.es>
2026-02-17 19:35:47 +0100

    Customize openges

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,12 +19,8 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import net.aonsolutions.db.up2date.config.EnterpriseDataAddAudit;
+import net.aonsolutions.db.up2date.customize.Customize;
 import net.aonsolutions.db.up2date.payroll.DeleteUnusedPaymentConcepts;
-import net.aonsolutions.db.up2date.registry.SanitizeDomainRegistries;
-import net.aonsolutions.db.up2date.tgss.MEITrainingUpdate2026;
-import net.aonsolutions.db.up2date.tgss.SMI2026Update;
-import net.aonsolutions.db.up2date.timecontrol.CalendarPersonalDaysAlter;
 
 public class Up2Date {
 
@@ -593,7 +589,8 @@ public class Up2Date {
 			// MEITrainingUpdate2026.MEITRAININGUPDATE2026,
 			// AlterFsMod347Detail2025Bis.ALTER_FS_MOD_347_DETAIL_2025_BIS,
 			// SanitizeDomainRegistries.SANITIZE_SIG_AONSOLUTIONS_ORG,
-			SMI2026Update.SMI2026UPDATE,
+			// SMI2026Update.SMI2026UPDATE,
+			Customize.CUSTOMIZE_OPENGES,
 			
 			
 			// ----------------------------------------------------------------
