510c5069c730e368c02b7641f3768e1ce02444e3
Raúl Trepiana <rtrepiana@aonsolutions.es>
2026-02-18 18:23:13 +0100

    Support for CUSTOM THEME variables at CUSTOM VIEWs.

--- aon-web-aio/src/main/webapp/services/localStorageService.js
+++ aon-web-aio/src/main/webapp/services/localStorageService.js
@@ -59,11 +59,11 @@ export const remove = (item) => {
 }
 
 export const closeSession = () => {
-    let theme = getTheme();
-    let language = getLanguage();    
-    let topMenu = getTopMenu();
-    let leftMenu = getLeftMenu();
-    let portalChecked = getPortalChecked();
+    let theme = localStorage.getItem(THEME);
+    let topMenu = localStorage.getItem(TOP_MENU);
+    let leftMenu = localStorage.getItem(LEFT_MENU);
+	let language = localStorage.getItem(AON_LANGUAGE);    
+    let portalChecked = localStorage.getItem(PORTAL_CHECKED);
     
     localStorage.clear();
     
