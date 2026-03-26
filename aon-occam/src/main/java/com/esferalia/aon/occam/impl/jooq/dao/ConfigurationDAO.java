c8b62b04101a95ca01faa0dbbc5f94091c1f79e2
EukeCastellano <ecastellano@aonsolutions.es>
2026-03-26 09:11:43 +0100

    Plantilla asientos contabilizacion de nominas [RDM #58979]

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/ConfigurationDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/ConfigurationDAO.java
@@ -291,6 +291,7 @@ public class ConfigurationDAO {
 			.setDefaultSocialInsurance(getAccount(ctx, AppParam.ACC_DEFAULT_SOCIAL_INSURANCE_ACC ))
 			.setDefaultPendingSalary(getAccount(ctx, AppParam.ACC_DEFAULT_PENDING_SALARY_ACC ))
 			.setSalaryDedAdvPayment(getAccount(ctx, AppParam.ACC_SALARY_DED_ADV_PAYMENT_ACC))
+			.setSalaryDedInKind(getAccount(ctx, AppParam.ACC_SALARY_DED_IN_KIND_ACC))
 			.setSalaryOtherDeductions(getAccount(ctx, AppParam.ACC_SALARY_DED_OTHER_ACC))
 			.setSalaryDedSeize(getAccount(ctx, AppParam.ACC_SALARY_DED_SEIZE_ACC))
 			.setDefaultPrepayment(getAccount(ctx, AppParam.ACC_DEFAULT_PREPAYMENT_ACC))
