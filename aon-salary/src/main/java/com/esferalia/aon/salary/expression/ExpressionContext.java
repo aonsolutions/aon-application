3dbb3f11adfd8b601aeef4562cb13d2f6e3502de
Raúl Trepiana <rtrepiana@aonsolutions.es>
2026-03-30 13:39:19 +0200

    Trace Salary Payments

--- aon-salary/src/main/java/com/esferalia/aon/salary/expression/ExpressionContext.java
+++ aon-salary/src/main/java/com/esferalia/aon/salary/expression/ExpressionContext.java
@@ -640,6 +640,10 @@ public class ExpressionContext {
 	}
 
 	public List<ITimedVariable<?>> putVariable(Object name, ITimedVariable<?> timedVariable) {
+		if ( name.equals("SALARIO_BASE") ) {
+			System.out.println("ExpressionContext.setVariable " + name + " = " + timedVariable.getValue(timedVariable.getPeriod()) + " [" + timedVariable.getPeriod().getStart() + ", " + timedVariable.getPeriod().getEnd() + "]");
+			Thread.dumpStack();
+		}
 		return variables.put(name.toString(), timedVariable);
 	}
 
