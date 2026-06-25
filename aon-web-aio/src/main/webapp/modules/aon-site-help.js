54adc8c254bbe65b8f7a22ff704a3da5b293cd99
anderibz <aibanez@aonsolutions.es>
2026-06-25 11:14:46 +0200

    aon site help fix.

--- aon-web-aio/src/main/webapp/modules/aon-site-help.js
+++ aon-web-aio/src/main/webapp/modules/aon-site-help.js
@@ -38,6 +38,7 @@ export class AonSiteHelp extends HELP {
 		iframe.name = this.IFRAME;
 		iframe.style.border = 'none';
 		iframe.style.width = '100%';
+		iframe.style.display = 'none';
 		let top = this.getBoundingClientRect().top;
 		iframe.style.height = `calc(100vh - ${top + 10}px)`;
 		iframe.src = `/proxy?url=${encodeURIComponent(this.site)}`;
@@ -47,7 +48,7 @@ export class AonSiteHelp extends HELP {
 		this.isLoaded().then(() => [...this.getIDocument().getElementsByTagName('header')].forEach( header => {
 			header.remove();
 			this.showIFrame();
-			this.shiowHomeButton();
+			this.showHomeButton();
 			this.getHomeButton().innerHTML = header.textContent; 
 		}));
 		
@@ -66,7 +67,7 @@ export class AonSiteHelp extends HELP {
 		iframe.style.display = 'block';
 	}
 	
-	shiowHomeButton() {
+	showHomeButton() {
 		let homeButton = this.getHomeButton();
 		homeButton.style.removeProperty('display');
 	}
@@ -111,16 +112,13 @@ export class AonSiteHelp extends HELP {
 	
 	isLoaded() {
 		let iframe = this.getIFrame();
-		let idocument = iframe.document || iframe.contentDocument || iframe.contentWindow?.document;
 
 		return new Promise((resolve, reject) => {
-			if (idocument?.readyState === "complete") {
-				resolve();
-			} else if ( iframe.isConnected ) {
+			if ( iframe.isConnected ) {
 				iframe.addEventListener( EVENT.LOAD, resolve );
 			} else  {
 				this.addEventListener(EVENT.BUILD, () => {
-					iframe.addEventListener( EVENT.LOAD, resolve );						
+					iframe.addEventListener( EVENT.LOAD, resolve );
 				});
 			}
 		});
