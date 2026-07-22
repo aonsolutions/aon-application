01e38ae7c30310dd8a9e95f43658abd4625668c1
anderibz <aibanez@aonsolutions.es>
2026-07-22 09:35:41 +0200

    Delete domain app with app 19.

--- aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
+++ aon-db-up2date/src/main/java/net/aonsolutions/db/up2date/Up2Date.java
@@ -19,16 +19,13 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import net.aonsolutions.db.up2date.payroll.ContractCostExpressionText;
-import net.aonsolutions.db.up2date.payroll.ContractCostSeaExpressionFix;
 import net.aonsolutions.db.up2date.payroll.ContractForeignKeysAddIfNotExist;
 import net.aonsolutions.db.up2date.payroll.DeleteUnusedPaymentConcepts;
 import net.aonsolutions.db.up2date.payroll.ImporteHoraFormacion2026Update;
 import net.aonsolutions.db.up2date.scope.InsertDefaultScope;
 import net.aonsolutions.db.up2date.scope.UserScopeStartDateNotNull;
+import net.aonsolutions.db.up2date.security.DeleteDomainAppApp19;
 import net.aonsolutions.db.up2date.security.UserScopeAlterStartEndAuditory;
-import net.aonsolutions.db.up2date.tgss.AgriculturalBaseCgcJornadasTotalesFix;
-import net.aonsolutions.db.up2date.tgss.CgcEMinInsert;
 import net.aonsolutions.db.up2date.timecontrol.CalendarFixNulls;
 
 public class Up2Date {
@@ -667,6 +664,7 @@ public class Up2Date {
 			
 			// RecordDataAlterTable.RECORD_DATA_ALTER_TABLE
 			// DeleteDomainAppApp7.DELETE_DOMAIN_APP_APP7
+			DeleteDomainAppApp19.DELETE_DOMAIN_APP_APP19,
 
 			// InvoiceTrackingAlterTableJson.INVOICE_TRACKING_ALTER_TABLE_JSON,
 			
