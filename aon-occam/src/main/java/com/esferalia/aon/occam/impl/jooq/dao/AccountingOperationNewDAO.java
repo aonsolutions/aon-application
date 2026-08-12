f3048a53742b8d41f378a9f2324bf90ec420ca0d
EukeCastellano <ecastellano@aonsolutions.es>
2026-08-12 14:57:10 +0200

    Libros AEAT. Filtro annulled.

--- aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/AccountingOperationNewDAO.java
+++ aon-occam/src/main/java/com/esferalia/aon/occam/impl/jooq/dao/AccountingOperationNewDAO.java
@@ -552,6 +552,8 @@ public class AccountingOperationNewDAO {
 		// Condicion completa
 		Condition condition = INVOICE_DETAIL.DOMAIN.equal(ctx.getDomainId())
 							.and(INVOICE_DETAIL.PREPAYMENT.equal((byte) 0)) // Lineas que no son suplidos
+							.and(INVOICE.NUMBER.ge(0))   // No facturas proforma (factura proforma es la que su numero de factura es menor que cero)
+							.and(InvoiceDAO.NOT_ANNULLED) // No facturas anuladas
 							.and(condition1.or(condition2));
 		
 		if (params.getActivity() != null) {
@@ -600,6 +602,8 @@ public class AccountingOperationNewDAO {
 		// Condiciones completas 
 		Condition condition = FINANCE.DOMAIN.equal(ctx.getDomainId())
 								.and(INVOICE.VAT_ACCRUAL_PAYMENT.equal(TRUE_BYTE))
+								.and(INVOICE.NUMBER.ge(0))   // No facturas proforma (factura proforma es la que su numero de factura es menor que cero)
+								.and(InvoiceDAO.NOT_ANNULLED) // No facturas anuladas
 								.and(condition1.or(condition2));	
 		
 		if (params.getActivity() != null) {
