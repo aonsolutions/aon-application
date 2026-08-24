b9ac240aaba29ae8f01261a2054aef7adfff5dcf
anderibz <aibanez@aonsolutions.es>
2026-08-24 10:08:33 +0200

    SII Araba, gipuzkoa & bizkaia external source remove.

--- aon-external-sources/src/main/java/https/sii_araba_eus/documentos/respuestaconsultalr/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://sii.araba.eus/documentos/RespuestaConsultaLR.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package https.sii_araba_eus.documentos.respuestaconsultalr;
