cc9b150d38805e0df7ce05772758c00d9418808f
anderibz <aibanez@aonsolutions.es>
2026-08-26 13:17:07 +0200

    annul invoice in faces & gwt only with InvoiceCommunicator.

--- aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/InvoiceCommunicationServiceAsync.java
+++ aon-gwt-fiscal/src/main/java/com/esferalia/aon/gwt/fiscal/client/InvoiceCommunicationServiceAsync.java
@@ -1,7 +1,6 @@
 package com.esferalia.aon.gwt.fiscal.client;
 
 import java.util.LinkedList;
-import java.util.List;
 
 import com.esferalia.aon.gwt.fiscal.shared.invoice.ICResponse;
 import com.esferalia.aon.occam.api.model.InvestAsset;
@@ -11,7 +10,6 @@ import com.esferalia.aon.occam.api.model.fiscal.aeat.AEATParams;
 import com.esferalia.aon.occam.api.model.invoice.InvoiceCommunicationConfiguration;
 import com.esferalia.aon.occam.api.model.invoice.InvoiceCommunicationParams;
 import com.esferalia.aon.occam.api.model.invoice.InvoiceCommunicationTracking;
-import com.esferalia.aon.occam.api.model.invoice.InvoiceCommunicationType;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface InvoiceCommunicationServiceAsync {
@@ -36,7 +34,7 @@ public interface InvoiceCommunicationServiceAsync {
 	void refresh140(String domainName, int domainId, String user, Invoice invoice, AEATParams aeatParams, AsyncCallback<Boolean> callback);
 	void refresh240(String domainName, int domainId, String user, Invoice invoice, AEATParams aeatParams, AsyncCallback<Boolean> callback);
 
-	void cancel(String domainName, int domainId, String user, InvoiceCommunicationType type, Invoice invoice, AEATParams aeatParams, AsyncCallback<String> callback);
+	void cancel(String domainName, int domainId, String user, Invoice invoice, AEATParams aeatParams, AsyncCallback<String> callback);
 	
 	void getInvoiceCommunicationTrackings(String domainName, int domainId, String login, Integer invoice, AsyncCallback<LinkedList<InvoiceCommunicationTracking>> callback);
 	void getRequestUrl(String domainName, int domainId, String login, Integer dataResponse, AsyncCallback<String> callback);
