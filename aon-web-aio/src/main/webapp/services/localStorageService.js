6d5c9ea15ee0f51745e7b807114056fd16f18125
Raúl Trepiana <rtrepiana@aonsolutions.es>
2026-08-27 14:50:23 +0200

    Default Welcome Logo, Header & Login logo now the same ...

--- aon-web-aio/src/main/webapp/services/localStorageService.js
+++ aon-web-aio/src/main/webapp/services/localStorageService.js
@@ -34,6 +34,7 @@ export const DARK_THEME = '/css/theme/dark.css';
 export const DARK_BETA_THEME = '/css/theme/darkBeta.css';
 export const FUTURE_THEME = '/css/theme/future.css';
 export const CUSTOM_THEME = '/css/theme/customview.css';
+export const DEFAULT_THEME = '/css/theme/standard.css';
 
 export const IS_FUTURE_THEME = 'isFutureTheme';
 
